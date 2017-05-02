9e2a9fd30907002a6822985f03b2a1f8345c9a0e
Guillaume Polaert <guillaume@logmatic.io>
2017-05-02 12:06:30 +0200

    [PR #1] Removing the equals and hashcode methods

--- src/main/java/com/datadoghq/trace/impl/DDSpan.java
+++ src/main/java/com/datadoghq/trace/impl/DDSpan.java
@@ -317,28 +317,4 @@ public class DDSpan implements io.opentracing.Span {
         return context.toString();
     }
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((context == null) ? 0 : context.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        DDSpan other = (DDSpan) obj;
-        if (context == null) {
-            if (other.context != null)
-                return false;
-        } else if (!context.equals(other.context))
-            return false;
-        return true;
-    }
 }
