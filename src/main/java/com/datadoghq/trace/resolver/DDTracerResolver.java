5921c42e87a5f83dbc020cf4aa27b27fd4912f21
Guillaume Polaert <guillaume@logmatic.io>
2017-05-10 14:47:54 +0200

    By default, report to the DD Agent

--- src/main/java/com/datadoghq/trace/resolver/DDTracerResolver.java
+++ src/main/java/com/datadoghq/trace/resolver/DDTracerResolver.java
@@ -1,6 +1,8 @@
 package com.datadoghq.trace.resolver;
 
 import com.datadoghq.trace.DDTracer;
+import com.datadoghq.trace.sampling.AllSampler;
+import com.datadoghq.trace.writer.DDAgentWriter;
 import com.google.auto.service.AutoService;
 import io.opentracing.Tracer;
 import io.opentracing.contrib.tracerresolver.TracerResolver;
@@ -11,6 +13,7 @@ public class DDTracerResolver extends TracerResolver{
 
     @Override
     protected Tracer resolve() {
-        return new DDTracer();
+        // TODO find a way to close the reader ...
+        return new DDTracer(new DDAgentWriter(), new AllSampler());
     }
 }
