5e957558e39721bfc1b2d226b4140fc91d87582c
renaudboutet <support@logmatic.io>
2017-05-29 16:28:32 +0200

    Moving resolver in agent

--- dd-trace/src/main/java/com/datadoghq/trace/resolver/DDSpanDecoratorConfig.java
+++ dd-trace/src/main/java/com/datadoghq/trace/resolver/DDSpanDecoratorConfig.java
@@ -4,9 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
-/**
- * 
- */
 public class DDSpanDecoratorConfig {
 	
 	private String type;
