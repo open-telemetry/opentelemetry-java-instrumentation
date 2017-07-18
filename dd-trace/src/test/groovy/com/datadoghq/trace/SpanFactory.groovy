e8dbc34beac475fb419688ba6b0d8263f955f755
Tyler Benson <tyler@newrelic.com>
2017-07-18 10:41:02 -0700

    Apply consistent formatting to other files

--- dd-trace/src/test/groovy/com/datadoghq/trace/SpanFactory.groovy
+++ dd-trace/src/test/groovy/com/datadoghq/trace/SpanFactory.groovy
@@ -1,20 +1,20 @@
 package com.datadoghq.trace
 
 class SpanFactory {
-    static newSpanOf(long timestampMicro) {
-        def context = new DDSpanContext(
-                1L,
-                1L,
-                0L,
-                "fakeService",
-                "fakeOperation",
-                "fakeResource",
-                Collections.emptyMap(),
-                false,
-                "fakeType",
-                Collections.emptyMap(),
-                null,
-                null)
-        return new DDSpan(timestampMicro, context)
-    }
+  static newSpanOf(long timestampMicro) {
+    def context = new DDSpanContext(
+      1L,
+      1L,
+      0L,
+      "fakeService",
+      "fakeOperation",
+      "fakeResource",
+      Collections.emptyMap(),
+      false,
+      "fakeType",
+      Collections.emptyMap(),
+      null,
+      null)
+    return new DDSpan(timestampMicro, context)
+  }
 }
