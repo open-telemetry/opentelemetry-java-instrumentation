03e1ea9014b8f6a178c46b76e096eaa12dbb59a9
Guillaume Polaert <gpolaert@gmail.com>
2017-08-07 14:25:01 +0200

    Updating example

--- dd-trace/src/test/java/ExampleWithLoggingWriter.java
+++ dd-trace/src/test/java/ExampleWithLoggingWriter.java
@@ -1,15 +1,17 @@
 import com.datadoghq.trace.DDTracer;
+import com.datadoghq.trace.Service;
 import com.datadoghq.trace.sampling.AllSampler;
 import com.datadoghq.trace.writer.LoggingWriter;
 import io.opentracing.Span;
 
 public class ExampleWithLoggingWriter {
 
-  public static void main(String[] args) throws Exception {
+  public static void main(final String[] args) throws Exception {
 
-    DDTracer tracer = new DDTracer(new LoggingWriter(), new AllSampler());
+    final DDTracer tracer = new DDTracer(new LoggingWriter(), new AllSampler());
+    tracer.addServiceInfo(new Service("service-foo", "mongo", Service.AppType.DB));
 
-    Span parent =
+    final Span parent =
         tracer
             .buildSpan("hello-world")
             .withServiceName("service-name")
@@ -20,7 +22,7 @@ public class ExampleWithLoggingWriter {
 
     Thread.sleep(100);
 
-    Span child =
+    final Span child =
         tracer
             .buildSpan("hello-world")
             .asChildOf(parent)
