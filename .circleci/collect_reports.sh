dee08266aed0305ba930be41f0490d018b51479e
Tyler Benson <tyler@newrelic.com>
2018-04-18 13:14:03 +0800

    Restructure how libs, reports, and test results are collected

---
+++ .circleci/collect_reports.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Save all important reports into (project-root)/reports
+# This folder will be saved by circleci and available after test runs.
+
+set -e
+
+REPORTS_DIR=./reports
+mkdir -p $REPORTS_DIR >/dev/null 2>&1
+
+function save_reports () {
+    project_to_save=$1
+    echo "saving reports for $project_to_save"
+
+    report_path=$REPORTS_DIR/$project_to_save
+    mkdir -p $report_path
+    cp -r workspace/$project_to_save/build/reports/* $report_path/
+}
+
+shopt -s globstar
+
+for report_path in workspace/**/build/reports; do
+    report_path=${report_path//workspace\//}
+    report_path=${report_path//\/build\/reports/}
+    save_reports $report_path
+done
