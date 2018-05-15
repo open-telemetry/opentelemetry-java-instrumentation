71396aee01afcb1a2173ddefee28b4223d4d32a5
Gary Huang <gary.huang@COMP10325.local>
2018-05-15 16:12:31 -0400

    Moved JDBC integration tests to become unit tests

--- dd-java-agent/instrumentation/jdbc/src/test/groovy/JDBCInstrumentationTest.groovy
+++ dd-java-agent/instrumentation/jdbc/src/test/groovy/JDBCInstrumentationTest.groovy
@@ -1,10 +1,12 @@
 import datadog.trace.agent.test.AgentTestRunner
 import org.apache.derby.jdbc.EmbeddedDriver
 import org.h2.Driver
+import org.hsqldb.jdbc.JDBCDriver
 import spock.lang.Shared
 import spock.lang.Unroll
 
 import java.sql.Connection
+import java.sql.PreparedStatement
 import java.sql.ResultSet
 import java.sql.Statement
 
@@ -12,8 +14,260 @@ class JDBCInstrumentationTest extends AgentTestRunner {
   @Shared
   def dbName = "jdbcUnitTest"
 
+  @Shared
+  private Map<String, Connection> connections
+
+  def setupSpec() {
+    Connection h2Connection = new Driver().connect("jdbc:h2:mem:" + dbName, null)
+    Connection hsqlConnection = new JDBCDriver().connect("jdbc:hsqldb:mem:" + dbName, null)
+    Connection derbyConnection = new EmbeddedDriver().connect("jdbc:derby:memory:" + dbName + ";create=true", null)
+
+    connections = [
+      h2    : h2Connection,
+      derby : derbyConnection,
+      hsqldb: hsqlConnection,
+    ]
+  }
+
+  def cleanupSpec() {
+    connections.values().each {
+      it.close()
+    }
+  }
+
   @Unroll
-  def "Connection constructor throwing then generating correct spans after recovery using #db connection (prepare statement = #prepareStatement)"() {
+  def "basic statement on #driver generates spans"() {
+    setup:
+    Statement statement = connection.createStatement()
+    ResultSet resultSet = statement.executeQuery(query)
+
+    expect:
+    resultSet.next()
+    resultSet.getInt(1) == 3
+    TEST_WRITER.size() == 1
+
+    def trace = TEST_WRITER.firstTrace()
+    trace.size() == 1
+    def span = trace[0]
+
+    span.context().operationName == "${driver}.query"
+    span.serviceName == driver
+    span.resourceName == query
+    span.type == "sql"
+    !span.context().getErrorFlag()
+    span.context().parentId == 0
+
+
+    def tags = span.context().tags
+    tags["db.type"] == driver
+    tags["db.user"] == username
+    tags["span.kind"] == "client"
+    tags["component"] == "java-jdbc-statement"
+
+    tags["db.jdbc.url"].contains(driver)
+    tags["span.origin.type"] != null
+
+    tags["thread.name"] != null
+    tags["thread.id"] != null
+    tags.size() == username == null ? 7 : 8
+
+    cleanup:
+    statement.close()
+
+    where:
+    driver   | connection                | username | query
+    "h2"     | connections.get("h2")     | null     | "SELECT 3"
+    "derby"  | connections.get("derby")  | "APP"    | "SELECT 3 FROM SYSIBM.SYSDUMMY1"
+    "hsqldb" | connections.get("hsqldb") | "SA"     | "SELECT 3 FROM INFORMATION_SCHEMA.SYSTEM_USERS"
+  }
+
+  @Unroll
+  def "prepared statement execute on #driver generates a span"() {
+    setup:
+    PreparedStatement statement = connection.prepareStatement(query)
+    assert statement.execute()
+    ResultSet resultSet = statement.resultSet
+
+    expect:
+    resultSet.next()
+    resultSet.getInt(1) == 3
+    TEST_WRITER.size() == 1
+
+    def trace = TEST_WRITER.firstTrace()
+    trace.size() == 1
+    def span = trace[0]
+
+    span.context().operationName == "${driver}.query"
+    span.serviceName == driver
+    span.resourceName == query
+    span.type == "sql"
+    !span.context().getErrorFlag()
+    span.context().parentId == 0
+
+
+    def tags = span.context().tags
+    tags["db.type"] == driver
+    tags["db.user"] == username
+    tags["span.kind"] == "client"
+    tags["component"] == "java-jdbc-prepared_statement"
+
+    tags["db.jdbc.url"].contains(driver)
+    tags["span.origin.type"] != null
+
+    tags["thread.name"] != null
+    tags["thread.id"] != null
+    tags.size() == username == null ? 7 : 8
+
+    cleanup:
+    statement.close()
+
+    where:
+    driver   | connection                | username | query
+    "h2"     | connections.get("h2")     | null     | "SELECT 3"
+    "derby"  | connections.get("derby")  | "APP"    | "SELECT 3 FROM SYSIBM.SYSDUMMY1"
+    "hsqldb" | connections.get("hsqldb") | "SA"     | "SELECT 3 FROM INFORMATION_SCHEMA.SYSTEM_USERS"
+  }
+
+  @Unroll
+  def "prepared statement query on #driver generates a span"() {
+    setup:
+    PreparedStatement statement = connection.prepareStatement(query)
+    ResultSet resultSet = statement.executeQuery()
+
+    expect:
+    resultSet.next()
+    resultSet.getInt(1) == 3
+    TEST_WRITER.size() == 1
+
+    def trace = TEST_WRITER.firstTrace()
+    trace.size() == 1
+    def span = trace[0]
+
+    span.context().operationName == "${driver}.query"
+    span.serviceName == driver
+    span.resourceName == query
+    span.type == "sql"
+    !span.context().getErrorFlag()
+    span.context().parentId == 0
+
+
+    def tags = span.context().tags
+    tags["db.type"] == driver
+    tags["db.user"] == username
+    tags["span.kind"] == "client"
+    tags["component"] == "java-jdbc-prepared_statement"
+
+    tags["db.jdbc.url"].contains(driver)
+    tags["span.origin.type"] != null
+
+    tags["thread.name"] != null
+    tags["thread.id"] != null
+    tags.size() == username == null ? 7 : 8
+
+    cleanup:
+    statement.close()
+
+    where:
+    driver   | connection                | username | query
+    "h2"     | connections.get("h2")     | null     | "SELECT 3"
+    "derby"  | connections.get("derby")  | "APP"    | "SELECT 3 FROM SYSIBM.SYSDUMMY1"
+    "hsqldb" | connections.get("hsqldb") | "SA"     | "SELECT 3 FROM INFORMATION_SCHEMA.SYSTEM_USERS"
+  }
+
+  @Unroll
+  def "statement update on #driver generates a span"() {
+    setup:
+    Statement statement = connection.createStatement()
+    def sql = connection.nativeSQL(query)
+
+    expect:
+    !statement.execute(sql)
+    statement.updateCount == 0
+
+    TEST_WRITER.size() == 1
+
+    def trace = TEST_WRITER.firstTrace()
+    trace.size() == 1
+    def span = trace[0]
+
+    span.context().operationName == "${driver}.query"
+    span.serviceName == driver
+    span.resourceName == query
+    span.type == "sql"
+    !span.context().getErrorFlag()
+    span.context().parentId == 0
+
+
+    def tags = span.context().tags
+    tags["db.type"] == driver
+    tags["db.user"] == username
+    tags["span.kind"] == "client"
+    tags["component"] == "java-jdbc-statement"
+
+    tags["db.jdbc.url"].contains(driver)
+    tags["span.origin.type"] != null
+
+    tags["thread.name"] != null
+    tags["thread.id"] != null
+    tags.size() == username == null ? 7 : 8
+
+    cleanup:
+    statement.close()
+
+    where:
+    driver   | connection                | username | query
+    "h2"     | connections.get("h2")     | null     | "CREATE TABLE S_H2 (id INTEGER not NULL, PRIMARY KEY ( id ))"
+    "derby"  | connections.get("derby")  | "APP"    | "CREATE TABLE S_DERBY (id INTEGER not NULL, PRIMARY KEY ( id ))"
+    "hsqldb" | connections.get("hsqldb") | "SA"     | "CREATE TABLE PUBLIC.S_HSQLDB (id INTEGER not NULL, PRIMARY KEY ( id ))"
+  }
+
+  @Unroll
+  def "prepared statement update on #driver generates a span"() {
+    setup:
+    def sql = connection.nativeSQL(query)
+    PreparedStatement statement = connection.prepareStatement(sql)
+
+    expect:
+    statement.executeUpdate() == 0
+    TEST_WRITER.size() == 1
+
+    def trace = TEST_WRITER.firstTrace()
+    trace.size() == 1
+    def span = trace[0]
+
+    span.context().operationName == "${driver}.query"
+    span.serviceName == driver
+    span.resourceName == query
+    span.type == "sql"
+    !span.context().getErrorFlag()
+    span.context().parentId == 0
+
+
+    def tags = span.context().tags
+    tags["db.type"] == driver
+    tags["db.user"] == username
+    tags["span.kind"] == "client"
+    tags["component"] == "java-jdbc-prepared_statement"
+
+    tags["db.jdbc.url"].contains(driver)
+    tags["span.origin.type"] != null
+
+    tags["thread.name"] != null
+    tags["thread.id"] != null
+    tags.size() == username == null ? 7 : 8
+
+    cleanup:
+    statement.close()
+
+    where:
+    driver   | connection                | username | query
+    "h2"     | connections.get("h2")     | null     | "CREATE TABLE PS_H2 (id INTEGER not NULL, PRIMARY KEY ( id ))"
+    "derby"  | connections.get("derby")  | "APP"    | "CREATE TABLE PS_DERBY (id INTEGER not NULL, PRIMARY KEY ( id ))"
+    "hsqldb" | connections.get("hsqldb") | "SA"     | "CREATE TABLE PUBLIC.PS_HSQLDB (id INTEGER not NULL, PRIMARY KEY ( id ))"
+  }
+  
+  @Unroll
+  def "connection constructor throwing then generating correct spans after recovery using #driver connection (prepare statement = #prepareStatement)"() {
     setup:
     Connection connection = null
 
@@ -21,7 +275,7 @@ class JDBCInstrumentationTest extends AgentTestRunner {
     try {
       connection = new DummyThrowingConnection()
     } catch (Exception e) {
-      connection = driver.connect(url, null)
+      connection = driverClass.connect(url, null)
     }
 
     Statement statement = null
@@ -44,15 +298,15 @@ class JDBCInstrumentationTest extends AgentTestRunner {
 
     and:
     def span = trace[0]
-    span.context().operationName == "${db}.query"
-    span.serviceName == db
+    span.context().operationName == "${driver}.query"
+    span.serviceName == driver
     span.resourceName == query
     span.type == "sql"
     !span.context().getErrorFlag()
     span.context().parentId == 0
 
     def tags = span.context().tags
-    tags["db.type"] == db
+    tags["db.type"] == driver
     tags["db.user"] == user
     tags["span.kind"] == "client"
     if (prepareStatement) {
@@ -61,7 +315,7 @@ class JDBCInstrumentationTest extends AgentTestRunner {
       tags["component"] == "java-jdbc-statement"
     }
 
-    tags["db.jdbc.url"].contains(db)
+    tags["db.jdbc.url"].contains(driver)
     tags["span.origin.type"] != null
 
     tags["thread.name"] != null
@@ -77,7 +331,7 @@ class JDBCInstrumentationTest extends AgentTestRunner {
     }
 
     where:
-    prepareStatement | db       | driver               | url                                            | user  | query
+    prepareStatement | driver   | driverClass          | url                                            | user  | query
     true             | "h2"     | new Driver()         | "jdbc:h2:mem:" + dbName                        | null  | "SELECT 3;"
     true             | "derby"  | new EmbeddedDriver() | "jdbc:derby:memory:" + dbName + ";create=true" | "APP" | "SELECT 3 FROM SYSIBM.SYSDUMMY1"
     false            | "h2"     | new Driver()         | "jdbc:h2:mem:" + dbName                        | null  | "SELECT 3;"
