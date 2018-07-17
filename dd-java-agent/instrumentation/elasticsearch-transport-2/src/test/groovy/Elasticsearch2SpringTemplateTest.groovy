35b980ea89b00d4bc645975e35cb167566718f07
Nikolay Martynov <mar.kolya@gmail.com>
2018-07-17 18:02:16 -0400

    Fix ElasticSearch config for 2.x client

--- dd-java-agent/instrumentation/elasticsearch-transport-2/src/test/groovy/Elasticsearch2SpringTemplateTest.groovy
+++ dd-java-agent/instrumentation/elasticsearch-transport-2/src/test/groovy/Elasticsearch2SpringTemplateTest.groovy
@@ -49,7 +49,7 @@ class Elasticsearch2SpringTemplateTest extends AgentTestRunner {
     def settings = Settings.builder()
       .put("path.home", esWorkingDir.path)
       // Since we use listeners to close spans this should make our span closing deterministic which is good for tests
-      .put("thread_pool.listener.size", 1)
+      .put("threadpool.listener.size", 1)
       .put("http.port", httpPort)
       .put("transport.tcp.port", tcpPort)
       .build()
