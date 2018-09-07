c7e8755492e83acc73606be4da1230e37e631133
Gary Huang <garyhuang@hotmail.ca>
2018-09-07 15:59:25 -0400

    Muzzle servlet-2 Instrumentation
    Add muzzle block and remove class loader matcher method.

--- dd-java-agent/instrumentation/servlet-2/src/main/java/datadog/trace/instrumentation/servlet2/AbstractServlet2Instrumentation.java
+++ dd-java-agent/instrumentation/servlet-2/src/main/java/datadog/trace/instrumentation/servlet2/AbstractServlet2Instrumentation.java
@@ -1,21 +1,11 @@
 package datadog.trace.instrumentation.servlet2;
 
-import static datadog.trace.agent.tooling.ClassLoaderMatcher.classLoaderHasClasses;
-import static net.bytebuddy.matcher.ElementMatchers.not;
-
 import datadog.trace.agent.tooling.Instrumenter;
-import net.bytebuddy.matcher.ElementMatcher;
 
 public abstract class AbstractServlet2Instrumentation extends Instrumenter.Default {
 
   public AbstractServlet2Instrumentation() {
     super("servlet", "servlet-2");
-  }
-
-  @Override
-  public ElementMatcher<ClassLoader> classLoaderMatcher() {
-    return not(classLoaderHasClasses("javax.servlet.AsyncEvent", "javax.servlet.AsyncListener"))
-        .and(classLoaderHasClasses("javax.servlet.ServletContextEvent"));
   }
 
   @Override
