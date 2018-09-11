faad7824ed6038143ad26e3f84d6a54ac14ee23f
Andrew Kent <andrew.kent@datadoghq.com>
2018-09-11 11:51:39 -0700

    Elasticsearch 5.3.0 instrumentation

---
+++ dd-java-agent/instrumentation/elasticsearch-transport-5.3/src/test/groovy/Elasticsearch53NodeClientTest.groovy
@@ -0,0 +1,136 @@
+import com.anotherchrisberry.spock.extensions.retry.RetryOnFailure
+import datadog.trace.agent.test.AgentTestRunner
+import datadog.trace.agent.test.TestUtils
+import datadog.trace.api.DDSpanTypes
+import datadog.trace.api.DDTags
+import io.opentracing.tag.Tags
+import org.elasticsearch.action.admin.cluster.health.ClusterHealthRequest
+import org.elasticsearch.common.io.FileSystemUtils
+import org.elasticsearch.common.settings.Settings
+import org.elasticsearch.env.Environment
+import org.elasticsearch.index.IndexNotFoundException
+import org.elasticsearch.node.Node
+import org.elasticsearch.node.InternalSettingsPreparer
+import org.elasticsearch.transport.Netty3Plugin
+import spock.lang.Shared
+
+import static datadog.trace.agent.test.TestUtils.runUnderTrace
+import static datadog.trace.agent.test.asserts.ListWriterAssert.assertTraces
+import static org.elasticsearch.cluster.ClusterName.CLUSTER_NAME_SETTING
+
+@RetryOnFailure
+class Elasticsearch53NodeClientTest extends AgentTestRunner {
+  public static final long TIMEOUT = 10000; // 10 seconds
+
+  @Shared
+  int httpPort
+  @Shared
+  int tcpPort
+  @Shared
+  Node testNode
+  @Shared
+  File esWorkingDir
+
+  def client = testNode.client()
+
+  def setupSpec() {
+    httpPort = TestUtils.randomOpenPort()
+    tcpPort = TestUtils.randomOpenPort()
+
+    esWorkingDir = File.createTempDir("test-es-working-dir-", "")
+    esWorkingDir.deleteOnExit()
+    println "ES work dir: $esWorkingDir"
+
+    def settings = Settings.builder()
+      .put("path.home", esWorkingDir.path)
+    // Since we use listeners to close spans this should make our span closing deterministic which is good for tests
+      .put("thread_pool.listener.size", 1)
+      .put("http.port", httpPort)
+      .put("transport.tcp.port", tcpPort)
+      .put("transport.type", "netty3")
+      .put("http.type", "netty3")
+      .put(CLUSTER_NAME_SETTING.getKey(), "test-cluster")
+      .build()
+    testNode = new Node(new Environment(InternalSettingsPreparer.prepareSettings(settings)), [Netty3Plugin])
+    testNode.start()
+    runUnderTrace("setup") {
+      // this may potentially create multiple requests and therefore multiple spans, so we wrap this call
+      // into a top level trace to get exactly one trace in the result.
+      testNode.client().admin().cluster().prepareHealth().setWaitForYellowStatus().execute().actionGet(TIMEOUT)
+    }
+    TEST_WRITER.waitForTraces(1)
+  }
+
+  def cleanupSpec() {
+    testNode?.close()
+    if (esWorkingDir != null) {
+      FileSystemUtils.deleteSubDirectories(esWorkingDir.toPath())
+      esWorkingDir.delete()
+    }
+  }
+
+  def "test elasticsearch status"() {
+    setup:
+    def result = client.admin().cluster().health(new ClusterHealthRequest())
+
+    def status = result.get().status
+
+    expect:
+    status.name() == "GREEN"
+
+    assertTraces(TEST_WRITER, 1) {
+      trace(0, 1) {
+        span(0) {
+          serviceName "elasticsearch"
+          resourceName "ClusterHealthAction"
+          operationName "elasticsearch.query"
+          spanType DDSpanTypes.ELASTICSEARCH
+          tags {
+            "$Tags.COMPONENT.key" "elasticsearch-java"
+            "$Tags.SPAN_KIND.key" Tags.SPAN_KIND_CLIENT
+            "$DDTags.SPAN_TYPE" DDSpanTypes.ELASTICSEARCH
+            "elasticsearch.action" "ClusterHealthAction"
+            "elasticsearch.request" "ClusterHealthRequest"
+            defaultTags()
+          }
+        }
+      }
+    }
+  }
+
+  def "test elasticsearch error"() {
+    when:
+    client.prepareGet(indexName, indexType, id).get()
+
+    then:
+    thrown IndexNotFoundException
+
+    and:
+    assertTraces(TEST_WRITER, 1) {
+      trace(0, 1) {
+        span(0) {
+          serviceName "elasticsearch"
+          resourceName "GetAction"
+          operationName "elasticsearch.query"
+          spanType DDSpanTypes.ELASTICSEARCH
+          errored true
+          tags {
+            "$Tags.COMPONENT.key" "elasticsearch-java"
+            "$Tags.SPAN_KIND.key" Tags.SPAN_KIND_CLIENT
+            "$DDTags.SPAN_TYPE" DDSpanTypes.ELASTICSEARCH
+            "elasticsearch.action" "GetAction"
+            "elasticsearch.request" "GetRequest"
+            "elasticsearch.request.indices" indexName
+            errorTags IndexNotFoundException, "no such index"
+            defaultTags()
+          }
+        }
+      }
+    }
+
+    where:
+    indexName = "invalid-index"
+    indexType = "test-type"
+    id = "1"
+  }
+}
