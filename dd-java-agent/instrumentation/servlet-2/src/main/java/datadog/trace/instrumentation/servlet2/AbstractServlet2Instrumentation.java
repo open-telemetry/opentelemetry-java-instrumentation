2312c13aa2188451244b40a920f12f6f946cbf35
Gary <garyhuang@hotmail.ca>
2018-09-28 20:44:39 -0400

    restore classLoaderMatcher to make instrumentation safer

--- dd-java-agent/instrumentation/servlet-2/src/main/java/datadog/trace/instrumentation/servlet2/AbstractServlet2Instrumentation.java
+++ dd-java-agent/instrumentation/servlet-2/src/main/java/datadog/trace/instrumentation/servlet2/AbstractServlet2Instrumentation.java
@@ -1,11 +1,21 @@
 package datadog.trace.instrumentation.servlet2;
 
+import static datadog.trace.agent.tooling.ClassLoaderMatcher.classLoaderHasClasses;
+import static net.bytebuddy.matcher.ElementMatchers.not;
+
 import datadog.trace.agent.tooling.Instrumenter;
+import net.bytebuddy.matcher.ElementMatcher;
 
 public abstract class AbstractServlet2Instrumentation extends Instrumenter.Default {
 
   public AbstractServlet2Instrumentation() {
     super("servlet", "servlet-2");
+  }
+
+  // this is required to make sure servlet 2 instrumentation won't apply to servlet 3
+  @Override
+  public ElementMatcher<ClassLoader> classLoaderMatcher() {
+    return not(classLoaderHasClasses("javax.servlet.AsyncEvent", "javax.servlet.AsyncListener"));
   }
 
   @Override
