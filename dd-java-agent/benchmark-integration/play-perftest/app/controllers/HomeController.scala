513cccd6982d4bc913035afb42393611a41f755e
Gary Huang <garyhuang@hotmail.ca>
2018-12-07 12:31:10 -0500

    Change shell script to work with play
    - Added an argument to the script to take [play-zip|jar] to distinguish between the frameworks to test
    - Updated README
    - Cleanup

--- dd-java-agent/benchmark-integration/play-perftest/app/controllers/HomeController.scala
+++ dd-java-agent/benchmark-integration/play-perftest/app/controllers/HomeController.scala
@@ -9,25 +9,20 @@ import play.api.mvc._
 
 /**
   * This controller creates an `Action` to handle HTTP requests to the
-  * application's home page.
+  * application's work page which does busy wait to simulate some work
   */
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   /**
-    * Create an Action to render an HTML page.
-    *
-    * The configuration in the `routes` file means that this method
-    * will be called when the application receives a `GET` request with
-    * a path of `/`.
+    * Create an Action to perform busy wait
     */
   def doGet(workTimeMS: Option[Long], error: Option[String]) = Action { implicit request: Request[AnyContent] =>
     error match {
       case Some(x) => throw new RuntimeException("some sync error")
       case None => {
-        //val workVal: String = request.queryString("workTimeMS").headOption.getOrElse("")
         var workTime = workTimeMS.getOrElse(0l)
         scheduleWork(workTime)
-        Ok("DONE WORK IN " + workTime + " MS")
+        Ok("Did " + workTime + "ms of work.")
       }
     }
 
