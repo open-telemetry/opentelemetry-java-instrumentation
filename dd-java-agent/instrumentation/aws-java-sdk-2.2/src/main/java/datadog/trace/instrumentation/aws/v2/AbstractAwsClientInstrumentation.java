4fb94b83683556a0bcc3138cd3a3ab5c26a631fb
Nikolay Martynov <mar.kolya@gmail.com>
2019-01-25 12:53:00 -0500

    AWS2 SDK: Remove class loader matcher that is no longer needed

--- dd-java-agent/instrumentation/aws-java-sdk-2.2/src/main/java/datadog/trace/instrumentation/aws/v2/AbstractAwsClientInstrumentation.java
+++ dd-java-agent/instrumentation/aws-java-sdk-2.2/src/main/java/datadog/trace/instrumentation/aws/v2/AbstractAwsClientInstrumentation.java
@@ -1,25 +1,12 @@
 package datadog.trace.instrumentation.aws.v2;
 
-import static datadog.trace.agent.tooling.ClassLoaderMatcher.classLoaderHasClassWithField;
-import static datadog.trace.agent.tooling.ClassLoaderMatcher.classLoaderHasClasses;
-
 import datadog.trace.agent.tooling.Instrumenter;
-import net.bytebuddy.matcher.ElementMatcher;
 
 public abstract class AbstractAwsClientInstrumentation extends Instrumenter.Default {
   private static final String INSTRUMENTATION_NAME = "aws-sdk";
 
   public AbstractAwsClientInstrumentation() {
     super(INSTRUMENTATION_NAME);
-  }
-
-  // this is required to make sure we do not apply instrumentation to versions before 2.2.0
-  @Override
-  public ElementMatcher<ClassLoader> classLoaderMatcher() {
-    return classLoaderHasClasses("software.amazon.awssdk.http.SdkHttpRequest$Builder")
-        .and(
-            classLoaderHasClassWithField(
-                "software.amazon.awssdk.core.interceptor.SdkExecutionAttribute", "OPERATION_NAME"));
   }
 
   @Override
