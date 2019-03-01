e098e6bd64ed3c45d96790d4ba95d5a942bb0cae
Tyler Benson <tyler.benson@datadoghq.com>
2019-02-28 16:27:09 -0800

    Migrate kafka instrumentation to Decorator.

--- dd-java-agent/instrumentation/kafka-clients-0.11/src/main/java/datadog/trace/instrumentation/kafka_clients/TracingIterable.java
+++ dd-java-agent/instrumentation/kafka-clients-0.11/src/main/java/datadog/trace/instrumentation/kafka_clients/TracingIterable.java
@@ -1,42 +1,44 @@
 package datadog.trace.instrumentation.kafka_clients;
 
 import io.opentracing.Scope;
-import io.opentracing.Tracer;
+import io.opentracing.SpanContext;
+import io.opentracing.propagation.Format;
 import io.opentracing.util.GlobalTracer;
 import java.util.Iterator;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-public class TracingIterable<T> implements Iterable<T> {
-  private final Iterable<T> delegateIterable;
+public class TracingIterable implements Iterable<ConsumerRecord> {
+  private final Iterable<ConsumerRecord> delegateIterable;
   private final String operationName;
-  private final SpanBuilderDecorator<T> decorator;
+  private final KafkaDecorator decorator;
 
   public TracingIterable(
-      final Iterable<T> delegateIterable,
+      final Iterable<ConsumerRecord> delegateIterable,
       final String operationName,
-      final SpanBuilderDecorator<T> decorator) {
+      final KafkaDecorator decorator) {
     this.delegateIterable = delegateIterable;
     this.operationName = operationName;
     this.decorator = decorator;
   }
 
   @Override
-  public Iterator<T> iterator() {
-    return new TracingIterator<>(delegateIterable.iterator(), operationName, decorator);
+  public Iterator<ConsumerRecord> iterator() {
+    return new TracingIterator(delegateIterable.iterator(), operationName, decorator);
   }
 
   @Slf4j
-  public static class TracingIterator<T> implements Iterator<T> {
-    private final Iterator<T> delegateIterator;
+  public static class TracingIterator implements Iterator<ConsumerRecord> {
+    private final Iterator<ConsumerRecord> delegateIterator;
     private final String operationName;
-    private final SpanBuilderDecorator<T> decorator;
+    private final KafkaDecorator decorator;
 
     private Scope currentScope;
 
     public TracingIterator(
-        final Iterator<T> delegateIterator,
+        final Iterator<ConsumerRecord> delegateIterator,
         final String operationName,
-        final SpanBuilderDecorator<T> decorator) {
+        final KafkaDecorator decorator) {
       this.delegateIterator = delegateIterator;
       this.operationName = operationName;
       this.decorator = decorator;
@@ -52,20 +54,24 @@ public class TracingIterable<T> implements Iterable<T> {
     }
 
     @Override
-    public T next() {
+    public ConsumerRecord next() {
       if (currentScope != null) {
         // in case they didn't call hasNext()...
         currentScope.close();
         currentScope = null;
       }
 
-      final T next = delegateIterator.next();
+      final ConsumerRecord next = delegateIterator.next();
 
       try {
         if (next != null) {
-          final Tracer.SpanBuilder spanBuilder = GlobalTracer.get().buildSpan(operationName);
-          decorator.decorate(spanBuilder, next);
-          currentScope = spanBuilder.startActive(true);
+          final SpanContext spanContext =
+              GlobalTracer.get()
+                  .extract(Format.Builtin.TEXT_MAP, new TextMapExtractAdapter(next.headers()));
+          currentScope =
+              GlobalTracer.get().buildSpan(operationName).asChildOf(spanContext).startActive(true);
+          decorator.afterStart(currentScope);
+          decorator.onConsume(currentScope, next);
         }
       } catch (final Exception e) {
         log.debug("Error during decoration", e);
@@ -77,9 +83,5 @@ public class TracingIterable<T> implements Iterable<T> {
     public void remove() {
       delegateIterator.remove();
     }
-  }
-
-  public interface SpanBuilderDecorator<T> {
-    void decorate(Tracer.SpanBuilder spanBuilder, T context);
   }
 }
