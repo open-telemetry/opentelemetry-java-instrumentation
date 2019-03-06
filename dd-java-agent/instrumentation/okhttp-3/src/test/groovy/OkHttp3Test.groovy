c500d4b23615bd521d3653f8f7c1c5d63eaf77e7
Tyler Benson <tyler.benson@datadoghq.com>
2019-03-06 14:59:26 -0500

    Migrate OkHttp Instrumentation to Decorators

--- dd-java-agent/instrumentation/okhttp-3/src/test/groovy/OkHttp3Test.groovy
+++ dd-java-agent/instrumentation/okhttp-3/src/test/groovy/OkHttp3Test.groovy
@@ -1,31 +1,62 @@
 import datadog.trace.agent.test.AgentTestRunner
+import datadog.trace.agent.test.utils.PortUtils
 import datadog.trace.api.Config
 import datadog.trace.api.DDSpanTypes
 import io.opentracing.tag.Tags
+import okhttp3.Call
+import okhttp3.Callback
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.Response
+import spock.lang.AutoCleanup
+import spock.lang.Shared
+
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.atomic.AtomicReference
 
 import static datadog.trace.agent.test.server.http.TestHttpServer.httpServer
 import static datadog.trace.agent.test.utils.TraceUtils.withConfigOverride
+import static java.util.concurrent.TimeUnit.SECONDS
 
 class OkHttp3Test extends AgentTestRunner {
+  @AutoCleanup
+  @Shared
+  def server = httpServer {
+    handlers {
+      all {
+        response.status(200).send("pong")
+      }
+    }
+  }
+
+  def client = new OkHttpClient()
 
   def "sending a request creates spans and sends headers"() {
     setup:
-    def server = httpServer {
-      handlers {
-        all {
-          response.status(200).send("pong")
-        }
-      }
-    }
-    def client = new OkHttpClient()
     def request = new Request.Builder()
       .url("http://localhost:$server.address.port/ping")
       .build()
 
-    def response = withConfigOverride(Config.HTTP_CLIENT_HOST_SPLIT_BY_DOMAIN, "$renameService") {
-      client.newCall(request).execute()
+    Response response = withConfigOverride(Config.HTTP_CLIENT_HOST_SPLIT_BY_DOMAIN, "$renameService") {
+      if (!async) {
+        return client.newCall(request).execute()
+      }
+      
+      AtomicReference<Response> responseRef = new AtomicReference()
+      def latch = new CountDownLatch(1)
+
+      client.newCall(request).enqueue(new Callback() {
+        void onResponse(Call call, Response response) {
+          responseRef.set(response)
+          latch.countDown()
+        }
+
+        void onFailure(Call call, IOException e) {
+          latch.countDown()
+        }
+      })
+      latch.await(10, SECONDS)
+      return responseRef.get()
     }
 
     expect:
@@ -40,7 +71,8 @@ class OkHttp3Test extends AgentTestRunner {
           errored false
           parent()
           tags {
-            "component" "okhttp"
+            "$Tags.COMPONENT.key" "okhttp"
+            "$Tags.SPAN_KIND.key" Tags.SPAN_KIND_CLIENT
             defaultTags()
           }
         }
@@ -53,7 +85,7 @@ class OkHttp3Test extends AgentTestRunner {
           childOf(span(0))
           tags {
             defaultTags()
-            "component" "okhttp"
+            "$Tags.COMPONENT.key" "okhttp-network"
             "$Tags.SPAN_KIND.key" Tags.SPAN_KIND_CLIENT
             "$Tags.HTTP_METHOD.key" "GET"
             "$Tags.HTTP_STATUS.key" 200
@@ -69,8 +101,47 @@ class OkHttp3Test extends AgentTestRunner {
     server.lastRequest.headers.get("x-datadog-trace-id") == TEST_WRITER[0][1].traceId
     server.lastRequest.headers.get("x-datadog-parent-id") == TEST_WRITER[0][1].spanId
 
-    cleanup:
-    server.close()
+    where:
+    renameService | async
+    false         | false
+    true          | false
+    false         | true
+    true          | true
+  }
+
+  def "sending an invalid request creates an error span"() {
+    setup:
+    def unusablePort = PortUtils.UNUSABLE_PORT
+    def request = new Request.Builder()
+      .url("http://localhost:$unusablePort/ping")
+      .build()
+
+    when:
+    withConfigOverride(Config.HTTP_CLIENT_HOST_SPLIT_BY_DOMAIN, "$renameService") {
+      client.newCall(request).execute()
+    }
+
+    then:
+    thrown(ConnectException)
+
+    assertTraces(1) {
+      trace(0, 1) {
+        span(0) {
+          operationName "okhttp.http"
+          serviceName "okhttp"
+          resourceName "okhttp.http"
+          spanType DDSpanTypes.HTTP_CLIENT
+          errored true
+          parent()
+          tags {
+            "$Tags.COMPONENT.key" "okhttp"
+            "$Tags.SPAN_KIND.key" Tags.SPAN_KIND_CLIENT
+            errorTags(ConnectException, ~/Failed to connect to localhost\/[\d:\.]+:61/)
+            defaultTags()
+          }
+        }
+      }
+    }
 
     where:
     renameService << [false, true]
