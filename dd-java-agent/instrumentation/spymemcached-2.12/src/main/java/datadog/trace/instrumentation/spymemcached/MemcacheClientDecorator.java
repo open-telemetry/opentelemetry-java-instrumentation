bca5614508b05e1b178bd8c78c5c20475c1ec343
Tyler Benson <tyler.benson@datadoghq.com>
2019-03-12 11:18:45 -0700

    Migrate Memcache instrumentation to Decorator

---
+++ dd-java-agent/instrumentation/spymemcached-2.12/src/main/java/datadog/trace/instrumentation/spymemcached/MemcacheClientDecorator.java
@@ -0,0 +1,62 @@
+package datadog.trace.instrumentation.spymemcached;
+
+import datadog.trace.agent.decorator.DatabaseClientDecorator;
+import datadog.trace.api.DDSpanTypes;
+import datadog.trace.api.DDTags;
+import io.opentracing.Span;
+import net.spy.memcached.MemcachedConnection;
+
+public class MemcacheClientDecorator extends DatabaseClientDecorator<MemcachedConnection> {
+  public static final MemcacheClientDecorator DECORATE = new MemcacheClientDecorator();
+
+  @Override
+  protected String[] instrumentationNames() {
+    return new String[] {"spymemcached"};
+  }
+
+  @Override
+  protected String service() {
+    return "memcached";
+  }
+
+  @Override
+  protected String component() {
+    return "java-spymemcached";
+  }
+
+  @Override
+  protected String spanType() {
+    return DDSpanTypes.MEMCACHED;
+  }
+
+  @Override
+  protected String dbType() {
+    return "memcached";
+  }
+
+  @Override
+  protected String dbUser(final MemcachedConnection session) {
+    return null;
+  }
+
+  @Override
+  protected String dbInstance(final MemcachedConnection connection) {
+    return connection.connectionsStatus();
+  }
+
+  public Span onOperation(final Span span, final String methodName) {
+
+    final char[] chars =
+        methodName
+            .replaceFirst("^async", "")
+            // 'CAS' name is special, we have to lowercase whole name
+            .replaceFirst("^CAS", "cas")
+            .toCharArray();
+
+    // Lowercase first letter
+    chars[0] = Character.toLowerCase(chars[0]);
+
+    span.setTag(DDTags.RESOURCE_NAME, new String(chars));
+    return span;
+  }
+}
--- dd-java-agent/instrumentation/spymemcached-2.12/src/main/java/datadog/trace/instrumentation/spymemcached/OperationCompletionListener.java
+++ dd-java-agent/instrumentation/spymemcached-2.12/src/main/java/datadog/trace/instrumentation/spymemcached/OperationCompletionListener.java
@@ -1,24 +1,25 @@
 package datadog.trace.instrumentation.spymemcached;
 
 import io.opentracing.Span;
-import io.opentracing.Tracer;
 import java.util.concurrent.ExecutionException;
+import net.spy.memcached.MemcachedConnection;
 import net.spy.memcached.internal.OperationFuture;
 
 public class OperationCompletionListener
     extends CompletionListener<OperationFuture<? extends Object>>
     implements net.spy.memcached.internal.OperationCompletionListener {
-  public OperationCompletionListener(Tracer tracer, String methodName) {
-    super(tracer, methodName, true);
+  public OperationCompletionListener(
+      final MemcachedConnection connection, final String methodName) {
+    super(connection, methodName);
   }
 
   @Override
-  public void onComplete(OperationFuture<? extends Object> future) {
+  public void onComplete(final OperationFuture<? extends Object> future) {
     closeAsyncSpan(future);
   }
 
   @Override
-  protected void processResult(Span span, OperationFuture<? extends Object> future)
+  protected void processResult(final Span span, final OperationFuture<? extends Object> future)
       throws ExecutionException, InterruptedException {
     future.get();
   }
