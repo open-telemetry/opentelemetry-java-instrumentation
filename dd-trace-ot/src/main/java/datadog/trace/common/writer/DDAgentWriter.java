aantono/dd-trace-java

Change DDAgentWriter to use Disruptor
Serialize payloads as they arrive, send to agent when size exceeds 5MB.

If a flood of traces fills up the RingBuffer, we still count the traces that are discarded for stats reporting.

Do we want to allow configuration of the RingBuffer size?

dd-trace-ot/src/main/java/datadog/trace/common/writer/DDAgentWriter.java
@@ -2,122 +2,165 @@ package datadog.trace.common.writer;
 
 import static datadog.trace.api.Config.DEFAULT_AGENT_HOST;
 import static datadog.trace.api.Config.DEFAULT_TRACE_AGENT_PORT;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lmax.disruptor.EventFactory;
+import com.lmax.disruptor.EventHandler;
+import com.lmax.disruptor.EventTranslator;
+import com.lmax.disruptor.EventTranslatorOneArg;
+import com.lmax.disruptor.SleepingWaitStrategy;
+import com.lmax.disruptor.dsl.Disruptor;
+import com.lmax.disruptor.dsl.ProducerType;
 import datadog.opentracing.DDSpan;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.Phaser;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 import lombok.extern.slf4j.Slf4j;
+import org.msgpack.jackson.dataformat.MessagePackFactory;
 
 /**
- * This writer write provided traces to the a DD agent which is most of time located on the same
- * host.
+ * This writer buffers traces and sends them to the provided DDApi instance.
  *
- * <p>
- *
- * <p>It handles writes asynchronuously so the calling threads are automatically released. However,
- * if too much spans are collected the writers can reach a state where it is forced to drop incoming
- * spans.
+ * <p>Written traces are passed off to a disruptor so as to avoid blocking the application's thread.
+ * If a flood of traces arrives that exceeds the disruptor ring size, the traces exceeding the
+ * threshold will be counted and sampled.
  */
 @Slf4j
 public class DDAgentWriter implements Writer {
+  private static final int DISRUPTOR_BUFFER_SIZE = 8192;
+  private static final int FLUSH_PAYLOAD_BYTES = 5_000_000; // 5 MB
+  private static final int FLUSH_PAYLOAD_DELAY = 1; // 1/second
 
-  /** Maximum number of traces kept in memory */
-  static final int DEFAULT_MAX_TRACES = 7000;
+  private static final EventTranslatorOneArg<Event<List<DDSpan>>, List<DDSpan>> TRANSLATOR =
+      new EventTranslatorOneArg<Event<List<DDSpan>>, List<DDSpan>>() {
+        @Override
+        public void translateTo(
+            final Event<List<DDSpan>> event, final long sequence, final List<DDSpan> trace) {
+          final List<DDSpan> discarded = event.getAndSet(trace);
+          if (discarded != null) {
+            log.debug("Trace discarded due to spike in traces: {}", discarded);
+          }
+        }
+      };
+  private static final EventTranslator<Event<List<DDSpan>>> FLUSH_TRANSLATOR =
+      new EventTranslator<Event<List<DDSpan>>>() {
+        @Override
+        public void translateTo(final Event<List<DDSpan>> event, final long sequence) {
+          event.shouldFlush = true;
+        }
+      };
 
-  /** Timeout for the API in seconds */
-  static final long API_TIMEOUT_SECONDS = 1;
-
-  /** Flush interval for the API in seconds */
-  static final long FLUSH_TIME_SECONDS = 1;
-
-  private final ThreadFactory agentWriterThreadFactory =
+  private static final ThreadFactory DISRUPTOR_THREAD_FACTORY =
       new ThreadFactory() {
         @Override
         public Thread newThread(final Runnable r) {
-          final Thread thread = new Thread(r, "dd-agent-writer");
+          final Thread thread = new Thread(r, "dd-trace-disruptor");
+          thread.setDaemon(true);
+          return thread;
+        }
+      };
+  private static final ThreadFactory SCHEDULED_FLUSH_THREAD_FACTORY =
+      new ThreadFactory() {
+        @Override
+        public Thread newThread(final Runnable r) {
+          final Thread thread = new Thread(r, "dd-trace-writer");
           thread.setDaemon(true);
           return thread;
         }
       };
 
-  /** Scheduled thread pool, acting like a cron */
-  private final ScheduledExecutorService scheduledExecutor =
-      Executors.newScheduledThreadPool(1, agentWriterThreadFactory);
+  private static final ObjectMapper objectMapper = new ObjectMapper(new MessagePackFactory());
 
-  /** Effective thread pool, where real logic is done */
-  private final ExecutorService executor =
-      Executors.newSingleThreadExecutor(agentWriterThreadFactory);
-
-  /** The DD agent api */
   private final DDApi api;
-
-  /** In memory collection of traces waiting for departure */
-  private final WriterQueue<List<DDSpan>> traces;
-
-  private boolean queueFullReported = false;
+  private final int flushFrequencySeconds;
+  private final Disruptor<Event<List<DDSpan>>> disruptor;
+  private final EventHandler consumer = new TraceConsumer();
+  private final ScheduledExecutorService scheduledWriterExecutor;
+  private final AtomicInteger traceCount = new AtomicInteger(0);
+  private final AtomicReference<ScheduledFuture<?>> flushSchedule = new AtomicReference<>();
+  private final Phaser apiPhaser;
+  private volatile boolean running = false;
 
   public DDAgentWriter() {
     this(new DDApi(DEFAULT_AGENT_HOST, DEFAULT_TRACE_AGENT_PORT));
   }
 
   public DDAgentWriter(final DDApi api) {
-    this(api, new WriterQueue<List<DDSpan>>(DEFAULT_MAX_TRACES));
+    this(api, DISRUPTOR_BUFFER_SIZE, FLUSH_PAYLOAD_DELAY);
   }
 
-  public DDAgentWriter(final DDApi api, final WriterQueue<List<DDSpan>> queue) {
-    super();
-    this.api = api;
-    traces = queue;
-  }
-
-  /* (non-Javadoc)
-   * @see datadog.trace.Writer#write(java.util.List)
+  /**
+   * Used in the tests.
+   *
+   * @param api
+   * @param disruptorSize Rounded up to next power of 2
+   * @param flushFrequencySeconds value < 1 disables scheduled flushes
    */
+  private DDAgentWriter(final DDApi api, final int disruptorSize, final int flushFrequencySeconds) {
+    this.api = api;
+    this.flushFrequencySeconds = flushFrequencySeconds;
+    disruptor =
+        new Disruptor<>(
+            new DisruptorEventFactory<List<DDSpan>>(),
+            Math.max(2, Integer.highestOneBit(disruptorSize - 1) << 1), // Next power of 2
+            DISRUPTOR_THREAD_FACTORY,
+            ProducerType.MULTI,
+            new SleepingWaitStrategy(0, TimeUnit.MILLISECONDS.toNanos(5)));
+    disruptor.handleEventsWith(consumer);
+    scheduledWriterExecutor = Executors.newScheduledThreadPool(1, SCHEDULED_FLUSH_THREAD_FACTORY);
+    apiPhaser = new Phaser(); // Ensure API calls are completed when flushing
+    apiPhaser.register(); // Register for the executor thread.
+  }
+
   @Override
   public void write(final List<DDSpan> trace) {
-    final List<DDSpan> removed = traces.add(trace);
-    if (removed != null && !queueFullReported) {
-      log.debug("Queue is full, traces will be discarded, queue size: {}", DEFAULT_MAX_TRACES);
-      queueFullReported = true;
-      return;
+    // We can't add events after shutdown otherwise it will never complete shutting down.
+    if (running) {
+      final boolean published = disruptor.getRingBuffer().tryPublishEvent(TRANSLATOR, trace);
+      if (!published) {
+        // We're discarding the trace, but we still want to count it.
+        traceCount.incrementAndGet();
+      }
     }
-    queueFullReported = false;
   }
 
-  /* (non-Javadoc)
-   * @see Writer#start()
-   */
+  public int getTotalTraces() {
+    return traceCount.get();
+  }
+
+  public DDApi getApi() {
+    return api;
+  }
+
   @Override
   public void start() {
-    scheduledExecutor.scheduleAtFixedRate(
-        new TracesSendingTask(), 0, FLUSH_TIME_SECONDS, TimeUnit.SECONDS);
+    disruptor.start();
+    running = true;
+    scheduleFlush();
   }
 
-  /* (non-Javadoc)
-   * @see datadog.trace.Writer#close()
-   */
   @Override
   public void close() {
-    scheduledExecutor.shutdownNow();
-    executor.shutdownNow();
-    try {
-      scheduledExecutor.awaitTermination(500, TimeUnit.MILLISECONDS);
-    } catch (final InterruptedException e) {
-      log.info("Writer properly closed and async writer interrupted.");
-    }
+    running = false;
+    flush();
+    disruptor.shutdown();
+    scheduledWriterExecutor.shutdown();
+  }
 
-    try {
-      executor.awaitTermination(500, TimeUnit.MILLISECONDS);
-    } catch (final InterruptedException e) {
-      log.info("Writer properly closed and async writer interrupted.");
-    }
+  public void flush() {
+    apiPhaser.register();
+    disruptor.publishEvent(FLUSH_TRANSLATOR);
+    apiPhaser.arriveAndAwaitAdvance();
+    apiPhaser.arriveAndDeregister();
   }
 
   @Override
@@ -125,54 +168,104 @@ public class DDAgentWriter implements Writer {
     return "DDAgentWriter { api=" + api + " }";
   }
 
-  public DDApi getApi() {
-    return api;
+  private void scheduleFlush() {
+    if (flushFrequencySeconds > 0) {
+      final ScheduledFuture<?> previous =
+          flushSchedule.getAndSet(
+              scheduledWriterExecutor.schedule(flushTask, flushFrequencySeconds, SECONDS));
+      if (previous != null) {
+        previous.cancel(true);
+      }
+    }
   }
 
-  /** Infinite tasks blocking until some spans come in the blocking queue. */
-  class TracesSendingTask implements Runnable {
+  private final Runnable flushTask = new FlushTask();
 
+  private class FlushTask implements Runnable {
     @Override
     public void run() {
-      final Future<Long> future = executor.submit(new SendingTask());
-      try {
-        final long nbTraces = future.get(API_TIMEOUT_SECONDS, TimeUnit.SECONDS);
-        if (nbTraces > 0) {
-          log.debug("Successfully sent {} traces to the API", nbTraces);
+      // Don't call flush() because it would block the thread also used for sending the traces.
+      disruptor.publishEvent(FLUSH_TRANSLATOR);
+    }
+  }
+
+  /**
+   * This class is not threadsafe. It should only be used by a disruptor with a single consumer (the
+   * default executor).
+   */
+  private class TraceConsumer implements EventHandler<Event<List<DDSpan>>> {
+    private List<byte[]> serializedTraces = new ArrayList<>();
+    private int payloadSize = 0;
+
+    @Override
+    public void onEvent(
+        final Event<List<DDSpan>> event, final long sequence, final boolean endOfBatch) {
+      final List<DDSpan> trace = event.getAndSet(null);
+      if (trace != null) {
+        traceCount.incrementAndGet();
+        try {
+          final byte[] serializedTrace = objectMapper.writeValueAsBytes(trace);
+          payloadSize += serializedTrace.length;
+          serializedTraces.add(serializedTrace);
+        } catch (final JsonProcessingException e) {
+          log.warn("Error serializing trace", e);
         }
-      } catch (final TimeoutException e) {
-        log.debug("Timeout! Failed to send traces to the API: {}", e.getMessage());
-      } catch (final Throwable e) {
-        log.debug("Failed to send traces to the API: {}", e.getMessage());
+      }
+      if (event.shouldFlush || payloadSize >= FLUSH_PAYLOAD_BYTES) {
+        reportTraces();
+        event.shouldFlush = false;
       }
     }
 
-    class SendingTask implements Callable<Long> {
-
-      @Override
-      public Long call() throws Exception {
-        if (traces.isEmpty()) {
-          return 0L;
+    private void reportTraces() {
+      try {
+        if (serializedTraces.isEmpty()) {
+          apiPhaser.arrive(); // Allow flush to return
+          return;
         }
+        final List<byte[]> toSend = serializedTraces;
+        serializedTraces = new ArrayList<>(toSend.size());
+        // ^ Initialize with similar size to reduce arraycopy churn.
 
-        final List<List<DDSpan>> payload = traces.getAll();
+        final int totalSize = traceCount.getAndSet(0);
+        final int sizeInBytes = payloadSize;
 
-        if (log.isDebugEnabled()) {
-          int nbSpans = 0;
-          for (final List<?> trace : payload) {
-            nbSpans += trace.size();
-          }
-
-          log.debug("Sending {} traces ({} spans) to the API (async)", payload.size(), nbSpans);
-        }
-        final boolean isSent = api.sendTraces(payload);
-
-        if (!isSent) {
-          log.debug("Failing to send {} traces to the API", payload.size());
-          return 0L;
-        }
-        return (long) payload.size();
+        // Run the actual IO task on a different thread to avoid blocking the consumer.
+        scheduledWriterExecutor.schedule(
+            new Runnable() {
+              @Override
+              public void run() {
+                try {
+                  final boolean sent = api.sendSerializedTraces(totalSize, toSend);
+                  if (!sent) {
+                    log.debug(
+                        "Failed to send {} traces (representing {}) of size {} bytes to the API",
+                        toSend.size(),
+                        totalSize,
+                        sizeInBytes);
+                  }
+                } finally {
+                  apiPhaser.arrive(); // Flush completed.
+                }
+              }
+            },
+            0,
+            SECONDS);
+      } finally {
+        payloadSize = 0;
+        scheduleFlush();
       }
+    }
+  }
+
+  private static class Event<T> extends AtomicReference<T> {
+    private volatile boolean shouldFlush = false;
+  }
+
+  private static class DisruptorEventFactory<T> implements EventFactory<Event<T>> {
+    @Override
+    public Event<T> newInstance() {
+      return new Event<>();
     }
   }
 }

open-telemetry/opentelemetry-java-instrumentation

dd-trace-ot/src/main/java/datadog/trace/common/writer/DDAgentWriter.java
@@ -3,103 +3,154 @@ package datadog.trace.common.writer;
 import static datadog.trace.api.Config.DEFAULT_AGENT_HOST;
 import static datadog.trace.api.Config.DEFAULT_AGENT_UNIX_DOMAIN_SOCKET;
 import static datadog.trace.api.Config.DEFAULT_TRACE_AGENT_PORT;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.lmax.disruptor.EventFactory;
+import com.lmax.disruptor.EventHandler;
+import com.lmax.disruptor.EventTranslator;
+import com.lmax.disruptor.EventTranslatorOneArg;
+import com.lmax.disruptor.SleepingWaitStrategy;
+import com.lmax.disruptor.dsl.Disruptor;
+import com.lmax.disruptor.dsl.ProducerType;
 import datadog.opentracing.DDSpan;
+import datadog.trace.common.util.DaemonThreadFactory;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Phaser;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * This writer write provided traces to the a DD agent which is most of time located on the same
- * host.
+ * This writer buffers traces and sends them to the provided DDApi instance.
  *
- * <p>
- *
- * <p>It handles writes asynchronuously so the calling threads are automatically released. However,
- * if too much spans are collected the writers can reach a state where it is forced to drop incoming
- * spans.
+ * <p>Written traces are passed off to a disruptor so as to avoid blocking the application's thread.
+ * If a flood of traces arrives that exceeds the disruptor ring size, the traces exceeding the
+ * threshold will be counted and sampled.
  */
 @Slf4j
 public class DDAgentWriter implements Writer {
+  private static final int DISRUPTOR_BUFFER_SIZE = 8192;
+  private static final int FLUSH_PAYLOAD_BYTES = 5_000_000; // 5 MB
+  private static final int FLUSH_PAYLOAD_DELAY = 1; // 1/second
 
-  /** Maximum number of traces kept in memory */
-  static final int DEFAULT_MAX_TRACES = 7000;
-
-  /** Flush interval for the API in seconds */
-  static final long FLUSH_TIME_SECONDS = 1;
-
-  private final ThreadFactory agentWriterThreadFactory =
-      new ThreadFactory() {
+  private static final EventTranslatorOneArg<Event<List<DDSpan>>, List<DDSpan>> TRANSLATOR =
+      new EventTranslatorOneArg<Event<List<DDSpan>>, List<DDSpan>>() {
         @Override
-        public Thread newThread(final Runnable r) {
-          final Thread thread = new Thread(r, "dd-agent-writer");
-          thread.setDaemon(true);
-          return thread;
+        public void translateTo(
+            final Event<List<DDSpan>> event, final long sequence, final List<DDSpan> trace) {
+          event.data = trace;
+        }
+      };
+  private static final EventTranslator<Event<List<DDSpan>>> FLUSH_TRANSLATOR =
+      new EventTranslator<Event<List<DDSpan>>>() {
+        @Override
+        public void translateTo(final Event<List<DDSpan>> event, final long sequence) {
+          event.shouldFlush = true;
         }
       };
 
-  /** Scheduled thread pool, acting like a cron */
-  private final ScheduledExecutorService scheduledExecutor =
-      Executors.newScheduledThreadPool(1, agentWriterThreadFactory);
+  private static final ThreadFactory DISRUPTOR_THREAD_FACTORY =
+      new DaemonThreadFactory("dd-trace-disruptor");
+  private static final ThreadFactory SCHEDULED_FLUSH_THREAD_FACTORY =
+      new DaemonThreadFactory("dd-trace-writer");
 
-  /** The DD agent api */
   private final DDApi api;
-
-  /** In memory collection of traces waiting for departure */
-  private final WriterQueue<List<DDSpan>> traces;
-
-  private boolean queueFullReported = false;
+  private final int flushFrequencySeconds;
+  private final Disruptor<Event<List<DDSpan>>> disruptor;
+  private final ScheduledExecutorService scheduledWriterExecutor;
+  private final AtomicInteger traceCount = new AtomicInteger(0);
+  private final AtomicReference<ScheduledFuture<?>> flushSchedule = new AtomicReference<>();
+  private final Phaser apiPhaser;
+  private volatile boolean running = false;
 
   public DDAgentWriter() {
     this(new DDApi(DEFAULT_AGENT_HOST, DEFAULT_TRACE_AGENT_PORT, DEFAULT_AGENT_UNIX_DOMAIN_SOCKET));
   }
 
   public DDAgentWriter(final DDApi api) {
-    this(api, new WriterQueue<List<DDSpan>>(DEFAULT_MAX_TRACES));
+    this(api, DISRUPTOR_BUFFER_SIZE, FLUSH_PAYLOAD_DELAY);
   }
 
-  public DDAgentWriter(final DDApi api, final WriterQueue<List<DDSpan>> queue) {
-    super();
-    this.api = api;
-    traces = queue;
-  }
-
-  /* (non-Javadoc)
-   * @see datadog.trace.Writer#write(java.util.List)
+  /**
+   * Used in the tests.
+   *
+   * @param api
+   * @param disruptorSize Rounded up to next power of 2
+   * @param flushFrequencySeconds value < 1 disables scheduled flushes
    */
+  private DDAgentWriter(final DDApi api, final int disruptorSize, final int flushFrequencySeconds) {
+    this.api = api;
+    this.flushFrequencySeconds = flushFrequencySeconds;
+    disruptor =
+        new Disruptor<>(
+            new DisruptorEventFactory<List<DDSpan>>(),
+            Math.max(2, Integer.highestOneBit(disruptorSize - 1) << 1), // Next power of 2
+            DISRUPTOR_THREAD_FACTORY,
+            ProducerType.MULTI,
+            new SleepingWaitStrategy(0, TimeUnit.MILLISECONDS.toNanos(5)));
+    disruptor.handleEventsWith(new TraceConsumer());
+    scheduledWriterExecutor = Executors.newScheduledThreadPool(1, SCHEDULED_FLUSH_THREAD_FACTORY);
+    apiPhaser = new Phaser(); // Ensure API calls are completed when flushing
+    apiPhaser.register(); // Register on behalf of the scheduled executor thread.
+  }
+
   @Override
   public void write(final List<DDSpan> trace) {
-    final List<DDSpan> removed = traces.add(trace);
-    if (removed != null && !queueFullReported) {
-      log.debug("Queue is full, traces will be discarded, queue size: {}", DEFAULT_MAX_TRACES);
-      queueFullReported = true;
-      return;
+    // We can't add events after shutdown otherwise it will never complete shutting down.
+    if (running) {
+      final boolean published = disruptor.getRingBuffer().tryPublishEvent(TRANSLATOR, trace);
+      if (!published) {
+        // We're discarding the trace, but we still want to count it.
+        traceCount.incrementAndGet();
+        log.debug("Trace written to overfilled buffer. Counted but dropping trace: {}", trace);
+      }
+    } else {
+      log.debug("Trace written after shutdown. Ignoring trace: {}", trace);
     }
-    queueFullReported = false;
   }
 
-  /* (non-Javadoc)
-   * @see Writer#start()
-   */
+  @Override
+  public void incrementTraceCount() {
+    traceCount.incrementAndGet();
+  }
+
+  public DDApi getApi() {
+    return api;
+  }
+
   @Override
   public void start() {
-    scheduledExecutor.scheduleAtFixedRate(
-        new TracesSendingTask(), 0, FLUSH_TIME_SECONDS, TimeUnit.SECONDS);
+    disruptor.start();
+    running = true;
+    scheduleFlush();
   }
 
-  /* (non-Javadoc)
-   * @see datadog.trace.Writer#close()
-   */
   @Override
   public void close() {
-    scheduledExecutor.shutdownNow();
+    running = false;
+    flush();
+    disruptor.shutdown();
+    scheduledWriterExecutor.shutdown();
+  }
+
+  /** This method will block until the flush is complete. */
+  public void flush() {
+    log.info("Flushing any remaining traces.");
+    // Register with the phaser so we can block until the flush completion.
+    apiPhaser.register();
+    disruptor.publishEvent(FLUSH_TRANSLATOR);
     try {
-      scheduledExecutor.awaitTermination(500, TimeUnit.MILLISECONDS);
+      // Allow thread to be interrupted.
+      apiPhaser.awaitAdvanceInterruptibly(apiPhaser.arriveAndDeregister());
     } catch (final InterruptedException e) {
-      log.info("Writer properly closed and async writer interrupted.");
+      log.warn("Waiting for flush interrupted.", e);
     }
   }
 
@@ -108,37 +159,109 @@ public class DDAgentWriter implements Writer {
     return "DDAgentWriter { api=" + api + " }";
   }
 
-  public DDApi getApi() {
-    return api;
+  private void scheduleFlush() {
+    if (flushFrequencySeconds > 0) {
+      final ScheduledFuture<?> previous =
+          flushSchedule.getAndSet(
+              scheduledWriterExecutor.schedule(flushTask, flushFrequencySeconds, SECONDS));
+      if (previous != null) {
+        previous.cancel(true);
+      }
+    }
   }
 
-  class TracesSendingTask implements Runnable {
+  private final Runnable flushTask = new FlushTask();
+
+  private class FlushTask implements Runnable {
     @Override
     public void run() {
-      try {
-        if (traces.isEmpty()) {
-          return;
-        }
+      // Don't call flush() because it would block the thread also used for sending the traces.
+      disruptor.publishEvent(FLUSH_TRANSLATOR);
+    }
+  }
 
-        final List<List<DDSpan>> payload = traces.getAll();
+  /** This class is intentionally not threadsafe. */
+  private class TraceConsumer implements EventHandler<Event<List<DDSpan>>> {
+    private List<byte[]> serializedTraces = new ArrayList<>();
+    private int payloadSize = 0;
 
-        if (log.isDebugEnabled()) {
-          int nbSpans = 0;
-          for (final List<?> trace : payload) {
-            nbSpans += trace.size();
-          }
-          log.debug("Sending {} traces ({} spans) to the API (async)", payload.size(), nbSpans);
+    @Override
+    public void onEvent(
+        final Event<List<DDSpan>> event, final long sequence, final boolean endOfBatch) {
+      final List<DDSpan> trace = event.data;
+      event.data = null; // clear the event for reuse.
+      if (trace != null) {
+        traceCount.incrementAndGet();
+        try {
+          final byte[] serializedTrace = api.serializeTrace(trace);
+          payloadSize += serializedTrace.length;
+          serializedTraces.add(serializedTrace);
+        } catch (final JsonProcessingException e) {
+          log.warn("Error serializing trace", e);
+        } catch (final Throwable e) {
+          log.debug("Error while serializing trace", e);
         }
-
-        final boolean isSent = api.sendTraces(payload);
-        if (isSent) {
-          log.debug("Successfully sent {} traces to the API", payload.size());
-        } else {
-          log.debug("Failed to send {} traces to the API", payload.size());
-        }
-      } catch (final Throwable e) {
-        log.debug("Failed to send traces to the API: {}", e.getMessage());
       }
+      if (event.shouldFlush || payloadSize >= FLUSH_PAYLOAD_BYTES) {
+        reportTraces();
+        event.shouldFlush = false;
+      }
+    }
+
+    private void reportTraces() {
+      try {
+        if (serializedTraces.isEmpty()) {
+          apiPhaser.arrive(); // Allow flush to return
+          return;
+          // scheduleFlush called in finally block.
+        }
+        final List<byte[]> toSend = serializedTraces;
+        serializedTraces = new ArrayList<>(toSend.size());
+        // ^ Initialize with similar size to reduce arraycopy churn.
+
+        final int representativeCount = traceCount.getAndSet(0);
+        final int sizeInBytes = payloadSize;
+
+        // Run the actual IO task on a different thread to avoid blocking the consumer.
+        scheduledWriterExecutor.execute(
+            new Runnable() {
+              @Override
+              public void run() {
+                try {
+                  final boolean sent =
+                      api.sendSerializedTraces(representativeCount, sizeInBytes, toSend);
+                  if (sent) {
+                    log.debug("Successfully sent {} traces to the API", toSend.size());
+                  } else {
+                    log.debug(
+                        "Failed to send {} traces (representing {}) of size {} bytes to the API",
+                        toSend.size(),
+                        representativeCount,
+                        sizeInBytes);
+                  }
+                } catch (final Throwable e) {
+                  log.debug("Failed to send traces to the API: {}", e.getMessage());
+                } finally {
+                  apiPhaser.arrive(); // Flush completed.
+                }
+              }
+            });
+      } finally {
+        payloadSize = 0;
+        scheduleFlush();
+      }
+    }
+  }
+
+  private static class Event<T> {
+    private volatile boolean shouldFlush = false;
+    private volatile T data = null;
+  }
+
+  private static class DisruptorEventFactory<T> implements EventFactory<Event<T>> {
+    @Override
+    public Event<T> newInstance() {
+      return new Event<>();
     }
   }
 }