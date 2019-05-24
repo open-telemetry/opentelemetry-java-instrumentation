80a5cc60259955c56b83b3870d6d6c80f7d14421
Nikolay Martynov <mar.kolya@gmail.com>
2019-05-23 20:07:57 -0400

    Set kafka client service name to application default service name.
    So that spans inheriting from that client span have application
    service name rather than 'kafka'

--- dd-java-agent/instrumentation/kafka-clients-0.11/src/test/groovy/KafkaClientTest.groovy
+++ dd-java-agent/instrumentation/kafka-clients-0.11/src/test/groovy/KafkaClientTest.groovy
@@ -94,7 +94,7 @@ class KafkaClientTest extends AgentTestRunner {
       trace(1, 1) {
         // CONSUMER span 0
         span(0) {
-          serviceName "kafka"
+          serviceName "unnamed-java-app"
           operationName "kafka.consume"
           resourceName "Consume Topic $SHARED_TOPIC"
           spanType "queue"
