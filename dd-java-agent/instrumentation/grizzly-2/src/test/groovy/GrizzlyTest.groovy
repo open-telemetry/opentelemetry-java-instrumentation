f5b73260ed7c7d3e4c2ecf1d78aede8a675c81cf
Tyler Benson <tyler.benson@datadoghq.com>
2019-08-12 23:55:53 -0700

    Add instrumentation and tests for Grizzly 2.x Http Server

---
+++ dd-java-agent/instrumentation/grizzly-2/src/test/groovy/GrizzlyTest.groovy
@@ -0,0 +1,89 @@
+import datadog.trace.agent.test.base.HttpServerTest
+import datadog.trace.instrumentation.grizzly.GrizzlyDecorator
+import javax.ws.rs.GET
+import javax.ws.rs.NotFoundException
+import javax.ws.rs.Path
+import javax.ws.rs.core.Response
+import javax.ws.rs.ext.ExceptionMapper
+import org.glassfish.grizzly.http.server.HttpServer
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory
+import org.glassfish.jersey.server.ResourceConfig
+
+import static datadog.trace.agent.test.base.HttpServerTest.ServerEndpoint.ERROR
+import static datadog.trace.agent.test.base.HttpServerTest.ServerEndpoint.EXCEPTION
+import static datadog.trace.agent.test.base.HttpServerTest.ServerEndpoint.REDIRECT
+import static datadog.trace.agent.test.base.HttpServerTest.ServerEndpoint.SUCCESS
+
+class GrizzlyTest extends HttpServerTest<HttpServer, GrizzlyDecorator> {
+
+  @Override
+  HttpServer startServer(int port) {
+    ResourceConfig rc = new ResourceConfig()
+    rc.register(SimpleExceptionMapper)
+    rc.register(ServiceResource)
+    GrizzlyHttpServerFactory.createHttpServer(new URI("http://localhost:$port"), rc)
+  }
+
+  @Override
+  void stopServer(HttpServer server) {
+    server.stop()
+  }
+
+  @Override
+  GrizzlyDecorator decorator() {
+    return GrizzlyDecorator.DECORATE
+  }
+
+  @Override
+  String expectedOperationName() {
+    return "grizzly.request"
+  }
+
+  static class SimpleExceptionMapper implements ExceptionMapper<Throwable> {
+
+    @Override
+    Response toResponse(Throwable exception) {
+      if (exception instanceof NotFoundException) {
+        return exception.getResponse()
+      }
+      Response.status(500).entity(exception.message).build()
+    }
+  }
+
+  @Path("/")
+  static class ServiceResource {
+
+    @GET
+    @Path("success")
+    Response success() {
+      controller(SUCCESS) {
+        Response.status(SUCCESS.status).entity(SUCCESS.body).build()
+      }
+    }
+
+    @GET
+    @Path("redirect")
+    Response redirect() {
+      controller(REDIRECT) {
+        Response.status(REDIRECT.status).location(new URI(REDIRECT.body)).build()
+      }
+    }
+
+    @GET
+    @Path("error")
+    Response error() {
+      controller(ERROR) {
+        Response.status(ERROR.status).entity(ERROR.body).build()
+      }
+    }
+
+    @GET
+    @Path("exception")
+    Response exception() {
+      controller(EXCEPTION) {
+        throw new Exception(EXCEPTION.body)
+      }
+      return null
+    }
+  }
+}
