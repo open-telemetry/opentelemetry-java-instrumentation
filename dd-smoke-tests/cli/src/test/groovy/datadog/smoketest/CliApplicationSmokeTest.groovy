a05201130ba96436eea7b6e92a04459c41e8c435
Nikolay Martynov <mar.kolya@gmail.com>
2019-08-15 11:05:57 -0400

    Increase cli app test timeout
    This test sometimes fails on underpowered boxes.

--- dd-smoke-tests/cli/src/test/groovy/datadog/smoketest/CliApplicationSmokeTest.groovy
+++ dd-smoke-tests/cli/src/test/groovy/datadog/smoketest/CliApplicationSmokeTest.groovy
@@ -1,12 +1,11 @@
 package datadog.smoketest
 
-import spock.lang.Timeout
 
 import java.util.concurrent.TimeUnit
 
 class CliApplicationSmokeTest extends AbstractSmokeTest {
   // Estimate for the amount of time instrumentation, plus request, plus some extra
-  private static final int TIMEOUT_SECS = 15
+  private static final int TIMEOUT_SECS = 30
 
   @Override
   ProcessBuilder createProcessBuilder() {
@@ -20,9 +19,9 @@ class CliApplicationSmokeTest extends AbstractSmokeTest {
     processBuilder.directory(new File(buildDirectory))
   }
 
-  @Timeout(value = TIMEOUT_SECS, unit = TimeUnit.SECONDS)
   def "Cli application process ends before timeout"() {
     expect:
-    assert serverProcess.waitFor() == 0
+    serverProcess.waitFor(TIMEOUT_SECS, TimeUnit.SECONDS)
+    serverProcess.exitValue() == 0
   }
 }
