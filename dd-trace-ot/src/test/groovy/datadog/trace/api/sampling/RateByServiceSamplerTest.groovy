a88182ab3bb4b18cb92f5fb35be4a119f8b2e776
Tyler Benson <tyler.benson@datadoghq.com>
2019-09-10 12:56:42 -0700

    fix test

--- dd-trace-ot/src/test/groovy/datadog/trace/api/sampling/RateByServiceSamplerTest.groovy
+++ dd-trace-ot/src/test/groovy/datadog/trace/api/sampling/RateByServiceSamplerTest.groovy
@@ -21,7 +21,7 @@ class RateByServiceSamplerTest extends Specification {
     rate | expectedRate
     null | 1
     1    | 1
-    0    | 1
+    0    | 0.0
     -5   | 1
     5    | 1
     0.5  | 0.5
