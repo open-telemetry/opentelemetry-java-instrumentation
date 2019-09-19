43603e970c166072b0a6a11fc583466dbcda0edd
Nikolay Martynov <mar.kolya@gmail.com>
2019-09-19 08:27:56 -0400

    Disable Log4j1 tests for java versions without '.'
    Log4j1 seems to be broken on those.

--- dd-java-agent/instrumentation/log4j1/src/test/groovy/Log4j1MDCTest.groovy
+++ dd-java-agent/instrumentation/log4j1/src/test/groovy/Log4j1MDCTest.groovy
@@ -1,6 +1,13 @@
 import datadog.trace.agent.test.log.injection.LogContextInjectionTestBase
 import org.apache.log4j.MDC
+import spock.lang.Requires
 
+/**
+ It looks like log4j1 is broken for any java version that doesn't have '.' in version number
+ - it thinks it runs on ancient version. For example this happens for java13.
+ See {@link org.apache.log4j.helpers.Loader}.
+ */
+@Requires({ System.getProperty("java.version").contains(".") })
 class Log4j1MDCTest extends LogContextInjectionTestBase {
 
   @Override
