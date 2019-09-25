64a5fd61f928e6caa50c8fe0ec918e6d16e4e7ab
dougqh <dougqh@gmail.com>
2019-09-25 16:50:45 -0400

    Tinkering with version compatibility
    RepositoryProxyPostProcessor has different method sigs depending on the version of spring-data-commons.
    
    As stop gap implemented both signatures, but probably need to split spring-data support by version.

--- dd-java-agent/instrumentation/spring-data/src/main/java/datadog/trace/instrumentation/springdata/SpringRepositoryInstrumentation.java
+++ dd-java-agent/instrumentation/spring-data/src/main/java/datadog/trace/instrumentation/springdata/SpringRepositoryInstrumentation.java
@@ -21,6 +21,7 @@ import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.core.RepositoryInformation;
 import org.springframework.data.repository.core.support.RepositoryFactorySupport;
 import org.springframework.data.repository.core.support.RepositoryProxyPostProcessor;
 
@@ -66,8 +67,14 @@ public final class SpringRepositoryInstrumentation extends Instrumenter.Default 
     public static final RepositoryProxyPostProcessor INSTANCE =
         new InterceptingRepositoryProxyPostProcessor();
 
-    @Override
-    public void postProcess(ProxyFactory factory) {
+    // DQH - TODO: Implement old & new signatures of postProcess for versiosn compatibility
+    // Probably better to split by version
+
+    public void postProcess(final ProxyFactory factory) {
+      factory.addAdvice(0, RepositoryInterceptor.INSTANCE);
+    }
+
+    public void postProcess(final ProxyFactory factory, final RepositoryInformation repositoryInformation) {
       factory.addAdvice(0, RepositoryInterceptor.INSTANCE);
     }
   }
