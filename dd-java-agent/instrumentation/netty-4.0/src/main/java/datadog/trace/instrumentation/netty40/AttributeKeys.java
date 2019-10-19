bb500f9c549348417b172f60e7b8082aa2451206
Trask Stalnaker <trask.stalnaker@gmail.com>
2019-10-19 11:57:45 -0700

    Update netty-4.0 to new agent api

--- dd-java-agent/instrumentation/netty-4.0/src/main/java/datadog/trace/instrumentation/netty40/AttributeKeys.java
+++ dd-java-agent/instrumentation/netty-4.0/src/main/java/datadog/trace/instrumentation/netty40/AttributeKeys.java
@@ -2,10 +2,10 @@ package datadog.trace.instrumentation.netty40;
 
 import datadog.trace.bootstrap.WeakMap;
 import datadog.trace.context.TraceScope;
+import datadog.trace.instrumentation.api.AgentSpan;
 import datadog.trace.instrumentation.netty40.client.HttpClientTracingHandler;
 import datadog.trace.instrumentation.netty40.server.HttpServerTracingHandler;
 import io.netty.util.AttributeKey;
-import io.opentracing.Span;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -26,13 +26,13 @@ public class AttributeKeys {
       PARENT_CONNECT_CONTINUATION_ATTRIBUTE_KEY =
           attributeKey("datadog.trace.instrumentation.netty40.parent.connect.continuation");
 
-  public static final AttributeKey<Span> SERVER_ATTRIBUTE_KEY =
+  public static final AttributeKey<AgentSpan> SERVER_ATTRIBUTE_KEY =
       attributeKey(HttpServerTracingHandler.class.getName() + ".span");
 
-  public static final AttributeKey<Span> CLIENT_ATTRIBUTE_KEY =
+  public static final AttributeKey<AgentSpan> CLIENT_ATTRIBUTE_KEY =
       attributeKey(HttpClientTracingHandler.class.getName() + ".span");
 
-  public static final AttributeKey<Span> CLIENT_PARENT_ATTRIBUTE_KEY =
+  public static final AttributeKey<AgentSpan> CLIENT_PARENT_ATTRIBUTE_KEY =
       attributeKey(HttpClientTracingHandler.class.getName() + ".parent");
 
   /**
