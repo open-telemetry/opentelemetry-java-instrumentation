9ffde5d261a58cee35640711879723d00a1e0beb
Trask Stalnaker <trask.stalnaker@gmail.com>
2019-10-19 11:57:38 -0700

    Update apache-httpasyncclient-4 to new agent api

---
+++ dd-java-agent/instrumentation/apache-httpasyncclient-4/src/main/java/datadog/trace/instrumentation/apachehttpasyncclient/HttpHeadersInjectAdapter.java
@@ -0,0 +1,14 @@
+package datadog.trace.instrumentation.apachehttpasyncclient;
+
+import datadog.trace.instrumentation.api.AgentPropagation;
+import org.apache.http.HttpRequest;
+
+public class HttpHeadersInjectAdapter implements AgentPropagation.Setter<HttpRequest> {
+
+  public static final HttpHeadersInjectAdapter SETTER = new HttpHeadersInjectAdapter();
+
+  @Override
+  public void set(final HttpRequest carrier, final String key, final String value) {
+    carrier.setHeader(key, value);
+  }
+}
