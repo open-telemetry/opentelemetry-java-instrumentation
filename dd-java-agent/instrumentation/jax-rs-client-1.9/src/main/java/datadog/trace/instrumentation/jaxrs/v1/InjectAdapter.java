b1f48185fa7ddca81ad92b3c52b33c3bb15a5eec
Trask Stalnaker <trask.stalnaker@gmail.com>
2019-10-19 11:57:42 -0700

    Update jax-rs-client-1.9 to new agent api

--- dd-java-agent/instrumentation/jax-rs-client-1.9/src/main/java/datadog/trace/instrumentation/jaxrs/v1/InjectAdapter.java
+++ dd-java-agent/instrumentation/jax-rs-client-1.9/src/main/java/datadog/trace/instrumentation/jaxrs/v1/InjectAdapter.java
@@ -1,26 +1,15 @@
 package datadog.trace.instrumentation.jaxrs.v1;
 
-import io.opentracing.propagation.TextMap;
-import java.util.Iterator;
-import java.util.Map;
-import javax.ws.rs.core.MultivaluedMap;
+import com.sun.jersey.api.client.ClientRequest;
+import datadog.trace.instrumentation.api.AgentPropagation;
 
-public final class InjectAdapter implements TextMap {
-  private final MultivaluedMap<String, Object> map;
+public final class InjectAdapter implements AgentPropagation.Setter<ClientRequest> {
 
-  public InjectAdapter(final MultivaluedMap<String, Object> map) {
-    this.map = map;
-  }
+  public static final InjectAdapter SETTER = new InjectAdapter();
 
   @Override
-  public Iterator<Map.Entry<String, String>> iterator() {
-    throw new UnsupportedOperationException(
-        "InjectAdapter should only be used with Tracer.inject()");
-  }
-
-  @Override
-  public void put(final String key, final String value) {
+  public void set(final ClientRequest carrier, final String key, final String value) {
     // Don't allow duplicates.
-    map.putSingle(key, value);
+    carrier.getHeaders().putSingle(key, value);
   }
 }
