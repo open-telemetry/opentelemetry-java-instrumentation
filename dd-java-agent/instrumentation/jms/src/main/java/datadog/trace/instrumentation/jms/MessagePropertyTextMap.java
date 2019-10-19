6b23ffbae89cfb61e4abb9e55dc629f7c2b945a0
Trask Stalnaker <trask.stalnaker@gmail.com>
2019-10-19 11:57:44 -0700

    Update jms to new agent api

--- dd-java-agent/instrumentation/jms/src/main/java/datadog/trace/instrumentation/jms/MessagePropertyTextMap.java
+++ dd-java-agent/instrumentation/jms/src/main/java/datadog/trace/instrumentation/jms/MessagePropertyTextMap.java
@@ -1,49 +1,63 @@
 package datadog.trace.instrumentation.jms;
 
-import io.opentracing.propagation.TextMap;
+import datadog.trace.instrumentation.api.AgentPropagation;
+import java.util.ArrayList;
 import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.List;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class MessagePropertyTextMap implements TextMap {
+public class MessagePropertyTextMap
+    implements AgentPropagation.Getter<Message>, AgentPropagation.Setter<Message> {
+
+  public static final MessagePropertyTextMap GETTER = new MessagePropertyTextMap();
+  public static final MessagePropertyTextMap SETTER = new MessagePropertyTextMap();
+
   static final String DASH = "__dash__";
 
-  private final Message message;
-
-  public MessagePropertyTextMap(final Message message) {
-    this.message = message;
-  }
-
   @Override
-  public Iterator<Map.Entry<String, String>> iterator() {
-    final Map<String, String> map = new HashMap<>();
+  public Iterable<String> keys(final Message carrier) {
+    final List<String> keys = new ArrayList<>();
     try {
-      final Enumeration<?> enumeration = message.getPropertyNames();
+      final Enumeration<?> enumeration = carrier.getPropertyNames();
       if (enumeration != null) {
         while (enumeration.hasMoreElements()) {
           final String key = (String) enumeration.nextElement();
-          final Object value = message.getObjectProperty(key);
+          final Object value = carrier.getObjectProperty(key);
           if (value instanceof String) {
-            map.put(key.replace(DASH, "-"), (String) value);
+            keys.add(key.replace(DASH, "-"));
           }
         }
       }
     } catch (final JMSException e) {
       throw new RuntimeException(e);
     }
-    return map.entrySet().iterator();
+    return keys;
   }
 
   @Override
-  public void put(final String key, final String value) {
+  public String get(final Message carrier, final String key) {
+    final String propName = key.replace("-", DASH);
+    final Object value;
+    try {
+      value = carrier.getObjectProperty(propName);
+    } catch (final JMSException e) {
+      throw new RuntimeException(e);
+    }
+    if (value instanceof String) {
+      return (String) value;
+    } else {
+      return null;
+    }
+  }
+
+  @Override
+  public void set(final Message carrier, final String key, final String value) {
     final String propName = key.replace("-", DASH);
     try {
-      message.setStringProperty(propName, value);
+      carrier.setStringProperty(propName, value);
     } catch (final JMSException e) {
       if (log.isDebugEnabled()) {
         log.debug("Failure setting jms property: " + propName, e);
