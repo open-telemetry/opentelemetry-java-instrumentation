5f0a2ae6b41490ef5d4ce2f7d0582fe5c530b1ca
Trask Stalnaker <trask.stalnaker@gmail.com>
2019-10-19 11:57:44 -0700

    Update kafka-streams-0.11 to new agent api

--- dd-java-agent/instrumentation/kafka-streams-0.11/src/main/java/datadog/trace/instrumentation/kafka_streams/KafkaStreamsDecorator.java
+++ dd-java-agent/instrumentation/kafka-streams-0.11/src/main/java/datadog/trace/instrumentation/kafka_streams/KafkaStreamsDecorator.java
@@ -3,8 +3,7 @@ package datadog.trace.instrumentation.kafka_streams;
 import datadog.trace.agent.decorator.ClientDecorator;
 import datadog.trace.api.DDSpanTypes;
 import datadog.trace.api.DDTags;
-import io.opentracing.Scope;
-import io.opentracing.Span;
+import datadog.trace.instrumentation.api.AgentSpan;
 import io.opentracing.tag.Tags;
 import org.apache.kafka.streams.processor.internals.StampedRecord;
 
@@ -36,8 +35,7 @@ public class KafkaStreamsDecorator extends ClientDecorator {
     return DDSpanTypes.MESSAGE_CONSUMER;
   }
 
-  public void onConsume(final Scope scope, final StampedRecord record) {
-    final Span span = scope.span();
+  public void onConsume(final AgentSpan span, final StampedRecord record) {
     if (record != null) {
       final String topic = record.topic() == null ? "kafka" : record.topic();
       span.setTag(DDTags.RESOURCE_NAME, "Consume Topic " + topic);
