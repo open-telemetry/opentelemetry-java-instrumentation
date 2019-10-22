0107551c0eb92141a4536a60c97c74e71ac2d5ee
Trask Stalnaker <trask.stalnaker@gmail.com>
2019-10-21 18:36:14 -0700

    Remove outdated comment

--- dd-java-agent/instrumentation/apache-httpclient-4/src/main/java/datadog/trace/instrumentation/apachehttpclient/HttpHeadersInjectAdapter.java
+++ dd-java-agent/instrumentation/apache-httpclient-4/src/main/java/datadog/trace/instrumentation/apachehttpclient/HttpHeadersInjectAdapter.java
@@ -3,7 +3,6 @@ package datadog.trace.instrumentation.apachehttpclient;
 import datadog.trace.instrumentation.api.AgentPropagation;
 import org.apache.http.client.methods.HttpUriRequest;
 
-// not sure why, but gradle build fails without fully qualified HttpUriRequest class name
 public class HttpHeadersInjectAdapter implements AgentPropagation.Setter<HttpUriRequest> {
 
   public static final HttpHeadersInjectAdapter SETTER = new HttpHeadersInjectAdapter();
