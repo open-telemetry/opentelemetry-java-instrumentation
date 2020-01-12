edf4b5ee0e1e7236ab51229e6d2b68e62e2ff52b
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-01-11 19:34:03 -0800

    Improve comment

--- dd-java-agent/src/test/java/io/opentelemetry/test/ClassToInstrument.java
+++ dd-java-agent/src/test/java/io/opentelemetry/test/ClassToInstrument.java
@@ -3,7 +3,8 @@ package io.opentelemetry.test;
 import io.opentelemetry.auto.api.Trace;
 
 /**
- * Note: this has to stay in 'io.opentelemetry.test' package to be considered for instrumentation
+ * Note: this has to stay outside of 'io.opentelemetry.auto' package to be considered for
+ * instrumentation
  */
 public class ClassToInstrument {
   @Trace
--- dd-java-agent/src/test/java/io/opentelemetry/test/ClassToInstrumentChild.java
+++ dd-java-agent/src/test/java/io/opentelemetry/test/ClassToInstrumentChild.java
@@ -1,6 +1,7 @@
 package io.opentelemetry.test;
 
 /**
- * Note: this has to stay in 'io.opentelemetry.test' package to be considered for instrumentation
+ * Note: this has to stay outside of 'io.opentelemetry.auto' package to be considered for
+ * instrumentation
  */
 public class ClassToInstrumentChild extends ClassToInstrument {}
