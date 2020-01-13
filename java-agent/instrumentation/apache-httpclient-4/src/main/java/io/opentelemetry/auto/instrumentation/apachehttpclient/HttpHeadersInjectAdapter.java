7d277caf5d61f2b71eec4b8feb559fbc04fa227e
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-01-12 17:35:32 -0800

    Update apache-httpclient

--- java-agent/instrumentation/apache-httpclient-4/src/main/java/io/opentelemetry/auto/instrumentation/apachehttpclient/HttpHeadersInjectAdapter.java
+++ java-agent/instrumentation/apache-httpclient-4/src/main/java/io/opentelemetry/auto/instrumentation/apachehttpclient/HttpHeadersInjectAdapter.java
@@ -1,14 +1,14 @@
 package io.opentelemetry.auto.instrumentation.apachehttpclient;
 
-import io.opentelemetry.auto.instrumentation.api.AgentPropagation;
+import io.opentelemetry.context.propagation.HttpTextFormat;
 import org.apache.http.client.methods.HttpUriRequest;
 
-public class HttpHeadersInjectAdapter implements AgentPropagation.Setter<HttpUriRequest> {
+public class HttpHeadersInjectAdapter implements HttpTextFormat.Setter<HttpUriRequest> {
 
   public static final HttpHeadersInjectAdapter SETTER = new HttpHeadersInjectAdapter();
 
   @Override
-  public void set(final HttpUriRequest carrier, final String key, final String value) {
+  public void put(final HttpUriRequest carrier, final String key, final String value) {
     carrier.addHeader(key, value);
   }
 }
