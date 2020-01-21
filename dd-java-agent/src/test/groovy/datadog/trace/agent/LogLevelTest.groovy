f9e43516ad69d0443e1269e6ca6f9113f8c4bedc
Tyler Benson <tyler.benson@datadoghq.com>
2020-01-21 08:29:48 -0800

    Set timeouts on individual tests and increase overall test timeout.

--- dd-java-agent/src/test/groovy/datadog/trace/agent/LogLevelTest.groovy
+++ dd-java-agent/src/test/groovy/datadog/trace/agent/LogLevelTest.groovy
@@ -1,11 +1,11 @@
 package datadog.trace.agent
 
 import datadog.trace.agent.test.IntegrationTestUtils
-
 import jvmbootstraptest.LogLevelChecker
 import spock.lang.Specification
+import spock.lang.Timeout
 
-
+@Timeout(30)
 class LogLevelTest extends Specification {
 
 
@@ -17,23 +17,25 @@ class LogLevelTest extends Specification {
   def "dd.trace.debug false"() {
     expect:
     IntegrationTestUtils.runOnSeparateJvm(LogLevelChecker.getName()
-      , ["-Ddd.trace.debug=false","-Ddd.jmxfetch.enabled=false","-Ddd.trace.enabled=false"] as String[]
+      , ["-Ddd.trace.debug=false", "-Ddd.jmxfetch.enabled=false", "-Ddd.trace.enabled=false"] as String[]
       , "" as String[]
       , [:]
       , true) == 1
   }
+
   def "SLF4J DEBUG &&  dd.trace.debug is false"() {
     expect:
     IntegrationTestUtils.runOnSeparateJvm(LogLevelChecker.getName()
-      , ["-Ddd.trace.debug=false","-Ddatadog.slf4j.simpleLogger.defaultLogLevel=debug","-Ddd.jmxfetch.enabled=false","-Ddd.trace.enabled=false"] as String[]
+      , ["-Ddd.trace.debug=false", "-Ddatadog.slf4j.simpleLogger.defaultLogLevel=debug", "-Ddd.jmxfetch.enabled=false", "-Ddd.trace.enabled=false"] as String[]
       , "" as String[]
       , [:]
       , true) == 0
   }
+
   def "dd.trace.debug is false && DD_TRACE_DEBUG is true"() {
     expect:
     IntegrationTestUtils.runOnSeparateJvm(LogLevelChecker.getName()
-      , ["-Ddd.trace.debug=false","-Ddd.jmxfetch.enabled=false","-Ddd.trace.enabled=false"] as String[]
+      , ["-Ddd.trace.debug=false", "-Ddd.jmxfetch.enabled=false", "-Ddd.trace.enabled=false"] as String[]
       , "" as String[]
       , ["DD_TRACE_DEBUG": "true"]
       , true) == 1
@@ -42,7 +44,7 @@ class LogLevelTest extends Specification {
   def "dd.trace.debug is true"() {
     expect:
     IntegrationTestUtils.runOnSeparateJvm(LogLevelChecker.getName()
-      , ["-Ddd.trace.debug=true","-Ddd.jmxfetch.enabled=false","-Ddd.trace.enabled=false"] as String[]
+      , ["-Ddd.trace.debug=true", "-Ddd.jmxfetch.enabled=false", "-Ddd.trace.enabled=false"] as String[]
       , "" as String[]
       , [:]
       , true) == 0
@@ -52,7 +54,7 @@ class LogLevelTest extends Specification {
   def "DD_TRACE_DEBUG is true"() {
     expect:
     IntegrationTestUtils.runOnSeparateJvm(LogLevelChecker.getName()
-      , ["-Ddd.jmxfetch.enabled=false","-Ddd.trace.enabled=false"] as String[]
+      , ["-Ddd.jmxfetch.enabled=false", "-Ddd.trace.enabled=false"] as String[]
       , "" as String[]
       , ["DD_TRACE_DEBUG": "true"]
       , true) == 0
@@ -61,18 +63,17 @@ class LogLevelTest extends Specification {
   def "dd.trace.debug is true && DD_TRACE_DEBUG is false"() {
     expect:
     IntegrationTestUtils.runOnSeparateJvm(LogLevelChecker.getName()
-      , ["-Ddd.trace.debug=true","-Ddd.jmxfetch.enabled=false","-Ddd.trace.enabled=false"] as String[]
+      , ["-Ddd.trace.debug=true", "-Ddd.jmxfetch.enabled=false", "-Ddd.trace.enabled=false"] as String[]
       , "" as String[]
       , ["DD_TRACE_DEBUG": "false"]
       , true) == 0
   }
 
 
-
   def "SLF4J DEBUG && DD_TRACE_DEBUG is false"() {
     expect:
     IntegrationTestUtils.runOnSeparateJvm(LogLevelChecker.getName()
-      , ["-Ddatadog.slf4j.simpleLogger.defaultLogLevel=debug","-Ddd.jmxfetch.enabled=false","-Ddd.trace.enabled=false"] as String[]
+      , ["-Ddatadog.slf4j.simpleLogger.defaultLogLevel=debug", "-Ddd.jmxfetch.enabled=false", "-Ddd.trace.enabled=false"] as String[]
       , "" as String[]
       , ["DD_TRACE_DEBUG": "false"]
       , true) == 0
@@ -81,7 +82,7 @@ class LogLevelTest extends Specification {
   def "SLF4J INFO && DD_TRACE_DEBUG is true"() {
     expect:
     IntegrationTestUtils.runOnSeparateJvm(LogLevelChecker.getName()
-      , ["-Ddatadog.slf4j.simpleLogger.defaultLogLevel=info","-Ddd.jmxfetch.enabled=false","-Ddd.trace.enabled=false"] as String[]
+      , ["-Ddatadog.slf4j.simpleLogger.defaultLogLevel=info", "-Ddd.jmxfetch.enabled=false", "-Ddd.trace.enabled=false"] as String[]
       , "" as String[]
       , ["DD_TRACE_DEBUG": "true"]
       , true) == 1
