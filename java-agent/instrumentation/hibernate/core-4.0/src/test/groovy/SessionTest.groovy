ec148c78d13383b5da912e506935f926aa4e08f5
prydin <prydin@vmware.com>
2020-01-22 16:00:41 -0500

    Finished refactoring hibernate instrumentation

--- java-agent/instrumentation/hibernate/core-4.0/src/test/groovy/SessionTest.groovy
+++ java-agent/instrumentation/hibernate/core-4.0/src/test/groovy/SessionTest.groovy
@@ -1,14 +1,16 @@
 import io.opentelemetry.auto.api.MoreTags
 import io.opentelemetry.auto.api.SpanTypes
-import io.opentelemetry.auto.instrumentation.api.SpanScopePair
+import io.opentelemetry.auto.instrumentation.api.AgentScope
 import io.opentelemetry.auto.instrumentation.api.Tags
-import io.opentelemetry.trace.Span
 import org.hibernate.LockMode
 import org.hibernate.MappingException
 import org.hibernate.Query
 import org.hibernate.ReplicationMode
 import org.hibernate.Session
 import spock.lang.Shared
+
+import static io.opentelemetry.auto.instrumentation.api.AgentTracer.activateSpan
+import static io.opentelemetry.auto.instrumentation.api.AgentTracer.startSpan
 
 class SessionTest extends AbstractHibernateTest {
 
@@ -390,7 +392,7 @@ class SessionTest extends AbstractHibernateTest {
             "$MoreTags.SERVICE_NAME" "hibernate"
             "$MoreTags.SPAN_TYPE" SpanTypes.HIBERNATE
             "$Tags.COMPONENT" "java-hibernate"
-            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT "overlapping Sessions"
+            "$Tags.SPAN_KIND" Tags.SPAN_KIND_CLIENT
           }
         }
         span(1) {
@@ -444,8 +446,7 @@ class SessionTest extends AbstractHibernateTest {
   def "test hibernate overlapping Sessions"() {
     setup:
 
-    Span span = TEST_TRACER.spanBuilder("overlapping Sessions").startSpan()
-    SpanScopePair scope = new SpanScopePair(span, TEST_TRACER.withSpan(span))
+    AgentScope scope = activateSpan(startSpan("overlapping Sessions"), true)
 
     def session1 = sessionFactory.openSession()
     session1.beginTransaction()
@@ -463,8 +464,7 @@ class SessionTest extends AbstractHibernateTest {
     session1.close()
     session3.close()
 
-    span.end()
-    scope.getScope().close()
+    scope.close()
 
 
     expect:
