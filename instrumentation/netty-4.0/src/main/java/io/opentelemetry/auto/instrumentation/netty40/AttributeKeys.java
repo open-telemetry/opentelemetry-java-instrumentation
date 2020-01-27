c1d0437b7171b56d1144713ffb6f66e15ffdd790
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-01-26 22:50:29 -0800

    Convert Netty 4.0 to use OpenTelemetry API directly

--- instrumentation/netty-4.0/src/main/java/io/opentelemetry/auto/instrumentation/netty40/AttributeKeys.java
+++ instrumentation/netty-4.0/src/main/java/io/opentelemetry/auto/instrumentation/netty40/AttributeKeys.java
@@ -2,9 +2,9 @@ package io.opentelemetry.auto.instrumentation.netty40;
 
 import io.netty.util.AttributeKey;
 import io.opentelemetry.auto.bootstrap.WeakMap;
-import io.opentelemetry.auto.instrumentation.api.AgentSpan;
 import io.opentelemetry.auto.instrumentation.netty40.client.HttpClientTracingHandler;
 import io.opentelemetry.auto.instrumentation.netty40.server.HttpServerTracingHandler;
+import io.opentelemetry.trace.Span;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -21,16 +21,16 @@ public class AttributeKeys {
         }
       };
 
-  public static final AttributeKey<AgentSpan> PARENT_CONNECT_SPAN_ATTRIBUTE_KEY =
+  public static final AttributeKey<Span> PARENT_CONNECT_SPAN_ATTRIBUTE_KEY =
       attributeKey("io.opentelemetry.auto.instrumentation.netty40.parent.connect.span");
 
-  public static final AttributeKey<AgentSpan> SERVER_ATTRIBUTE_KEY =
+  public static final AttributeKey<Span> SERVER_ATTRIBUTE_KEY =
       attributeKey(HttpServerTracingHandler.class.getName() + ".span");
 
-  public static final AttributeKey<AgentSpan> CLIENT_ATTRIBUTE_KEY =
+  public static final AttributeKey<Span> CLIENT_ATTRIBUTE_KEY =
       attributeKey(HttpClientTracingHandler.class.getName() + ".span");
 
-  public static final AttributeKey<AgentSpan> CLIENT_PARENT_ATTRIBUTE_KEY =
+  public static final AttributeKey<Span> CLIENT_PARENT_ATTRIBUTE_KEY =
       attributeKey(HttpClientTracingHandler.class.getName() + ".parent");
 
   /**
--- instrumentation/netty-4.0/src/main/java/io/opentelemetry/auto/instrumentation/netty40/client/HttpClientResponseTracingHandler.java
+++ instrumentation/netty-4.0/src/main/java/io/opentelemetry/auto/instrumentation/netty40/client/HttpClientResponseTracingHandler.java
@@ -1,39 +1,39 @@
 package io.opentelemetry.auto.instrumentation.netty40.client;
 
-import static io.opentelemetry.auto.instrumentation.api.AgentTracer.activateSpan;
-import static io.opentelemetry.auto.instrumentation.api.AgentTracer.noopSpan;
 import static io.opentelemetry.auto.instrumentation.netty40.client.NettyHttpClientDecorator.DECORATE;
+import static io.opentelemetry.auto.instrumentation.netty40.client.NettyHttpClientDecorator.TRACER;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.http.HttpResponse;
 import io.netty.util.Attribute;
-import io.opentelemetry.auto.instrumentation.api.AgentScope;
-import io.opentelemetry.auto.instrumentation.api.AgentSpan;
 import io.opentelemetry.auto.instrumentation.netty40.AttributeKeys;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.trace.DefaultSpan;
+import io.opentelemetry.trace.Span;
 
 public class HttpClientResponseTracingHandler extends ChannelInboundHandlerAdapter {
 
   @Override
   public void channelRead(final ChannelHandlerContext ctx, final Object msg) {
-    final Attribute<AgentSpan> parentAttr =
+    final Attribute<Span> parentAttr =
         ctx.channel().attr(AttributeKeys.CLIENT_PARENT_ATTRIBUTE_KEY);
-    parentAttr.setIfAbsent(noopSpan());
-    final AgentSpan parent = parentAttr.get();
-    final AgentSpan span = ctx.channel().attr(AttributeKeys.CLIENT_ATTRIBUTE_KEY).get();
+    parentAttr.setIfAbsent(DefaultSpan.getInvalid());
+    final Span parent = parentAttr.get();
+    final Span span = ctx.channel().attr(AttributeKeys.CLIENT_ATTRIBUTE_KEY).get();
 
     final boolean finishSpan = msg instanceof HttpResponse;
 
     if (span != null && finishSpan) {
-      try (final AgentScope scope = activateSpan(span, false)) {
+      try (final Scope scope = TRACER.withSpan(span)) {
         DECORATE.onResponse(span, (HttpResponse) msg);
         DECORATE.beforeFinish(span);
-        span.finish();
+        span.end();
       }
     }
 
     // We want the callback in the scope of the parent, not the client span
-    try (final AgentScope scope = activateSpan(parent, false)) {
+    try (final Scope scope = TRACER.withSpan(parent)) {
       ctx.fireChannelRead(msg);
     }
   }
