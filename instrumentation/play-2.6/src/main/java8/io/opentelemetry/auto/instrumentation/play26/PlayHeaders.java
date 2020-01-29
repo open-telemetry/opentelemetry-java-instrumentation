f948ef0a6d45265074b0658ee0f19ce794e64b07
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-01-28 20:41:37 -0800

    Convert Play 2.6 to use OpenTelemetry API directly

--- instrumentation/play-2.6/src/main/java8/io/opentelemetry/auto/instrumentation/play26/PlayHeaders.java
+++ instrumentation/play-2.6/src/main/java8/io/opentelemetry/auto/instrumentation/play26/PlayHeaders.java
@@ -1,24 +1,12 @@
 package io.opentelemetry.auto.instrumentation.play26;
 
-import io.opentelemetry.auto.instrumentation.api.AgentPropagation;
-import java.util.ArrayList;
-import java.util.List;
+import io.opentelemetry.context.propagation.HttpTextFormat;
 import play.api.mvc.Headers;
 import scala.Option;
 
-public class PlayHeaders implements AgentPropagation.Getter<Headers> {
+public class PlayHeaders implements HttpTextFormat.Getter<Headers> {
 
   public static final PlayHeaders GETTER = new PlayHeaders();
-
-  @Override
-  public Iterable<String> keys(final Headers headers) {
-    final List<String> javaList = new ArrayList<>();
-    final scala.collection.Iterator<String> scalaIterator = headers.keys().iterator();
-    while (scalaIterator.hasNext()) {
-      javaList.add(scalaIterator.next());
-    }
-    return javaList;
-  }
 
   @Override
   public String get(final Headers headers, final String key) {
