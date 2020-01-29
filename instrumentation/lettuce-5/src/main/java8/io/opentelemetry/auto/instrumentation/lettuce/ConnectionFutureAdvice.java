d3d5a8e7ef5ddeeb8f5289790b66fcd9df66c538
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-01-29 13:03:51 -0800

    Convert Lettuce to use OpenTelemetry API directly

--- instrumentation/lettuce-5/src/main/java8/io/opentelemetry/auto/instrumentation/lettuce/ConnectionFutureAdvice.java
+++ instrumentation/lettuce-5/src/main/java8/io/opentelemetry/auto/instrumentation/lettuce/ConnectionFutureAdvice.java
@@ -1,39 +1,38 @@
 package io.opentelemetry.auto.instrumentation.lettuce;
 
-import static io.opentelemetry.auto.instrumentation.api.AgentTracer.activateSpan;
-import static io.opentelemetry.auto.instrumentation.api.AgentTracer.startSpan;
 import static io.opentelemetry.auto.instrumentation.lettuce.LettuceClientDecorator.DECORATE;
+import static io.opentelemetry.auto.instrumentation.lettuce.LettuceClientDecorator.TRACER;
 
 import io.lettuce.core.ConnectionFuture;
 import io.lettuce.core.RedisURI;
-import io.opentelemetry.auto.instrumentation.api.AgentScope;
-import io.opentelemetry.auto.instrumentation.api.AgentSpan;
+import io.opentelemetry.auto.instrumentation.api.SpanScopePair;
+import io.opentelemetry.trace.Span;
 import net.bytebuddy.asm.Advice;
 
 public class ConnectionFutureAdvice {
 
   @Advice.OnMethodEnter(suppress = Throwable.class)
-  public static AgentScope onEnter(@Advice.Argument(1) final RedisURI redisURI) {
-    final AgentSpan span = startSpan("redis.query");
+  public static SpanScopePair onEnter(@Advice.Argument(1) final RedisURI redisURI) {
+    final Span span = TRACER.spanBuilder("redis.query").startSpan();
     DECORATE.afterStart(span);
     DECORATE.onConnection(span, redisURI);
-    return activateSpan(span, false);
+    return new SpanScopePair(span, TRACER.withSpan(span));
   }
 
   @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
   public static void stopSpan(
-      @Advice.Enter final AgentScope scope,
+      @Advice.Enter final SpanScopePair spanScopePair,
       @Advice.Thrown final Throwable throwable,
       @Advice.Return final ConnectionFuture<?> connectionFuture) {
-    final AgentSpan span = scope.span();
+    final Span span = spanScopePair.getSpan();
     if (throwable != null) {
       DECORATE.onError(span, throwable);
       DECORATE.beforeFinish(span);
-      span.finish();
-      scope.close();
+      span.end();
+      spanScopePair.getScope().close();
       return;
     }
     connectionFuture.handleAsync(new LettuceAsyncBiFunction<>(span));
-    scope.close();
+    spanScopePair.getScope().close();
   }
 }
