fcb6728901e467eec8509bc4edace8b340003b0e
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-01-28 20:26:47 -0800

    Convert Play 2.4 to use OpenTelemetry API directly

--- instrumentation/play-2.4/src/main/java8/io/opentelemetry/auto/instrumentation/play24/PlayHttpServerDecorator.java
+++ instrumentation/play-2.4/src/main/java8/io/opentelemetry/auto/instrumentation/play24/PlayHttpServerDecorator.java
@@ -1,9 +1,12 @@
 package io.opentelemetry.auto.instrumentation.play24;
 
+import io.opentelemetry.OpenTelemetry;
 import io.opentelemetry.auto.api.MoreTags;
 import io.opentelemetry.auto.decorator.HttpServerDecorator;
-import io.opentelemetry.auto.instrumentation.api.AgentSpan;
 import io.opentelemetry.auto.instrumentation.api.Tags;
+import io.opentelemetry.trace.Span;
+import io.opentelemetry.trace.Status;
+import io.opentelemetry.trace.Tracer;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.UndeclaredThrowableException;
 import java.net.URI;
@@ -16,6 +19,8 @@ import scala.Option;
 @Slf4j
 public class PlayHttpServerDecorator extends HttpServerDecorator<Request, Request, Result> {
   public static final PlayHttpServerDecorator DECORATE = new PlayHttpServerDecorator();
+
+  public static final Tracer TRACER = OpenTelemetry.getTracerFactory().get("io.opentelemetry.auto");
 
   @Override
   protected String[] instrumentationNames() {
@@ -58,7 +63,7 @@ public class PlayHttpServerDecorator extends HttpServerDecorator<Request, Reques
   }
 
   @Override
-  public AgentSpan onRequest(final AgentSpan span, final Request request) {
+  public Span onRequest(final Span span, final Request request) {
     super.onRequest(span, request);
     if (request != null) {
       // more about routes here:
@@ -73,9 +78,9 @@ public class PlayHttpServerDecorator extends HttpServerDecorator<Request, Reques
   }
 
   @Override
-  public AgentSpan onError(final AgentSpan span, Throwable throwable) {
+  public Span onError(final Span span, Throwable throwable) {
     span.setAttribute(Tags.HTTP_STATUS, 500);
-    span.setError(true);
+    span.setStatus(Status.UNKNOWN);
     if (throwable != null
         // This can be moved to instanceof check when using Java 8.
         && throwable.getClass().getName().equals("java.util.concurrent.CompletionException")
