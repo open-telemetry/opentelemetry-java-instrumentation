feb21ab2959c98abd052d8f1b61be5ddb44fe4de
Pontus Rydin <prydin@vmware.com>
2020-01-30 14:31:32 -0500

    Refactored jedis-* (#113)

--- instrumentation/jedis-3.0/src/main/java/io/opentelemetry/auto/instrumentation/jedis30/JedisClientDecorator.java
+++ instrumentation/jedis-3.0/src/main/java/io/opentelemetry/auto/instrumentation/jedis30/JedisClientDecorator.java
@@ -1,11 +1,16 @@
 package io.opentelemetry.auto.instrumentation.jedis30;
 
+import io.opentelemetry.OpenTelemetry;
 import io.opentelemetry.auto.api.SpanTypes;
 import io.opentelemetry.auto.decorator.DatabaseClientDecorator;
+import io.opentelemetry.trace.Tracer;
 import redis.clients.jedis.commands.ProtocolCommand;
 
 public class JedisClientDecorator extends DatabaseClientDecorator<ProtocolCommand> {
   public static final JedisClientDecorator DECORATE = new JedisClientDecorator();
+
+  public static final Tracer TRACER =
+      OpenTelemetry.getTracerFactory().get("io.opentelemetry.auto.jedis-1.4");
 
   @Override
   protected String[] instrumentationNames() {
