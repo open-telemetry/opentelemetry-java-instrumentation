a57040fddec60cdd4c585c1a1a241741335a9e91
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-01-30 13:43:20 -0800

    Remove Trace annotation from benchmark code

--- benchmark-integration/jetty-perftest/src/main/java/io/opentelemetry/perftest/Worker.java
+++ benchmark-integration/jetty-perftest/src/main/java/io/opentelemetry/perftest/Worker.java
@@ -1,7 +1,7 @@
 package io.opentelemetry.perftest;
 
 import io.opentelemetry.OpenTelemetry;
-import io.opentelemetry.auto.api.Trace;
+import io.opentelemetry.context.Scope;
 import io.opentelemetry.trace.Span;
 import io.opentelemetry.trace.Tracer;
 import java.util.concurrent.TimeUnit;
@@ -11,19 +11,21 @@ public class Worker {
   private static final Tracer TRACER =
       OpenTelemetry.getTracerFactory().get("io.opentelemetry.auto");
 
-  @Trace
   /** Simulate work for the give number of milliseconds. */
   public static void doWork(final long workTimeMS) {
-    final Span span = TRACER.getCurrentSpan();
-    if (span != null) {
-      span.setAttribute("work-time", workTimeMS);
-      span.setAttribute("info", "interesting stuff");
-      span.setAttribute("additionalInfo", "interesting stuff");
-    }
+    final Span span = TRACER.spanBuilder("work").startSpan();
+    try (final Scope scope = TRACER.withSpan(span)) {
+      if (span != null) {
+        span.setAttribute("work-time", workTimeMS);
+        span.setAttribute("info", "interesting stuff");
+        span.setAttribute("additionalInfo", "interesting stuff");
+      }
 
-    final long doneTimestamp = System.nanoTime() + TimeUnit.MILLISECONDS.toNanos(workTimeMS);
-    while (System.nanoTime() < doneTimestamp) {
-      // busy-wait to simulate work
+      final long doneTimestamp = System.nanoTime() + TimeUnit.MILLISECONDS.toNanos(workTimeMS);
+      while (System.nanoTime() < doneTimestamp) {
+        // busy-wait to simulate work
+      }
+      span.end();
     }
   }
 }
