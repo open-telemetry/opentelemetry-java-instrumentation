1252494fbc0df6fe5967e9e0b6d61695a3ea03ab
Pontus Rydin <prydin@vmware.com>
2020-01-31 17:18:06 -0500

    Update trace-api/src/main/java/io/opentelemetry/auto/api/Config.java
    Co-Authored-By: Trask Stalnaker <trask.stalnaker@gmail.com>

--- trace-api/src/main/java/io/opentelemetry/auto/api/Config.java
+++ trace-api/src/main/java/io/opentelemetry/auto/api/Config.java
@@ -164,7 +164,7 @@ public class Config {
   // Read order: Properties -> Parent
   private Config(final Properties properties, final Config parent) {
     exporter = properties.getProperty(EXPORTER, parent.exporter);
-    serviceName = properties.getProperty(SERVICE, "(unknown)");
+    serviceName = properties.getProperty(SERVICE, parent.serviceName);
 
     traceEnabled = getPropertyBooleanValue(properties, TRACE_ENABLED, parent.traceEnabled);
     integrationsEnabled =
