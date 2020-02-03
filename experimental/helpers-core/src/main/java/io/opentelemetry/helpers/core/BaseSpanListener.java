3c5e00c50dfbee4e8e7dcadb2a1d52bc8bb2504a
Kevin Brockhoff <kbrockhoff@codekaizen.org>
2020-02-03 07:56:51 -0600

    Initial dev of helpers core

---
+++ experimental/helpers-core/src/main/java/io/opentelemetry/helpers/core/BaseSpanListener.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2020, OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentelemetry.helpers.core;
+
+import io.opentelemetry.trace.Span;
+import io.opentelemetry.trace.Status;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.lang.reflect.Method;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.util.concurrent.ExecutionException;
+
+public abstract class BaseSpanListener implements SpanListener {
+
+  protected BaseSpanListener() {
+    super();
+  }
+
+  protected abstract Span.Kind getSpanType();
+
+  @Override
+  public Span afterStart(final Span span) {
+    assert span != null;
+    return span;
+  }
+
+  @Override
+  public Span beforeFinish(final Span span) {
+    assert span != null;
+    return span;
+  }
+
+  @Override
+  public Span onError(final Span span, final Throwable throwable) {
+    assert span != null;
+    if (throwable != null) {
+      span.setStatus(Status.UNKNOWN);
+      addThrowable(
+        span, throwable instanceof ExecutionException ? throwable.getCause() : throwable);
+    }
+    return span;
+  }
+
+  @Override
+  public Span onPeerConnection(final Span span, final InetSocketAddress remoteConnection) {
+    assert span != null;
+    if (remoteConnection != null) {
+      onPeerConnection(span, remoteConnection.getAddress());
+
+      span.setAttribute(SemanticConventions.NET_PEER_NAME, remoteConnection.getHostName());
+      span.setAttribute(SemanticConventions.NET_PEER_PORT, remoteConnection.getPort());
+    }
+    return span;
+  }
+
+  @Override
+  public Span onPeerConnection(final Span span, final InetAddress remoteAddress) {
+    assert span != null;
+    if (remoteAddress != null) {
+      span.setAttribute(SemanticConventions.NET_PEER_NAME, remoteAddress.getHostName());
+      span.setAttribute(SemanticConventions.NET_PEER_IP, remoteAddress.getHostAddress());
+    }
+    return span;
+  }
+
+  public static void addThrowable(final Span span, final Throwable throwable) {
+    final String message = throwable.getMessage();
+    if (message != null) {
+      span.setAttribute(SemanticConventions.ERROR_MESSAGE, message);
+    }
+    span.setAttribute(SemanticConventions.ERROR_KIND, throwable.getClass().getName());
+
+    final StringWriter errorString = new StringWriter();
+    throwable.printStackTrace(new PrintWriter(errorString));
+    span.setAttribute(SemanticConventions.ERROR_STACK, errorString.toString());
+  }
+
+  /**
+   * This method is used to generate an acceptable span (operation) name based on a given method
+   * reference. Anonymous classes are named based on their parent.
+   *
+   * @param method
+   * @return
+   */
+  public String spanNameForMethod(final Method method) {
+    return spanNameForClass(method.getDeclaringClass()) + "." + method.getName();
+  }
+
+  /**
+   * This method is used to generate an acceptable span (operation) name based on a given class
+   * reference. Anonymous classes are named based on their parent.
+   *
+   * @param clazz
+   * @return
+   */
+  public String spanNameForClass(final Class clazz) {
+    if (!clazz.isAnonymousClass()) {
+      return clazz.getSimpleName();
+    }
+    String className = clazz.getName();
+    if (clazz.getPackage() != null) {
+      final String pkgName = clazz.getPackage().getName();
+      if (!pkgName.isEmpty()) {
+        className = clazz.getName().replace(pkgName, "").substring(1);
+      }
+    }
+    return className;
+  }
+}
