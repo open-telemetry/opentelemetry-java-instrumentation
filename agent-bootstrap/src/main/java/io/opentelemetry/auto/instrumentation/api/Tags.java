f6fbe657fad3a7838263ef0b8a1f59d73aab0234
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-02-05 09:52:13 -0800

    Remove unused constants

--- agent-bootstrap/src/main/java/io/opentelemetry/auto/instrumentation/api/Tags.java
+++ agent-bootstrap/src/main/java/io/opentelemetry/auto/instrumentation/api/Tags.java
@@ -3,11 +3,6 @@ package io.opentelemetry.auto.instrumentation.api;
 // standard tag names (and span kind values) from OpenTracing (see io.opentracing.tag.Tags)
 public class Tags {
 
-  public static final String SPAN_KIND_SERVER = "server";
-  public static final String SPAN_KIND_CLIENT = "client";
-  public static final String SPAN_KIND_PRODUCER = "producer";
-  public static final String SPAN_KIND_CONSUMER = "consumer";
-
   public static final String HTTP_URL = "http.url";
   public static final String HTTP_STATUS = "http.status_code";
   public static final String HTTP_METHOD = "http.method";
@@ -16,7 +11,6 @@ public class Tags {
   public static final String PEER_SERVICE = "peer.service";
   public static final String PEER_HOSTNAME = "peer.hostname";
   public static final String PEER_PORT = "peer.port";
-  public static final String SPAN_KIND = "span.kind";
   public static final String COMPONENT = "component";
   public static final String DB_TYPE = "db.type";
   public static final String DB_INSTANCE = "db.instance";
