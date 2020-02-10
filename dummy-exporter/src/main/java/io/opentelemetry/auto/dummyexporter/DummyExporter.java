b7bbcfd19df1c0e4a4c50321f65ca8738cc90b0f
prydin <prydin@vmware.com>
2020-02-10 13:33:36 -0500

    Made the dummy exporter slightly less stupid

--- dummy-exporter/src/main/java/io/opentelemetry/auto/dummyexporter/DummyExporter.java
+++ dummy-exporter/src/main/java/io/opentelemetry/auto/dummyexporter/DummyExporter.java
@@ -2,7 +2,9 @@ package io.opentelemetry.auto.dummyexporter;
 
 import io.opentelemetry.sdk.trace.SpanData;
 import io.opentelemetry.sdk.trace.export.SpanExporter;
+import io.opentelemetry.trace.AttributeValue;
 import java.util.List;
+import java.util.Map;
 
 public class DummyExporter implements SpanExporter {
   private final String prefix;
@@ -14,8 +16,28 @@ public class DummyExporter implements SpanExporter {
   @Override
   public ResultCode export(final List<SpanData> list) {
     for (final SpanData span : list) {
-      System.out.println(prefix + " " + span.getName() + " " + span.getResource().toString());
+      System.out.print(prefix + " " + span.getName() + " ");
+      for (final Map.Entry<String, AttributeValue> attr : span.getAttributes().entrySet()) {
+        System.out.print(attr.getKey() + "=");
+        final AttributeValue value = attr.getValue();
+        switch (value.getType()) {
+          case STRING:
+            System.out.print('"' + value.getStringValue() + '"');
+            break;
+          case BOOLEAN:
+            System.out.print(value.getBooleanValue());
+            break;
+          case LONG:
+            System.out.print(value.getLongValue());
+            break;
+          case DOUBLE:
+            System.out.print(value.getDoubleValue());
+            break;
+        }
+        System.out.print(" ");
+      }
     }
+    System.out.println();
     return ResultCode.SUCCESS;
   }
 
