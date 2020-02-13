8235f7bd0455400fe461acaf50906f6b0bd85c33
Pontus Rydin <prydin@vmware.com>
2020-02-13 17:22:22 -0500

    Third-party exporter loading framework (#159)

--- agent-tooling/src/main/java/io/opentelemetry/auto/tooling/TracerInstaller.java
+++ agent-tooling/src/main/java/io/opentelemetry/auto/tooling/TracerInstaller.java
@@ -1,37 +1,81 @@
 package io.opentelemetry.auto.tooling;
 
+import com.google.common.annotations.VisibleForTesting;
 import io.opentelemetry.auto.config.Config;
+import io.opentelemetry.auto.exportersupport.SpanExporterFactory;
 import io.opentelemetry.auto.tooling.exporter.ExporterConfigException;
 import io.opentelemetry.auto.tooling.exporter.ExporterRegistry;
-import io.opentelemetry.auto.tooling.exporter.SpanExporterFactory;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.trace.export.SimpleSpansProcessor;
+import io.opentelemetry.sdk.trace.export.SpanExporter;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.ServiceLoader;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class TracerInstaller {
-
   /** Register agent tracer if no agent tracer is already registered. */
   public static synchronized void installAgentTracer() {
     if (Config.get().isTraceEnabled()) {
 
       // Try to create an exporter
-      final String exporter = Config.get().getExporter();
-      if (exporter != null) {
+      SpanExporter exporter = null;
+      final String expName = Config.get().getExporter();
+      if (expName != null) {
         try {
-          final SpanExporterFactory f = ExporterRegistry.getInstance().getFactory(exporter);
-          OpenTelemetrySdk.getTracerFactory()
-              .addSpanProcessor(SimpleSpansProcessor.newBuilder(f.newExporter()).build());
-          log.info("Loaded span exporter: " + exporter);
+          final io.opentelemetry.auto.tooling.exporter.SpanExporterFactory f =
+              ExporterRegistry.getInstance().getFactory(expName);
+          exporter = f.newExporter();
+          log.info("Loaded span exporter: " + expName);
         } catch (final ExporterConfigException e) {
           log.warn("Error loading exporter. Spans will be dropped", e);
         }
       } else {
+        final String exporterJar = Config.get().getExporterJar();
+        if (exporterJar != null) {
+          exporter = loadFromJar(exporterJar);
+        }
+      }
+      if (exporter != null) {
+        OpenTelemetrySdk.getTracerFactory()
+            .addSpanProcessor(SimpleSpansProcessor.newBuilder(exporter).build());
+        log.info("Installed span exporter: " + exporter.getClass().getCanonicalName());
+      } else {
         log.warn("No exporter is specified. Tracing will run but spans are dropped");
       }
     } else {
-      log.debug("Tracing is disabled.");
+      log.info("Tracing is disabled.");
     }
+  }
+
+  @VisibleForTesting
+  private static synchronized SpanExporter loadFromJar(final String exporterJar) {
+    final URL url;
+    try {
+      url = new File(exporterJar).toURI().toURL();
+    } catch (final MalformedURLException e) {
+      log.warn("Filename could not be parsed: " + exporterJar + ". Exporter is not installed");
+      return null;
+    }
+
+    final ExporterClassLoader exporterLoader =
+        new ExporterClassLoader(new URL[] {url}, TracerInstaller.class.getClassLoader());
+    final ServiceLoader<SpanExporterFactory> sl =
+        ServiceLoader.load(SpanExporterFactory.class, exporterLoader);
+    final Iterator<SpanExporterFactory> itor = sl.iterator();
+    if (itor.hasNext()) {
+      final SpanExporterFactory f = itor.next();
+      if (itor.hasNext()) {
+        log.warn(
+            "Exporter JAR defines more than one factory. Only the first one found will be used");
+      }
+      return f.fromConfig(new DefaultConfigProvider("exporter"));
+    }
+    log.warn("No matching providers in jar " + exporterJar);
+    return null;
   }
 
   public static void logVersionInfo() {
