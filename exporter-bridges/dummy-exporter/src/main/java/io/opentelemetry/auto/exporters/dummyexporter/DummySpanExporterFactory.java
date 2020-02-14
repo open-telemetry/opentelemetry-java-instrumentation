b6ac07bf21ac88f7148b33f11763c57400be8c4e
prydin <prydin@vmware.com>
2020-02-13 20:31:46 -0500

    Initial work on exporter bridges

---
+++ exporter-bridges/dummy-exporter/src/main/java/io/opentelemetry/auto/exporters/dummyexporter/DummySpanExporterFactory.java
@@ -0,0 +1,12 @@
+package io.opentelemetry.auto.exporters.dummyexporter;
+
+import io.opentelemetry.auto.exportersupport.ConfigProvider;
+import io.opentelemetry.auto.exportersupport.SpanExporterFactory;
+import io.opentelemetry.sdk.trace.export.SpanExporter;
+
+public class DummySpanExporterFactory implements SpanExporterFactory {
+  @Override
+  public SpanExporter fromConfig(final ConfigProvider config) {
+    return new DummyExporter(config.getString("prefix", "no-prefix"));
+  }
+}
