fb44fb516a47a893b80a424c1f10ad78106adc4e
prydin <prydin@vmware.com>
2020-02-15 15:59:04 -0500

    Implemented dynamically loaded exporter adapters

--- agent-tooling/src/test/groovy/io/opentelemetry/auto/tooling/ExporterLoaderTest.groovy
+++ agent-tooling/src/test/groovy/io/opentelemetry/auto/tooling/ExporterLoaderTest.groovy
@@ -5,7 +5,8 @@ import io.opentelemetry.auto.util.test.AgentSpecification
 import io.opentelemetry.sdk.OpenTelemetrySdk
 
 class ExporterLoaderTest extends AgentSpecification {
-  def jarName = "../dummy-exporter/build/libs/dummy-exporter-0.1.0-all.jar"
+  def projectVersion = System.getProperty("projectVersion")
+  def jarName = "../exporter-adapters/dummy-exporter-adapter/build/libs/dummy-exporter-adapter-${projectVersion}-all.jar"
   def tracer = OpenTelemetrySdk.getTracerFactory().get("test")
 
   def "test load exporter"() {
@@ -13,6 +14,6 @@ class ExporterLoaderTest extends AgentSpecification {
     def exporter = TracerInstaller.loadFromJar(jarName)
 
     then:
-    exporter.getClass().getName() == "io.opentelemetry.auto.dummyexporter.DummyExporter"
+    exporter.getClass().getName() == "io.opentelemetry.auto.exporters.dummyexporter.DummyExporter"
   }
 }
