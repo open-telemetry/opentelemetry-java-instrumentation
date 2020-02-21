c5489a2a81f4a70c2882a98a02ea1ff4bc4217cd
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-02-21 10:06:48 -0800

    Add the tests back under servlet module

---
+++ instrumentation/servlet/glassfish-testing/src/test/groovy/GlassFishServerTest.groovy
@@ -0,0 +1,121 @@
+import io.opentelemetry.auto.instrumentation.api.MoreTags
+import io.opentelemetry.auto.instrumentation.api.SpanTypes
+import io.opentelemetry.auto.instrumentation.api.Tags
+import io.opentelemetry.auto.instrumentation.servlet3.Servlet3Decorator
+import io.opentelemetry.auto.test.asserts.TraceAssert
+import io.opentelemetry.auto.test.base.HttpServerTest
+import org.apache.catalina.servlets.DefaultServlet
+import org.glassfish.embeddable.BootstrapProperties
+import org.glassfish.embeddable.Deployer
+import org.glassfish.embeddable.GlassFish
+import org.glassfish.embeddable.GlassFishProperties
+import org.glassfish.embeddable.GlassFishRuntime
+import org.glassfish.embeddable.archive.ScatteredArchive
+
+import static io.opentelemetry.auto.test.base.HttpServerTest.ServerEndpoint.EXCEPTION
+import static io.opentelemetry.auto.test.base.HttpServerTest.ServerEndpoint.SUCCESS
+import static io.opentelemetry.trace.Span.Kind.SERVER
+
+/**
+ * Unfortunately because we're using an embedded GlassFish instance, we aren't exercising the standard
+ * OSGi setup that required {@link io.opentelemetry.auto.instrumentation.glassfish.GlassFishInstrumentation}.
+ */
+// TODO: Figure out a better way to test with OSGi included.
+class GlassFishServerTest extends HttpServerTest<GlassFish, Servlet3Decorator> {
+
+//  static {
+//    System.setProperty("ota.integration.grizzly.enabled", "true")
+//  }
+
+  @Override
+  URI buildAddress() {
+    return new URI("http://localhost:$port/$context/")
+  }
+
+  String getContext() {
+    "test-gf"
+  }
+
+  @Override
+  GlassFish startServer(int port) {
+    // Setup the deployment archive
+    def testDir = new File(TestServlets.protectionDomain.codeSource.location.path)
+    assert testDir.exists() && testDir.directory
+    def testResourcesDir = new File(TestServlets.getResource("error.jsp").path).parentFile
+    assert testResourcesDir.exists() && testResourcesDir.directory
+    ScatteredArchive archive = new ScatteredArchive(context, ScatteredArchive.Type.WAR, testResourcesDir)
+    archive.addClassPath(testDir)
+
+    // Initialize the server
+    BootstrapProperties bootstrapProperties = new BootstrapProperties()
+    GlassFishRuntime glassfishRuntime = GlassFishRuntime.bootstrap(bootstrapProperties)
+    GlassFishProperties glassfishProperties = new GlassFishProperties()
+    glassfishProperties.setPort('http-listener', port)
+    def server = glassfishRuntime.newGlassFish(glassfishProperties)
+    server.start()
+
+    // Deploy war to server
+    Deployer deployer = server.getDeployer()
+    println "Deploying $testDir.absolutePath with $testResourcesDir.absolutePath"
+    deployer.deploy(archive.toURI())
+
+    return server
+  }
+
+  @Override
+  void stopServer(GlassFish server) {
+    server.stop()
+  }
+
+  @Override
+  Servlet3Decorator decorator() {
+    return Servlet3Decorator.DECORATE
+  }
+
+  @Override
+  String expectedOperationName() {
+    return "servlet.request"
+  }
+
+  @Override
+  boolean redirectHasBody() {
+    true
+  }
+
+  @Override
+  void serverSpan(TraceAssert trace, int index, String traceID = null, String parentID = null, String method = "GET", ServerEndpoint endpoint = SUCCESS) {
+    trace.span(index) {
+      operationName expectedOperationName()
+      spanKind SERVER
+      errored endpoint.errored
+      if (parentID != null) {
+        traceId traceID
+        parentId parentID
+      } else {
+        parent()
+      }
+      tags {
+        "$MoreTags.SPAN_TYPE" SpanTypes.HTTP_SERVER
+        "$Tags.COMPONENT" serverDecorator.getComponentName()
+        "$Tags.PEER_HOSTNAME" { it == "localhost" || it == "127.0.0.1" }
+        "$Tags.PEER_HOST_IPV4" { it == null || it == "127.0.0.1" } // Optional
+        "$Tags.PEER_PORT" Long
+        "$Tags.HTTP_STATUS" endpoint.status
+        "$Tags.HTTP_METHOD" method
+        "$Tags.HTTP_URL" "${endpoint.resolve(address)}"
+        "servlet.context" "/$context"
+        "servlet.path" endpoint.path
+        "span.origin.type" { it.startsWith("TestServlets\$") || it == DefaultServlet.name }
+        if (endpoint.errored) {
+          "error.msg" { it == null || it == EXCEPTION.body }
+          "error.type" { it == null || it == Exception.name }
+          "error.stack" { it == null || it instanceof String }
+        }
+        if (endpoint.query) {
+          "$MoreTags.HTTP_QUERY" endpoint.query
+        }
+      }
+    }
+  }
+}
+
