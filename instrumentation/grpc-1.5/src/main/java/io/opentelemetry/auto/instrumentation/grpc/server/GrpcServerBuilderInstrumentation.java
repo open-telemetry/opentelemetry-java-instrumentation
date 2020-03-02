72697a11a221252c2be2d01dc26a578dbcd09b70
prydin <prydin@vmware.com>
2020-03-02 17:42:46 -0500

    Fixed muzzle issue

--- instrumentation/grpc-1.5/src/main/java/io/opentelemetry/auto/instrumentation/grpc/server/GrpcServerBuilderInstrumentation.java
+++ instrumentation/grpc-1.5/src/main/java/io/opentelemetry/auto/instrumentation/grpc/server/GrpcServerBuilderInstrumentation.java
@@ -47,6 +47,8 @@ public class GrpcServerBuilderInstrumentation extends Instrumenter.Default {
       "io.opentelemetry.auto.instrumentation.grpc.server.TracingServerInterceptor",
       "io.opentelemetry.auto.instrumentation.grpc.server.TracingServerInterceptor$TracingServerCall",
       "io.opentelemetry.auto.instrumentation.grpc.server.TracingServerInterceptor$TracingServerCallListener",
+      "io.opentelemetry.auto.instrumentation.grpc.common.GrpcHelper",
+      "io.opentelemetry.auto.instrumentation.grpc.common.GrpcHelper$AddressAndPort",
       "io.opentelemetry.auto.decorator.BaseDecorator",
       "io.opentelemetry.auto.decorator.ServerDecorator",
       packageName + ".GrpcServerDecorator",
