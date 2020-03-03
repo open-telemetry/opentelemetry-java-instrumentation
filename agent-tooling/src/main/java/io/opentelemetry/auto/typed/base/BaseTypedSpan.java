b2d6f514f862242eda35f964e71681725b5b4609
Tyler Benson <tyler.benson@datadoghq.com>
2020-03-03 12:01:21 -0800

    Add deprecation warnings to unused end methods
    [skip ci]

--- agent-tooling/src/main/java/io/opentelemetry/auto/typed/base/BaseTypedSpan.java
+++ agent-tooling/src/main/java/io/opentelemetry/auto/typed/base/BaseTypedSpan.java
@@ -1,5 +1,6 @@
 package io.opentelemetry.auto.typed.base;
 
+import io.opentelemetry.trace.EndSpanOptions;
 import io.opentelemetry.trace.Span;
 
 public abstract class BaseTypedSpan<T extends BaseTypedSpan> extends DelegatingSpan {
@@ -11,6 +12,24 @@ public abstract class BaseTypedSpan<T extends BaseTypedSpan> extends DelegatingS
   public void end(Throwable throwable) {
     // add error details to the span.
     super.end();
+  }
+
+  /**
+   * The end(Throwable), or end(RESPONSE) methods should be used instead.
+   */
+  @Deprecated
+  @Override
+  public void end() {
+    super.end();
+  }
+
+  /**
+   * The end(Throwable), or end(RESPONSE) methods should be used instead.
+   */
+  @Deprecated
+  @Override
+  public void end(EndSpanOptions endOptions) {
+    super.end(endOptions);
   }
 
   protected abstract T self();
