85bd59a17ba10a32436427808afbf711dcc17cec
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-03-03 15:32:08 -0800

    Fix build

--- instrumentation/reactor-core-3.1/src/test/groovy/ReactorCoreTest.groovy
+++ instrumentation/reactor-core-3.1/src/test/groovy/ReactorCoreTest.groovy
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import io.opentelemetry.auto.instrumentation.reactor.core.ReactorCoreAdviceUtils
 import io.opentelemetry.auto.test.AgentTestRunner
 import io.opentelemetry.trace.Span
