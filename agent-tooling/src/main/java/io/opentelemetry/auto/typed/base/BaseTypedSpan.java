49a6aa2c8e1b583297251e2a18244b73a05bb6e2
Tyler Benson <tyler.benson@datadoghq.com>
2020-03-12 15:17:26 -0700

    Add headers.

--- agent-tooling/src/main/java/io/opentelemetry/auto/typed/base/BaseTypedSpan.java
+++ agent-tooling/src/main/java/io/opentelemetry/auto/typed/base/BaseTypedSpan.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020, OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.opentelemetry.auto.typed.base;
 
 import io.opentelemetry.trace.EndSpanOptions;
@@ -14,18 +29,14 @@ public abstract class BaseTypedSpan<T extends BaseTypedSpan> extends DelegatingS
     super.end();
   }
 
-  /**
-   * The end(Throwable), or end(RESPONSE) methods should be used instead.
-   */
+  /** The end(Throwable), or end(RESPONSE) methods should be used instead. */
   @Deprecated
   @Override
   public void end() {
     super.end();
   }
 
-  /**
-   * The end(Throwable), or end(RESPONSE) methods should be used instead.
-   */
+  /** The end(Throwable), or end(RESPONSE) methods should be used instead. */
   @Deprecated
   @Override
   public void end(EndSpanOptions endOptions) {
--- agent-tooling/src/main/java/io/opentelemetry/auto/typed/base/DelegatingSpan.java
+++ agent-tooling/src/main/java/io/opentelemetry/auto/typed/base/DelegatingSpan.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020, OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.opentelemetry.auto.typed.base;
 
 import io.opentelemetry.trace.AttributeValue;
@@ -6,7 +21,6 @@ import io.opentelemetry.trace.Event;
 import io.opentelemetry.trace.Span;
 import io.opentelemetry.trace.SpanContext;
 import io.opentelemetry.trace.Status;
-
 import java.util.Map;
 
 // TODO: This should be moved into the API.
