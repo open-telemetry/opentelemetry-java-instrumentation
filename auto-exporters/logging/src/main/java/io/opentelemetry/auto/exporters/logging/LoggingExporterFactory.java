1aa5932a844625eac2387eb4e6676136fe20fe0b
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-03-28 21:01:43 -0700

    Rename loggingexporter package for consistency

--- auto-exporters/logging/src/main/java/io/opentelemetry/auto/exporters/logging/LoggingExporterFactory.java
+++ auto-exporters/logging/src/main/java/io/opentelemetry/auto/exporters/logging/LoggingExporterFactory.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.opentelemetry.auto.exporters.loggingexporter;
+package io.opentelemetry.auto.exporters.logging;
 
 import io.opentelemetry.sdk.contrib.auto.config.Config;
 import io.opentelemetry.sdk.contrib.auto.config.SpanExporterFactory;
