99fa73e96b92b748e60e0d4a4d1ad631d2d09260
Nikita Salnikov-Tarnovski <gnikem@gmail.com>
2020-05-05 13:39:00 +0300

    Add api contrib into shaded parts of the agent

--- instrumentation/trace-annotation/src/test/groovy/WithSpanInstrumentationTest.groovy
+++ instrumentation/trace-annotation/src/test/groovy/WithSpanInstrumentationTest.groovy
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import io.opentelemetry.auto.test.AgentTestRunner
 import io.opentelemetry.auto.test.utils.ConfigUtils
 import io.opentelemetry.test.annotation.TracedWithSpan
