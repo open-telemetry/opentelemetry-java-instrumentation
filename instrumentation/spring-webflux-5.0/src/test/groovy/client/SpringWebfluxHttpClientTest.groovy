7730fea16c6f85b29612176d78eb517c795a32cb
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-05-14 14:45:14 -0700

    Bring back WebFlux client instrumentation

--- instrumentation/spring-webflux-5.0/src/test/groovy/client/SpringWebfluxHttpClientTest.groovy
+++ instrumentation/spring-webflux-5.0/src/test/groovy/client/SpringWebfluxHttpClientTest.groovy
@@ -27,9 +27,6 @@ import spock.lang.Timeout
 
 import static io.opentelemetry.trace.Span.Kind.CLIENT
 
-// FIXME this instrumentation is not currently reliable and so is currently disabled
-// see DefaultWebClientInstrumentation and DefaultWebClientAdvice
-@Ignore
 @Timeout(5)
 class SpringWebfluxHttpClientTest extends HttpClientTest {
 
@@ -54,7 +51,7 @@ class SpringWebfluxHttpClientTest extends HttpClientTest {
     if (!exception) {
       trace.span(index + 1) {
         childOf(trace.span(index))
-        operationName "netty.client.request"
+        operationName "HTTP $method"
         spanKind CLIENT
         errored exception != null
         tags {
