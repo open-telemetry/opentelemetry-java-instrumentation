fe8819fb8f1443b2e2671ff93bf23bca9ae9062d
Nikita Salnikov-Tarnovski <gnikem@gmail.com>
2020-05-17 20:41:41 +0300

    Support for khttp library (#401)

---
+++ instrumentation/khttp-0.1/src/test/groovy/KHttpClientTest.groovy
@@ -0,0 +1,34 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import io.opentelemetry.auto.test.base.HttpClientTest
+import khttp.KHttp
+
+class KHttpClientTest extends HttpClientTest {
+
+  @Override
+  int doRequest(String method, URI uri, Map<String, String> headers, Closure callback) {
+    def response = KHttp.request(method, uri.toString(), headers, Collections.emptyMap(), null, null, null, null, 1)
+    if (callback != null) {
+      callback.call()
+    }
+    return response.statusCode
+  }
+
+  @Override
+  boolean testCircularRedirects() {
+    return false
+  }
+}
