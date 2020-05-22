ad3231a610beff8000573ea4ddc8f641e176f736
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-05-22 09:15:55 +0900

    Oops:

---
+++ testing/src/main/groovy/io/opentelemetry/auto/test/InstrumentationTestRunner.groovy
@@ -0,0 +1,51 @@
+package io.opentelemetry.auto.test
+
+import com.google.common.base.Predicate
+import com.google.common.base.Predicates
+import groovy.transform.stc.ClosureParams
+import groovy.transform.stc.SimpleType
+import io.opentelemetry.auto.test.asserts.InMemoryExporterAssert
+import io.opentelemetry.sdk.OpenTelemetrySdk
+import io.opentelemetry.sdk.trace.data.SpanData
+import org.junit.Before
+import spock.lang.Specification
+/**
+ * A spock test runner which automatically initializes an in-memory exporter that can be used to
+ * verify traces.
+ */
+abstract class InstrumentationTestRunner extends Specification {
+
+  protected static final InMemoryExporter TEST_WRITER;
+
+  static {
+    TEST_WRITER = new InMemoryExporter();
+    OpenTelemetrySdk.getTracerProvider().addSpanProcessor(TEST_WRITER);
+  }
+
+  @Before
+  public void beforeTest() {
+    TEST_WRITER.clear();
+  }
+
+  protected void assertTraces(
+      final int size,
+      @ClosureParams(
+          value = SimpleType.class,
+          options = "io.opentelemetry.auto.test.asserts.ListWriterAssert")
+      @DelegatesTo(value = InMemoryExporterAssert.class, strategy = Closure.DELEGATE_FIRST)
+      final Closure spec) {
+    InMemoryExporterAssert.assertTraces(
+        TEST_WRITER, size, Predicates.<List<SpanData>>alwaysFalse(), spec);
+  }
+
+  protected void assertTracesWithFilter(
+      final int size,
+      final Predicate<List<SpanData>> excludes,
+      @ClosureParams(
+          value = SimpleType.class,
+          options = "io.opentelemetry.auto.test.asserts.ListWriterAssert")
+      @DelegatesTo(value = InMemoryExporterAssert.class, strategy = Closure.DELEGATE_FIRST)
+      final Closure spec) {
+    InMemoryExporterAssert.assertTraces(TEST_WRITER, size, excludes, spec);
+  }
+}
