0637f6097cc4720dc04127faecee214c83f3fbb6
Nikita Salnikov-Tarnovski <gnikem@gmail.com>
2020-05-31 20:53:29 +0300

    Update to otel-java 0.5.0-SNAPSHOT (#462)
    * Update to otel-java 0.5.0-SNAPSHOT
    
    * Tests pass
    
    * Check fix

--- testing/src/main/groovy/io/opentelemetry/auto/test/asserts/EventAssert.groovy
+++ testing/src/main/groovy/io/opentelemetry/auto/test/asserts/EventAssert.groovy
@@ -17,19 +17,19 @@ package io.opentelemetry.auto.test.asserts
 
 import groovy.transform.stc.ClosureParams
 import groovy.transform.stc.SimpleType
-import io.opentelemetry.sdk.trace.data.SpanData.TimedEvent
+import io.opentelemetry.trace.Event
 
 import static TagsAssert.assertTags
 
 class EventAssert {
-  private final TimedEvent event
+  private final Event event
   private final checked = [:]
 
   private EventAssert(event) {
     this.event = event
   }
 
-  static void assertEvent(TimedEvent event,
+  static void assertEvent(Event event,
                           @ClosureParams(value = SimpleType, options = ['io.opentelemetry.auto.test.asserts.EventAssert'])
                           @DelegatesTo(value = EventAssert, strategy = Closure.DELEGATE_FIRST) Closure spec) {
     def asserter = new EventAssert(event)
