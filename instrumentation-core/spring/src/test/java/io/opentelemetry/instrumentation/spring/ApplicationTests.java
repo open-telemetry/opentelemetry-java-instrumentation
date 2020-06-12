3cd9ffa370eb53614cf4c9d98c9a15fa5f5cb919
Munir Abdinur <62392438+mabdinur@users.noreply.github.com>
2020-06-11 21:41:18 -0400

    Empty gradle project (#497)

---
+++ instrumentation-core/spring/src/test/java/io/opentelemetry/instrumentation/spring/ApplicationTests.java
@@ -0,0 +1,12 @@
+package io.opentelemetry.instrumentation.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {}
+
+}
