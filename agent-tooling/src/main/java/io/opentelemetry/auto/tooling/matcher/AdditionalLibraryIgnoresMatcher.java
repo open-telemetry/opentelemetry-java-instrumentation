45dc02d9649bddd18342fd1141b676fc301474a7
Laplie Anderson <laplie.anderson@datadoghq.com>
2020-06-12 09:21:20 -0400

    Fix Spring handler mapping affecting character encoding (DataDog/dd-trace-java#1595)

--- agent-tooling/src/main/java/io/opentelemetry/auto/tooling/matcher/AdditionalLibraryIgnoresMatcher.java
+++ agent-tooling/src/main/java/io/opentelemetry/auto/tooling/matcher/AdditionalLibraryIgnoresMatcher.java
@@ -116,7 +116,11 @@ public class AdditionalLibraryIgnoresMatcher<T extends TypeDescription>
             || name.startsWith(
                 "org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainer$")
             || name.equals(
-                "org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedWebappClassLoader")) {
+                "org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedWebappClassLoader")
+            || name.equals(
+                "org.springframework.boot.context.embedded.EmbeddedWebApplicationContext")
+            || name.equals(
+                "org.springframework.boot.context.embedded.AnnotationConfigEmbeddedWebApplicationContext")) {
           return false;
         }
         return true;
@@ -183,7 +187,11 @@ public class AdditionalLibraryIgnoresMatcher<T extends TypeDescription>
       if (name.startsWith("org.springframework.web.")) {
         if (name.startsWith("org.springframework.web.servlet.")
             || name.startsWith("org.springframework.web.reactive.")
-            || name.startsWith("org.springframework.web.context.request.async.")) {
+            || name.startsWith("org.springframework.web.context.request.async.")
+            || name.equals(
+                "org.springframework.web.context.support.AbstractRefreshableWebApplicationContext")
+            || name.equals("org.springframework.web.context.support.GenericWebApplicationContext")
+            || name.equals("org.springframework.web.context.support.XmlWebApplicationContext")) {
           return false;
         }
         return true;
---
+++ instrumentation/spring-webmvc-3.1/src/main/java/io/opentelemetry/auto/instrumentation/springwebmvc/WebApplicationContextInstrumentation.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentelemetry.auto.instrumentation.springwebmvc;
+
+import static io.opentelemetry.auto.tooling.ClassLoaderMatcher.hasClassesNamed;
+import static io.opentelemetry.auto.tooling.bytebuddy.matcher.AgentElementMatchers.extendsClass;
+import static io.opentelemetry.auto.tooling.bytebuddy.matcher.AgentElementMatchers.implementsInterface;
+import static java.util.Collections.singletonMap;
+import static net.bytebuddy.matcher.ElementMatchers.isMethod;
+import static net.bytebuddy.matcher.ElementMatchers.named;
+import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
+
+import com.google.auto.service.AutoService;
+import io.opentelemetry.auto.tooling.Instrumenter;
+import java.util.Map;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+
+@AutoService(Instrumenter.class)
+public class WebApplicationContextInstrumentation extends Instrumenter.Default {
+  public WebApplicationContextInstrumentation() {
+    super("spring-web");
+  }
+
+  @Override
+  public ElementMatcher<ClassLoader> classLoaderMatcher() {
+    // Optimization for expensive typeMatcher.
+    return hasClassesNamed(
+        "org.springframework.context.support.AbstractApplicationContext",
+        "org.springframework.web.context.WebApplicationContext");
+  }
+
+  @Override
+  public ElementMatcher<TypeDescription> typeMatcher() {
+    return extendsClass(named("org.springframework.context.support.AbstractApplicationContext"))
+        .and(implementsInterface(named("org.springframework.web.context.WebApplicationContext")));
+  }
+
+  @Override
+  public String[] helperClassNames() {
+    return new String[] {
+      packageName + ".SpringWebMvcDecorator",
+      packageName + ".HandlerMappingResourceNameFilter",
+      packageName + ".HandlerMappingResourceNameFilter$BeanDefinition",
+    };
+  }
+
+  @Override
+  public Map<? extends ElementMatcher<? super MethodDescription>, String> transformers() {
+
+    return singletonMap(
+        isMethod()
+            .and(named("postProcessBeanFactory"))
+            .and(
+                takesArgument(
+                    0,
+                    named(
+                        "org.springframework.beans.factory.config.ConfigurableListableBeanFactory"))),
+        WebApplicationContextInstrumentation.class.getName() + "$FilterInjectingAdvice");
+  }
+
+  public static class FilterInjectingAdvice {
+    @Advice.OnMethodEnter(suppress = Throwable.class)
+    public static void onEnter(
+        @Advice.Argument(0) final ConfigurableListableBeanFactory beanFactory) {
+      if (beanFactory instanceof BeanDefinitionRegistry
+          && !beanFactory.containsBean("otelAutoDispatcherFilter")) {
+
+        ((BeanDefinitionRegistry) beanFactory)
+            .registerBeanDefinition(
+                "otelAutoDispatcherFilter", new HandlerMappingResourceNameFilter.BeanDefinition());
+      }
+    }
+  }
+}
