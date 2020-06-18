8b5e8c78be855b28f03b228091d46bfb9c4ccd69
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-06-18 15:46:31 +0900

    Spotless

--- instrumentation/lettuce/lettuce-5.1/src/main/java8/io/opentelemetry/auto/instrumentation/lettuce/v5_1/OpenTelemetryTracing.java
+++ instrumentation/lettuce/lettuce-5.1/src/main/java8/io/opentelemetry/auto/instrumentation/lettuce/v5_1/OpenTelemetryTracing.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.opentelemetry.auto.instrumentation.lettuce.v5_1;
 
 import io.grpc.Context;
