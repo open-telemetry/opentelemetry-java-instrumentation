cb0406a36c7fe9bda151a8d6163b445e157fb5f2
Munir Abdinur <munir_abdinur@hotmail.com>
2020-06-19 15:12:32 -0400

    fix failing default test by excluding data source autoconfig

--- instrumentation-core/spring/src/test/java/io/opentelemetry/instrumentation/spring/ApplicationTests.java
+++ instrumentation-core/spring/src/test/java/io/opentelemetry/instrumentation/spring/ApplicationTests.java
@@ -1,9 +1,13 @@
 package io.opentelemetry.instrumentation.spring;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration (exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 class ApplicationTests {
 
   @Test
