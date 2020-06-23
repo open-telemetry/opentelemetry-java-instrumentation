ecf1d4b31bea663709d63ff3c9e60b055702f90d
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-06-23 17:16:08 +0900

    Format

--- agent-bootstrap/src/main/java/io/opentelemetry/auto/bootstrap/spi/TracerCustomizer.java
+++ agent-bootstrap/src/main/java/io/opentelemetry/auto/bootstrap/spi/TracerCustomizer.java
@@ -7,10 +7,10 @@ import io.opentelemetry.sdk.trace.config.TraceConfig;
 /**
  * A service provider to allow programmatic customization of the tracing configuration. It will be
  * executed after the SDK has initialized a {@link TracerSdkProvider}. This means that not only can
- * the provided {@link TracerSdkProvider} be configured e.g. by calling
- * {@link TracerSdkProvider#updateActiveTraceConfig(TraceConfig)}, but static methods on
- * {@link io.opentelemetry.OpenTelemetry}, e.g.,
- * {@link io.opentelemetry.OpenTelemetry#setPropagators(ContextPropagators)} can be used as well.
+ * the provided {@link TracerSdkProvider} be configured e.g. by calling {@link
+ * TracerSdkProvider#updateActiveTraceConfig(TraceConfig)}, but static methods on {@link
+ * io.opentelemetry.OpenTelemetry}, e.g., {@link
+ * io.opentelemetry.OpenTelemetry#setPropagators(ContextPropagators)} can be used as well.
  *
  * <p>An implementation of {@link TracerCustomizer} can either be provided as part of an initializer
  * JAR, using the {@code ota.initializer.jar} property or can be included in the same JAR as the
@@ -18,8 +18,6 @@ import io.opentelemetry.sdk.trace.config.TraceConfig;
  */
 public interface TracerCustomizer {
 
-  /**
-   * Callback executed after the initial {@link TracerSdkProvider} has been initialized.
-   */
+  /** Callback executed after the initial {@link TracerSdkProvider} has been initialized. */
   void configure(TracerSdkProvider tracerSdkProvider);
 }
