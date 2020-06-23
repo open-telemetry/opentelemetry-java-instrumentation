b558c5022925dcbfe71cba756f37ce4de5507b64
Richard Startin <richard.startin@datadoghq.com>
2020-06-23 09:40:53 +0100

    Add missing classloader matchers for expensive matchers (DataDog/dd-trace-java#1617)

--- instrumentation/jdbc/src/main/java/io/opentelemetry/auto/instrumentation/jdbc/PreparedStatementInstrumentation.java
+++ instrumentation/jdbc/src/main/java/io/opentelemetry/auto/instrumentation/jdbc/PreparedStatementInstrumentation.java
@@ -17,6 +17,7 @@
 package io.opentelemetry.auto.instrumentation.jdbc;
 
 import static io.opentelemetry.auto.instrumentation.jdbc.JdbcTracer.TRACER;
+import static io.opentelemetry.auto.tooling.ClassLoaderMatcher.hasClassesNamed;
 import static io.opentelemetry.auto.tooling.bytebuddy.matcher.AgentElementMatchers.implementsInterface;
 import static java.util.Collections.singletonMap;
 import static net.bytebuddy.matcher.ElementMatchers.isPublic;
@@ -41,6 +42,11 @@ public final class PreparedStatementInstrumentation extends Instrumenter.Default
 
   public PreparedStatementInstrumentation() {
     super("jdbc");
+  }
+
+  @Override
+  public ElementMatcher<ClassLoader> classLoaderMatcher() {
+    return hasClassesNamed("java.sql.PreparedStatement");
   }
 
   @Override
