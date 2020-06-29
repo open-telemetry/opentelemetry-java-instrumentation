c5d8530da39e667d0fd7e0eb6c67b7bcf53ca3eb
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-06-28 18:56:22 -0700

    Spotless

--- instrumentation/grizzly-2.0/src/test/groovy/GrizzlyTest.groovy
+++ instrumentation/grizzly-2.0/src/test/groovy/GrizzlyTest.groovy
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-
 import io.opentelemetry.auto.test.base.HttpServerTest
 import org.glassfish.grizzly.http.server.HttpServer
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory
