3418ebf6f7e82ed3503121a72ae5bf6f9715cc0e
Nikita Salnikov-Tarnovski <gnikem@gmail.com>
2020-06-29 23:28:39 +0300

    More module renames (#610)

--- instrumentation/elasticsearch/elasticsearch-transport-common/src/main/java/io/opentelemetry/auto/instrumentation/elasticsearch/transport/ElasticsearchTransportClientDecorator.java
+++ instrumentation/elasticsearch/elasticsearch-transport-common/src/main/java/io/opentelemetry/auto/instrumentation/elasticsearch/transport/ElasticsearchTransportClientDecorator.java
@@ -14,19 +14,16 @@
  * limitations under the License.
  */
 
-package io.opentelemetry.auto.instrumentation.elasticsearch;
+package io.opentelemetry.auto.instrumentation.elasticsearch.transport;
 
 import io.opentelemetry.OpenTelemetry;
 import io.opentelemetry.auto.bootstrap.instrumentation.decorator.DatabaseClientDecorator;
-import io.opentelemetry.auto.instrumentation.api.MoreTags;
-import io.opentelemetry.auto.instrumentation.api.Tags;
 import io.opentelemetry.trace.Span;
 import io.opentelemetry.trace.Tracer;
-import org.elasticsearch.client.Response;
 
-public class ElasticsearchRestClientDecorator extends DatabaseClientDecorator {
-  public static final ElasticsearchRestClientDecorator DECORATE =
-      new ElasticsearchRestClientDecorator();
+public class ElasticsearchTransportClientDecorator extends DatabaseClientDecorator {
+  public static final ElasticsearchTransportClientDecorator DECORATE =
+      new ElasticsearchTransportClientDecorator();
 
   public static final Tracer TRACER =
       OpenTelemetry.getTracerProvider().get("io.opentelemetry.auto.elasticsearch");
@@ -46,17 +43,9 @@ public class ElasticsearchRestClientDecorator extends DatabaseClientDecorator {
     return null;
   }
 
-  public Span onRequest(final Span span, final String method, final String endpoint) {
-    span.setAttribute(Tags.HTTP_METHOD, method);
-    span.setAttribute(Tags.HTTP_URL, endpoint);
-    return span;
-  }
-
-  public Span onResponse(final Span span, final Response response) {
-    if (response != null && response.getHost() != null) {
-      span.setAttribute(MoreTags.NET_PEER_NAME, response.getHost().getHostName());
-      span.setAttribute(MoreTags.NET_PEER_PORT, response.getHost().getPort());
-    }
+  public Span onRequest(final Span span, final Class action, final Class request) {
+    span.setAttribute("elasticsearch.action", action.getSimpleName());
+    span.setAttribute("elasticsearch.request", request.getSimpleName());
     return span;
   }
 }
