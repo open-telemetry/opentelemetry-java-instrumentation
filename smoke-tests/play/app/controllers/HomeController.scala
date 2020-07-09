50ca8600042f5b6db6552b8c5f22d83145fcd9e0
Nikita Salnikov-Tarnovski <gnikem@gmail.com>
2020-07-09 22:16:33 +0300

    Polish

--- smoke-tests/play/app/controllers/HomeController.scala
+++ smoke-tests/play/app/controllers/HomeController.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
 import javax.inject.Inject
@@ -8,7 +24,8 @@ import play.api.mvc._
   * This controller creates an `Action` to handle HTTP requests to the
   * application's welcome greeting
   */
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(cc: ControllerComponents)
+    extends AbstractController(cc) {
 
   /**
     * Create an Action to return a greeting
