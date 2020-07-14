f1d08a45a341db6ce4043773aa33f97b09260bab
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-07-14 15:02:13 -0700

    Fix muzzle failure

--- instrumentation/jetty-8.0/src/main/java/io/opentelemetry/auto/instrumentation/jetty/JettyHandlerInstrumentation.java
+++ instrumentation/jetty-8.0/src/main/java/io/opentelemetry/auto/instrumentation/jetty/JettyHandlerInstrumentation.java
@@ -53,11 +53,11 @@ public final class JettyHandlerInstrumentation extends Instrumenter.Default {
   @Override
   public String[] helperClassNames() {
     return new String[] {
-      packageName + ".JettyHttpServerTracer",
+      "io.opentelemetry.instrumentation.servlet.HttpServletRequestGetter",
+      "io.opentelemetry.instrumentation.servlet.ServletHttpServerTracer",
       "io.opentelemetry.auto.instrumentation.servlet.v3_0.Servlet3HttpServerTracer",
       "io.opentelemetry.auto.instrumentation.servlet.v3_0.TagSettingAsyncListener",
-      "io.opentelemetry.instrumentation.servlet.ServletHttpServerTracer",
-      "io.opentelemetry.instrumentation.servlet.HttpServletRequestGetter",
+      packageName + ".JettyHttpServerTracer",
     };
   }
 
