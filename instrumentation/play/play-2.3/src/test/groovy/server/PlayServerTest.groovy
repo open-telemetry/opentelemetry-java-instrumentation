b0333d60b0224f790843c1ba5a15e9be2f26c6da
Nikita Salnikov-Tarnovski <gnikem@gmail.com>
2020-07-15 15:29:05 +0300

    Play 2.3 migrated from ServerDecorator

--- instrumentation/play/play-2.3/src/test/groovy/server/PlayServerTest.groovy
+++ instrumentation/play/play-2.3/src/test/groovy/server/PlayServerTest.groovy
@@ -42,7 +42,6 @@ class PlayServerTest extends HttpServerTest<TestServer> {
     server.stop()
   }
 
-  // We don't have instrumentation for this version of netty yet
   @Override
   boolean hasHandlerSpan() {
     true
@@ -53,18 +52,11 @@ class PlayServerTest extends HttpServerTest<TestServer> {
     trace.span(index) {
       operationName "play.request"
       spanKind INTERNAL
-      errored endpoint == ERROR || endpoint == EXCEPTION || endpoint == NOT_FOUND
+      errored endpoint == EXCEPTION
       childOf((SpanData) parent)
       attributes {
-        "${SemanticAttributes.NET_PEER_IP.key()}" { it == null || it == "127.0.0.1" } // Optional
-        "${SemanticAttributes.HTTP_URL.key()}" String
-        "${SemanticAttributes.HTTP_METHOD.key()}" String
-        "${SemanticAttributes.HTTP_STATUS_CODE.key()}" Long
         if (endpoint == EXCEPTION) {
           errorAttributes(Exception, EXCEPTION.body)
-        }
-        if (endpoint.query) {
-          "$MoreAttributes.HTTP_QUERY" endpoint.query
         }
       }
     }
