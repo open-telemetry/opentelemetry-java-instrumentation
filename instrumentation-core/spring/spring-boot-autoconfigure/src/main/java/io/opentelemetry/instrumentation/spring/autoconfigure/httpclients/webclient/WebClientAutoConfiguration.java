56d249e08dcd2afb6c01864f5fe54ebe2b5f60a9
Munir Abdinur <62392438+mabdinur@users.noreply.github.com>
2020-07-17 10:16:41 -0400

    Update instrumentation-core/spring/spring-boot-autoconfigure/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/httpclients/webclient/WebClientAutoConfiguration.java
    Co-authored-by: Anuraag Agrawal <anuraaga@gmail.com>

--- instrumentation-core/spring/spring-boot-autoconfigure/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/httpclients/webclient/WebClientAutoConfiguration.java
+++ instrumentation-core/spring/spring-boot-autoconfigure/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/httpclients/webclient/WebClientAutoConfiguration.java
@@ -26,7 +26,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
-/** Configures RestTemplateBeanPostProcessor bean */
+/** Configures {@link WebClient} for tracing. */
 @Configuration
 @ConditionalOnClass(WebClient.class)
 @EnableConfigurationProperties(HttpClientsProperties.class)
