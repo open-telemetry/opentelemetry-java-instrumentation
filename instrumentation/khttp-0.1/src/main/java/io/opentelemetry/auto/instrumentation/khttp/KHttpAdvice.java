2dfb355332ac1ccc518df1702e02641a929f198a
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-07-18 01:47:32 +0900

    Support user agent in khttp (#719)

--- instrumentation/khttp-0.1/src/main/java/io/opentelemetry/auto/instrumentation/khttp/KHttpAdvice.java
+++ instrumentation/khttp-0.1/src/main/java/io/opentelemetry/auto/instrumentation/khttp/KHttpAdvice.java
@@ -50,7 +50,7 @@ public class KHttpAdvice {
     final Span span = TRACER.spanBuilder("HTTP " + method).setSpanKind(CLIENT).startSpan();
 
     DECORATE.afterStart(span);
-    DECORATE.onRequest(span, new RequestWrapper(method, uri));
+    DECORATE.onRequest(span, new RequestWrapper(method, uri, headers));
 
     final Context context = withSpan(span, Context.current());
 
