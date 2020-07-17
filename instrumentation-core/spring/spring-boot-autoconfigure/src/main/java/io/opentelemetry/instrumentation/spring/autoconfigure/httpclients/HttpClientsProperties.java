5cd531e2a827684f328913e426669623d0cec2b6
Munir Abdinur <62392438+mabdinur@users.noreply.github.com>
2020-07-17 11:45:36 -0400

    Update instrumentation-core/spring/spring-boot-autoconfigure/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/httpclients/HttpClientsProperties.java
    Co-authored-by: Anuraag Agrawal <anuraaga@gmail.com>

--- instrumentation-core/spring/spring-boot-autoconfigure/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/httpclients/HttpClientsProperties.java
+++ instrumentation-core/spring/spring-boot-autoconfigure/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/httpclients/HttpClientsProperties.java
@@ -22,7 +22,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  * Configuration for the tracing instrumentation of HTTP clients.
  *
  * <p>Sets default value of opentelemetry.trace.httpclients.enabled to true if the configuration
- * does not exist in application context
+ * does not exist in application context.
  */
 @ConfigurationProperties(prefix = "opentelemetry.trace.httpclients")
 public final class HttpClientsProperties {
