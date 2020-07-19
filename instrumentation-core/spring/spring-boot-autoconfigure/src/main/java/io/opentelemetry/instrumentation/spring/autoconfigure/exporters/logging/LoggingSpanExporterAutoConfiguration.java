5b873ca3e4eae8d7edbb4cd05b2125f5810e6fab
Munir Abdinur <munir_abdinur@hotmail.com>
2020-07-18 22:57:28 -0400

    add exporter configurations to springboot autoconfigure

--- instrumentation-core/spring/spring-boot-autoconfigure/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/exporters/logging/LoggingSpanExporterAutoConfiguration.java
+++ instrumentation-core/spring/spring-boot-autoconfigure/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/exporters/logging/LoggingSpanExporterAutoConfiguration.java
@@ -19,6 +19,7 @@ package io.opentelemetry.instrumentation.spring.autoconfigure.exporters.logging;
 import io.opentelemetry.exporters.logging.LoggingSpanExporter;
 import io.opentelemetry.instrumentation.spring.autoconfigure.TracerAutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -33,6 +34,7 @@ import org.springframework.context.annotation.Configuration;
     prefix = "opentelemetry.trace.exporter.logging",
     name = "enabled",
     matchIfMissing = true)
+@ConditionalOnClass(LoggingSpanExporter.class)
 public class LoggingSpanExporterAutoConfiguration {
 
   @Bean
