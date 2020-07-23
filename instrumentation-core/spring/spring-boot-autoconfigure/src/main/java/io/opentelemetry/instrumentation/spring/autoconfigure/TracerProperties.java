d52ebd7c26f9f6795f6a03cee874a49b6c6623e9
Munir Abdinur <munir_abdinur@hotmail.com>
2020-07-22 21:29:58 -0400

    format

--- instrumentation-core/spring/spring-boot-autoconfigure/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/TracerProperties.java
+++ instrumentation-core/spring/spring-boot-autoconfigure/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/TracerProperties.java
@@ -32,11 +32,11 @@ public final class TracerProperties {
 
   private String name = "otel-spring-tracer";
 
-  /** 
-  * If Sample probability == 1: always sample
-  *
-  * <p> If Sample probability == 0: never sample 
-  */
+  /**
+   * If Sample probability == 1: always sample
+   *
+   * <p>If Sample probability == 0: never sample
+   */
   @DecimalMin("0.0")
   @DecimalMax("1.0")
   private double samplerProbability = 1.0;
