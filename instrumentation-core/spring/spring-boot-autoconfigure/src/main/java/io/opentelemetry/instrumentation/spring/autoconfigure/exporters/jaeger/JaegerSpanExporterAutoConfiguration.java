72c36b40b414e572df69a07827c02e2335e1bfd3
Munir Abdinur <62392438+mabdinur@users.noreply.github.com>
2020-07-26 22:55:03 -0400

    add exporter configurations to springboot autoconfigure (#729)
    * add exporter configurations to springboot autoconfigure
    
    * clean up javadoc and jaegerspanexporter name
    
    * format
    
    * fix javadoc
    
    * Update spring-boot-autoconfigure.gradle
    
    * format and clean up java docs
    
    * review changes
    
    * edit host name
    
    * Update TracerProperties.java
    
    * Update instrumentation-core/spring/spring-boot-autoconfigure/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/exporters/jaeger/JaegerSpanExporterProperties.java
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>
    
    * Update instrumentation-core/spring/spring-boot-autoconfigure/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/exporters/otlp/OtlpGrpcSpanExporterProperties.java
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>
    
    * format
    
    * Update instrumentation-core/spring/spring-boot-autoconfigure/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/exporters/otlp/OtlpGrpcSpanExporterProperties.java
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>
    
    * Update instrumentation-core/spring/spring-boot-autoconfigure/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/exporters/jaeger/JaegerSpanExporterProperties.java
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>
    
    * Update instrumentation-core/spring/spring-boot-autoconfigure/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/exporters/zipkin/ZipkinSpanExporterProperties.java
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>
    
    * Update instrumentation-core/spring/spring-boot-autoconfigure/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/exporters/zipkin/ZipkinSpanExporterProperties.java
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>
    
    * fixed properties
    
    * update active tracer config with sampling probability and clean up host/port property javadocs
    
    * fix duration property name to align with docs
    
    * Update spring-boot-autoconfigure.gradle
    
    * Update spring-boot-autoconfigure.gradle
    
    * Update spring-boot-autoconfigure.gradle
    
    * add conditional on managed channel
    
    * conditional on managed channel
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>

---
+++ instrumentation-core/spring/spring-boot-autoconfigure/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/exporters/jaeger/JaegerSpanExporterAutoConfiguration.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentelemetry.instrumentation.spring.autoconfigure.exporters.jaeger;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.opentelemetry.exporters.jaeger.JaegerGrpcSpanExporter;
+import io.opentelemetry.instrumentation.spring.autoconfigure.TracerAutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configures {@link JaegerGrpcSpanExporter} for tracing.
+ *
+ * <p>Initializes {@link JaegerGrpcSpanExporter} bean if bean is missing.
+ */
+@Configuration
+@AutoConfigureBefore(TracerAutoConfiguration.class)
+@EnableConfigurationProperties(JaegerSpanExporterProperties.class)
+@ConditionalOnProperty(
+    prefix = "opentelemetry.trace.exporter.jaeger",
+    name = "enabled",
+    matchIfMissing = true)
+@ConditionalOnClass({JaegerGrpcSpanExporter.class, ManagedChannel.class})
+public class JaegerSpanExporterAutoConfiguration {
+
+  @Bean
+  @ConditionalOnMissingBean
+  public JaegerGrpcSpanExporter otelJaegerSpanExporter(
+      JaegerSpanExporterProperties jaegerSpanExporterProperties) {
+
+    ManagedChannel channel =
+        ManagedChannelBuilder.forTarget(jaegerSpanExporterProperties.getEndpoint())
+            .usePlaintext()
+            .build();
+
+    return JaegerGrpcSpanExporter.newBuilder()
+        .setServiceName(jaegerSpanExporterProperties.getServiceName())
+        .setDeadlineMs(jaegerSpanExporterProperties.getSpanTimeout().toMillis())
+        .setChannel(channel)
+        .build();
+  }
+}
