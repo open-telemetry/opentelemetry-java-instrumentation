5e1a495d9af44c8731663d3de10f6b96e58bbfe1
Helen Yang <heya@microsoft.com>
2020-07-27 17:07:23 -0700

    Delete unused DbSystem

--- agent-bootstrap/src/main/java/io/opentelemetry/auto/bootstrap/instrumentation/jdbc/DbSystem.java
+++ agent-bootstrap/src/main/java/io/opentelemetry/auto/bootstrap/instrumentation/jdbc/DbSystem.java
@@ -20,27 +20,18 @@ public final class DbSystem {
 
   public static final String AS400 = "as400";
   public static final String CASSANDRA = "cassandra";
-  public static final String COSMOSDB = "cosmosdb";
   public static final String COUCHBASE = "couchbase";
-  public static final String COUCHDB = "couchdb";
   public static final String DB2 = "db2";
   public static final String DERBY = "derby";
-  public static final String DYNAMODB = "dynamodb";
   public static final String GEODE = "geode";
   public static final String H2 = "h2";
-  public static final String HBASE = "hbase";
-  public static final String HIVE = "hive";
   public static final String HSQLDB = "hsqldb";
   public static final String MARIADB = "mariadb";
   public static final String MONGODB = "mongodb";
   public static final String MSSQL = "mssql";
   public static final String MYSQL = "mysql";
-  public static final String NEO4J = "neo4j";
   public static final String ORACLE = "oracle";
   public static final String OTHER_SQL = "other_sql";
   public static final String POSTGRESQL = "postgresql";
   public static final String REDIS = "redis";
-  public static final String SAP = "sap";
-  public static final String SQLITE = "sqlite";
-  public static final String TERADATA = "teradata";
 }
