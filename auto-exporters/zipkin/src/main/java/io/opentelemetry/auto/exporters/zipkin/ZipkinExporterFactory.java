f4ada08c49ccd1027a9cab018925b7b5cf8c8015
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-07-30 11:56:35 -0700

    Use default zipkin endpoint from otel-java (#837)

--- auto-exporters/zipkin/src/main/java/io/opentelemetry/auto/exporters/zipkin/ZipkinExporterFactory.java
+++ auto-exporters/zipkin/src/main/java/io/opentelemetry/auto/exporters/zipkin/ZipkinExporterFactory.java
@@ -20,25 +20,14 @@ import io.opentelemetry.exporters.zipkin.ZipkinSpanExporter;
 import io.opentelemetry.sdk.extensions.auto.config.Config;
 import io.opentelemetry.sdk.extensions.auto.config.SpanExporterFactory;
 import io.opentelemetry.sdk.trace.export.SpanExporter;
-import zipkin2.reporter.okhttp3.OkHttpSender;
 
 public class ZipkinExporterFactory implements SpanExporterFactory {
-  private static final String ZIPKIN_ENDPOINT_PROPERTY = "otel.zipkin.endpoint";
-  private static final String ZIPKIN_ENDPOINT_ENV_VARIABLE = "OTEL_ZIPKIN_ENDPOINT";
-  private static final String DEFAULT_ZIPKIN_ENDPOINT = "http://localhost:9411/api/v2/spans";
 
   @Override
   public SpanExporter fromConfig(Config config) {
-    String zipkinEndpoint =
-        System.getProperty(ZIPKIN_ENDPOINT_PROPERTY, System.getenv(ZIPKIN_ENDPOINT_ENV_VARIABLE));
-    if (zipkinEndpoint == null) {
-      zipkinEndpoint = DEFAULT_ZIPKIN_ENDPOINT;
-    }
-
     return ZipkinSpanExporter.newBuilder()
         .readEnvironmentVariables()
         .readSystemProperties()
-        .setSender(OkHttpSender.create(zipkinEndpoint))
         .build();
   }
 }
