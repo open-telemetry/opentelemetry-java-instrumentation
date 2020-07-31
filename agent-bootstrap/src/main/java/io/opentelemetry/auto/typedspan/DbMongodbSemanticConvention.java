6247348bd0e2e1a90f5845b5d5f752ce7c309833
Giovanni Liva <giovanni.liva@dynatrace.com>
2020-07-31 11:26:37 +0200

    Better documentation. No public in interface

--- agent-bootstrap/src/main/java/io/opentelemetry/auto/typedspan/DbMongodbSemanticConvention.java
+++ agent-bootstrap/src/main/java/io/opentelemetry/auto/typedspan/DbMongodbSemanticConvention.java
@@ -27,70 +27,70 @@ public interface DbMongodbSemanticConvention {
    * Sets a value for db.system
    *
    * @param dbSystem An identifier for the database management system (DBMS) product being used. See
-   *     below for a list of well-known identifiers..
+   *     below for a list of well-known identifiers.
    */
-  public DbMongodbSemanticConvention setDbSystem(String dbSystem);
+  DbMongodbSemanticConvention setDbSystem(String dbSystem);
 
   /**
    * Sets a value for db.connection_string
    *
-   * @param dbConnectionString The connection string used to connect to the database..
+   * @param dbConnectionString The connection string used to connect to the database.
    *     <p>It is recommended to remove embedded credentials.
    */
-  public DbMongodbSemanticConvention setDbConnectionString(String dbConnectionString);
+  DbMongodbSemanticConvention setDbConnectionString(String dbConnectionString);
 
   /**
    * Sets a value for db.user
    *
-   * @param dbUser Username for accessing the database..
+   * @param dbUser Username for accessing the database.
    */
-  public DbMongodbSemanticConvention setDbUser(String dbUser);
+  DbMongodbSemanticConvention setDbUser(String dbUser);
 
   /**
    * Sets a value for db.jdbc.driver_classname
    *
    * @param dbJdbcDriverClassname The fully-qualified class name of the [Java Database Connectivity
    *     (JDBC)](https://docs.oracle.com/javase/8/docs/technotes/guides/jdbc/) driver used to
-   *     connect..
+   *     connect.
    */
-  public DbMongodbSemanticConvention setDbJdbcDriverClassname(String dbJdbcDriverClassname);
+  DbMongodbSemanticConvention setDbJdbcDriverClassname(String dbJdbcDriverClassname);
 
   /**
    * Sets a value for db.name
    *
    * @param dbName If no tech-specific attribute is defined, this attribute is used to report the
    *     name of the database being accessed. For commands that switch the database, this should be
-   *     set to the target database (even if the command fails)..
-   *     <p>In some SQL databases, the database name to be used is called &#34;schema name&#34;.
+   *     set to the target database (even if the command fails).
+   *     <p>In some SQL databases, the database name to be used is called "schema name".
    */
-  public DbMongodbSemanticConvention setDbName(String dbName);
+  DbMongodbSemanticConvention setDbName(String dbName);
 
   /**
    * Sets a value for db.statement
    *
-   * @param dbStatement The database statement being executed..
+   * @param dbStatement The database statement being executed.
    *     <p>The value may be sanitized to exclude sensitive information.
    */
-  public DbMongodbSemanticConvention setDbStatement(String dbStatement);
+  DbMongodbSemanticConvention setDbStatement(String dbStatement);
 
   /**
    * Sets a value for db.operation
    *
    * @param dbOperation The name of the operation being executed, e.g. the [MongoDB command
    *     name](https://docs.mongodb.com/manual/reference/command/#database-operations) such as
-   *     `findAndModify`..
+   *     `findAndModify`.
    *     <p>While it would semantically make sense to set this, e.g., to a SQL keyword like `SELECT`
    *     or `INSERT`, it is not recommended to attempt any client-side parsing of `db.statement`
    *     just to get this property (the back end can do that if required).
    */
-  public DbMongodbSemanticConvention setDbOperation(String dbOperation);
+  DbMongodbSemanticConvention setDbOperation(String dbOperation);
 
   /**
    * Sets a value for net.peer.name
    *
-   * @param netPeerName Remote hostname or similar, see note below..
+   * @param netPeerName Remote hostname or similar, see note below.
    */
-  public DbMongodbSemanticConvention setNetPeerName(String netPeerName);
+  DbMongodbSemanticConvention setNetPeerName(String netPeerName);
 
   /**
    * Sets a value for net.peer.ip
@@ -98,27 +98,27 @@ public interface DbMongodbSemanticConvention {
    * @param netPeerIp Remote address of the peer (dotted decimal for IPv4 or
    *     [RFC5952](https://tools.ietf.org/html/rfc5952) for IPv6).
    */
-  public DbMongodbSemanticConvention setNetPeerIp(String netPeerIp);
+  DbMongodbSemanticConvention setNetPeerIp(String netPeerIp);
 
   /**
    * Sets a value for net.peer.port
    *
-   * @param netPeerPort Remote port number..
+   * @param netPeerPort Remote port number.
    */
-  public DbMongodbSemanticConvention setNetPeerPort(long netPeerPort);
+  DbMongodbSemanticConvention setNetPeerPort(long netPeerPort);
 
   /**
    * Sets a value for net.transport
    *
-   * @param netTransport Transport protocol used. See note below..
+   * @param netTransport Transport protocol used. See note below.
    */
-  public DbMongodbSemanticConvention setNetTransport(String netTransport);
+  DbMongodbSemanticConvention setNetTransport(String netTransport);
 
   /**
    * Sets a value for db.mongodb.collection
    *
    * @param dbMongodbCollection The collection being accessed within the database stated in
-   *     `db.name`..
+   *     `db.name`.
    */
-  public DbMongodbSemanticConvention setDbMongodbCollection(String dbMongodbCollection);
+  DbMongodbSemanticConvention setDbMongodbCollection(String dbMongodbCollection);
 }
