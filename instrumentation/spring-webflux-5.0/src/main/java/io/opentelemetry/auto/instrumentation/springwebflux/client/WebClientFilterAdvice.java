fee0e1e4b5506e01a7cc827fed764c6e7faa2b77
Munir Abdinur <mabdinur@google.com>
2020-08-07 00:33:02 +0000

    fix gradle

--- instrumentation/spring-webflux-5.0/src/main/java/io/opentelemetry/auto/instrumentation/springwebflux/client/WebClientFilterAdvice.java
+++ instrumentation/spring-webflux-5.0/src/main/java/io/opentelemetry/auto/instrumentation/springwebflux/client/WebClientFilterAdvice.java
@@ -16,7 +16,11 @@
 
 package io.opentelemetry.auto.instrumentation.springwebflux.client;
 
+<<<<<<< HEAD
 import io.opentelemetry.instrumentation.springwebflux.client.WebClientTracingFilter;
+=======
+import io.opentelemetry.auto.instrumentation.spring.webflux.client.shaded.WebClientTracingFilter;
+>>>>>>> 8d9cdd87f... fix gradle
 import net.bytebuddy.asm.Advice;
 import org.springframework.web.reactive.function.client.WebClient;
 
