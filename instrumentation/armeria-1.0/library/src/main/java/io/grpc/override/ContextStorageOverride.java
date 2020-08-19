18cf6507e8de94cf3c71da75bd1f4893e360b783
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-08-19 17:13:11 +0900

    Clean

--- instrumentation/armeria-1.0/library/src/main/java/io/grpc/override/ContextStorageOverride.java
+++ instrumentation/armeria-1.0/library/src/main/java/io/grpc/override/ContextStorageOverride.java
@@ -16,29 +16,14 @@
 
 package io.grpc.override;
 
-import com.linecorp.armeria.common.RequestContext;
 import io.grpc.Context;
 import io.grpc.Context.Storage;
 import io.opentelemetry.instrumentation.armeria.v1_0.common.ArmeriaContextStorageOverride;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
- * A {@link Storage} override of the gRPC context that uses an Armeria {@link RequestContext} as
- * backing storage when available. Armeria provides idioms for propagating {@link RequestContext}
- * which are inherently tied to request processing, so by using it for our storage as well, we can
- * make sure a {@link io.opentelemetry.trace.Span} is propagated throughout the lifecycle of the
- * request with it.
- *
- * <p>This class is automatically picked up by gRPC because it follows a specific package / class
- * naming convention.
- *
- * <p>Contexts created when an Armeria context is not available follow the same pattern as the gRPC
- * default of using a normal {@link ThreadLocal}.
+ * Initialization point for overriding {@link Storage} with {@link ArmeriaContextStorageOverride}.
  */
 public class ContextStorageOverride extends Storage {
-
-  private static final Logger logger = LoggerFactory.getLogger(ContextStorageOverride.class);
 
   private static final ArmeriaContextStorageOverride DELEGATE = new ArmeriaContextStorageOverride();
 
