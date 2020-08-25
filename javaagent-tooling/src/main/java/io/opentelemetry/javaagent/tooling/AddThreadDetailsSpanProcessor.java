dc6809443606fd3a1aea2f5a0e773deafd65c72c
mateuszrzeszutek <mrzeszutek@splunk.com>
2020-08-25 12:46:08 +0200

    Log current thread id and name as span attribute
    * Bump opentelementry-java versions
    * Fix all compilation issues
    * Add a new `SpanProcessor` that appends thread attributes to each span

---
+++ javaagent-tooling/src/main/java/io/opentelemetry/javaagent/tooling/AddThreadDetailsSpanProcessor.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentelemetry.javaagent.tooling;
+
+import io.opentelemetry.sdk.trace.ReadWriteSpan;
+import io.opentelemetry.sdk.trace.ReadableSpan;
+import io.opentelemetry.sdk.trace.SpanProcessor;
+import io.opentelemetry.trace.attributes.SemanticAttributes;
+
+public class AddThreadDetailsSpanProcessor implements SpanProcessor {
+  @Override
+  public void onStart(ReadWriteSpan span) {
+    Thread currentThread = Thread.currentThread();
+    SemanticAttributes.THREAD_ID.set(span, currentThread.getId());
+    SemanticAttributes.THREAD_NAME.set(span, currentThread.getName());
+  }
+
+  @Override
+  public boolean isStartRequired() {
+    return true;
+  }
+
+  @Override
+  public void onEnd(ReadableSpan span) {}
+
+  @Override
+  public boolean isEndRequired() {
+    return false;
+  }
+
+  @Override
+  public void shutdown() {}
+
+  @Override
+  public void forceFlush() {}
+}
