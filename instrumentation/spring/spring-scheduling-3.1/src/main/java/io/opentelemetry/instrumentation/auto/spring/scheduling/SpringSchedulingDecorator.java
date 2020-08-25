d58eca23f8f77182180ab8b1f2b90db1e2021dd5
Munir Abdinur <62392438+mabdinur@users.noreply.github.com>
2020-08-25 06:32:01 +0000

    move instrumentation-core:spring project to instrumentation (#1092)

---
+++ instrumentation/spring/spring-scheduling-3.1/src/main/java/io/opentelemetry/instrumentation/auto/spring/scheduling/SpringSchedulingDecorator.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentelemetry.instrumentation.auto.spring.scheduling;
+
+import io.opentelemetry.OpenTelemetry;
+import io.opentelemetry.instrumentation.api.decorator.BaseDecorator;
+import io.opentelemetry.trace.Tracer;
+import org.springframework.scheduling.support.ScheduledMethodRunnable;
+
+public class SpringSchedulingDecorator extends BaseDecorator {
+  public static final SpringSchedulingDecorator DECORATE = new SpringSchedulingDecorator();
+
+  public static final Tracer TRACER =
+      OpenTelemetry.getTracer("io.opentelemetry.auto.spring-scheduling-3.1");
+
+  private SpringSchedulingDecorator() {}
+
+  public String spanNameOnRun(Runnable runnable) {
+    if (runnable instanceof ScheduledMethodRunnable) {
+      ScheduledMethodRunnable scheduledMethodRunnable = (ScheduledMethodRunnable) runnable;
+      return spanNameForMethod(scheduledMethodRunnable.getMethod());
+    } else {
+      return spanNameForClass(runnable.getClass()) + "/run";
+    }
+  }
+}
