66d37c78baedd95b282045cf60635063b0c56500
dengliming <liming.d.pro@gmail.com>
2020-08-30 12:46:24 +0800

    Add support for redisson instrumentation

---
+++ instrumentation/redisson-3.0/src/main/java/io/opentelemetry/instrumentation/auto/redisson/RedissonClientTracer.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentelemetry.instrumentation.auto.redisson;
+
+import io.netty.channel.Channel;
+import io.opentelemetry.instrumentation.api.tracer.DatabaseClientTracer;
+import io.opentelemetry.instrumentation.auto.api.jdbc.DbSystem;
+import java.net.InetSocketAddress;
+import java.util.List;
+import org.redisson.client.RedisConnection;
+import org.redisson.client.protocol.CommandData;
+import org.redisson.client.protocol.CommandsData;
+
+public class RedissonClientTracer extends DatabaseClientTracer<RedisConnection, Object> {
+
+  public static final RedissonClientTracer TRACER = new RedissonClientTracer();
+
+  @Override
+  protected String getInstrumentationName() {
+    return "io.opentelemetry.auto.redisson-3.0";
+  }
+
+  @Override
+  protected String normalizeQuery(Object args) {
+    String commandName = "Redis Command";
+    // get command
+    if (args instanceof CommandsData) {
+      List<CommandData<?, ?>> commands = ((CommandsData) args).getCommands();
+      if (commands != null && !commands.isEmpty()) {
+        commandName = commands.get(0).getCommand().getName() + "... [bulk]";
+      }
+    } else if (args instanceof CommandData) {
+      commandName = ((CommandData) args).getCommand().getName();
+    }
+    return commandName;
+  }
+
+  @Override
+  protected String dbSystem(RedisConnection connection) {
+    return DbSystem.REDIS;
+  }
+
+  @Override
+  protected String dbUser(RedisConnection o) {
+    return null;
+  }
+
+  @Override
+  protected String dbName(RedisConnection o) {
+    return null;
+  }
+
+  @Override
+  protected InetSocketAddress peerAddress(RedisConnection connection) {
+    Channel channel = connection.getChannel();
+    return (InetSocketAddress) channel.remoteAddress();
+  }
+}
---
+++ instrumentation/redisson-3.0/src/main/java/io/opentelemetry/instrumentation/auto/redisson/RedissonInstrumentation.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentelemetry.instrumentation.auto.redisson;
+
+import static io.opentelemetry.instrumentation.auto.redisson.RedissonClientTracer.TRACER;
+import static java.util.Collections.singletonMap;
+import static net.bytebuddy.matcher.ElementMatchers.isMethod;
+import static net.bytebuddy.matcher.ElementMatchers.named;
+
+import com.google.auto.service.AutoService;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.javaagent.tooling.Instrumenter;
+import io.opentelemetry.trace.Span;
+import java.util.Map;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+import org.redisson.client.RedisConnection;
+
+@AutoService(Instrumenter.class)
+public final class RedissonInstrumentation extends Instrumenter.Default {
+
+  public RedissonInstrumentation() {
+    super("redisson", "redis");
+  }
+
+  @Override
+  public ElementMatcher<TypeDescription> typeMatcher() {
+    return named("org.redisson.client.RedisConnection");
+  }
+
+  @Override
+  public String[] helperClassNames() {
+    return new String[] {
+        packageName + ".RedissonClientTracer",
+    };
+  }
+
+  @Override
+  public Map<? extends ElementMatcher<? super MethodDescription>, String> transformers() {
+    return singletonMap(
+        isMethod()
+            .and(named("send")),
+        RedissonInstrumentation.class.getName() + "$RedissonAdvice");
+  }
+
+  public static class RedissonAdvice {
+
+    @Advice.OnMethodEnter(suppress = Throwable.class)
+    public static void onEnter(
+        @Advice.This RedisConnection connection, @Advice.Argument(0) Object arg,
+        @Advice.Local("otelSpan") Span span,
+        @Advice.Local("otelScope") Scope scope) {
+      span = TRACER.startSpan(connection, arg);
+      scope = TRACER.startScope(span);
+    }
+
+    @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
+    public static void stopSpan(
+        @Advice.Thrown Throwable throwable,
+        @Advice.Local("otelSpan") Span span,
+        @Advice.Local("otelScope") Scope scope) {
+      scope.close();
+
+      if (throwable != null) {
+        TRACER.endExceptionally(span, throwable);
+      } else {
+        TRACER.end(span);
+      }
+    }
+  }
+}
