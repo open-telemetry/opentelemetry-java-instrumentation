1d405359124229b8fb9b9501aa18f59159923552
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-08-31 12:20:38 +0900

    Use a fake OTLP backend for storing smoke tests traces instead of file exporter.

---
+++ smoke-tests/fake-backend/src/main/java/io/opentelemetry/smoketest/fakebackend/FakeCollectorService.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentelemetry.smoketest.fakebackend;
+
+import com.google.common.collect.ImmutableList;
+import io.grpc.stub.StreamObserver;
+import io.opentelemetry.proto.collector.trace.v1.ExportTraceServiceRequest;
+import io.opentelemetry.proto.collector.trace.v1.ExportTraceServiceResponse;
+import io.opentelemetry.proto.collector.trace.v1.TraceServiceGrpc;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+
+class FakeCollectorService extends TraceServiceGrpc.TraceServiceImplBase {
+
+  private final BlockingQueue<ExportTraceServiceRequest> exportRequests =
+      new LinkedBlockingDeque<>();
+
+  List<ExportTraceServiceRequest> getRequests() {
+    return ImmutableList.copyOf(exportRequests);
+  }
+
+  void clearRequests() {
+    exportRequests.clear();
+  }
+
+  @Override
+  public void export(
+      ExportTraceServiceRequest request,
+      StreamObserver<ExportTraceServiceResponse> responseObserver) {
+    exportRequests.add(request);
+    responseObserver.onNext(ExportTraceServiceResponse.getDefaultInstance());
+    responseObserver.onCompleted();
+  }
+}
