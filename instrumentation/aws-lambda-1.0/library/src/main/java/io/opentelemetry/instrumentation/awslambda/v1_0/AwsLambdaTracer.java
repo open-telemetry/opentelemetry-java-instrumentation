01ec2ad6a793436115189ecc294580a421dbfd92
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-09-14 12:57:51 +0900

    Add initial baseline instrumentation for AWS lambda runtime. (#1192)
    * Add initial baseline instrumentation for AWS lambda runtime.
    
    * narc
    
    * Cleanup
    
    * docs
    
    * Remove sentence which is redundant with every manual instrumentation we provide.

---
+++ instrumentation/aws-lambda-1.0/library/src/main/java/io/opentelemetry/instrumentation/awslambda/v1_0/AwsLambdaTracer.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentelemetry.instrumentation.awslambda.v1_0;
+
+import static io.opentelemetry.context.ContextUtils.withScopedContext;
+import static io.opentelemetry.trace.TracingContextUtils.withSpan;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.instrumentation.api.tracer.BaseTracer;
+import io.opentelemetry.trace.Span;
+import io.opentelemetry.trace.Span.Kind;
+import io.opentelemetry.trace.Tracer;
+
+public class AwsLambdaTracer extends BaseTracer {
+
+  public AwsLambdaTracer() {}
+
+  public AwsLambdaTracer(Tracer tracer) {
+    super(tracer);
+  }
+
+  public Span startSpan(Context context) {
+    Span span = startSpan(context.getFunctionName(), Kind.SERVER);
+    // TODO(anuraaga): Migrate to SemanticAttributes when they exist.
+    span.setAttribute("faas.execution", context.getAwsRequestId());
+    return span;
+  }
+
+  /** Creates new scoped context with the given span. */
+  public Scope startScope(Span span) {
+    // TODO we could do this in one go, but TracingContextUtils.CONTEXT_SPAN_KEY is private
+    io.grpc.Context newContext =
+        withSpan(span, io.grpc.Context.current().withValue(CONTEXT_SERVER_SPAN_KEY, span));
+    return withScopedContext(newContext);
+  }
+
+  @Override
+  protected String getInstrumentationName() {
+    return "io.opentelemetry.aws-lambda-1.0";
+  }
+}
