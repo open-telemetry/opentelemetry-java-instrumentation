1651cb7e5520dea180cf305c32a5010e06ca8669
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-09-15 18:19:57 +0900

    bleh

--- instrumentation/armeria-1.0/library/src/test/groovy/io/opentelemetry/instrumentation/armeria/v1_0/ArmeriaTest.groovy
+++ instrumentation/armeria-1.0/library/src/test/groovy/io/opentelemetry/instrumentation/armeria/v1_0/ArmeriaTest.groovy
@@ -24,10 +24,6 @@ import io.opentelemetry.instrumentation.armeria.v1_0.server.OpenTelemetryService
 
 class ArmeriaTest extends AbstractArmeriaTest implements InstrumentationTestTrait {
 
-  def cleanup() {
-    assert testWriter.forceFlushCalled()
-  }
-
   @Override
   ServerBuilder configureServer(ServerBuilder sb) {
     return sb.decorator(OpenTelemetryService.newDecorator())
