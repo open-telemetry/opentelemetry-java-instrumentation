c45a8cd432e687ea487912d9d97381733b4fc094
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2020-09-21 16:51:23 +0200

    Add smoke test for Logback MDC instrumentation - Spring Boot app changes (#1228)

--- smoke-tests/springboot/src/main/java/io/opentelemetry/smoketest/springboot/controller/WebController.java
+++ smoke-tests/springboot/src/main/java/io/opentelemetry/smoketest/springboot/controller/WebController.java
@@ -17,13 +17,18 @@
 package io.opentelemetry.smoketest.springboot.controller;
 
 import io.opentelemetry.extensions.auto.annotations.WithSpan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebController {
+  private static final Logger LOGGER = LoggerFactory.getLogger(WebController.class);
+
   @RequestMapping("/greeting")
   public String greeting() {
+    LOGGER.info("HTTP request received");
     return withSpan();
   }
 
