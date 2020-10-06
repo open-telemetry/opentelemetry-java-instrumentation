c23d919187573dafedf7c7258cc12716f82de24d
Ioannis Mavroukakis <imavroukakis@gmail.com>
2020-10-06 13:23:06 +0100

    spotless fixes

--- instrumentation/servlet/glassfish-testing/src/test/groovy/TestServlets.java
+++ instrumentation/servlet/glassfish-testing/src/test/groovy/TestServlets.java
@@ -4,12 +4,11 @@
  */
 
 import io.opentelemetry.auto.test.base.HttpServerTest;
-
+import java.util.concurrent.Callable;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.concurrent.Callable;
 
 public class TestServlets {
 
