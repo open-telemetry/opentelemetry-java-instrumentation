f5c729dd21d391640a50a3c6ae087ceff23a8bd4
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-10-27 13:26:04 -0700

    Add javadoc for ServletContextPath

--- instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/servlet/ServletContextPath.java
+++ instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/servlet/ServletContextPath.java
@@ -8,6 +8,16 @@ package io.opentelemetry.instrumentation.api.servlet;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.context.ContextKey;
 
+/**
+ * The context key here is used to propagate the servlet context path throughout the request, so
+ * that routing framework instrumentation that updates the span name with a more specific route can
+ * prepend the servlet context path in front of that route.
+ *
+ * <p>This needs to be in the instrumentation-api module, instead of injected as a helper class into
+ * the different modules that need it, in order to make sure that there is only a single instance of
+ * the context key, since otherwise instrumentation across different class loaders would use
+ * different context keys and not be able to share the servlet context path.
+ */
 public class ServletContextPath {
 
   // Keeps track of the servlet context path that needs to be prepended to the route when updating
