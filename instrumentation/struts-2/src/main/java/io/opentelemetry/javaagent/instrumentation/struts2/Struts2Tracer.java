70fed543175562a882bbbfc18174847f431089cb
Vladimir Sor <vsor@splunk.com>
2020-11-09 15:47:18 +0200

    Instrument ActionInvocation to get correct timings. Refactor to use latest o11y SDK and API.

--- instrumentation/struts-2/src/main/java/io/opentelemetry/javaagent/instrumentation/struts2/Struts2Tracer.java
+++ instrumentation/struts-2/src/main/java/io/opentelemetry/javaagent/instrumentation/struts2/Struts2Tracer.java
@@ -1,22 +1,47 @@
 package io.opentelemetry.javaagent.instrumentation.struts2;
 
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.ActionProxy;
+import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.instrumentation.api.tracer.BaseTracer;
-import io.opentelemetry.trace.Span;
-import org.apache.struts2.dispatcher.mapper.ActionMapping;
+import io.opentelemetry.instrumentation.api.typedspan.HttpServerSpan;
 
 public class Struts2Tracer extends BaseTracer {
 
   public static final Struts2Tracer TRACER = new Struts2Tracer();
 
-  public Span startSpan(ActionMapping actionMapping) {
-//    String spanName = namespace + name;
-//    if (method != null) {
-//      spanName += "." + method;
-//    }
-    return tracer.spanBuilder(actionMapping.toString()).startSpan();
+  public Span startSpan(ActionInvocation actionInvocation) {
+    updateHttpRouteInServerSpan(actionInvocation);
+
+    Object action = actionInvocation.getAction();
+    String namespace = action.getClass().getName();
+
+    String method = actionInvocation.getProxy().getMethod();
+
+    String spanName = spanNameForMethod(action.getClass(), method);
+
+    Span strutsSpan = tracer.spanBuilder(spanName).startSpan();
+
+    strutsSpan.setAttribute("action.name", actionInvocation.getProxy().getActionName());
+    strutsSpan.setAttribute("code.namespace", namespace);
+    if (method != null) {
+      strutsSpan.setAttribute("code.function", method);
+    }
+
+
+    return strutsSpan;
   }
 
-  public void endSpan() {
+  private void updateHttpRouteInServerSpan(ActionInvocation actionInvocation) {
+    Span serverSpan = getCurrentServerSpan();
+    if (serverSpan instanceof HttpServerSpan) {
+      ActionProxy proxy = actionInvocation.getProxy();
+      String method = proxy.getMethod();
+      ((HttpServerSpan)serverSpan).setHttpRoute(proxy.getNamespace() + proxy.getActionName() + (method != null ? "." + method : ""));
+    }
+    else {
+      System.out.println("========== MY SERVER SPAN IS NOT HTTP!!!!! " + serverSpan.getClass().getName());
+    }
   }
 
   @Override
