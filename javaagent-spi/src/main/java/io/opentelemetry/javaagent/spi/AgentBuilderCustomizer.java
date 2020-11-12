da639e9d98884d5e31743582f1cb29c61680d67f
Pavol Loffay <p.loffay@gmail.com>
2020-11-12 11:27:21 +0100

    Add more comments
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- javaagent-spi/src/main/java/io/opentelemetry/javaagent/spi/AgentBuilderCustomizer.java
+++ javaagent-spi/src/main/java/io/opentelemetry/javaagent/spi/AgentBuilderCustomizer.java
@@ -9,8 +9,10 @@ import java.lang.instrument.Instrumentation;
 import net.bytebuddy.agent.builder.AgentBuilder;
 
 /**
- * {@link AgentBuilderCustomizer} customizes ByteBuddy agent builder right before {@link
- * AgentBuilder#installOn(Instrumentation)} is called.
+ * {@link AgentBuilderCustomizer} customizes ByteBuddy agent builder right before the agent is
+ * installed - {@link AgentBuilder#installOn(Instrumentation)}. This SPI can be used to customize
+ * {@link AgentBuilder} for vendor specific needs. For example install custom listeners or exclude
+ * classes. Use this SPI carefully because it can change {@link net.bytebuddy.ByteBuddy} behaviour.
  *
  * <p>This is a service provider interface that requires implementations to be registered in {@code
  * META-INF/services} folder.
@@ -22,7 +24,7 @@ public interface AgentBuilderCustomizer {
    * called.
    *
    * @param agentBuilder ByteBuddy agent from {@code AgentInstaller}.
-   * @return
+   * @return customized agent builder.
    */
   AgentBuilder customize(AgentBuilder agentBuilder);
 }
