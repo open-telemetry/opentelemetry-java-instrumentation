467875bfe6d2caca0e5c4680cce398e0c52801e0
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-11-19 16:30:40 -0800

    Spotless

--- instrumentation/opentelemetry-api-beta/src/test/groovy/WithSpanInstrumentationTest.groovy
+++ instrumentation/opentelemetry-api-beta/src/test/groovy/WithSpanInstrumentationTest.groovy
@@ -3,7 +3,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-
 import io.opentelemetry.api.trace.Span
 import io.opentelemetry.instrumentation.test.AgentTestRunner
 import io.opentelemetry.instrumentation.test.utils.ConfigUtils
