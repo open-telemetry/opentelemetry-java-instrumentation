dd1404ee827382417af5cbeb5bf756b60a36b915
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-11-20 19:16:42 -0800

    Rename java-runtime-metrics to runtime-metrics

--- instrumentation/runtime-metrics/library/src/main/java/io/opentelemetry/instrumentation/runtimemetrics/GarbageCollector.java
+++ instrumentation/runtime-metrics/library/src/main/java/io/opentelemetry/instrumentation/runtimemetrics/GarbageCollector.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.opentelemetry.instrumentation.java.runtime;
+package io.opentelemetry.instrumentation.runtimemetrics;
 
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.common.Labels;
--- instrumentation/runtime-metrics/library/src/test/java/io/opentelemetry/instrumentation/runtimemetrics/MemoryPoolsTest.java
+++ instrumentation/runtime-metrics/library/src/test/java/io/opentelemetry/instrumentation/runtimemetrics/MemoryPoolsTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.opentelemetry.instrumentation.java.runtime;
+package io.opentelemetry.instrumentation.runtimemetrics;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
