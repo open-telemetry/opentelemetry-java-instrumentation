c3b6b95ef77b342c0c4de2435ab7a179e43f5cec
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-11-20 18:58:26 -0800

    Remove grizzly artifact

--- instrumentation/async-http-client-1.9/src/main/java/io/opentelemetry/javaagent/instrumentation/asynchttpclient/AsyncHttpClientTracer.java
+++ instrumentation/async-http-client-1.9/src/main/java/io/opentelemetry/javaagent/instrumentation/asynchttpclient/AsyncHttpClientTracer.java
@@ -52,6 +52,6 @@ public class AsyncHttpClientTracer extends HttpClientTracer<Request, Request, Re
 
   @Override
   protected String getInstrumentationName() {
-    return "io.opentelemetry.javaagent.grizzly-client";
+    return "io.opentelemetry.javaagent.async-http-client";
   }
 }
