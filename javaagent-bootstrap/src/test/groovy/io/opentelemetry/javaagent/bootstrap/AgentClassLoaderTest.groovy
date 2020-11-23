6dd7295885c3dd024ff7080fdb9f9bc0a33384e7
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-11-22 17:43:18 -0800

    Codenarc

--- javaagent-bootstrap/src/test/groovy/io/opentelemetry/javaagent/bootstrap/AgentClassLoaderTest.groovy
+++ javaagent-bootstrap/src/test/groovy/io/opentelemetry/javaagent/bootstrap/AgentClassLoaderTest.groovy
@@ -16,7 +16,6 @@
 
 package io.opentelemetry.javaagent.bootstrap
 
-import io.opentelemetry.javaagent.bootstrap.AgentClassLoader
 import java.util.concurrent.Phaser
 import java.util.concurrent.TimeUnit
 import spock.lang.Specification
