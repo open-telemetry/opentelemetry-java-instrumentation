2689653ee797b87d20f615c86a2c8d76265c6e85
Jakub Wach <jwach@splunk.com>
2020-11-27 03:12:52 +0100

    open AWS lambda wrapper for extensions (#1777)

--- instrumentation/aws-lambda-1.0/library/src/main/java/io/opentelemetry/instrumentation/awslambda/v1_0/TracingRequestWrapper.java
+++ instrumentation/aws-lambda-1.0/library/src/main/java/io/opentelemetry/instrumentation/awslambda/v1_0/TracingRequestWrapper.java
@@ -9,4 +9,4 @@ package io.opentelemetry.instrumentation.awslambda.v1_0;
  * Wrapper for {@link TracingRequestHandler}. Allows for wrapping a regular lambda, not proxied
  * through API Gateway. Therefore, HTTP headers propagation is not supported.
  */
-public final class TracingRequestWrapper extends TracingRequestWrapperBase<Object, Object> {}
+public class TracingRequestWrapper extends TracingRequestWrapperBase<Object, Object> {}
