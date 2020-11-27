2222905aa27ca5dee44f892d61b6c8f781a2ebac
Nikita Salnikov-Tarnovski <gnikem@gmail.com>
2020-11-27 21:57:21 +0200

    Preparation for e2e propagators test (#1781)

---
+++ smoke-tests/springboot/src/main/java/io/opentelemetry/smoketest/springboot/controller/PropagatingController.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentelemetry.smoketest.springboot.controller;
+
+import io.opentelemetry.api.trace.Span;
+import java.net.URI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+/**
+ * This controller demonstrates that context propagation works across http calls.
+ * Calling <code>/front</code> should return a string which contains two traceId separated by ";".
+ * First traceId was reported by <code>/front</code> handler, the second one was returned by
+ * <code>/back</code> handler which was called by <code>/front</code>. If context propagation
+ * works correctly, then both values should be the same.
+ */
+@RestController
+public class PropagatingController {
+  private static final Logger LOGGER = LoggerFactory.getLogger(PropagatingController.class);
+
+  private final RestTemplate restTemplate;
+
+  public PropagatingController(RestTemplateBuilder restTemplateBuilder) {
+    this.restTemplate = restTemplateBuilder.build();
+  }
+
+  @RequestMapping("/front")
+  public String front() {
+    URI backend = ServletUriComponentsBuilder
+        .fromCurrentContextPath()
+        .path("/back")
+        .build()
+        .toUri();
+    String backendTraceId = restTemplate.getForObject(backend, String.class);
+    String frontendTraceId = Span.current().getSpanContext().getTraceIdAsHexString();
+    return String.format("%s;%s", frontendTraceId, backendTraceId);
+  }
+
+  @RequestMapping("/back")
+  public String back() {
+    return Span.current().getSpanContext().getTraceIdAsHexString();
+  }
+}
