818b84e02549129b88084143afc27a1ffa24df5d
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-11-27 20:40:51 -0800

    Remove ConfigUtils

--- instrumentation/external-annotations/src/test/groovy/ConfiguredTraceAnnotationsTest.groovy
+++ instrumentation/external-annotations/src/test/groovy/ConfiguredTraceAnnotationsTest.groovy
@@ -3,23 +3,12 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import static io.opentelemetry.javaagent.instrumentation.extannotations.TraceAnnotationsInstrumentationModule.DEFAULT_ANNOTATIONS
 
 import io.opentelemetry.instrumentation.test.AgentTestRunner
-import io.opentelemetry.instrumentation.test.utils.ConfigUtils
-import io.opentelemetry.javaagent.instrumentation.extannotations.TraceAnnotationsInstrumentationModule
 import io.opentelemetry.test.annotation.SayTracedHello
 import java.util.concurrent.Callable
 
 class ConfiguredTraceAnnotationsTest extends AgentTestRunner {
-  static final PREVIOUS_CONFIG = ConfigUtils.updateConfigAndResetInstrumentation {
-    it.setProperty("otel.instrumentation.external-annotations.include",
-      "package.Class\$Name;${OuterClass.InterestingMethod.name}")
-  }
-
-  def specCleanup() {
-    ConfigUtils.setConfig(PREVIOUS_CONFIG)
-  }
 
   def "method with disabled NewRelic annotation should be ignored"() {
     setup:
@@ -45,34 +34,35 @@ class ConfiguredTraceAnnotationsTest extends AgentTestRunner {
     }
   }
 
-  def "test configuration #value"() {
-    setup:
-    def previousConfig = ConfigUtils.updateConfig {
-      if (value) {
-        it.setProperty("otel.instrumentation.external-annotations.include", value)
-      } else {
-        it.remove("otel.instrumentation.external.annotations.include")
-      }
-    }
-
-    expect:
-    TraceAnnotationsInstrumentationModule.AnnotatedMethodsInstrumentation.configureAdditionalTraceAnnotations() == expected.toSet()
-
-    cleanup:
-    ConfigUtils.setConfig(previousConfig)
-
-    where:
-    value                               | expected
-    null                                | DEFAULT_ANNOTATIONS.toList()
-    " "                                 | []
-    "some.Invalid[]"                    | []
-    "some.package.ClassName "           | ["some.package.ClassName"]
-    " some.package.Class\$Name"         | ["some.package.Class\$Name"]
-    "  ClassName  "                     | ["ClassName"]
-    "ClassName"                         | ["ClassName"]
-    "Class\$1;Class\$2;"                | ["Class\$1", "Class\$2"]
-    "Duplicate ;Duplicate ;Duplicate; " | ["Duplicate"]
-  }
+  // TODO rewrite as unit test
+//  def "test configuration #value"() {
+//    setup:
+//    def previousConfig = ConfigUtils.updateConfig {
+//      if (value) {
+//        it.setProperty("otel.instrumentation.external-annotations.include", value)
+//      } else {
+//        it.remove("otel.instrumentation.external.annotations.include")
+//      }
+//    }
+//
+//    expect:
+//    TraceAnnotationsInstrumentationModule.AnnotatedMethodsInstrumentation.configureAdditionalTraceAnnotations() == expected.toSet()
+//
+//    cleanup:
+//    ConfigUtils.setConfig(previousConfig)
+//
+//    where:
+//    value                               | expected
+//    null                                | DEFAULT_ANNOTATIONS.toList()
+//    " "                                 | []
+//    "some.Invalid[]"                    | []
+//    "some.package.ClassName "           | ["some.package.ClassName"]
+//    " some.package.Class\$Name"         | ["some.package.Class\$Name"]
+//    "  ClassName  "                     | ["ClassName"]
+//    "ClassName"                         | ["ClassName"]
+//    "Class\$1;Class\$2;"                | ["Class\$1", "Class\$2"]
+//    "Duplicate ;Duplicate ;Duplicate; " | ["Duplicate"]
+//  }
 
   static class AnnotationTracedCallable implements Callable<String> {
     @OuterClass.InterestingMethod
