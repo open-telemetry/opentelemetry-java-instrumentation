6afad5c5db0ba0d4972fa0627a1f06603f087737
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-11-28 10:52:45 -0800

    Spotless

--- instrumentation/external-annotations/src/test/groovy/IncludeTest.groovy
+++ instrumentation/external-annotations/src/test/groovy/IncludeTest.groovy
@@ -3,7 +3,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-
 import static io.opentelemetry.instrumentation.api.config.Config.normalizePropertyName
 import static io.opentelemetry.javaagent.instrumentation.extannotations.TraceAnnotationsInstrumentationModule.DEFAULT_ANNOTATIONS
 
