db6633c9e6b7cd3318a6e7facf377230ed97e716
Lauri Tulmin <ltulmin@splunk.com>
2020-12-14 18:32:53 +0200

    Add more testable servlets to some test app, update tomcat 10 and jetty 10 versions

---
+++ smoke-tests/matrix/src/main/java/io/opentelemetry/smoketest/matrix/AsyncGreetingServlet.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.smoketest.matrix;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class AsyncGreetingServlet extends GreetingServlet {
+  private static final BlockingQueue<AsyncContext> jobQueue = new LinkedBlockingQueue<>();
+  private static final ExecutorService executor = Executors.newFixedThreadPool(2);
+
+  @Override
+  public void init() throws ServletException {
+    executor.submit(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          while (true) {
+            AsyncContext ac = jobQueue.take();
+            executor.submit(() -> handleRequest(ac));
+         }
+        }
+        catch (InterruptedException e) {
+          Thread.currentThread().interrupt();
+        }
+      }
+    });
+  }
+
+  @Override
+  public void destroy() {
+    executor.shutdownNow();
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+    AsyncContext ac = req.startAsync(req, resp);
+    jobQueue.add(ac);
+  }
+
+  private void handleRequest(AsyncContext ac) {
+    ac.dispatch("/greeting");
+  }
+
+}
---
+++ smoke-tests/matrix/src/main/java/io/opentelemetry/smoketest/matrix/ExceptionRequestListener.java
@@ -0,0 +1,21 @@
+package io.opentelemetry.smoketest.matrix;
+
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.ServletRequestListener;
+
+public class ExceptionRequestListener implements ServletRequestListener {
+
+  @Override
+  public void requestDestroyed(ServletRequestEvent sre) {
+    if ("true".equals(sre.getServletRequest().getParameter("throwOnRequestDestroyed"))) {
+      throw new RuntimeException("This is expected");
+    }
+  }
+
+  @Override
+  public void requestInitialized(ServletRequestEvent sre) {
+    if ("true".equals(sre.getServletRequest().getParameter("throwOnRequestInitialized"))) {
+      throw new RuntimeException("This is expected");
+    }
+  }
+}
