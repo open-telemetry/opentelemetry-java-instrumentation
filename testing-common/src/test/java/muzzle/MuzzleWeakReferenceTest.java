cd58ac816e52e7804484650038c6d8321b0a2295
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2020-12-17 06:54:04 +0100

    Muzzle should add SPI classes defined in helperResourceNames as references (#1918)

--- testing-common/src/test/java/muzzle/MuzzleWeakReferenceTest.java
+++ testing-common/src/test/java/muzzle/MuzzleWeakReferenceTest.java
@@ -23,7 +23,7 @@ public class MuzzleWeakReferenceTest {
     ClassLoader loader = new URLClassLoader(new URL[0], null);
     WeakReference<ClassLoader> clRef = new WeakReference<>(loader);
     ReferenceCollector collector = new ReferenceCollector();
-    collector.collectReferencesFrom(TestClasses.MethodBodyAdvice.class.getName());
+    collector.collectReferencesFromAdvice(TestClasses.MethodBodyAdvice.class.getName());
     Reference[] refs = collector.getReferences().values().toArray(new Reference[0]);
     ReferenceMatcher refMatcher = new ReferenceMatcher(refs);
     refMatcher.getMismatchedReferenceSources(loader);
