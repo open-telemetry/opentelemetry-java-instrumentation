189a68f341d931c8158b370e953f5541724b16df
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-12-21 14:28:00 -0800

    Spotless

--- testing-common/src/main/java/io/opentelemetry/javaagent/testing/common/Java8BytecodeBridge.java
+++ testing-common/src/main/java/io/opentelemetry/javaagent/testing/common/Java8BytecodeBridge.java
@@ -8,9 +8,9 @@ package io.opentelemetry.javaagent.testing.common;
 import io.opentelemetry.context.Context;
 
 /**
- * A helper for scala and kotlin test code, since those tests are compiled to Java 6 bytecode,
- * and so they cannot access methods that rely on new Java 8 bytecode features such as calling a
- * static interface methods.
+ * A helper for scala and kotlin test code, since those tests are compiled to Java 6 bytecode, and
+ * so they cannot access methods that rely on new Java 8 bytecode features such as calling a static
+ * interface methods.
  */
 public final class Java8BytecodeBridge {
 
