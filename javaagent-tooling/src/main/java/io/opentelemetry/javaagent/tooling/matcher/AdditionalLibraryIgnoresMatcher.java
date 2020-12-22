5f19bb225f7fa8a99b96fb0cafa2b6fab555885f
Trask Stalnaker <trask.stalnaker@gmail.com>
2020-12-21 23:53:47 -0800

    Bring back cglib ignore

--- javaagent-tooling/src/main/java/io/opentelemetry/javaagent/tooling/matcher/AdditionalLibraryIgnoresMatcher.java
+++ javaagent-tooling/src/main/java/io/opentelemetry/javaagent/tooling/matcher/AdditionalLibraryIgnoresMatcher.java
@@ -42,6 +42,7 @@ public class AdditionalLibraryIgnoresMatcher<T extends TypeDescription>
         || name.startsWith("com.jayway.jsonpath.")
         || name.startsWith("com.lightbend.lagom.")
         || name.startsWith("javax.el.")
+        || name.startsWith("net.sf.cglib.")
         || name.startsWith("org.apache.lucene.")
         || name.startsWith("org.apache.tartarus.")
         || name.startsWith("org.json.simple.")
