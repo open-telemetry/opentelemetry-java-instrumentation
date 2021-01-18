378d7ae8a05dd47eb083abbe3dcf165f7dbe73fc
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2021-01-18 14:04:50 +0100

    Use constants

--- instrumentation/spring/spring-batch-3.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/spring/batch/SpringBatchInstrumentationConfig.java
+++ instrumentation/spring/spring-batch-3.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/spring/batch/SpringBatchInstrumentationConfig.java
@@ -15,19 +15,23 @@ public final class SpringBatchInstrumentationConfig {
   private static final List<String> INSTRUMENTATION_NAMES =
       unmodifiableList(asList("spring-batch", "spring-batch-3.0"));
 
+  // the item level instrumentation is very chatty so it's disabled by default
+  private static final boolean ITEM_LEVEL_TRACING_ENABLED =
+      Config.get().isInstrumentationPropertyEnabled(instrumentationNames(), "item.enabled", false);
+  private static final boolean CREATE_ROOT_SPAN_FOR_CHUNK =
+      Config.get()
+          .isInstrumentationPropertyEnabled(instrumentationNames(), "chunk.root-span", false);
+
   public static List<String> instrumentationNames() {
     return INSTRUMENTATION_NAMES;
   }
 
-  // the item level instrumentation is very chatty so it's disabled by default
   public static boolean isItemLevelTracingEnabled() {
-    return Config.get()
-        .isInstrumentationPropertyEnabled(instrumentationNames(), "item.enabled", false);
+    return ITEM_LEVEL_TRACING_ENABLED;
   }
 
   public static boolean shouldCreateRootSpanForChunk() {
-    return Config.get()
-        .isInstrumentationPropertyEnabled(instrumentationNames(), "chunk.root-span", false);
+    return CREATE_ROOT_SPAN_FOR_CHUNK;
   }
 
   private SpringBatchInstrumentationConfig() {}
