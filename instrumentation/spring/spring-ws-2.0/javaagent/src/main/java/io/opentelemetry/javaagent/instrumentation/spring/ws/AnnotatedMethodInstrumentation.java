87ddfb8d0edf209bb3635064616dfc327089fb95
Lauri Tulmin <ltulmin@splunk.com>
2021-01-27 10:24:59 +0200

    Add instrumentation for spring-ws (#2111)
    * Add instrumentation for spring-ws
    
    * add spring-ws to supported frameworks
    
    * Add class loader optimization
    
    * Remov unnecessary latestDepTestLibrary
    
    * review fixes
    
    Co-authored-by: Anuraag Agrawal <aanuraag@amazon.co.jp>

---
+++ instrumentation/spring/spring-ws-2.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/spring/ws/AnnotatedMethodInstrumentation.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.spring.ws;
+
+import static io.opentelemetry.javaagent.instrumentation.spring.ws.SpringWsTracer.tracer;
+import static io.opentelemetry.javaagent.tooling.bytebuddy.matcher.ClassLoaderMatcher.hasClassesNamed;
+import static java.util.Collections.singletonMap;
+import static net.bytebuddy.matcher.ElementMatchers.declaresMethod;
+import static net.bytebuddy.matcher.ElementMatchers.isAnnotatedWith;
+import static net.bytebuddy.matcher.ElementMatchers.isMethod;
+import static net.bytebuddy.matcher.ElementMatchers.namedOneOf;
+
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.javaagent.instrumentation.api.CallDepthThreadLocalMap;
+import io.opentelemetry.javaagent.tooling.TypeInstrumentation;
+import java.lang.reflect.Method;
+import java.util.Map;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+
+public class AnnotatedMethodInstrumentation implements TypeInstrumentation {
+  private static final String[] ANNOTATION_CLASSES =
+      new String[] {
+        "org.springframework.ws.server.endpoint.annotation.PayloadRoot",
+        "org.springframework.ws.soap.server.endpoint.annotation.SoapAction",
+        "org.springframework.ws.soap.addressing.server.annotation.Action"
+      };
+
+  @Override
+  public ElementMatcher<ClassLoader> classLoaderOptimization() {
+    return hasClassesNamed("org.springframework.ws.server.endpoint.annotation.PayloadRoot");
+  }
+
+  @Override
+  public ElementMatcher<TypeDescription> typeMatcher() {
+    return declaresMethod(isAnnotatedWith(namedOneOf(ANNOTATION_CLASSES)));
+  }
+
+  @Override
+  public Map<? extends ElementMatcher<? super MethodDescription>, String> transformers() {
+    return singletonMap(
+        isMethod().and(isAnnotatedWith(namedOneOf(ANNOTATION_CLASSES))),
+        AnnotatedMethodInstrumentation.class.getName() + "$AnnotatedMethodAdvice");
+  }
+
+  public static class AnnotatedMethodAdvice {
+
+    @Advice.OnMethodEnter(suppress = Throwable.class)
+    public static void startSpan(
+        @Advice.Origin Method method,
+        @Advice.Local("otelSpan") Span span,
+        @Advice.Local("otelScope") Scope scope) {
+      if (CallDepthThreadLocalMap.incrementCallDepth(PayloadRoot.class) > 0) {
+        return;
+      }
+      span = tracer().startSpan(method);
+      scope = span.makeCurrent();
+    }
+
+    @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
+    public static void stopSpan(
+        @Advice.Thrown Throwable throwable,
+        @Advice.Local("otelSpan") Span span,
+        @Advice.Local("otelScope") Scope scope) {
+      if (scope == null) {
+        return;
+      }
+      CallDepthThreadLocalMap.reset(PayloadRoot.class);
+
+      scope.close();
+      if (throwable == null) {
+        tracer().end(span);
+      } else {
+        tracer().endExceptionally(span, throwable);
+      }
+    }
+  }
+}
---
+++ instrumentation/spring/spring-ws-2.0/javaagent/src/test/groovy/test/boot/SpringWsTest.groovy
@@ -0,0 +1,149 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package test.boot
+
+import io.opentelemetry.api.trace.Span
+import io.opentelemetry.instrumentation.test.AgentTestRunner
+import io.opentelemetry.instrumentation.test.asserts.TraceAssert
+import io.opentelemetry.instrumentation.test.base.HttpServerTestTrait
+import io.opentelemetry.sdk.trace.data.SpanData
+import io.opentelemetry.semconv.trace.attributes.SemanticAttributes
+import io.opentelemetry.test.hello_web_service.HelloRequest
+import io.opentelemetry.test.hello_web_service.HelloRequestSoapAction
+import io.opentelemetry.test.hello_web_service.HelloRequestWsAction
+import io.opentelemetry.test.hello_web_service.HelloResponse
+import org.springframework.boot.SpringApplication
+import org.springframework.context.ConfigurableApplicationContext
+import org.springframework.oxm.jaxb.Jaxb2Marshaller
+import org.springframework.util.ClassUtils
+import org.springframework.ws.client.core.WebServiceMessageCallback
+import org.springframework.ws.client.core.WebServiceTemplate
+import org.springframework.ws.soap.addressing.client.ActionCallback
+import org.springframework.ws.soap.client.SoapFaultClientException
+import org.springframework.ws.soap.client.core.SoapActionCallback
+import spock.lang.Shared
+
+class SpringWsTest extends AgentTestRunner implements HttpServerTestTrait<ConfigurableApplicationContext> {
+
+  @Shared
+  private Jaxb2Marshaller marshaller = new Jaxb2Marshaller()
+
+  def setupSpec() {
+    marshaller.setPackagesToScan(ClassUtils.getPackageName(HelloRequest))
+    marshaller.afterPropertiesSet()
+  }
+
+  @Override
+  ConfigurableApplicationContext startServer(int port) {
+    def app = new SpringApplication(AppConfig, WebServiceConfig)
+    app.setDefaultProperties([
+      "server.port"                 : port,
+      "server.context-path"         : getContextPath(),
+      "server.servlet.contextPath"  : getContextPath(),
+      "server.error.include-message": "always"])
+    def context = app.run()
+    return context
+  }
+
+  @Override
+  void stopServer(ConfigurableApplicationContext ctx) {
+    ctx.close()
+  }
+
+  @Override
+  String getContextPath() {
+    return "/xyz"
+  }
+
+  HelloResponse makeRequest(methodName, name) {
+    WebServiceTemplate webServiceTemplate = new WebServiceTemplate(marshaller)
+
+    Object request = null
+    WebServiceMessageCallback callback = null
+    if ("hello" == methodName) {
+      request = new HelloRequest(name: name)
+    } else if ("helloSoapAction" == methodName) {
+      request = new HelloRequestSoapAction(name: name)
+      callback = new SoapActionCallback("http://opentelemetry.io/test/hello-soap-action")
+    } else if ("helloWsAction" == methodName) {
+      request = new HelloRequestWsAction(name: name)
+      callback = new ActionCallback("http://opentelemetry.io/test/hello-ws-action")
+    } else {
+      throw new IllegalArgumentException(methodName)
+    }
+
+    return (HelloResponse) webServiceTemplate.marshalSendAndReceive(address.resolve("ws").toString(), request, callback)
+  }
+
+  def "test #methodName"(methodName) {
+    setup:
+    HelloResponse response = makeRequest(methodName, "Test")
+
+    expect:
+    response.getMessage() == "Hello Test"
+
+    and:
+    assertTraces(1) {
+      trace(0, 2) {
+        serverSpan(it, 0, getContextPath() + "/ws")
+        handlerSpan(it, 1, methodName, span(0))
+      }
+    }
+
+    where:
+    methodName << ["hello", "helloSoapAction", "helloWsAction"]
+  }
+
+  def "test #methodName exception"(methodName) {
+    when:
+    makeRequest(methodName, "exception")
+
+    then:
+    def error = thrown(SoapFaultClientException)
+    error.getMessage() == "hello exception"
+
+    and:
+    def expectedException = new Exception("hello exception")
+    assertTraces(1) {
+      trace(0, 2) {
+        serverSpan(it, 0, getContextPath() + "/ws", expectedException)
+        handlerSpan(it, 1, methodName, span(0), expectedException)
+      }
+    }
+
+    where:
+    methodName << ["hello", "helloSoapAction", "helloWsAction"]
+  }
+
+  static serverSpan(TraceAssert trace, int index, String operation, Throwable exception = null) {
+    trace.span(index) {
+      hasNoParent()
+      name operation
+      kind Span.Kind.SERVER
+      errored exception != null
+    }
+  }
+
+  static handlerSpan(TraceAssert trace, int index, String methodName, Object parentSpan = null, Throwable exception = null) {
+    trace.span(index) {
+      if (parentSpan == null) {
+        hasNoParent()
+      } else {
+        childOf((SpanData) parentSpan)
+      }
+      name "HelloEndpoint." + methodName
+      kind Span.Kind.INTERNAL
+      errored exception != null
+      if (exception) {
+        errorEvent(exception.class, exception.message)
+      }
+      attributes {
+        "${SemanticAttributes.CODE_NAMESPACE.key}" "test.boot.HelloEndpoint"
+        "${SemanticAttributes.CODE_FUNCTION.key}" methodName
+      }
+    }
+  }
+}
