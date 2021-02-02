32c34de5931d9522daddf8184fdd96fcadf34c09
Lauri Tulmin <ltulmin@splunk.com>
2021-02-02 12:34:49 +0200

    Disable retry on connection failure for okhttp tests (#2154)

--- instrumentation/okhttp/okhttp-3.0/javaagent/src/test/groovy/OkHttp3Test.groovy
+++ instrumentation/okhttp/okhttp-3.0/javaagent/src/test/groovy/OkHttp3Test.groovy
@@ -18,6 +18,7 @@ class OkHttp3Test extends HttpClientTest {
 
   def client = new OkHttpClient.Builder()
     .connectTimeout(CONNECT_TIMEOUT_MS, TimeUnit.MILLISECONDS)
+    .retryOnConnectionFailure(false)
     .build()
 
   @Override
