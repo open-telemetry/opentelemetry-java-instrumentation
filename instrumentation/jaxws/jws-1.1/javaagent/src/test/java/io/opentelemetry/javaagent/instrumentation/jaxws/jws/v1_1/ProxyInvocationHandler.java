7181bcca558a4f166470212c6bdcb44700c68364
Vladimir Sor <vsor@splunk.com>
2021-02-04 12:13:25 +0200

    Added copyright notice, removed test-main.

--- instrumentation/jaxws/jws-1.1/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/jaxws/jws/v1_1/ProxyInvocationHandler.java
+++ instrumentation/jaxws/jws-1.1/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/jaxws/jws/v1_1/ProxyInvocationHandler.java
@@ -2,7 +2,6 @@ package io.opentelemetry.javaagent.instrumentation.jaxws.jws.v1_1;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 
 public class ProxyInvocationHandler implements InvocationHandler {
 
@@ -15,15 +14,5 @@ public class ProxyInvocationHandler implements InvocationHandler {
   @Override
   public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
     return method.invoke(target, args);
-  }
-
-  public static void main(String[] args) {
-    WebServiceDefinitionInterface proxy =
-        (WebServiceDefinitionInterface)
-            Proxy.newProxyInstance(
-                WebServiceFromInterface.class.getClassLoader(),
-                new Class<?>[] {WebServiceDefinitionInterface.class},
-                new ProxyInvocationHandler(new WebServiceFromInterface()));
-    proxy.partOfPublicInterface();
   }
 }
