15019e27b283976ea69486c9b600996e3b15b5b1
addname <addname@126..com>
2021-02-07 14:53:14 +0800

    add rocketmq support

---
+++ instrumentation/rocketmq-client-4.8/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/rocketmq/RocketMqOrderlyConsumeInstrumentation.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.rocketmq;
+
+import static io.opentelemetry.javaagent.tooling.bytebuddy.matcher.AgentElementMatchers.implementsInterface;
+import static io.opentelemetry.instrumentation.rocketmq.RocketMqConsumerTracer.tracer;
+import static java.util.Collections.singletonMap;
+import static net.bytebuddy.matcher.ElementMatchers.nameStartsWith;
+import static net.bytebuddy.matcher.ElementMatchers.named;
+
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.javaagent.tooling.TypeInstrumentation;
+import java.util.List;
+import java.util.Map;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyStatus;
+import org.apache.rocketmq.common.message.MessageExt;
+
+public class RocketMqOrderlyConsumeInstrumentation implements TypeInstrumentation {
+
+  @Override
+  public ElementMatcher<TypeDescription> typeMatcher() {
+    return implementsInterface(
+        named("org.apache.rocketmq.client.consumer.listener.MessageListenerOrderly"));
+  }
+
+  @Override
+  public Map<? extends ElementMatcher<? super MethodDescription>, String> transformers() {
+    return singletonMap(
+        nameStartsWith("consumeMessage"),
+        RocketMqOrderlyConsumeInstrumentation.class.getName() + "$OrderlyConsumeAdvice");
+  }
+
+  public static class OrderlyConsumeAdvice {
+    @Advice.OnMethodEnter(suppress = Throwable.class)
+    public static void onEnter(
+        @Advice.Argument(0) List<MessageExt> msgs,
+        @Advice.Local("otelSpan") Span span,
+        @Advice.Local("otelScope") Scope scope) {
+
+      span = tracer().startSpan(msgs);
+      scope = tracer().startScope(span);
+    }
+
+    @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
+    public static void stopSpan(
+        @Advice.Return ConsumeOrderlyStatus status,
+        @Advice.Thrown Throwable throwable,
+        @Advice.Local("otelSpan") Span span,
+        @Advice.Local("otelScope") Scope scope) {
+      if (scope == null) {
+        return;
+      }
+      tracer().endOrderlySpan(span, status);
+      scope.close();
+      if (throwable == null) {
+        tracer().end(span);
+      } else {
+        tracer().endExceptionally(span, throwable);
+      }
+    }
+  }
+}
---
+++ instrumentation/rocketmq-client-4.8/library/src/main/java/io/opentelemetry/instrumentation/rocketmq/RocketMqClientConfig.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.instrumentation.rocketmq;
+import io.opentelemetry.instrumentation.api.config.Config;
+
+public final class RocketMqClientConfig {
+
+  public static boolean isPropagationEnabled() {
+    return Config.get()
+        .getBooleanProperty("otel.instrumentation.rocketmq.client-propagation", true);
+  }
+
+  private RocketMqClientConfig() {}
+}
