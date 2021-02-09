42850e3ac23201b79a016dde4d1e4057c403b5d3
ET <evantorrie@users.noreply.github.com>
2021-02-09 13:35:41 -0800

    Add OpenTelemetry parameter constructors to all api Tracer classes (#2233)

--- instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/tracer/RpcServerTracer.java
+++ instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/tracer/RpcServerTracer.java
@@ -5,6 +5,7 @@
 
 package io.opentelemetry.instrumentation.api.tracer;
 
+import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.propagation.TextMapPropagator;
 
@@ -12,8 +13,19 @@ public abstract class RpcServerTracer<REQUEST> extends BaseTracer {
 
   protected RpcServerTracer() {}
 
+  /**
+   * Prefer to pass in an OpenTelemetry instance, rather than just a Tracer, so you don't have to
+   * use the GlobalOpenTelemetry Propagator instance.
+   *
+   * @deprecated prefer to pass in an OpenTelemetry instance, instead.
+   */
+  @Deprecated
   protected RpcServerTracer(Tracer tracer) {
     super(tracer);
+  }
+
+  protected RpcServerTracer(OpenTelemetry openTelemetry) {
+    super(openTelemetry);
   }
 
   protected abstract TextMapPropagator.Getter<REQUEST> getGetter();
