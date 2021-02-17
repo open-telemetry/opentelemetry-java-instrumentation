836da8dbfd27c2dc5288a6fd3cf8ce6d8777e7ca
John Watson <jkwatson@gmail.com>
2021-02-16 23:29:58 -0800

    Add a metric for counting the number of suppressed spans. (#2135)
    * Add debug logging when a span is suppressed.
    
    * cleanup from PR comments
    
    * add a `default` to the switch to make checkstyle happy
    
    * rather than logging, use a counter to keep track of the number of suppressed spans.
    
    * update for latest from upstream
    
    * add a default branch for checkstyle

--- instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/tracer/BaseTracer.java
+++ instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/tracer/BaseTracer.java
@@ -7,6 +7,10 @@ package io.opentelemetry.instrumentation.api.tracer;
 
 import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.metrics.GlobalMetricsProvider;
+import io.opentelemetry.api.metrics.LongCounter;
+import io.opentelemetry.api.metrics.Meter;
+import io.opentelemetry.api.metrics.common.Labels;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.SpanBuilder;
 import io.opentelemetry.api.trace.SpanKind;
@@ -21,6 +25,8 @@ import io.opentelemetry.instrumentation.api.context.ContextPropagationDebug;
 import java.lang.reflect.Method;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Base class for all instrumentation specific tracer implementations.
@@ -37,6 +43,11 @@ import java.util.concurrent.TimeUnit;
  * are able to see those attributes in the {@code onStart()} method and can freely read/modify them.
  */
 public abstract class BaseTracer {
+  private static final Meter meterProvider =
+      GlobalMetricsProvider.getMeter("io.opentelemetry.instrumentation.api.tracer");
+
+  private static final Logger log = LoggerFactory.getLogger(BaseTracer.class);
+
   // Keeps track of the server span for the current trace.
   // TODO(anuraaga): Should probably be renamed to local root key since it could be a consumer span
   // or other non-server root.
@@ -49,6 +60,12 @@ public abstract class BaseTracer {
 
   protected final Tracer tracer;
   protected final ContextPropagators propagators;
+  private final LongCounter suppressionCounter =
+      meterProvider
+          .longCounterBuilder("agent.suppressed.spans")
+          .setDescription("The number of spans that have been suppressed by the instrumentation.")
+          .setUnit("1")
+          .build();
 
   public BaseTracer() {
     tracer = GlobalOpenTelemetry.getTracer(getInstrumentationName(), getVersion());
@@ -110,14 +127,29 @@ public abstract class BaseTracer {
   }
 
   protected final boolean shouldStartSpan(SpanKind proposedKind, Context context) {
+    boolean suppressed = false;
     switch (proposedKind) {
       case CLIENT:
-        return !inClientSpan(context);
+        suppressed = inClientSpan(context);
+        break;
       case SERVER:
-        return !inServerSpan(context);
+        suppressed = inServerSpan(context);
+        break;
       default:
-        return true;
+        break;
     }
+    if (suppressed) {
+      suppressionCounter.add(
+          1,
+          // note: an optimization here could be to make sure to re-use the labels,
+          //  since the set of possible labels will be quite small in a given application.
+          //  We could consider lazily creating bound counters for each combination of label values.
+          Labels.of(
+              "span.kind", proposedKind.name(),
+              "instrumentation.name", getInstrumentationName(),
+              "instrumentation.version", getVersion()));
+    }
+    return !suppressed;
   }
 
   private boolean inClientSpan(Context parentContext) {
