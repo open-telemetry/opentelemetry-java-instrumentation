e34223a984630dc1a657c867b9722fd460152b59
Lauri Tulmin <ltulmin@splunk.com>
2021-02-17 11:16:25 +0200

    copy commnet from httpclient-4 instrumentation

--- instrumentation/apache-httpclient/apache-httpclient-5.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/apachehttpclient/v5_0/WrappingStatusSettingResponseHandler.java
+++ instrumentation/apache-httpclient/apache-httpclient-5.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/apachehttpclient/v5_0/WrappingStatusSettingResponseHandler.java
@@ -29,6 +29,11 @@ public class WrappingStatusSettingResponseHandler<T> implements HttpClientRespon
   @Override
   public T handleResponse(ClassicHttpResponse response) throws IOException, HttpException {
     tracer().end(context, response);
+    // ending the span before executing the callback handler (and scoping the callback handler to
+    // the parent context), even though we are inside of a synchronous http client callback
+    // underneath HttpClient.execute(..), in order to not attribute other CLIENT span timings that
+    // may be performed in the callback handler to the http client span (and so we don't end up with
+    // nested CLIENT spans, which we currently suppress)
     try (Scope ignored = parentContext.makeCurrent()) {
       return handler.handleResponse(response);
     }
