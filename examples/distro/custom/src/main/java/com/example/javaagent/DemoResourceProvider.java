25c8422da26169b8621a1bf9a74b6661c6f78bc8
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2021-02-22 18:25:27 +0100

    Code review comments

--- examples/distro/custom/src/main/java/com/example/javaagent/DemoResourceProvider.java
+++ examples/distro/custom/src/main/java/com/example/javaagent/DemoResourceProvider.java
@@ -6,24 +6,9 @@ import io.opentelemetry.sdk.autoconfigure.spi.ResourceProvider;
 import io.opentelemetry.sdk.resources.Resource;
 
 public class DemoResourceProvider implements ResourceProvider {
-
   @Override
   public Resource createResource(ConfigProperties config) {
-    return DemoResource.get();
-  }
-
-  private static final class DemoResource {
-    private static final Resource INSTANCE = buildResource();
-
-    private static Resource buildResource() {
-      Attributes attributes = Attributes.builder().put("custom.resource", "demo").build();
-      return Resource.create(attributes);
-    }
-
-    static Resource get() {
-      return INSTANCE;
-    }
-
-    private DemoResource() {}
+    Attributes attributes = Attributes.builder().put("custom.resource", "demo").build();
+    return Resource.create(attributes);
   }
 }
