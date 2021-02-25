5b502dee6c204b5a2d32ccdf860f175eaf269b84
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-02-25 11:40:18 +0900

    Add a log statement to gRPC smoke test.

--- smoke-tests/grpc/src/main/java/io/opentelemetry/smoketest/grpc/TestService.java
+++ smoke-tests/grpc/src/main/java/io/opentelemetry/smoketest/grpc/TestService.java
@@ -10,13 +10,18 @@ import io.opentelemetry.extension.annotations.WithSpan;
 import io.opentelemetry.proto.collector.trace.v1.ExportTraceServiceRequest;
 import io.opentelemetry.proto.collector.trace.v1.ExportTraceServiceResponse;
 import io.opentelemetry.proto.collector.trace.v1.TraceServiceGrpc;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class TestService extends TraceServiceGrpc.TraceServiceImplBase {
+
+  private static final Logger logger = LogManager.getLogger();
 
   @Override
   public void export(
       ExportTraceServiceRequest request,
       StreamObserver<ExportTraceServiceResponse> responseObserver) {
+    logger.info("Request received");
     withSpan();
     responseObserver.onNext(ExportTraceServiceResponse.getDefaultInstance());
     responseObserver.onCompleted();
