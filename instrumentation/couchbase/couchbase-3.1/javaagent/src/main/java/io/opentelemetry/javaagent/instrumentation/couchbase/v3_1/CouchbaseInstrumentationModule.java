0dde62b4987e43a935d7c2ca4ba7b56f74676da7
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-03-09 09:44:28 +0900

    Load couchbase otel instrumentation for 3.1 (#2524)
    * Load couchbase otel instrumentation for 3.1
    
    * Drift

---
+++ instrumentation/couchbase/couchbase-3.1/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/couchbase/v3_1/CouchbaseInstrumentationModule.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.couchbase.v3_1;
+
+import static io.opentelemetry.javaagent.tooling.bytebuddy.matcher.ClassLoaderMatcher.hasClassesNamed;
+
+import com.google.auto.service.AutoService;
+import io.opentelemetry.javaagent.tooling.InstrumentationModule;
+import io.opentelemetry.javaagent.tooling.TypeInstrumentation;
+import java.util.Collections;
+import java.util.List;
+import net.bytebuddy.matcher.ElementMatcher;
+
+@AutoService(InstrumentationModule.class)
+public class CouchbaseInstrumentationModule extends InstrumentationModule {
+  public CouchbaseInstrumentationModule() {
+    super("couchbase", "couchbase-3.1");
+  }
+
+  @Override
+  protected String[] additionalHelperClassNames() {
+    return new String[] {
+      "com.couchbase.client.tracing.opentelemetry.OpenTelemetryRequestSpan",
+      "com.couchbase.client.tracing.opentelemetry.OpenTelemetryRequestTracer"
+    };
+  }
+
+  @Override
+  public ElementMatcher.Junction<ClassLoader> classLoaderMatcher() {
+    // New class introduced in 3.1, the minimum version we support.
+    // NB: Couchbase does not provide any API guarantees on their core IO artifact so reconsider
+    // instrumenting it instead of each individual JVM artifact if this becomes unmaintainable.
+    return hasClassesNamed("com.couchbase.client.core.cnc.TracingIdentifiers");
+  }
+
+  @Override
+  public List<TypeInstrumentation> typeInstrumentations() {
+    return Collections.singletonList(new CouchbaseEnvironmentInstrumentation());
+  }
+}
