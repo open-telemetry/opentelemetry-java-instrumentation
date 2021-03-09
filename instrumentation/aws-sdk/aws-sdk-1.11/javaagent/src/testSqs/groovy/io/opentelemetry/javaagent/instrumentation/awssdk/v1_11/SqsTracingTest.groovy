f76ce68168d16b4f0788f997846d6798c1296831
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-03-09 16:53:40 +0900

    Drift

--- instrumentation/aws-sdk/aws-sdk-1.11/javaagent/src/testSqs/groovy/io/opentelemetry/javaagent/instrumentation/awssdk/v1_11/SqsTracingTest.groovy
+++ instrumentation/aws-sdk/aws-sdk-1.11/javaagent/src/testSqs/groovy/io/opentelemetry/javaagent/instrumentation/awssdk/v1_11/SqsTracingTest.groovy
@@ -4,10 +4,6 @@
  */
 
 package io.opentelemetry.javaagent.instrumentation.awssdk.v1_11
-/*
- * Copyright The OpenTelemetry Authors
- * SPDX-License-Identifier: Apache-2.0
- */
 
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder
 import io.opentelemetry.instrumentation.awssdk.v1_11.AbstractSqsTracingTest
