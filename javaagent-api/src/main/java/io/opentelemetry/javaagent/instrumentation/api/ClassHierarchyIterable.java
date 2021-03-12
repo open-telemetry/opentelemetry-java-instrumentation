670a9cde3f3b2fd4b4329f6036f223711a588bca
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2021-03-12 16:36:34 +0100

    Minor InstrumentationClassPredicate refactor (don't treat javaagent-t… (#2536)
    * Minor InstrumentationClassPredicate refactor (don't treat javaagent-tooling as helper classes)
    
    * Update javaagent-tooling/src/main/java/io/opentelemetry/javaagent/tooling/muzzle/InstrumentationClassPredicate.java
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>
    
    * fix tests
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>

--- javaagent-api/src/main/java/io/opentelemetry/javaagent/instrumentation/api/ClassHierarchyIterable.java
+++ javaagent-api/src/main/java/io/opentelemetry/javaagent/instrumentation/api/ClassHierarchyIterable.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.opentelemetry.javaagent.tooling;
+package io.opentelemetry.javaagent.instrumentation.api;
 
 import java.util.ArrayDeque;
 import java.util.HashSet;
