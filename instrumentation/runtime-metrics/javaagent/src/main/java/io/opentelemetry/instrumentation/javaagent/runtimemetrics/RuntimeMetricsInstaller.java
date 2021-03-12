f8dd8c8f561240392ce3f4b17dd9caeecaa0499b
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2021-03-12 18:49:25 +0100

    Move RuntimeMetricsInstaller to correct package (#2567)

--- instrumentation/runtime-metrics/javaagent/src/main/java/io/opentelemetry/instrumentation/javaagent/runtimemetrics/RuntimeMetricsInstaller.java
+++ instrumentation/runtime-metrics/javaagent/src/main/java/io/opentelemetry/instrumentation/javaagent/runtimemetrics/RuntimeMetricsInstaller.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.instrumentation.javaagent.instrumentation.runtimemetrics;
+package io.opentelemetry.instrumentation.javaagent.runtimemetrics;
 
 import com.google.auto.service.AutoService;
 import io.opentelemetry.instrumentation.api.config.Config;
