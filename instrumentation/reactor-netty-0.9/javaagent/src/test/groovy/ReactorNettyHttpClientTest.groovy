be1aff28717b1fc986e3aaf2f50e7c969ab2b668
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-03-28 21:58:59 -0700

    Support reactor netty HttpClient.from construction

--- instrumentation/reactor-netty-0.9/javaagent/src/test/groovy/ReactorNettyHttpClientTest.groovy
+++ instrumentation/reactor-netty-0.9/javaagent/src/test/groovy/ReactorNettyHttpClientTest.groovy
@@ -3,50 +3,15 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import io.opentelemetry.instrumentation.test.AgentTestTrait
-import io.opentelemetry.instrumentation.test.base.HttpClientTest
 import io.opentelemetry.instrumentation.test.base.SingleConnection
 import java.util.concurrent.ExecutionException
 import java.util.concurrent.TimeoutException
 import reactor.netty.http.client.HttpClient
-import reactor.netty.http.client.HttpClientResponse
 
-class ReactorNettyHttpClientTest extends HttpClientTest implements AgentTestTrait {
+class ReactorNettyHttpClientTest extends AbstractReactorNettyHttpClientTest {
 
-  @Override
-  boolean testRedirects() {
-    false
-  }
-
-  @Override
-  boolean testConnectionFailure() {
-    false
-  }
-
-  @Override
-  boolean testRemoteConnection() {
-    false
-  }
-
-  @Override
-  String userAgent() {
-    return "ReactorNetty"
-  }
-
-  @Override
-  int doRequest(String method, URI uri, Map<String, String> headers = [:], Closure callback = null) {
-    HttpClientResponse resp = HttpClient.create()
-      .followRedirect(true)
-      .headers({ h -> headers.each { k, v -> h.add(k, v) } })
-      .baseUrl(server.address.toString())
-      ."${method.toLowerCase()}"()
-      .uri(uri.toString())
-      .response()
-      .block()
-    if (callback != null) {
-      callback.call()
-    }
-    return resp.status().code()
+  HttpClient createHttpClient() {
+    return HttpClient.create()
   }
 
   @Override
@@ -76,5 +41,4 @@ class ReactorNettyHttpClientTest extends HttpClientTest implements AgentTestTrai
       }
     }
   }
-
 }
