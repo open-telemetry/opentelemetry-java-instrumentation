1c055bced91857398a229208e39bf883b84c5917
Ago Allikmaa <ago.allikmaa@eesti.ee>
2021-04-02 19:32:11 +0300

    Fix out of date test code.

--- instrumentation/servlet/servlet-javax-common/library/src/test/java/RequestOnlyTracer.java
+++ instrumentation/servlet/servlet-javax-common/library/src/test/java/RequestOnlyTracer.java
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import io.opentelemetry.instrumentation.servlet.ServletAsyncListener;
 import io.opentelemetry.instrumentation.servlet.javax.JavaxServletAccessor;
 import io.opentelemetry.instrumentation.servlet.javax.JavaxServletHttpServerTracer;
 import javax.servlet.http.HttpServletRequest;
@@ -14,6 +15,17 @@ public class RequestOnlyTracer extends JavaxServletHttpServerTracer<Void> {
           @Override
           public Integer getRequestRemotePort(HttpServletRequest httpServletRequest) {
             return null;
+          }
+
+          @Override
+          public boolean isRequestAsyncStarted(HttpServletRequest request) {
+            return false;
+          }
+
+          @Override
+          public void addRequestAsyncListener(HttpServletRequest request,
+              ServletAsyncListener<Void> listener) {
+
           }
 
           @Override
