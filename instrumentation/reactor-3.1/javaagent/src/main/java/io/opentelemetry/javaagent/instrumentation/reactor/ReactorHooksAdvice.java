1d3541a38151da02da7bd89802579662aaa3f3c2
Spindler, Justin <Justin_Spindler@comcast.com>
2021-04-03 20:59:37 -0400

    Add AsyncSpanEndStrategy for Reactor 3.x instrumentation

--- instrumentation/reactor-3.1/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/reactor/ReactorHooksAdvice.java
+++ instrumentation/reactor-3.1/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/reactor/ReactorHooksAdvice.java
@@ -5,6 +5,8 @@
 
 package io.opentelemetry.javaagent.instrumentation.reactor;
 
+import io.opentelemetry.instrumentation.api.tracer.async.AsyncSpanEndStrategies;
+import io.opentelemetry.instrumentation.reactor.ReactorAsyncSpanEndStrategy;
 import io.opentelemetry.instrumentation.reactor.TracingOperator;
 import net.bytebuddy.asm.Advice;
 
@@ -12,5 +14,6 @@ public class ReactorHooksAdvice {
   @Advice.OnMethodExit(suppress = Throwable.class)
   public static void postStaticInitializer() {
     TracingOperator.registerOnEachOperator();
+    AsyncSpanEndStrategies.getInstance().registerStrategy(ReactorAsyncSpanEndStrategy.INSTANCE);
   }
 }
