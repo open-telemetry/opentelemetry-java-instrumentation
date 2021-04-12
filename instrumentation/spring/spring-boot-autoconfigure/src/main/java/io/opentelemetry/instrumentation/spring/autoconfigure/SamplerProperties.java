5719bc30a6d60be551aaa7c3a72cd37663d1d650
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-04-12 03:21:40 -0700

    Sync spring-boot-autoconfigure property names with SDK property names (#2770)
    * Sync spring-boot-autoconfigure property names
    
    * Change from otel.instrumentation to otel.springboot
    
    * Remove duplicate/outdated doc

--- instrumentation/spring/spring-boot-autoconfigure/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/SamplerProperties.java
+++ instrumentation/spring/spring-boot-autoconfigure/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/SamplerProperties.java
@@ -10,24 +10,24 @@ import javax.validation.constraints.DecimalMin;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
- * Configuration for OpenTelemetry Tracer.
+ * Configuration for OpenTelemetry Sampler.
  *
  * <p>Get Sampling Probability
  */
-@ConfigurationProperties(prefix = "opentelemetry.trace.tracer")
-public final class TracerProperties {
+@ConfigurationProperties(prefix = "otel.traces.sampler")
+public final class SamplerProperties {
 
   // if Sample probability == 1: always sample
   // if Sample probability == 0: never sample
   @DecimalMin("0.0")
   @DecimalMax("1.0")
-  private double samplerProbability = 1.0;
+  private double probability = 1.0;
 
-  public double getSamplerProbability() {
-    return samplerProbability;
+  public double getProbability() {
+    return probability;
   }
 
-  public void setSamplerProbability(double samplerProbability) {
-    this.samplerProbability = samplerProbability;
+  public void setProbability(double probability) {
+    this.probability = probability;
   }
 }
--- instrumentation/spring/spring-boot-autoconfigure/src/test/java/io/opentelemetry/instrumentation/spring/autoconfigure/exporters/JaegerSpanExporterAutoConfigurationTest.java
+++ instrumentation/spring/spring-boot-autoconfigure/src/test/java/io/opentelemetry/instrumentation/spring/autoconfigure/exporters/JaegerSpanExporterAutoConfigurationTest.java
@@ -36,7 +36,7 @@ class JaegerSpanExporterAutoConfigurationTest {
   @DisplayName("when exporters are ENABLED should initialize JaegerGrpcSpanExporter bean")
   void exportersEnabled() {
     this.contextRunner
-        .withPropertyValues("opentelemetry.trace.exporters.jaeger.enabled=true")
+        .withPropertyValues("otel.exporter.jaeger.enabled=true")
         .run(
             (context) -> {
               assertThat(context.getBean("otelJaegerSpanExporter", JaegerGrpcSpanExporter.class))
@@ -46,20 +46,20 @@ class JaegerSpanExporterAutoConfigurationTest {
 
   @Test
   @DisplayName(
-      "when opentelemetry.trace.exporter.jaeger properties are set should initialize JaegerSpanExporterProperties")
+      "when otel.exporter.jaeger properties are set should initialize JaegerSpanExporterProperties")
   void handlesProperties() {
     this.contextRunner
         .withPropertyValues(
-            "opentelemetry.trace.exporter.jaeger.enabled=true",
-            "opentelemetry.trace.exporter.jaeger.endpoint=http://localhost:8080/test",
-            "opentelemetry.trace.exporter.jaeger.spantimeout=420ms")
+            "otel.exporter.jaeger.enabled=true",
+            "otel.exporter.jaeger.endpoint=http://localhost:8080/test",
+            "otel.exporter.jaeger.timeout=420ms")
         .run(
             (context) -> {
               JaegerSpanExporterProperties jaegerSpanExporterProperties =
                   context.getBean(JaegerSpanExporterProperties.class);
               assertThat(jaegerSpanExporterProperties.getEndpoint())
                   .isEqualTo("http://localhost:8080/test");
-              assertThat(jaegerSpanExporterProperties.getSpanTimeout()).hasMillis(420);
+              assertThat(jaegerSpanExporterProperties.getTimeout()).hasMillis(420);
             });
   }
 
@@ -67,7 +67,7 @@ class JaegerSpanExporterAutoConfigurationTest {
   @DisplayName("when exporters are DISABLED should NOT initialize JaegerGrpcSpanExporter bean")
   void disabledProperty() {
     this.contextRunner
-        .withPropertyValues("opentelemetry.trace.exporter.jaeger.enabled=false")
+        .withPropertyValues("otel.exporter.jaeger.enabled=false")
         .run(
             (context) -> {
               assertThat(context.containsBean("otelJaegerSpanExporter")).isFalse();
