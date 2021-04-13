f9a37af71de248723ed773403b2ea1b5fc844a89
Spindler, Justin <Justin_Spindler@comcast.com>
2021-04-13 10:14:01 -0400

    Move registration of async strategy to TracingOperator

--- instrumentation/reactor-3.1/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/reactor/ReactorHooksAdvice.java
+++ instrumentation/reactor-3.1/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/reactor/ReactorHooksAdvice.java
@@ -5,8 +5,6 @@
 
 package io.opentelemetry.javaagent.instrumentation.reactor;
 
-import io.opentelemetry.instrumentation.api.tracer.async.AsyncSpanEndStrategies;
-import io.opentelemetry.instrumentation.reactor.ReactorAsyncSpanEndStrategy;
 import io.opentelemetry.instrumentation.reactor.TracingOperator;
 import net.bytebuddy.asm.Advice;
 
@@ -14,6 +12,5 @@ public class ReactorHooksAdvice {
   @Advice.OnMethodExit(suppress = Throwable.class)
   public static void postStaticInitializer() {
     TracingOperator.registerOnEachOperator();
-    AsyncSpanEndStrategies.getInstance().registerStrategy(ReactorAsyncSpanEndStrategy.INSTANCE);
   }
 }
