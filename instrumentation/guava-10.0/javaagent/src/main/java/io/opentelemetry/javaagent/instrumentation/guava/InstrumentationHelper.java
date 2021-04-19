75c1c9adc351b6f46ba5382ce9a2906272c363e6
Spindler, Justin <Justin_Spindler@comcast.com>
2021-04-19 10:26:36 -0400

    Make helper class final and add comment about relying on static initializer

--- instrumentation/guava-10.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/guava/InstrumentationHelper.java
+++ instrumentation/guava-10.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/guava/InstrumentationHelper.java
@@ -8,10 +8,16 @@ package io.opentelemetry.javaagent.instrumentation.guava;
 import io.opentelemetry.instrumentation.api.tracer.async.AsyncSpanEndStrategies;
 import io.opentelemetry.instrumentation.guava.GuavaAsyncSpanEndStrategy;
 
-public class InstrumentationHelper {
+public final class InstrumentationHelper {
   static {
     AsyncSpanEndStrategies.getInstance().registerStrategy(GuavaAsyncSpanEndStrategy.INSTANCE);
   }
 
+  /**
+   * This method is invoked to trigger the runtime system to execute the static initializer block
+   * ensuring that the {@link GuavaAsyncSpanEndStrategy} is registered exactly once.
+   */
   public static void initialize() {}
+
+  private InstrumentationHelper() {}
 }
