599f137d4f751c7f739d7cc44d5817be2e37a108
Lauri Tulmin <ltulmin@splunk.com>
2021-04-21 00:32:02 +0300

    fix tomee test with jdk8

--- instrumentation/jaxrs/jaxrs-2.0/jaxrs-2.0-tomee-testing/src/test/groovy/TomeeRestTest.groovy
+++ instrumentation/jaxrs/jaxrs-2.0/jaxrs-2.0-tomee-testing/src/test/groovy/TomeeRestTest.groovy
@@ -3,5 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import javax.enterprise.inject.Vetoed
+
+// exclude this class from CDI as it causes NullPointerException when tomee is run with jdk8
+@Vetoed
 class TomeeRestTest extends ArquillianRestTest {
 }
