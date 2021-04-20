83d920daa093a7c4f84e70498591dc9f4ad302c6
Jason Plumb <jplumb@splunk.com>
2021-04-20 13:27:39 -0700

    populate the InstrumentationVersion.VERSION from a file within the jar, instead of the manifest.

--- instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/InstrumentationVersion.java
+++ instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/InstrumentationVersion.java
@@ -5,7 +5,29 @@
 
 package io.opentelemetry.instrumentation.api;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class InstrumentationVersion {
-  public static final String VERSION =
-      InstrumentationVersion.class.getPackage().getImplementationVersion();
+  private static final Logger log = LoggerFactory.getLogger(InstrumentationVersion.class);
+  private static final String VERSION_FILE = "/io/opentelemetry/javaagent/shaded/instrumentation/api/instrumentation-version.txt";
+
+  public static final String VERSION = findVersion();
+
+  private static String findVersion() {
+      try {
+        InputStream in = InstrumentationVersion.class.getResourceAsStream(VERSION_FILE);
+        char[] buff = new char[256];
+        int ct = new InputStreamReader(in).read(buff);
+        return new String(buff, 0, ct);
+      } catch (Exception e) {
+        log.warn("Error reading instrumentation version", e);
+        return "UNKNOWN";
+      }
+  }
 }
