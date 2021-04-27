2df0bb4b5191fb0f39ae8a7d4bccd9cec6ac395c
Lauri Tulmin <ltulmin@splunk.com>
2021-04-27 11:22:19 +0300

    Pass jvm arguments from env var on windows payara (#2874)
    * Pass jvm arguments from env var on windows payara
    
    * enable more debug logging
    
    * capture test output
    
    * use the same fake backend as regular smoke test

--- smoke-tests/matrix/src/main/docker/payara/launch.bat
+++ smoke-tests/matrix/src/main/docker/payara/launch.bat
@@ -1,2 +1,1 @@
-java -jar glassfish/lib/client/appserver-cli.jar start-domain domain1
-powershell -command "Get-Content /server/glassfish/domains/domain1/logs/server.log -Wait"
+java %JVM_ARGS% -jar glassfish/lib/client/appserver-cli.jar start-domain --verbose domain1
