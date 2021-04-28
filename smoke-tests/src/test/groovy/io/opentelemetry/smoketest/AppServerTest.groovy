aea5cac9f489e4d5320acc3b5c1db7b3cd27f01e
Lauri Tulmin <ltulmin@splunk.com>
2021-04-28 14:49:31 +0300

    update windows image version

--- smoke-tests/src/test/groovy/io/opentelemetry/smoketest/AppServerTest.groovy
+++ smoke-tests/src/test/groovy/io/opentelemetry/smoketest/AppServerTest.groovy
@@ -43,7 +43,7 @@ abstract class AppServerTest extends SmokeTest {
   @Override
   protected String getTargetImage(String jdk, String serverVersion, boolean windows) {
     String platformSuffix = windows ? "-windows" : ""
-    String extraTag = "20210427.788400023"
+    String extraTag = "20210428.792292726"
     String fullSuffix = "-${serverVersion}-jdk$jdk$platformSuffix-$extraTag"
     return getTargetImagePrefix() + fullSuffix
   }
