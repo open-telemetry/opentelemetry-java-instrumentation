bd2f1f0fda31299dcafb4311996f42784f9a893e
Lauri Tulmin <ltulmin@splunk.com>
2021-05-11 16:28:19 +0300

    Don't set span status to unset

--- instrumentation/apache-dubbo/apache-dubbo-2.7/library/src/main/java/io/opentelemetry/instrumentation/apachedubbo/v2_7/DubboTracer.java
+++ instrumentation/apache-dubbo/apache-dubbo-2.7/library/src/main/java/io/opentelemetry/instrumentation/apachedubbo/v2_7/DubboTracer.java
@@ -10,6 +10,7 @@ import static io.opentelemetry.api.trace.SpanKind.SERVER;
 
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.SpanBuilder;
+import io.opentelemetry.api.trace.StatusCode;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.context.propagation.TextMapGetter;
 import io.opentelemetry.instrumentation.api.tracer.RpcServerTracer;
@@ -46,7 +47,10 @@ class DubboTracer extends RpcServerTracer<RpcInvocation> {
 
   public void end(Context context, Result result) {
     Span span = Span.fromContext(context);
-    span.setStatus(DubboHelper.statusFromResult(result));
+    StatusCode statusCode = DubboHelper.statusFromResult(result);
+    if (statusCode != StatusCode.UNSET) {
+      span.setStatus(statusCode);
+    }
     end(context);
   }
 
