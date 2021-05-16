362b889ea5ce259f1d80567b75a5e640d345e8d6
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-05-15 20:29:42 -0700

    Test

---
+++ instrumentation/internal/internal-url-class-loader/javaagent-integration-tests/src/main/java/instrumentation/TestTypeInstrumentation.java
@@ -0,0 +1,37 @@
+package instrumentation;
+
+import static io.opentelemetry.javaagent.extension.matcher.ClassLoaderMatcher.hasClassesNamed;
+import static net.bytebuddy.matcher.ElementMatchers.named;
+
+import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
+import java.util.Collections;
+import java.util.Map;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+
+public class TestTypeInstrumentation implements TypeInstrumentation {
+  @Override
+  public ElementMatcher<ClassLoader> classLoaderOptimization() {
+    return hasClassesNamed("org.apache.commons.lang3.SystemUtils");
+  }
+
+  @Override
+  public ElementMatcher<TypeDescription> typeMatcher() {
+    return named("org.apache.commons.lang3.SystemUtils");
+  }
+
+  @Override
+  public Map<? extends ElementMatcher<? super MethodDescription>, String> transformers() {
+    return Collections.singletonMap(named("getHostName"),
+        TestTypeInstrumentation.class.getName() + "$GetHostNameAdvice");
+  }
+
+  public static class GetHostNameAdvice {
+    @Advice.OnMethodExit
+    public static void methodExit(@Advice.Return(readOnly = false) String hostName) {
+      hostName = "not-the-host-name";
+    }
+  }
+}
