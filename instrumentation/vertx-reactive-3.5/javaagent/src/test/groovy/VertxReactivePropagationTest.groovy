c28af1f50e828ae8a8350317b4071f2f491f8e69
Ago Allikmaa <ago.allikmaa@eesti.ee>
2021-05-24 22:01:05 +0300

    Vert.x Reactive HTTP server and client concurrency tests (#3061)
    * Vert.x Reactive HTTP server and client concurrency tests
    
    * Concurrency test for VertxReactivePropagationTest

--- instrumentation/vertx-reactive-3.5/javaagent/src/test/groovy/VertxReactivePropagationTest.groovy
+++ instrumentation/vertx-reactive-3.5/javaagent/src/test/groovy/VertxReactivePropagationTest.groovy
@@ -3,16 +3,26 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import static VertxReactiveWebServer.TEST_REQUEST_ID_ATTRIBUTE
+import static VertxReactiveWebServer.TEST_REQUEST_ID_PARAMETER
 import static io.opentelemetry.api.trace.SpanKind.CLIENT
 import static io.opentelemetry.api.trace.SpanKind.SERVER
 import static io.opentelemetry.instrumentation.test.base.HttpServerTest.ServerEndpoint.SUCCESS
+import static io.opentelemetry.instrumentation.test.utils.TraceUtils.basicClientSpan
+import static io.opentelemetry.instrumentation.test.utils.TraceUtils.basicServerSpan
 import static io.opentelemetry.instrumentation.test.utils.TraceUtils.basicSpan
+import static io.opentelemetry.instrumentation.test.utils.TraceUtils.runUnderTrace
 
+import io.opentelemetry.api.GlobalOpenTelemetry
+import io.opentelemetry.api.trace.Span
+import io.opentelemetry.context.Context
 import io.opentelemetry.instrumentation.test.AgentInstrumentationSpecification
 import io.opentelemetry.instrumentation.test.utils.OkHttpUtils
 import io.opentelemetry.instrumentation.test.utils.PortUtils
 import io.opentelemetry.semconv.trace.attributes.SemanticAttributes
 import io.vertx.reactivex.core.Vertx
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import spock.lang.Shared
@@ -85,5 +95,76 @@ class VertxReactivePropagationTest extends AgentInstrumentationSpecification {
     }
   }
 
+  def "should propagate context correctly over vert.x rx-java framework with high concurrency"() {
+    setup:
+    int count = 100
+    def baseUrl = "http://localhost:$port/listProducts"
+    def latch = new CountDownLatch(1)
 
+    def pool = Executors.newFixedThreadPool(8)
+    def propagator = GlobalOpenTelemetry.getPropagators().getTextMapPropagator()
+    def setter = { Request.Builder carrier, String name, String value ->
+      carrier.header(name, value)
+    }
+
+    when:
+    count.times { index ->
+      def job = {
+        latch.await()
+        Request.Builder builder = new Request.Builder().url("$baseUrl?$TEST_REQUEST_ID_PARAMETER=$index").get()
+
+        runUnderTrace("client " + index) {
+          Span.current().setAttribute(TEST_REQUEST_ID_ATTRIBUTE, index)
+          propagator.inject(Context.current(), builder, setter)
+          client.newCall(builder.build()).execute()
+        }
+      }
+      pool.submit(job)
+    }
+
+    latch.countDown()
+
+    then:
+    assertTraces(count) {
+      (0..count - 1).each {
+        trace(it, 5) {
+          def rootSpan = it.span(0)
+          def requestId = Long.valueOf(rootSpan.name.substring("client ".length()))
+
+          basicSpan(it, 0, "client $requestId", null, null) {
+            "${TEST_REQUEST_ID_ATTRIBUTE}" requestId
+          }
+          basicServerSpan(it, 1, "/listProducts", span(0), null) {
+            "${SemanticAttributes.NET_PEER_PORT.key}" Long
+            "${SemanticAttributes.NET_PEER_IP.key}" "127.0.0.1"
+            "${SemanticAttributes.HTTP_URL.key}" "$baseUrl?$TEST_REQUEST_ID_PARAMETER=$requestId"
+            "${SemanticAttributes.HTTP_METHOD.key}" "GET"
+            "${SemanticAttributes.HTTP_STATUS_CODE.key}" 200
+            "${SemanticAttributes.HTTP_FLAVOR.key}" "1.1"
+            "${SemanticAttributes.HTTP_USER_AGENT.key}" String
+            "${SemanticAttributes.HTTP_CLIENT_IP.key}" "127.0.0.1"
+            "${TEST_REQUEST_ID_ATTRIBUTE}" requestId
+          }
+          basicSpan(it, 2, "handleListProducts", span(1), null) {
+            "${TEST_REQUEST_ID_ATTRIBUTE}" requestId
+          }
+          basicSpan(it, 3, "listProducts", span(2), null) {
+            "${TEST_REQUEST_ID_ATTRIBUTE}" requestId
+          }
+          basicClientSpan(it, 4, "SELECT test.products", span(3), null) {
+            "${SemanticAttributes.DB_SYSTEM.key}" "hsqldb"
+            "${SemanticAttributes.DB_NAME.key}" "test"
+            "${SemanticAttributes.DB_USER.key}" "SA"
+            "${SemanticAttributes.DB_CONNECTION_STRING.key}" "hsqldb:mem:"
+            "${SemanticAttributes.DB_STATEMENT.key}" "SELECT id AS request$requestId, name, price, weight FROM products"
+            "${SemanticAttributes.DB_OPERATION.key}" "SELECT"
+            "${SemanticAttributes.DB_SQL_TABLE.key}" "products"
+          }
+        }
+      }
+    }
+
+    cleanup:
+    pool.shutdownNow()
+  }
 }
