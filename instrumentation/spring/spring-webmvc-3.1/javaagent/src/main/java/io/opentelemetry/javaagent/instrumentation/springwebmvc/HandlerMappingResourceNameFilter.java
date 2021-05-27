985cfca4d517b561e4790f39ac70fb4be199008a
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-05-26 20:09:18 -0700

    Fix potential ClassNotFoundException

--- instrumentation/spring/spring-webmvc-3.1/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/springwebmvc/HandlerMappingResourceNameFilter.java
+++ instrumentation/spring/spring-webmvc-3.1/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/springwebmvc/HandlerMappingResourceNameFilter.java
@@ -121,7 +121,6 @@ public class HandlerMappingResourceNameFilter implements Filter, Ordered {
     public BeanDefinition() {
       setScope(SCOPE_SINGLETON);
       setBeanClass(HandlerMappingResourceNameFilter.class);
-      setBeanClassName(HandlerMappingResourceNameFilter.class.getName());
     }
   }
 }
