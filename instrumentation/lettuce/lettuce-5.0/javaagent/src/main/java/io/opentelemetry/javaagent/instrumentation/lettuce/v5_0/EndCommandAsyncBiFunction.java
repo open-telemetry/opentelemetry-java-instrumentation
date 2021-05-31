7d46c336b0e3642f5201aaf79d33f31b0482cbfc
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-05-31 07:49:25 -0700

    Update lettuce-5.0 to Instrumenter API (#3125)

---
+++ instrumentation/lettuce/lettuce-5.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/lettuce/v5_0/EndCommandAsyncBiFunction.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.lettuce.v5_0;
+
+import static io.opentelemetry.javaagent.instrumentation.lettuce.v5_0.LettuceInstrumenters.instrumenter;
+
+import io.lettuce.core.protocol.RedisCommand;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.instrumentation.api.config.Config;
+import java.util.concurrent.CancellationException;
+import java.util.function.BiFunction;
+
+/**
+ * Callback class to close the command span on an error or a success in the RedisFuture returned by
+ * the lettuce async API.
+ *
+ * @param <T> the normal completion result
+ * @param <U> the error
+ * @param <R> the return type, should be null since nothing else should happen from tracing
+ *     standpoint after the span is closed
+ */
+public class EndCommandAsyncBiFunction<T, U extends Throwable, R>
+    implements BiFunction<T, Throwable, R> {
+
+  private static final boolean CAPTURE_EXPERIMENTAL_SPAN_ATTRIBUTES =
+      Config.get()
+          .getBooleanProperty("otel.instrumentation.lettuce.experimental-span-attributes", false);
+
+  private final Context context;
+  private final RedisCommand<?, ?, ?> command;
+
+  public EndCommandAsyncBiFunction(Context context, RedisCommand<?, ?, ?> command) {
+    this.context = context;
+    this.command = command;
+  }
+
+  @Override
+  public R apply(T t, Throwable throwable) {
+    end(context, command, throwable);
+    return null;
+  }
+
+  public static void end(Context context, RedisCommand<?, ?, ?> command, Throwable throwable) {
+    if (throwable instanceof CancellationException) {
+      if (CAPTURE_EXPERIMENTAL_SPAN_ATTRIBUTES) {
+        Span.fromContext(context).setAttribute("lettuce.command.cancelled", true);
+      }
+      // and don't report this as an error
+      throwable = null;
+    }
+    instrumenter().end(context, command, null, throwable);
+  }
+}
