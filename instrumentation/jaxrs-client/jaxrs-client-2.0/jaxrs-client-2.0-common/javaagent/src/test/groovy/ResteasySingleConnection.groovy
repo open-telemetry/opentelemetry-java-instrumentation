b277fd96c575c39dd2685eb932b5ef2b1af28d46
Ago Allikmaa <ago.allikmaa@eesti.ee>
2021-06-07 20:31:28 +0100

    JAX-RS RestEasy single connection test, comment on others where not applicable (#3187)

---
+++ instrumentation/jaxrs-client/jaxrs-client-2.0/jaxrs-client-2.0-common/javaagent/src/test/groovy/ResteasySingleConnection.groovy
@@ -0,0 +1,58 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import io.opentelemetry.instrumentation.test.base.SingleConnection
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
+import javax.ws.rs.core.MediaType
+import org.apache.http.client.params.ClientPNames
+import org.jboss.resteasy.client.jaxrs.ResteasyClient
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder
+import org.jboss.resteasy.client.jaxrs.engines.ApacheHttpClient4Engine
+
+class ResteasySingleConnection implements SingleConnection {
+  private final ResteasyClient client
+  private final String host
+  private final int port
+
+  ResteasySingleConnection(String host, int port) {
+    this.host = host
+    this.port = port
+    this.client = new ResteasyClientBuilder()
+      .establishConnectionTimeout(5000, TimeUnit.MILLISECONDS)
+      .connectionPoolSize(1)
+      .build()
+
+    ((ApacheHttpClient4Engine) client.httpEngine()).httpClient.params
+      .setParameter(ClientPNames.CONN_MANAGER_TIMEOUT, 20000L)
+  }
+
+  @Override
+  int doRequest(String path, Map<String, String> headers) throws ExecutionException, InterruptedException, TimeoutException {
+    String requestId = Objects.requireNonNull(headers.get(REQUEST_ID_HEADER))
+
+    URI uri
+    try {
+      uri = new URL("http", host, port, path).toURI()
+    } catch (MalformedURLException e) {
+      throw new ExecutionException(e)
+    }
+
+    def requestBuilder = client.target(uri).request(MediaType.TEXT_PLAIN)
+    headers.each { requestBuilder.header(it.key, it.value) }
+
+    def response = requestBuilder.buildGet().invoke()
+    response.close()
+
+    String responseId = response.getHeaderString(REQUEST_ID_HEADER)
+    if (requestId != responseId) {
+      throw new IllegalStateException(
+        String.format("Received response with id %s, expected %s", responseId, requestId))
+    }
+
+    return response.getStatus()
+  }
+}
