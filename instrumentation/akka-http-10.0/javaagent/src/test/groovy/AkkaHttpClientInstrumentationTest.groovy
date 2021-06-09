00f0ca8fb34d51190f432bd2c5c7422c06f589aa
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-06-09 13:24:27 +0900

    Handle split responses in netty41 and akka

--- instrumentation/akka-http-10.0/javaagent/src/test/groovy/AkkaHttpClientInstrumentationTest.groovy
+++ instrumentation/akka-http-10.0/javaagent/src/test/groovy/AkkaHttpClientInstrumentationTest.groovy
@@ -10,6 +10,7 @@ import akka.actor.ActorSystem
 import akka.http.javadsl.Http
 import akka.http.javadsl.model.HttpMethods
 import akka.http.javadsl.model.HttpRequest
+import akka.http.javadsl.model.HttpResponse
 import akka.http.javadsl.model.headers.RawHeader
 import akka.stream.ActorMaterializer
 import io.opentelemetry.instrumentation.test.AgentTestTrait
@@ -34,17 +35,20 @@ class AkkaHttpClientInstrumentationTest extends HttpClientTest<HttpRequest> impl
 
   @Override
   int sendRequest(HttpRequest request, String method, URI uri, Map<String, String> headers) {
-    return Http.get(system)
+    HttpResponse response = Http.get(system)
       .singleRequest(request, materializer)
       .toCompletableFuture()
       .get(10, TimeUnit.SECONDS)
-      .status()
-      .intValue()
+
+    response.discardEntityBytes(materializer)
+
+    return response.status().intValue()
   }
 
   @Override
   void sendRequestWithCallback(HttpRequest request, String method, URI uri, Map<String, String> headers, RequestResult requestResult) {
     Http.get(system).singleRequest(request, materializer).whenComplete {response, throwable ->
+      response.discardEntityBytes(materializer)
       requestResult.complete({ response.status().intValue() }, throwable)
     }
   }
