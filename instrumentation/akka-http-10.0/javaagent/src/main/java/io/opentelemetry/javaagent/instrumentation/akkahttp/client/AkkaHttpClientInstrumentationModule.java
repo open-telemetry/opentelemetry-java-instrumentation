06033ed3f2922008278e1a0838e445133955629e
Lauri Tulmin <ltulmin@splunk.com>
2021-06-12 03:17:11 +0300

    Akka http: propagate context to callbacks (#3263)
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>

--- instrumentation/akka-http-10.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/akkahttp/client/AkkaHttpClientInstrumentationModule.java
+++ instrumentation/akka-http-10.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/akkahttp/client/AkkaHttpClientInstrumentationModule.java
@@ -5,7 +5,7 @@
 
 package io.opentelemetry.javaagent.instrumentation.akkahttp.client;
 
-import static java.util.Collections.singletonList;
+import static java.util.Arrays.asList;
 
 import com.google.auto.service.AutoService;
 import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
@@ -20,6 +20,6 @@ public class AkkaHttpClientInstrumentationModule extends InstrumentationModule {
 
   @Override
   public List<TypeInstrumentation> typeInstrumentations() {
-    return singletonList(new HttpExtClientInstrumentation());
+    return asList(new HttpExtClientInstrumentation(), new PoolMasterActorInstrumentation());
   }
 }
