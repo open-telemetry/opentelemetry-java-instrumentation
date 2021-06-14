cdcfe7b7b4bcd8b23a694da1594114a1151266a2
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-06-13 19:33:42 -0700

    Cache simple name using ClassValue

--- instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/tracer/ClassNames.java
+++ instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/tracer/ClassNames.java
@@ -7,21 +7,29 @@ package io.opentelemetry.instrumentation.api.tracer;
 
 public class ClassNames {
 
+  private static final ClassValue<String> simpleNames =
+      new ClassValue<String>() {
+        @Override
+        protected String computeValue(Class<?> type) {
+          if (!type.isAnonymousClass()) {
+            return type.getSimpleName();
+          }
+          String className = type.getName();
+          if (type.getPackage() != null) {
+            String pkgName = type.getPackage().getName();
+            if (!pkgName.isEmpty()) {
+              className = className.substring(pkgName.length() + 1);
+            }
+          }
+          return className;
+        }
+      };
+
   /**
    * This method is used to generate a simple name based on a given class reference, e.g. for use in
    * span names and span attributes. Anonymous classes are named based on their parent.
    */
-  public static String simpleName(Class<?> clazz) {
-    if (!clazz.isAnonymousClass()) {
-      return clazz.getSimpleName();
-    }
-    String className = clazz.getName();
-    if (clazz.getPackage() != null) {
-      String pkgName = clazz.getPackage().getName();
-      if (!pkgName.isEmpty()) {
-        className = className.substring(pkgName.length() + 1);
-      }
-    }
-    return className;
+  public static String simpleName(Class<?> type) {
+    return simpleNames.get(type);
   }
 }
