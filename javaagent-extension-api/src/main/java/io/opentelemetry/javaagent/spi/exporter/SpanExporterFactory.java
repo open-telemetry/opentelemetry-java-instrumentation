198206527037eea3b3b7d173c7f14c13abf9edf6
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2021-06-14 20:56:02 +0200

    Deprecate SpanExporterFactory in favor of ConfigurableSpanExporterProvider (#3299)

--- javaagent-extension-api/src/main/java/io/opentelemetry/javaagent/spi/exporter/SpanExporterFactory.java
+++ javaagent-extension-api/src/main/java/io/opentelemetry/javaagent/spi/exporter/SpanExporterFactory.java
@@ -13,7 +13,11 @@ import java.util.Set;
  * A {@link SpanExporterFactory} acts as the bootstrap for a {@link SpanExporter} implementation. An
  * exporter must register its implementation of a {@link SpanExporterFactory} through the Java SPI
  * framework.
+ *
+ * @deprecated Use {@code io.opentelemetry.sdk.autoconfigure.spi.ConfigurableSpanExporterProvider}
+ *     from the {@code opentelemetry-sdk-extension-autoconfigure} instead.
  */
+@Deprecated
 public interface SpanExporterFactory {
   /**
    * Creates an instance of a {@link SpanExporter} based on the provided configuration.
