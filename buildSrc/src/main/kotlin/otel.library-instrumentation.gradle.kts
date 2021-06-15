ce51a733aca4b6e232d8b2b341655b2b3131766b
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-06-15 22:45:41 +0900

    Migrate instrumentation-library and common to plugin.

---
+++ buildSrc/src/main/kotlin/otel.library-instrumentation.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+  id("otel.java-conventions")
+  id("otel.publish-conventions")
+  id("otel.instrumentation-conventions")
+}
+
+extra["mavenGroupId"] = "io.opentelemetry.instrumentation"
+
+base.archivesBaseName = projectDir.parentFile.name
+
+dependencies {
+  api(project(":instrumentation-api"))
+
+  api("io.opentelemetry:opentelemetry-api")
+
+  testImplementation(project(":testing-common"))
+}
