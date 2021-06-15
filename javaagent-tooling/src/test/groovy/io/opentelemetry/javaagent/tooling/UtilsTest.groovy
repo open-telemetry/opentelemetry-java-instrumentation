6052432f8467136fa5bbf34f5edd52743357572f
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2021-06-15 19:32:41 +0200

    Remove unused Utils method

--- javaagent-tooling/src/test/groovy/io/opentelemetry/javaagent/tooling/UtilsTest.groovy
+++ javaagent-tooling/src/test/groovy/io/opentelemetry/javaagent/tooling/UtilsTest.groovy
@@ -9,15 +9,6 @@ import spock.lang.Specification
 
 class UtilsTest extends Specification {
 
-  def "getStackTraceAsString() returns the stack trace as a single new line separated string"() {
-    setup:
-    def stackTrace = Utils.stackTraceAsString
-
-    expect:
-    stackTrace.contains('io.opentelemetry.javaagent.tooling.Utils')
-    stackTrace.contains(System.getProperty("line.separator"))
-  }
-
   def "getResourceName() adds suffix and converts dots to slashes"() {
     setup:
     def result = Utils.getResourceName("com.example.Something")
