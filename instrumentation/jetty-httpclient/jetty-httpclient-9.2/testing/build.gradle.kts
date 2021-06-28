47c8a9bfa56d77934df75ba1aaff2d47992db783
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-06-28 20:25:28 +0900

    Migrate jetty httpclient build files to kotlin (#3421)

---
+++ instrumentation/jetty-httpclient/jetty-httpclient-9.2/testing/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+  id("otel.java-conventions")
+}
+
+//Jetty client 9.2 is the best starting point, HttpClient.send() is stable there
+val jettyVers_base9 = "9.2.0.v20140526"
+
+dependencies {
+  api(project(":testing-common"))
+
+
+  api("org.eclipse.jetty:jetty-client:${jettyVers_base9}")
+
+  implementation("org.junit.jupiter:junit-jupiter-api")
+
+  implementation("org.codehaus.groovy:groovy-all")
+  implementation("io.opentelemetry:opentelemetry-api")
+  implementation("org.spockframework:spock-core")
+}
