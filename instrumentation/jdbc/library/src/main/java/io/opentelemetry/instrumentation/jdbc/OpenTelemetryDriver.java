09851dc2e4de490d0a21fc163fe46e789c42bae9
Alexey Zhokhov <alexey@zhokhov.com>
2021-06-28 13:24:57 +0800

    Update instrumentation/jdbc/library/src/main/java/io/opentelemetry/instrumentation/jdbc/OpenTelemetryDriver.java
    Co-authored-by: Anuraag Agrawal <anuraaga@gmail.com>

--- instrumentation/jdbc/library/src/main/java/io/opentelemetry/instrumentation/jdbc/OpenTelemetryDriver.java
+++ instrumentation/jdbc/library/src/main/java/io/opentelemetry/instrumentation/jdbc/OpenTelemetryDriver.java
@@ -198,7 +198,7 @@ public final class OpenTelemetryDriver implements Driver {
 
   @Override
   public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
-    if (url == null || url.trim().length() == 0) {
+    if (url == null || url.trim().isEmpty()) {
       throw new IllegalArgumentException("url is required");
     }
 
