371a855bf5e83f7d7177bba9e42888c4a25dcda4
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-06-30 16:49:59 +0900

    Migrate

--- testing-common/build.gradle.kts
+++ testing-common/build.gradle.kts
@@ -3,53 +3,57 @@ plugins {
   id("otel.publish-conventions")
 }
 
-description = 'OpenTelemetry Javaagent testing commons'
-group = 'io.opentelemetry.javaagent'
+description = "OpenTelemetry Javaagent testing commons"
+group = "io.opentelemetry.javaagent"
 
 dependencies {
-  api "org.codehaus.groovy:groovy-all"
-  api "org.spockframework:spock-core"
-  implementation "org.junit.jupiter:junit-jupiter-api"
+  api("org.codehaus.groovy:groovy-all")
+  api("org.spockframework:spock-core")
+  implementation("org.junit.jupiter:junit-jupiter-api")
 
-  api "io.opentelemetry:opentelemetry-api"
-  api "io.opentelemetry:opentelemetry-semconv"
-  api "io.opentelemetry:opentelemetry-sdk"
-  api "io.opentelemetry:opentelemetry-sdk-metrics"
-  api "io.opentelemetry:opentelemetry-sdk-testing"
+  api("io.opentelemetry:opentelemetry-api")
+  api("io.opentelemetry:opentelemetry-semconv")
+  api("io.opentelemetry:opentelemetry-sdk")
+  api("io.opentelemetry:opentelemetry-sdk-metrics")
+  api("io.opentelemetry:opentelemetry-sdk-testing")
 
-  api project(path: ":testing:armeria-shaded-for-testing", configuration: "shadow")
+  api(project(path = ":testing:armeria-shaded-for-testing", configuration = "shadow"))
 
   implementation("io.opentelemetry:opentelemetry-proto") {
     // Only need the proto, not gRPC.
-    exclude group: 'io.grpc'
+    exclude("io.grpc")
   }
 
-  implementation "com.google.guava:guava"
-  implementation "net.bytebuddy:byte-buddy"
-  implementation "net.bytebuddy:byte-buddy-agent"
-  implementation "org.slf4j:slf4j-api"
-  implementation "ch.qos.logback:logback-classic"
-  implementation "org.slf4j:log4j-over-slf4j"
-  implementation "org.slf4j:jcl-over-slf4j"
-  implementation "org.slf4j:jul-to-slf4j"
-  implementation "io.opentelemetry:opentelemetry-extension-annotations"
-  implementation "io.opentelemetry:opentelemetry-exporter-logging"
-  implementation project(':instrumentation-api')
+  implementation("com.google.guava:guava")
+  implementation("net.bytebuddy:byte-buddy")
+  implementation("net.bytebuddy:byte-buddy-agent")
+  implementation("org.slf4j:slf4j-api")
+  implementation("ch.qos.logback:logback-classic")
+  implementation("org.slf4j:log4j-over-slf4j")
+  implementation("org.slf4j:jcl-over-slf4j")
+  implementation("org.slf4j:jul-to-slf4j")
+  implementation("io.opentelemetry:opentelemetry-extension-annotations")
+  implementation("io.opentelemetry:opentelemetry-exporter-logging")
+  implementation(project(":instrumentation-api"))
 
-  annotationProcessor "com.google.auto.service:auto-service"
-  compileOnly "com.google.auto.service:auto-service"
+  annotationProcessor("com.google.auto.service:auto-service")
+  compileOnly("com.google.auto.service:auto-service")
 
-  testImplementation "org.assertj:assertj-core"
+  testImplementation("org.assertj:assertj-core")
 
-  testImplementation project(':javaagent-api')
-  testImplementation project(':javaagent-tooling')
-  testImplementation project(':javaagent-bootstrap')
-  testImplementation project(':javaagent-extension-api')
-  testImplementation project(':instrumentation:external-annotations:javaagent')
+  testImplementation(project(":javaagent-api"))
+  testImplementation(project(":javaagent-tooling"))
+  testImplementation(project(":javaagent-bootstrap"))
+  testImplementation(project(":javaagent-extension-api"))
+  testImplementation(project(":instrumentation:external-annotations:javaagent"))
 
   // We have autoservices defined in test subtree, looks like we need this to be able to properly rebuild this
-  testAnnotationProcessor "com.google.auto.service:auto-service"
-  testCompileOnly "com.google.auto.service:auto-service"
+  testAnnotationProcessor("com.google.auto.service:auto-service")
+  testCompileOnly("com.google.auto.service:auto-service")
 }
 
-javadoc.enabled = false
+tasks {
+  javadoc {
+    enabled = false
+  }
+}
