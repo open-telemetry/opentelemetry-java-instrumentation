0a02925002229822d8f02e995f9cd79ad0751e7c
Spindler, Justin <Justin_Spindler@comcast.com>
2021-07-01 08:55:14 -0400

    Port RxJava 3 async strategy to new interface

--- instrumentation/rxjava/rxjava-3.0/library/src/main/java/io/opentelemetry/instrumentation/rxjava3/RxJava3AsyncOperationEndStrategyBuilder.java
+++ instrumentation/rxjava/rxjava-3.0/library/src/main/java/io/opentelemetry/instrumentation/rxjava3/RxJava3AsyncOperationEndStrategyBuilder.java
@@ -5,19 +5,19 @@
 
 package io.opentelemetry.instrumentation.rxjava3;
 
-public final class RxJava3AsyncSpanEndStrategyBuilder {
+public final class RxJava3AsyncOperationEndStrategyBuilder {
 
   private boolean captureExperimentalSpanAttributes;
 
-  RxJava3AsyncSpanEndStrategyBuilder() {}
+  RxJava3AsyncOperationEndStrategyBuilder() {}
 
-  public RxJava3AsyncSpanEndStrategyBuilder setCaptureExperimentalSpanAttributes(
+  public RxJava3AsyncOperationEndStrategyBuilder setCaptureExperimentalSpanAttributes(
       boolean captureExperimentalSpanAttributes) {
     this.captureExperimentalSpanAttributes = captureExperimentalSpanAttributes;
     return this;
   }
 
-  public RxJava3AsyncSpanEndStrategy build() {
-    return new RxJava3AsyncSpanEndStrategy(captureExperimentalSpanAttributes);
+  public RxJava3AsyncOperationEndStrategy build() {
+    return new RxJava3AsyncOperationEndStrategy(captureExperimentalSpanAttributes);
   }
 }
