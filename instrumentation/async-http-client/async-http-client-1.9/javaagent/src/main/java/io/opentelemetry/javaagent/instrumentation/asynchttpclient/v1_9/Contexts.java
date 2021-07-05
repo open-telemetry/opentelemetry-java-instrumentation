aa26139bc9b2f36e5b9eb60d86c2610f903f45b0
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2021-07-05 13:10:12 +0200

    Convert Pair class to AutoValue

---
+++ instrumentation/async-http-client/async-http-client-1.9/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/asynchttpclient/v1_9/Contexts.java
@@ -0,0 +1,18 @@
+package io.opentelemetry.javaagent.instrumentation.asynchttpclient.v1_9;
+
+import com.google.auto.value.AutoValue;
+import io.opentelemetry.context.Context;
+
+@AutoValue
+public abstract class Contexts {
+
+  public static Contexts create(Context parentContext, Context context) {
+    return new AutoValue_Contexts(parentContext, context);
+  }
+
+  public abstract Context getParentContext();
+
+  public abstract Context getContext();
+
+  Contexts() {}
+}
