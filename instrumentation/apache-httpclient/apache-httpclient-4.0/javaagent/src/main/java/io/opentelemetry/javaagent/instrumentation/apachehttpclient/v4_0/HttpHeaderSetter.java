b030b1ca1086c47b79f4e549059f3811bfa257c9
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-07-05 12:53:32 -0700

    Simpler Apache HttpClient request wrapper (#3489)

--- instrumentation/apache-httpclient/apache-httpclient-4.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/apachehttpclient/v4_0/HttpHeaderSetter.java
+++ instrumentation/apache-httpclient/apache-httpclient-4.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/apachehttpclient/v4_0/HttpHeaderSetter.java
@@ -6,12 +6,11 @@
 package io.opentelemetry.javaagent.instrumentation.apachehttpclient.v4_0;
 
 import io.opentelemetry.context.propagation.TextMapSetter;
-import org.apache.http.client.methods.HttpUriRequest;
 
-final class HttpHeaderSetter implements TextMapSetter<HttpUriRequest> {
+final class HttpHeaderSetter implements TextMapSetter<ApacheHttpClientRequest> {
 
   @Override
-  public void set(HttpUriRequest carrier, String key, String value) {
+  public void set(ApacheHttpClientRequest carrier, String key, String value) {
     carrier.setHeader(key, value);
   }
 }
