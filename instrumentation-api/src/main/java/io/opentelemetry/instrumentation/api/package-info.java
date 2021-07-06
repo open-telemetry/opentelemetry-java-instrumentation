15c0abc6cd67dfa8fc7181134bc67156f9684af6
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-07-06 13:59:03 -0700

    Set default to non-null

---
+++ instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/package-info.java
@@ -0,0 +1,5 @@
+@DefaultQualifier(value = NonNull.class)
+package io.opentelemetry.instrumentation.api;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.framework.qual.DefaultQualifier;
