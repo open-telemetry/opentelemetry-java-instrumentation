1d3021685fea38af24e0c21dc18e558cc13d2140
Nikita Salnikov-Tarnovski <gnikem@gmail.com>
2021-07-06 13:52:58 +0300

    Try to build examples against locally published snapshot

---
+++ .github/scripts/prepare-extension-snapshot.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+sed -i -e 's/opentelemetryJavaagent[ ]*: "1.3.0"/opentelemetryJavaagent:"1.4.0-SNAPSHOT"/' build.gradle
+sed -i -e 's/opentelemetryJavaagentAlpha[ ]*: "1.3.0-alpha"/opentelemetryJavaagentAlpha:"1.4.0-alpha-SNAPSHOT"/' build.gradle
+echo 'dependencyResolutionManagement {repositories{mavenLocal()}}' >> settings.gradle
