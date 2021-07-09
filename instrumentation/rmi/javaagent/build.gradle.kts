c05dcf38b1e9a9abff475eff9a626d3d6093052d
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-07-09 11:26:00 +0900

    Allow RMI test to run on Java 16 (#3524)
    * Allow RMI test to run on Java 16
    
    * testJavaVersion

--- instrumentation/rmi/javaagent/build.gradle.kts
+++ instrumentation/rmi/javaagent/build.gradle.kts
@@ -46,5 +46,12 @@ tasks {
   }
   withType<Test>().configureEach {
     jvmArgs("-Djava.rmi.server.hostname=127.0.0.1")
+
+    // Can only export on Java 9+
+    val testJavaVersion = gradle.startParameter.projectProperties.get("testJavaVersion")?.let(JavaVersion::toVersion) ?: JavaVersion.current()
+    if (testJavaVersion.isJava9Compatible) {
+      jvmArgs("--add-exports=java.rmi/sun.rmi.server=ALL-UNNAMED")
+      jvmArgs("--add-exports=java.rmi/sun.rmi.transport=ALL-UNNAMED")
+    }
   }
 }
