f0f4138f14f8ad37bcbf07ebd0acee25b436becd
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2021-07-13 10:28:29 +0200

    Clean up javaagent-instrumentation-api and javaagent-extension-api deps a bit

--- javaagent-extension-api/build.gradle.kts
+++ javaagent-extension-api/build.gradle.kts
@@ -7,12 +7,11 @@ group = "io.opentelemetry.javaagent"
 
 dependencies {
   api("io.opentelemetry:opentelemetry-sdk")
-
   api("net.bytebuddy:byte-buddy")
-  api("org.slf4j:slf4j-api")
 
   implementation(project(":instrumentation-api"))
   implementation(project(":javaagent-instrumentation-api"))
+  implementation("org.slf4j:slf4j-api")
 
   // metrics are unstable, do not expose as api
   implementation("io.opentelemetry:opentelemetry-sdk-metrics")
