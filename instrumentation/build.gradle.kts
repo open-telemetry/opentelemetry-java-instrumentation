833b4cf9414a76d1c3fe8bcf03a6afa9ca43e6c5
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2021-07-14 15:28:39 +0200

    Clean up bootstrap dependencies and build scripts

--- instrumentation/build.gradle.kts
+++ instrumentation/build.gradle.kts
@@ -6,8 +6,6 @@ plugins {
   id("otel.java-conventions")
 }
 
-val bootstrap by configurations.creating
-
 val instrumentationProjectTest = tasks.named("test")
 val instrumentationProjectDependencies = dependencies
 
@@ -16,12 +14,6 @@ subprojects {
   plugins.withId("java") {
     instrumentationProjectTest.configure {
       dependsOn(subProj.tasks.named("test"))
-    }
-
-    if (subProj.name == "bootstrap") {
-      instrumentationProjectDependencies.run {
-        add(bootstrap.name, project(subProj.path))
-      }
     }
   }
 
