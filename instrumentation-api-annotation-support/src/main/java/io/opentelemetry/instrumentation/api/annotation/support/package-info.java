0027763a4c393722eb45f71cde5cc80ed87f948c
Spindler, Justin <Justin_Spindler@comcast.com>
2021-07-15 07:55:32 -0400

    Move AsyncSpanEndStrategy to annotation support project

---
+++ instrumentation-api-annotation-support/src/main/java/io/opentelemetry/instrumentation/api/annotation/support/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides utility classes to provide support for instrumenting annotated methods.
+ */
+@UnstableApi
+package io.opentelemetry.instrumentation.api.annotation.support;
+
+import io.opentelemetry.instrumentation.api.annotations.UnstableApi;
