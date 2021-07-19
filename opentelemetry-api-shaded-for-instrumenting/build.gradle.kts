a124c3ed9cbda951e13bd65b61c089d590b8571e
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-07-19 11:21:14 +0900

    Disable publishing of SDK shaded artifacts.

--- opentelemetry-api-shaded-for-instrumenting/build.gradle.kts
+++ opentelemetry-api-shaded-for-instrumenting/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
   id("com.github.johnrengelman.shadow")
 
   id("otel.java-conventions")
-  id("otel.publish-conventions")
 }
 
 description = "opentelemetry-api shaded for internal javaagent usage"
