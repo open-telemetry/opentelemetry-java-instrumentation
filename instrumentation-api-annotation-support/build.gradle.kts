22efae6997b1dd7145d33db2b261137215fcf5a8
Spindler, Justin <Justin_Spindler@comcast.com>
2021-07-21 09:02:26 -0400

    Add comment about IntelliJ dependency workaround

--- instrumentation-api-annotation-support/build.gradle.kts
+++ instrumentation-api-annotation-support/build.gradle.kts
@@ -8,6 +8,9 @@ group = "io.opentelemetry.instrumentation"
 
 dependencies {
   implementation(project(":instrumentation-api"))
+
+  // this only exists to make Intellij happy since it doesn't (currently at least) understand our
+  // inclusion of this artifact inside of :instrumentation-api
   compileOnly(project(":instrumentation-api-caching"))
 
   api("io.opentelemetry:opentelemetry-api")
