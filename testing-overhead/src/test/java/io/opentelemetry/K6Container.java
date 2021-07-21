c0653ac602618e2b96418e4cc8b9b07c527cfe90
Jason Plumb <jplumb@splunk.com>
2021-07-21 11:21:35 -0700

    remove builder

--- testing-overhead/src/test/java/io/opentelemetry/K6Container.java
+++ testing-overhead/src/test/java/io/opentelemetry/K6Container.java
@@ -16,56 +16,36 @@ import java.time.Duration;
 
 class K6Container {
   private static final Logger logger = LoggerFactory.getLogger(K6Container.class);
+  private final Network network;
+  private final String agent;
+  private final TestConfig config;
 
-  private K6Container() {
+  K6Container(Network network, String agent, TestConfig config) {
+    this.network = network;
+    this.agent = agent;
+    this.config = config;
   }
 
-  static Builder builder() {
-    return new Builder();
-  }
-
-  static class Builder {
-
-    private Network network;
-    private String agent;
-    private TestConfig config;
-
-    Builder withNetwork(Network network) {
-      this.network = network;
-      return this;
-    }
-
-    Builder withConfig(TestConfig config) {
-      this.config = config;
-      return this;
-    }
-
-    Builder withAgentNamed(String agent) {
-      this.agent = agent;
-      return this;
-    }
-
-    GenericContainer<?> build(){
-      String k6OutputFile = "/results/k6_out_" + agent + ".json";
-      return new GenericContainer<>(
-          DockerImageName.parse("loadimpact/k6"))
-          .withNetwork(network)
-          .withNetworkAliases("k6")
-          .withLogConsumer(new Slf4jLogConsumer(logger))
-          .withCopyFileToContainer(
-              MountableFile.forHostPath("./k6"), "/app")
-          .withFileSystemBind(".", "/results")
-          .withCommand(
-              "run",
-              "-u", String.valueOf(config.getConcurrentConnections()),
-              "-i", String.valueOf(config.getTotalIterations()),
-              "--rps", String.valueOf(config.getMaxRequestRate()),
-              "--summary-export", k6OutputFile,
-              "/app/basic.js"
-          )
-          .withStartupCheckStrategy(
-              new OneShotStartupCheckStrategy().withTimeout(Duration.ofMinutes(5))
-          );
-    }
+  GenericContainer<?> build(){
+    String k6OutputFile = "/results/k6_out_" + agent + ".json";
+    return new GenericContainer<>(
+        DockerImageName.parse("loadimpact/k6"))
+        .withNetwork(network)
+        .withNetworkAliases("k6")
+        .withLogConsumer(new Slf4jLogConsumer(logger))
+        .withCopyFileToContainer(
+            MountableFile.forHostPath("./k6"), "/app")
+        .withFileSystemBind(".", "/results")
+        .withCommand(
+            "run",
+            "-u", String.valueOf(config.getConcurrentConnections()),
+            "-i", String.valueOf(config.getTotalIterations()),
+            "--rps", String.valueOf(config.getMaxRequestRate()),
+            "--summary-export", k6OutputFile,
+            "/app/basic.js"
+        )
+        .withStartupCheckStrategy(
+            new OneShotStartupCheckStrategy().withTimeout(Duration.ofMinutes(5))
+        );
   }
 }
