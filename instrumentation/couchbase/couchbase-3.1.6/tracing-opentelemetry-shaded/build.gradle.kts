7a4215507523d09b401120a94c487f9552d5c0e2
Lauri Tulmin <ltulmin@splunk.com>
2021-07-21 15:22:33 +0300

    couchbase 3.2.0

---
+++ instrumentation/couchbase/couchbase-3.1.6/tracing-opentelemetry-shaded/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+  id("com.github.johnrengelman.shadow")
+
+  id("otel.java-conventions")
+}
+
+group = "io.opentelemetry.javaagent.instrumentation"
+
+dependencies {
+  implementation("com.couchbase.client:tracing-opentelemetry:0.3.6")
+}
+
+tasks {
+  shadowJar {
+    dependencies {
+      // including only tracing-opentelemetry excludes its transitive dependencies
+      include(dependency("com.couchbase.client:tracing-opentelemetry"))
+    }
+    relocate("com.couchbase.client.tracing.opentelemetry", "com.couchbase.v3_1_6.client.tracing.opentelemetry")
+  }
+
+  assemble {
+    dependsOn(shadowJar)
+  }
+}
