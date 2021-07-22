d019e096260a8bb193b371d898a2ef323d89b75c
Anuraag Agrawal <anuraaga@gmail.com>
2021-07-22 19:05:35 +0900

    Update instrumentation/apache-httpclient/apache-httpclient-4.3/library/src/main/java/io/opentelemetry/instrumentation/apachehttpclient/v4_3/ApacheHttpClientRequest.java
    Co-authored-by: Lauri Tulmin <tulmin@gmail.com>

--- instrumentation/apache-httpclient/apache-httpclient-4.3/library/src/main/java/io/opentelemetry/instrumentation/apachehttpclient/v4_3/ApacheHttpClientRequest.java
+++ instrumentation/apache-httpclient/apache-httpclient-4.3/library/src/main/java/io/opentelemetry/instrumentation/apachehttpclient/v4_3/ApacheHttpClientRequest.java
@@ -99,7 +99,7 @@ final class ApacheHttpClientRequest {
     if (major == 2 && minor == 0) {
       return SemanticAttributes.HttpFlavorValues.HTTP_2_0;
     }
-    logger.debug("unexpected http protocol version: " + protocolVersion);
+    logger.debug("unexpected http protocol version: {}", protocolVersion);
     return null;
   }
 
