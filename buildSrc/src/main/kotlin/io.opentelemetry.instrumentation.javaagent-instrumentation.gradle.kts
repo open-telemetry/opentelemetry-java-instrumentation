dc520e7c15adac4d3bd3ee5672e6f9dcd5ad09f5
Nikita Salnikov-Tarnovski <gnikem@gmail.com>
2021-07-22 10:17:19 +0300

    Add comment

--- buildSrc/src/main/kotlin/io.opentelemetry.instrumentation.javaagent-instrumentation.gradle.kts
+++ buildSrc/src/main/kotlin/io.opentelemetry.instrumentation.javaagent-instrumentation.gradle.kts
@@ -11,5 +11,11 @@ dependencies {
   add("muzzleTooling", "io.opentelemetry.javaagent:opentelemetry-javaagent-tooling")
   add("muzzleTooling", "org.slf4j:slf4j-simple")
 
+  /*
+    Dependencies added to this configuration will be found by the muzzle gradle plugin during code
+    generation phase. They differ from the implementation dependencies declared in plugin's build
+    script, because these classes become part of the code that plugin inspects and traverses during
+    references collection phase. They are not part of the observer, they are part of the observation.
+   */
   add("codegen", "io.opentelemetry.javaagent:opentelemetry-javaagent-tooling")
 }
--- testing-common/src/test/java/muzzle/MuzzleWeakReferenceTest.java
+++ testing-common/src/test/java/muzzle/MuzzleWeakReferenceTest.java
@@ -11,17 +11,17 @@ public class MuzzleWeakReferenceTest {
   // our strong references away from Spock in this java class.
   // Even returning a WeakReference<ClassLoader> is enough for spock to create a strong ref.
   public static boolean classLoaderRefIsGarbageCollected() throws InterruptedException {
-//    ClassLoader loader = new URLClassLoader(new URL[0], null);
-//    WeakReference<ClassLoader> clRef = new WeakReference<>(loader);
-//    ReferenceCollector collector = new ReferenceCollector(className -> false);
-//    collector.collectReferencesFromAdvice(TestClasses.MethodBodyAdvice.class.getName());
-//    ReferenceMatcher refMatcher =
-//        new ReferenceMatcher(
-//            Collections.emptyList(), collector.getReferences(), className -> false);
-//    refMatcher.getMismatchedReferenceSources(loader);
-//    loader = null;
-//    GcUtils.awaitGc(clRef);
-//    return clRef.get() == null;
+    //    ClassLoader loader = new URLClassLoader(new URL[0], null);
+    //    WeakReference<ClassLoader> clRef = new WeakReference<>(loader);
+    //    ReferenceCollector collector = new ReferenceCollector(className -> false);
+    //    collector.collectReferencesFromAdvice(TestClasses.MethodBodyAdvice.class.getName());
+    //    ReferenceMatcher refMatcher =
+    //        new ReferenceMatcher(
+    //            Collections.emptyList(), collector.getReferences(), className -> false);
+    //    refMatcher.getMismatchedReferenceSources(loader);
+    //    loader = null;
+    //    GcUtils.awaitGc(clRef);
+    //    return clRef.get() == null;
 
     return true;
   }
