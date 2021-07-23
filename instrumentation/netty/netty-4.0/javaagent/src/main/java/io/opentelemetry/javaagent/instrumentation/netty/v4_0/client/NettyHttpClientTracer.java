1b5df6d78a6877dab38f573b44d0358927d1ab3d
Lauri Tulmin <ltulmin@splunk.com>
2021-07-23 11:21:15 +0300

    Netty connection failure handling (#3569)
    * Netty connection failure handling
    
    * Update instrumentation/netty/netty-4-common/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/netty/common/client/AbstractNettyHttpClientTracer.java
    
    Co-authored-by: Mateusz Rzeszutek <mrzeszutek@splunk.com>
    
    * Remove unused import
    
    Co-authored-by: Mateusz Rzeszutek <mrzeszutek@splunk.com>

--- instrumentation/netty/netty-4.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/netty/v4_0/client/NettyHttpClientTracer.java
+++ instrumentation/netty/netty-4.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/netty/v4_0/client/NettyHttpClientTracer.java
@@ -8,32 +8,31 @@ package io.opentelemetry.javaagent.instrumentation.netty.v4_0.client;
 import static io.netty.handler.codec.http.HttpHeaders.Names.HOST;
 import static io.opentelemetry.api.trace.SpanKind.CLIENT;
 import static io.opentelemetry.javaagent.instrumentation.netty.common.client.NettyResponseInjectAdapter.SETTER;
-import static io.opentelemetry.semconv.trace.attributes.SemanticAttributes.NetTransportValues.IP_TCP;
-import static io.opentelemetry.semconv.trace.attributes.SemanticAttributes.NetTransportValues.IP_UDP;
 
-import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.socket.DatagramChannel;
 import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.HttpResponse;
 import io.opentelemetry.api.trace.SpanBuilder;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.context.propagation.TextMapSetter;
-import io.opentelemetry.instrumentation.api.tracer.HttpClientTracer;
 import io.opentelemetry.instrumentation.api.tracer.net.NetPeerAttributes;
-import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
+import io.opentelemetry.javaagent.instrumentation.netty.common.client.AbstractNettyHttpClientTracer;
+import io.opentelemetry.javaagent.instrumentation.netty.common.client.NettyHttpClientTracerAccess;
+import io.opentelemetry.javaagent.instrumentation.netty.v4_0.AttributeKeys;
 import java.net.InetSocketAddress;
 import java.net.URI;
 import java.net.URISyntaxException;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
-public class NettyHttpClientTracer
-    extends HttpClientTracer<NettyRequestWrapper, HttpHeaders, HttpResponse> {
+public class NettyHttpClientTracer extends AbstractNettyHttpClientTracer<NettyRequestWrapper> {
   private static final NettyHttpClientTracer TRACER = new NettyHttpClientTracer();
 
-  private NettyHttpClientTracer() {
-    super(NetPeerAttributes.INSTANCE);
+  static {
+    NettyHttpClientTracerAccess.setTracer(TRACER);
   }
+
+  private NettyHttpClientTracer() {}
 
   public static NettyHttpClientTracer tracer() {
     return TRACER;
@@ -51,14 +50,9 @@ public class NettyHttpClientTracer
     return context;
   }
 
-  public void connectionFailure(Context parentContext, Channel channel, Throwable throwable) {
-    SpanBuilder spanBuilder = spanBuilder(parentContext, "CONNECT", CLIENT);
-    spanBuilder.setAttribute(
-        SemanticAttributes.NET_TRANSPORT, channel instanceof DatagramChannel ? IP_UDP : IP_TCP);
-    NetPeerAttributes.INSTANCE.setNetPeer(spanBuilder, (InetSocketAddress) channel.remoteAddress());
-
-    Context context = withClientSpan(parentContext, spanBuilder.startSpan());
-    tracer().endExceptionally(context, throwable);
+  @Override
+  protected Context getAndRemoveConnectContext(ChannelFuture channelFuture) {
+    return channelFuture.channel().attr(AttributeKeys.CONNECT_CONTEXT).getAndRemove();
   }
 
   @Override
