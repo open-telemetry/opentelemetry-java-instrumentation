09ca9927b1168a6601dd0cd2d86c4965442a5cf9
Jason Plumb <jplumb@splunk.com>
2021-07-26 09:56:11 -0700

    leverage an agent object

--- testing-overhead/src/test/java/io/opentelemetry/containers/PetClinicRestContainer.java
+++ testing-overhead/src/test/java/io/opentelemetry/containers/PetClinicRestContainer.java
@@ -10,7 +10,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import io.opentelemetry.agents.Agent;
 import io.opentelemetry.agents.AgentResolver;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
