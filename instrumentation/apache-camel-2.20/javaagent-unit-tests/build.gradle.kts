15a40a00596cfe592a52db973d028d12f173878a
Anna Nosek <aniano13@gmail.com>
2021-07-29 03:30:52 +0200

    Sanitize SQL in Apache Camel instrumentation (#3683)
    * sanitize and test sql and jdbc, remove mongo branch
    
    * checkstyle fix
    
    * move and simplify unit tests
    
    * add @Unroll to parametrized tests

--- instrumentation/apache-camel-2.20/javaagent-unit-tests/build.gradle.kts
+++ instrumentation/apache-camel-2.20/javaagent-unit-tests/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
 
 dependencies {
   testImplementation(project(":instrumentation:apache-camel-2.20:javaagent"))
+  testImplementation(project(":instrumentation-api"))
+
   testImplementation("org.apache.camel:camel-core:2.20.1")
   testImplementation("org.apache.camel:camel-aws:2.20.1")
   testImplementation("org.apache.camel:camel-http:2.20.1")
