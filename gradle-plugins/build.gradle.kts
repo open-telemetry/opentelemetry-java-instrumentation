b14e28513fb73705bec3f984beede133ecd07979
Nikita Salnikov-Tarnovski <gnikem@gmail.com>
2021-07-30 10:06:15 +0300

    Muzzle classloading hell (#3708)
    * Running muzzle check from the correct classloader
    
    * Apply suggestions from code review
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>

--- gradle-plugins/build.gradle.kts
+++ gradle-plugins/build.gradle.kts
@@ -22,6 +22,7 @@ repositories {
 dependencies {
   implementation("com.google.guava:guava:30.1.1-jre")
   implementation("net.bytebuddy:byte-buddy-gradle-plugin:1.11.2")
+
   implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-api:1.5.0-alpha-SNAPSHOT")
   implementation("io.opentelemetry.javaagent:opentelemetry-muzzle:1.5.0-alpha-SNAPSHOT")
   implementation("io.opentelemetry.javaagent:opentelemetry-javaagent-extension-api:1.5.0-alpha-SNAPSHOT")
@@ -37,7 +38,7 @@ dependencies {
   testImplementation("org.junit.jupiter:junit-jupiter-params")
   testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 
-  testImplementation("io.opentelemetry.javaagent:opentelemetry-javaagent-instrumentation-api:1.4.0-alpha-SNAPSHOT")
+  testImplementation("io.opentelemetry.javaagent:opentelemetry-javaagent-instrumentation-api:1.5.0-alpha-SNAPSHOT")
 }
 
 pluginBundle {
