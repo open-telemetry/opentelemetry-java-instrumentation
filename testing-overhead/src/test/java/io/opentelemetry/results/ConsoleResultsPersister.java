f96e545330213373362f9a1eb53383a77aedaf82
jason plumb <75337021+breedx-splk@users.noreply.github.com>
2021-08-03 13:33:47 -0700

    Response to additional code review comments from #3648. (#3761)

--- testing-overhead/src/test/java/io/opentelemetry/results/ConsoleResultsPersister.java
+++ testing-overhead/src/test/java/io/opentelemetry/results/ConsoleResultsPersister.java
@@ -4,7 +4,6 @@
  */
 package io.opentelemetry.results;
 
-import io.opentelemetry.agents.Agent;
 import io.opentelemetry.config.TestConfig;
 import java.util.Comparator;
 import java.util.List;
@@ -14,25 +13,25 @@ public class ConsoleResultsPersister implements ResultsPersister {
 
   @Override
   public void write(List<AppPerfResults> results) {
-    TestConfig config = results.stream().findFirst().get().getConfig();
+    TestConfig config = results.stream().findFirst().get().config;
     System.out.println("----------------------------------------------------------");
     System.out.printf(" %s : %s\n", config.getName(), config.getDescription());
     System.out.printf(" %d users, %d iterations\n", config.getConcurrentConnections(), config.getTotalIterations());
     System.out.println("----------------------------------------------------------");
 
-    display(results, "Agent", appPerfResults -> appPerfResults.getAgent().getName());
-    display(results, "Startup time (ms)", res -> String.valueOf(res.getStartupDurationMs()));
+    display(results, "Agent", appPerfResults -> appPerfResults.agent.getName());
+    display(results, "Startup time (ms)", res -> String.valueOf(res.startupDurationMs));
     display(results, "Total allocated MB", res -> format(res.getTotalAllocatedMB()));
-    display(results, "Heap (min)", res -> String.valueOf(res.getHeapUsed().min));
-    display(results, "Heap (max)", res -> String.valueOf(res.getHeapUsed().max));
+    display(results, "Heap (min)", res -> String.valueOf(res.heapUsed.min));
+    display(results, "Heap (max)", res -> String.valueOf(res.heapUsed.max));
     display(results, "Thread switch rate",
-        res -> String.valueOf(res.getMaxThreadContextSwitchRate()));
-    display(results, "GC time", res -> String.valueOf(res.getTotalGCTime()));
-    display(results, "Req. mean", res -> format(res.getRequestAvg()));
-    display(results, "Req. p95", res -> format(res.getRequestP95()));
-    display(results, "Iter. mean", res -> format(res.getIterationAvg()));
-    display(results, "Iter. p95", res -> format(res.getIterationP95()));
-    display(results, "Peak threads", res -> String.valueOf(res.getPeakThreadCount()));
+        res -> String.valueOf(res.maxThreadContextSwitchRate));
+    display(results, "GC time", res -> String.valueOf(res.totalGCTime));
+    display(results, "Req. mean", res -> format(res.requestAvg));
+    display(results, "Req. p95", res -> format(res.requestP95));
+    display(results, "Iter. mean", res -> format(res.iterationAvg));
+    display(results, "Iter. p95", res -> format(res.iterationP95));
+    display(results, "Peak threads", res -> String.valueOf(res.peakThreadCount));
   }
 
   private void display(List<AppPerfResults> results, String pref,
@@ -41,7 +40,6 @@ public class ConsoleResultsPersister implements ResultsPersister {
     results.stream()
         .sorted(Comparator.comparing(AppPerfResults::getAgentName))
         .forEach(result -> {
-          Agent agent = result.getAgent();
           System.out.printf("%17s", vs.apply(result));
       });
     System.out.println();
