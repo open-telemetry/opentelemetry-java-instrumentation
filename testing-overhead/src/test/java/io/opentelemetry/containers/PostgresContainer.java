0c4743e82971e09e3ee268d159cd6bd595cbb5bc
Jason Plumb <jplumb@splunk.com>
2021-08-03 15:19:24 -0700

    use the PostgreSQLContainer abstraction container class

--- testing-overhead/src/test/java/io/opentelemetry/containers/PostgresContainer.java
+++ testing-overhead/src/test/java/io/opentelemetry/containers/PostgresContainer.java
@@ -6,35 +6,37 @@ package io.opentelemetry.containers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
-import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.utility.MountableFile;
 
 public class PostgresContainer {
 
   private static final Logger logger = LoggerFactory.getLogger(PostgresContainer.class);
+  public static final String PASSWORD = "petclinic";
+  public static final String USERNAME = "petclinic";
+  public static final String DATABASE_NAME = "petclinic";
+
   private final Network network;
 
   public PostgresContainer(Network network) {
     this.network = network;
   }
 
-  public GenericContainer<?> build() throws Exception {
-    return new GenericContainer<>(
-        DockerImageName.parse("postgres:9.6.22"))
+  public PostgreSQLContainer<?> build() throws Exception {
+    return new PostgreSQLContainer<>("postgres:9.6.22")
         .withNetwork(network)
         .withNetworkAliases("postgres")
         .withLogConsumer(new Slf4jLogConsumer(logger))
-        .withEnv("POSTGRES_PASSWORD", "petclinic")
-        .withEnv("POSTGRES_DB", "petclinic")
+        .withUsername(USERNAME)
+        .withPassword(PASSWORD)
+        .withDatabaseName(DATABASE_NAME)
         .withCopyFileToContainer(
             MountableFile.forClasspathResource("initDB.sql"), "/docker-entrypoint-initdb.d/initDB.sql")
         .withCopyFileToContainer(
             MountableFile.forClasspathResource("populateDB.sql"), "/docker-entrypoint-initdb.d/populateDB.sql")
-        .withReuse(false)
-        .withExposedPorts(5432);
+        .withReuse(false);
   }
 
 }
