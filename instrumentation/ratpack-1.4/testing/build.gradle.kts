36d20316322c5621c1d597985d5241df0530029d
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-08-04 17:30:40 +0900

    Migrate Ratpack HTTP Client tests to Java.

--- instrumentation/ratpack-1.4/testing/build.gradle.kts
+++ instrumentation/ratpack-1.4/testing/build.gradle.kts
@@ -6,6 +6,7 @@ dependencies {
   api(project(":testing-common"))
 
   api("io.ratpack:ratpack-core:1.4.0")
+  api("io.ratpack:ratpack-test:1.4.0")
 
   implementation("org.codehaus.groovy:groovy-all")
   implementation("io.opentelemetry:opentelemetry-api")
---
+++ instrumentation/ratpack-1.4/testing/src/main/java/io/opentelemetry/instrumentation/ratpack/client/AbstractRatpackHttpClientTest.java
@@ -0,0 +1,167 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.instrumentation.ratpack.client;
+
+import io.netty.channel.ConnectTimeoutException;
+import io.netty.handler.timeout.ReadTimeoutException;
+import io.opentelemetry.instrumentation.testing.junit.http.AbstractHttpClientTest;
+import io.opentelemetry.instrumentation.testing.junit.http.HttpClientTestOptions;
+import java.lang.reflect.Method;
+import java.net.URI;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Map;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import ratpack.exec.ExecController;
+import ratpack.exec.Operation;
+import ratpack.exec.Promise;
+import ratpack.func.Action;
+import ratpack.http.client.HttpClient;
+import ratpack.http.client.HttpClientSpec;
+import ratpack.http.client.ReceivedResponse;
+import ratpack.test.exec.ExecHarness;
+
+public abstract class AbstractRatpackHttpClientTest extends AbstractHttpClientTest<Void> {
+
+  private static final ExecHarness exec = ExecHarness.harness();
+
+  private HttpClient client;
+  private HttpClient singleConnectionClient;
+
+  @BeforeAll
+  void setUpClient() throws Exception {
+    client = buildHttpClient();
+    singleConnectionClient = buildHttpClient(spec -> spec.poolSize(1));
+  }
+
+  @AfterAll
+  void cleanUpClient() {
+    client.close();
+    singleConnectionClient.close();
+  }
+
+  protected HttpClient buildHttpClient() throws Exception {
+    return buildHttpClient(Action.noop());
+  }
+
+  protected HttpClient buildHttpClient(Action<? super HttpClientSpec> action) throws Exception {
+    return HttpClient.of(
+        spec -> {
+          // execController method added in 1.9
+          try {
+            Method execController =
+                HttpClientSpec.class.getMethod("execController", ExecController.class);
+            execController.invoke(spec, exec.getController());
+          } catch (NoSuchMethodException e) {
+            // Ignore
+          }
+
+          action.execute(spec);
+        });
+  }
+
+  @Override
+  protected final Void buildRequest(String method, URI uri, Map<String, String> headers) {
+    return null;
+  }
+
+  @Override
+  protected final int sendRequest(Void request, String method, URI uri, Map<String, String> headers)
+      throws Exception {
+    return exec.yield(unused -> internalSendRequest(client, method, uri, headers))
+        .getValueOrThrow();
+  }
+
+  @Override
+  protected final void sendRequestWithCallback(
+      Void request,
+      String method,
+      URI uri,
+      Map<String, String> headers,
+      RequestResult requestResult)
+      throws Exception {
+    exec.execute(
+        Operation.of(
+            () ->
+                internalSendRequest(client, method, uri, headers)
+                    .result(
+                        result ->
+                            requestResult.complete(result::getValue, result.getThrowable()))));
+  }
+
+  // overridden in RatpackForkedHttpClientTest
+  protected Promise<Integer> internalSendRequest(
+      HttpClient client, String method, URI uri, Map<String, String> headers) {
+    Promise<ReceivedResponse> resp =
+        client.request(
+            uri,
+            spec -> {
+              // Connect timeout for the whole client was added in 1.5 so we need to add timeout for
+              // each request
+              spec.connectTimeout(Duration.ofSeconds(2));
+              if (uri.getPath().equals("/read-timeout")) {
+                spec.readTimeout(readTimeout());
+              }
+              spec.method(method);
+              spec.headers(headersSpec -> headers.forEach(headersSpec::add));
+            });
+
+    return resp.map(ReceivedResponse::getStatusCode);
+  }
+
+  @Override
+  protected void configure(HttpClientTestOptions options) {
+    options.setSingleConnectionFactory(
+        (host, port) ->
+            (path, headers) -> {
+              URI uri = resolveAddress(path);
+              return exec.yield(
+                      unused -> internalSendRequest(singleConnectionClient, "GET", uri, headers))
+                  .getValueOrThrow();
+            });
+
+    options.setExpectedClientSpanNameMapper(
+        (uri, method) -> {
+          switch (uri.toString()) {
+            case "http://localhost:61/": // unopened port
+            case "https://192.0.2.1/": // non routable address
+              return "CONNECT";
+            default:
+              return HttpClientTestOptions.DEFAULT_EXPECTED_CLIENT_SPAN_NAME_MAPPER.apply(
+                  uri, method);
+          }
+        });
+
+    options.setClientSpanErrorMapper(
+        (uri, exception) -> {
+          if (uri.toString().equals("https://192.0.2.1/")) {
+            return new ConnectTimeoutException("connection timed out: /192.0.2.1:443");
+          } else if (uri.getPath().equals("/read-timeout")) {
+            return ReadTimeoutException.INSTANCE;
+          }
+          return exception;
+        });
+
+    options.setHttpAttributes(
+        uri -> {
+          switch (uri.toString()) {
+            case "http://localhost:61/": // unopened port
+            case "https://192.0.2.1/": // non routable address
+              return Collections.emptySet();
+            default:
+              return HttpClientTestOptions.DEFAULT_HTTP_ATTRIBUTES;
+          }
+        });
+
+    options.disableTestRedirects();
+
+    // these tests will pass, but they don't really test anything since REQUEST is Void
+    options.disableTestReusedRequest();
+
+    options.enableTestReadTimeout();
+  }
+}
