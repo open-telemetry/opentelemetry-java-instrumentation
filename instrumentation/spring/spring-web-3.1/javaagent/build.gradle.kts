b7944e800833d2da6a5ef066c4038387a3d18b60
Lauri Tulmin <ltulmin@splunk.com>
2021-08-04 23:35:41 +0300

    remove unneeded dependency

--- instrumentation/spring/spring-web-3.1/javaagent/build.gradle.kts
+++ instrumentation/spring/spring-web-3.1/javaagent/build.gradle.kts
@@ -7,17 +7,12 @@ muzzle {
     group.set("org.springframework")
     module.set("spring-web")
     versions.set("[3.1.0.RELEASE,]")
-    // these versions depend on org.springframework:spring-web which has a bad dependency on
-    // javax.faces:jsf-api:1.1 which was released as pom only
+    // these versions depend on javax.faces:jsf-api:1.1 which was released as pom only
     skip("1.2.1", "1.2.2", "1.2.3", "1.2.4")
-    // 3.2.1.RELEASE has transitive dependencies like spring-web as "provided" instead of "compile"
-    skip("3.2.1.RELEASE")
-    extraDependency("javax.servlet:javax.servlet-api:3.0.1")
     assertInverse.set(true)
   }
 }
 
 dependencies {
   compileOnly("org.springframework:spring-web:3.1.0.RELEASE")
-  compileOnly("javax.servlet:javax.servlet-api:3.1.0")
 }
