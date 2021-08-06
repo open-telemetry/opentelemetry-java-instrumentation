af2bbcc026b532fb7a7bfd5ad7844d34d19c11fc
jack-berg <jberg@newrelic.com>
2021-08-06 17:53:18 -0500

    Convert java-http-client to use Instrumenter API

--- instrumentation/java-http-client/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/httpclient/HttpHeadersInstrumentation.java
+++ instrumentation/java-http-client/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/httpclient/HttpHeadersInstrumentation.java
@@ -6,7 +6,7 @@
 package io.opentelemetry.javaagent.instrumentation.httpclient;
 
 import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.extendsClass;
-import static io.opentelemetry.javaagent.instrumentation.httpclient.JdkHttpClientTracer.tracer;
+import static io.opentelemetry.javaagent.instrumentation.httpclient.JdkHttpClientSingletons.setter;
 import static net.bytebuddy.matcher.ElementMatchers.isMethod;
 import static net.bytebuddy.matcher.ElementMatchers.nameStartsWith;
 import static net.bytebuddy.matcher.ElementMatchers.named;
@@ -39,7 +39,7 @@ public class HttpHeadersInstrumentation implements TypeInstrumentation {
 
     @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
     public static void methodExit(@Advice.Return(readOnly = false) HttpHeaders headers) {
-      headers = tracer().inject(headers);
+      headers = setter().inject(headers);
     }
   }
 }
---
+++ instrumentation/java-http-client/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/httpclient/JdkHttpClientSingletons.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.httpclient;
+
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.instrumentation.api.instrumenter.Instrumenter;
+import io.opentelemetry.instrumentation.api.instrumenter.http.HttpClientMetrics;
+import io.opentelemetry.instrumentation.api.instrumenter.http.HttpSpanNameExtractor;
+import io.opentelemetry.instrumentation.api.instrumenter.http.HttpSpanStatusExtractor;
+import io.opentelemetry.javaagent.instrumentation.api.instrumenter.PeerServiceAttributesExtractor;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class JdkHttpClientSingletons {
+
+  private static final HttpHeadersInjectAdapter SETTER;
+  private static final Instrumenter<HttpRequest, HttpResponse<?>> INSTRUMENTER;
+
+  static {
+    SETTER = new HttpHeadersInjectAdapter(GlobalOpenTelemetry.getPropagators());
+    var httpAttributesExtractor = new JdkHttpAttributesExtractor();
+    var spanNameExtractor = HttpSpanNameExtractor.create(httpAttributesExtractor);
+    var spanStatusExtractor = HttpSpanStatusExtractor.create(httpAttributesExtractor);
+    var netAttributesExtractor = new JdkHttpNetAttributesExtractor();
+
+    INSTRUMENTER =
+        Instrumenter.<HttpRequest, HttpResponse<?>>newBuilder(
+                GlobalOpenTelemetry.get(), "io.opentelemetry.java-http-client", spanNameExtractor)
+            .setSpanStatusExtractor(spanStatusExtractor)
+            .addAttributesExtractor(httpAttributesExtractor)
+            .addAttributesExtractor(netAttributesExtractor)
+            .addAttributesExtractor(PeerServiceAttributesExtractor.create(netAttributesExtractor))
+            .addRequestMetrics(HttpClientMetrics.get())
+            .newClientInstrumenter(SETTER);
+  }
+
+  public static Instrumenter<HttpRequest, HttpResponse<?>> instrumenter() {
+    return INSTRUMENTER;
+  }
+
+  public static HttpHeadersInjectAdapter setter() {
+    return SETTER;
+  }
+
+  private JdkHttpClientSingletons() {}
+}
