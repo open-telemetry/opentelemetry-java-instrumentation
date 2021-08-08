6d77db7d24af1f8348d2186962b5d8d67c1e92b7
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-08-07 19:58:27 -0700

    Spotless

--- instrumentation/jaxrs/jaxrs-1.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/jaxrs/v1_0/HandlerData.java
+++ instrumentation/jaxrs/jaxrs-1.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/jaxrs/v1_0/HandlerData.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package io.opentelemetry.javaagent.instrumentation.jaxrs.v1_0;
 
 import io.opentelemetry.instrumentation.api.tracer.SpanNames;
