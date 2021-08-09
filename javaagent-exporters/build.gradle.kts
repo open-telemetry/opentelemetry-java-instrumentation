cbd68e516753c2533a07644ef636298b29f4a7e9
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2021-08-09 18:27:30 +0200

    Add otlp-logging exporter

--- javaagent-exporters/build.gradle.kts
+++ javaagent-exporters/build.gradle.kts
@@ -8,6 +8,7 @@ dependencies {
   implementation("io.opentelemetry:opentelemetry-exporter-jaeger")
   implementation("io.opentelemetry:opentelemetry-exporter-otlp")
   implementation("io.opentelemetry:opentelemetry-exporter-otlp-metrics")
+  implementation("io.opentelemetry:opentelemetry-exporter-otlp-logging")
 
   implementation("io.opentelemetry:opentelemetry-exporter-prometheus")
   implementation("io.prometheus:simpleclient")
