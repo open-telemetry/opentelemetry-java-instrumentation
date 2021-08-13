614faf4724b158cd94e71b1a78f8097052905c5d
Jason Plumb <jplumb@splunk.com>
2021-08-13 08:22:05 -0700

    sort once.

--- benchmark-overhead/src/test/java/io/opentelemetry/results/PrintStreamPersister.java
+++ benchmark-overhead/src/test/java/io/opentelemetry/results/PrintStreamPersister.java
@@ -10,6 +10,7 @@ import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 class PrintStreamPersister implements ResultsPersister {
 
@@ -19,34 +20,34 @@ class PrintStreamPersister implements ResultsPersister {
 
   @Override
   public void write(List<AppPerfResults> results) {
-    TestConfig config = results.stream().findFirst().get().config;
+    List<AppPerfResults> sorted = results.stream()
+        .sorted(Comparator.comparing(AppPerfResults::getAgentName)).collect(Collectors.toList());
+    TestConfig config = sorted.stream().findFirst().get().config;
     out.println("----------------------------------------------------------");
     out.println(" Run at " + new Date());
     out.printf(" %s : %s\n", config.getName(), config.getDescription());
     out.printf(" %d users, %d iterations\n", config.getConcurrentConnections(), config.getTotalIterations());
     out.println("----------------------------------------------------------");
 
-    display(results, "Agent", appPerfResults -> appPerfResults.agent.getName());
-    display(results, "Startup time (ms)", res -> String.valueOf(res.startupDurationMs));
-    display(results, "Total allocated MB", res -> format(res.getTotalAllocatedMB()));
-    display(results, "Heap (min)", res -> String.valueOf(res.heapUsed.min));
-    display(results, "Heap (max)", res -> String.valueOf(res.heapUsed.max));
-    display(results, "Thread switch rate",
+    display(sorted, "Agent", appPerfResults -> appPerfResults.agent.getName());
+    display(sorted, "Startup time (ms)", res -> String.valueOf(res.startupDurationMs));
+    display(sorted, "Total allocated MB", res -> format(res.getTotalAllocatedMB()));
+    display(sorted, "Heap (min)", res -> String.valueOf(res.heapUsed.min));
+    display(sorted, "Heap (max)", res -> String.valueOf(res.heapUsed.max));
+    display(sorted, "Thread switch rate",
         res -> String.valueOf(res.maxThreadContextSwitchRate));
-    display(results, "GC time", res -> String.valueOf(res.totalGCTime));
-    display(results, "Req. mean", res -> format(res.requestAvg));
-    display(results, "Req. p95", res -> format(res.requestP95));
-    display(results, "Iter. mean", res -> format(res.iterationAvg));
-    display(results, "Iter. p95", res -> format(res.iterationP95));
-    display(results, "Peak threads", res -> String.valueOf(res.peakThreadCount));
+    display(sorted, "GC time", res -> String.valueOf(res.totalGCTime));
+    display(sorted, "Req. mean", res -> format(res.requestAvg));
+    display(sorted, "Req. p95", res -> format(res.requestP95));
+    display(sorted, "Iter. mean", res -> format(res.iterationAvg));
+    display(sorted, "Iter. p95", res -> format(res.iterationP95));
+    display(sorted, "Peak threads", res -> String.valueOf(res.peakThreadCount));
   }
 
   private void display(List<AppPerfResults> results, String pref,
       Function<AppPerfResults, String> vs) {
     out.printf("%-20s: ", pref);
-    results.stream()
-        .sorted(Comparator.comparing(AppPerfResults::getAgentName))
-        .forEach(result -> {
+    results.forEach(result -> {
           out.printf("%17s", vs.apply(result));
       });
     out.println();
