24bbafc04f71832e7e87fcd0263c9ecd04f84b68
Lauri Tulmin <ltulmin@splunk.com>
2021-08-14 14:37:01 +0300

    Review comments

--- instrumentation/tapestry-5.4/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/tapestry/TapestryServerSpanNaming.java
+++ instrumentation/tapestry-5.4/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/tapestry/TapestryServerSpanNaming.java
@@ -10,18 +10,13 @@ import static io.opentelemetry.instrumentation.api.servlet.ServerSpanNaming.Sour
 import io.opentelemetry.context.Context;
 import io.opentelemetry.instrumentation.api.servlet.ServerSpanNaming;
 import io.opentelemetry.instrumentation.api.servlet.ServletContextPath;
-import java.util.function.Supplier;
 
 public class TapestryServerSpanNaming {
 
   public static void updateServerSpanName(String pageName) {
     Context parentContext = Context.current();
     ServerSpanNaming.updateServerSpanName(
-        parentContext, CONTROLLER, getServerSpanNameSupplier(parentContext, pageName));
-  }
-
-  private static Supplier<String> getServerSpanNameSupplier(Context context, String pageName) {
-    return () -> getServerSpanName(context, pageName);
+        parentContext, CONTROLLER, () -> getServerSpanName(parentContext, pageName));
   }
 
   private static String getServerSpanName(Context context, String pageName) {
