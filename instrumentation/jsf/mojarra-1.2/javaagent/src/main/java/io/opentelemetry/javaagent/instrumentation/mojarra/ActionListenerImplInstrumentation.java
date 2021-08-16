912ce91b60ae0f1a9224df3a09a8645600c3bbde
Lauri Tulmin <ltulmin@splunk.com>
2021-08-16 20:06:04 +0300

    Convert jsf to instrumenter api (#3838)
    * Convert jsf to instrumenter api
    
    * Review comments
    
    * Use span name not null

--- instrumentation/jsf/mojarra-1.2/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/mojarra/ActionListenerImplInstrumentation.java
+++ instrumentation/jsf/mojarra-1.2/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/mojarra/ActionListenerImplInstrumentation.java
@@ -5,13 +5,15 @@
 
 package io.opentelemetry.javaagent.instrumentation.mojarra;
 
-import static io.opentelemetry.javaagent.instrumentation.mojarra.MojarraTracer.tracer;
+import static io.opentelemetry.javaagent.instrumentation.mojarra.MojarraSingletons.instrumenter;
 import static net.bytebuddy.matcher.ElementMatchers.named;
 
 import io.opentelemetry.context.Context;
 import io.opentelemetry.context.Scope;
+import io.opentelemetry.instrumentation.jsf.JsfRequest;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeTransformer;
+import io.opentelemetry.javaagent.instrumentation.api.Java8BytecodeBridge;
 import javax.faces.event.ActionEvent;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.description.type.TypeDescription;
@@ -37,28 +39,31 @@ public class ActionListenerImplInstrumentation implements TypeInstrumentation {
     @Advice.OnMethodEnter(suppress = Throwable.class)
     public static void onEnter(
         @Advice.Argument(0) ActionEvent event,
+        @Advice.Local("otelRequest") JsfRequest request,
         @Advice.Local("otelContext") Context context,
         @Advice.Local("otelScope") Scope scope) {
-      context = tracer().startSpan(event);
-      if (context != null) {
-        scope = context.makeCurrent();
+      Context parentContext = Java8BytecodeBridge.currentContext();
+
+      request = new JsfRequest(event);
+      if (!request.shouldStartSpan() || !instrumenter().shouldStart(parentContext, request)) {
+        return;
       }
+
+      context = instrumenter().start(parentContext, request);
+      scope = context.makeCurrent();
     }
 
     @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
     public static void stopSpan(
         @Advice.Thrown Throwable throwable,
+        @Advice.Local("otelRequest") JsfRequest request,
         @Advice.Local("otelContext") Context context,
         @Advice.Local("otelScope") Scope scope) {
       if (scope == null) {
         return;
       }
       scope.close();
-      if (throwable != null) {
-        tracer().endExceptionally(context, throwable);
-      } else {
-        tracer().end(context);
-      }
+      instrumenter().end(context, request, null, throwable);
     }
   }
 }
