658cb1e51804036a0f585a51f7757062140dbb1f
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-08-19 16:58:12 -0700

    Spotless

--- muzzle/src/main/java/io/opentelemetry/javaagent/tooling/muzzle/HelperResource.java
+++ muzzle/src/main/java/io/opentelemetry/javaagent/tooling/muzzle/HelperResource.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package io.opentelemetry.javaagent.tooling.muzzle;
 
 import com.google.auto.value.AutoValue;
