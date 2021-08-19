4ef4b2212164bd5a54d922cde107646469e08c9f
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-08-19 14:52:50 -0700

    Versioned helper resources, part 2

---
+++ muzzle/src/main/java/io/opentelemetry/javaagent/tooling/muzzle/HelperResource.java
@@ -0,0 +1,24 @@
+package io.opentelemetry.javaagent.tooling.muzzle;
+
+import com.google.auto.value.AutoValue;
+
+@AutoValue
+public abstract class HelperResource {
+
+  /**
+   * Create a new helper resource object.
+   *
+   * @param applicationPath The path in the user's class loader at which to inject the resource.
+   * @param agentPath The path in the agent class loader from which to get the content for the
+   *     resource.
+   */
+  public static HelperResource create(String applicationPath, String agentPath) {
+    return new AutoValue_HelperResource(applicationPath, agentPath);
+  }
+
+  /** The path in the user's class loader at which to inject the resource. */
+  public abstract String getApplicationPath();
+
+  /** The path in the agent class loader from which to get the content for the resource. */
+  public abstract String getAgentPath();
+}
