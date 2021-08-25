ad5b47e0bc0f04a9ebfc381aab016bdbc5b1f02a
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-08-25 10:10:59 -0700

    No need for lazy

--- instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/config/ExperimentalConfig.java
+++ instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/config/ExperimentalConfig.java
@@ -5,28 +5,18 @@
 
 package io.opentelemetry.instrumentation.api.config;
 
-import org.checkerframework.checker.nullness.qual.Nullable;
-
 public final class ExperimentalConfig {
 
-  // lazy initialized, so that javaagent can set it, and library instrumentation can fall back and
-  // read system properties
-  @Nullable private static ExperimentalConfig instance = null;
+  private static final ExperimentalConfig instance = new ExperimentalConfig(Config.get());
 
   private final Config config;
 
   /** Returns the global agent configuration. */
   public static ExperimentalConfig get() {
-    if (instance == null) {
-      // this should only happen in library instrumentation
-      //
-      // no need to synchronize because worst case is creating instance more than once
-      instance = new ExperimentalConfig(Config.get());
-    }
     return instance;
   }
 
-  private ExperimentalConfig(Config config) {
+  public ExperimentalConfig(Config config) {
     this.config = config;
   }
 
