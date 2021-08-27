97156556406c7d62c10bc382940f283decf57773
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2021-08-27 16:46:37 +0200

    fix DoNotMockAutoValue

--- instrumentation/jms-1.1/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/jms/Timer.java
+++ instrumentation/jms-1.1/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/jms/Timer.java
@@ -5,24 +5,28 @@
 
 package io.opentelemetry.javaagent.instrumentation.jms;
 
-import com.google.auto.value.AutoValue;
 import java.time.Instant;
 
-@AutoValue
-public abstract class Timer {
+public final class Timer {
 
   public static Timer start() {
-    return new AutoValue_Timer(Instant.now(), System.nanoTime());
+    return new Timer(Instant.now(), System.nanoTime());
   }
 
-  Timer() {}
+  private final Instant startTime;
+  private final long startNanoTime;
 
-  public abstract Instant startTime();
+  private Timer(Instant startTime, long startNanoTime) {
+    this.startTime = startTime;
+    this.startNanoTime = startNanoTime;
+  }
 
-  abstract long startNanoTime();
+  public Instant startTime() {
+    return startTime;
+  }
 
   public Instant endTime() {
-    long durationNanos = System.nanoTime() - startNanoTime();
+    long durationNanos = System.nanoTime() - startNanoTime;
     return startTime().plusNanos(durationNanos);
   }
 }
