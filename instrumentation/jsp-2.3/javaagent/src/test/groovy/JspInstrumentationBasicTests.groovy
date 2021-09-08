2f99b6fef60a2048e821b34e116c5ae7b308a80a
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-09-07 19:25:47 -0700

    And tracers too (so that tests pass)

--- instrumentation/jsp-2.3/javaagent/src/test/groovy/JspInstrumentationBasicTests.groovy
+++ instrumentation/jsp-2.3/javaagent/src/test/groovy/JspInstrumentationBasicTests.groovy
@@ -345,7 +345,6 @@ class JspInstrumentationBasicTests extends AgentInstrumentationSpecification {
             "${SemanticAttributes.HTTP_STATUS_CODE.key}" 200
             "${SemanticAttributes.HTTP_FLAVOR.key}" "1.1"
             "${SemanticAttributes.HTTP_USER_AGENT.key}" String
-            "${SemanticAttributes.HTTP_CLIENT_IP.key}" "127.0.0.1"
           }
         }
         span(1) {
