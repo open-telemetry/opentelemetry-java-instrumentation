c30be2c2d3bf67e3f53d76d12dd28f1f303635e4
Lauri Tulmin <ltulmin@splunk.com>
2021-09-09 23:05:30 +0300

    add comment

--- instrumentation/jaxrs-client/jaxrs-client-2.0/jaxrs-client-2.0-common/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/jaxrsclient/v2_0/ClientTracingFilter.java
+++ instrumentation/jaxrs-client/jaxrs-client-2.0/jaxrs-client-2.0-common/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/jaxrsclient/v2_0/ClientTracingFilter.java
@@ -31,10 +31,12 @@ public class ClientTracingFilter implements ClientRequestFilter, ClientResponseF
   @Override
   public void filter(ClientRequestContext requestContext, ClientResponseContext responseContext) {
     Object contextObj = requestContext.getProperty(CONTEXT_PROPERTY_NAME);
-    requestContext.removeProperty(CONTEXT_PROPERTY_NAME);
     if (contextObj instanceof Context) {
       Context context = (Context) contextObj;
       instrumenter().end(context, requestContext, responseContext, null);
     }
+    // we are done with this request, remove context so it could be gcd immediately in case request
+    // context stays around for whatever reason
+    requestContext.removeProperty(CONTEXT_PROPERTY_NAME);
   }
 }
