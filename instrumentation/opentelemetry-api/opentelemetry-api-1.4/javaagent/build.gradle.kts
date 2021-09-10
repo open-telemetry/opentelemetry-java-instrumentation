dd628dde1e268e0dea402b248dafbc88563f1419
Lauri Tulmin <ltulmin@splunk.com>
2021-09-10 21:54:19 +0300

    remove testing dependencies

--- instrumentation/opentelemetry-api/opentelemetry-api-1.4/javaagent/build.gradle.kts
+++ instrumentation/opentelemetry-api/opentelemetry-api-1.4/javaagent/build.gradle.kts
@@ -5,16 +5,4 @@ plugins {
 dependencies {
   compileOnly(project(path = ":opentelemetry-api-shaded-for-instrumenting", configuration = "shadow"))
   implementation(project(":instrumentation:opentelemetry-api:opentelemetry-api-1.0:javaagent"))
-
-  // using OpenTelemetry SDK to make sure that instrumentation doesn't cause
-  // OpenTelemetrySdk.getTracerProvider() to throw ClassCastException
-  testImplementation("io.opentelemetry:opentelemetry-sdk")
-  testImplementation(project(":instrumentation-api"))
-
-  // @WithSpan annotation is used to generate spans in ContextBridgeTest
-  testImplementation("io.opentelemetry:opentelemetry-extension-annotations")
-  testInstrumentation(project(":instrumentation:opentelemetry-annotations-1.0:javaagent"))
-
-  testImplementation(project(":instrumentation:opentelemetry-api:opentelemetry-api-1.0:testing"))
-  testInstrumentation(project(":instrumentation:opentelemetry-api:opentelemetry-api-1.0:testing"))
 }
