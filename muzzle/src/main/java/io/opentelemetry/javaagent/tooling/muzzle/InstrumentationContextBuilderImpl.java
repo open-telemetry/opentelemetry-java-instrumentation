7424f33dfbc113947cb2d72a8d3960a3cb3c401e
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2021-09-10 22:50:26 +0200

    Allow adding multiple ContextStore fields to one key class, part 2 (#4084)
    * Allow adding multiple ContextStore fields to one key class, part 2
    
    * checkstyle

--- muzzle/src/main/java/io/opentelemetry/javaagent/tooling/muzzle/InstrumentationContextBuilderImpl.java
+++ muzzle/src/main/java/io/opentelemetry/javaagent/tooling/muzzle/InstrumentationContextBuilderImpl.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.opentelemetry.javaagent.tooling.context;
+package io.opentelemetry.javaagent.tooling.muzzle;
 
 import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationContextBuilder;
 import java.util.AbstractMap;
@@ -11,13 +11,17 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public class InstrumentationContextBuilderImpl implements InstrumentationContextBuilder {
+public final class InstrumentationContextBuilderImpl implements InstrumentationContextBuilder {
   private final Set<Map.Entry<String, String>> entrySet = new HashSet<>();
 
   @Override
   public InstrumentationContextBuilder register(String keyClassName, String contextClassName) {
     entrySet.add(new AbstractMap.SimpleImmutableEntry<>(keyClassName, contextClassName));
     return this;
+  }
+
+  void registerAll(ContextStoreMappings mappings) {
+    entrySet.addAll(mappings.entrySet());
   }
 
   public ContextStoreMappings build() {
