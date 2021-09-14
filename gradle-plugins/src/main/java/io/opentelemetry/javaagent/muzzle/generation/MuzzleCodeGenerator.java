dd430131ed49a183ef9f022fed8af240c9ddb6fa
Nikita Salnikov-Tarnovski <gnikem@gmail.com>
2021-09-14 10:06:06 +0200

    spotless

--- gradle-plugins/src/main/java/io/opentelemetry/javaagent/muzzle/generation/MuzzleCodeGenerator.java
+++ gradle-plugins/src/main/java/io/opentelemetry/javaagent/muzzle/generation/MuzzleCodeGenerator.java
@@ -20,7 +20,6 @@ import io.opentelemetry.javaagent.tooling.muzzle.ReferenceCollector;
 import java.net.URLClassLoader;
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
