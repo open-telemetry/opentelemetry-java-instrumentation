8d334cca374e4206ef34308929167e4412c02ff2
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-09-29 14:00:56 -0700

    Partial cherry-pick of #4183

--- muzzle/src/main/java/io/opentelemetry/javaagent/tooling/muzzle/generation/Utils.java
+++ muzzle/src/main/java/io/opentelemetry/javaagent/tooling/muzzle/generation/Utils.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.opentelemetry.javaagent.muzzle.generation;
+package io.opentelemetry.javaagent.tooling.muzzle.generation;
 
 final class Utils {
 
