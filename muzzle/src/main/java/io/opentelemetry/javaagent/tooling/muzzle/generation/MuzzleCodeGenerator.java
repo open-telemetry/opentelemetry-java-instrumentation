07871686ea18251e27c583798691c28831298f03
Nikita Salnikov-Tarnovski <gnikem@gmail.com>
2021-09-29 11:26:39 +0300

    Hide InstrumentationContextBuilder from public API (#4216)
    * Introduce muzzle-specific interface to InstrumentationModule
    
    * Moved more methods to the InstrumentationModuleMuzzle interface
    
    * Fixing tests
    
    * More logs
    
    * Fix log name
    
    * Hide InstrumentationContextBuilder from public API

--- muzzle/src/main/java/io/opentelemetry/javaagent/tooling/muzzle/generation/MuzzleCodeGenerator.java
+++ muzzle/src/main/java/io/opentelemetry/javaagent/tooling/muzzle/generation/MuzzleCodeGenerator.java
@@ -533,7 +533,7 @@ final class MuzzleCodeGenerator implements AsmVisitorWrapper {
           super.visitMethod(
               Opcodes.ACC_PUBLIC,
               MUZZLE_CONTEXT_STORE_CLASSES_METHOD_NAME,
-              "(Lio/opentelemetry/javaagent/extension/instrumentation/InstrumentationContextBuilder;)V",
+              "(Lio/opentelemetry/javaagent/tooling/muzzle/InstrumentationContextBuilder;)V",
               null,
               null);
       mv.visitCode();
@@ -550,9 +550,9 @@ final class MuzzleCodeGenerator implements AsmVisitorWrapper {
             // stack: builder, className, contextClassName
             mv.visitMethodInsn(
                 Opcodes.INVOKEINTERFACE,
-                "io/opentelemetry/javaagent/extension/instrumentation/InstrumentationContextBuilder",
+                "io/opentelemetry/javaagent/tooling/muzzle/InstrumentationContextBuilder",
                 "register",
-                "(Ljava/lang/String;Ljava/lang/String;)Lio/opentelemetry/javaagent/extension/instrumentation/InstrumentationContextBuilder;",
+                "(Ljava/lang/String;Ljava/lang/String;)Lio/opentelemetry/javaagent/tooling/muzzle/InstrumentationContextBuilder;",
                 /* isInterface= */ true);
             // stack: builder
           });
