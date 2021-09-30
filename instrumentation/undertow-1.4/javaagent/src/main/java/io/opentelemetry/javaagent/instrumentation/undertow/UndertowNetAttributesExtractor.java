1049f0ead19928c6bdddf54abcddfb3f85fdc67c
Lauri Tulmin <ltulmin@splunk.com>
2021-09-30 14:09:57 +0300

    Convert undertow instrumentation to instrumenter api (#4239)

---
+++ instrumentation/undertow-1.4/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/undertow/UndertowNetAttributesExtractor.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.undertow;
+
+import io.opentelemetry.instrumentation.api.instrumenter.net.InetSocketAddressNetAttributesExtractor;
+import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
+import io.undertow.server.HttpServerExchange;
+import java.net.InetSocketAddress;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+public class UndertowNetAttributesExtractor
+    extends InetSocketAddressNetAttributesExtractor<HttpServerExchange, HttpServerExchange> {
+
+  @Override
+  public @Nullable InetSocketAddress getAddress(
+      HttpServerExchange exchange, @Nullable HttpServerExchange unused) {
+    return exchange.getConnection().getPeerAddress(InetSocketAddress.class);
+  }
+
+  @Override
+  public String transport(HttpServerExchange exchange) {
+    return SemanticAttributes.NetTransportValues.IP_TCP;
+  }
+}
