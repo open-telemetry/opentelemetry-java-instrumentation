df004892f74852c689a5576e0703532c002a0361
aras112nowe <aras112nowe@o2.pl>
2021-10-03 22:03:06 +0200

    Check if span name is null before use

--- instrumentation/jaxws/jaxws-2.0-cxf-3.0/library/build.gradle.kts
+++ instrumentation/jaxws/jaxws-2.0-cxf-3.0/library/build.gradle.kts
@@ -5,4 +5,6 @@ plugins {
 dependencies {
   compileOnly("javax.servlet:javax.servlet-api:3.0.1")
   compileOnly("org.apache.cxf:cxf-rt-frontend-jaxws:3.0.0")
+
+  testImplementation("org.apache.cxf:cxf-rt-frontend-jaxws:3.0.0")
 }
