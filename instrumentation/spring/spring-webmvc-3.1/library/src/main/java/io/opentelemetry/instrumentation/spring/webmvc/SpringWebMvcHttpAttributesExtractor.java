37b7c6cb021014dffce58ced9cbfe3103056d340
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2021-10-04 13:50:30 +0200

    fix spring webmvc extractor

--- instrumentation/spring/spring-webmvc-3.1/library/src/main/java/io/opentelemetry/instrumentation/spring/webmvc/SpringWebMvcHttpAttributesExtractor.java
+++ instrumentation/spring/spring-webmvc-3.1/library/src/main/java/io/opentelemetry/instrumentation/spring/webmvc/SpringWebMvcHttpAttributesExtractor.java
@@ -5,13 +5,25 @@
 
 package io.opentelemetry.instrumentation.spring.webmvc;
 
+import io.opentelemetry.instrumentation.api.instrumenter.http.CapturedHttpHeaders;
 import io.opentelemetry.instrumentation.api.instrumenter.http.HttpServerAttributesExtractor;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 final class SpringWebMvcHttpAttributesExtractor
     extends HttpServerAttributesExtractor<HttpServletRequest, HttpServletResponse> {
+
+  // TODO: add support for capturing HTTP headers in library instrumentations
+  SpringWebMvcHttpAttributesExtractor() {
+    super(CapturedHttpHeaders.empty());
+  }
+
   @Override
   protected @Nullable String method(HttpServletRequest request) {
     return request.getMethod();
@@ -20,6 +32,12 @@ final class SpringWebMvcHttpAttributesExtractor
   @Override
   protected @Nullable String userAgent(HttpServletRequest request) {
     return request.getHeader("user-agent");
+  }
+
+  @Override
+  protected List<String> requestHeader(HttpServletRequest request, String name) {
+    Enumeration<String> headers = request.getHeaders(name);
+    return headers == null ? Collections.emptyList() : Collections.list(headers);
   }
 
   @Override
@@ -55,6 +73,19 @@ final class SpringWebMvcHttpAttributesExtractor
   protected @Nullable Long responseContentLengthUncompressed(
       HttpServletRequest request, HttpServletResponse response) {
     return null;
+  }
+
+  @Override
+  protected List<String> responseHeader(
+      HttpServletRequest request, HttpServletResponse response, String name) {
+    Collection<String> headers = response.getHeaders(name);
+    if (headers == null) {
+      return Collections.emptyList();
+    }
+    if (headers instanceof List) {
+      return (List<String>) headers;
+    }
+    return new ArrayList<>(headers);
   }
 
   @Override
