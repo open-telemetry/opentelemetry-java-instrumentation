f04f89376925b53de44e846f847a11618fc77b62
Lauri Tulmin <ltulmin@splunk.com>
2021-10-04 19:59:40 +0300

    add comment

--- instrumentation/kafka-clients/kafka-clients-0.11/bootstrap/src/main/java/io/opentelemetry/javaagent/bootstrap/kafka/KafkaClientsConsumerProcessWrapper.java
+++ instrumentation/kafka-clients/kafka-clients-0.11/bootstrap/src/main/java/io/opentelemetry/javaagent/bootstrap/kafka/KafkaClientsConsumerProcessWrapper.java
@@ -5,6 +5,10 @@
 
 package io.opentelemetry.javaagent.bootstrap.kafka;
 
+// Classes used by multiple instrumentations should be in a bootstrap module to ensure that all
+// instrumentations see the same class. Helper classes are injected into each class loader that
+// contains an instrumentation that uses them, so instrumentations in different class loaders will
+// have separate copies of helper classes.
 public interface KafkaClientsConsumerProcessWrapper<T> {
 
   /**
