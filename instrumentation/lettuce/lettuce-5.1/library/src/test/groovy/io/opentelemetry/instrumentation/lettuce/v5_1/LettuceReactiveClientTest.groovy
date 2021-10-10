3dc41753ab50c1ac33616b1a5615deb018a4285d
Liudmila Molkova <limolkova@microsoft.com>
2021-10-10 10:52:14 -0700

    fix build

--- instrumentation/lettuce/lettuce-5.1/library/src/test/groovy/io/opentelemetry/instrumentation/lettuce/v5_1/LettuceReactiveClientTest.groovy
+++ instrumentation/lettuce/lettuce-5.1/library/src/test/groovy/io/opentelemetry/instrumentation/lettuce/v5_1/LettuceReactiveClientTest.groovy
@@ -7,13 +7,13 @@ package io.opentelemetry.instrumentation.lettuce.v5_1
 
 import io.lettuce.core.RedisClient
 import io.lettuce.core.resource.ClientResources
-import io.opentelemetry.instrumentation.reactor.ContextPropagationReactorOperator
+import io.opentelemetry.instrumentation.reactor.ContextPropagationOperator
 import io.opentelemetry.instrumentation.test.LibraryTestTrait
 import spock.lang.Shared
 
 class LettuceReactiveClientTest extends AbstractLettuceReactiveClientTest implements LibraryTestTrait {
   @Shared
-  ContextPropagationReactorOperator tracingOperator = ContextPropagationReactorOperator.create()
+  ContextPropagationOperator tracingOperator = ContextPropagationOperator.create()
 
   @Override
   RedisClient createClient(String uri) {
