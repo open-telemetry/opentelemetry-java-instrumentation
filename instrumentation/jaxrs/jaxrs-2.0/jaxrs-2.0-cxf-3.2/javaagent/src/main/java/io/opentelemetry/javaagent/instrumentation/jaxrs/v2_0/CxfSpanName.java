55f987613af269d4251e7b18b6039859c04a2c60
Nikita Salnikov-Tarnovski <gnikem@gmail.com>
2021-10-12 13:45:18 +0300

    Remove deprecated ServerSpanNaming.updateSource method (#4327)
    * Remove deprecated ServerSpanNaming.updateSource method

--- instrumentation/jaxrs/jaxrs-2.0/jaxrs-2.0-cxf-3.2/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/jaxrs/v2_0/CxfSpanName.java
+++ instrumentation/jaxrs/jaxrs-2.0/jaxrs-2.0-cxf-3.2/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/jaxrs/v2_0/CxfSpanName.java
@@ -7,41 +7,38 @@ package io.opentelemetry.javaagent.instrumentation.jaxrs.v2_0;
 
 import static io.opentelemetry.javaagent.instrumentation.jaxrs.v2_0.JaxrsPathUtil.normalizePath;
 
-import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.context.Context;
+import io.opentelemetry.instrumentation.api.servlet.ServerSpanNameSupplier;
 import io.opentelemetry.instrumentation.api.servlet.ServerSpanNaming;
 import io.opentelemetry.instrumentation.api.servlet.ServletContextPath;
-import io.opentelemetry.instrumentation.api.tracer.ServerSpan;
 import io.opentelemetry.javaagent.bootstrap.jaxrs.JaxrsContextPath;
 import org.apache.cxf.jaxrs.model.ClassResourceInfo;
 import org.apache.cxf.jaxrs.model.OperationResourceInfo;
 import org.apache.cxf.jaxrs.model.URITemplate;
 import org.apache.cxf.message.Exchange;
 
-public final class CxfTracingUtil {
+public final class CxfSpanName implements ServerSpanNameSupplier<String> {
 
-  private CxfTracingUtil() {}
+  public static final CxfSpanName INSTANCE = new CxfSpanName();
 
-  public static Context updateServerSpanName(Exchange exchange) {
+  public Context updateServerSpanName(Exchange exchange) {
     Context context = Context.current();
-    Span serverSpan = ServerSpan.fromContextOrNull(context);
-    if (serverSpan == null) {
-      return null;
-    }
+    String jaxrsName = calculateJaxrsName(context, exchange);
 
+    ServerSpanNaming.updateServerSpanName(
+        context, ServerSpanNaming.Source.NESTED_CONTROLLER, this, jaxrsName);
+
+    return JaxrsContextPath.init(context, jaxrsName);
+  }
+
+  private static String calculateJaxrsName(Context context, Exchange exchange) {
     OperationResourceInfo ori = exchange.get(OperationResourceInfo.class);
     ClassResourceInfo cri = ori.getClassResourceInfo();
     String name = getName(cri.getURITemplate(), ori.getURITemplate());
     if (name.isEmpty()) {
       return null;
     }
-
-    serverSpan.updateName(
-        ServletContextPath.prepend(context, JaxrsContextPath.prepend(context, name)));
-    // mark span name as updated from controller to avoid JaxRsAnnotationsTracer updating it
-    ServerSpanNaming.updateSource(context, ServerSpanNaming.Source.CONTROLLER);
-
-    return JaxrsContextPath.init(context, JaxrsContextPath.prepend(context, name));
+    return JaxrsContextPath.prepend(context, name);
   }
 
   private static String getName(URITemplate classTemplate, URITemplate operationTemplate) {
@@ -58,4 +55,11 @@ public final class CxfTracingUtil {
 
     return normalizePath(uriTemplate.getValue());
   }
+
+  @Override
+  public String get(Context context, String jaxrsName) {
+    return ServletContextPath.prepend(context, jaxrsName);
+  }
+
+  private CxfSpanName() {}
 }
