bb59ac2018249b32d39c3254ee837d1278b1b4e0
Nikita Salnikov-Tarnovski <gnikem@gmail.com>
2021-10-20 10:49:39 +0300

    Use new smoke test images

--- smoke-tests/src/test/groovy/io/opentelemetry/smoketest/JettySmokeTest.groovy
+++ smoke-tests/src/test/groovy/io/opentelemetry/smoketest/JettySmokeTest.groovy
@@ -5,20 +5,20 @@
 
 package io.opentelemetry.smoketest
 
-@AppServer(version = "9.4.35", jdk = "8")
-@AppServer(version = "9.4.35", jdk = "8-openj9")
-@AppServer(version = "9.4.35", jdk = "11")
-@AppServer(version = "9.4.35", jdk = "11-openj9")
-@AppServer(version = "9.4.35", jdk = "16")
-@AppServer(version = "9.4.35", jdk = "16-openj9")
-@AppServer(version = "10.0.0", jdk = "11")
-@AppServer(version = "10.0.0", jdk = "11-openj9")
-@AppServer(version = "10.0.0", jdk = "16")
-@AppServer(version = "10.0.0", jdk = "16-openj9")
-@AppServer(version = "11.0.1", jdk = "11")
-@AppServer(version = "11.0.1", jdk = "11-openj9")
-@AppServer(version = "11.0.1", jdk = "16")
-@AppServer(version = "11.0.1", jdk = "16-openj9")
+@AppServer(version = "9.4.39", jdk = "8")
+@AppServer(version = "9.4.39", jdk = "8-openj9")
+@AppServer(version = "9.4.39", jdk = "11")
+@AppServer(version = "9.4.39", jdk = "11-openj9")
+@AppServer(version = "9.4.39", jdk = "17")
+@AppServer(version = "9.4.39", jdk = "16-openj9")
+@AppServer(version = "10.0.7", jdk = "11")
+@AppServer(version = "10.0.7", jdk = "11-openj9")
+@AppServer(version = "10.0.7", jdk = "17")
+@AppServer(version = "10.0.7", jdk = "16-openj9")
+@AppServer(version = "11.0.7", jdk = "11")
+@AppServer(version = "11.0.7", jdk = "11-openj9")
+@AppServer(version = "11.0.7", jdk = "17")
+@AppServer(version = "11.0.7", jdk = "16-openj9")
 class JettySmokeTest extends AppServerTest {
 
   protected String getTargetImagePrefix() {
--- smoke-tests/src/test/groovy/io/opentelemetry/smoketest/TomcatSmokeTest.groovy
+++ smoke-tests/src/test/groovy/io/opentelemetry/smoketest/TomcatSmokeTest.groovy
@@ -8,20 +8,20 @@ package io.opentelemetry.smoketest
 @AppServer(version = "7.0.109", jdk = "8")
 @AppServer(version = "7.0.109", jdk = "8-openj9")
 @AppServer(version = "8.5.71", jdk = "8")
+@AppServer(version = "8.5.71", jdk = "8-openj9")
 @AppServer(version = "8.5.71", jdk = "11")
+@AppServer(version = "8.5.71", jdk = "11-openj9")
 @AppServer(version = "8.5.71", jdk = "17")
-@AppServer(version = "8.5.70", jdk = "8-openj9")
-@AppServer(version = "8.5.70", jdk = "11-openj9")
 @AppServer(version = "9.0.53", jdk = "8")
+@AppServer(version = "9.0.53", jdk = "8-openj9")
 @AppServer(version = "9.0.53", jdk = "11")
+@AppServer(version = "9.0.53", jdk = "11-openj9")
 @AppServer(version = "9.0.53", jdk = "17")
-@AppServer(version = "9.0.52", jdk = "8-openj9")
-@AppServer(version = "9.0.52", jdk = "11-openj9")
-// TODO (trask) there are Tomcat 10 images for Java 8 (both hotspot and openj9)
-//  but smoke tests are currently failing on them
+@AppServer(version = "10.0.11", jdk = "8")
+@AppServer(version = "10.0.11", jdk = "8-openj9")
 @AppServer(version = "10.0.11", jdk = "11")
+@AppServer(version = "10.0.11", jdk = "11-openj9")
 @AppServer(version = "10.0.11", jdk = "17")
-@AppServer(version = "10.0.8", jdk = "11-openj9")
 class TomcatSmokeTest extends AppServerTest {
 
   protected String getTargetImagePrefix() {
