b1d638fc350741865cc50b51195f463dc9b1b34a
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-10-20 15:02:06 -0700

    Move jaxrs bootstrap module (#4448)

--- instrumentation/jaxrs/jaxrs-2.0/jaxrs-2.0-common/javaagent/build.gradle.kts
+++ instrumentation/jaxrs/jaxrs-2.0/jaxrs-2.0-common/javaagent/build.gradle.kts
@@ -16,7 +16,7 @@ muzzle {
 }
 
 dependencies {
-  compileOnly(project(":instrumentation:jaxrs:bootstrap"))
+  compileOnly(project(":instrumentation:jaxrs:jaxrs-common:bootstrap"))
 
   compileOnly("javax.ws.rs:javax.ws.rs-api:2.0")
 
