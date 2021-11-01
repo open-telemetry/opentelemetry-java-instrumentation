5a410f32cd3efe8ce5d9695da8cdb94254147eef
Josh Suereth <joshuasuereth@google.com>
2021-11-01 08:55:28 -0400

    Update HTTP metrics 'view' code to match the specification, including optional attribute rules.

--- instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/instrumenter/http/TemporaryMetricsView.java
+++ instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/instrumenter/http/TemporaryMetricsView.java
@@ -18,25 +18,18 @@ import java.util.function.BiConsumer;
 @SuppressWarnings("rawtypes")
 final class TemporaryMetricsView {
 
-  private static final Set<AttributeKey> durationView = buildDurationView();
+  private static final Set<AttributeKey> durationAlwaysInclude = buildDurationAlwaysInclude();
 
   private static final Set<AttributeKey> activeRequestsView = buildActiveRequestsView();
 
-  private static Set<AttributeKey> buildDurationView() {
+
+  private static Set<AttributeKey> buildDurationAlwaysInclude() {
     // the list of included metrics is from
     // https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/metrics/semantic_conventions/http-metrics.md#attributes
     Set<AttributeKey> view = new HashSet<>();
     view.add(SemanticAttributes.HTTP_METHOD);
-    view.add(SemanticAttributes.HTTP_HOST);
-    view.add(SemanticAttributes.HTTP_SCHEME);
-    view.add(SemanticAttributes.HTTP_STATUS_CODE);
-    view.add(SemanticAttributes.HTTP_FLAVOR);
-    view.add(SemanticAttributes.NET_PEER_NAME);
-    view.add(SemanticAttributes.NET_PEER_PORT);
-    view.add(SemanticAttributes.NET_PEER_IP);
-    view.add(SemanticAttributes.HTTP_SERVER_NAME);
-    view.add(SemanticAttributes.NET_HOST_NAME);
-    view.add(SemanticAttributes.NET_HOST_PORT);
+    view.add(SemanticAttributes.HTTP_STATUS_CODE); // Optional
+    view.add(SemanticAttributes.HTTP_FLAVOR); // Optional
     return view;
   }
 
@@ -52,10 +45,68 @@ final class TemporaryMetricsView {
     return view;
   }
 
-  static Attributes applyDurationView(Attributes startAttributes, Attributes endAttributes) {
+  static Attributes applyClientDurationView(Attributes startAttributes, Attributes endAttributes) {
+    Set<AttributeKey> fullSet = new HashSet<>(durationAlwaysInclude);
+    // We pull identifying attributes according to:
+    // https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/metrics/semantic_conventions/http-metrics.md#attribute-alternatives
+    if (containsAttribute(SemanticAttributes.HTTP_URL, startAttributes, endAttributes)) {
+      // Use http.url alone
+      fullSet.add(SemanticAttributes.HTTP_URL);
+    } else if (containsAttribute(SemanticAttributes.HTTP_HOST, startAttributes, endAttributes)) {
+      // Use http.scheme, http.host and http.target
+      fullSet.add(SemanticAttributes.HTTP_SCHEME);
+      fullSet.add(SemanticAttributes.HTTP_HOST);
+      fullSet.add(SemanticAttributes.HTTP_TARGET);
+    } else if (containsAttribute(SemanticAttributes.NET_PEER_NAME, startAttributes, endAttributes)) {
+      // Use http.scheme, net.peer.name, net.peer.port and http.target
+      fullSet.add(SemanticAttributes.HTTP_SCHEME);
+      fullSet.add(SemanticAttributes.NET_PEER_NAME);
+      fullSet.add(SemanticAttributes.NET_PEER_PORT);
+      fullSet.add(SemanticAttributes.HTTP_TARGET);
+    } else {
+      // Use http.scheme, net.peer.ip, net.peer.port and http.target
+      fullSet.add(SemanticAttributes.HTTP_SCHEME);
+      fullSet.add(SemanticAttributes.NET_PEER_IP);
+      fullSet.add(SemanticAttributes.NET_PEER_PORT);
+      fullSet.add(SemanticAttributes.HTTP_TARGET);
+    }
     AttributesBuilder filtered = Attributes.builder();
-    applyView(filtered, startAttributes, durationView);
-    applyView(filtered, endAttributes, durationView);
+    applyView(filtered, startAttributes, fullSet);
+    applyView(filtered, endAttributes, fullSet);
+    return filtered.build();
+  }
+
+  private static <T> boolean containsAttribute(AttributeKey<T> key, Attributes startAttributes, Attributes endAttributes) {
+    return startAttributes.get(key) != null || endAttributes.get(key) != null;
+  }
+
+  static Attributes applyServerDurationView(Attributes startAttributes, Attributes endAttributes) {
+    Set<AttributeKey> fullSet = new HashSet<>(durationAlwaysInclude);
+    // We pull identifying attributes according to:
+    // https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/metrics/semantic_conventions/http-metrics.md#attribute-alternatives
+    if (containsAttribute(SemanticAttributes.HTTP_HOST, startAttributes, endAttributes)) {
+      // Use http.scheme, http.host and http.target
+      fullSet.add(SemanticAttributes.HTTP_SCHEME);
+      fullSet.add(SemanticAttributes.HTTP_HOST);
+      fullSet.add(SemanticAttributes.HTTP_TARGET);
+    } else if (containsAttribute(SemanticAttributes.HTTP_SERVER_NAME, startAttributes, endAttributes)) {
+      // Use http.scheme, http.server_name, net.host.port, http.target
+      fullSet.add(SemanticAttributes.HTTP_SCHEME);
+      fullSet.add(SemanticAttributes.HTTP_SERVER_NAME);
+      fullSet.add(SemanticAttributes.HTTP_TARGET);
+    } else if (containsAttribute(SemanticAttributes.NET_HOST_NAME, startAttributes, endAttributes)) {
+      // Use http.scheme, net.host.name, net.host.port, http.target
+      fullSet.add(SemanticAttributes.HTTP_SCHEME);
+      fullSet.add(SemanticAttributes.NET_HOST_NAME);
+      fullSet.add(SemanticAttributes.NET_HOST_PORT);
+      fullSet.add(SemanticAttributes.HTTP_TARGET);
+    } else {
+      // Use http.url
+      fullSet.add(SemanticAttributes.HTTP_URL);
+    }
+    AttributesBuilder filtered = Attributes.builder();
+    applyView(filtered, startAttributes, fullSet);
+    applyView(filtered, endAttributes, fullSet);
     return filtered.build();
   }
 
--- instrumentation-api/src/test/java/io/opentelemetry/instrumentation/api/instrumenter/http/TemporaryMetricsViewTest.java
+++ instrumentation-api/src/test/java/io/opentelemetry/instrumentation/api/instrumenter/http/TemporaryMetricsViewTest.java
@@ -6,7 +6,8 @@
 package io.opentelemetry.instrumentation.api.instrumenter.http;
 
 import static io.opentelemetry.instrumentation.api.instrumenter.http.TemporaryMetricsView.applyActiveRequestsView;
-import static io.opentelemetry.instrumentation.api.instrumenter.http.TemporaryMetricsView.applyDurationView;
+import static io.opentelemetry.instrumentation.api.instrumenter.http.TemporaryMetricsView.applyClientDurationView;
+import static io.opentelemetry.instrumentation.api.instrumenter.http.TemporaryMetricsView.applyServerDurationView;
 import static io.opentelemetry.sdk.testing.assertj.OpenTelemetryAssertions.attributeEntry;
 
 import io.opentelemetry.api.common.Attributes;
@@ -17,24 +18,224 @@ import org.junit.jupiter.api.Test;
 public class TemporaryMetricsViewTest {
 
   @Test
-  public void shouldApplyDurationView() {
+  public void shouldApplyClientDurationView_urlIdentity() {
     Attributes startAttributes =
         Attributes.builder()
+            .put(SemanticAttributes.HTTP_URL, "https://somehost/high/cardinality/12345")
+            .put(SemanticAttributes.HTTP_SCHEME, "https")
             .put(SemanticAttributes.HTTP_METHOD, "GET")
-            .put(SemanticAttributes.HTTP_URL, "http://somehost/high/cardinality/12345")
-            .put(SemanticAttributes.NET_PEER_NAME, "somehost")
+            .put(SemanticAttributes.HTTP_HOST, "somehost")
+            .put(SemanticAttributes.HTTP_TARGET, "/high/cardinality/12345")
             .build();
 
     Attributes endAttributes =
         Attributes.builder()
             .put(SemanticAttributes.HTTP_STATUS_CODE, 500)
             .put(SemanticAttributes.NET_PEER_NAME, "somehost2")
+            .put(SemanticAttributes.NET_PEER_IP, "127.0.0.1")
+            .put(SemanticAttributes.NET_PEER_PORT, 443)
             .build();
 
-    OpenTelemetryAssertions.assertThat(applyDurationView(startAttributes, endAttributes))
+    OpenTelemetryAssertions.assertThat(applyClientDurationView(startAttributes, endAttributes))
         .containsOnly(
+            attributeEntry(SemanticAttributes.HTTP_URL.getKey(), "https://somehost/high/cardinality/12345"),
             attributeEntry(SemanticAttributes.HTTP_METHOD.getKey(), "GET"),
-            attributeEntry(SemanticAttributes.NET_PEER_NAME.getKey(), "somehost2"),
+            attributeEntry(SemanticAttributes.HTTP_STATUS_CODE.getKey(), 500));
+  }
+
+  @Test
+  public void shouldApplyClientDurationView_httpHostIdentity() {
+    Attributes startAttributes =
+        Attributes.builder()
+            .put(SemanticAttributes.HTTP_METHOD, "GET")
+            .put(SemanticAttributes.HTTP_SCHEME, "https")
+            .put(SemanticAttributes.HTTP_HOST, "somehost")
+            .put(SemanticAttributes.HTTP_TARGET, "/high/cardinality/12345")
+            .build();
+
+    Attributes endAttributes =
+        Attributes.builder()
+            .put(SemanticAttributes.HTTP_STATUS_CODE, 500)
+            .put(SemanticAttributes.NET_PEER_NAME, "somehost2")
+            .put(SemanticAttributes.NET_PEER_IP, "127.0.0.1")
+            .put(SemanticAttributes.NET_PEER_PORT, 443)
+            .build();
+
+    OpenTelemetryAssertions.assertThat(applyClientDurationView(startAttributes, endAttributes))
+        .containsOnly(
+            attributeEntry(SemanticAttributes.HTTP_SCHEME.getKey(), "https"),
+            attributeEntry(SemanticAttributes.HTTP_HOST.getKey(), "somehost"),
+            attributeEntry(SemanticAttributes.HTTP_TARGET.getKey(), "/high/cardinality/12345"),
+            attributeEntry(SemanticAttributes.HTTP_METHOD.getKey(), "GET"),
+            attributeEntry(SemanticAttributes.HTTP_STATUS_CODE.getKey(), 500));
+  }
+
+  @Test
+  public void shouldApplyClientDurationView_netPeerNameIdentity() {
+    Attributes startAttributes =
+        Attributes.builder()
+            .put(SemanticAttributes.HTTP_METHOD, "GET")
+            .put(SemanticAttributes.HTTP_SCHEME, "https")
+            .put(SemanticAttributes.HTTP_TARGET, "/high/cardinality/12345")
+            .build();
+
+    Attributes endAttributes =
+        Attributes.builder()
+            .put(SemanticAttributes.HTTP_STATUS_CODE, 500)
+            .put(SemanticAttributes.NET_PEER_NAME, "somehost")
+            .put(SemanticAttributes.NET_PEER_IP, "127.0.0.1")
+            .put(SemanticAttributes.NET_PEER_PORT, 443)
+            .build();
+
+    OpenTelemetryAssertions.assertThat(applyClientDurationView(startAttributes, endAttributes))
+        .containsOnly(
+            attributeEntry(SemanticAttributes.HTTP_SCHEME.getKey(), "https"),
+            attributeEntry(SemanticAttributes.NET_PEER_NAME.getKey(), "somehost"),
+            attributeEntry(SemanticAttributes.NET_PEER_PORT.getKey(), 443),
+            attributeEntry(SemanticAttributes.HTTP_TARGET.getKey(), "/high/cardinality/12345"),
+            attributeEntry(SemanticAttributes.HTTP_METHOD.getKey(), "GET"),
+            attributeEntry(SemanticAttributes.HTTP_STATUS_CODE.getKey(), 500));
+  }
+
+  @Test
+  public void shouldApplyClientDurationView_netPeerIpIdentity() {
+    Attributes startAttributes =
+        Attributes.builder()
+            .put(SemanticAttributes.HTTP_METHOD, "GET")
+            .put(SemanticAttributes.HTTP_SCHEME, "https")
+            .put(SemanticAttributes.HTTP_TARGET, "/high/cardinality/12345")
+            .build();
+
+    Attributes endAttributes =
+        Attributes.builder()
+            .put(SemanticAttributes.HTTP_STATUS_CODE, 500)
+            .put(SemanticAttributes.NET_PEER_IP, "127.0.0.1")
+            .put(SemanticAttributes.NET_PEER_PORT, 443)
+            .build();
+
+    OpenTelemetryAssertions.assertThat(applyClientDurationView(startAttributes, endAttributes))
+        .containsOnly(
+            attributeEntry(SemanticAttributes.HTTP_SCHEME.getKey(), "https"),
+            attributeEntry(SemanticAttributes.NET_PEER_IP.getKey(), "127.0.0.1"),
+            attributeEntry(SemanticAttributes.NET_PEER_PORT.getKey(), 443),
+            attributeEntry(SemanticAttributes.HTTP_TARGET.getKey(), "/high/cardinality/12345"),
+            attributeEntry(SemanticAttributes.HTTP_METHOD.getKey(), "GET"),
+            attributeEntry(SemanticAttributes.HTTP_STATUS_CODE.getKey(), 500));
+  }
+
+
+  @Test
+  public void shouldApplyServerDurationView_hostIdentity() {
+    Attributes startAttributes =
+        Attributes.builder()
+            .put(SemanticAttributes.HTTP_METHOD, "GET")
+            .put(SemanticAttributes.HTTP_URL, "https://somehost/high/cardinality/12345")
+            .put(SemanticAttributes.HTTP_SCHEME, "https")
+            .put(SemanticAttributes.HTTP_HOST, "somehost")
+            .put(SemanticAttributes.HTTP_SERVER_NAME, "somehost")
+            .put(SemanticAttributes.HTTP_TARGET, "/somehost/high/cardinality/12345")
+            .put(SemanticAttributes.NET_HOST_NAME, "somehost")
+            .put(SemanticAttributes.NET_HOST_PORT, 443)
+            .build();
+
+    Attributes endAttributes =
+        Attributes.builder()
+            .put(SemanticAttributes.HTTP_STATUS_CODE, 500)
+            .put(SemanticAttributes.NET_PEER_NAME, "somehost2")
+            .put(SemanticAttributes.NET_PEER_IP, "127.0.0.1")
+            .put(SemanticAttributes.NET_PEER_PORT, 443)
+            .build();
+
+    OpenTelemetryAssertions.assertThat(applyServerDurationView(startAttributes, endAttributes))
+        .containsOnly(
+            attributeEntry(SemanticAttributes.HTTP_SCHEME.getKey(), "https"),
+            attributeEntry(SemanticAttributes.HTTP_HOST.getKey(), "somehost"),
+            attributeEntry(SemanticAttributes.HTTP_TARGET.getKey(), "/somehost/high/cardinality/12345"),
+            attributeEntry(SemanticAttributes.HTTP_METHOD.getKey(), "GET"),
+            attributeEntry(SemanticAttributes.HTTP_STATUS_CODE.getKey(), 500));
+  }
+
+  @Test
+  public void shouldApplyServerDurationView_serverNameIdentity() {
+    Attributes startAttributes =
+        Attributes.builder()
+            .put(SemanticAttributes.HTTP_METHOD, "GET")
+            .put(SemanticAttributes.HTTP_URL, "https://somehost/high/cardinality/12345")
+            .put(SemanticAttributes.HTTP_SCHEME, "https")
+            .put(SemanticAttributes.HTTP_SERVER_NAME, "somehost")
+            .put(SemanticAttributes.HTTP_TARGET, "/somehost/high/cardinality/12345")
+            .put(SemanticAttributes.NET_HOST_NAME, "somehost")
+            .put(SemanticAttributes.NET_HOST_PORT, 443)
+            .build();
+
+    Attributes endAttributes =
+        Attributes.builder()
+            .put(SemanticAttributes.HTTP_STATUS_CODE, 500)
+            .put(SemanticAttributes.NET_PEER_NAME, "somehost2")
+            .put(SemanticAttributes.NET_PEER_IP, "127.0.0.1")
+            .put(SemanticAttributes.NET_PEER_PORT, 443)
+            .build();
+
+    OpenTelemetryAssertions.assertThat(applyServerDurationView(startAttributes, endAttributes))
+        .containsOnly(
+            attributeEntry(SemanticAttributes.HTTP_SCHEME.getKey(), "https"),
+            attributeEntry(SemanticAttributes.HTTP_SERVER_NAME.getKey(), "somehost"),
+            attributeEntry(SemanticAttributes.HTTP_TARGET.getKey(), "/somehost/high/cardinality/12345"),
+            attributeEntry(SemanticAttributes.HTTP_METHOD.getKey(), "GET"),
+            attributeEntry(SemanticAttributes.HTTP_STATUS_CODE.getKey(), 500));
+  }
+
+  @Test
+  public void shouldApplyServerDurationView_netHostIdentity() {
+    Attributes startAttributes =
+        Attributes.builder()
+            .put(SemanticAttributes.HTTP_METHOD, "GET")
+            .put(SemanticAttributes.HTTP_URL, "https://somehost/high/cardinality/12345")
+            .put(SemanticAttributes.HTTP_SCHEME, "https")
+            .put(SemanticAttributes.HTTP_TARGET, "/somehost/high/cardinality/12345")
+            .put(SemanticAttributes.NET_HOST_NAME, "somehost")
+            .put(SemanticAttributes.NET_HOST_PORT, 443)
+            .build();
+
+    Attributes endAttributes =
+        Attributes.builder()
+            .put(SemanticAttributes.HTTP_STATUS_CODE, 500)
+            .put(SemanticAttributes.NET_PEER_IP, "127.0.0.1")
+            .put(SemanticAttributes.NET_PEER_PORT, 14204)
+            .build();
+
+    OpenTelemetryAssertions.assertThat(applyServerDurationView(startAttributes, endAttributes))
+        .containsOnly(
+            attributeEntry(SemanticAttributes.HTTP_SCHEME.getKey(), "https"),
+            attributeEntry(SemanticAttributes.NET_HOST_NAME.getKey(), "somehost"),
+            attributeEntry(SemanticAttributes.NET_HOST_PORT.getKey(), 443),
+            attributeEntry(SemanticAttributes.HTTP_TARGET.getKey(), "/somehost/high/cardinality/12345"),
+            attributeEntry(SemanticAttributes.HTTP_METHOD.getKey(), "GET"),
+            attributeEntry(SemanticAttributes.HTTP_STATUS_CODE.getKey(), 500));
+  }
+
+  @Test
+  public void shouldApplyServerDurationView_urlIdentity() {
+    Attributes startAttributes =
+        Attributes.builder()
+            .put(SemanticAttributes.HTTP_METHOD, "GET")
+            .put(SemanticAttributes.HTTP_URL, "https://somehost/high/cardinality/12345")
+            .put(SemanticAttributes.HTTP_SCHEME, "https")
+            .put(SemanticAttributes.HTTP_TARGET, "/somehost/high/cardinality/12345")
+            .put(SemanticAttributes.NET_HOST_PORT, 443)
+            .build();
+
+    Attributes endAttributes =
+        Attributes.builder()
+            .put(SemanticAttributes.HTTP_STATUS_CODE, 500)
+            .put(SemanticAttributes.NET_PEER_IP, "127.0.0.1")
+            .put(SemanticAttributes.NET_PEER_PORT, 443)
+            .build();
+
+    OpenTelemetryAssertions.assertThat(applyServerDurationView(startAttributes, endAttributes))
+        .containsOnly(
+            attributeEntry(SemanticAttributes.HTTP_URL.getKey(), "https://somehost/high/cardinality/12345"),
+            attributeEntry(SemanticAttributes.HTTP_METHOD.getKey(), "GET"),
             attributeEntry(SemanticAttributes.HTTP_STATUS_CODE.getKey(), 500));
   }
 
