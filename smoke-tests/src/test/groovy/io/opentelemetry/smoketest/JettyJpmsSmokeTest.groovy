b3d9ae82680850d55e56a4208b2c900e28401105
Lauri Tulmin <ltulmin@splunk.com>
2021-11-02 16:05:51 +0200

    Fix sun.misc.Unsafe generation on jdk17 (#4558)
    * Fix sun.misc.Unsafe generation on jdk17
    
    * remove jmxremote argument for now
    
    * spotless
    
    * remove unneeded annotation

--- smoke-tests/src/test/groovy/io/opentelemetry/smoketest/JettyJpmsSmokeTest.groovy
+++ smoke-tests/src/test/groovy/io/opentelemetry/smoketest/JettyJpmsSmokeTest.groovy
@@ -6,8 +6,7 @@
 package io.opentelemetry.smoketest
 
 // jetty test with java module system
-@AppServer(version = "11.0.7", jdk = "11")
-class JettyJpmsSmokeTest extends AppServerTest {
+abstract class JettyJpmsSmokeTest extends AppServerTest {
 
   @Override
   protected String getTargetImagePrefix() {
@@ -19,4 +18,11 @@ class JettyJpmsSmokeTest extends AppServerTest {
     // --jpms flags enables using java module system
     return ["java", "-jar", "/server/start.jar", "--jpms"]
   }
+}
+
+@AppServer(version = "11.0.7", jdk = "11")
+class Jetty11JpmsJdk11 extends JettyJpmsSmokeTest {
+}
+@AppServer(version = "11.0.7", jdk = "17")
+class Jetty11JpmsJdk17 extends JettyJpmsSmokeTest {
 }
