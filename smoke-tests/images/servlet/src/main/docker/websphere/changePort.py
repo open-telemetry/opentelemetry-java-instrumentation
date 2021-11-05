b70d9b61639ab135f643fb9501e6c050cd26d80d
Lauri Tulmin <ltulmin@splunk.com>
2021-11-05 22:30:15 +0200

    fix typo

--- smoke-tests/images/servlet/src/main/docker/websphere/changePort.py
+++ smoke-tests/images/servlet/src/main/docker/websphere/changePort.py
@@ -1,4 +1,4 @@
-# change port from 9080 to 88080
+# change port from 9080 to 8080
 AdminTask.modifyServerPort('server1', '[-nodeName ' + AdminControl.getNode() + ' -endPointName WC_defaulthost -host * -port 8080 -modifyShared true]')
 # add new port to default virtual host
 AdminConfig.create('HostAlias', AdminConfig.getid('/Cell:' + AdminControl.getCell() + '/VirtualHost:default_host/'), '[[port "8080"] [hostname "*"]]')
