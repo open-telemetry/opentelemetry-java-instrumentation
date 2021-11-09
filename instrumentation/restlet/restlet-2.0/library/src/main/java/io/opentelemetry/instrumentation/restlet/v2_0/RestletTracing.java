8b7c09791283addefb3687ab9025c2eb1b2cf052
Anna Nosek <aniano13@gmail.com>
2021-11-09 20:52:04 +0100

    Restlet 2.0 instrumentation (#4535)
    * add restlet 2.0 instrumentation
    
    * add restlet 2.0 instrumentation
    
    * revies: testLibrary, create RestletInstrumenterFactory

---
+++ instrumentation/restlet/restlet-2.0/library/src/main/java/io/opentelemetry/instrumentation/restlet/v2_0/RestletTracing.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.instrumentation.restlet.v2_0;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.instrumentation.api.instrumenter.Instrumenter;
+import org.restlet.Request;
+import org.restlet.Response;
+import org.restlet.routing.Filter;
+
+/** Entrypoint for tracing Restlet servers. */
+public final class RestletTracing {
+
+  /** Returns a new {@link RestletTracing} configured with the given {@link OpenTelemetry}. */
+  public static RestletTracing create(OpenTelemetry openTelemetry) {
+    return builder(openTelemetry).build();
+  }
+
+  /**
+   * Returns a new {@link RestletTracingBuilder} configured with the given {@link OpenTelemetry}.
+   */
+  public static RestletTracingBuilder builder(OpenTelemetry openTelemetry) {
+    return new RestletTracingBuilder(openTelemetry);
+  }
+
+  private final Instrumenter<Request, Response> serverInstrumenter;
+
+  RestletTracing(Instrumenter<Request, Response> serverInstrumenter) {
+    this.serverInstrumenter = serverInstrumenter;
+  }
+
+  /**
+   * Returns a new {@link Filter} which can be used to wrap {@link org.restlet.Restlet}
+   * implementations.
+   */
+  public Filter newFilter(String path) {
+    return new TracingFilter(serverInstrumenter, path);
+  }
+}
---
+++ instrumentation/restlet/restlet-2.0/library/src/main/java/io/opentelemetry/instrumentation/restlet/v2_0/internal/RestletHeadersGetter.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.instrumentation.restlet.v2_0.internal;
+
+import io.opentelemetry.context.propagation.TextMapGetter;
+import org.restlet.Message;
+import org.restlet.Request;
+import org.restlet.util.Series;
+
+final class RestletHeadersGetter implements TextMapGetter<Request> {
+
+  @Override
+  public Iterable<String> keys(Request carrier) {
+    return getHeaders(carrier).getNames();
+  }
+
+  @Override
+  public String get(Request carrier, String key) {
+    Series<?> headers = getHeaders(carrier);
+    return headers.getFirstValue(key, /* ignoreCase = */ true);
+  }
+
+  static Series<?> getHeaders(Message carrier) {
+    return (Series<?>) carrier.getAttributes().get("org.restlet.http.headers");
+  }
+}
