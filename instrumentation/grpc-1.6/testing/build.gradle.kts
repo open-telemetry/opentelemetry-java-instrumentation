9fa4e6768b7c05b1144b6fa901fc26f29f351f5a
Anuraag Agrawal <anuraaga@gmail.com>
2021-11-16 10:45:45 +0900

    Spot

--- instrumentation/grpc-1.6/testing/build.gradle.kts
+++ instrumentation/grpc-1.6/testing/build.gradle.kts
@@ -1,9 +1,3 @@
-import com.google.protobuf.gradle.generateProtoTasks
-import com.google.protobuf.gradle.id
-import com.google.protobuf.gradle.plugins
-import com.google.protobuf.gradle.protobuf
-import com.google.protobuf.gradle.protoc
-
 plugins {
   id("otel.java-conventions")
   id("otel.protobuf-conventions")
