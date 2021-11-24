663fb45764e80920321456ac5faf3df167a67900
Lauri Tulmin <ltulmin@splunk.com>
2021-11-24 20:19:44 +0200

    use concurrenthashmap for cache

--- instrumentation/servlet/servlet-common/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/servlet/ServletRequestParametersExtractor.java
+++ instrumentation/servlet/servlet-common/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/servlet/ServletRequestParametersExtractor.java
@@ -7,12 +7,13 @@ package io.opentelemetry.javaagent.instrumentation.servlet;
 
 import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.AttributesBuilder;
-import io.opentelemetry.instrumentation.api.caching.Cache;
 import io.opentelemetry.instrumentation.api.config.Config;
 import io.opentelemetry.instrumentation.api.instrumenter.AttributesExtractor;
 import io.opentelemetry.instrumentation.servlet.ServletAccessor;
 import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.function.BiConsumer;
 import javax.annotation.Nullable;
 
@@ -22,8 +23,8 @@ public class ServletRequestParametersExtractor<REQUEST, RESPONSE>
   private static final List<String> CAPTURE_REQUEST_PARAMETERS =
       Config.get().getList("otel.instrumentation.servlet.experimental.capture-request-parameters");
 
-  private static final Cache<String, AttributeKey<List<String>>> parameterKeysCache =
-      Cache.builder().build();
+  private static final ConcurrentMap<String, AttributeKey<List<String>>> parameterKeysCache =
+      new ConcurrentHashMap<>();
 
   private final ServletAccessor<REQUEST, RESPONSE> accessor;
 
