87d5930cb23e5f2de775a7bcbcbb3d7e73fe5778
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-11-23 17:56:04 -0800

    Handle CompletionException

--- instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/instrumenter/JdkErrorCauseExtractor.java
+++ instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/instrumenter/JdkErrorCauseExtractor.java
@@ -7,22 +7,39 @@ package io.opentelemetry.instrumentation.api.instrumenter;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.UndeclaredThrowableException;
-import java.util.concurrent.CompletionException;
 import java.util.concurrent.ExecutionException;
+import javax.annotation.Nullable;
 
 final class JdkErrorCauseExtractor implements ErrorCauseExtractor {
   static final ErrorCauseExtractor INSTANCE = new JdkErrorCauseExtractor();
+
+  @Nullable
+  private static final Class<?> COMPLETION_EXCEPTION_CLASS = getCompletionExceptionClass();
 
   @Override
   public Throwable extractCause(Throwable error) {
     if (error.getCause() != null
         && (error instanceof ExecutionException
-            || error instanceof CompletionException
+            || isInstanceOfCompletionException(error)
             || error instanceof InvocationTargetException
             || error instanceof UndeclaredThrowableException)) {
       return extractCause(error.getCause());
     }
     return error;
+  }
+
+  private static boolean isInstanceOfCompletionException(Throwable error) {
+    return COMPLETION_EXCEPTION_CLASS != null && COMPLETION_EXCEPTION_CLASS.isInstance(error);
+  }
+
+  @Nullable
+  private static Class<?> getCompletionExceptionClass() {
+    try {
+      return Class.forName("java.util.concurrent.CompletionException");
+    } catch (ClassNotFoundException e) {
+      // Android level 21 does not support java.util.concurrent.CompletionException
+      return null;
+    }
   }
 
   private JdkErrorCauseExtractor() {}
