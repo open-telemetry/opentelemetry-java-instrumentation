5c36ed3d23853d26d5d3d9501b8272db54f152c9
jack-berg <34418638+jack-berg@users.noreply.github.com>
2021-11-29 11:46:32 -0600

    Prototype Log4j2 Appender (#4375)
    * WIP
    
    * Use Log SDK snapshot
    
    * Update to 1.9.0 release, refine
    
    * Remove appenders from common log config
    
    * Respond to PR feedback
    
    * Update readme
    
    * Switch to compileOnly log sdk dependency, use logger name as instrumentation name
    
    * Switch to minimalist LogEvent mapping
    
    * PR feedback
    
    * PR feedback

--- instrumentation/log4j/log4j-2.13.2/library/build.gradle.kts
+++ instrumentation/log4j/log4j-2.13.2/library/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
 }
 
 dependencies {
+  compileOnly("io.opentelemetry:opentelemetry-sdk-logs")
+
   library("org.apache.logging.log4j:log4j-core:2.13.2")
 
   // Library instrumentation cannot be applied to 2.13.2 due to a bug in Log4J. The agent works
@@ -10,4 +12,7 @@ dependencies {
   testLibrary("org.apache.logging.log4j:log4j-core:2.13.3")
 
   testImplementation(project(":instrumentation:log4j:log4j-2-common:testing"))
+  testImplementation("io.opentelemetry:opentelemetry-sdk-logs")
+
+  testImplementation("org.mockito:mockito-core")
 }
