b2abf6899c5a50242c09b6820fcd77ee8d110f09
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-12-05 19:14:34 -0800

    Update tests now that netty captures http.scheme

--- testing-common/src/main/groovy/io/opentelemetry/instrumentation/test/base/HttpServerTest.groovy
+++ testing-common/src/main/groovy/io/opentelemetry/instrumentation/test/base/HttpServerTest.groovy
@@ -630,8 +630,7 @@ abstract class HttpServerTest<SERVER> extends InstrumentationSpecification imple
         "${SemanticAttributes.HTTP_USER_AGENT.key}" TEST_USER_AGENT
 
         "${SemanticAttributes.HTTP_HOST}" { it == "localhost" || it == "localhost:${port}" }
-        // TODO netty does not set http.scheme - refactor HTTP server tests so that it's possible to specify extracted attributes, like in HTTP client tests
-        "${SemanticAttributes.HTTP_SCHEME}" { it == "http" || it == null }
+        "${SemanticAttributes.HTTP_SCHEME}" "http"
         "${SemanticAttributes.HTTP_TARGET}" endpoint.resolvePath(address).getPath() + "${endpoint == QUERY_PARAM ? "?${endpoint.body}" : ""}"
 
         if (extraAttributes.contains(SemanticAttributes.HTTP_REQUEST_CONTENT_LENGTH)) {
