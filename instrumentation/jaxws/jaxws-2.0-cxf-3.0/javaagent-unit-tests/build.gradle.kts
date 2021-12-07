140581a8059f1689a064646cacec2ea7dbec9e59
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2021-12-07 11:41:08 +0100

    fix tests

--- instrumentation/jaxws/jaxws-2.0-cxf-3.0/javaagent-unit-tests/build.gradle.kts
+++ instrumentation/jaxws/jaxws-2.0-cxf-3.0/javaagent-unit-tests/build.gradle.kts
@@ -8,5 +8,6 @@ dependencies {
 
   implementation(project(":instrumentation:jaxws:jaxws-2.0-cxf-3.0:javaagent"))
 
+  testImplementation(project(":instrumentation-api"))
   testImplementation("org.apache.cxf:cxf-rt-frontend-jaxws:3.0.0")
 }
