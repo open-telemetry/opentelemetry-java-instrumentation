b5516c85303761be82d2738a45df2f980b286d7a
jason plumb <75337021+breedx-splk@users.noreply.github.com>
2021-12-08 09:23:13 -0800

    Enhancements to overhead tests (#4833)
    * add average machine cpu total and total gc pause nanos
    
    * Collect run durations.
    
    * Report in config order (don't sort by name). Output new fields.
    
    * add additional metrics to table.
    
    * consistent casing
    
    * bump
    
    * instead of grouping agent data, group by field. consolidate fields into one place.
    
    * consolidate and simplify config
    
    * typo
    
    * Update benchmark-overhead/src/test/java/io/opentelemetry/results/AppPerfResults.java
    
    Co-authored-by: Mateusz Rzeszutek <mrzeszutek@splunk.com>
    
    * migrate heap to MB and add units where missing
    
    Co-authored-by: Mateusz Rzeszutek <mrzeszutek@splunk.com>

--- benchmark-overhead/src/test/java/io/opentelemetry/results/PrintStreamPersister.java
+++ benchmark-overhead/src/test/java/io/opentelemetry/results/PrintStreamPersister.java
@@ -4,66 +4,64 @@
  */
 package io.opentelemetry.results;
 
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+
 import io.opentelemetry.config.TestConfig;
 import java.io.PrintStream;
-import java.util.Comparator;
+import java.time.Duration;
 import java.util.Date;
 import java.util.List;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 class PrintStreamPersister implements ResultsPersister {
 
   private final PrintStream out;
 
-  public PrintStreamPersister(PrintStream out) {
-    this.out = out;
-  }
+  public PrintStreamPersister(PrintStream out) {this.out = out;}
 
   @Override
   public void write(List<AppPerfResults> results) {
-    List<AppPerfResults> sorted =
-        results.stream()
-            .sorted(Comparator.comparing(AppPerfResults::getAgentName))
-            .collect(Collectors.toList());
-    TestConfig config = sorted.stream().findFirst().get().config;
+    TestConfig config = results.stream().findFirst().get().config;
     out.println("----------------------------------------------------------");
     out.println(" Run at " + new Date());
     out.printf(" %s : %s\n", config.getName(), config.getDescription());
-    out.printf(
-        " %d users, %d iterations\n",
-        config.getConcurrentConnections(), config.getTotalIterations());
+    out.printf(" %d users, %d iterations\n", config.getConcurrentConnections(), config.getTotalIterations());
     out.println("----------------------------------------------------------");
 
-    display(sorted, "Agent", appPerfResults -> appPerfResults.agent.getName());
-    display(sorted, "Avg. CPU (user)", res -> String.valueOf(res.averageJvmUserCpu));
-    display(sorted, "Max. CPU (user)", res -> String.valueOf(res.maxJvmUserCpu));
-    display(sorted, "Startup time (ms)", res -> String.valueOf(res.startupDurationMs));
-    display(sorted, "Total allocated MB", res -> format(res.getTotalAllocatedMB()));
-    display(sorted, "Heap (min)", res -> String.valueOf(res.heapUsed.min));
-    display(sorted, "Heap (max)", res -> String.valueOf(res.heapUsed.max));
-    display(sorted, "Thread switch rate", res -> String.valueOf(res.maxThreadContextSwitchRate));
-    display(sorted, "GC time", res -> String.valueOf(res.totalGCTime));
-    display(sorted, "Req. mean", res -> format(res.requestAvg));
-    display(sorted, "Req. p95", res -> format(res.requestP95));
-    display(sorted, "Iter. mean", res -> format(res.iterationAvg));
-    display(sorted, "Iter. p95", res -> format(res.iterationP95));
-    display(sorted, "Net read avg (bps)", res -> format(res.averageNetworkRead));
-    display(sorted, "Net write avg (bps)", res -> format(res.averageNetworkWrite));
-    display(sorted, "Peak threads", res -> String.valueOf(res.peakThreadCount));
+    display(results, "Agent", appPerfResults -> appPerfResults.agent.getName());
+    display(results, "Run duration", res -> {
+      Duration duration = Duration.ofMillis(res.runDurationMs);
+      return String.format("%02d:%02d:%02d", duration.toHours(), duration.toMinutesPart(), duration.toSecondsPart());
+    });
+    display(results, "Avg. CPU (user) %", res -> String.valueOf(res.averageJvmUserCpu));
+    display(results, "Max. CPU (user) %", res -> String.valueOf(res.maxJvmUserCpu));
+    display(results, "Avg. mch tot cpu %", res -> String.valueOf(res.averageMachineCpuTotal));
+    display(results, "Startup time (ms)", res -> String.valueOf(res.startupDurationMs));
+    display(results, "Total allocated MB", res -> format(res.getTotalAllocatedMB()));
+    display(results, "Min heap used (MB)", res -> format(res.getMinHeapUsedMB()));
+    display(results, "Max heap used (MB)", res -> format(res.getMaxHeapUsedMB()));
+    display(results, "Thread switch rate", res -> String.valueOf(res.maxThreadContextSwitchRate));
+    display(results, "GC time (ms)", res -> String.valueOf(res.totalGCTime));
+    display(results, "GC pause time (ms)", res -> String.valueOf(NANOSECONDS.toMillis(res.totalGcPauseNanos)));
+    display(results, "Req. mean (ms)", res -> format(res.requestAvg));
+    display(results, "Req. p95 (ms)", res -> format(res.requestP95));
+    display(results, "Iter. mean (ms)", res -> format(res.iterationAvg));
+    display(results, "Iter. p95 (ms)", res -> format(res.iterationP95));
+    display(results, "Net read avg (bps)", res -> format(res.averageNetworkRead));
+    display(results, "Net write avg (bps)", res -> format(res.averageNetworkWrite));
+    display(results, "Peak threads", res -> String.valueOf(res.peakThreadCount));
   }
 
-  private void display(
-      List<AppPerfResults> results, String pref, Function<AppPerfResults, String> vs) {
+  private void display(List<AppPerfResults> results, String pref, Function<AppPerfResults, String> vs) {
     out.printf("%-20s: ", pref);
-    results.forEach(
-        result -> {
-          out.printf("%17s", vs.apply(result));
-        });
+    results.forEach(result -> {
+      out.printf("%17s", vs.apply(result));
+    });
     out.println();
   }
 
   private String format(double d) {
     return String.format("%.2f", d);
   }
+
 }
