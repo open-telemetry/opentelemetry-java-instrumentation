458ebc5e5502ae0341ac0d1787e8cad82ed899db
Anuraag Agrawal <anuraaga@gmail.com>
2021-12-10 07:55:09 +0900

    Small cleanups to conventions projects. (#4839)

--- gradle-plugins/settings.gradle.kts
+++ gradle-plugins/settings.gradle.kts
@@ -1,7 +1,6 @@
 pluginManagement {
   plugins {
     id("com.gradle.plugin-publish") version "0.15.0"
-    id("org.jetbrains.kotlin.jvm") version "1.5.10"
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
   }
 }
