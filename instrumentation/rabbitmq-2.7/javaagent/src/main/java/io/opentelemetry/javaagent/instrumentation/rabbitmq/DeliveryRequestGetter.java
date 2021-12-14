5869df80e5b9a46cb5165b55899895e5c53c1f74
Asaf Ben Aharon <asafb26@gmail.com>
2021-12-14 22:01:16 +0200

    Fix RabbitMQ instrumentation consumption on empty headers (#4903)
    * Handle null on headers getters
    
    * early returns

--- instrumentation/rabbitmq-2.7/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/rabbitmq/DeliveryRequestGetter.java
+++ instrumentation/rabbitmq-2.7/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/rabbitmq/DeliveryRequestGetter.java
@@ -7,24 +7,33 @@ package io.opentelemetry.javaagent.instrumentation.rabbitmq;
 
 import io.opentelemetry.context.propagation.TextMapGetter;
 import java.util.Collections;
+import java.util.Map;
 
 enum DeliveryRequestGetter implements TextMapGetter<DeliveryRequest> {
   INSTANCE;
 
   @Override
   public Iterable<String> keys(DeliveryRequest carrier) {
-    return carrier != null
-        ? carrier.getProperties().getHeaders().keySet()
-        : Collections.emptyList();
+    if (carrier == null) {
+      return Collections.emptyList();
+    }
+    Map<String, Object> headers = carrier.getProperties().getHeaders();
+    if (headers == null) {
+      return Collections.emptyList();
+    }
+    return headers.keySet();
   }
 
   @Override
   public String get(DeliveryRequest carrier, String key) {
-    if (carrier != null) {
-      Object obj = carrier.getProperties().getHeaders().get(key);
-      return obj == null ? null : obj.toString();
-    } else {
+    if (carrier == null) {
       return null;
     }
+    Map<String, Object> headers = carrier.getProperties().getHeaders();
+    if (headers == null) {
+      return null;
+    }
+    Object obj = headers.get(key);
+    return obj == null ? null : obj.toString();
   }
 }
