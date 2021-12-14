78f05916da727a9f7e5b3967a43a19366d4375b0
asaf.ben-aharon <asafb26@gmail.com>
2021-12-14 20:39:18 +0200

    Handle null on headers getters

--- instrumentation/rabbitmq-2.7/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/rabbitmq/DeliveryRequestGetter.java
+++ instrumentation/rabbitmq-2.7/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/rabbitmq/DeliveryRequestGetter.java
@@ -7,21 +7,32 @@ package io.opentelemetry.javaagent.instrumentation.rabbitmq;
 
 import io.opentelemetry.context.propagation.TextMapGetter;
 import java.util.Collections;
+import java.util.Map;
 
 enum DeliveryRequestGetter implements TextMapGetter<DeliveryRequest> {
   INSTANCE;
 
   @Override
   public Iterable<String> keys(DeliveryRequest carrier) {
-    return carrier != null
-        ? carrier.getProperties().getHeaders().keySet()
-        : Collections.emptyList();
+    if (carrier != null) {
+      Map<String, Object> headers = carrier.getProperties().getHeaders();
+      if (headers == null) {
+        return Collections.emptyList();
+      }
+      return headers.keySet();
+    } else {
+      return Collections.emptyList();
+    }
   }
 
   @Override
   public String get(DeliveryRequest carrier, String key) {
     if (carrier != null) {
-      Object obj = carrier.getProperties().getHeaders().get(key);
+      Map<String, Object> headers = carrier.getProperties().getHeaders();
+      if (headers == null) {
+        return null;
+      }
+      Object obj = headers.get(key);
       return obj == null ? null : obj.toString();
     } else {
       return null;
