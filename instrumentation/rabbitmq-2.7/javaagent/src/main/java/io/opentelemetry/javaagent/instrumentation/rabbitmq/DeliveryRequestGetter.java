0b960fc934f3fe3dc72423b27bf96d5e9ef4a567
asaf.ben-aharon <asafb26@gmail.com>
2021-12-14 21:12:49 +0200

    early returns

--- instrumentation/rabbitmq-2.7/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/rabbitmq/DeliveryRequestGetter.java
+++ instrumentation/rabbitmq-2.7/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/rabbitmq/DeliveryRequestGetter.java
@@ -14,28 +14,26 @@ enum DeliveryRequestGetter implements TextMapGetter<DeliveryRequest> {
 
   @Override
   public Iterable<String> keys(DeliveryRequest carrier) {
-    if (carrier != null) {
-      Map<String, Object> headers = carrier.getProperties().getHeaders();
-      if (headers == null) {
-        return Collections.emptyList();
-      }
-      return headers.keySet();
-    } else {
+    if (carrier == null) {
       return Collections.emptyList();
     }
+    Map<String, Object> headers = carrier.getProperties().getHeaders();
+    if (headers == null) {
+      return Collections.emptyList();
+    }
+    return headers.keySet();
   }
 
   @Override
   public String get(DeliveryRequest carrier, String key) {
-    if (carrier != null) {
-      Map<String, Object> headers = carrier.getProperties().getHeaders();
-      if (headers == null) {
-        return null;
-      }
-      Object obj = headers.get(key);
-      return obj == null ? null : obj.toString();
-    } else {
+    if (carrier == null) {
       return null;
     }
+    Map<String, Object> headers = carrier.getProperties().getHeaders();
+    if (headers == null) {
+      return null;
+    }
+    Object obj = headers.get(key);
+    return obj == null ? null : obj.toString();
   }
 }
