f79bb3145300a9d6efd434372e3b3c4850b3d109
Anuraag Agrawal <anuraaga@gmail.com>
2021-12-15 02:39:38 +0000

    Fix test

--- instrumentation-api/src/test/java/io/opentelemetry/instrumentation/api/instrumenter/rpc/RpcServerMetricsTest.java
+++ instrumentation-api/src/test/java/io/opentelemetry/instrumentation/api/instrumenter/rpc/RpcServerMetricsTest.java
@@ -29,7 +29,7 @@ class RpcServerMetricsTest {
 
   @Test
   void collectsMetrics() {
-    InMemoryMetricReader metricReader = InMemoryMetricReader.create();
+    InMemoryMetricReader metricReader = InMemoryMetricReader.createDelta();
     SdkMeterProvider meterProvider =
         SdkMeterProvider.builder().registerMetricReader(metricReader).build();
 
