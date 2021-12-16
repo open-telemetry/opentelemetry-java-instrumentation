df19407a62e263dbae210efb8e2cd38c12c9170f
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-12-16 10:18:17 -0800

    Run servlet smoke tests against Java 18

--- smoke-tests/src/test/groovy/io/opentelemetry/smoketest/LibertySmokeTest.groovy
+++ smoke-tests/src/test/groovy/io/opentelemetry/smoketest/LibertySmokeTest.groovy
@@ -22,36 +22,51 @@ abstract class LibertySmokeTest extends AppServerTest {
 @AppServer(version = "20.0.0.12", jdk = "8")
 class Liberty20Jdk8 extends LibertySmokeTest {
 }
+
 @AppServer(version = "20.0.0.12", jdk = "8-openj9")
 class Liberty20Jdk8Openj9 extends LibertySmokeTest {
 }
+
 @AppServer(version = "20.0.0.12", jdk = "11")
 class Liberty20Jdk11 extends LibertySmokeTest {
 }
+
 @AppServer(version = "20.0.0.12", jdk = "11-openj9")
 class Liberty20Jdk11Openj9 extends LibertySmokeTest {
 }
+
 @AppServer(version = "20.0.0.12", jdk = "16")
 class Liberty20Jdk16 extends LibertySmokeTest {
 }
+
 @AppServer(version = "20.0.0.12", jdk = "16-openj9")
 class Liberty20Jdk16Openj9 extends LibertySmokeTest {
 }
+
 @AppServer(version = "21.0.0.10", jdk = "8")
 class Liberty21Jdk8 extends LibertySmokeTest {
 }
+
 @AppServer(version = "21.0.0.10", jdk = "8-openj9")
 class Liberty21Jdk8Openj9 extends LibertySmokeTest {
 }
+
 @AppServer(version = "21.0.0.10", jdk = "11")
 class Liberty21Jdk11 extends LibertySmokeTest {
 }
+
 @AppServer(version = "21.0.0.10", jdk = "11-openj9")
 class Liberty21Jdk11Openj9 extends LibertySmokeTest {
 }
+
 @AppServer(version = "21.0.0.10", jdk = "17")
 class Liberty21Jdk17 extends LibertySmokeTest {
 }
+
+@AppServer(version = "21.0.0.10", jdk = "18")
+class Liberty21Jdk18 extends LibertySmokeTest {
+}
+
 @AppServer(version = "21.0.0.10", jdk = "16-openj9")
 class Liberty21Jdk16Openj9 extends LibertySmokeTest {
 }
