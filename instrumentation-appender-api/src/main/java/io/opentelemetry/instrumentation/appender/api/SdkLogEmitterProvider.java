76b724581e628ec0dece2e1bf1e75fad6c286954
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-12-15 20:53:36 -0800

    drift

--- instrumentation-appender-api/src/main/java/io/opentelemetry/instrumentation/appender/api/SdkLogEmitterProvider.java
+++ instrumentation-appender-api/src/main/java/io/opentelemetry/instrumentation/appender/api/SdkLogEmitterProvider.java
@@ -5,34 +5,21 @@
 
 package io.opentelemetry.instrumentation.appender.api;
 
-import javax.annotation.Nullable;
-
 public final class SdkLogEmitterProvider implements LogEmitterProvider {
 
-  private static final SdkLogEmitterProvider INSTANCE = new SdkLogEmitterProvider(null);
-
-  @Nullable private final io.opentelemetry.sdk.logs.SdkLogEmitterProvider delegate;
+  private final io.opentelemetry.sdk.logs.SdkLogEmitterProvider delegate;
 
   public static SdkLogEmitterProvider from(
       io.opentelemetry.sdk.logs.SdkLogEmitterProvider delegate) {
     return new SdkLogEmitterProvider(delegate);
   }
 
-  public static SdkLogEmitterProvider noop() {
-    return INSTANCE;
-  }
-
-  private SdkLogEmitterProvider(
-      @Nullable io.opentelemetry.sdk.logs.SdkLogEmitterProvider delegate) {
+  private SdkLogEmitterProvider(io.opentelemetry.sdk.logs.SdkLogEmitterProvider delegate) {
     this.delegate = delegate;
   }
 
   @Override
   public LogEmitterBuilder logEmitterBuilder(String instrumentationName) {
-    if (delegate != null) {
-      return new SdkLogEmitterBuilder(delegate.logEmitterBuilder(instrumentationName));
-    } else {
-      return NoopLogEmitterBuilder.INSTANCE;
-    }
+    return new SdkLogEmitterBuilder(delegate.logEmitterBuilder(instrumentationName));
   }
 }
