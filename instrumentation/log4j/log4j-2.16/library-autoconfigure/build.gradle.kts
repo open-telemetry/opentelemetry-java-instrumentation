6776a7e1811ad3b0393bbb64a8ccb44c4e3e0d77
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-12-17 08:59:18 -0800

    Fix log4j library-autoconfigure publishing (#4928)

--- instrumentation/log4j/log4j-2.16/library-autoconfigure/build.gradle.kts
+++ instrumentation/log4j/log4j-2.16/library-autoconfigure/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
   id("otel.library-instrumentation")
 }
 
+base.archivesName.set("${base.archivesName.get()}-autoconfigure")
+
 dependencies {
   library("org.apache.logging.log4j:log4j-core:2.16.0")
 
