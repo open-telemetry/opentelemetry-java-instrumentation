37e79fd73b50d4b285e19bef1f29966178f3bbb5
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-12-19 13:50:53 -0800

    Use assertInverse

--- instrumentation/logback/logback-appender-1.0/javaagent/build.gradle.kts
+++ instrumentation/logback/logback-appender-1.0/javaagent/build.gradle.kts
@@ -7,14 +7,17 @@ muzzle {
     group.set("ch.qos.logback")
     module.set("logback-classic")
     versions.set("[0.9.16,)")
+    skip("0.9.6") // has dependency on SNAPSHOT org.slf4j:slf4j-api:1.4.0-SNAPSHOT
+    skip("0.8") // has dependency on non-existent org.slf4j:slf4j-api:1.1.0-RC0
+    skip("0.6") // has dependency on pom only javax.jms:jms:1.1
+    assertInverse.set(true)
   }
 }
 
 dependencies {
-  compileOnly("ch.qos.logback:logback-classic:0.9.16")
+  library("ch.qos.logback:logback-classic:0.9.16")
 
   compileOnly(project(":instrumentation-api-appender"))
 
-  testImplementation("ch.qos.logback:logback-classic:0.9.16")
   testImplementation("org.awaitility:awaitility")
 }
