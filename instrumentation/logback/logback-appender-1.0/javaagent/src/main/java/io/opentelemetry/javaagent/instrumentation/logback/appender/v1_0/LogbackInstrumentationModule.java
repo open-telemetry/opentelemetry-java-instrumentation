f5fb1deeb88c44a5b2828cbc50705c6616d9e28a
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-12-19 17:51:22 -0800

    sync

--- instrumentation/logback/logback-appender-1.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/logback/appender/v1_0/LogbackInstrumentationModule.java
+++ instrumentation/logback/logback-appender-1.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/logback/appender/v1_0/LogbackInstrumentationModule.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @AutoService(InstrumentationModule.class)
 public class LogbackInstrumentationModule extends InstrumentationModule {
+
   public LogbackInstrumentationModule() {
     super("logback-appender", "logback-appender-1.0");
   }
