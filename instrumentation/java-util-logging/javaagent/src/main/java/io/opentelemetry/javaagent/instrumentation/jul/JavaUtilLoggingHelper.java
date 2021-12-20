83a2bde5e33922cf86491e5d9d60042f043b2d17
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-12-19 20:37:45 -0800

    Update instrumentation/java-util-logging/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/jul/JavaUtilLoggingHelper.java
    Co-authored-by: Anuraag Agrawal <anuraaga@gmail.com>

--- instrumentation/java-util-logging/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/jul/JavaUtilLoggingHelper.java
+++ instrumentation/java-util-logging/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/jul/JavaUtilLoggingHelper.java
@@ -20,7 +20,7 @@ import java.util.logging.Formatter;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
-public class JavaUtilLoggingHelper {
+public final class JavaUtilLoggingHelper {
 
   private static final Formatter FORMATTER = new AccessibleFormatter();
 
