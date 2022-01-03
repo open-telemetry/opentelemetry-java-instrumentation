a022f0ce598319d4156dc7d253ed39a0cde9ed31
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2022-01-03 13:33:39 +0100

    Micrometer bridge instrumentation (#4919)
    * Micrometer bridge instrumentation
    
    * gauges with the same name and different attributes
    
    * weak ref gauge
    
    * one more test
    
    * disable by default + muzzle
    
    * code review comments
    
    * log one-time warning
    
    * make AsyncInstrumentRegistry actually thread safe
    
    * code review comments
    
    * one more minor fix

---
+++ instrumentation/micrometer/micrometer-1.5/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/micrometer/v1_5/MicrometerInstrumentationModule.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.micrometer.v1_5;
+
+import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.hasClassesNamed;
+
+import com.google.auto.service.AutoService;
+import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
+import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
+import java.util.Collections;
+import java.util.List;
+import net.bytebuddy.matcher.ElementMatcher;
+
+@AutoService(InstrumentationModule.class)
+public class MicrometerInstrumentationModule extends InstrumentationModule {
+
+  public MicrometerInstrumentationModule() {
+    super("micrometer", "micrometer-1.5");
+  }
+
+  @Override
+  public ElementMatcher.Junction<ClassLoader> classLoaderMatcher() {
+    // added in 1.5
+    return hasClassesNamed("io.micrometer.core.instrument.config.validate.Validated");
+  }
+
+  @Override
+  protected boolean defaultEnabled() {
+    // TODO: disabled by default, since not all instruments are implemented
+    return false;
+  }
+
+  @Override
+  public List<TypeInstrumentation> typeInstrumentations() {
+    return Collections.singletonList(new MetricsInstrumentation());
+  }
+}
---
+++ instrumentation/micrometer/micrometer-1.5/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/micrometer/v1_5/MicrometerSingletons.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.micrometer.v1_5;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.opentelemetry.api.GlobalOpenTelemetry;
+
+public final class MicrometerSingletons {
+
+  private static final MeterRegistry METER_REGISTRY =
+      OpenTelemetryMeterRegistry.create(GlobalOpenTelemetry.get());
+
+  public static MeterRegistry meterRegistry() {
+    return METER_REGISTRY;
+  }
+
+  private MicrometerSingletons() {}
+}
