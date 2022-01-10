2ac6d2231e3cd86f58368192b61c3aa0bde53676
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-01-10 03:31:09 -0800

    Add OTLP logs exporters (#5060)

--- javaagent-exporters/build.gradle.kts
+++ javaagent-exporters/build.gradle.kts
@@ -8,8 +8,10 @@ dependencies {
   implementation("io.opentelemetry:opentelemetry-exporter-jaeger")
   implementation("io.opentelemetry:opentelemetry-exporter-otlp")
   implementation("io.opentelemetry:opentelemetry-exporter-otlp-metrics")
+  implementation("io.opentelemetry:opentelemetry-exporter-otlp-logs")
   implementation("io.opentelemetry:opentelemetry-exporter-otlp-http-trace")
   implementation("io.opentelemetry:opentelemetry-exporter-otlp-http-metrics")
+  implementation("io.opentelemetry:opentelemetry-exporter-otlp-http-logs")
   implementation("io.opentelemetry:opentelemetry-exporter-logging-otlp")
 
   implementation("io.opentelemetry:opentelemetry-exporter-prometheus")
