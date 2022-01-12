5577afbf4518b79c26dc7e3a06d9c37c3ad563ef
Anuraag Agrawal <anuraaga@gmail.com>
2022-01-13 00:21:13 +0900

    Update dependencies (#5083)
    * Update dependencies
    
    * Downgrade jackson for jax test

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 pluginManagement {
   plugins {
-    id("com.github.ben-manes.versions") version "0.39.0"
+    id("com.github.ben-manes.versions") version "0.41.0"
     id("com.github.jk1.dependency-license-report") version "2.0"
     id("com.gradle.plugin-publish") version "0.18.0"
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
