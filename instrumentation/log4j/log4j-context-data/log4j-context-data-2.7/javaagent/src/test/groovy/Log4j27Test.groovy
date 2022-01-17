bf8d953ee0bdeafc7c0f93546ebbe090ed3fcd35
Harold Dost <h.dost@criteo.com>
2022-01-17 11:39:41 +0100

    Remove log4j-context-data-2.7
    It doesn't make sense to encourage people to use vulnerable software for new projects.
    
    Affected by Log4j Major CVEs
    https://www.cvedetails.com/cve/CVE-2021-45046
    https://www.cvedetails.com/cve/CVE-2021-44832
    https://www.cvedetails.com/cve/CVE-2021-44228
    https://www.cvedetails.com/cve/CVE-2021-45105
    
    Relates to #5152
    
    Signed-off-by: Harold Dost <h.dost@criteo.com>

--- instrumentation/log4j/log4j-context-data/log4j-context-data-2.7/javaagent/src/test/groovy/Log4j27Test.groovy
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright The OpenTelemetry Authors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import io.opentelemetry.instrumentation.test.AgentTestTrait
-
-class Log4j27Test extends Log4j2Test implements AgentTestTrait {
-}
