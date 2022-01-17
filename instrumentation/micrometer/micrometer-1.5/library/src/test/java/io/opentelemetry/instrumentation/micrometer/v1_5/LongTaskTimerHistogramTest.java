c875c6b46c5d61d9368d1840c3bbbe367b3cc363
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2022-01-17 17:02:53 +0100

    spotless

--- instrumentation/micrometer/micrometer-1.5/library/src/test/java/io/opentelemetry/instrumentation/micrometer/v1_5/LongTaskTimerHistogramTest.java
+++ instrumentation/micrometer/micrometer-1.5/library/src/test/java/io/opentelemetry/instrumentation/micrometer/v1_5/LongTaskTimerHistogramTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package io.opentelemetry.instrumentation.micrometer.v1_5;
 
 import static io.opentelemetry.sdk.testing.assertj.MetricAssertions.assertThat;
