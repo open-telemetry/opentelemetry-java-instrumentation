a565173ac482e04ea9e98a82a13dc5b6396d90d0
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2022-01-19 19:38:54 +0100

    fix servlet2 tests

--- instrumentation/servlet/servlet-2.2/javaagent/src/test/groovy/JettyServlet2Test.groovy
+++ instrumentation/servlet/servlet-2.2/javaagent/src/test/groovy/JettyServlet2Test.groovy
@@ -73,10 +73,10 @@ class JettyServlet2Test extends HttpServerTest<Server> implements AgentTestTrait
 
   @Override
   Set<AttributeKey<?>> httpAttributes(ServerEndpoint endpoint) {
-    def attributes = super.httpAttributes(endpoint)
-    attributes.remove(SemanticAttributes.HTTP_ROUTE)
-    attributes.addAll(SemanticAttributes.HTTP_SERVER_NAME)
-    attributes
+    [
+      SemanticAttributes.HTTP_SERVER_NAME,
+      SemanticAttributes.NET_TRANSPORT
+    ] as Set
   }
 
   @Override
