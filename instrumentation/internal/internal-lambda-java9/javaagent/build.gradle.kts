527ff646f0c54b3701fbcfe7d38936b7e3f11cd5
Anuraag Agrawal <anuraaga@gmail.com>
2022-01-19 08:21:22 +0000

    Drift

--- instrumentation/internal/internal-lambda-java9/javaagent/build.gradle.kts
+++ instrumentation/internal/internal-lambda-java9/javaagent/build.gradle.kts
@@ -3,6 +3,5 @@ plugins {
 }
 
 otelJava {
-  // DO NOT MERGE: For some reason below hack doesn't work on this module and produces no error message!!!
   minJavaVersionSupported.set(JavaVersion.VERSION_1_9)
 }
