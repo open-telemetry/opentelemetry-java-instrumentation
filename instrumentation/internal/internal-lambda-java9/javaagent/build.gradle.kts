ddba03ba6e0449029f9c04944ff983d268c838a3
Anuraag Agrawal <anuraaga@gmail.com>
2022-01-20 09:31:21 +0000

    Enable java lint

--- instrumentation/internal/internal-lambda-java9/javaagent/build.gradle.kts
+++ instrumentation/internal/internal-lambda-java9/javaagent/build.gradle.kts
@@ -3,5 +3,19 @@ plugins {
 }
 
 otelJava {
+  // Fails with no error message :crying_cat_face:
   minJavaVersionSupported.set(JavaVersion.VERSION_1_9)
+  // Works:
+  // minJavaVersionSupported.set(JavaVersion.VERSION_11)
+}
+
+tasks {
+  compileJava {
+    with(options) {
+      // Because this module targets Java 9, we trigger this compiler bug which was fixed but not
+      // backported to Java 9 compilation.
+      // https://bugs.openjdk.java.net/browse/JDK-8209058
+      compilerArgs.add("-Xlint:none")
+    }
+  }
 }
