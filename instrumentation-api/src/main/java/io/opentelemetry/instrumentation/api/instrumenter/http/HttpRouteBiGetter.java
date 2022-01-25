344c7f4efbce460b78524c031c62115992873d51
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2022-01-25 10:40:14 +0100

    HttpRouteBiGetter

--- instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/instrumenter/http/HttpRouteBiGetter.java
+++ instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/instrumenter/http/HttpRouteBiGetter.java
@@ -10,7 +10,7 @@ import javax.annotation.Nullable;
 
 /** An interface for getting the {@code http.route} attribute. */
 @FunctionalInterface
-public interface HttpRouteGetter2<T, U> {
+public interface HttpRouteBiGetter<T, U> {
 
   /**
    * Returns the {@code http.route} attribute extracted from {@code context}, {@code arg1} and
--- instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/instrumenter/http/HttpRouteHolder.java
+++ instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/instrumenter/http/HttpRouteHolder.java
@@ -69,18 +69,18 @@ public final class HttpRouteHolder {
    *
    * <p>If there is a server span in the context, and the context has been customized with a {@link
    * HttpRouteHolder}, then this method will update the route using the provided {@link
-   * HttpRouteGetter2} if and only if the last {@link HttpRouteSource} to update the route using
+   * HttpRouteBiGetter} if and only if the last {@link HttpRouteSource} to update the route using
    * this method has strictly lower priority than the provided {@link HttpRouteSource}, and the
-   * value returned from the {@link HttpRouteGetter2} is non-null.
+   * value returned from the {@link HttpRouteBiGetter} is non-null.
    *
    * <p>If there is a server span in the context, and the context has NOT been customized with a
    * {@code ServerSpanName}, then this method will update the route using the provided {@link
-   * HttpRouteGetter2} if the value returned from it is non-null.
+   * HttpRouteBiGetter} if the value returned from it is non-null.
    */
   public static <T, U> void updateHttpRoute(
       Context context,
       HttpRouteSource source,
-      HttpRouteGetter2<T, U> httpRouteGetter,
+      HttpRouteBiGetter<T, U> httpRouteGetter,
       T arg1,
       U arg2) {
     Span serverSpan = ServerSpan.fromContextOrNull(context);
@@ -139,7 +139,7 @@ public final class HttpRouteHolder {
     return httpRouteHolder == null ? null : httpRouteHolder.route;
   }
 
-  private static final class OneArgAdapter<T> implements HttpRouteGetter2<T, HttpRouteGetter<T>> {
+  private static final class OneArgAdapter<T> implements HttpRouteBiGetter<T, HttpRouteGetter<T>> {
 
     private static final OneArgAdapter<Object> INSTANCE = new OneArgAdapter<>();
 
