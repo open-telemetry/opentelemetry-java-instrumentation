091f904bab754219dd49233209d2fd1dc6c131fd
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-01-25 14:02:52 -0800

    Update version for patch release

--- version.gradle.kts
+++ version.gradle.kts
@@ -1,7 +1,7 @@
 allprojects {
   if (findProperty("otel.stable") != "true") {
-    version = "1.10.0-alpha"
+    version = "1.10.1-alpha"
   } else {
-    version = "1.10.0"
+    version = "1.10.1"
   }
 }
