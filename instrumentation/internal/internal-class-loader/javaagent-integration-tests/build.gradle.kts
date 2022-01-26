cfb21f2f05510c88e79bca9045880da7e689d54d
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-01-25 16:38:11 -0800

    Recover from duplicate class definition errors (#5185) (#5217)
    * Recover from duplicate class definition errors
    
    * fix hotspot8
    
    * Suppress dupicate class definiton errors only when helper classes were injected
    
    * exit define class context when there is an exception, update pseudocode in comment
    
    Co-authored-by: Lauri Tulmin <ltulmin@splunk.com>

--- instrumentation/internal/internal-class-loader/javaagent-integration-tests/build.gradle.kts
+++ instrumentation/internal/internal-class-loader/javaagent-integration-tests/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 }
 
 dependencies {
+  compileOnly("org.apache.commons:commons-lang3:3.12.0")
   testImplementation("org.apache.commons:commons-lang3:3.12.0")
 
   testInstrumentation(project(":instrumentation:internal:internal-class-loader:javaagent"))
