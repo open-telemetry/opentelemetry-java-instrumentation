aec599a8389a4ca0880e96e591d940f33513aaf4
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-01-25 20:45:07 -0800

    fix

--- instrumentation/vertx/vertx-http-client/vertx-http-client-4.0/javaagent/build.gradle.kts
+++ instrumentation/vertx/vertx-http-client/vertx-http-client-4.0/javaagent/build.gradle.kts
@@ -14,9 +14,8 @@ muzzle {
 dependencies {
   library("io.vertx:vertx-core:4.0.0")
 
-  // vertx-codegen and vertx-docgen dependencies are needed for Xlint's annotation checking
+  // vertx-codegen dependency is needed for Xlint's annotation checking
   library("io.vertx:vertx-codegen:4.0.0")
-  testLibrary("io.vertx:vertx-docgen:4.0.0")
 
   implementation(project(":instrumentation:vertx:vertx-http-client:vertx-http-client-common:javaagent"))
 
