c5745c7c5f31f03084ecebf60861f1ebef862f9a
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2022-01-27 15:54:53 +0100

    Remove a couple of TODOs from instrumentation-api metrics tests

--- instrumentation-api/src/test/java/io/opentelemetry/instrumentation/api/instrumenter/http/HttpServerMetricsTest.java
+++ instrumentation-api/src/test/java/io/opentelemetry/instrumentation/api/instrumenter/http/HttpServerMetricsTest.java
@@ -7,7 +7,6 @@ package io.opentelemetry.instrumentation.api.instrumenter.http;
 
 import static io.opentelemetry.sdk.testing.assertj.MetricAssertions.assertThat;
 import static io.opentelemetry.sdk.testing.assertj.OpenTelemetryAssertions.attributeEntry;
-import static org.awaitility.Awaitility.await;
 
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.trace.Span;
@@ -17,9 +16,8 @@ import io.opentelemetry.api.trace.TraceState;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.instrumentation.api.instrumenter.RequestListener;
 import io.opentelemetry.sdk.metrics.SdkMeterProvider;
-import io.opentelemetry.sdk.metrics.data.MetricData;
 import io.opentelemetry.sdk.testing.exporter.InMemoryMetricReader;
-import java.util.Collection;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.Test;
 
@@ -29,7 +27,10 @@ class HttpServerMetricsTest {
   void collectsMetrics() {
     InMemoryMetricReader metricReader = InMemoryMetricReader.create();
     SdkMeterProvider meterProvider =
-        SdkMeterProvider.builder().registerMetricReader(metricReader).build();
+        SdkMeterProvider.builder()
+            .registerMetricReader(metricReader)
+            .setMinimumCollectionInterval(Duration.ZERO)
+            .build();
 
     RequestListener listener = HttpServerMetrics.get().create(meterProvider.get("test"));
 
@@ -62,122 +63,97 @@ class HttpServerMetricsTest {
 
     Context context1 = listener.start(parent, requestAttributes, nanos(100));
 
-    // TODO(anuraaga): Remove await from this file after 1.8.0 hopefully fixes
-    // https://github.com/open-telemetry/opentelemetry-java/issues/3725
-    await()
-        .untilAsserted(
-            () -> {
-              Collection<MetricData> metrics = metricReader.collectAllMetrics();
-              assertThat(metrics).hasSize(1);
-              assertThat(metrics)
-                  .anySatisfy(
-                      metric ->
-                          assertThat(metric)
-                              .hasName("http.server.active_requests")
-                              .hasDescription(
-                                  "The number of concurrent HTTP requests that are currently in-flight")
-                              .hasUnit("requests")
-                              .hasLongSum()
-                              .points()
-                              .satisfiesExactly(
-                                  point -> {
-                                    assertThat(point)
-                                        .hasValue(1)
-                                        .attributes()
-                                        .containsOnly(
-                                            attributeEntry("http.host", "host"),
-                                            attributeEntry("http.method", "GET"),
-                                            attributeEntry("http.scheme", "https"));
-                                    assertThat(point).exemplars().hasSize(1);
-                                    assertThat(point.getExemplars().get(0))
-                                        .hasTraceId("ff01020304050600ff0a0b0c0d0e0f00")
-                                        .hasSpanId("090a0b0c0d0e0f00");
-                                  }));
-            });
+    assertThat(metricReader.collectAllMetrics())
+        .hasSize(1)
+        .anySatisfy(
+            metric ->
+                assertThat(metric)
+                    .hasName("http.server.active_requests")
+                    .hasDescription(
+                        "The number of concurrent HTTP requests that are currently in-flight")
+                    .hasUnit("requests")
+                    .hasLongSum()
+                    .points()
+                    .satisfiesExactly(
+                        point -> {
+                          assertThat(point)
+                              .hasValue(1)
+                              .attributes()
+                              .containsOnly(
+                                  attributeEntry("http.host", "host"),
+                                  attributeEntry("http.method", "GET"),
+                                  attributeEntry("http.scheme", "https"));
+                          assertThat(point).exemplars().hasSize(1);
+                          assertThat(point.getExemplars().get(0))
+                              .hasTraceId("ff01020304050600ff0a0b0c0d0e0f00")
+                              .hasSpanId("090a0b0c0d0e0f00");
+                        }));
 
     Context context2 = listener.start(Context.root(), requestAttributes, nanos(150));
 
-    await()
-        .untilAsserted(
-            () -> {
-              Collection<MetricData> metrics = metricReader.collectAllMetrics();
-              assertThat(metrics).hasSize(1);
-              assertThat(metrics)
-                  .anySatisfy(
-                      metric ->
-                          assertThat(metric)
-                              .hasName("http.server.active_requests")
-                              .hasLongSum()
-                              .points()
-                              .satisfiesExactly(point -> assertThat(point).hasValue(2)));
-            });
+    assertThat(metricReader.collectAllMetrics())
+        .hasSize(1)
+        .anySatisfy(
+            metric ->
+                assertThat(metric)
+                    .hasName("http.server.active_requests")
+                    .hasLongSum()
+                    .points()
+                    .satisfiesExactly(point -> assertThat(point).hasValue(2)));
 
     listener.end(context1, responseAttributes, nanos(250));
 
-    await()
-        .untilAsserted(
-            () -> {
-              Collection<MetricData> metrics = metricReader.collectAllMetrics();
-              assertThat(metrics).hasSize(2);
-              assertThat(metrics)
-                  .anySatisfy(
-                      metric ->
-                          assertThat(metric)
-                              .hasName("http.server.active_requests")
-                              .hasLongSum()
-                              .points()
-                              .satisfiesExactly(point -> assertThat(point).hasValue(1)));
-              assertThat(metrics)
-                  .anySatisfy(
-                      metric ->
-                          assertThat(metric)
-                              .hasName("http.server.duration")
-                              .hasUnit("ms")
-                              .hasDoubleHistogram()
-                              .points()
-                              .satisfiesExactly(
-                                  point -> {
-                                    assertThat(point)
-                                        .hasSum(150 /* millis */)
-                                        .attributes()
-                                        .containsOnly(
-                                            attributeEntry("http.scheme", "https"),
-                                            attributeEntry("http.host", "host"),
-                                            attributeEntry("http.method", "GET"),
-                                            attributeEntry("http.status_code", 200),
-                                            attributeEntry("http.flavor", "2.0"));
-                                    assertThat(point).exemplars().hasSize(1);
-                                    assertThat(point.getExemplars().get(0))
-                                        .hasTraceId("ff01020304050600ff0a0b0c0d0e0f00")
-                                        .hasSpanId("090a0b0c0d0e0f00");
-                                  }));
-            });
+    assertThat(metricReader.collectAllMetrics())
+        .hasSize(2)
+        .anySatisfy(
+            metric ->
+                assertThat(metric)
+                    .hasName("http.server.active_requests")
+                    .hasLongSum()
+                    .points()
+                    .satisfiesExactly(point -> assertThat(point).hasValue(1)))
+        .anySatisfy(
+            metric ->
+                assertThat(metric)
+                    .hasName("http.server.duration")
+                    .hasUnit("ms")
+                    .hasDoubleHistogram()
+                    .points()
+                    .satisfiesExactly(
+                        point -> {
+                          assertThat(point)
+                              .hasSum(150 /* millis */)
+                              .attributes()
+                              .containsOnly(
+                                  attributeEntry("http.scheme", "https"),
+                                  attributeEntry("http.host", "host"),
+                                  attributeEntry("http.method", "GET"),
+                                  attributeEntry("http.status_code", 200),
+                                  attributeEntry("http.flavor", "2.0"));
+                          assertThat(point).exemplars().hasSize(1);
+                          assertThat(point.getExemplars().get(0))
+                              .hasTraceId("ff01020304050600ff0a0b0c0d0e0f00")
+                              .hasSpanId("090a0b0c0d0e0f00");
+                        }));
 
     listener.end(context2, responseAttributes, nanos(300));
 
-    await()
-        .untilAsserted(
-            () -> {
-              Collection<MetricData> metrics = metricReader.collectAllMetrics();
-              assertThat(metrics).hasSize(2);
-              assertThat(metrics)
-                  .anySatisfy(
-                      metric ->
-                          assertThat(metric)
-                              .hasName("http.server.active_requests")
-                              .hasLongSum()
-                              .points()
-                              .satisfiesExactly(point -> assertThat(point).hasValue(0)));
-              assertThat(metrics)
-                  .anySatisfy(
-                      metric ->
-                          assertThat(metric)
-                              .hasName("http.server.duration")
-                              .hasDoubleHistogram()
-                              .points()
-                              .satisfiesExactly(
-                                  point -> assertThat(point).hasSum(300 /* millis */)));
-            });
+    assertThat(metricReader.collectAllMetrics())
+        .hasSize(2)
+        .anySatisfy(
+            metric ->
+                assertThat(metric)
+                    .hasName("http.server.active_requests")
+                    .hasLongSum()
+                    .points()
+                    .satisfiesExactly(point -> assertThat(point).hasValue(0)))
+        .anySatisfy(
+            metric ->
+                assertThat(metric)
+                    .hasName("http.server.duration")
+                    .hasDoubleHistogram()
+                    .points()
+                    .satisfiesExactly(point -> assertThat(point).hasSum(300 /* millis */)));
   }
 
   private static long nanos(int millis) {
