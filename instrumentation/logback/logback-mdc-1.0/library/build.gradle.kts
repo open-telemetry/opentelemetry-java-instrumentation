7df2b0e2df2c2ba5d37dec887d3407c1dea88059
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-01-28 08:21:53 -0800

    Don't run testLatestDeps on alpha/beta/rc versions (#5258)
    * Don't run testLatestDeps on alpha/beta/rc versions
    
    * More logback

--- instrumentation/logback/logback-mdc-1.0/library/build.gradle.kts
+++ instrumentation/logback/logback-mdc-1.0/library/build.gradle.kts
@@ -6,7 +6,4 @@ dependencies {
   library("ch.qos.logback:logback-classic:1.0.0")
 
   testImplementation(project(":instrumentation:logback:logback-mdc-1.0:testing"))
-
-  // 1.3+ contains breaking changes, check back after it stabilizes.
-  latestDepTestLibrary("ch.qos.logback:logback-classic:1.2.+")
 }
