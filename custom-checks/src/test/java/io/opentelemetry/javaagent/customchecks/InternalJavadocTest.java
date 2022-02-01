7658033d565a89b27ec3cf7751c3cd52074b4fef
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-01-31 20:03:54 -0800

    feedback

--- custom-checks/src/test/java/io/opentelemetry/javaagent/customchecks/InternalJavadocTest.java
+++ custom-checks/src/test/java/io/opentelemetry/javaagent/customchecks/InternalJavadocTest.java
@@ -8,15 +8,15 @@ package io.opentelemetry.javaagent.customchecks;
 import com.google.errorprone.CompilationTestHelper;
 import org.junit.jupiter.api.Test;
 
-public class InternalJavadocTest {
+class InternalJavadocTest {
 
   @Test
-  public void test() {
-    test("internal/InternalJavadocPositiveCases.java");
-    test("internal/InternalJavadocNegativeCases.java");
+  void test() {
+    doTest("internal/InternalJavadocPositiveCases.java");
+    doTest("internal/InternalJavadocNegativeCases.java");
   }
 
-  private static void test(String path) {
+  private static void doTest(String path) {
     CompilationTestHelper.newInstance(InternalJavadoc.class, InternalJavadocTest.class)
         .addSourceFile(path)
         .doTest();
