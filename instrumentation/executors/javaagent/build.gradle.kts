9aa2849497c71cddb5ac9188f40a631a4e643ea4
Lauri Tulmin <tulmin@gmail.com>
2022-02-02 11:26:01 +0200

    Update instrumentation/executors/javaagent/build.gradle.kts

--- instrumentation/executors/javaagent/build.gradle.kts
+++ instrumentation/executors/javaagent/build.gradle.kts
@@ -12,6 +12,6 @@ tasks.withType<Test>().configureEach {
   jvmArgs("-Dotel.instrumentation.executors.include=ExecutorInstrumentationTest\$CustomThreadPoolExecutor")
   jvmArgs("-Djava.awt.headless=true")
   // ExecutorInstrumentationTest tess internal JDK class instrumentation
-  jvmArgs("--add-opens", "java.base/java.util.concurrent=ALL-UNNAMED")
+  jvmArgs("--add-opens=java.base/java.util.concurrent=ALL-UNNAMED")
   jvmArgs("-XX:+IgnoreUnrecognizedVMOptions")
 }
