a3ce5d5e2a76a2b232ca2c2ba5f367f8b6a35a36
Nikita Salnikov-Tarnovski <gnikem@gmail.com>
2022-02-03 08:22:49 +0200

    Use "--add-opens" to allow test to run on java 16+ (#5290)
    * Use "--add-opens" to allow test to run on java 16+
    
    * Update instrumentation/executors/javaagent/build.gradle.kts
    
    Co-authored-by: Lauri Tulmin <tulmin@gmail.com>
    
    * Update instrumentation/executors/javaagent/build.gradle.kts
    
    Co-authored-by: Anuraag Agrawal <anuraaga@gmail.com>
    Co-authored-by: Lauri Tulmin <tulmin@gmail.com>

--- instrumentation/executors/javaagent/build.gradle.kts
+++ instrumentation/executors/javaagent/build.gradle.kts
@@ -11,4 +11,7 @@ muzzle {
 tasks.withType<Test>().configureEach {
   jvmArgs("-Dotel.instrumentation.executors.include=ExecutorInstrumentationTest\$CustomThreadPoolExecutor")
   jvmArgs("-Djava.awt.headless=true")
+  // ExecutorInstrumentationTest tess internal JDK class instrumentation
+  jvmArgs("--add-opens=java.base/java.util.concurrent=ALL-UNNAMED")
+  jvmArgs("-XX:+IgnoreUnrecognizedVMOptions")
 }
