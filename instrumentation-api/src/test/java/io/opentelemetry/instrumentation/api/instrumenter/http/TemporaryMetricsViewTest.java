4b050523548e97305285bfaec7454ea6696137b9
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2022-02-08 08:45:45 +0100

    Make HttpClientMetrics report low cardinality metrics

--- instrumentation-api/src/test/java/io/opentelemetry/instrumentation/api/instrumenter/http/TemporaryMetricsViewTest.java
+++ instrumentation-api/src/test/java/io/opentelemetry/instrumentation/api/instrumenter/http/TemporaryMetricsViewTest.java
@@ -15,37 +15,10 @@ import io.opentelemetry.sdk.testing.assertj.OpenTelemetryAssertions;
 import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
 import org.junit.jupiter.api.Test;
 
-public class TemporaryMetricsViewTest {
+class TemporaryMetricsViewTest {
 
   @Test
-  public void shouldApplyClientDurationView_urlIdentity() {
-    Attributes startAttributes =
-        Attributes.builder()
-            .put(SemanticAttributes.HTTP_URL, "https://somehost/high/cardinality/12345")
-            .put(SemanticAttributes.HTTP_SCHEME, "https")
-            .put(SemanticAttributes.HTTP_METHOD, "GET")
-            .put(SemanticAttributes.HTTP_HOST, "somehost")
-            .put(SemanticAttributes.HTTP_TARGET, "/high/cardinality/12345")
-            .build();
-
-    Attributes endAttributes =
-        Attributes.builder()
-            .put(SemanticAttributes.HTTP_STATUS_CODE, 500)
-            .put(SemanticAttributes.NET_PEER_NAME, "somehost2")
-            .put(SemanticAttributes.NET_PEER_IP, "127.0.0.1")
-            .put(SemanticAttributes.NET_PEER_PORT, 443)
-            .build();
-
-    OpenTelemetryAssertions.assertThat(applyClientDurationView(startAttributes, endAttributes))
-        .containsOnly(
-            attributeEntry(
-                SemanticAttributes.HTTP_URL.getKey(), "https://somehost/high/cardinality/12345"),
-            attributeEntry(SemanticAttributes.HTTP_METHOD.getKey(), "GET"),
-            attributeEntry(SemanticAttributes.HTTP_STATUS_CODE.getKey(), 500));
-  }
-
-  @Test
-  public void shouldApplyClientDurationView_urlWithQuery() {
+  void shouldApplyClientDurationView() {
     Attributes startAttributes =
         Attributes.builder()
             .put(
@@ -67,14 +40,14 @@ public class TemporaryMetricsViewTest {
 
     OpenTelemetryAssertions.assertThat(applyClientDurationView(startAttributes, endAttributes))
         .containsOnly(
-            attributeEntry(
-                SemanticAttributes.HTTP_URL.getKey(), "https://somehost/high/cardinality/12345"),
+            attributeEntry(SemanticAttributes.NET_PEER_NAME.getKey(), "somehost2"),
+            attributeEntry(SemanticAttributes.NET_PEER_PORT.getKey(), 443),
             attributeEntry(SemanticAttributes.HTTP_METHOD.getKey(), "GET"),
             attributeEntry(SemanticAttributes.HTTP_STATUS_CODE.getKey(), 500));
   }
 
   @Test
-  public void shouldApplyServerDurationView_withRoute() {
+  void shouldApplyServerDurationView() {
     Attributes startAttributes =
         Attributes.builder()
             .put(SemanticAttributes.HTTP_METHOD, "GET")
@@ -110,7 +83,7 @@ public class TemporaryMetricsViewTest {
   }
 
   @Test
-  public void shouldApplyActiveRequestsView() {
+  void shouldApplyActiveRequestsView() {
     Attributes attributes =
         Attributes.builder()
             .put(SemanticAttributes.HTTP_METHOD, "GET")
