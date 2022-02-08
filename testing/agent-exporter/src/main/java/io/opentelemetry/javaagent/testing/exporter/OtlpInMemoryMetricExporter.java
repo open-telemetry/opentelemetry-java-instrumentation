34bb257c4c37f6469fb098f19e9ed4e4e299a422
Anuraag Agrawal <anuraaga@gmail.com>
2022-02-08 03:30:56 +0000

    Cleanup

--- testing/agent-exporter/src/main/java/io/opentelemetry/javaagent/testing/exporter/OtlpInMemoryMetricExporter.java
+++ testing/agent-exporter/src/main/java/io/opentelemetry/javaagent/testing/exporter/OtlpInMemoryMetricExporter.java
@@ -6,36 +6,16 @@
 package io.opentelemetry.javaagent.testing.exporter;
 
 import io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter;
-import io.opentelemetry.proto.collector.metrics.v1.ExportMetricsServiceRequest;
 import io.opentelemetry.sdk.common.CompletableResultCode;
 import io.opentelemetry.sdk.metrics.data.MetricData;
 import io.opentelemetry.sdk.metrics.export.MetricExporter;
 import java.util.Collection;
-import java.util.List;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 class OtlpInMemoryMetricExporter implements MetricExporter {
 
   private static final Logger logger = LoggerFactory.getLogger(OtlpInMemoryMetricExporter.class);
-
-  private final BlockingQueue<ExportMetricsServiceRequest> collectedRequests =
-      new LinkedBlockingQueue<>();
-
-  List<byte[]> getCollectedExportRequests() {
-    return collectedRequests.stream()
-        .map(ExportMetricsServiceRequest::toByteArray)
-        .collect(Collectors.toList());
-  }
-
-  void reset() {
-    delegate.flush().join(1, TimeUnit.SECONDS);
-    collectedRequests.clear();
-  }
 
   private final OtlpInMemoryCollector collector;
   private final MetricExporter delegate;
