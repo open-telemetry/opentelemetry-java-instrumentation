3c83ad6d60778d2af0d7ab956c5a27ad56406ec0
Lauri Tulmin <ltulmin@splunk.com>
2022-02-14 16:42:11 +0200

    Exclude broken oshi release

--- instrumentation/oshi/javaagent/build.gradle.kts
+++ instrumentation/oshi/javaagent/build.gradle.kts
@@ -7,6 +7,8 @@ muzzle {
     group.set("com.github.oshi")
     module.set("oshi-core")
     versions.set("[5.3.1,)")
+    // Could not parse POM https://repo.maven.apache.org/maven2/com/github/oshi/oshi-core/6.1.1/oshi-core-6.1.1.pom
+    skip("6.1.1")
   }
 }
 
@@ -18,6 +20,9 @@ dependencies {
   library("com.github.oshi:oshi-core:5.3.1")
 
   testImplementation(project(":instrumentation:oshi:testing"))
+
+  // Could not parse POM https://repo.maven.apache.org/maven2/com/github/oshi/oshi-core/6.1.1/oshi-core-6.1.1.pom
+  latestDepTestLibrary("com.github.oshi:oshi-core:6.1.0")
 }
 
 tasks {
--- instrumentation/oshi/library/build.gradle.kts
+++ instrumentation/oshi/library/build.gradle.kts
@@ -6,4 +6,7 @@ dependencies {
   library("com.github.oshi:oshi-core:5.3.1")
 
   testImplementation(project(":instrumentation:oshi:testing"))
+
+  // Could not parse POM https://repo.maven.apache.org/maven2/com/github/oshi/oshi-core/6.1.1/oshi-core-6.1.1.pom
+  latestDepTestLibrary("com.github.oshi:oshi-core:6.1.0")
 }
