9584708eb591c02a3246b5d3c33ebd198142f478
Lauri Tulmin <ltulmin@splunk.com>
2022-02-15 18:05:53 +0200

    Remove oshi latest dep test restriction

--- instrumentation/oshi/library/build.gradle.kts
+++ instrumentation/oshi/library/build.gradle.kts
@@ -3,11 +3,7 @@ plugins {
 }
 
 dependencies {
-  compileOnly("com.github.oshi:oshi-core:5.3.1")
-  testImplementation("com.github.oshi:oshi-core:5.3.1")
+  library("com.github.oshi:oshi-core:5.3.1")
 
   testImplementation(project(":instrumentation:oshi:testing"))
-
-  // Could not parse POM https://repo.maven.apache.org/maven2/com/github/oshi/oshi-core/6.1.1/oshi-core-6.1.1.pom
-  latestDepTestLibrary("com.github.oshi:oshi-core:6.1.0")
 }
