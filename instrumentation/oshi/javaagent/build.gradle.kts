3b81bc49e804fd65fa9e35b223596a64f6763526
Lauri Tulmin <ltulmin@splunk.com>
2022-02-15 21:11:02 +0200

    Remove oshi latest dep test restriction (#5383)

--- instrumentation/oshi/javaagent/build.gradle.kts
+++ instrumentation/oshi/javaagent/build.gradle.kts
@@ -17,13 +17,9 @@ dependencies {
 
   compileOnly("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure")
 
-  compileOnly("com.github.oshi:oshi-core:5.3.1")
-  testImplementation("com.github.oshi:oshi-core:5.3.1")
+  library("com.github.oshi:oshi-core:5.3.1")
 
   testImplementation(project(":instrumentation:oshi:testing"))
-
-  // Could not parse POM https://repo.maven.apache.org/maven2/com/github/oshi/oshi-core/6.1.1/oshi-core-6.1.1.pom
-  latestDepTestLibrary("com.github.oshi:oshi-core:6.1.0")
 }
 
 tasks {
