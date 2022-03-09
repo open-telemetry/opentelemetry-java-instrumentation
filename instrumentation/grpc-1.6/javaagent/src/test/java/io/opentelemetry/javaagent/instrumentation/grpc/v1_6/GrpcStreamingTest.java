9e5fdcebd59a23b00de3274a6b45884584827e77
Anuraag Agrawal <anuraaga@gmail.com>
2022-03-09 14:37:59 +0900

    Migrate gRPC tests to Java (#5521)
    * Migrate gRPC tests to Java
    
    * check
    
    * clean

---
+++ instrumentation/grpc-1.6/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/grpc/v1_6/GrpcStreamingTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.grpc.v1_6;
+
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.ServerBuilder;
+import io.opentelemetry.instrumentation.grpc.v1_6.AbstractGrpcStreamingTest;
+import io.opentelemetry.instrumentation.testing.junit.AgentInstrumentationExtension;
+import io.opentelemetry.instrumentation.testing.junit.InstrumentationExtension;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+class GrpcStreamingTest extends AbstractGrpcStreamingTest {
+
+  @RegisterExtension
+  static final InstrumentationExtension testing = AgentInstrumentationExtension.create();
+
+  @Override
+  protected ServerBuilder<?> configureServer(ServerBuilder<?> server) {
+    return server;
+  }
+
+  @Override
+  protected ManagedChannelBuilder<?> configureClient(ManagedChannelBuilder<?> client) {
+    return client;
+  }
+
+  @Override
+  protected InstrumentationExtension testing() {
+    return testing;
+  }
+}
---
+++ instrumentation/grpc-1.6/library/src/test/java/io/opentelemetry/instrumentation/grpc/v1_6/GrpcTest.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.instrumentation.grpc.v1_6;
+
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.ServerBuilder;
+import io.opentelemetry.instrumentation.testing.junit.InstrumentationExtension;
+import io.opentelemetry.instrumentation.testing.junit.LibraryInstrumentationExtension;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+class GrpcTest extends AbstractGrpcTest {
+
+  @RegisterExtension
+  static final InstrumentationExtension testing = LibraryInstrumentationExtension.create();
+
+  @Override
+  protected ServerBuilder<?> configureServer(ServerBuilder<?> server) {
+    return server.intercept(GrpcTracing.create(testing.getOpenTelemetry()).newServerInterceptor());
+  }
+
+  @Override
+  protected ManagedChannelBuilder<?> configureClient(ManagedChannelBuilder<?> client) {
+    return client.intercept(GrpcTracing.create(testing.getOpenTelemetry()).newClientInterceptor());
+  }
+
+  @Override
+  protected InstrumentationExtension testing() {
+    return testing;
+  }
+}
