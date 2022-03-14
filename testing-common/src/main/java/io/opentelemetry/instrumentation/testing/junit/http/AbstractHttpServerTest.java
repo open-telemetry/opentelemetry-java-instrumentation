abf75d74ff2835034f3725803e42b8f4baf46eda
Anuraag Agrawal <anuraaga@gmail.com>
2022-03-14 08:23:54 +0000

    Migrate ktor tests to kotlin

--- testing-common/src/main/java/io/opentelemetry/instrumentation/testing/junit/http/AbstractHttpServerTest.java
+++ testing-common/src/main/java/io/opentelemetry/instrumentation/testing/junit/http/AbstractHttpServerTest.java
@@ -511,8 +511,13 @@ public abstract class AbstractHttpServerTest<SERVER> {
       SpanDataAssert span, String method, ServerEndpoint endpoint) {
     Set<AttributeKey<?>> httpAttributes = options.httpAttributes.apply(endpoint);
 
-    span.hasName(options.expectedServerSpanNameMapper.apply(endpoint, method))
-        .hasKind(SpanKind.SERVER);
+    String expectedRoute = options.expectedHttpRoute.apply(endpoint);
+    String name =
+        expectedRoute != null
+            ? expectedRoute
+            : options.expectedServerSpanNameMapper.apply(endpoint, method);
+
+    span.hasName(name).hasKind(SpanKind.SERVER);
     if (endpoint.status >= 500) {
       span.hasStatus(StatusData.error());
     }
@@ -593,7 +598,6 @@ public abstract class AbstractHttpServerTest<SERVER> {
                     entry -> assertThat(entry).isInstanceOf(String.class));
           }
           if (httpAttributes.contains(SemanticAttributes.HTTP_ROUTE)) {
-            String expectedRoute = options.expectedHttpRoute.apply(endpoint);
             if (expectedRoute != null) {
               assertThat(attrs).containsEntry(SemanticAttributes.HTTP_ROUTE, expectedRoute);
             }
