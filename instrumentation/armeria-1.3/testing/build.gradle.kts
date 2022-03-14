75f796f287e0c6188b646a37086b6ba91675de57
Anuraag Agrawal <anuraaga@gmail.com>
2022-03-14 07:53:28 +0000

    Migrate Armeria server test to Java

--- instrumentation/armeria-1.3/testing/build.gradle.kts
+++ instrumentation/armeria-1.3/testing/build.gradle.kts
@@ -7,10 +7,4 @@ dependencies {
 
   api("com.linecorp.armeria:armeria:1.3.0")
   api("com.linecorp.armeria:armeria-junit4:1.3.0")
-
-  implementation("com.google.guava:guava")
-
-  implementation("org.apache.groovy:groovy")
-  implementation("io.opentelemetry:opentelemetry-api")
-  implementation("org.spockframework:spock-core")
 }
