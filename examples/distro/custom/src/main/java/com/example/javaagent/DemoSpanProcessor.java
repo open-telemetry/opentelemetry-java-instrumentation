309618845c11445468c750c3a6f79f2b43772f07
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2022-03-15 01:44:14 +0100

    Update distro and extension examples to use AutoConfigurationCustomiz… (#5574)
    * Update distro and extension examples to use AutoConfigurationCustomizerProvider
    
    * Rename the SPI file too

--- examples/distro/custom/src/main/java/com/example/javaagent/DemoSpanProcessor.java
+++ examples/distro/custom/src/main/java/com/example/javaagent/DemoSpanProcessor.java
@@ -16,7 +16,7 @@ import io.opentelemetry.sdk.trace.SpanProcessor;
  * href="https://github.com/open-telemetry/opentelemetry-specification/blob/master/specification/trace/sdk.md#span-processor">
  * OpenTelemetry Specification</a> for more information about {@link SpanProcessor}.
  *
- * @see DemoSdkTracerProviderConfigurer
+ * @see DemoAutoConfigurationCustomizerProvider
  */
 public class DemoSpanProcessor implements SpanProcessor {
   @Override
--- examples/extension/src/main/java/com/example/javaagent/DemoAutoConfigurationCustomizerProvider.java
+++ examples/extension/src/main/java/com/example/javaagent/DemoAutoConfigurationCustomizerProvider.java
@@ -5,26 +5,37 @@
 
 package com.example.javaagent;
 
+import com.google.auto.service.AutoService;
+import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizer;
+import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvider;
 import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
-import io.opentelemetry.sdk.autoconfigure.spi.traces.SdkTracerProviderConfigurer;
 import io.opentelemetry.sdk.trace.SdkTracerProviderBuilder;
 import io.opentelemetry.sdk.trace.SpanLimits;
 import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
 
 /**
  * This is one of the main entry points for Instrumentation Agent's customizations. It allows
- * configuring {@link SdkTracerProviderBuilder}. See the {@link
- * #configure(SdkTracerProviderBuilder)} method below.
+ * configuring the {@link AutoConfigurationCustomizer}. See the {@link
+ * #customize(AutoConfigurationCustomizer)} method below.
  *
  * <p>Also see https://github.com/open-telemetry/opentelemetry-java/issues/2022
  *
- * @see SdkTracerProviderConfigurer
+ * @see AutoConfigurationCustomizerProvider
  * @see DemoPropagatorProvider
  */
-public class DemoSdkTracerProviderConfigurer implements SdkTracerProviderConfigurer {
+@AutoService(AutoConfigurationCustomizerProvider.class)
+public class DemoAutoConfigurationCustomizerProvider
+    implements AutoConfigurationCustomizerProvider {
+
   @Override
-  public void configure(SdkTracerProviderBuilder tracerProvider, ConfigProperties config) {
-    tracerProvider
+  public void customize(AutoConfigurationCustomizer autoConfiguration) {
+    autoConfiguration.addTracerProviderCustomizer(this::configureSdkTracerProvider);
+  }
+
+  private SdkTracerProviderBuilder configureSdkTracerProvider(
+      SdkTracerProviderBuilder tracerProvider, ConfigProperties config) {
+
+    return tracerProvider
         .setIdGenerator(new DemoIdGenerator())
         .setSpanLimits(SpanLimits.builder().setMaxNumberOfAttributes(1024).build())
         .setSampler(new DemoSampler())
