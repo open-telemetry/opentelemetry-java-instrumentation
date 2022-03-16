3f87c2de10695c9f8cf892ade4a94f51a5caae43
Lauri Tulmin <ltulmin@splunk.com>
2022-03-16 10:01:22 +0200

    Elasticsearch rest client low cardinality span name (#5584)

--- instrumentation/elasticsearch/elasticsearch-rest-5.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/elasticsearch/rest/v5_0/RestClientInstrumentation.java
+++ instrumentation/elasticsearch/elasticsearch-rest-5.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/elasticsearch/rest/v5_0/RestClientInstrumentation.java
@@ -17,6 +17,7 @@ import io.opentelemetry.context.Context;
 import io.opentelemetry.context.Scope;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeTransformer;
+import io.opentelemetry.javaagent.instrumentation.elasticsearch.rest.ElasticsearchRestRequest;
 import io.opentelemetry.javaagent.instrumentation.elasticsearch.rest.RestResponseListener;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.description.type.TypeDescription;
@@ -48,13 +49,13 @@ public class RestClientInstrumentation implements TypeInstrumentation {
     public static void onEnter(
         @Advice.Argument(0) String method,
         @Advice.Argument(1) String endpoint,
-        @Advice.Local("otelRequest") String request,
+        @Advice.Local("otelRequest") ElasticsearchRestRequest request,
         @Advice.Local("otelContext") Context context,
         @Advice.Local("otelScope") Scope scope,
         @Advice.Argument(value = 5, readOnly = false) ResponseListener responseListener) {
 
       Context parentContext = currentContext();
-      request = method + " " + endpoint;
+      request = ElasticsearchRestRequest.create(method, endpoint);
       if (!instrumenter().shouldStart(parentContext, request)) {
         return;
       }
@@ -70,7 +71,7 @@ public class RestClientInstrumentation implements TypeInstrumentation {
     @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
     public static void stopSpan(
         @Advice.Thrown Throwable throwable,
-        @Advice.Local("otelRequest") String request,
+        @Advice.Local("otelRequest") ElasticsearchRestRequest request,
         @Advice.Local("otelContext") Context context,
         @Advice.Local("otelScope") Scope scope) {
 
--- instrumentation/elasticsearch/elasticsearch-rest-6.4/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/elasticsearch/rest/v6_4/ElasticsearchRest6Singletons.java
+++ instrumentation/elasticsearch/elasticsearch-rest-6.4/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/elasticsearch/rest/v6_4/ElasticsearchRest6Singletons.java
@@ -7,14 +7,15 @@ package io.opentelemetry.javaagent.instrumentation.elasticsearch.rest.v6_4;
 
 import io.opentelemetry.instrumentation.api.instrumenter.Instrumenter;
 import io.opentelemetry.javaagent.instrumentation.elasticsearch.rest.ElasticsearchRestInstrumenterFactory;
+import io.opentelemetry.javaagent.instrumentation.elasticsearch.rest.ElasticsearchRestRequest;
 import org.elasticsearch.client.Response;
 
 public final class ElasticsearchRest6Singletons {
 
-  private static final Instrumenter<String, Response> INSTRUMENTER =
+  private static final Instrumenter<ElasticsearchRestRequest, Response> INSTRUMENTER =
       ElasticsearchRestInstrumenterFactory.create("io.opentelemetry.elasticsearch-rest-6.4");
 
-  public static Instrumenter<String, Response> instrumenter() {
+  public static Instrumenter<ElasticsearchRestRequest, Response> instrumenter() {
     return INSTRUMENTER;
   }
 
