411b8c8ee24dbfff775c2338546862bfc5e16639
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2022-04-01 15:53:08 +0200

    spotless

--- javaagent-tooling/src/main/java/io/opentelemetry/javaagent/tooling/config/AgentConfig.java
+++ javaagent-tooling/src/main/java/io/opentelemetry/javaagent/tooling/config/AgentConfig.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package io.opentelemetry.javaagent.tooling.config;
 
 import io.opentelemetry.instrumentation.api.config.Config;
