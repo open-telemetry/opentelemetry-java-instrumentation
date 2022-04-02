c680f00b0998ceab3f5ea78b6e998c8b3c2b5d88
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2022-04-03 04:13:14 +0530

    Uses abstract apache test class and moves boilerplate

---
+++ instrumentation/apache-httpclient/apache-httpclient-5.0/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/apachehttpclient/AbstractApacheHttpClientTest.java
@@ -0,0 +1,106 @@
+package io.opentelemetry.javaagent.instrumentation.apachehttpclient;
+
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.instrumentation.testing.junit.http.AbstractHttpClientTest;
+import io.opentelemetry.instrumentation.testing.junit.http.HttpClientTestOptions;
+import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
+import java.net.URI;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import org.apache.hc.core5.http.HttpHost;
+import org.apache.hc.core5.http.HttpRequest;
+import org.apache.hc.core5.http.HttpResponse;
+import org.apache.hc.core5.http.message.BasicHeader;
+import org.apache.hc.core5.http.protocol.BasicHttpContext;
+import org.apache.hc.core5.http.protocol.HttpContext;
+
+abstract class AbstractApacheHttpClientTest<T extends HttpRequest>
+    extends AbstractHttpClientTest<T> {
+
+  @Override
+  protected String userAgent() {
+    return "apachehttpclient";
+  }
+
+  @Override
+  protected void configure(HttpClientTestOptions options) {
+    options.setUserAgent(userAgent());
+    options.enableTestReadTimeout();
+    options.setHttpAttributes(this::getHttpAttributes);
+  }
+
+  protected Set<AttributeKey<?>> getHttpAttributes(URI endpoint) {
+    Set<AttributeKey<?>> attributes = new HashSet<>();
+    attributes.add(SemanticAttributes.NET_PEER_NAME);
+    attributes.add(SemanticAttributes.NET_PEER_PORT);
+    attributes.add(SemanticAttributes.HTTP_URL);
+    attributes.add(SemanticAttributes.HTTP_METHOD);
+    if (endpoint.toString().contains("/success")) {
+      attributes.add(SemanticAttributes.HTTP_FLAVOR);
+    }
+    attributes.add(SemanticAttributes.HTTP_USER_AGENT);
+    return attributes;
+  }
+
+  @Override
+  protected T buildRequest(String method, URI uri, Map<String, String> headers) {
+    T request = createRequest(method, uri);
+    request.addHeader("user-agent", userAgent());
+    headers.forEach((key, value) -> request.setHeader(new BasicHeader(key, value)));
+    return request;
+  }
+
+  @Override
+  protected int sendRequest(T request, String method, URI uri, Map<String, String> headers)
+      throws Exception {
+    return getResponseCode(executeRequest(request, uri));
+  }
+
+  @Override
+  protected void sendRequestWithCallback(
+      T request, String method, URI uri, Map<String, String> headers, RequestResult requestResult) {
+    try {
+      executeRequestWithCallback(request, uri, requestResult);
+    } catch (Throwable throwable) {
+      requestResult.complete(throwable);
+    }
+  }
+
+  protected HttpHost getHost(URI uri) {
+    return new HttpHost(uri.getScheme(), uri.getHost(), uri.getPort());
+  }
+
+  protected HttpContext getContext() {
+    return new BasicHttpContext();
+  }
+
+  protected static String fullPathFromURI(URI uri) {
+    StringBuilder builder = new StringBuilder();
+    if (uri.getPath() != null) {
+      builder.append(uri.getPath());
+    }
+
+    if (uri.getQuery() != null) {
+      builder.append('?');
+      builder.append(uri.getQuery());
+    }
+
+    if (uri.getFragment() != null) {
+      builder.append('#');
+      builder.append(uri.getFragment());
+    }
+    return builder.toString();
+  }
+
+  abstract T createRequest(String method, URI uri);
+
+  abstract HttpResponse executeRequest(T request, URI uri) throws Exception;
+
+  abstract void executeRequestWithCallback(T request, URI uri, RequestResult requestResult)
+      throws Exception;
+
+  private static int getResponseCode(HttpResponse response) {
+    return response.getCode();
+  }
+}
--- instrumentation/apache-httpclient/apache-httpclient-5.0/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/apachehttpclient/ApacheHttpAsyncClientTest.java
+++ instrumentation/apache-httpclient/apache-httpclient-5.0/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/apachehttpclient/ApacheHttpAsyncClientTest.java
@@ -5,26 +5,19 @@
 
 package io.opentelemetry.javaagent.instrumentation.apachehttpclient;
 
-import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.instrumentation.testing.junit.InstrumentationExtension;
 import io.opentelemetry.instrumentation.testing.junit.http.AbstractHttpClientTest;
 import io.opentelemetry.instrumentation.testing.junit.http.HttpClientInstrumentationExtension;
 import io.opentelemetry.instrumentation.testing.junit.http.HttpClientTestOptions;
-import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
 import java.net.URI;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.CancellationException;
-import java.util.concurrent.ExecutionException;
 import org.apache.hc.client5.http.async.methods.SimpleHttpRequest;
 import org.apache.hc.client5.http.async.methods.SimpleHttpResponse;
 import org.apache.hc.client5.http.config.RequestConfig;
 import org.apache.hc.client5.http.impl.async.CloseableHttpAsyncClient;
 import org.apache.hc.client5.http.impl.async.HttpAsyncClients;
 import org.apache.hc.core5.concurrent.FutureCallback;
-import org.apache.hc.core5.http.HttpHost;
-import org.apache.hc.core5.http.message.BasicHeader;
+import org.apache.hc.core5.http.HttpResponse;
 import org.apache.hc.core5.http2.HttpVersionPolicy;
 import org.apache.hc.core5.io.CloseMode;
 import org.apache.hc.core5.util.Timeout;
@@ -36,27 +29,21 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ApacheHttpAsyncClientTest {
-
   @RegisterExtension
   static final InstrumentationExtension testing = HttpClientInstrumentationExtension.forAgent();
 
-  // TODO(anuraaga): AbstractHttpClientTest should provide timeout values statically
-  private final RequestConfig requestConfig =
-      RequestConfig.custom().setConnectTimeout(Timeout.ofSeconds(5)).build();
+  private final CloseableHttpAsyncClient client;
+  private final CloseableHttpAsyncClient clientWithReadTimeout;
 
-  private final RequestConfig requestWithReadTimeoutConfig =
-      RequestConfig.copy(requestConfig).setResponseTimeout(Timeout.ofSeconds(2)).build();
-
-  private final CloseableHttpAsyncClient client =
-      HttpAsyncClients.custom()
-          .setDefaultRequestConfig(requestConfig)
-          .setVersionPolicy(HttpVersionPolicy.FORCE_HTTP_1)
-          .build();
-  private final CloseableHttpAsyncClient clientWithReadTimeout =
-      HttpAsyncClients.custom()
-          .setDefaultRequestConfig(requestWithReadTimeoutConfig)
-          .setVersionPolicy(HttpVersionPolicy.FORCE_HTTP_1)
-          .build();
+  public ApacheHttpAsyncClientTest() {
+    // TODO(anuraaga): AbstractHttpClientTest should provide timeout values statically
+    RequestConfig requestConfig =
+        RequestConfig.custom().setConnectTimeout(Timeout.ofSeconds(5)).build();
+    client = createClient(requestConfig);
+    RequestConfig requestWithReadTimeoutConfig =
+        RequestConfig.copy(requestConfig).setResponseTimeout(Timeout.ofSeconds(2)).build();
+    clientWithReadTimeout = createClient(requestWithReadTimeoutConfig);
+  }
 
   @BeforeAll
   void setUp() {
@@ -70,7 +57,14 @@ class ApacheHttpAsyncClientTest {
     clientWithReadTimeout.close(CloseMode.GRACEFUL);
   }
 
-  CloseableHttpAsyncClient getClient(URI uri) {
+  private static CloseableHttpAsyncClient createClient(RequestConfig requestConfig) {
+    return HttpAsyncClients.custom()
+        .setDefaultRequestConfig(requestConfig)
+        .setVersionPolicy(HttpVersionPolicy.FORCE_HTTP_1)
+        .build();
+  }
+
+  private CloseableHttpAsyncClient getClient(URI uri) {
     if (uri.toString().contains("/read-timeout")) {
       return clientWithReadTimeout;
     }
@@ -78,108 +72,45 @@ class ApacheHttpAsyncClientTest {
   }
 
   @Nested
-  class ApacheClientUriRequestTest extends AbstractHttpClientTest<SimpleHttpRequest> {
-
+  class ApacheClientUriRequestTest extends AbstractTest {
     @Override
-    protected SimpleHttpRequest buildRequest(String method, URI uri, Map<String, String> headers) {
-      return configureRequest(new SimpleHttpRequest(method, uri), headers);
-    }
-
-    @Override
-    protected int sendRequest(
-        SimpleHttpRequest request, String method, URI uri, Map<String, String> headers)
-        throws Exception {
-      return getResponseCode(getClient(uri).execute(request, null).get());
-    }
-
-    @Override
-    protected void sendRequestWithCallback(
-        SimpleHttpRequest request,
-        String method,
-        URI uri,
-        Map<String, String> headers,
-        RequestResult requestResult) {
-      getClient(uri).execute(request, new ResponseCallback(requestResult));
-    }
-
-    @Override
-    protected void configure(HttpClientTestOptions options) {
-      configureTest(options);
+    SimpleHttpRequest createRequest(String method, URI uri) {
+      return new SimpleHttpRequest(method, uri);
     }
   }
 
   @Nested
-  class ApacheClientHostRequestTest extends AbstractHttpClientTest<SimpleHttpRequest> {
-
+  class ApacheClientHostRequestTest extends AbstractTest {
     @Override
-    protected SimpleHttpRequest buildRequest(String method, URI uri, Map<String, String> headers) {
-      HttpHost httpHost = new HttpHost(uri.getScheme(), uri.getHost(), uri.getPort());
-      return configureRequest(
-          new SimpleHttpRequest(method, httpHost, fullPathFromUri(uri)), headers);
-    }
-
-    @Override
-    protected int sendRequest(
-        SimpleHttpRequest request, String method, URI uri, Map<String, String> headers)
-        throws ExecutionException, InterruptedException {
-      return getResponseCode(getClient(uri).execute(request, null).get());
-    }
-
-    @Override
-    protected void sendRequestWithCallback(
-        SimpleHttpRequest request,
-        String method,
-        URI uri,
-        Map<String, String> headers,
-        RequestResult requestResult) {
-      getClient(uri).execute(request, new ResponseCallback(requestResult));
-    }
-
-    @Override
-    protected void configure(HttpClientTestOptions options) {
-      configureTest(options);
+    SimpleHttpRequest createRequest(String method, URI uri) {
+      return new SimpleHttpRequest(method, getHost(uri), fullPathFromURI(uri));
     }
   }
 
   @Nested
-  class ApacheClientHostAbsoluteUriRequestTest extends AbstractHttpClientTest<SimpleHttpRequest> {
-
+  class ApacheClientHostAbsoluteUriRequestTest extends AbstractTest {
     @Override
-    protected SimpleHttpRequest buildRequest(String method, URI uri, Map<String, String> headers) {
-      return configureRequest(new SimpleHttpRequest(method, uri), headers);
+    SimpleHttpRequest createRequest(String method, URI uri) {
+      return new SimpleHttpRequest(method, URI.create(uri.toString()));
+    }
+  }
+
+  abstract class AbstractTest extends AbstractApacheHttpClientTest<SimpleHttpRequest> {
+    @Override
+    HttpResponse executeRequest(SimpleHttpRequest request, URI uri) throws Exception {
+      return getClient(uri).execute(request, null).get();
     }
 
     @Override
-    protected int sendRequest(
-        SimpleHttpRequest request, String method, URI uri, Map<String, String> headers)
-        throws ExecutionException, InterruptedException {
-      return getResponseCode(getClient(uri).execute(request, null).get());
-    }
-
-    @Override
-    protected void sendRequestWithCallback(
-        SimpleHttpRequest request,
-        String method,
-        URI uri,
-        Map<String, String> headers,
-        RequestResult requestResult) {
-      getClient(uri).execute(request, new ResponseCallback(requestResult));
+    void executeRequestWithCallback(SimpleHttpRequest request, URI uri, RequestResult result) {
+      getClient(uri).execute(request, new ResponseCallback(result));
     }
 
     @Override
     protected void configure(HttpClientTestOptions options) {
-      configureTest(options);
+      super.configure(options);
+      options.setResponseCodeOnRedirectError(302);
     }
-  }
-
-  SimpleHttpRequest configureRequest(SimpleHttpRequest request, Map<String, String> headers) {
-    request.addHeader("user-agent", "httpasyncclient");
-    headers.forEach((key, value) -> request.setHeader(new BasicHeader(key, value)));
-    return request;
-  }
-
-  int getResponseCode(SimpleHttpResponse response) {
-    return response.getCode();
   }
 
   private static class ResponseCallback implements FutureCallback<SimpleHttpResponse> {
@@ -203,42 +134,5 @@ class ApacheHttpAsyncClientTest {
     public void cancelled() {
       requestResult.complete(new CancellationException());
     }
-  }
-
-  void configureTest(HttpClientTestOptions options) {
-    options.setUserAgent("httpasyncclient");
-    options.setResponseCodeOnRedirectError(302);
-    options.enableTestReadTimeout();
-    options.setHttpAttributes(
-        endpoint -> {
-          Set<AttributeKey<?>> attributes = new HashSet<>();
-          attributes.add(SemanticAttributes.NET_PEER_NAME);
-          attributes.add(SemanticAttributes.NET_PEER_PORT);
-          attributes.add(SemanticAttributes.HTTP_URL);
-          attributes.add(SemanticAttributes.HTTP_METHOD);
-          if (endpoint.toString().contains("/success")) {
-            attributes.add(SemanticAttributes.HTTP_FLAVOR);
-          }
-          attributes.add(SemanticAttributes.HTTP_USER_AGENT);
-          return attributes;
-        });
-  }
-
-  static String fullPathFromUri(URI uri) {
-    StringBuilder builder = new StringBuilder();
-    if (uri.getPath() != null) {
-      builder.append(uri.getPath());
-    }
-
-    if (uri.getQuery() != null) {
-      builder.append('?');
-      builder.append(uri.getQuery());
-    }
-
-    if (uri.getFragment() != null) {
-      builder.append('#');
-      builder.append(uri.getFragment());
-    }
-    return builder.toString();
   }
 }
