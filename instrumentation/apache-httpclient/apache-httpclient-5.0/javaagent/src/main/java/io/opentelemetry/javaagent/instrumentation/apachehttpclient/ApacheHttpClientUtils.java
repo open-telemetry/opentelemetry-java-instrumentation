9d2cf7c8fca7683acbae0ba55c644ba2815029b2
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2022-04-03 22:32:05 +0530

    Corrects build errors

--- instrumentation/apache-httpclient/apache-httpclient-5.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/apachehttpclient/ApacheHttpClientUtils.java
+++ instrumentation/apache-httpclient/apache-httpclient-5.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/apachehttpclient/ApacheHttpClientUtils.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package io.opentelemetry.javaagent.instrumentation.apachehttpclient;
 
 import static java.util.logging.Level.FINE;
