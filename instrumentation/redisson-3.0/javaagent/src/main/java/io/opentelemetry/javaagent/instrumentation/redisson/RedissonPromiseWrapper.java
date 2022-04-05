27e8201618258c85c185daffaa0d5a92c6c61502
Lauri Tulmin <ltulmin@splunk.com>
2022-04-05 19:59:22 +0300

    Rework context propagation to redisson async callback (#5748)
    * Rework context propagation to redisson async callback
    
    * add comments

--- instrumentation/redisson-3.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/redisson/RedissonPromiseWrapper.java
+++ instrumentation/redisson-3.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/redisson/RedissonPromiseWrapper.java
@@ -13,29 +13,57 @@ import org.redisson.misc.RedissonPromise;
 public class RedissonPromiseWrapper<T> extends RedissonPromise<T> implements PromiseWrapper<T> {
   private volatile EndOperationListener<T> endOperationListener;
 
-  private RedissonPromiseWrapper(RPromise<T> delegate, Context context) {
+  private RedissonPromiseWrapper(RPromise<T> delegate) {
     this.whenComplete(
         (result, error) -> {
           EndOperationListener<T> endOperationListener = this.endOperationListener;
           if (endOperationListener != null) {
             endOperationListener.accept(result, error);
           }
-          try (Scope ignored = context.makeCurrent()) {
-            if (error != null) {
-              delegate.tryFailure(error);
-            } else {
-              delegate.trySuccess(result);
-            }
+          if (error != null) {
+            delegate.tryFailure(error);
+          } else {
+            delegate.trySuccess(result);
           }
         });
   }
 
+  /**
+   * Wrap {@link RPromise} so that {@link EndOperationListener}, that is used to end the span, could
+   * be attached to it.
+   */
   public static <T> RPromise<T> wrap(RPromise<T> delegate) {
     if (delegate instanceof RedissonPromiseWrapper) {
       return delegate;
     }
 
-    return new RedissonPromiseWrapper<>(delegate, Context.current());
+    return new RedissonPromiseWrapper<>(delegate);
+  }
+
+  /**
+   * Wrap {@link RPromise} to run callbacks with the context that was current at the time this
+   * method was called.
+   *
+   * <p>This method should be called on, or as close as possible to, the {@link RPromise} that is
+   * returned to the user to ensure that the callbacks added by user are run in appropriate context.
+   */
+  public static <T> RPromise<T> wrapContext(RPromise<T> promise) {
+    Context context = Context.current();
+    // when returned promise is completed, complete input promise with context that was current
+    // at the time when the promise was wrapped
+    RPromise<T> result = new RedissonPromise<T>();
+    result.whenComplete(
+        (value, error) -> {
+          try (Scope ignored = context.makeCurrent()) {
+            if (error != null) {
+              promise.tryFailure(error);
+            } else {
+              promise.trySuccess(value);
+            }
+          }
+        });
+
+    return result;
   }
 
   @Override
