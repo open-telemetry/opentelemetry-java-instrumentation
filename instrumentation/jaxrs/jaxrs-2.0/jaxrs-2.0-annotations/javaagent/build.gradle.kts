7f25306764216dca9a8f7bed4e4165cbfe3b3101
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-04-10 15:10:43 -0700

    Extract instrumentation out of jaxrs-common module

---
+++ instrumentation/jaxrs/jaxrs-2.0/jaxrs-2.0-annotations/javaagent/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+  id("otel.javaagent-instrumentation")
+}
+
+muzzle {
+  fail {
+    group.set("javax.ws.rs")
+    module.set("jsr311-api")
+    versions.set("[,]")
+  }
+  pass {
+    group.set("javax.ws.rs")
+    module.set("javax.ws.rs-api")
+    versions.set("[,]")
+  }
+}
+
+dependencies {
+  bootstrap(project(":instrumentation:jaxrs:jaxrs-common:bootstrap"))
+
+  implementation(project(":instrumentation:jaxrs:jaxrs-2.0:jaxrs-2.0-common:javaagent"))
+
+  compileOnly("javax.ws.rs:javax.ws.rs-api:2.0")
+}
