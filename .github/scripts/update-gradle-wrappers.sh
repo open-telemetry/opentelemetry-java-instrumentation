d60e89e1affaaa38078c755645e43a836e2a0039
Anuraag Agrawal <anuraaga@gmail.com>
2022-04-14 06:35:34 +0000

    Update updater

--- .github/scripts/update-gradle-wrappers.sh
+++ .github/scripts/update-gradle-wrappers.sh
@@ -5,9 +5,7 @@ gradle_version=$1
 for dir in . \
            benchmark-overhead \
            examples/distro \
-           examples/extension \
-           smoke-tests/images/fake-backend \
-           smoke-tests/images/servlet
+           examples/extension
 do
   (cd $dir && ./gradlew wrapper --gradle-version $gradle_version)
 done
