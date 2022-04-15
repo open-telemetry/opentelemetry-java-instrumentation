007077a8e40497e4fe5f4f05c0c3336650ea6bcc
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-04-14 17:19:14 -0700

    spring-kafka

---
+++ instrumentation/spring/spring-kafka-2.7/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/spring/kafka/SuppressingKafkaClientsInstrumentation.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.spring.kafka;
+
+import static net.bytebuddy.matcher.ElementMatchers.named;
+
+import io.opentelemetry.javaagent.bootstrap.kafka.KafkaClientsConsumerProcessTracing;
+import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
+import io.opentelemetry.javaagent.extension.instrumentation.TypeTransformer;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+
+public class SuppressingKafkaClientsInstrumentation implements TypeInstrumentation {
+
+  @Override
+  public ElementMatcher<TypeDescription> typeMatcher() {
+    return named(
+        "org.springframework.kafka.listener.KafkaMessageListenerContainer$ListenerConsumer");
+  }
+
+  @Override
+  public void transform(TypeTransformer transformer) {
+    transformer.applyAdviceToMethod(named("run"), this.getClass().getName() + "$RunLoopAdvice");
+  }
+
+  // this advice suppresses the CONSUMER spans created by the kafka-clients instrumentation
+  @SuppressWarnings("unused")
+  public static class RunLoopAdvice {
+    @Advice.OnMethodEnter(suppress = Throwable.class)
+    public static void onEnter() {
+      System.out.println("DISABLE WRAPPER AT THE START");
+      KafkaClientsConsumerProcessTracing.disableWrapping();
+    }
+
+    @Advice.OnMethodExit(suppress = Throwable.class)
+    public static void onExit() {
+      KafkaClientsConsumerProcessTracing.enableWrapping();
+    }
+  }
+}
