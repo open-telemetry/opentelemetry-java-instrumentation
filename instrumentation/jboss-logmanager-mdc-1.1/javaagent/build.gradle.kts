0a639af5173419c13c116272fd7e61d88cfd30be
Cuichen Li <cuichli@cisco.com>
2022-04-15 22:07:04 +0800

    add support for jboss-logmanager-mdc
    Signed-off-by: Cuichen Li <cuichli@cisco.com>

---
+++ instrumentation/jboss-logmanager-mdc-1.1/javaagent/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+  id("otel.javaagent-instrumentation")
+}
+
+muzzle {
+  pass {
+    group.set("org.jboss.logmanager")
+    module.set("jboss-logmanager")
+    versions.set("[1.1.0.GA,)")
+    assertInverse.set(true)
+  }
+}
+
+dependencies {
+  library("org.jboss.logmanager:jboss-logmanager:1.1.0.GA")
+
+  compileOnly(project(":instrumentation-appender-api-internal"))
+
+  // ensure no cross interference
+  testInstrumentation(project(":instrumentation:java-util-logging:javaagent"))
+}
