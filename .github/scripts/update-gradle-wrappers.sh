e2f1c43493be5341590205c9d4e0799c3a5c712a
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-05-03 15:23:50 -0700

    Update gradle in standalone modules

--- .github/scripts/update-gradle-wrappers.sh
+++ .github/scripts/update-gradle-wrappers.sh
@@ -4,8 +4,9 @@ gradle_version=$1
 
 for dir in . \
            benchmark-overhead \
+           gradle-plugins \
            examples/distro \
            examples/extension
 do
-  (cd $dir && ./gradlew wrapper --gradle-version $gradle_version)
+  (cd $dir && ./gradlew wrapper --gradle-version "$gradle_version")
 done
