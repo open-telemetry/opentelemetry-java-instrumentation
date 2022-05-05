400bd5af3f409721c09fe214af94d10feb1399ee
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-05-05 08:39:21 -0700

    Update log4j package names (#5978)

---
+++ instrumentation/log4j/log4j-appender-2.17/library/src/main/java/io/opentelemetry/instrumentation/log4j/appender/v2_17/internal/ContextDataAccessor.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.instrumentation.log4j.appender.v2_17.internal;
+
+import java.util.function.BiConsumer;
+import javax.annotation.Nullable;
+
+/**
+ * This class is internal and is hence not for public use. Its APIs are unstable and can change at
+ * any time.
+ */
+public interface ContextDataAccessor<T> {
+
+  @Nullable
+  Object getValue(T contextData, String key);
+
+  void forEach(T contextData, BiConsumer<String, Object> action);
+}
