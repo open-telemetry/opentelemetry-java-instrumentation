cfe4a223071f4897aef5d60a11a0837d491fffbf
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-05-06 11:51:02 -0700

    Remove netty-4.1-common (#5985)
    * Remove netty-4.1-common
    
    * reactor-netty-1.0

--- instrumentation/reactor/reactor-netty/reactor-netty-0.9/javaagent/build.gradle.kts
+++ instrumentation/reactor/reactor-netty/reactor-netty-0.9/javaagent/build.gradle.kts
@@ -6,17 +6,21 @@ muzzle {
   pass {
     group.set("io.projectreactor.netty")
     module.set("reactor-netty")
-    versions.set("[0.9.0.RELEASE,1.0.0)")
+    versions.set("[0.8.2.RELEASE,1.0.0)")
+    assertInverse.set(true)
+    excludeInstrumentationModule("io.opentelemetry.javaagent.instrumentation.netty.v4_1.NettyInstrumentationModule")
   }
   fail {
     group.set("io.projectreactor.netty")
     module.set("reactor-netty-http")
     versions.set("[1.0.0,)")
+    assertInverse.set(true)
+    excludeInstrumentationModule("io.opentelemetry.javaagent.instrumentation.netty.v4_1.NettyInstrumentationModule")
   }
 }
 
 dependencies {
-  implementation(project(":instrumentation:netty:netty-4.1-common:javaagent"))
+  implementation(project(":instrumentation:netty:netty-4.1:javaagent"))
   library("io.projectreactor.netty:reactor-netty:0.9.0.RELEASE")
 
   testInstrumentation(project(":instrumentation:reactor:reactor-netty:reactor-netty-1.0:javaagent"))
