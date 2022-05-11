7a3efb3d19fab6ef628ae320a82299b6ce25672d
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2022-05-11 13:24:39 +0200

    fix muzzle

--- instrumentation/hikaricp-3.0/javaagent/build.gradle.kts
+++ instrumentation/hikaricp-3.0/javaagent/build.gradle.kts
@@ -8,6 +8,9 @@ muzzle {
     module.set("HikariCP")
     versions.set("[3.0.0,)")
     // muzzle does not detect PoolStats method references used - some of these methods were introduced in 3.0 and we can't assertInverse
+
+    // 4.0.0 uses a broken slf4j version: the "${slf4j.version}" placeholder is taken literally
+    skip("4.0.0")
   }
 }
 
