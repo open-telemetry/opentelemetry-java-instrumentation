4fa30a84b0fe391612141986433a6b348e533a18
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-05-16 19:59:42 -0700

    Fix release workflow (#6040)

--- .github/scripts/update-version.sh
+++ .github/scripts/update-version.sh
@@ -1,16 +1,15 @@
 #!/bin/bash -e
 
-sed -Ei "s/[0-9]+\.[0-9]+\.[0-9]+/$1/" version.gradle.kts
-
-# also update versions in these files
-# - examples/distro/build.gradle
-# - examples/extension/build.gradle
+version=$1
 
 if [[ $version == *-SNAPSHOT ]]; then
   alpha_version=${version//-SNAPSHOT/-alpha-SNAPSHOT}
 else
   alpha_version=$version-alpha
 fi
+
+sed -Ei "s/val stableVersion = \"[^\"]*\"/val stableVersion = \"$version\"/" version.gradle.kts
+sed -Ei "s/val alphaVersion = \"[^\"]*\"/val alphaVersion = \"$alpha_version\"/" version.gradle.kts
 
 sed -Ei "s/(opentelemetryJavaagent *: )\"[^\"]*\"/\1\"$version\"/" examples/distro/build.gradle
 sed -Ei "s/(opentelemetryJavaagentAlpha *: )\"[^\"]*\"/\1\"$alpha_version\"/" examples/distro/build.gradle
