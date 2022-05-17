4341f0154534c3b5b517a928967f4930864bf280
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-05-16 21:51:10 -0700

    Add diff-suppression-keys-with-docs script back

---
+++ .github/scripts/diff-suppression-keys-with-docs.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# this script reports some false positives, but is helpful in comparing the keys listed
+# in suppressing-instrumentation.md with the keys listed in the source files
+
+set -e -u -o pipefail
+
+comm -3 \
+  <(
+    sed -n '/----------------------/,${p;/^$/q}' \
+        ../opentelemetry.io/content/en/docs/instrumentation/java/automatic/agent-config.md \
+      | sed '1d;$d' \
+      | cut -d '|' -f 3 \
+      | tr -d ' ' \
+      | sort -u
+  ) \
+  <(
+    git ls-files '*Module.java' \
+      | xargs grep super \
+      | grep -oP '(?<=super\(")[^"]+' \
+      | sort -u
+  )
