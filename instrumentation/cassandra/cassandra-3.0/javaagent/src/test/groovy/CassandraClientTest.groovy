a2a8c11ecc87800431096bce4d661121af40d883
Lauri Tulmin <ltulmin@splunk.com>
2022-05-19 16:07:30 +0300

    Limit cassandra memory usage (#6068)

--- instrumentation/cassandra/cassandra-3.0/javaagent/src/test/groovy/CassandraClientTest.groovy
+++ instrumentation/cassandra/cassandra-3.0/javaagent/src/test/groovy/CassandraClientTest.groovy
@@ -37,6 +37,8 @@ class CassandraClientTest extends AgentInstrumentationSpecification {
 
   def setupSpec() {
     cassandra = new GenericContainer("cassandra:3")
+      // limit memory usage
+      .withEnv("JVM_OPTS", "-Xmx128m -Xms128m")
       .withExposedPorts(9042)
       .withLogConsumer(new Slf4jLogConsumer(logger))
       .withStartupTimeout(Duration.ofSeconds(120))
