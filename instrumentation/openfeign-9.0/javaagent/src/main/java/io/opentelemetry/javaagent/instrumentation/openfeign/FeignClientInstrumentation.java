14dd7dce8179d5f290a3580dc93b576fdd39aa81
zmapleshine <715017@163.com>
2022-05-30 13:48:48 +0800

    Fix openfeign instrumentation call depth condition

--- instrumentation/openfeign-9.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/openfeign/FeignClientInstrumentation.java
+++ instrumentation/openfeign-9.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/openfeign/FeignClientInstrumentation.java
@@ -11,8 +11,10 @@ import static net.bytebuddy.matcher.ElementMatchers.isMethod;
 import static net.bytebuddy.matcher.ElementMatchers.named;
 
 import feign.Feign;
+import feign.Request;
 import feign.Response;
 import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.context.Scope;
 import io.opentelemetry.instrumentation.api.util.VirtualField;
 import io.opentelemetry.javaagent.bootstrap.CallDepth;
 import io.opentelemetry.javaagent.bootstrap.Java8BytecodeBridge;
@@ -47,22 +49,28 @@ public class FeignClientInstrumentation implements TypeInstrumentation {
   @SuppressWarnings("unused")
   public static class ClientExecuteAdvice {
 
+    @Advice.OnMethodEnter(suppress = Throwable.class)
+    public static void onEnter(
+        @Advice.Argument(0) Request request,
+        @Advice.Local("callDepth") CallDepth callDepth,
+        @Advice.Local("scope") Scope scope) {
+
+      callDepth = CallDepth.forClass(Feign.class);
+      callDepth.getAndIncrement();
+    }
+
     @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
     public static void onExit(
         @Advice.Return Response response, @Advice.Thrown Throwable throwable) {
 
       CallDepth callDepth = CallDepth.forClass(Feign.class);
-      if (callDepth.getAndIncrement() > 0) {
+      if (callDepth.decrementAndGet() > 0) {
         return;
       }
 
-      try {
-        Span span = Java8BytecodeBridge.currentSpan();
-        VirtualField<Span, Response> virtualField = VirtualField.find(Span.class, Response.class);
-        virtualField.set(span, response);
-      } finally {
-        callDepth.decrementAndGet();
-      }
+      Span span = Java8BytecodeBridge.currentSpan();
+      VirtualField<Span, Response> virtualField = VirtualField.find(Span.class, Response.class);
+      virtualField.set(span, response);
     }
   }
 }
