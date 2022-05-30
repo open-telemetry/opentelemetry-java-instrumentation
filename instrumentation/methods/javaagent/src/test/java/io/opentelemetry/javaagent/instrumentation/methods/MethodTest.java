7d84abe476a6e295eef9a9b415d6e9cc150d2e85
i.darcydarcy@Gmail.com <i.darcydarcy@Gmail.com>
2022-05-30 23:55:58 +0800

    add testcase for method instrumentation

--- instrumentation/methods/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/methods/MethodTest.java
+++ instrumentation/methods/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/methods/MethodTest.java
@@ -11,7 +11,10 @@ import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.instrumentation.testing.junit.AgentInstrumentationExtension;
 import io.opentelemetry.instrumentation.testing.junit.InstrumentationExtension;
+import io.opentelemetry.sdk.trace.data.SpanData;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
@@ -23,7 +26,6 @@ class MethodTest {
   @Test
   void methodTraced() {
     assertThat(new ConfigTracedCallable().call()).isEqualTo("Hello!");
-
     testing.waitAndAssertTraces(
         trace ->
             trace.hasSpansSatisfyingExactly(
@@ -31,6 +33,34 @@ class MethodTest {
                     span.hasName("ConfigTracedCallable.call")
                         .hasKind(SpanKind.INTERNAL)
                         .hasAttributes(Attributes.empty())));
+    testing.clearData();
+    new ConfigTracedCompletableFuture().getResult()
+        .thenAccept(t -> assertThat(t).isEqualTo("Hello!"));
+    testing.waitAndAssertTraces(
+        trace -> {
+          SpanData spanData = trace.getSpan(0);
+          assertThat(spanData).isNotNull();
+          assertThat(spanData.getName()).isEqualTo("ConfigTracedCompletableFuture.getResult");
+          assertThat(spanData.getEndEpochNanos() - spanData.getStartEpochNanos())
+              .isGreaterThan(TimeUnit.MILLISECONDS.toNanos(100));
+        });
+  }
+
+
+  static class ConfigTracedCompletableFuture {
+    CompletableFuture<String> getResult() {
+      CompletableFuture<String> completableFuture = new CompletableFuture<>();
+      //can not use CompletableFuture#completeOnTimeout on language level 8
+      new Thread(() -> {
+        try {
+          TimeUnit.MILLISECONDS.sleep(100);
+        } catch (InterruptedException e) {
+          //ignore
+        }
+        completableFuture.complete("Hello!");
+      }).start();
+      return completableFuture;
+    }
   }
 
   static class ConfigTracedCallable implements Callable<String> {
