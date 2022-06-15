0b1211c42a2a327949a5d395928892434489809a
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-06-15 15:21:39 -0700

    Update change log

--- .github/scripts/draft-change-log-entries.sh
+++ .github/scripts/draft-change-log-entries.sh
@@ -25,6 +25,23 @@ fi
 
 echo "## Unreleased"
 echo
+echo "### Migration notes"
+echo
+echo
+echo "### 🌟 New javaagent instrumentation"
+echo
+echo
+echo "### 🌟 New library instrumentation"
+echo
+echo
+echo "### 📈 Enhancements"
+echo
+echo
+echo "### 🛠️ Bug fixes"
+echo
+echo
+echo "### 🧰 Tooling"
+echo
 
 git log --reverse --pretty=format:"- %s" $range \
-  | sed -E 's,\(#([0-9]+)\),\n  ([#\1](https://github.com/open-telemetry/opentelemetry-java-instrumentation/pull/\1)),'
+  | sed -E 's,\(#([0-9]+)\)$,\n  ([#\1](https://github.com/open-telemetry/opentelemetry-java-instrumentation/pull/\1)),'
