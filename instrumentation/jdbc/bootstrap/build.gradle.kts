e85eb8c2f9440b002448f755f43c2b6a11c65437
Lauri Tulmin <ltulmin@splunk.com>
2022-06-16 15:04:00 +0300

    add comment

--- instrumentation/jdbc/bootstrap/build.gradle.kts
+++ instrumentation/jdbc/bootstrap/build.gradle.kts
@@ -2,6 +2,14 @@ plugins {
   id("otel.javaagent-bootstrap")
 }
 
+/*
+JDDC instrumentation uses VirtualField<Connection, DbInfo>. Add DbInfo, that is used as the value of
+VirtualField, to boot loader. We do this because when JDBC instrumentation is started in multiple
+class loaders in the same hierarchy each would define their own version of DbInfo. It is possible
+that the value read from virutal field would be from the wrong class loader and could produce a
+ClassCastException. Having a single copy of DbInfo that is in boot loader avoids this issue.
+ */
+
 sourceSets {
   main {
     val shadedDep = project(":instrumentation:jdbc:library")
