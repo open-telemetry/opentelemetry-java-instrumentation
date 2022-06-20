958da79cce9cb3742abe96c70e134e4ced1f8b08
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-06-20 16:13:24 -0700

    Update azure-sdk instrumentation

--- instrumentation/azure-core/azure-core-1.19/library-instrumentation-shaded/build.gradle.kts
+++ instrumentation/azure-core/azure-core-1.19/library-instrumentation-shaded/build.gradle.kts
@@ -8,9 +8,9 @@ group = "io.opentelemetry.javaagent.instrumentation"
 
 dependencies {
   // to look at (potentially incompatible) differences in new versions of the injected artifact, run:
-  // git diff azure-core-tracing-opentelemetry_1.0.0-beta.19 azure-core-tracing-opentelemetry_1.0.0-beta.20
-  //          -- sdk/core/azure-core-tracing-opentelemetry
-  implementation("com.azure:azure-core-tracing-opentelemetry:1.0.0-beta.20")
+  // git diff azure-core-tracing-opentelemetry_1.0.0-beta.25 azure-core-tracing-opentelemetry_1.0.0-beta.26
+  //          -- sdk/core/azure-core-tracing-opentelemetry/src/main
+  implementation("com.azure:azure-core-tracing-opentelemetry:1.0.0-beta.25")
 }
 
 tasks {
