6eade17f2da68fe380086f3b3ff40f31cdc2f77d
Lauri Tulmin <ltulmin@splunk.com>
2022-07-08 00:40:31 +0300

    add test

---
+++ javaagent-tooling/src/testMissingType/java/io/opentelemetry/javaagent/tooling/bytebuddy/TestAdvice.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.tooling.bytebuddy;
+
+import net.bytebuddy.asm.Advice;
+
+@SuppressWarnings("unused")
+public class TestAdvice {
+
+  @Advice.OnMethodExit
+  public static void returnTrue(@Advice.Return(readOnly = false) boolean returnVal) {
+    returnVal = true;
+  }
+}
