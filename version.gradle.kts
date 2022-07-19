a7b5a6cb91aaa604a4f3d29463e20560a922292c
opentelemetrybot <107717825+opentelemetrybot@users.noreply.github.com>
2022-07-19 16:16:42 +0000

    Update version to 1.17.0-SNAPSHOT

--- version.gradle.kts
+++ version.gradle.kts
@@ -1,5 +1,5 @@
-val stableVersion = "1.16.0-SNAPSHOT"
-val alphaVersion = "1.16.0-alpha-SNAPSHOT"
+val stableVersion = "1.17.0-SNAPSHOT"
+val alphaVersion = "1.17.0-alpha-SNAPSHOT"
 
 allprojects {
   if (findProperty("otel.stable") != "true") {
