8be51b3dde09a203f741571a94846b6e8ace7704
Lauri Tulmin <ltulmin@splunk.com>
2022-07-19 18:04:08 +0300

    Fix spring kafka latest dep tests (#6339)

--- instrumentation/spring/spring-kafka-2.7/library/build.gradle.kts
+++ instrumentation/spring/spring-kafka-2.7/library/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
   testImplementation(project(":instrumentation:kafka:kafka-clients:kafka-clients-2.6:library"))
 
   // 2.7.0 has a bug that makes decorating a Kafka Producer impossible
-  testImplementation("org.springframework.kafka:spring-kafka:2.7.1")
+  testLibrary("org.springframework.kafka:spring-kafka:2.7.1")
 
   testLibrary("org.springframework.boot:spring-boot-starter-test:2.5.3")
   testLibrary("org.springframework.boot:spring-boot-starter:2.5.3")
