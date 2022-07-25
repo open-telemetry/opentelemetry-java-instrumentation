6e71502085f8ded1ad6c4a45bcde511843330f61
LironKS <liron@gethelios.dev>
2022-07-25 13:06:13 +0300

    remove test runs for debugging - revert this commit!

--- version.gradle.kts
+++ version.gradle.kts
@@ -1,5 +1,5 @@
-val stableVersion = "1.17.1-SNAPSHOT"
-val alphaVersion = "1.17.1-alpha-SNAPSHOT"
+val stableVersion = "1.17.0-SNAPSHOT"
+val alphaVersion = "1.17.0-alpha-SNAPSHOT"
 
 allprojects {
   if (findProperty("otel.stable") != "true") {
