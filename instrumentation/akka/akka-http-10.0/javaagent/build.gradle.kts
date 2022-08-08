9366e3aedbdfba85e20eef077cec6f76b1497e86
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-08-08 08:29:14 -0700

    Muzzle review (#6437)
    * Muzzle review
    
    * Fix

--- instrumentation/akka/akka-http-10.0/javaagent/build.gradle.kts
+++ instrumentation/akka/akka-http-10.0/javaagent/build.gradle.kts
@@ -7,38 +7,26 @@ muzzle {
   pass {
     group.set("com.typesafe.akka")
     module.set("akka-http_2.11")
-    versions.set("[10.0.0,10.1.0)")
+    versions.set("[10,)")
+    assertInverse.set(true)
     // later versions of akka-http expect streams to be provided
-    extraDependency("com.typesafe.akka:akka-stream_2.11:2.4.14")
+    extraDependency("com.typesafe.akka:akka-stream_2.11:2.5.32")
   }
   pass {
     group.set("com.typesafe.akka")
     module.set("akka-http_2.12")
-    versions.set("[10.0.0,10.1.0)")
+    versions.set("[10,)")
+    assertInverse.set(true)
     // later versions of akka-http expect streams to be provided
-    extraDependency("com.typesafe.akka:akka-stream_2.12:2.4.14")
+    extraDependency("com.typesafe.akka:akka-stream_2.12:2.5.32")
   }
-  pass {
-    group.set("com.typesafe.akka")
-    module.set("akka-http_2.11")
-    versions.set("[10.1.0,)")
-    // later versions of akka-http expect streams to be provided
-    extraDependency("com.typesafe.akka:akka-stream_2.11:2.5.11")
-  }
-  pass {
-    group.set("com.typesafe.akka")
-    module.set("akka-http_2.12")
-    versions.set("[10.1.0,)")
-    // later versions of akka-http expect streams to be provided
-    extraDependency("com.typesafe.akka:akka-stream_2.12:2.5.11")
-  }
-  // There is no akka-http 10.0.x series for scala 2.13
   pass {
     group.set("com.typesafe.akka")
     module.set("akka-http_2.13")
-    versions.set("[10.1.8,)")
+    versions.set("[10,)")
+    assertInverse.set(true)
     // later versions of akka-http expect streams to be provided
-    extraDependency("com.typesafe.akka:akka-stream_2.13:2.5.23")
+    extraDependency("com.typesafe.akka:akka-stream_2.13:2.5.32")
   }
 }
 
