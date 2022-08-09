df0e8d39af79faf7a1c967bdec30099493d82344
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-08-08 17:11:37 -0700

    Fix the build

--- instrumentation/aws-sdk/aws-sdk-2.2/javaagent/build.gradle.kts
+++ instrumentation/aws-sdk/aws-sdk-2.2/javaagent/build.gradle.kts
@@ -10,6 +10,8 @@ muzzle {
     // Used by all SDK services, the only case it isn't is an SDK extension such as a custom HTTP
     // client, which is not target of instrumentation anyways.
     extraDependency("software.amazon.awssdk:protocol-core")
+    // several software.amazon.awssdk artifacts are missing for this version
+    skip("2.17.200")
   }
 }
 
