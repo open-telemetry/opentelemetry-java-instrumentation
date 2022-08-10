36b7955d154ccbac3fb1e2959157c885bdba37d4
Oren Levy <oren@gethelios.dev>
2022-08-10 15:54:10 +0300

    and more

--- instrumentation/mongo/mongo-common/testing/build.gradle.kts
+++ instrumentation/mongo/mongo-common/testing/build.gradle.kts
@@ -12,3 +12,9 @@ dependencies {
   implementation("io.opentelemetry:opentelemetry-api")
   implementation("org.spockframework:spock-core")
 }
+
+tasks {
+  test {
+    jvmArgs("-Dotel.instrumentation.common.db-statement-sanitizer.enabled=true")
+  }
+}
