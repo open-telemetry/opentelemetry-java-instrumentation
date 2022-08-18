fdc325e2090206d8abc7d43f2474b64913af4797
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2022-08-18 10:28:16 +0200

    Deprecate agent config SPIs (#6476)

--- javaagent-extension-api/src/main/java/io/opentelemetry/javaagent/extension/config/ConfigPropertySource.java
+++ javaagent-extension-api/src/main/java/io/opentelemetry/javaagent/extension/config/ConfigPropertySource.java
@@ -6,7 +6,10 @@
 package io.opentelemetry.javaagent.extension.config;
 
 import io.opentelemetry.javaagent.extension.Ordered;
+import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizer;
+import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvider;
 import java.util.Map;
+import java.util.function.Supplier;
 
 /**
  * A service provider that allows to override default OTel agent configuration. Properties returned
@@ -16,7 +19,11 @@ import java.util.Map;
  *
  * <p>This is a service provider interface that requires implementations to be registered in a
  * provider-configuration file stored in the {@code META-INF/services} resource directory.
+ *
+ * @deprecated Use the {@link AutoConfigurationCustomizerProvider} and {@link
+ *     AutoConfigurationCustomizer#addPropertiesSupplier(Supplier)} instead.
  */
+@Deprecated
 public interface ConfigPropertySource extends Ordered {
 
   /**
