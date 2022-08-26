956da5b340a0d5d55b320e65ccb45e13d071d494
Jason Plumb <jplumb@splunk.com>
2022-08-26 15:45:21 -0700

    Add spring boot service name guesser.

---
+++ instrumentation/spring/spring-boot-autoconfigure/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/resources/SpringBootServiceNameGuesser.java
@@ -0,0 +1,264 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.instrumentation.spring.autoconfigure.resources;
+
+import com.google.auto.service.AutoService;
+import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
+import io.opentelemetry.sdk.autoconfigure.spi.ResourceProvider;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Properties;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+import javax.annotation.Nullable;
+import org.yaml.snakeyaml.Yaml;
+
+/**
+ * A ResourceProvider that will attempt to guess the application name for a Spring Boot service.
+ * When successful, it will return a Resource that has the service name attribute populated with the
+ * name of the Spring Boot application. It uses the following strategies, and the first successful
+ * strategy wins:
+ *
+ * <ul>
+ *   <li>Check for the SPRING_APPLICATION_NAME environment variable
+ *   <li>Check for spring.application.name system property
+ *   <li>Check for application.properties file on the classpath
+ *   <li>Check for application.properties in the current working dir
+ *   <li>Check for application.yml on the classpath
+ *   <li>Check for application.yml in the current working dir
+ *   <li>Check for --spring.application.name program argument (not jvm arg) via ProcessHandle
+ *   <li>Check for --spring.application.name program argument via sun.java.command system property
+ * </ul>
+ */
+@AutoService(ResourceProvider.class)
+public class SpringBootServiceNameGuesser implements ResourceProvider {
+
+  private static final Pattern COMMANDLINE_PATTERN =
+      Pattern.compile(".*--spring\\.application\\.name=([a-zA-Z.\\-_]+).*");
+  private static final Logger logger =
+      Logger.getLogger(SpringBootServiceNameGuesser.class.getName());
+  private final SystemHelper system;
+
+  public SpringBootServiceNameGuesser() {
+    this(new SystemHelper());
+  }
+
+  // Exists for testing
+  SpringBootServiceNameGuesser(SystemHelper system) {
+    this.system = system;
+  }
+
+  @Override
+  public Resource createResource(ConfigProperties config) {
+
+    logger.log(Level.FINER, "Performing Spring Boot service name auto-detection...");
+    Stream<Supplier<String>> finders =
+        Stream.of(
+            this::findByCommandlineArgument,
+            this::findBySystemProperties,
+            this::findByEnvironmentVariable,
+            this::findByCurrentDirectoryApplicationProperties,
+            this::findByCurrentDirectoryApplicationYaml,
+            this::findByClasspathApplicationProperties,
+            this::findByClasspathApplicationYaml);
+    return finders
+        .map(Supplier::get)
+        .filter(Objects::nonNull)
+        .findFirst()
+        .map(
+            serviceName -> {
+              logger.log(Level.FINER, "Guessed Spring Boot service name: " + serviceName);
+              return Resource.builder().put(ResourceAttributes.SERVICE_NAME, serviceName).build();
+            })
+        .orElseGet(Resource::empty);
+  }
+
+  @Nullable
+  private String findByEnvironmentVariable() {
+    String result = system.getenv("SPRING_APPLICATION_NAME");
+    logger.log(Level.FINER, "Checking for SPRING_APPLICATION_NAME in env: " + result);
+    return result;
+  }
+
+  @Nullable
+  private String findBySystemProperties() {
+    String result = system.getProperty("spring.application.name");
+    logger.log(Level.FINER, "Checking for spring.application.name system property: " + result);
+    return result;
+  }
+
+  @Nullable
+  private String findByClasspathApplicationProperties() {
+    String result = readNameFromAppProperties();
+    logger.log(
+        Level.FINER,
+        "Checking for spring.application.name in application.properties file: " + result);
+    return result;
+  }
+
+  @Nullable
+  private String findByCurrentDirectoryApplicationProperties() {
+    String result = null;
+    try (InputStream in = system.openFile("application.properties")) {
+      result = getAppNamePropertyFromStream(in);
+    } catch (Exception e) {
+      // expected to fail sometimes
+    }
+    logger.log(Level.FINER, "Checking application.properties in current dir: " + result);
+    return result;
+  }
+
+  @Nullable
+  private String findByClasspathApplicationYaml() {
+    String result =
+        loadFromClasspath("application.yml", SpringBootServiceNameGuesser::parseNameFromYaml);
+    logger.log(Level.FINER, "Checking application.yml in classpath: " + result);
+    return result;
+  }
+
+  @Nullable
+  private String findByCurrentDirectoryApplicationYaml() {
+    String result = null;
+    try (InputStream in = system.openFile("application.yml")) {
+      result = parseNameFromYaml(in);
+    } catch (Exception e) {
+      // expected to fail sometimes
+    }
+    logger.log(Level.FINER, "Checking application.yml in current dir: " + result);
+    return result;
+  }
+
+  @Nullable
+  @SuppressWarnings("unchecked")
+  private static String parseNameFromYaml(InputStream in) {
+    Yaml yaml = new Yaml();
+    try {
+      Map<String, Object> data = yaml.load(in);
+      Map<String, Map<String, Object>> spring =
+          (Map<String, Map<String, Object>>) data.get("spring");
+      if (spring != null) {
+        Map<String, Object> app = spring.get("application");
+        if (app != null) {
+          Object name = app.get("name");
+          return (String) name;
+        }
+      }
+    } catch (RuntimeException e) {
+      // expected to fail sometimes
+    }
+    return null;
+  }
+
+  @Nullable
+  private String findByCommandlineArgument() {
+    String result = attemptProcessHandleReflection();
+    if (result == null) {
+      String javaCommand = system.getProperty("sun.java.command");
+      result = parseNameFromCommandLine(javaCommand);
+    }
+    logger.log(Level.FINER, "Checking application commandline args: " + result);
+    return result;
+  }
+
+  @Nullable
+  private String attemptProcessHandleReflection() {
+    try {
+      String commandLine = system.attemptGetCommandLineViaReflection();
+      return parseNameFromCommandLine(commandLine);
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
+  @Nullable
+  private static String parseNameFromCommandLine(@Nullable String commandLine) {
+    if (commandLine == null) {
+      return null;
+    }
+    Matcher matcher = COMMANDLINE_PATTERN.matcher(commandLine);
+    if (matcher.find()) { // Required before group()
+      return matcher.group(1);
+    }
+    return null;
+  }
+
+  @Nullable
+  private String readNameFromAppProperties() {
+    return loadFromClasspath(
+        "application.properties", SpringBootServiceNameGuesser::getAppNamePropertyFromStream);
+  }
+
+  @Nullable
+  private static String getAppNamePropertyFromStream(InputStream in) {
+    Properties properties = new Properties();
+    try {
+      // Note: load() uses ISO 8859-1 encoding, same as spring uses by default for property files
+      properties.load(in);
+      return properties.getProperty("spring.application.name");
+    } catch (IOException e) {
+      return null;
+    }
+  }
+
+  @Nullable
+  private String loadFromClasspath(String filename, Function<InputStream, String> parser) {
+    try (InputStream in = system.openClasspathResource(filename)) {
+      return parser.apply(in);
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
+  // Exists for testing
+  static class SystemHelper {
+
+    String getenv(String name) {
+      return System.getenv(name);
+    }
+
+    String getProperty(String key) {
+      return System.getProperty(key);
+    }
+
+    InputStream openClasspathResource(String filename) {
+      return ClassLoader.getSystemClassLoader().getResourceAsStream(filename);
+    }
+
+    InputStream openFile(String filename) throws Exception {
+      return Files.newInputStream(Paths.get(filename));
+    }
+
+    /**
+     * Attempts to use ProcessHandle to get the full commandline of the current process. Will only
+     * succeed on java 9+.
+     */
+    @SuppressWarnings("unchecked")
+    @Nullable
+    String attemptGetCommandLineViaReflection() throws Exception {
+      Class<?> clazz = Class.forName("java.lang.ProcessHandle");
+      Method currentMethod = clazz.getDeclaredMethod("current");
+      Method infoMethod = clazz.getDeclaredMethod("info");
+      Object currentInstance = currentMethod.invoke(null);
+      Object info = infoMethod.invoke(currentInstance);
+      Method commandLineMethod = info.getClass().getInterfaces()[0].getMethod("commandLine");
+      Object optionalCommandLine = commandLineMethod.invoke(info);
+      return ((Optional<String>) optionalCommandLine).orElse(null);
+    }
+  }
+}
---
+++ instrumentation/spring/spring-boot-autoconfigure/src/test/java/io/opentelemetry/instrumentation/spring/autoconfigure/resources/SpringBootServiceNameGuesserTest.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.instrumentation.spring.autoconfigure.resources;
+
+import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
+import io.opentelemetry.sdk.resources.Resource;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static io.opentelemetry.semconv.resource.attributes.ResourceAttributes.SERVICE_NAME;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class SpringBootServiceNameGuesserTest {
+
+  static final String PROPS = "application.properties";
+  static final String APPLICATION_YML = "application.yml";
+  @Mock ConfigProperties config;
+  @Mock SpringBootServiceNameGuesser.SystemHelper system;
+
+  @Test
+  void findByEnvVar() {
+    String expected = "fur-city";
+    when(system.getenv("SPRING_APPLICATION_NAME")).thenReturn(expected);
+
+    SpringBootServiceNameGuesser guesser = new SpringBootServiceNameGuesser(system);
+
+    Resource result = guesser.createResource(config);
+    expectServiceName(result, expected);
+  }
+
+  @Test
+  void classpathApplicationProperties() {
+    when(system.openClasspathResource(PROPS)).thenCallRealMethod();
+    SpringBootServiceNameGuesser guesser = new SpringBootServiceNameGuesser(system);
+    Resource result = guesser.createResource(config);
+    expectServiceName(result, "dog-store");
+  }
+
+  @Test
+  void propertiesFileInCurrentDir() throws Exception {
+    Path propsPath = Paths.get(PROPS);
+    try {
+      writeString(propsPath, "spring.application.name=fish-tank\n");
+      when(system.openFile(PROPS)).thenCallRealMethod();
+      SpringBootServiceNameGuesser guesser = new SpringBootServiceNameGuesser(system);
+      Resource result = guesser.createResource(config);
+      expectServiceName(result, "fish-tank");
+    } finally {
+      Files.delete(propsPath);
+    }
+  }
+
+  @Test
+  void classpathApplicationYaml() {
+    when(system.openClasspathResource(APPLICATION_YML)).thenCallRealMethod();
+    SpringBootServiceNameGuesser guesser = new SpringBootServiceNameGuesser(system);
+    Resource result = guesser.createResource(config);
+    expectServiceName(result, "cat-store");
+  }
+
+  @Test
+  void yamlFileInCurrentDir() throws Exception {
+    Path yamlPath = Paths.get(APPLICATION_YML);
+    try {
+      URL url = getClass().getClassLoader().getResource(APPLICATION_YML);
+      String content = readString(Paths.get(url.toURI()));
+      writeString(yamlPath, content);
+      when(system.openFile(PROPS)).thenThrow(new FileNotFoundException());
+      when(system.openFile(APPLICATION_YML)).thenCallRealMethod();
+      SpringBootServiceNameGuesser guesser = new SpringBootServiceNameGuesser(system);
+      Resource result = guesser.createResource(config);
+      expectServiceName(result, "cat-store");
+    } finally {
+      Files.delete(yamlPath);
+    }
+  }
+
+  @Test
+  void getFromCommandlineArgsWithProcessHandle() throws Exception {
+    when(system.attemptGetCommandLineViaReflection())
+        .thenReturn(
+            "/bin/java sweet-spring.jar --spring.application.name=tiger-town --quiet=never");
+    SpringBootServiceNameGuesser guesser = new SpringBootServiceNameGuesser(system);
+    Resource result = guesser.createResource(config);
+    expectServiceName(result, "tiger-town");
+  }
+
+  @Test
+  void getFromCommandlineArgsWithSystemProperty() throws Exception {
+    when(system.getProperty("sun.java.command"))
+        .thenReturn("/bin/java sweet-spring.jar --spring.application.name=bullpen --quiet=never");
+    SpringBootServiceNameGuesser guesser = new SpringBootServiceNameGuesser(system);
+    Resource result = guesser.createResource(config);
+    expectServiceName(result, "bullpen");
+  }
+
+  private static void expectServiceName(Resource result, String expected) {
+    assertThat(result.getAttribute(SERVICE_NAME)).isEqualTo(expected);
+  }
+
+  private static void writeString(Path path, String value) throws Exception {
+    try(OutputStream out = Files.newOutputStream(path)){
+      out.write(value.getBytes(StandardCharsets.UTF_8));
+    }
+  }
+
+  private static String readString(Path path) throws Exception {
+    StringBuilder buff = new StringBuilder();
+    try (InputStream in = Files.newInputStream(path)) {
+      BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+      char[] block = new char[1024];
+      int rc;
+      while((rc = reader.read(block)) > 0){
+        buff.append(block, 0, rc);
+      }
+      return buff.toString();
+    }
+  }
+}
