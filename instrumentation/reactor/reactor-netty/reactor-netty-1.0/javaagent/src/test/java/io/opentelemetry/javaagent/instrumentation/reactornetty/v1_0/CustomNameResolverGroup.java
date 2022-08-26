892fb8a38e4e3e8c7817c5836fe3902e9188fd3d
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2022-08-26 10:37:33 +0200

    Refactor Reactor-Netty 1.0 tests to Java (#6497)

---
+++ instrumentation/reactor/reactor-netty/reactor-netty-1.0/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/reactornetty/v1_0/CustomNameResolverGroup.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.reactornetty.v1_0;
+
+import static java.util.Collections.singletonList;
+
+import io.netty.resolver.AddressResolver;
+import io.netty.resolver.AddressResolverGroup;
+import io.netty.resolver.InetNameResolver;
+import io.netty.util.concurrent.EventExecutor;
+import io.netty.util.concurrent.Promise;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.util.List;
+
+public class CustomNameResolverGroup extends AddressResolverGroup<InetSocketAddress> {
+
+  public static final CustomNameResolverGroup INSTANCE = new CustomNameResolverGroup();
+
+  private CustomNameResolverGroup() {}
+
+  @Override
+  protected AddressResolver<InetSocketAddress> newResolver(EventExecutor executor) {
+    return new CustomNameResolver(executor).asAddressResolver();
+  }
+
+  private static class CustomNameResolver extends InetNameResolver {
+
+    private CustomNameResolver(EventExecutor executor) {
+      super(executor);
+    }
+
+    @Override
+    protected void doResolve(String inetHost, Promise<InetAddress> promise) {
+      try {
+        promise.setSuccess(InetAddress.getByName(inetHost));
+      } catch (UnknownHostException exception) {
+        promise.setFailure(exception);
+      }
+    }
+
+    @Override
+    protected void doResolveAll(String inetHost, Promise<List<InetAddress>> promise) {
+      try {
+        // default implementation calls InetAddress.getAllByName
+        promise.setSuccess(singletonList(InetAddress.getByName(inetHost)));
+      } catch (UnknownHostException exception) {
+        promise.setFailure(exception);
+      }
+    }
+  }
+}
