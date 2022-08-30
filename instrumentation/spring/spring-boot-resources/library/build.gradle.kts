b5f6bc31e11d22dbc62cda906a5735a4f3e2dcf0
Jason Plumb <jplumb@splunk.com>
2022-08-30 11:31:36 -0700

    group for readability

--- instrumentation/spring/spring-boot-resources/library/build.gradle.kts
+++ instrumentation/spring/spring-boot-resources/library/build.gradle.kts
@@ -4,9 +4,12 @@ plugins {
 
 dependencies {
   compileOnly("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure-spi")
+
   annotationProcessor("com.google.auto.service:auto-service")
   compileOnly("com.google.auto.service:auto-service-annotations")
-  implementation("org.yaml:snakeyaml:1.31")
   testCompileOnly("com.google.auto.service:auto-service-annotations")
+
+  implementation("org.yaml:snakeyaml:1.31")
+
   testImplementation("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure-spi")
 }
