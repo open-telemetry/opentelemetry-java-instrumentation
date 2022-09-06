65b12c7e1252354fecaef125c3f0221ccff3ccb2
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-09-06 13:20:40 -0700

    Update logback test dependencies

--- instrumentation/logback/logback-appender-1.0/javaagent/build.gradle.kts
+++ instrumentation/logback/logback-appender-1.0/javaagent/build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
   // pin the version strictly to avoid overriding by dependencyManagement versions
   compileOnly("ch.qos.logback:logback-classic") {
     version {
-      strictly("0.9.16")
+      strictly("1.0.0")
     }
   }
   compileOnly("org.slf4j:slf4j-api") {
@@ -30,11 +30,9 @@ dependencies {
   if (findProperty("testLatestDeps") as Boolean) {
     testImplementation("ch.qos.logback:logback-classic:+")
   } else {
-    // TODO these versions are actually used during test
-    // currently our tests fail for logback-classic 0.9.16
     testImplementation("ch.qos.logback:logback-classic") {
       version {
-        strictly("1.2.11")
+        strictly("1.0.0")
       }
     }
     testImplementation("org.slf4j:slf4j-api") {
--- instrumentation/logback/logback-appender-1.0/library/build.gradle.kts
+++ instrumentation/logback/logback-appender-1.0/library/build.gradle.kts
@@ -9,28 +9,26 @@ dependencies {
   // pin the version strictly to avoid overriding by dependencyManagement versions
   compileOnly("ch.qos.logback:logback-classic") {
     version {
-      strictly("0.9.16")
+      strictly("1.0.0")
     }
   }
   compileOnly("org.slf4j:slf4j-api") {
     version {
-      strictly("1.5.8")
+      strictly("1.6.4")
     }
   }
 
   if (findProperty("testLatestDeps") as Boolean) {
     testImplementation("ch.qos.logback:logback-classic:+")
   } else {
-    // TODO these versions are actually used during test
-    // currently our tests fail for logback-classic 0.9.16
     testImplementation("ch.qos.logback:logback-classic") {
       version {
-        strictly("1.2.11")
+        strictly("1.0.0")
       }
     }
     testImplementation("org.slf4j:slf4j-api") {
       version {
-        strictly("1.7.36")
+        strictly("1.6.4")
       }
     }
   }
