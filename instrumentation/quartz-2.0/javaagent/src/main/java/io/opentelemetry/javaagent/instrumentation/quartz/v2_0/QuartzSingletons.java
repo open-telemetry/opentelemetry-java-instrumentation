0ad6e0486788bd43a88fb14eb614bcded833a20f
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-09-11 18:04:28 -0700

    Add experimental span attribute job.system

--- instrumentation/quartz-2.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/quartz/v2_0/QuartzSingletons.java
+++ instrumentation/quartz-2.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/quartz/v2_0/QuartzSingletons.java
@@ -6,11 +6,17 @@
 package io.opentelemetry.javaagent.instrumentation.quartz.v2_0;
 
 import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.instrumentation.api.internal.ConfigPropertiesUtil;
 import io.opentelemetry.instrumentation.quartz.v2_0.QuartzTelemetry;
 
 public final class QuartzSingletons {
 
-  public static final QuartzTelemetry TELEMETRY = QuartzTelemetry.create(GlobalOpenTelemetry.get());
+  public static final QuartzTelemetry TELEMETRY =
+      QuartzTelemetry.builder(GlobalOpenTelemetry.get())
+          .setCaptureExperimentalSpanAttributes(
+              ConfigPropertiesUtil.getBoolean(
+                  "otel.instrumentation.quartz.experimental-span-attributes", false))
+          .build();
 
   private QuartzSingletons() {}
 }
