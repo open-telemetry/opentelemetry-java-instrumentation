35c2e214db090bf6819775a9eedaafeab0a77537
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-09-12 07:36:07 -0700

    Move extractFromRequest() to internal (#6585)
    * Move extractFromRequest() to internal
    
    * Javadoc

--- instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/internal/PropagatorBasedSpanLinksExtractor.java
+++ instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/internal/PropagatorBasedSpanLinksExtractor.java
@@ -3,18 +3,27 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.opentelemetry.instrumentation.api.instrumenter;
+package io.opentelemetry.instrumentation.api.internal;
 
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.context.propagation.TextMapGetter;
 import io.opentelemetry.context.propagation.TextMapPropagator;
+import io.opentelemetry.instrumentation.api.instrumenter.SpanLinksBuilder;
+import io.opentelemetry.instrumentation.api.instrumenter.SpanLinksExtractor;
 
-final class PropagatorBasedSpanLinksExtractor<REQUEST> implements SpanLinksExtractor<REQUEST> {
+/**
+ * This class is internal and is hence not for public use. Its APIs are unstable and can change at
+ * any time.
+ */
+public final class PropagatorBasedSpanLinksExtractor<REQUEST>
+    implements SpanLinksExtractor<REQUEST> {
+
   private final TextMapPropagator propagator;
   private final TextMapGetter<REQUEST> getter;
 
-  PropagatorBasedSpanLinksExtractor(TextMapPropagator propagator, TextMapGetter<REQUEST> getter) {
+  public PropagatorBasedSpanLinksExtractor(
+      TextMapPropagator propagator, TextMapGetter<REQUEST> getter) {
     this.propagator = propagator;
     this.getter = getter;
   }
