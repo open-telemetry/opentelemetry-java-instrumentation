046d7c1e98f9ff9501557cd9f9bfa59f37ba3545
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2022-09-12 13:53:31 +0200

    spotless

--- javaagent/src/test/java/jvmbootstraptest/LogLevelChecker.java
+++ javaagent/src/test/java/jvmbootstraptest/LogLevelChecker.java
@@ -11,8 +11,7 @@ public class LogLevelChecker {
   public static void main(String[] args) {
 
     String str =
-        System.getProperty(
-            "io.opentelemetry.javaagent.slf4j.simpleLogger.defaultLogLevel");
+        System.getProperty("io.opentelemetry.javaagent.slf4j.simpleLogger.defaultLogLevel");
 
     if ((str == null) || (str != null && !str.equalsIgnoreCase("debug"))) {
       throw new IllegalStateException("debug mode not set");
