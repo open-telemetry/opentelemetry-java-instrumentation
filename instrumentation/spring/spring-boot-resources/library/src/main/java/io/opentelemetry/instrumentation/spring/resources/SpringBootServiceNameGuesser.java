32352d677d5e12305cdfffe1e89adf2ade1a0341
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2022-09-13 14:21:34 +0200

    spellcheck

--- instrumentation/spring/spring-boot-resources/library/src/main/java/io/opentelemetry/instrumentation/spring/resources/SpringBootServiceNameGuesser.java
+++ instrumentation/spring/spring-boot-resources/library/src/main/java/io/opentelemetry/instrumentation/spring/resources/SpringBootServiceNameGuesser.java
@@ -99,7 +99,7 @@ public class SpringBootServiceNameGuesser implements ConditionalResourceProvider
   @Override
   public boolean shouldApply(ConfigProperties config, Resource resource) {
     // we're skipping this provider if the service name was manually set by the user -- no need to
-    // waste time trying to compute the service name if it's going to be overriden anyway
+    // waste time trying to compute the service name if it's going to be overridden anyway
     String serviceName = config.getString("otel.service.name");
     Map<String, String> resourceAttributes = config.getMap("otel.resource.attributes");
     return serviceName == null
