f200f9bb7d5f89b375f8722bb3de8f62f308d1ca
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2022-09-14 06:41:13 +0200

    Fix latest dep builds (#6603)
    * Fix latest dep builds
    
    * fix couchbase 3.2 too
    
    * codenarc
    
    * fix jedis 4.0 tests
    
    * fix restlet 2.0 tests
    
    * fix armeria tests
    
    * Update instrumentation/couchbase/couchbase-3.2/javaagent/src/test/groovy/CouchbaseClient32Test.groovy
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>

--- instrumentation/restlet/restlet-2.0/library/src/main/java/io/opentelemetry/instrumentation/restlet/v2_0/internal/RestletNetAttributesGetter.java
+++ instrumentation/restlet/restlet-2.0/library/src/main/java/io/opentelemetry/instrumentation/restlet/v2_0/internal/RestletNetAttributesGetter.java
@@ -5,14 +5,74 @@
 
 package io.opentelemetry.instrumentation.restlet.v2_0.internal;
 
+import static java.lang.invoke.MethodType.methodType;
+
 import io.opentelemetry.instrumentation.api.instrumenter.net.NetServerAttributesGetter;
 import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
 import javax.annotation.Nullable;
 import org.restlet.Request;
-import org.restlet.engine.http.HttpRequest;
-import org.restlet.engine.http.ServerCall;
 
 final class RestletNetAttributesGetter implements NetServerAttributesGetter<Request> {
+
+  private static final Class<?> HTTP_REQUEST_CLASS;
+  private static final MethodHandle GET_HTTP_CALL;
+  private static final MethodHandle GET_HOST_DOMAIN;
+  private static final MethodHandle GET_SERVER_PORT;
+  private static final MethodHandle GET_SERVER_ADDRESS;
+
+  static {
+    Class<?> httpRequestClass = null;
+    Class<?> serverCallClass = null;
+    MethodHandle getHttpCall = null;
+    MethodHandle getHostDomain = null;
+    MethodHandle getServerPort = null;
+    MethodHandle getServerAddress = null;
+
+    try {
+      httpRequestClass = Class.forName("org.restlet.engine.http.HttpRequest");
+    } catch (ClassNotFoundException e) {
+      // moved to another package in version 2.4
+      try {
+        httpRequestClass = Class.forName("org.restlet.engine.adapter.HttpRequest");
+      } catch (ClassNotFoundException ex) {
+        // ignored
+      }
+    }
+
+    try {
+      serverCallClass = Class.forName("org.restlet.engine.http.ServerCall");
+    } catch (ClassNotFoundException e) {
+      // moved to another package in version 2.4
+      try {
+        serverCallClass = Class.forName("org.restlet.engine.adapter.ServerCall");
+      } catch (ClassNotFoundException ex) {
+        // ignored
+      }
+    }
+
+    if (httpRequestClass != null && serverCallClass != null) {
+      try {
+        MethodHandles.Lookup lookup = MethodHandles.publicLookup();
+        getHttpCall =
+            lookup.findVirtual(httpRequestClass, "getHttpCall", methodType(serverCallClass));
+        getHostDomain =
+            lookup.findVirtual(serverCallClass, "getHostDomain", methodType(String.class));
+        getServerPort = lookup.findVirtual(serverCallClass, "getServerPort", methodType(int.class));
+        getServerAddress =
+            lookup.findVirtual(serverCallClass, "getServerAddress", methodType(String.class));
+      } catch (NoSuchMethodException | IllegalAccessException e) {
+        // ignored
+      }
+    }
+
+    HTTP_REQUEST_CLASS = httpRequestClass;
+    GET_HTTP_CALL = getHttpCall;
+    GET_HOST_DOMAIN = getHostDomain;
+    GET_SERVER_PORT = getServerPort;
+    GET_SERVER_ADDRESS = getServerAddress;
+  }
 
   @Override
   public String transport(Request request) {
@@ -22,15 +82,35 @@ final class RestletNetAttributesGetter implements NetServerAttributesGetter<Requ
   @Nullable
   @Override
   public String hostName(Request request) {
-    ServerCall call = serverCall(request);
-    return call == null ? null : call.getHostDomain();
+    if (GET_HOST_DOMAIN == null) {
+      return null;
+    }
+    Object call = serverCall(request);
+    if (call == null) {
+      return null;
+    }
+    try {
+      return (String) GET_HOST_DOMAIN.invoke(call);
+    } catch (Throwable e) {
+      return null;
+    }
   }
 
   @Nullable
   @Override
   public Integer hostPort(Request request) {
-    ServerCall call = serverCall(request);
-    return call == null ? null : call.getServerPort();
+    if (GET_SERVER_PORT == null) {
+      return null;
+    }
+    Object call = serverCall(request);
+    if (call == null) {
+      return null;
+    }
+    try {
+      return (int) GET_SERVER_PORT.invoke(call);
+    } catch (Throwable e) {
+      return null;
+    }
   }
 
   @Override
@@ -47,14 +127,31 @@ final class RestletNetAttributesGetter implements NetServerAttributesGetter<Requ
   @Nullable
   @Override
   public String sockHostAddr(Request request) {
-    ServerCall call = serverCall(request);
-    return call == null ? null : call.getServerAddress();
+    if (GET_SERVER_ADDRESS == null) {
+      return null;
+    }
+    Object call = serverCall(request);
+    if (call == null) {
+      return null;
+    }
+    try {
+      return (String) GET_SERVER_ADDRESS.invoke(call);
+    } catch (Throwable e) {
+      return null;
+    }
   }
 
   @Nullable
-  private static ServerCall serverCall(Request request) {
-    if (request instanceof HttpRequest) {
-      return ((HttpRequest) request).getHttpCall();
+  private static Object serverCall(Request request) {
+    if (GET_HTTP_CALL == null || HTTP_REQUEST_CLASS == null) {
+      return null;
+    }
+    if (HTTP_REQUEST_CLASS.isInstance(request)) {
+      try {
+        return GET_HTTP_CALL.invoke(request);
+      } catch (Throwable e) {
+        return null;
+      }
     }
     return null;
   }
