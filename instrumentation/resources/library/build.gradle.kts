6b607c1e18e6936f7a7eef46a3be77892cef9b3a
jack-berg <34418638+jack-berg@users.noreply.github.com>
2022-09-14 17:45:24 -0500

    Add resource providers (#6574)
    * Add resource providers
    
    * Use autoservice annotation

---
+++ instrumentation/resources/library/build.gradle.kts
@@ -0,0 +1,64 @@
+plugins {
+  id("otel.library-instrumentation")
+  id("otel.animalsniffer-conventions")
+}
+
+val mrJarVersions = listOf(11)
+
+dependencies {
+  implementation("io.opentelemetry:opentelemetry-sdk-common")
+  implementation("io.opentelemetry:opentelemetry-semconv")
+  implementation("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure-spi")
+
+  compileOnly("org.codehaus.mojo:animal-sniffer-annotations")
+
+  annotationProcessor("com.google.auto.service:auto-service")
+  compileOnly("com.google.auto.service:auto-service-annotations")
+
+  testImplementation("org.junit.jupiter:junit-jupiter-api")
+}
+
+for (version in mrJarVersions) {
+  sourceSets {
+    create("java$version") {
+      java {
+        setSrcDirs(listOf("src/main/java$version"))
+      }
+    }
+  }
+
+  tasks {
+    named<JavaCompile>("compileJava${version}Java") {
+      sourceCompatibility = "$version"
+      targetCompatibility = "$version"
+      options.release.set(version)
+    }
+  }
+
+  configurations {
+    named("java${version}Implementation") {
+      extendsFrom(configurations["implementation"])
+    }
+    named("java${version}CompileOnly") {
+      extendsFrom(configurations["compileOnly"])
+    }
+  }
+
+  dependencies {
+    // Common to reference classes in main sourceset from Java 9 one (e.g., to return a common interface)
+    add("java${version}Implementation", files(sourceSets.main.get().output.classesDirs))
+  }
+}
+
+tasks {
+  withType(Jar::class) {
+    for (version in mrJarVersions) {
+      into("META-INF/versions/$version") {
+        from(sourceSets["java$version"].output)
+      }
+    }
+    manifest.attributes(
+      "Multi-Release" to "true"
+    )
+  }
+}
---
+++ instrumentation/resources/library/src/main/java/io/opentelemetry/instrumentation/resources/ProcessRuntimeResource.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.instrumentation.resources;
+
+import static io.opentelemetry.semconv.resource.attributes.ResourceAttributes.PROCESS_RUNTIME_DESCRIPTION;
+import static io.opentelemetry.semconv.resource.attributes.ResourceAttributes.PROCESS_RUNTIME_NAME;
+import static io.opentelemetry.semconv.resource.attributes.ResourceAttributes.PROCESS_RUNTIME_VERSION;
+
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
+
+/** Factory of a {@link Resource} which provides information about the Java runtime. */
+public final class ProcessRuntimeResource {
+
+  private static final Resource INSTANCE = buildResource();
+
+  /** Returns a factory for a {@link Resource} which provides information about the Java runtime. */
+  public static Resource get() {
+    return INSTANCE;
+  }
+
+  // Visible for testing
+  static Resource buildResource() {
+    try {
+      String name = System.getProperty("java.runtime.name");
+      String version = System.getProperty("java.runtime.version");
+      String description =
+          System.getProperty("java.vm.vendor")
+              + " "
+              + System.getProperty("java.vm.name")
+              + " "
+              + System.getProperty("java.vm.version");
+
+      return Resource.create(
+          Attributes.of(
+              PROCESS_RUNTIME_NAME,
+              name,
+              PROCESS_RUNTIME_VERSION,
+              version,
+              PROCESS_RUNTIME_DESCRIPTION,
+              description),
+          ResourceAttributes.SCHEMA_URL);
+    } catch (SecurityException ignored) {
+      return Resource.empty();
+    }
+  }
+
+  private ProcessRuntimeResource() {}
+}
