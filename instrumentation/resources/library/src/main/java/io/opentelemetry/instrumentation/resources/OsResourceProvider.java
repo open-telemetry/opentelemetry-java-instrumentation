bc1e1798087c1aead2e73f8ca7622316cd0b99c8
jack-berg <34418638+jack-berg@users.noreply.github.com>
2022-09-14 17:45:24 -0500

    Add resource providers (#6574)
    * Add resource providers
    
    * Use autoservice annotation

---
+++ instrumentation/resources/library/src/main/java/io/opentelemetry/instrumentation/resources/OsResourceProvider.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.instrumentation.resources;
+
+import com.google.auto.service.AutoService;
+import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
+import io.opentelemetry.sdk.autoconfigure.spi.ResourceProvider;
+import io.opentelemetry.sdk.resources.Resource;
+
+/** {@link ResourceProvider} for automatically configuring {@link OsResource}. */
+@AutoService(ResourceProvider.class)
+public final class OsResourceProvider implements ResourceProvider {
+  @Override
+  public Resource createResource(ConfigProperties config) {
+    return OsResource.get();
+  }
+}
