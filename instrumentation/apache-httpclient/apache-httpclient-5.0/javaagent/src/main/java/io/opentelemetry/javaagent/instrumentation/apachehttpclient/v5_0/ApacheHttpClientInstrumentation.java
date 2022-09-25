230755aceb39e032c97f2d807296093da13ed16e
Anurag Agarwal <anuragagarwal561994@users.noreply.github.com>
2022-09-25 10:51:30 +0530

    Apply spotless checks

--- instrumentation/apache-httpclient/apache-httpclient-5.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/apachehttpclient/v5_0/ApacheHttpClientInstrumentation.java
+++ instrumentation/apache-httpclient/apache-httpclient-5.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/apachehttpclient/v5_0/ApacheHttpClientInstrumentation.java
@@ -92,7 +92,8 @@ class ApacheHttpClientInstrumentation implements TypeInstrumentation {
       }
 
       scope.close();
-      HttpRequest httpRequest = (HttpRequest) httpContext.getAttribute(HttpCoreContext.HTTP_REQUEST);
+      HttpRequest httpRequest =
+          (HttpRequest) httpContext.getAttribute(HttpCoreContext.HTTP_REQUEST);
       ApacheHttpClientHelper.doMethodExit(context, httpRequest, result, throwable);
     }
   }
