4057dcc04a2e1124d22f18a49e052aa5ce3635c5
yingziisme <yingzi_is_sun@163.com>
2022-09-27 01:03:31 +0800

    Fix testLatestDeps in dubbo instrumentation (#6754)
    Co-authored-by: Mateusz Rzeszutek <mrzeszutek@splunk.com>

--- instrumentation/apache-dubbo-2.7/library-autoconfigure/src/test/groovy/io/opentelemetry/instrumentation/apachedubbo/v2_7/DubboTraceChainTest.groovy
+++ instrumentation/apache-dubbo-2.7/library-autoconfigure/src/test/groovy/io/opentelemetry/instrumentation/apachedubbo/v2_7/DubboTraceChainTest.groovy
@@ -6,9 +6,6 @@
 package io.opentelemetry.instrumentation.apachedubbo.v2_7
 
 import io.opentelemetry.instrumentation.test.LibraryTestTrait
-import spock.lang.IgnoreIf
 
-// TODO (trask) fix the test on latest version of dubbo
-@IgnoreIf({ Boolean.getBoolean("testLatestDeps") })
 class DubboTraceChainTest extends AbstractDubboTraceChainTest implements LibraryTestTrait {
 }
