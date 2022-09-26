619ed8b15ffb3e43108d71480f1603b22b1075d5
yingziisme <yingzi_is_sun@163.com>
2022-09-27 01:03:31 +0800

    Fix testLatestDeps in dubbo instrumentation (#6754)
    Co-authored-by: Mateusz Rzeszutek <mrzeszutek@splunk.com>

--- instrumentation/apache-dubbo-2.7/library-autoconfigure/build.gradle.kts
+++ instrumentation/apache-dubbo-2.7/library-autoconfigure/build.gradle.kts
@@ -19,6 +19,4 @@ tasks.withType<Test>().configureEach {
   jvmArgs("--add-opens=java.base/java.math=ALL-UNNAMED")
   // required on jdk17
   jvmArgs("--add-opens=java.base/java.lang=ALL-UNNAMED")
-
-  systemProperty("testLatestDeps", findProperty("testLatestDeps") as Boolean)
 }
