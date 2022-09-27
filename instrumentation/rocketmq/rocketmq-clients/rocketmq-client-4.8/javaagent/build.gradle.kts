48ed7cfe70e31a756549b0d849df616a1cf3a9f3
Aaron Ai <yangkun.ayk@gmail.com>
2022-09-27 14:39:20 +0800

    Move rocketmq-client-4.8 to 'rocketmq-clients'

---
+++ instrumentation/rocketmq/rocketmq-clients/rocketmq-client-4.8/javaagent/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+  id("otel.javaagent-instrumentation")
+}
+
+muzzle {
+  pass {
+    group.set("org.apache.rocketmq")
+    module.set("rocketmq-client")
+    versions.set("[4.0.0,)")
+    assertInverse.set(true)
+  }
+}
+
+dependencies {
+  library("org.apache.rocketmq:rocketmq-client:4.8.0")
+
+  implementation(project(":instrumentation:rocketmq:rocketmq-clients:rocketmq-client-4.8:library"))
+
+  testImplementation(project(":instrumentation:rocketmq:rocketmq-clients:rocketmq-client-4.8:testing"))
+
+  testLibrary("org.apache.rocketmq:rocketmq-test:4.8.0")
+}
+
+tasks.withType<Test>().configureEach {
+  jvmArgs("-Dotel.instrumentation.rocketmq-client.experimental-span-attributes=true")
+
+  systemProperty("testLatestDeps", findProperty("testLatestDeps") as Boolean)
+}
---
+++ instrumentation/rocketmq/rocketmq-clients/rocketmq-client-4.8/library/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+  id("otel.library-instrumentation")
+}
+
+dependencies {
+  library("org.apache.rocketmq:rocketmq-client:4.8.0")
+
+  compileOnly("com.google.auto.value:auto-value-annotations")
+  annotationProcessor("com.google.auto.value:auto-value")
+
+  testLibrary("org.apache.rocketmq:rocketmq-test:4.8.0")
+
+  testImplementation(project(":instrumentation:rocketmq:rocketmq-clients:rocketmq-client-4.8:testing"))
+}
+
+tasks.withType<Test>().configureEach {
+  systemProperty("testLatestDeps", findProperty("testLatestDeps") as Boolean)
+}
