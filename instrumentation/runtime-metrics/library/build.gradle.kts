0ca7a0d9957d10b571755f8f2a0a31de28701521
Peter Findeisen <pfindeis@cisco.com>
2022-09-28 10:34:29 -0700

    Cleanup in runtime-metrics.

--- instrumentation/runtime-metrics/library/build.gradle.kts
+++ instrumentation/runtime-metrics/library/build.gradle.kts
@@ -3,10 +3,6 @@ plugins {
 }
 
 dependencies {
-  implementation("org.yaml:snakeyaml")
-
-  compileOnly("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure")
-
   testImplementation("io.opentelemetry:opentelemetry-sdk-metrics")
   testImplementation(project(":testing-common"))
 }
