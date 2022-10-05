e84ada291b1a06bf1203225891b46ff62523d889
Jack Berg <jberg@newrelic.com>
2022-10-05 17:06:08 -0500

    Drop auto value dependency

--- instrumentation/netty/netty-4-common/javaagent/build.gradle.kts
+++ instrumentation/netty/netty-4-common/javaagent/build.gradle.kts
@@ -3,9 +3,6 @@ plugins {
 }
 
 dependencies {
-  compileOnly("com.google.auto.value:auto-value-annotations")
-  annotationProcessor("com.google.auto.value:auto-value")
-
   implementation(project(":instrumentation:netty:netty-4-common:library"))
   implementation(project(":instrumentation:netty:netty-common:library"))
 
