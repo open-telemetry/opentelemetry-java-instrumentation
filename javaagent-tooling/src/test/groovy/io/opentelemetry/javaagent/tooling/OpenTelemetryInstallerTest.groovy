b7b871feafaf64990834ed40d42065e9256b2ebb
Jack Berg <jberg@newrelic.com>
2022-10-07 15:50:29 -0500

    Fix build

--- javaagent-tooling/src/test/groovy/io/opentelemetry/javaagent/tooling/OpenTelemetryInstallerTest.groovy
+++ javaagent-tooling/src/test/groovy/io/opentelemetry/javaagent/tooling/OpenTelemetryInstallerTest.groovy
@@ -8,19 +8,18 @@ package io.opentelemetry.javaagent.tooling
 import io.opentelemetry.api.GlobalOpenTelemetry
 import io.opentelemetry.api.OpenTelemetry
 import io.opentelemetry.api.logs.GlobalLoggerProvider
-import io.opentelemetry.api.logs.LoggerProvider
 import spock.lang.Specification
 
 class OpenTelemetryInstallerTest extends Specification {
 
   void setup() {
     GlobalOpenTelemetry.resetForTest()
-    GlobalLoggerProvider.set(LoggerProvider.noop())
+    GlobalLoggerProvider.resetForTest()
   }
 
   void cleanup() {
     GlobalOpenTelemetry.resetForTest()
-    GlobalLoggerProvider.set(LoggerProvider.noop())
+    GlobalLoggerProvider.resetForTest()
   }
 
   def "should initialize GlobalOpenTelemetry"() {
