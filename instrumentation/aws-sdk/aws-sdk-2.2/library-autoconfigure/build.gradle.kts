5c2c86c65da7787f1ab5e630d8aef0c5806a7221
Lauri Tulmin <ltulmin@splunk.com>
2022-10-23 20:33:48 +0300

    Run aws-sdk tests against 2.18.x (#6956)
    Resolves
    https://github.com/open-telemetry/opentelemetry-java-instrumentation/issues/6945

--- instrumentation/aws-sdk/aws-sdk-2.2/library-autoconfigure/build.gradle.kts
+++ instrumentation/aws-sdk/aws-sdk-2.2/library-autoconfigure/build.gradle.kts
@@ -12,18 +12,19 @@ dependencies {
 
   testImplementation(project(":instrumentation:aws-sdk:aws-sdk-2.2:testing"))
 
-  latestDepTestLibrary("software.amazon.awssdk:aws-core:2.17.+")
-  latestDepTestLibrary("software.amazon.awssdk:aws-json-protocol:2.17.+")
-  latestDepTestLibrary("software.amazon.awssdk:dynamodb:2.17.+")
-  latestDepTestLibrary("software.amazon.awssdk:ec2:2.17.+")
-  latestDepTestLibrary("software.amazon.awssdk:kinesis:2.17.+")
-  latestDepTestLibrary("software.amazon.awssdk:rds:2.17.+")
-  latestDepTestLibrary("software.amazon.awssdk:s3:2.17.+")
-  latestDepTestLibrary("software.amazon.awssdk:sqs:2.17.+")
+  latestDepTestLibrary("software.amazon.awssdk:aws-core:+")
+  latestDepTestLibrary("software.amazon.awssdk:aws-json-protocol:+")
+  latestDepTestLibrary("software.amazon.awssdk:dynamodb:+")
+  latestDepTestLibrary("software.amazon.awssdk:ec2:+")
+  latestDepTestLibrary("software.amazon.awssdk:kinesis:+")
+  latestDepTestLibrary("software.amazon.awssdk:rds:+")
+  latestDepTestLibrary("software.amazon.awssdk:s3:+")
+  latestDepTestLibrary("software.amazon.awssdk:sqs:+")
 }
 
 tasks {
   test {
+    systemProperty("testLatestDeps", findProperty("testLatestDeps") as Boolean)
     systemProperty("otel.instrumentation.aws-sdk.experimental-span-attributes", true)
   }
 }
