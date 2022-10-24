1e324bb3038624138d22f2334e271432120259ce
Aaron Ai <yangkun.ayk@gmail.com>
2022-10-24 17:39:47 +0800

    Add comment about not using earlier versions

--- instrumentation/rocketmq/rocketmq-client/rocketmq-client-java-5.0/testing/build.gradle.kts
+++ instrumentation/rocketmq/rocketmq-client/rocketmq-client-java-5.0/testing/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 dependencies {
   api(project(":testing-common"))
 
+  // earlier versions have bugs that may make tests flaky.
   implementation("org.apache.rocketmq:rocketmq-client-java:5.0.2")
   implementation("org.testcontainers:testcontainers:1.17.5")
   implementation("io.opentelemetry:opentelemetry-api")
