807101d999ae1d7b7e2b67e6abdc86f7b89cb7ea
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-10-25 12:03:48 -0700

    Remove usage of deprecated docker image

--- smoke-tests/images/grpc/build.gradle.kts
+++ smoke-tests/images/grpc/build.gradle.kts
@@ -28,7 +28,15 @@ val targetJDK = project.findProperty("targetJDK") ?: "11"
 val tag = findProperty("tag")
   ?: DateTimeFormatter.ofPattern("yyyyMMdd.HHmmSS").format(LocalDateTime.now())
 
+java {
+  // this is needed to avoid jib failing with
+  // "Your project is using Java 17 but the base image is for Java 8"
+  // (it seems the jib plugins does not understand toolchains yet)
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
+}
+
 jib {
-  from.image = "openjdk:$targetJDK"
+  from.image = "eclipse-temurin:$targetJDK"
   to.image = "ghcr.io/open-telemetry/opentelemetry-java-instrumentation/smoke-test-grpc:jdk$targetJDK-$tag"
 }
