70242b04809ddd626ca518b52e7dd98e94c9385f
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-10-27 14:28:26 -0700

    Update dependencies (#7000)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,12 @@
 pluginManagement {
   plugins {
-    id("com.bmuschko.docker-remote-api") version "7.3.0"
-    id("com.github.ben-manes.versions") version "0.42.0"
+    id("com.bmuschko.docker-remote-api") version "8.1.0"
+    id("com.github.ben-manes.versions") version "0.43.0"
     id("com.github.jk1.dependency-license-report") version "2.1"
-    id("com.google.cloud.tools.jib") version "3.2.1"
+    id("com.google.cloud.tools.jib") version "3.3.0"
     id("com.gradle.plugin-publish") version "1.0.0"
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
-    id("org.jetbrains.kotlin.jvm") version "1.6.20"
+    id("org.jetbrains.kotlin.jvm") version "1.7.20"
     id("org.unbroken-dome.test-sets") version "4.0.0"
     id("org.xbib.gradle.plugin.jflex") version "1.6.0"
     id("org.unbroken-dome.xjc") version "2.0.0"
