252be4593b0a97a8fa9ff58d67a9d28e23f205c1
Peter Findeisen <pfindeis@cisco.com>
2022-10-31 12:14:06 -0700

    Refining comments

--- instrumentation/jmx-metrics/library/src/main/java/io/opentelemetry/instrumentation/jmx/engine/MetricAttributeExtractor.java
+++ instrumentation/jmx-metrics/library/src/main/java/io/opentelemetry/instrumentation/jmx/engine/MetricAttributeExtractor.java
@@ -14,5 +14,14 @@ import javax.management.ObjectName;
  */
 public interface MetricAttributeExtractor {
 
+  /**
+   * Provide a String value to be used as the value of a metric attribute.
+   *
+   * @param server MBeanServer to query, must not be null if the extraction is from an MBean
+   *     attribute
+   * @param objectName the identifier of the MBean to query, must not be null if the extraction is
+   *     from an MBean attribute, or from the ObjectName parameter
+   * @return the value of the attribute, can be null if extraction failed
+   */
   String extractValue(MBeanServer server, ObjectName objectName);
 }
