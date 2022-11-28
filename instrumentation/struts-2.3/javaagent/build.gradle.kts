c336bf4cb184ff1269637ff5a0a170898b5ebef5
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-11-28 11:21:53 -0800

    Add test latest dep limit for struts2 (#7322)

--- instrumentation/struts-2.3/javaagent/build.gradle.kts
+++ instrumentation/struts-2.3/javaagent/build.gradle.kts
@@ -24,4 +24,6 @@ dependencies {
   testInstrumentation(project(":instrumentation:servlet:servlet-3.0:javaagent"))
   testInstrumentation(project(":instrumentation:servlet:servlet-javax-common:javaagent"))
   testInstrumentation(project(":instrumentation:jetty:jetty-8.0:javaagent"))
+
+  latestDepTestLibrary("org.apache.struts:struts2-core:6.0.+")
 }
