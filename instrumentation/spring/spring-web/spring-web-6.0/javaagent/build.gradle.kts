7b8742357113a86f0e10ce2e6ae70d587e7cf980
Lauri Tulmin <ltulmin@splunk.com>
2022-12-16 00:02:22 +0200

    Skip muzzle for broken spring-web versions (#7435)

--- instrumentation/spring/spring-web/spring-web-6.0/javaagent/build.gradle.kts
+++ instrumentation/spring/spring-web/spring-web-6.0/javaagent/build.gradle.kts
@@ -7,6 +7,8 @@ muzzle {
     group.set("org.springframework")
     module.set("spring-web")
     versions.set("[6.0.0,)")
+    // these versions depend on javax.faces:jsf-api:1.1 which was released as pom only
+    skip("1.2.1", "1.2.2", "1.2.3", "1.2.4")
     assertInverse.set(true)
   }
 }
