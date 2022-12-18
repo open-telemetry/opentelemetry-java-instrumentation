a65a61207976506675f5503fa4d8fe2d5c46f812
Lauri Tulmin <ltulmin@splunk.com>
2022-12-18 22:04:47 +0200

    Convert vaadin tests to java

---
+++ instrumentation/vaadin-14.2/javaagent/src/vaadin14LatestTest/java/test/io/opentelemetry/javaagent/instrumentation/vaadin/Vaadin14LatestTest.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package test.io.opentelemetry.javaagent.instrumentation.vaadin;
+
+import io.opentelemetry.javaagent.instrumentation.vaadin.AbstractVaadin14Test;
+
+public class Vaadin14LatestTest extends AbstractVaadin14Test {}
