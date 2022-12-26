0aa83071e5cef84538cb19f57c6bc8f75b2483b7
Anurag Agarwal <anuragagarwal561994@users.noreply.github.com>
2022-12-26 05:43:39 +0530

    Removes redundant comments

--- instrumentation/apache-httpclient/apache-httpclient-5.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/apachehttpclient/v5_0/ApacheHttpClientContentLengthAttributesGetter.java
+++ instrumentation/apache-httpclient/apache-httpclient-5.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/apachehttpclient/v5_0/ApacheHttpClientContentLengthAttributesGetter.java
@@ -30,18 +30,10 @@ public class ApacheHttpClientContentLengthAttributesGetter
     Context parentContext = request.getParentContext();
     BytesTransferMetrics metrics = getContentLengthMetrics(parentContext);
     if (metrics != null) {
-      // response-length indicates bytes read by the stream, when content-length is present via
-      // header we use that directly, else the value is computed for the bytes read in
-      // even when content-length header is not
-      // present (for example: chunked encoding). Note that we can't calculate this metric for
-      // non async client, because the input stream is lazily consumed, while for async client,
-      // the stream is eagerly consumed and hence it is possible to get the metric.
       Long responseLength = metrics.getResponseContentLength();
       if (responseLength != null) {
         attributes.put(SemanticAttributes.HTTP_RESPONSE_CONTENT_LENGTH, responseLength);
       }
-      // request-length indicates bytes written to the stream even when content-length header is not
-      // present (for example: chunked encoding).
       Long requestLength = metrics.getRequestContentLength();
       if (requestLength != null) {
         attributes.put(SemanticAttributes.HTTP_REQUEST_CONTENT_LENGTH, requestLength);
