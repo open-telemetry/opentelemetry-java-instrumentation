55a7e41beac9ce8299514c58d246fe6c574c2129
Jason Plumb <jplumb@splunk.com>
2023-02-09 14:43:30 -0800

    move tests from groovy to java

---
+++ instrumentation-annotations-support/src/test/java/io/opentelemetry/instrumentation/api/annotation/support/AnnotationReflectionHelperTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.instrumentation.api.annotation.support;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.annotation.Annotation;
+import java.lang.invoke.MethodHandles;
+import java.util.function.Function;
+import org.junit.jupiter.api.Test;
+
+class AnnotationReflectionHelperTest {
+
+  @Test
+  void returnTheAnnotationTypeByName() {
+    Class<? extends Annotation> clazz =
+        AnnotationReflectionHelper.forNameOrNull(
+            AnnotationReflectionHelperTest.class.getClassLoader(),
+            "io.opentelemetry.instrumentation.api.annotation.support.CustomAnnotation");
+    assertThat(clazz).isEqualTo(CustomAnnotation.class);
+  }
+
+  @Test
+  void returnsNullForAnAnnotationNotFound() {
+    Class<? extends Annotation> clazz =
+        AnnotationReflectionHelper.forNameOrNull(
+            AnnotationReflectionHelperTest.class.getClassLoader(),
+            "io.opentelemetry.instrumentation.api.annotation.support.NonExistentAnnotation");
+    assertThat(clazz).isNull();
+  }
+
+  @Test
+  void returnsNullForClassThatIsNotAnnotation() {
+    Class<? extends Annotation> clazz =
+        AnnotationReflectionHelper.forNameOrNull(
+            AnnotationReflectionHelperTest.class.getClassLoader(), "java.util.List");
+    assertThat(clazz).isNull();
+  }
+
+  @Test
+  void returnsBoundFunctionalInterfaceToAnnotationElement() throws Throwable {
+    Function<Annotation, String> function =
+        AnnotationReflectionHelper.bindAnnotationElementMethod(
+            getLookup(), CustomAnnotation.class, "value", String.class);
+    CustomAnnotation annotation = Annotated.class.getDeclaredAnnotation(CustomAnnotation.class);
+
+    String result = function.apply(annotation);
+
+    assertThat(result).isEqualTo("Value");
+  }
+
+  private static MethodHandles.Lookup getLookup() {
+    return MethodHandles.lookup();
+  }
+
+  @CustomAnnotation(value = "Value")
+  static class Annotated {}
+}
