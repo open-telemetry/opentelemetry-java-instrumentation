e5522d43f8918a62ae529fbf25b077bfcfd32278
Oleh Astappiev <astappiev@l3s.de>
2023-02-10 17:24:25 +0100

    Add Mojarra JSF 3 instrumentation

---
+++ instrumentation/jsf/jsf-mojarra-3.0/javaagent/src/mojarra3Test/groovy/Mojarra3Test.groovy
@@ -0,0 +1,11 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+class Mojarra3Test extends BaseJsfTest {
+  @Override
+  String getJsfVersion() {
+    "3"
+  }
+}
---
+++ instrumentation/jsf/jsf-mojarra-3.0/javaagent/src/mojarra4Test/groovy/Mojarra4Test.groovy
@@ -0,0 +1,11 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+class Mojarra4Test extends BaseJsfTest {
+  @Override
+  String getJsfVersion() {
+    "4"
+  }
+}
