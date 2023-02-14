1919eb015190773b6e671d478652c03e6ea6f61d
siyuniu-ms <siyuniu@microsoft.com>
2023-02-14 15:11:41 -0800

    JavaScript snippet injection

---
+++ instrumentation/servlet/servlet-3.0/javaagent-unit-tests/src/test/java/io/opentelemetry/javaagent/instrumentation/servlet/v3_0/snippet/InjectionTest.java
@@ -0,0 +1,184 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.servlet.v3_0.snippet;
+
+import static io.opentelemetry.javaagent.instrumentation.servlet.v3_0.snippet.TestUtil.readFile;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import io.opentelemetry.javaagent.bootstrap.servlet.ExperimentalSnippetHolder;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+import javax.servlet.ServletOutputStream;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class InjectionTest {
+
+  @Test
+  void testInjectionForStringContainHeadTag() throws IOException {
+    String testSnippet = "\n  <script type=\"text/javascript\"> Test </script>";
+    ExperimentalSnippetHolder.setSnippet(testSnippet);
+    // read the originalFile
+    String original = readFile("staticHtmlOrigin.html");
+    // read the correct answer
+    String correct = readFile("staticHtmlAfter.html");
+    byte[] originalBytes = original.getBytes(StandardCharsets.UTF_8);
+    SnippetInjectingResponseWrapper response = mock(SnippetInjectingResponseWrapper.class);
+    when(response.isCommitted()).thenReturn(false);
+    when(response.getCharacterEncoding()).thenReturn(StandardCharsets.UTF_8.name());
+    InjectionState obj = new InjectionState(response);
+
+    StringWriter writer = new StringWriter();
+
+    ServletOutputStream sp =
+        new ServletOutputStream() {
+          @Override
+          public void write(int b) throws IOException {
+            writer.write(b);
+          }
+        };
+    boolean injected =
+        ServletOutputStreamInjectionHelper.handleWrite(
+            originalBytes, 0, originalBytes.length, obj, sp);
+    assertThat(obj.getHeadTagBytesSeen()).isEqualTo(-1);
+    assertThat(injected).isEqualTo(true);
+    writer.flush();
+
+    String result = writer.toString();
+    writer.close();
+    assertThat(result).isEqualTo(correct);
+  }
+
+  @Test
+  @Disabled
+  void testInjectionForChinese() throws IOException {
+    String testSnippet = "\n  <script type=\"text/javascript\"> Test </script>";
+    ExperimentalSnippetHolder.setSnippet(testSnippet);
+    // read the originalFile
+    String original = readFile("staticHtmlChineseOrigin.html");
+    // read the correct answer
+    String correct = readFile("staticHtmlChineseAfter.html");
+    byte[] originalBytes = original.getBytes(StandardCharsets.UTF_8);
+    SnippetInjectingResponseWrapper response = mock(SnippetInjectingResponseWrapper.class);
+    when(response.isCommitted()).thenReturn(false);
+    when(response.getCharacterEncoding()).thenReturn(StandardCharsets.UTF_8.name());
+    InjectionState obj = new InjectionState(response);
+
+    StringWriter writer = new StringWriter();
+
+    ServletOutputStream sp =
+        new ServletOutputStream() {
+          @Override
+          public void write(int b) throws IOException {
+            writer.write(b);
+          }
+        };
+    boolean injected =
+        ServletOutputStreamInjectionHelper.handleWrite(
+            originalBytes, 0, originalBytes.length, obj, sp);
+    assertThat(obj.getHeadTagBytesSeen()).isEqualTo(-1);
+    assertThat(injected).isEqualTo(true);
+    writer.flush();
+
+    String result = writer.toString();
+    writer.close();
+    assertThat(result).isEqualTo(correct);
+  }
+
+  @Test
+  void testInjectionForStringWithoutHeadTag() throws IOException {
+    String testSnippet = "\n  <script type=\"text/javascript\"> Test </script>";
+    ExperimentalSnippetHolder.setSnippet(testSnippet);
+    // read the originalFile
+    String original = readFile("htmlWithoutHeadTag.html");
+
+    byte[] originalBytes = original.getBytes(StandardCharsets.UTF_8);
+    SnippetInjectingResponseWrapper response = mock(SnippetInjectingResponseWrapper.class);
+    when(response.isCommitted()).thenReturn(false);
+    when(response.getCharacterEncoding()).thenReturn(StandardCharsets.UTF_8.name());
+    InjectionState obj = new InjectionState(response);
+    StringWriter writer = new StringWriter();
+
+    ServletOutputStream sp =
+        new ServletOutputStream() {
+          @Override
+          public void write(int b) throws IOException {
+            writer.write(b);
+          }
+        };
+    boolean injected =
+        ServletOutputStreamInjectionHelper.handleWrite(
+            originalBytes, 0, originalBytes.length, obj, sp);
+    assertThat(obj.getHeadTagBytesSeen()).isEqualTo(0);
+    assertThat(injected).isEqualTo(false);
+    writer.flush();
+    String result = writer.toString();
+    writer.close();
+    assertThat(result).isEqualTo("");
+  }
+
+  @Test
+  void testHalfHeadTag() throws IOException {
+    String testSnippet = "\n  <script type=\"text/javascript\"> Test </script>";
+    ExperimentalSnippetHolder.setSnippet(testSnippet);
+    // read the original string
+    String originalFirstPart = "<!DOCTYPE html>\n" + "<html lang=\"en\">\n" + "<he";
+    byte[] originalFirstPartBytes = originalFirstPart.getBytes(StandardCharsets.UTF_8);
+    SnippetInjectingResponseWrapper response = mock(SnippetInjectingResponseWrapper.class);
+    when(response.isCommitted()).thenReturn(false);
+    when(response.getCharacterEncoding()).thenReturn(StandardCharsets.UTF_8.name());
+    InjectionState obj = new InjectionState(response);
+    StringWriter writer = new StringWriter();
+
+    ServletOutputStream sp =
+        new ServletOutputStream() {
+          @Override
+          public void write(int b) throws IOException {
+            writer.write(b);
+          }
+        };
+    boolean injected =
+        ServletOutputStreamInjectionHelper.handleWrite(
+            originalFirstPartBytes, 0, originalFirstPartBytes.length, obj, sp);
+
+    writer.flush();
+    String result = writer.toString();
+    assertThat(obj.getHeadTagBytesSeen()).isEqualTo(3);
+    assertThat(result).isEqualTo("");
+    assertThat(injected).isEqualTo(false);
+    String originalSecondPart =
+        "ad>\n"
+            + "  <meta charset=\"UTF-8\">\n"
+            + "  <title>Title</title>\n"
+            + "</head>\n"
+            + "<body>\n"
+            + "\n"
+            + "</body>\n"
+            + "</html>";
+    byte[] originalSecondPartBytes = originalSecondPart.getBytes(StandardCharsets.UTF_8);
+    injected =
+        ServletOutputStreamInjectionHelper.handleWrite(
+            originalSecondPartBytes, 0, originalSecondPartBytes.length, obj, sp);
+    assertThat(obj.getHeadTagBytesSeen()).isEqualTo(-1);
+    assertThat(injected).isEqualTo(true);
+    String correctSecondPart =
+        "ad>\n"
+            + "  <script type=\"text/javascript\"> Test </script>\n"
+            + "  <meta charset=\"UTF-8\">\n"
+            + "  <title>Title</title>\n"
+            + "</head>\n"
+            + "<body>\n"
+            + "\n"
+            + "</body>\n"
+            + "</html>";
+    writer.flush();
+    result = writer.toString();
+    assertThat(result).isEqualTo(correctSecondPart);
+  }
+}
---
+++ instrumentation/servlet/servlet-3.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/servlet/v3_0/snippet/SnippetInjectingResponseWrapper.java
@@ -0,0 +1,178 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.servlet.v3_0.snippet;
+
+import static io.opentelemetry.javaagent.instrumentation.servlet.v3_0.snippet.Injection.initializeInjectionStateIfNeeded;
+import static java.util.logging.Level.FINE;
+
+import io.opentelemetry.javaagent.bootstrap.servlet.ExperimentalSnippetHolder;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.util.logging.Logger;
+import javax.annotation.Nullable;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+
+/**
+ * Notes on Content-Length: the snippet length is only added to the content length when injection
+ * occurs and the content length was set previously.
+ *
+ * <p>If the Content-Length is set after snippet injection occurs (either for the first time or is
+ * set again for some reason),we intentionally do not add the snippet length, because the
+ * application server may be making that call at the end of a request when it sees the request has
+ * not been submitted, in which case it is likely using the real length of content that has been
+ * written, including the snippet length.
+ */
+public class SnippetInjectingResponseWrapper extends HttpServletResponseWrapper {
+  private static final Logger logger = Logger.getLogger(HttpServletResponseWrapper.class.getName());
+  public static final String FAKE_SNIPPET_HEADER = "FAKE_SNIPPET_HEADER";
+  private static final String SNIPPET = ExperimentalSnippetHolder.getSnippet();
+  private static final int SNIPPET_LENGTH = SNIPPET.length();
+
+  private static final int UNSET = -1;
+  @Nullable private static final MethodHandle setContentLengthLongHandler = getMethodHandle();
+
+  private long contentLength = UNSET;
+
+  private SnippetInjectingPrintWriter snippetInjectingPrintWriter = null;
+
+  public SnippetInjectingResponseWrapper(HttpServletResponse response) {
+    super(response);
+  }
+
+  @Override
+  public boolean containsHeader(String name) {
+    // override this function in order to make sure the response is wrapped
+    // but not wrapped twice
+    // we didn't use the traditional method req.setAttribute
+    // because async would set the original request attribute but didn't pass down the wrapped
+    // response
+    // then the response would never be wrapped again
+    // see also https://docs.oracle.com/javaee/7/api/javax/servlet/AsyncContext.html
+    if (name.equals(FAKE_SNIPPET_HEADER)) {
+      return true;
+    }
+    return super.containsHeader(name);
+  }
+
+  @Override
+  public void setHeader(String name, String value) {
+    // checking content-type is just an optimization to avoid unnecessary parsing
+    if ("Content-Length".equalsIgnoreCase(name) && isContentTypeTextHtml()) {
+      try {
+        contentLength = Long.valueOf(value);
+      } catch (NumberFormatException ex) {
+        logger.log(FINE, "NumberFormatException", ex);
+      }
+    }
+    super.setHeader(name, value);
+  }
+
+  @Override
+  public void addHeader(String name, String value) {
+    // checking content-type is just an optimization to avoid unnecessary parsing
+    if ("Content-Length".equalsIgnoreCase(name) && isContentTypeTextHtml()) {
+      try {
+        contentLength = Long.valueOf(value);
+      } catch (NumberFormatException ex) {
+        logger.log(FINE, "NumberFormatException", ex);
+      }
+    }
+    super.addHeader(name, value);
+  }
+
+  @Override
+  public void setIntHeader(String name, int value) {
+    // checking content-type is just an optimization to avoid unnecessary parsing
+    if ("Content-Length".equalsIgnoreCase(name) && isContentTypeTextHtml()) {
+      contentLength = value;
+    }
+    super.setIntHeader(name, value);
+  }
+
+  @Override
+  public void addIntHeader(String name, int value) {
+    // checking content-type is just an optimization to avoid unnecessary parsing
+    if ("Content-Length".equalsIgnoreCase(name) && isContentTypeTextHtml()) {
+      contentLength = value;
+    }
+    super.addIntHeader(name, value);
+  }
+
+  @Override
+  public void setContentLength(int len) {
+    contentLength = len;
+    super.setContentLength(len);
+  }
+
+  @Nullable
+  private static MethodHandle getMethodHandle() {
+    try {
+      return MethodHandles.lookup()
+          .findSpecial(
+              HttpServletResponseWrapper.class,
+              "setContentLengthLong",
+              MethodType.methodType(void.class),
+              SnippetInjectingResponseWrapper.class);
+    } catch (NoSuchMethodException | IllegalAccessException e) {
+      logger.log(FINE, "SnippetInjectingResponseWrapper setContentLengthLong", e);
+      return null;
+    }
+  }
+
+  public void setContentLengthLong(long length) throws Throwable {
+    contentLength = length;
+    if (setContentLengthLongHandler == null) {
+      super.setContentLength((int) length);
+    } else {
+      setContentLengthLongHandler.invokeWithArguments(this, length);
+    }
+  }
+
+  public boolean isContentTypeTextHtml() {
+    String contentType = super.getContentType();
+    if (contentType == null) {
+      contentType = super.getHeader("content-type");
+    }
+    return contentType != null && contentType.startsWith("text/html");
+  }
+
+  @Override
+  public ServletOutputStream getOutputStream() throws IOException {
+    ServletOutputStream output = super.getOutputStream();
+    initializeInjectionStateIfNeeded(output, this);
+    return output;
+  }
+
+  @Override
+  public PrintWriter getWriter() throws IOException {
+    if (!isContentTypeTextHtml()) {
+      return super.getWriter();
+    }
+    if (snippetInjectingPrintWriter == null) {
+      snippetInjectingPrintWriter =
+          new SnippetInjectingPrintWriter(super.getWriter(), SNIPPET, this);
+    }
+    return snippetInjectingPrintWriter;
+  }
+
+  public void updateContentLengthIfPreviouslySet() {
+    if (contentLength != UNSET) {
+      setContentLength((int) contentLength + SNIPPET_LENGTH);
+    }
+  }
+
+  public boolean isNotSafeToInject() {
+    // if content-length was set and response was already committed (headers sent to the client),
+    // then not safe to inject because the content-length header cannot be updated to account for
+    // the snippet length
+    return isCommitted() && (contentLength != UNSET);
+  }
+}
