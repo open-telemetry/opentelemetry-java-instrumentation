c6ef44621b347472ab26408e007cab9c12124e7b
Felix Wong <fmhwong@ca.ibm.com>
2023-02-22 15:42:08 -0500

    Add Java 19 to Open Liberty 22.0.0.12

--- smoke-tests/images/servlet/build.gradle.kts
+++ smoke-tests/images/servlet/build.gradle.kts
@@ -33,11 +33,11 @@ val targets = mapOf(
   ),
   "liberty" to listOf(
     ImageTarget(listOf("20.0.0.12"), listOf("hotspot", "openj9"), listOf("8", "11"), mapOf("release" to "2020-11-11_0736")),
-    // running configure.sh is failing while building the image with Java 19
     ImageTarget(listOf("21.0.0.12"), listOf("hotspot"), listOf("8", "11", "17"), mapOf("release" to "2021-11-17_1256")),
     ImageTarget(listOf("21.0.0.12"), listOf("openj9"), listOf("8", "11", "17"), mapOf("release" to "2021-11-17_1256")),
-    ImageTarget(listOf("22.0.0.12"), listOf("hotspot"), listOf("8", "11", "17"), mapOf("release" to "22.0.0.12")),
-    ImageTarget(listOf("22.0.0.12"), listOf("openj9"), listOf("8", "11", "17"), mapOf("release" to "22.0.0.12")),
+    // Java 19 is not supported until 22.0.0.10
+    ImageTarget(listOf("22.0.0.12"), listOf("hotspot"), listOf("8", "11", "17", "19"), mapOf("release" to "22.0.0.12")),
+    ImageTarget(listOf("22.0.0.12"), listOf("openj9"), listOf("8", "11", "17", "19"), mapOf("release" to "22.0.0.12")),
   ),
   "payara" to listOf(
     ImageTarget(listOf("5.2020.6"), listOf("hotspot", "openj9"), listOf("8", "11")),
