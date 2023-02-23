77f9e9eeec3750a7f942ff8899518e073e41eaea
Lauri Tulmin <ltulmin@splunk.com>
2023-02-23 18:01:21 +0200

    debug

--- instrumentation/vaadin-14.2/javaagent/src/vaadin14LatestTest/java/test/io/opentelemetry/javaagent/instrumentation/vaadin/Vaadin14LatestTest.java
+++ instrumentation/vaadin-14.2/javaagent/src/vaadin14LatestTest/java/test/io/opentelemetry/javaagent/instrumentation/vaadin/Vaadin14LatestTest.java
@@ -7,12 +7,29 @@ package test.io.opentelemetry.javaagent.instrumentation.vaadin;
 
 import io.opentelemetry.javaagent.instrumentation.vaadin.AbstractVaadin14Test;
 import java.io.File;
+import org.junit.jupiter.api.AfterAll;
 
 public class Vaadin14LatestTest extends AbstractVaadin14Test {
+  private File dir;
 
   @Override
+  @SuppressWarnings("checkstyle:SystemOut")
   protected void prepareVaadinBaseDir(File baseDir) {
+    dir = baseDir;
     copyResource("/pnpm/package.json", baseDir);
     copyResource("/pnpm/pnpm-lock.yaml", baseDir);
+    System.err.println("------ prepareVaadinBaseDir");
+    for (File f : dir.listFiles()) {
+      System.err.println(f.getName() + " " + f.length());
+    }
+  }
+
+  @AfterAll
+  @SuppressWarnings("checkstyle:SystemOut")
+  void debug() {
+    System.err.println("------ AfterAll");
+    for (File f : dir.listFiles()) {
+      System.err.println(f.getName() + " " + f.length());
+    }
   }
 }
