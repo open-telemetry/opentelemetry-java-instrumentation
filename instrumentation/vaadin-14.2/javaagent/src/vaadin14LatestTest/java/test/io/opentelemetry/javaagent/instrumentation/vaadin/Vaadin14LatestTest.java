d94f5d80fb80b4b9114fcfbced4bbdee08a54c4c
Lauri Tulmin <ltulmin@splunk.com>
2023-02-23 15:19:31 +0200

    Fix vaadin14 latest dep test

--- instrumentation/vaadin-14.2/javaagent/src/vaadin14LatestTest/java/test/io/opentelemetry/javaagent/instrumentation/vaadin/Vaadin14LatestTest.java
+++ instrumentation/vaadin-14.2/javaagent/src/vaadin14LatestTest/java/test/io/opentelemetry/javaagent/instrumentation/vaadin/Vaadin14LatestTest.java
@@ -6,5 +6,13 @@
 package test.io.opentelemetry.javaagent.instrumentation.vaadin;
 
 import io.opentelemetry.javaagent.instrumentation.vaadin.AbstractVaadin14Test;
+import java.io.File;
 
-public class Vaadin14LatestTest extends AbstractVaadin14Test {}
+public class Vaadin14LatestTest extends AbstractVaadin14Test {
+
+  @Override
+  protected void prepareVaadinBaseDir(File baseDir) {
+    copyResource("/pnpm/package.json", baseDir);
+    copyResource("/pnpm/pnpm-lock.yaml", baseDir);
+  }
+}
--- instrumentation/vaadin-14.2/javaagent/src/vaadin16Test/java/io/opentelemetry/javaagent/instrumentation/vaadin/Vaadin16Test.java
+++ instrumentation/vaadin-14.2/javaagent/src/vaadin16Test/java/io/opentelemetry/javaagent/instrumentation/vaadin/Vaadin16Test.java
@@ -6,48 +6,12 @@
 package io.opentelemetry.javaagent.instrumentation.vaadin;
 
 import java.io.File;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
 
 public class Vaadin16Test extends AbstractVaadin16Test {
 
   @Override
   protected void prepareVaadinBaseDir(File baseDir) {
-    URL lockFile = AbstractVaadin16Test.class.getResource("/pnpm-lock.yaml");
-    if (lockFile == null) {
-      return;
-    }
-    URI uri;
-    try {
-      uri = lockFile.toURI();
-    } catch (URISyntaxException e) {
-      throw new IllegalStateException(e);
-    }
-    Path sourceDirectory = Paths.get(uri).getParent();
-    String sourcePath = sourceDirectory.toString();
-    Path destinationDirectory = Paths.get(baseDir.toURI());
-    String destinationPath = destinationDirectory.toString();
-    try (Stream<Path> stream = Files.walk(sourceDirectory)) {
-      stream.forEach(
-          source -> {
-            Path destination =
-                Paths.get(destinationPath, source.toString().substring(sourcePath.length()));
-            if (!Files.exists(destination)) {
-              try {
-                Files.copy(source, destination);
-              } catch (IOException e) {
-                throw new IllegalStateException(e);
-              }
-            }
-          });
-    } catch (IOException e) {
-      throw new IllegalStateException(e);
-    }
+    copyResource("/pnpm/package.json", baseDir);
+    copyResource("/pnpm/pnpm-lock.yaml", baseDir);
   }
 }
