dfb94a0017c390efa7e7045b3b12f721f80dbb3c
Domantas Petrauskas <dom.petrauskas@gmail.com>
2023-02-24 14:57:38 +0200

    Make AggregationTemporality configurable for OtlpInMemoryMetricExporter

--- testing/agent-exporter/src/main/java/io/opentelemetry/javaagent/testing/exporter/OtlpInMemoryMetricExporter.java
+++ testing/agent-exporter/src/main/java/io/opentelemetry/javaagent/testing/exporter/OtlpInMemoryMetricExporter.java
@@ -5,6 +5,8 @@
 
 package io.opentelemetry.javaagent.testing.exporter;
 
+import static java.util.logging.Level.CONFIG;
+
 import io.opentelemetry.exporter.internal.otlp.metrics.MetricsRequestMarshaler;
 import io.opentelemetry.sdk.common.CompletableResultCode;
 import io.opentelemetry.sdk.metrics.InstrumentType;
@@ -19,10 +21,25 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.logging.Logger;
 
 class OtlpInMemoryMetricExporter implements MetricExporter {
 
+  private static final Logger logger = Logger.getLogger(OtlpInMemoryMetricExporter.class.getName());
+
   private final Queue<byte[]> collectedRequests = new ConcurrentLinkedQueue<>();
+
+  private final AggregationTemporality aggregationTemporality;
+
+  OtlpInMemoryMetricExporter() {
+    String temporalityProperty = System.getProperty("otel.javaagent.testing.exporter.temporality");
+    if (temporalityProperty == null) {
+      aggregationTemporality = AggregationTemporality.DELTA;
+    } else {
+      aggregationTemporality = AggregationTemporality.valueOf(temporalityProperty.toUpperCase());
+    }
+    logger.log(CONFIG, "Setting aggregation temporality to {0}", aggregationTemporality.toString());
+  }
 
   List<byte[]> getCollectedExportRequests() {
     return new ArrayList<>(collectedRequests);
@@ -34,7 +51,7 @@ class OtlpInMemoryMetricExporter implements MetricExporter {
 
   @Override
   public AggregationTemporality getAggregationTemporality(InstrumentType instrumentType) {
-    return AggregationTemporality.DELTA;
+    return aggregationTemporality;
   }
 
   @Override
