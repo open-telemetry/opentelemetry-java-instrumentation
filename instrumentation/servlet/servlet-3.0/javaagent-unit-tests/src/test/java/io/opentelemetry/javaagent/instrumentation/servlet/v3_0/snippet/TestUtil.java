b46cbff348c00e41d077eebf71e558fc4d1589f3
Trask Stalnaker <trask.stalnaker@gmail.com>
2023-02-25 11:34:30 -0800

    Some refactoring of the unit tests

--- instrumentation/servlet/servlet-3.0/javaagent-unit-tests/src/test/java/io/opentelemetry/javaagent/instrumentation/servlet/v3_0/snippet/TestUtil.java
+++ instrumentation/servlet/servlet-3.0/javaagent-unit-tests/src/test/java/io/opentelemetry/javaagent/instrumentation/servlet/v3_0/snippet/TestUtil.java
@@ -8,15 +8,18 @@ package io.opentelemetry.javaagent.instrumentation.servlet.v3_0.snippet;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 public class TestUtil {
 
+  public static byte[] readFileBytes(String resourceName, Charset charsetName) throws IOException {
+    return readFile(resourceName).getBytes(charsetName);
+  }
+
   public static String readFile(String resourceName) throws IOException {
     InputStream in =
-        SnippetInjectingResponseWrapperTest.class
-            .getClassLoader()
-            .getResourceAsStream(resourceName);
+        SnippetPrintWriterTest.class.getClassLoader().getResourceAsStream(resourceName);
     ByteArrayOutputStream result = new ByteArrayOutputStream();
     byte[] buffer = new byte[1024];
     int length;
