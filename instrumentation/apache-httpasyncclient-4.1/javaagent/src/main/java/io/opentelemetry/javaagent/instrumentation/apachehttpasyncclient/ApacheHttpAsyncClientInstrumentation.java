a527286e69ecf65f3dc7b77298e4e84f6459b2c7
Anurag Agarwal <anuragagarwal561994@users.noreply.github.com>
2023-02-27 23:50:09 +0530

    Spotless apply

--- instrumentation/apache-httpasyncclient-4.1/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/apachehttpasyncclient/ApacheHttpAsyncClientInstrumentation.java
+++ instrumentation/apache-httpasyncclient-4.1/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/apachehttpasyncclient/ApacheHttpAsyncClientInstrumentation.java
@@ -66,8 +66,7 @@ public final class ApacheHttpAsyncClientInstrumentation implements TypeInstrumen
       WrappedFutureCallback<?> wrappedFutureCallback =
           new WrappedFutureCallback<>(parentContext, futureCallback);
       requestProducer =
-          new WrappedRequestProducer(
-              parentContext, requestProducer, wrappedFutureCallback);
+          new WrappedRequestProducer(parentContext, requestProducer, wrappedFutureCallback);
       futureCallback = wrappedFutureCallback;
     }
   }
