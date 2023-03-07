f7baa0358307d58dd2d202b76392091b9c7988a8
sfriberg <sfriberg@users.noreply.github.com>
2023-03-07 09:01:11 -0600

    Update instrumentation/opentelemetry-instrumentation-annotations-1.16/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/instrumentationannotations/AnnotationInstrumentationModule.java
    Co-authored-by: Mateusz Rzeszutek <mrzeszutek@splunk.com>

--- instrumentation/opentelemetry-instrumentation-annotations-1.16/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/instrumentationannotations/AnnotationInstrumentationModule.java
+++ instrumentation/opentelemetry-instrumentation-annotations-1.16/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/instrumentationannotations/AnnotationInstrumentationModule.java
@@ -27,7 +27,7 @@ public class AnnotationInstrumentationModule extends InstrumentationModule {
 
   @Override
   public int order() {
-    // Run first to ensure other automatic intstrumentation is added after and therefore is executed
+    // Run first to ensure other automatic instrumentation is added after and therefore is executed
     // earlier in the instrumented method and create the span to attach attributes to.
     return -1000;
   }
