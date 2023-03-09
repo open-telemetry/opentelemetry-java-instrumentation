a793c247e66ec65c74c95352c166db63a1afc51b
Lauri Tulmin <ltulmin@splunk.com>
2023-03-09 22:02:33 +0200

    Remove duplicate -Xmx from kafka memory options (#8022)

--- instrumentation/kafka/kafka-clients/kafka-clients-0.11/testing/src/main/java/io/opentelemetry/instrumentation/kafka/internal/KafkaClientBaseTest.java
+++ instrumentation/kafka/kafka-clients/kafka-clients-0.11/testing/src/main/java/io/opentelemetry/instrumentation/kafka/internal/KafkaClientBaseTest.java
@@ -65,7 +65,7 @@ public abstract class KafkaClientBaseTest {
   void setupClass() throws ExecutionException, InterruptedException, TimeoutException {
     kafka =
         new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:5.4.3"))
-            .withEnv("KAFKA_HEAP_OPTS", "-Xmx256m -Xmx256m")
+            .withEnv("KAFKA_HEAP_OPTS", "-Xmx256m")
             .withLogConsumer(new Slf4jLogConsumer(logger))
             .waitingFor(Wait.forLogMessage(".*started \\(kafka.server.KafkaServer\\).*", 1))
             .withStartupTimeout(Duration.ofMinutes(1));
