bd3117b165201ebfd1315fdbb7db6c9f9b9f3937
Lauri Tulmin <ltulmin@splunk.com>
2023-03-23 13:50:55 +0200

    Fix flaky spring rabbit test (#8015)
    https://ge.opentelemetry.io/scans/tests?search.buildOutcome=success&search.relativeStartTime=P28D&search.tags=CI&search.timeZoneId=Europe/Tallinn&tests.container=ContextPropagationTest&tests.sortField=FLAKY&tests.test=should%20propagate%20context%20to%20consumer%2C%20test%20headers:%20true&tests.unstableOnly=true
    On jdk8 we can have 2 spans starting on the same millisecond.

--- instrumentation/spring/spring-rabbit-1.0/javaagent/src/test/groovy/ContextPropagationTest.groovy
+++ instrumentation/spring/spring-rabbit-1.0/javaagent/src/test/groovy/ContextPropagationTest.groovy
@@ -91,6 +91,10 @@ class ContextPropagationTest extends AgentInstrumentationSpecification {
     then:
     assertTraces(2) {
       trace(0, 5) {
+        spans.subList(2, 4).sort {
+          def destination = it.attributes.get(SemanticAttributes.MESSAGING_DESTINATION_NAME)
+          return destination == "<default>" ? 0 : 1
+        }
         span(0) {
           name "parent"
         }
