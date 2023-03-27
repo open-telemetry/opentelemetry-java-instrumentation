41a2b636a5301da9934c1d1a5c50f55baa027b67
Tyler Benson <tylerbenson@gmail.com>
2023-03-27 15:38:40 -0400

    Simplify expression

--- conventions/src/main/kotlin/io.opentelemetry.instrumentation.javaagent-shadowing.gradle.kts
+++ conventions/src/main/kotlin/io.opentelemetry.instrumentation.javaagent-shadowing.gradle.kts
@@ -17,7 +17,7 @@ tasks.withType<ShadowJar>().configureEach {
   // rewrite dependencies calling Logger.getLogger
   relocate("java.util.logging.Logger", "io.opentelemetry.javaagent.bootstrap.PatchLogger")
 
-  if (!project.hasProperty("disableShadowRelocate") || !(project.property("disableShadowRelocate").toString().toBoolean())) {
+  if (project.findProperty("disableShadowRelocate") != "true") {
     // prevents conflict with library instrumentation, since these classes live in the bootstrap class loader
     relocate("io.opentelemetry.instrumentation", "io.opentelemetry.javaagent.shaded.instrumentation") {
       // Exclude resource providers since they live in the agent class loader
