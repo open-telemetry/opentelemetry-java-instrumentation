0e85dbd4389d703c648f11b968d6aeb4cb5673ef
Lauri Tulmin <ltulmin@splunk.com>
2023-03-27 09:00:16 +0300

    Skip spotless and other checks in CI test step

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,17 +33,65 @@ nexusPublishing {
 
 description = "OpenTelemetry instrumentations for Java"
 
-// total of 4 partitions (see modulo 4 below)
-var testPartition = (project.findProperty("testPartition") as String?)?.toInt()
-if (testPartition != null) {
-  var testPartitionCounter = 0
+if (project.findProperty("skipTests") as String? == "true") {
   subprojects {
-    // relying on predictable ordering of subprojects
-    // (see https://docs.gradle.org/current/dsl/org.gradle.api.Project.html#N14CB4)
-    // since we are splitting these tasks across different github action jobs
-    val enabled = testPartitionCounter++ % 4 == testPartition
     tasks.withType<Test>().configureEach {
-      this.enabled = enabled
+      enabled = false
     }
+  }
+}
+
+tasks {
+  val listTestPartitions by registering {
+    group = "Help"
+    description = "List test tasks in given partition"
+
+    // total of 4 partitions (see modulo 4 below)
+    var testPartition = (project.findProperty("testPartition") as String?)?.toInt()
+    if (testPartition == null) {
+      throw GradleException("Test partition must be specified")
+    } else if (testPartition < 0 || testPartition >= 4) {
+      throw GradleException("Invalid test partition")
+    }
+
+    val partitionTasks = ArrayList<Test>()
+    var testPartitionCounter = 0
+    subprojects {
+      // relying on predictable ordering of subprojects
+      // (see https://docs.gradle.org/current/dsl/org.gradle.api.Project.html#N14CB4)
+      // since we are splitting these tasks across different github action jobs
+      val enabled = testPartitionCounter++ % 4 == testPartition
+      if (enabled) {
+        tasks.withType<Test>().configureEach {
+          partitionTasks.add(this)
+        }
+      }
+    }
+
+    doLast {
+      File("test-tasks.txt").printWriter().use { writer ->
+        partitionTasks.forEach { task ->
+          var taskPath = task.project.path + ":" + task.name
+          // smoke tests are run separately
+          if (taskPath != ":smoke-tests:test") {
+            writer.println(taskPath)
+          }
+        }
+      }
+    }
+
+    // disable all tasks to stop build
+    subprojects {
+      tasks.configureEach {
+        enabled = false
+      }
+    }
+  }
+}
+
+if (gradle.startParameter.taskNames.any { it.equals("listTestPartitions") }) {
+  // disable all tasks to stop build
+  project.tasks.configureEach {
+    enabled = false
   }
 }
