3601ec520106ca6f1f558bd38652d8ff650bfb43
Trask Stalnaker <trask.stalnaker@gmail.com>
2023-04-01 20:36:37 -0700

    Rename apache-pulsar to pulsar and apache-camel to camel

---
+++ instrumentation/pulsar/apache-pulsar-2.8/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/pulsar/v2_8/PulsarInstrumentationModule.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.pulsar.v2_8;
+
+import com.google.auto.service.AutoService;
+import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
+import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
+import java.util.Arrays;
+import java.util.List;
+
+@AutoService(InstrumentationModule.class)
+public class PulsarInstrumentationModule extends InstrumentationModule {
+  public PulsarInstrumentationModule() {
+    super("pulsar", "pulsar-2.8.0");
+  }
+
+  @Override
+  public List<TypeInstrumentation> typeInstrumentations() {
+    return Arrays.asList(
+        new ConsumerImplInstrumentation(),
+        new ProducerImplInstrumentation(),
+        new MessageInstrumentation(),
+        new MessageListenerInstrumentation());
+  }
+}
