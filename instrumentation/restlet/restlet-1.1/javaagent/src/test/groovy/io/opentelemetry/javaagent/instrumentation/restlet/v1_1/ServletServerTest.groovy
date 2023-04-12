1990b06a82cd43d4502cf388c936cb354c8ac7c4
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2023-04-12 17:26:32 +0200

    Implement HttpServerResponseCustomizer support for Restlet (#8272)

--- instrumentation/restlet/restlet-1.1/javaagent/src/test/groovy/io/opentelemetry/javaagent/instrumentation/restlet/v1_1/ServletServerTest.groovy
+++ instrumentation/restlet/restlet-1.1/javaagent/src/test/groovy/io/opentelemetry/javaagent/instrumentation/restlet/v1_1/ServletServerTest.groovy
@@ -8,7 +8,13 @@ package io.opentelemetry.javaagent.instrumentation.restlet.v1_1
 
 import io.opentelemetry.instrumentation.restlet.v1_1.AbstractServletServerTest
 import io.opentelemetry.instrumentation.test.AgentTestTrait
+import io.opentelemetry.instrumentation.testing.junit.http.ServerEndpoint
 
 class ServletServerTest extends AbstractServletServerTest implements AgentTestTrait {
+
+  @Override
+  boolean hasResponseCustomizer(ServerEndpoint endpoint) {
+    return true
+  }
 
 }
--- instrumentation/restlet/restlet-1.1/javaagent/src/test/groovy/io/opentelemetry/javaagent/instrumentation/restlet/v1_1/spring/SpringBeanRouterTest.groovy
+++ instrumentation/restlet/restlet-1.1/javaagent/src/test/groovy/io/opentelemetry/javaagent/instrumentation/restlet/v1_1/spring/SpringBeanRouterTest.groovy
@@ -7,11 +7,17 @@ package io.opentelemetry.javaagent.instrumentation.restlet.v1_1.spring
 
 import io.opentelemetry.instrumentation.restlet.v1_1.spring.AbstractSpringServerTest
 import io.opentelemetry.instrumentation.test.AgentTestTrait
+import io.opentelemetry.instrumentation.testing.junit.http.ServerEndpoint
 
 class SpringBeanRouterTest extends AbstractSpringServerTest implements AgentTestTrait {
   @Override
   String getConfigurationName() {
     return "springBeanRouterConf.xml"
+  }
+
+  @Override
+  boolean hasResponseCustomizer(ServerEndpoint endpoint) {
+    return true
   }
 
 }
