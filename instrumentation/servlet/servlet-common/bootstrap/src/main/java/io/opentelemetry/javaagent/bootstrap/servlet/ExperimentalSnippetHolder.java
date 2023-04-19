eff7aba0c2e20a338ed076aac813041f3bbc3fd4
siyuniu-ms <siyuniu@microsoft.com>
2023-04-19 00:20:57 -0700

    Update ExperimentalSnippetHolder.java

--- instrumentation/servlet/servlet-common/bootstrap/src/main/java/io/opentelemetry/javaagent/bootstrap/servlet/ExperimentalSnippetHolder.java
+++ instrumentation/servlet/servlet-common/bootstrap/src/main/java/io/opentelemetry/javaagent/bootstrap/servlet/ExperimentalSnippetHolder.java
@@ -11,14 +11,8 @@ public class ExperimentalSnippetHolder {
 
   private static final AtomicReference<String> snippet = new AtomicReference<>("");
 
-  private static boolean isSet = false;
-
   public static void setSnippet(String newValue) {
-    String oldValue = snippet.get();
-    while (!isSet) {
-      isSet = snippet.compareAndSet(oldValue, newValue);
-      oldValue = snippet.get();
-    }
+    snippet.compareAndSet(null, newValue);
   }
 
   public static String getSnippet() {
