52878ccd71fefe911a005a3251c6b1be1ff17cc2
Trask Stalnaker <trask.stalnaker@gmail.com>
2023-04-24 08:48:58 -0700

    Update testing-common/src/main/java/io/opentelemetry/instrumentation/testing/junit/http/ServerEndpoint.java

--- testing-common/src/main/java/io/opentelemetry/instrumentation/testing/junit/http/ServerEndpoint.java
+++ testing-common/src/main/java/io/opentelemetry/instrumentation/testing/junit/http/ServerEndpoint.java
@@ -29,9 +29,9 @@ public class ServerEndpoint {
       new ServerEndpoint("NOT_FOUND", "notFound", 404, "not found");
   public static final ServerEndpoint CAPTURE_HEADERS =
       new ServerEndpoint("CAPTURE_HEADERS", "captureHeaders", 200, "headers captured");
+  // TODO (heya) move it to webflux test module after this has been converted to a class
   public static final ServerEndpoint NESTED_PATH =
       new ServerEndpoint("NESTED_PATH", "nestedPath/hello/world", 200, "nested path");
-  // TODO (heya) move it to webflux test module after this has been converted to a class
   public static final ServerEndpoint CAPTURE_PARAMETERS =
       new ServerEndpoint("CAPTURE_PARAMETERS", "captureParameters", 200, "parameters captured");
 
