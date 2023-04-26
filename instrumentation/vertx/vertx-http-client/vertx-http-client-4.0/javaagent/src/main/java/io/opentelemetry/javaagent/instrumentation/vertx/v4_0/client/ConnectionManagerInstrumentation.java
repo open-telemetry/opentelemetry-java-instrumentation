354ba91ea8d8028d4f88c280e7d482581752924f
Lauri Tulmin <ltulmin@splunk.com>
2023-04-26 10:26:52 +0300

    Use namedOneOf instead of named(..).or(named(..)) (#8366)

--- instrumentation/vertx/vertx-http-client/vertx-http-client-4.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/vertx/v4_0/client/ConnectionManagerInstrumentation.java
+++ instrumentation/vertx/vertx-http-client/vertx-http-client-4.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/vertx/v4_0/client/ConnectionManagerInstrumentation.java
@@ -6,6 +6,7 @@
 package io.opentelemetry.javaagent.instrumentation.vertx.v4_0.client;
 
 import static net.bytebuddy.matcher.ElementMatchers.named;
+import static net.bytebuddy.matcher.ElementMatchers.namedOneOf;
 import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
 
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
@@ -20,8 +21,9 @@ public class ConnectionManagerInstrumentation implements TypeInstrumentation {
 
   @Override
   public ElementMatcher<TypeDescription> typeMatcher() {
-    return named("io.vertx.core.net.impl.clientconnection.ConnectionManager") // 4.0.0
-        .or(named("io.vertx.core.net.impl.pool.ConnectionManager")); // 4.1.0
+    return namedOneOf(
+        "io.vertx.core.net.impl.clientconnection.ConnectionManager", // 4.0.0
+        "io.vertx.core.net.impl.pool.ConnectionManager"); // 4.1.0
   }
 
   @Override
