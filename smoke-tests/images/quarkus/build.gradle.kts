c8a7b140f2917d43757c4af36deb7b7c644428cc
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2023-04-27 16:18:25 +0200

    Bump quarkus version to 3.0.1 (#8379)

--- smoke-tests/images/quarkus/build.gradle.kts
+++ smoke-tests/images/quarkus/build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 }
 
 dependencies {
-  implementation(enforcedPlatform("io.quarkus.platform:quarkus-bom:2.16.6.Final"))
+  implementation(enforcedPlatform("io.quarkus.platform:quarkus-bom:3.0.1.Final"))
   implementation("io.quarkus:quarkus-resteasy")
 }
 
