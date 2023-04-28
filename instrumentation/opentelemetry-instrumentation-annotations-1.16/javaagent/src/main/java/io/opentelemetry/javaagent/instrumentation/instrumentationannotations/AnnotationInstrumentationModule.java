e4467d460c0c060feb77800be945b5b5e7b8a37c
Lauri Tulmin <ltulmin@splunk.com>
2023-04-28 10:04:45 +0300

    Move WithSpan class presence check to instrumentation module

--- instrumentation/opentelemetry-instrumentation-annotations-1.16/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/instrumentationannotations/AnnotationInstrumentationModule.java
+++ instrumentation/opentelemetry-instrumentation-annotations-1.16/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/instrumentationannotations/AnnotationInstrumentationModule.java
@@ -5,6 +5,7 @@
 
 package io.opentelemetry.javaagent.instrumentation.instrumentationannotations;
 
+import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.hasClassesNamed;
 import static java.util.Arrays.asList;
 
 import application.io.opentelemetry.instrumentation.annotations.AddingSpanAttributes;
@@ -13,6 +14,7 @@ import com.google.auto.service.AutoService;
 import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import java.util.List;
+import net.bytebuddy.matcher.ElementMatcher;
 
 /**
  * Instrumentation for methods annotated with {@link WithSpan} and {@link AddingSpanAttributes}
@@ -30,6 +32,11 @@ public class AnnotationInstrumentationModule extends InstrumentationModule {
     // Run first to ensure other automatic instrumentation is added after and therefore is executed
     // earlier in the instrumented method and create the span to attach attributes to.
     return -1000;
+  }
+
+  @Override
+  public ElementMatcher.Junction<ClassLoader> classLoaderMatcher() {
+    return hasClassesNamed("application.io.opentelemetry.instrumentation.annotations.WithSpan");
   }
 
   @Override
