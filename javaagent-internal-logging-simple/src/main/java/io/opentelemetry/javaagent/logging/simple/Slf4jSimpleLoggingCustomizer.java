7b97caa1afa215281b3f1d82e97986aa8bde9ab4
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2023-05-04 18:47:21 +0200

    typo

--- javaagent-internal-logging-simple/src/main/java/io/opentelemetry/javaagent/logging/simple/Slf4jSimpleLoggingCustomizer.java
+++ javaagent-internal-logging-simple/src/main/java/io/opentelemetry/javaagent/logging/simple/Slf4jSimpleLoggingCustomizer.java
@@ -36,7 +36,7 @@ public final class Slf4jSimpleLoggingCustomizer implements LoggingCustomizer {
     setSystemPropertyDefault(
         SIMPLE_LOGGER_DATE_TIME_FORMAT_PROPERTY, SIMPLE_LOGGER_DATE_TIME_FORMAT_DEFAULT);
 
-    if (earlyConfig.getBoolean("otel.javaagent.debut", false)) {
+    if (earlyConfig.getBoolean("otel.javaagent.debug", false)) {
       setSystemPropertyDefault(SIMPLE_LOGGER_DEFAULT_LOG_LEVEL_PROPERTY, "DEBUG");
       setSystemPropertyDefault(SIMPLE_LOGGER_PREFIX + "okhttp3.internal.http2", "INFO");
     }
