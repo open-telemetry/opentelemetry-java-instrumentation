c0b977544061056fff8db912c71d457c668bc10c
Lauri Tulmin <ltulmin@splunk.com>
2023-05-10 10:24:44 +0300

    ignore tests

--- conventions/src/main/kotlin/otel.errorprone-conventions.gradle.kts
+++ conventions/src/main/kotlin/otel.errorprone-conventions.gradle.kts
@@ -123,8 +123,8 @@ tasks {
           // Allow underscore in test-type method names
           disable("MemberName")
         }
-        if (project.path.endsWith(":testing")) {
-          // This check causes too many failures, ignore the ones in testing modules
+        if (project.path.endsWith(":testing") || name.contains("Test")) {
+          // This check causes too many failures, ignore the ones in tests
           disable("CanIgnoreReturnValueSuggester")
         }
       }
