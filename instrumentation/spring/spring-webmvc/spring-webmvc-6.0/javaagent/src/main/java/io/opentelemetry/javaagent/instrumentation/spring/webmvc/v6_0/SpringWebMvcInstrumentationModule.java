d4c51648092832b86bb7b1429f65e08459c31902
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2023-05-11 10:10:51 +0200

    Add classLoaderMatcher() for Spring WebMVC instrumentations (#8463)

--- instrumentation/spring/spring-webmvc/spring-webmvc-6.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/spring/webmvc/v6_0/SpringWebMvcInstrumentationModule.java
+++ instrumentation/spring/spring-webmvc/spring-webmvc-6.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/spring/webmvc/v6_0/SpringWebMvcInstrumentationModule.java
@@ -5,17 +5,25 @@
 
 package io.opentelemetry.javaagent.instrumentation.spring.webmvc.v6_0;
 
+import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.hasClassesNamed;
 import static java.util.Arrays.asList;
 
 import com.google.auto.service.AutoService;
 import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import java.util.List;
+import net.bytebuddy.matcher.ElementMatcher;
 
 @AutoService(InstrumentationModule.class)
 public class SpringWebMvcInstrumentationModule extends InstrumentationModule {
+
   public SpringWebMvcInstrumentationModule() {
     super("spring-webmvc", "spring-webmvc-6.0");
+  }
+
+  @Override
+  public ElementMatcher.Junction<ClassLoader> classLoaderMatcher() {
+    return hasClassesNamed("jakarta.servlet.Filter");
   }
 
   @Override
