831b882ae7fb42b7443bbf328ba5077ad8166f1f
Lauri Tulmin <ltulmin@splunk.com>
2023-05-15 15:29:14 +0300

    address review comment

--- instrumentation/quarkus-resteasy-reactive/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/quarkus/resteasy/reactive/ResteasyReactiveSpanName.java
+++ instrumentation/quarkus-resteasy-reactive/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/quarkus/resteasy/reactive/ResteasyReactiveSpanName.java
@@ -8,16 +8,14 @@ package io.opentelemetry.javaagent.instrumentation.quarkus.resteasy.reactive;
 import static io.opentelemetry.javaagent.instrumentation.jaxrs.JaxrsPathUtil.normalizePath;
 
 import io.opentelemetry.context.Context;
-import io.opentelemetry.instrumentation.api.instrumenter.http.HttpRouteGetter;
 import io.opentelemetry.instrumentation.api.instrumenter.http.HttpRouteHolder;
 import io.opentelemetry.instrumentation.api.instrumenter.http.HttpRouteSource;
 import io.opentelemetry.instrumentation.api.util.VirtualField;
-import javax.annotation.Nullable;
 import org.jboss.resteasy.reactive.server.core.ResteasyReactiveRequestContext;
 import org.jboss.resteasy.reactive.server.mapping.RuntimeResource;
 import org.jboss.resteasy.reactive.server.mapping.URITemplate;
 
-public class ResteasyReactiveSpanName implements HttpRouteGetter<String> {
+public final class ResteasyReactiveSpanName {
   // remember previous path to handle sub path locators
   private static final VirtualField<ResteasyReactiveRequestContext, String> pathField =
       VirtualField.find(ResteasyReactiveRequestContext.class, String.class);
@@ -27,7 +25,7 @@ public class ResteasyReactiveSpanName implements HttpRouteGetter<String> {
   public void updateServerSpanName(ResteasyReactiveRequestContext requestContext) {
     Context context = Context.current();
     String jaxRsName = calculateJaxRsName(requestContext);
-    HttpRouteHolder.updateHttpRoute(context, HttpRouteSource.NESTED_CONTROLLER, this, jaxRsName);
+    HttpRouteHolder.updateHttpRoute(context, HttpRouteSource.NESTED_CONTROLLER, jaxRsName);
     pathField.set(requestContext, jaxRsName);
   }
 
@@ -43,12 +41,6 @@ public class ResteasyReactiveSpanName implements HttpRouteGetter<String> {
     return existingPath == null || existingPath.isEmpty() ? name : existingPath + name;
   }
 
-  @Override
-  @Nullable
-  public String get(Context context, String jaxRsName) {
-    return jaxRsName;
-  }
-
   private static String normalize(URITemplate uriTemplate) {
     if (uriTemplate == null) {
       return "";
@@ -56,4 +48,6 @@ public class ResteasyReactiveSpanName implements HttpRouteGetter<String> {
 
     return normalizePath(uriTemplate.template);
   }
+
+  private ResteasyReactiveSpanName() {}
 }
