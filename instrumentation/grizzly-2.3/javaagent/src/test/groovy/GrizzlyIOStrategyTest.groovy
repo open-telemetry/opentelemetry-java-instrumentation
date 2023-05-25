0decb06e1ffbbdf66b6464c5e6e8596bedef99b3
Lauri Tulmin <ltulmin@splunk.com>
2023-05-25 11:07:34 +0300

    Change grizzly supported version to 2.3

--- instrumentation/grizzly-2.3/javaagent/src/test/groovy/GrizzlyIOStrategyTest.groovy
+++ instrumentation/grizzly-2.3/javaagent/src/test/groovy/GrizzlyIOStrategyTest.groovy
@@ -4,29 +4,17 @@
  */
 
 import org.glassfish.grizzly.IOStrategy
-import org.glassfish.grizzly.http.server.HttpServer
+import org.glassfish.grizzly.http.server.NetworkListener
 import org.glassfish.grizzly.strategies.LeaderFollowerNIOStrategy
 import org.glassfish.grizzly.strategies.SameThreadIOStrategy
 import org.glassfish.grizzly.strategies.SimpleDynamicNIOStrategy
-import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory
-import org.glassfish.jersey.server.ResourceConfig
 
 abstract class GrizzlyIOStrategyTest extends GrizzlyTest {
+
   @Override
-  HttpServer startServer(int port) {
-    ResourceConfig rc = new ResourceConfig()
-    rc.register(SimpleExceptionMapper)
-    rc.register(ServiceResource)
-
-    def server = GrizzlyHttpServerFactory.createHttpServer(new URI("http://localhost:$port"), rc, false)
+  void configureListener(NetworkListener listener) {
     // Default in NIOTransportBuilder is WorkerThreadIOStrategy, so don't need to retest that.
-    server.getListener("grizzly").getTransport().setIOStrategy(strategy())
-    // jersey doesn't propagate exceptions up to the grizzly handler
-    // so we use a standalone HttpHandler to test exception capture
-    server.getServerConfiguration().addHttpHandler(new ExceptionHttpHandler(), "/exception")
-    server.start()
-
-    return server
+    listener.getTransport().setIOStrategy(strategy())
   }
 
   abstract IOStrategy strategy()
