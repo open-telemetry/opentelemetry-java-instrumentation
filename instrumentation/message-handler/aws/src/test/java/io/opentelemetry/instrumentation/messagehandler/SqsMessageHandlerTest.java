dfceb723d66d2a12cb2bb65817787de71d12e4e0
Adam Shaw <atshaw@amazon.com>
2023-06-07 14:37:31 -0700

    Refactoring test code

--- instrumentation/message-handler/aws/src/test/java/io/opentelemetry/instrumentation/messagehandler/SqsMessageHandlerTest.java
+++ instrumentation/message-handler/aws/src/test/java/io/opentelemetry/instrumentation/messagehandler/SqsMessageHandlerTest.java
@@ -5,7 +5,6 @@
 
 package io.opentelemetry.instrumentation.messagehandler;
 
-import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import io.opentelemetry.api.trace.Span;
@@ -13,158 +12,21 @@ import io.opentelemetry.api.trace.SpanContext;
 import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.api.trace.TraceFlags;
 import io.opentelemetry.api.trace.TraceState;
-import io.opentelemetry.context.Context;
 import io.opentelemetry.context.Scope;
-import io.opentelemetry.context.propagation.ContextPropagators;
-import io.opentelemetry.contrib.awsxray.propagator.AwsXrayPropagator;
 import io.opentelemetry.instrumentation.api.instrumenter.messaging.MessageOperation;
-import io.opentelemetry.instrumentation.testing.util.TelemetryDataUtil;
-import io.opentelemetry.sdk.OpenTelemetrySdk;
-import io.opentelemetry.sdk.common.CompletableResultCode;
-import io.opentelemetry.sdk.metrics.SdkMeterProvider;
-import io.opentelemetry.sdk.metrics.data.AggregationTemporality;
-import io.opentelemetry.sdk.metrics.export.MetricReader;
-import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader;
-import io.opentelemetry.sdk.testing.assertj.TraceAssert;
-import io.opentelemetry.sdk.testing.assertj.TracesAssert;
-import io.opentelemetry.sdk.testing.exporter.InMemoryMetricExporter;
-import io.opentelemetry.sdk.testing.exporter.InMemorySpanExporter;
-import io.opentelemetry.sdk.trace.ReadWriteSpan;
-import io.opentelemetry.sdk.trace.ReadableSpan;
-import io.opentelemetry.sdk.trace.SdkTracerProvider;
-import io.opentelemetry.sdk.trace.SpanProcessor;
 import io.opentelemetry.sdk.trace.data.LinkData;
-import io.opentelemetry.sdk.trace.data.SpanData;
-import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
 import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Consumer;
-import javax.annotation.Nullable;
-import org.awaitility.core.ConditionTimeoutException;
 import org.junit.Assert;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import software.amazon.awssdk.services.sqs.model.Message;
 import software.amazon.awssdk.services.sqs.model.MessageAttributeValue;
 
-public class SqsMessageHandlerTest {
-  private static final OpenTelemetrySdk openTelemetry;
-  private static final InMemorySpanExporter testSpanExporter;
-
-  static {
-    testSpanExporter = InMemorySpanExporter.create();
-    InMemoryMetricExporter testMetricExporter =
-        InMemoryMetricExporter.create(AggregationTemporality.DELTA);
-
-    MetricReader metricReader =
-        PeriodicMetricReader.builder(testMetricExporter)
-            // Set really long interval. We'll call forceFlush when we need the metrics
-            // instead of collecting them periodically.
-            .setInterval(Duration.ofNanos(Long.MAX_VALUE))
-            .build();
-
-    openTelemetry =
-        OpenTelemetrySdk.builder()
-            .setTracerProvider(
-                SdkTracerProvider.builder()
-                    .addSpanProcessor(new FlushTrackingSpanProcessor())
-                    .addSpanProcessor(SimpleSpanProcessor.create(testSpanExporter))
-                    .build())
-            .setMeterProvider(SdkMeterProvider.builder().registerMetricReader(metricReader).build())
-            .setPropagators(ContextPropagators.create(AwsXrayPropagator.getInstance()))
-            .buildAndRegisterGlobal();
-  }
-
-  private static class FlushTrackingSpanProcessor implements SpanProcessor {
-    @Override
-    public void onStart(Context parentContext, ReadWriteSpan span) {}
-
-    @Override
-    public boolean isStartRequired() {
-      return false;
-    }
-
-    @Override
-    public void onEnd(ReadableSpan span) {}
-
-    @Override
-    public boolean isEndRequired() {
-      return false;
-    }
-
-    @Override
-    public CompletableResultCode forceFlush() {
-      return CompletableResultCode.ofSuccess();
-    }
-  }
-
-  @SafeVarargs
-  @SuppressWarnings("varargs")
-  public final void waitAndAssertTraces(Consumer<TraceAssert>... assertions) {
-    waitAndAssertTraces(null, Arrays.asList(assertions), true);
-  }
-
-  private <T extends Consumer<TraceAssert>> void waitAndAssertTraces(
-      @Nullable Comparator<List<SpanData>> traceComparator,
-      Iterable<T> assertions,
-      boolean verifyScopeVersion) {
-    List<Consumer<TraceAssert>> assertionsList = new ArrayList<>();
-    assertions.forEach(assertionsList::add);
-
-    try {
-      await()
-          .untilAsserted(() -> doAssertTraces(traceComparator, assertionsList, verifyScopeVersion));
-    } catch (ConditionTimeoutException e) {
-      // Don't throw this failure since the stack is the awaitility thread, causing confusion.
-      // Instead, just assert one more time on the test thread, which will fail with a better stack
-      // trace.
-      // TODO(anuraaga): There is probably a better way to do this.
-      doAssertTraces(traceComparator, assertionsList, verifyScopeVersion);
-    }
-  }
-
-  private void doAssertTraces(
-      @Nullable Comparator<List<SpanData>> traceComparator,
-      List<Consumer<TraceAssert>> assertionsList,
-      boolean verifyScopeVersion) {
-    List<List<SpanData>> traces = waitForTraces(assertionsList.size());
-    if (verifyScopeVersion) {
-      TelemetryDataUtil.assertScopeVersion(traces);
-    }
-    if (traceComparator != null) {
-      traces.sort(traceComparator);
-    }
-    TracesAssert.assertThat(traces).hasTracesSatisfyingExactly(assertionsList);
-  }
-
-  public final List<List<SpanData>> waitForTraces(int numberOfTraces) {
-    try {
-      return TelemetryDataUtil.waitForTraces(
-          this::getExportedSpans, numberOfTraces, 20, TimeUnit.SECONDS);
-    } catch (TimeoutException | InterruptedException e) {
-      throw new AssertionError("Error waiting for " + numberOfTraces + " traces", e);
-    }
-  }
-
-  public List<SpanData> getExportedSpans() {
-    return testSpanExporter.getFinishedSpanItems();
-  }
-
-  @AfterEach
-  public void resetTests() {
-    testSpanExporter.reset();
-  }
-
+public class SqsMessageHandlerTest extends XrayTestInstrumenter {
   @Test
   public void simple() {
     Message sqsMessage =
@@ -178,14 +40,14 @@ public class SqsMessageHandlerTest {
     AtomicInteger counter = new AtomicInteger(0);
 
     SqsMessageHandler messageHandler =
-        new SqsMessageHandler(openTelemetry, messages -> "Batch of Messages") {
+        new SqsMessageHandler(getOpenTelemetry(), messages -> "Batch of Messages") {
           @Override
           protected void doHandleMessages(Collection<Message> messages) {
             counter.getAndIncrement();
           }
         };
 
-    Span parentSpan = openTelemetry.getTracer("test").spanBuilder("test").startSpan();
+    Span parentSpan = getOpenTelemetry().getTracer("test").spanBuilder("test").startSpan();
 
     try (Scope scope = parentSpan.makeCurrent()) {
       messageHandler.handleMessages(Collections.singletonList(sqsMessage));
@@ -234,14 +96,14 @@ public class SqsMessageHandlerTest {
     AtomicInteger counter = new AtomicInteger(0);
 
     SqsMessageHandler messageHandler =
-        new SqsMessageHandler(openTelemetry, messages -> "Batch of Messages") {
+        new SqsMessageHandler(getOpenTelemetry(), messages -> "Batch of Messages") {
           @Override
           protected void doHandleMessages(Collection<Message> messages) {
             counter.getAndIncrement();
           }
         };
 
-    Span parentSpan = openTelemetry.getTracer("test").spanBuilder("test").startSpan();
+    Span parentSpan = getOpenTelemetry().getTracer("test").spanBuilder("test").startSpan();
 
     try (Scope scope = parentSpan.makeCurrent()) {
       messageHandler.handleMessages(Collections.singletonList(sqsMessage));
@@ -299,14 +161,14 @@ public class SqsMessageHandlerTest {
     AtomicInteger counter = new AtomicInteger(0);
 
     SqsMessageHandler messageHandler =
-        new SqsMessageHandler(openTelemetry, messages -> "Batch of Messages") {
+        new SqsMessageHandler(getOpenTelemetry(), messages -> "Batch of Messages") {
           @Override
           protected void doHandleMessages(Collection<Message> messages) {
             counter.getAndIncrement();
           }
         };
 
-    Span parentSpan = openTelemetry.getTracer("test").spanBuilder("test").startSpan();
+    Span parentSpan = getOpenTelemetry().getTracer("test").spanBuilder("test").startSpan();
 
     try (Scope scope = parentSpan.makeCurrent()) {
       messageHandler.handleMessages(sqsMessages);
@@ -366,14 +228,14 @@ public class SqsMessageHandlerTest {
     AtomicInteger counter = new AtomicInteger(0);
 
     SqsMessageHandler messageHandler =
-        new SqsMessageHandler(openTelemetry, messages -> "Batch of Messages") {
+        new SqsMessageHandler(getOpenTelemetry(), messages -> "Batch of Messages") {
           @Override
           protected void doHandleMessages(Collection<Message> messages) {
             counter.getAndIncrement();
           }
         };
 
-    Span parentSpan = openTelemetry.getTracer("test").spanBuilder("test").startSpan();
+    Span parentSpan = getOpenTelemetry().getTracer("test").spanBuilder("test").startSpan();
 
     try (Scope scope = parentSpan.makeCurrent()) {
       messageHandler.handleMessages(Collections.singletonList(sqsMessage1));
@@ -429,14 +291,14 @@ public class SqsMessageHandlerTest {
     AtomicInteger counter = new AtomicInteger(0);
 
     SqsMessageHandler messageHandler =
-        new SqsMessageHandler(openTelemetry, messages -> "Batch of Messages") {
+        new SqsMessageHandler(getOpenTelemetry(), messages -> "Batch of Messages") {
           @Override
           protected void doHandleMessages(Collection<Message> messages) {
             counter.getAndIncrement();
           }
         };
 
-    Span parentSpan = openTelemetry.getTracer("test").spanBuilder("test").startSpan();
+    Span parentSpan = getOpenTelemetry().getTracer("test").spanBuilder("test").startSpan();
 
     try (Scope scope = parentSpan.makeCurrent()) {
       messageHandler.handleMessages(Collections.emptyList());
@@ -475,14 +337,14 @@ public class SqsMessageHandlerTest {
     AtomicInteger counter = new AtomicInteger(0);
 
     SqsMessageHandler messageHandler =
-        new SqsMessageHandler(openTelemetry, messages -> "New Name", MessageOperation.PROCESS) {
+        new SqsMessageHandler(getOpenTelemetry(), messages -> "New Name", MessageOperation.PROCESS) {
           @Override
           protected void doHandleMessages(Collection<Message> messages) {
             counter.getAndIncrement();
           }
         };
 
-    Span parentSpan = openTelemetry.getTracer("test").spanBuilder("test").startSpan();
+    Span parentSpan = getOpenTelemetry().getTracer("test").spanBuilder("test").startSpan();
 
     try (Scope scope = parentSpan.makeCurrent()) {
       messageHandler.handleMessages(Collections.singletonList(sqsMessage));
@@ -528,14 +390,14 @@ public class SqsMessageHandlerTest {
     AtomicInteger counter = new AtomicInteger(0);
 
     SqsMessageHandler messageHandler =
-        new SqsMessageHandler(openTelemetry, messages -> "New Name", MessageOperation.SEND) {
+        new SqsMessageHandler(getOpenTelemetry(), messages -> "New Name", MessageOperation.SEND) {
           @Override
           protected void doHandleMessages(Collection<Message> messages) {
             counter.getAndIncrement();
           }
         };
 
-    Span parentSpan = openTelemetry.getTracer("test").spanBuilder("test").startSpan();
+    Span parentSpan = getOpenTelemetry().getTracer("test").spanBuilder("test").startSpan();
 
     try (Scope scope = parentSpan.makeCurrent()) {
       messageHandler.handleMessages(Collections.singletonList(sqsMessage));
@@ -580,14 +442,14 @@ public class SqsMessageHandlerTest {
     AtomicInteger counter = new AtomicInteger(0);
 
     SqsMessageHandler messageHandler =
-        new SqsMessageHandler(openTelemetry, messages -> "Batch of Messages") {
+        new SqsMessageHandler(getOpenTelemetry(), messages -> "Batch of Messages") {
           @Override
           protected void doHandleMessages(Collection<Message> messages) {
             counter.getAndIncrement();
           }
         };
 
-    Span parentSpan = openTelemetry.getTracer("test").spanBuilder("test").startSpan();
+    Span parentSpan = getOpenTelemetry().getTracer("test").spanBuilder("test").startSpan();
 
     assertThrows(
         StringIndexOutOfBoundsException.class,
@@ -611,7 +473,7 @@ public class SqsMessageHandlerTest {
     AtomicInteger counter = new AtomicInteger(0);
 
     SqsMessageHandler messageHandler =
-        new SqsMessageHandler(openTelemetry, messages -> "Batch of Messages") {
+        new SqsMessageHandler(getOpenTelemetry(), messages -> "Batch of Messages") {
           @Override
           protected void doHandleMessages(Collection<Message> messages) {
             counter.getAndIncrement();
@@ -619,7 +481,7 @@ public class SqsMessageHandlerTest {
           }
         };
 
-    Span parentSpan = openTelemetry.getTracer("test").spanBuilder("test").startSpan();
+    Span parentSpan = getOpenTelemetry().getTracer("test").spanBuilder("test").startSpan();
 
     assertThrows(
         RuntimeException.class,
---
+++ instrumentation/message-handler/aws/src/test/java/io/opentelemetry/instrumentation/messagehandler/XrayTestInstrumenter.java
@@ -0,0 +1,148 @@
+package io.opentelemetry.instrumentation.messagehandler;
+
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.contrib.awsxray.propagator.AwsXrayPropagator;
+import io.opentelemetry.instrumentation.testing.util.TelemetryDataUtil;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.common.CompletableResultCode;
+import io.opentelemetry.sdk.metrics.SdkMeterProvider;
+import io.opentelemetry.sdk.metrics.data.AggregationTemporality;
+import io.opentelemetry.sdk.metrics.export.MetricReader;
+import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader;
+import io.opentelemetry.sdk.testing.assertj.TraceAssert;
+import io.opentelemetry.sdk.testing.assertj.TracesAssert;
+import io.opentelemetry.sdk.testing.exporter.InMemoryMetricExporter;
+import io.opentelemetry.sdk.testing.exporter.InMemorySpanExporter;
+import io.opentelemetry.sdk.trace.ReadWriteSpan;
+import io.opentelemetry.sdk.trace.ReadableSpan;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.SpanProcessor;
+import io.opentelemetry.sdk.trace.data.SpanData;
+import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
+import org.awaitility.core.ConditionTimeoutException;
+import org.junit.jupiter.api.AfterEach;
+import javax.annotation.Nullable;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.function.Consumer;
+
+import static org.awaitility.Awaitility.await;
+
+public class XrayTestInstrumenter {
+  private static final OpenTelemetrySdk openTelemetry;
+  private static final InMemorySpanExporter testSpanExporter;
+
+  static {
+    testSpanExporter = InMemorySpanExporter.create();
+    InMemoryMetricExporter testMetricExporter =
+        InMemoryMetricExporter.create(AggregationTemporality.DELTA);
+
+    MetricReader metricReader =
+        PeriodicMetricReader.builder(testMetricExporter)
+            // Set really long interval. We'll call forceFlush when we need the metrics
+            // instead of collecting them periodically.
+            .setInterval(Duration.ofNanos(Long.MAX_VALUE))
+            .build();
+
+    openTelemetry =
+        OpenTelemetrySdk.builder()
+            .setTracerProvider(
+                SdkTracerProvider.builder()
+                    .addSpanProcessor(new FlushTrackingSpanProcessor())
+                    .addSpanProcessor(SimpleSpanProcessor.create(testSpanExporter))
+                    .build())
+            .setMeterProvider(SdkMeterProvider.builder().registerMetricReader(metricReader).build())
+            .setPropagators(ContextPropagators.create(AwsXrayPropagator.getInstance()))
+            .buildAndRegisterGlobal();
+  }
+
+  @AfterEach
+  public void resetTests() {
+    testSpanExporter.reset();
+  }
+
+  protected OpenTelemetrySdk getOpenTelemetry() {
+    return openTelemetry;
+  }
+
+  private static class FlushTrackingSpanProcessor implements SpanProcessor {
+    @Override
+    public void onStart(Context parentContext, ReadWriteSpan span) {}
+
+    @Override
+    public boolean isStartRequired() {
+      return false;
+    }
+
+    @Override
+    public void onEnd(ReadableSpan span) {}
+
+    @Override
+    public boolean isEndRequired() {
+      return false;
+    }
+
+    @Override
+    public CompletableResultCode forceFlush() {
+      return CompletableResultCode.ofSuccess();
+    }
+  }
+
+  @SafeVarargs
+  @SuppressWarnings("varargs")
+  public final void waitAndAssertTraces(Consumer<TraceAssert>... assertions) {
+    waitAndAssertTraces(null, Arrays.asList(assertions), true);
+  }
+
+  private <T extends Consumer<TraceAssert>> void waitAndAssertTraces(
+      @Nullable Comparator<List<SpanData>> traceComparator,
+      Iterable<T> assertions,
+      boolean verifyScopeVersion) {
+    List<Consumer<TraceAssert>> assertionsList = new ArrayList<>();
+    assertions.forEach(assertionsList::add);
+
+    try {
+      await()
+          .untilAsserted(() -> doAssertTraces(traceComparator, assertionsList, verifyScopeVersion));
+    } catch (ConditionTimeoutException e) {
+      // Don't throw this failure since the stack is the awaitility thread, causing confusion.
+      // Instead, just assert one more time on the test thread, which will fail with a better stack
+      // trace.
+      // TODO(anuraaga): There is probably a better way to do this.
+      doAssertTraces(traceComparator, assertionsList, verifyScopeVersion);
+    }
+  }
+
+  private void doAssertTraces(
+      @Nullable Comparator<List<SpanData>> traceComparator,
+      List<Consumer<TraceAssert>> assertionsList,
+      boolean verifyScopeVersion) {
+    List<List<SpanData>> traces = waitForTraces(assertionsList.size());
+    if (verifyScopeVersion) {
+      TelemetryDataUtil.assertScopeVersion(traces);
+    }
+    if (traceComparator != null) {
+      traces.sort(traceComparator);
+    }
+    TracesAssert.assertThat(traces).hasTracesSatisfyingExactly(assertionsList);
+  }
+
+  public final List<List<SpanData>> waitForTraces(int numberOfTraces) {
+    try {
+      return TelemetryDataUtil.waitForTraces(
+          this::getExportedSpans, numberOfTraces, 20, TimeUnit.SECONDS);
+    } catch (TimeoutException | InterruptedException e) {
+      throw new AssertionError("Error waiting for " + numberOfTraces + " traces", e);
+    }
+  }
+
+  public List<SpanData> getExportedSpans() {
+    return testSpanExporter.getFinishedSpanItems();
+  }
+}
