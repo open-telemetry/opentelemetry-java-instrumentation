f1232dd31e0fca605aef9bfe6a8705a146fd316f
Gregor Zeitlinger <gregor.zeitlinger@zalando.de>
2023-06-08 08:06:25 +0200

    Update instrumentation/micrometer/micrometer-1.5/library/src/main/java/io/opentelemetry/instrumentation/micrometer/v1_5/OpenTelemetryFunctionTimer.java
    Co-authored-by: Mateusz Rzeszutek <mrzeszutek@splunk.com>

--- instrumentation/micrometer/micrometer-1.5/library/src/main/java/io/opentelemetry/instrumentation/micrometer/v1_5/OpenTelemetryFunctionTimer.java
+++ instrumentation/micrometer/micrometer-1.5/library/src/main/java/io/opentelemetry/instrumentation/micrometer/v1_5/OpenTelemetryFunctionTimer.java
@@ -44,7 +44,7 @@ final class OpenTelemetryFunctionTimer<T> extends AbstractMeter
         otelMeter
             .counterBuilder(name + ".count")
             .setDescription(Bridging.description(id))
-            .setUnit("{invocations}")
+            .setUnit("{invocation}")
             .buildWithCallback(new LongMeasurementRecorder<>(obj, countFunction, attributes));
 
     this.observableTotalTime =
