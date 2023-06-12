ad270af5b6b368bcb32a880630e5ad093e8b5969
Adam Zell <zellster@gmail.com>
2023-06-11 19:41:05 -0700

    Revert last change as muzzle tests are failing.

--- instrumentation/aws-lambda/aws-lambda-events-2.2/javaagent/build.gradle.kts
+++ instrumentation/aws-lambda/aws-lambda-events-2.2/javaagent/build.gradle.kts
@@ -7,7 +7,7 @@ muzzle {
     group.set("com.amazonaws")
     module.set("aws-lambda-java-core")
     versions.set("[1.0.0,)")
-    extraDependency("com.amazonaws:aws-lambda-java-events:2.2.1")
+    extraDependency("com.amazonaws:aws-lambda-java-events:3.9.0")
     extraDependency("com.amazonaws.serverless:aws-serverless-java-container-core:1.5.2")
   }
 }
