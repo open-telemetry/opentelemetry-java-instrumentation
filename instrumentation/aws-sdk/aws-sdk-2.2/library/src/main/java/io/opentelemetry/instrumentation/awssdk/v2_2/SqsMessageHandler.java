46feb1593cf5169aade37ee46fd70bd660b6e92f
Adam Shaw <atshaw@amazon.com>
2023-06-13 12:43:53 -0700

    Remove Warning

--- instrumentation/aws-sdk/aws-sdk-2.2/library/src/main/java/io/opentelemetry/instrumentation/awssdk/v2_2/SqsMessageHandler.java
+++ instrumentation/aws-sdk/aws-sdk-2.2/library/src/main/java/io/opentelemetry/instrumentation/awssdk/v2_2/SqsMessageHandler.java
@@ -67,6 +67,7 @@ public abstract class SqsMessageHandler extends MessageHandler<Message> {
 
       @Nullable
       @Override
+      @SuppressWarnings("deprecation")
       public String getDestinationKind(Collection<Message> messages) {
         return null;
       }
