41c461d3fd7e0fb1fc6246d8c0d0bc515cfcf059
oliver zhang <oliver_zhangjian@163.com>
2023-06-20 18:01:41 +0800

    Update instrumentation/servlet/servlet-common/bootstrap/src/main/java/io/opentelemetry/javaagent/bootstrap/servlet/InjectionState.java
    Co-authored-by: Mateusz Rzeszutek <mrzeszutek@splunk.com>

--- instrumentation/servlet/servlet-common/bootstrap/src/main/java/io/opentelemetry/javaagent/bootstrap/servlet/InjectionState.java
+++ instrumentation/servlet/servlet-common/bootstrap/src/main/java/io/opentelemetry/javaagent/bootstrap/servlet/InjectionState.java
@@ -8,7 +8,7 @@ package io.opentelemetry.javaagent.bootstrap.servlet;
 // this is shared by both ServletOutputStream and PrintWriter injection
 public class InjectionState {
   private static final int HEAD_TAG_WRITTEN_FAKE_VALUE = -1;
-  private static final int HEAD_TAG_PREFIX_LENGTH = 4;
+  private static final int HEAD_TAG_PREFIX_LENGTH = "<head".length();
   private final SnippetInjectingResponseWrapper wrapper;
   private int headTagBytesSeen = 0;
 
