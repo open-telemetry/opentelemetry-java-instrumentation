bb4211d0b7bf5215a55fbfd2da371691861e3dc5
Christian Neumüller <christian.neumueller@dynatrace.com>
2023-06-28 20:04:06 +0200

    Fix aws-sdk-2.2 library instrumentation crashing app when SQS is not on classpath (#8805)

--- instrumentation/aws-sdk/aws-sdk-2.2/library/src/main/java/io/opentelemetry/instrumentation/awssdk/v2_2/SqsAccess.java
+++ instrumentation/aws-sdk/aws-sdk-2.2/library/src/main/java/io/opentelemetry/instrumentation/awssdk/v2_2/SqsAccess.java
@@ -20,21 +20,7 @@ import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 final class SqsAccess {
   private SqsAccess() {}
 
-  private static final boolean enabled = isSqsImplPresent();
-
-  private static boolean isSqsImplPresent() {
-    try {
-      // for library instrumentation SqsImpl is always available
-      // for javaagent instrumentation SqsImpl is available only when SqsInstrumentationModule was
-      // successfully applied (muzzle passed)
-      // using package name here because library instrumentation classes are relocated when embedded
-      // in the agent
-      Class.forName(SqsAccess.class.getName().replace(".SqsAccess", ".SqsImpl"));
-      return true;
-    } catch (ClassNotFoundException e) {
-      return false;
-    }
-  }
+  private static final boolean enabled = PluginImplUtil.isImplPresent("SqsImpl");
 
   @NoMuzzle
   static boolean isSendMessageRequest(SdkRequest request) {
