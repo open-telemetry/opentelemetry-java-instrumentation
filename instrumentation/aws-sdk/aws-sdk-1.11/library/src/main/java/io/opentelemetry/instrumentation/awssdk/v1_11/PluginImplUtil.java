c2cb01d6c6df79385ed85215ff8c88d56b758c03
Christian Neumüller <christian.neumueller@dynatrace.com>
2023-07-06 10:59:01 +0200

    Clean up comment & log message.

--- instrumentation/aws-sdk/aws-sdk-1.11/library/src/main/java/io/opentelemetry/instrumentation/awssdk/v1_11/PluginImplUtil.java
+++ instrumentation/aws-sdk/aws-sdk-1.11/library/src/main/java/io/opentelemetry/instrumentation/awssdk/v1_11/PluginImplUtil.java
@@ -16,33 +16,40 @@ final class PluginImplUtil { // TODO: Copy & paste from v2
   /**
    * Check if the given {@code moduleNameImpl} is present.
    *
-   * <p>For library instrumentations, the Impls will always be available but might fail to load if
-   * the corresponding SDK classes are not on the class path. For javaagent, the Impl is available
-   * only when the corresponding InstrumentationModule was successfully applied (muzzle passed).
+   * <p>For library instrumentations, the Impls will always be available but might fail to
+   * load/link/initialize if the corresponding SDK classes are not on the class path. For javaagent,
+   * the Impl is available only when the corresponding InstrumentationModule was successfully
+   * applied (muzzle passed).
    *
-   * <p>Note that an present-but-incompatible library can only be detected by Muzzle. In
+   * <p>Note that an present-but-incompatible library can only be reliably detected by Muzzle. In
    * library-mode, users need to ensure they are using a compatible SDK (component) versions
    * themselves.
    *
    * @param implSimpleClassName The simple name of the impl class, e.g. {@code "SqsImpl"}. *
    */
   static boolean isImplPresent(String implSimpleClassName) {
+    // Computing the full name dynamically name here because library instrumentation classes are
+    // relocated when embedded in the agent.
     // We use getName().replace() instead of getPackage() because the latter is not guaranteed to
     // work in all cases (e.g., we might be loaded into a custom classloader that doesn't handle it)
     String implFullClassName =
         PluginImplUtil.class.getName().replace(".PluginImplUtil", "." + implSimpleClassName);
     try {
-      // using package name here because library instrumentation classes are relocated when embedded
-      // in the agent
       Class.forName(implFullClassName);
       return true;
     } catch (ClassNotFoundException | LinkageError e) {
       // ClassNotFoundException will happen when muzzle disabled us in javaagent mode; LinkageError
-      // (most likely NoClassDefFoundError) should happen when the class is loaded in library mode
-      // (where it is always available) but a dependency failed to load (most likely because the
-      // corresponding SDK dependency is not on the class path).
+      // (most likely a NoClassDefFoundError, potentially wrapped in an ExceptionInInitializerError)
+      // should be thrown when the class is loaded in library mode (where the Impl class itself can
+      // always be found) but a dependency failed to load (most likely because the corresponding SDK
+      // dependency is not on the class path).
       logger.log(
-          Level.FINE, e, () -> implFullClassName + " not present, probably incompatible version");
+          Level.FINE,
+          e,
+          () ->
+              implFullClassName
+                  + " not present. "
+                  + "Most likely, corresponding SDK component is either not on classpath or incompatible.");
       return false;
     }
   }
