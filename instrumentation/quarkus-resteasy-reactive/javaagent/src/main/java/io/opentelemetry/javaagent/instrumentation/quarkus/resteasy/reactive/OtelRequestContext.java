8011cb4af2fbea093ea6bba4375d81e25a494841
Lauri Tulmin <ltulmin@splunk.com>
2023-07-11 14:05:10 +0300

    Set server span name for aborted requests in quarkus resteasy native (#8891)
    Co-authored-by: Mateusz Rzeszutek <mrzeszutek@splunk.com>

---
+++ instrumentation/quarkus-resteasy-reactive/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/quarkus/resteasy/reactive/OtelRequestContext.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.quarkus.resteasy.reactive;
+
+import io.opentelemetry.instrumentation.api.instrumenter.http.HttpRouteSource;
+import org.jboss.resteasy.reactive.server.core.ResteasyReactiveRequestContext;
+
+public final class OtelRequestContext {
+  private static final ThreadLocal<OtelRequestContext> contextThreadLocal = new ThreadLocal<>();
+  private boolean firstInvoke = true;
+
+  public static OtelRequestContext start(ResteasyReactiveRequestContext requestContext) {
+    OtelRequestContext context = new OtelRequestContext();
+    contextThreadLocal.set(context);
+    ResteasyReactiveSpanName.INSTANCE.updateServerSpanName(
+        requestContext, HttpRouteSource.CONTROLLER);
+    return context;
+  }
+
+  public static void onInvoke(ResteasyReactiveRequestContext requestContext) {
+    OtelRequestContext context = contextThreadLocal.get();
+    if (context == null) {
+      return;
+    }
+    // we ignore the first invoke as it uses the same context that we get in start, the second etc.
+    // invoke will be for sub resource locator that changes the path
+    if (context.firstInvoke) {
+      context.firstInvoke = false;
+      return;
+    }
+    ResteasyReactiveSpanName.INSTANCE.updateServerSpanName(
+        requestContext, HttpRouteSource.NESTED_CONTROLLER);
+  }
+
+  public void close() {
+    contextThreadLocal.remove();
+  }
+
+  private OtelRequestContext() {}
+}
