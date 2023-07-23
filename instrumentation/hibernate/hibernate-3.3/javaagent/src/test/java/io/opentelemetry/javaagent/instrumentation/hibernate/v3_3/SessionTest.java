4ff036e8920d5b3b81fd023df1492606a8c83388
Jay DeLuca <jaydeluca4@gmail.com>
2023-07-23 19:18:55 -0400

    fix SessionTest remove public

--- instrumentation/hibernate/hibernate-3.3/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/hibernate/v3_3/SessionTest.java
+++ instrumentation/hibernate/hibernate-3.3/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/hibernate/v3_3/SessionTest.java
@@ -23,7 +23,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-public class SessionTest extends AbstractHibernateTest {
+class SessionTest extends AbstractHibernateTest {
 
   @ParameterizedTest
   @MethodSource("provideArguments")
