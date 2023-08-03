8b8115a7ea683c5db4341db0c13ff38617f0c4dd
Trask Stalnaker <trask.stalnaker@gmail.com>
2023-08-03 02:38:52 -0700

    Inline protobuf gradle conventions (#9133)

--- instrumentation/grpc-1.6/testing/build.gradle.kts
+++ instrumentation/grpc-1.6/testing/build.gradle.kts
@@ -1,6 +1,8 @@
+import com.google.protobuf.gradle.*
+
 plugins {
   id("otel.java-conventions")
-  id("otel.protobuf-conventions")
+  id("com.google.protobuf")
 }
 
 val grpcVersion = "1.6.0"
@@ -28,5 +30,41 @@ tasks {
       // where this lint error triggers.
       compilerArgs.add("-Xlint:-cast")
     }
+  }
+}
+
+protobuf {
+  protoc {
+    // The artifact spec for the Protobuf Compiler
+    artifact = "com.google.protobuf:protoc:3.3.0"
+    if (osdetector.os == "osx") {
+      // Always use x86_64 version as ARM binary is not available
+      artifact += ":osx-x86_64"
+    }
+  }
+  plugins {
+    id("grpc") {
+      artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion"
+      if (osdetector.os == "osx") {
+        // Always use x86_64 version as ARM binary is not available
+        artifact += ":osx-x86_64"
+      }
+    }
+  }
+  generateProtoTasks {
+    all().configureEach {
+      plugins {
+        id("grpc")
+      }
+    }
+  }
+}
+
+afterEvaluate {
+  // Classpath when compiling protos, we add dependency management directly
+  // since it doesn't follow Gradle conventions of naming / properties.
+  dependencies {
+    add("compileProtoPath", platform(project(":dependencyManagement")))
+    add("testCompileProtoPath", platform(project(":dependencyManagement")))
   }
 }
