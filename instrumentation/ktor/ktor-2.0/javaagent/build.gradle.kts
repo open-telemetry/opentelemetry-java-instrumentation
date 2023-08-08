405427f5d3bd392dca507dbc0c5818da7b8fbe97
Lauri Tulmin <ltulmin@splunk.com>
2023-08-08 17:09:37 +0300

    Add javaagent instrumentation for ktor2 (#9149)

--- instrumentation/ktor/ktor-2.0/javaagent/build.gradle.kts
+++ instrumentation/ktor/ktor-2.0/javaagent/build.gradle.kts
@@ -1,9 +1,16 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("otel.library-instrumentation")
-
   id("org.jetbrains.kotlin.jvm")
+  id("otel.javaagent-instrumentation")
+}
+
+muzzle {
+  pass {
+    group.set("org.jetbrains.kotlinx")
+    module.set("ktor-server-core")
+    versions.set("[2.0.0,)")
+  }
 }
 
 val ktorVersion = "2.0.0"
@@ -12,13 +19,15 @@ dependencies {
   library("io.ktor:ktor-client-core:$ktorVersion")
   library("io.ktor:ktor-server-core:$ktorVersion")
 
-  implementation(project(":instrumentation:ktor:ktor-common:library"))
-  implementation("io.opentelemetry:opentelemetry-extension-kotlin")
+  implementation(project(":instrumentation:ktor:ktor-2.0:library"))
 
   compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
+  testInstrumentation(project(":instrumentation:netty:netty-4.1:javaagent"))
+
   testImplementation(project(":instrumentation:ktor:ktor-2.0:testing"))
   testImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+  testImplementation("io.opentelemetry:opentelemetry-extension-kotlin")
 
   testLibrary("io.ktor:ktor-server-netty:$ktorVersion")
   testLibrary("io.ktor:ktor-client-cio:$ktorVersion")
