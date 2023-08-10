b87e3a35ed0189d5fdee482212089a179f112d39
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2023-08-10 12:39:35 +0200

    Fix context propagation in Executor#execute() for non-capturing lambdas

---
+++ instrumentation/executors/bootstrap/src/main/java/io/opentelemetry/javaagent/bootstrap/executors/ContextPropagatingRunnable.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.bootstrap.executors;
+
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
+
+public final class ContextPropagatingRunnable implements Runnable {
+
+  public static boolean shouldDecorateRunnable(Runnable task) {
+    // We wrap only lambdas' anonymous classes and if given object has not already been wrapped.
+    // Anonymous classes have '/' in class name which is not allowed in 'normal' classes.
+    return task.getClass().getName().contains("/") && !(task instanceof ContextPropagatingRunnable);
+  }
+
+  public static Runnable propagateContext(Runnable task, Context context) {
+    return new ContextPropagatingRunnable(task, context);
+  }
+
+  private final Runnable delegate;
+  private final Context context;
+
+  private ContextPropagatingRunnable(Runnable delegate, Context context) {
+    this.delegate = delegate;
+    this.context = context;
+  }
+
+  @Override
+  public void run() {
+    try (Scope ignored = context.makeCurrent()) {
+      delegate.run();
+    }
+  }
+}
