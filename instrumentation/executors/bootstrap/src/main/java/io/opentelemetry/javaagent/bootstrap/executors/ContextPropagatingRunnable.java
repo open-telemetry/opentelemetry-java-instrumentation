6d61f6ee60fb6ab79eaab661236914d8c707b368
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2023-08-11 13:03:14 +0200

    spotless

--- instrumentation/executors/bootstrap/src/main/java/io/opentelemetry/javaagent/bootstrap/executors/ContextPropagatingRunnable.java
+++ instrumentation/executors/bootstrap/src/main/java/io/opentelemetry/javaagent/bootstrap/executors/ContextPropagatingRunnable.java
@@ -13,7 +13,8 @@ public final class ContextPropagatingRunnable implements Runnable {
   public static boolean shouldDecorateRunnable(Runnable task) {
     // We wrap only lambdas' anonymous classes and if given object has not already been wrapped.
     // Anonymous classes have '/' in class name which is not allowed in 'normal' classes.
-    // note: it is always safe to decorate lambdas since downstream code cannot be expecting a specific runnable implementation anyways
+    // note: it is always safe to decorate lambdas since downstream code cannot be expecting a
+    // specific runnable implementation anyways
     return task.getClass().getName().contains("/") && !(task instanceof ContextPropagatingRunnable);
   }
 
