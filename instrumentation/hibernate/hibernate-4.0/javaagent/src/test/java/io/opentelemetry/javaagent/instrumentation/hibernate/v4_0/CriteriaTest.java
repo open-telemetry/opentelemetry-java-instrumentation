8a82e3a180f612f89c5dca759728c547957a7aea
Jay DeLuca <jaydeluca4@gmail.com>
2023-08-13 12:08:45 -0400

    last suppression for createCriteria

--- instrumentation/hibernate/hibernate-4.0/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/hibernate/v4_0/CriteriaTest.java
+++ instrumentation/hibernate/hibernate-4.0/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/hibernate/v4_0/CriteriaTest.java
@@ -22,6 +22,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+@SuppressWarnings("deprecation") // testing deprecated functionality (createCriteria)
 class CriteriaTest extends AbstractHibernateTest {
 
   @ParameterizedTest
