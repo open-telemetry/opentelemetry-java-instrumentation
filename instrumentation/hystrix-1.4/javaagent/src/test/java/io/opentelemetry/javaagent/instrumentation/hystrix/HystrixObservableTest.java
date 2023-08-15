61b063877539e3d5d0d00839f34e594e29a3eb64
Jay DeLuca <jaydeluca4@gmail.com>
2023-08-15 19:25:08 -0400

    more work on observable

--- instrumentation/hystrix-1.4/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/hystrix/HystrixObservableTest.java
+++ instrumentation/hystrix-1.4/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/hystrix/HystrixObservableTest.java
@@ -8,7 +8,12 @@ package io.opentelemetry.javaagent.instrumentation.hystrix;
 import static io.opentelemetry.api.common.AttributeKey.booleanKey;
 import static io.opentelemetry.api.common.AttributeKey.stringKey;
 import static io.opentelemetry.sdk.testing.assertj.OpenTelemetryAssertions.equalTo;
+import static io.opentelemetry.sdk.testing.assertj.OpenTelemetryAssertions.satisfies;
+import static io.opentelemetry.semconv.trace.attributes.SemanticAttributes.EXCEPTION_MESSAGE;
+import static io.opentelemetry.semconv.trace.attributes.SemanticAttributes.EXCEPTION_STACKTRACE;
+import static io.opentelemetry.semconv.trace.attributes.SemanticAttributes.EXCEPTION_TYPE;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchException;
 import static org.junit.jupiter.api.Named.named;
 
 import com.netflix.hystrix.HystrixCommandGroupKey;
@@ -16,6 +21,7 @@ import com.netflix.hystrix.HystrixCommandProperties;
 import com.netflix.hystrix.HystrixObservableCommand;
 import io.opentelemetry.instrumentation.testing.junit.AgentInstrumentationExtension;
 import io.opentelemetry.instrumentation.testing.junit.InstrumentationExtension;
+import io.opentelemetry.sdk.trace.data.StatusData;
 import java.util.Objects;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -37,7 +43,6 @@ class HystrixObservableTest {
 
   @ParameterizedTest
   @MethodSource("provideCommandActionArguments")
-  @SuppressWarnings("RxReturnValueIgnored")
   void testCommands(Parameter parameter) {
 
     String result =
@@ -45,7 +50,7 @@ class HystrixObservableTest {
             "parent",
             () -> {
               HystrixObservableCommand<String> val =
-                  new HystrixObservableCommand<String>(setter()) {
+                  new HystrixObservableCommand<String>(setter("ExampleGroup")) {
                     private String tracedMethod() {
                       testing.runWithSpan("tracedMethod", () -> {});
                       return "Hello!";
@@ -83,7 +88,7 @@ class HystrixObservableTest {
                 span -> span.hasName("tracedMethod").hasParent(trace.getSpan(1))));
   }
 
-  private static Stream<Arguments> provideCommandActionArguments() {
+  private static Stream<Arguments> baseArguments() {
     return Stream.of(
         Arguments.of(
             named(
@@ -196,41 +201,294 @@ class HystrixObservableTest {
             named(
                 "observe+NT",
                 new Parameter(
-                    null, Schedulers.newThread(), cmd -> cmd.observe().toBlocking().first()))),
-        Arguments.of(
-            named(
-                "toObservable block",
-                new Parameter(
-                    Schedulers.computation(),
-                    Schedulers.newThread(),
-                    cmd -> {
-                      BlockingQueue<String> queue = new LinkedBlockingQueue<>();
-                      Subscription subscription =
-                          cmd.observe()
-                              .subscribe(
-                                  next -> {
-                                    try {
-                                      queue.put(next);
-                                    } catch (InterruptedException e) {
-                                      throw new RuntimeException(e);
-                                    }
-                                  });
-                      String returnValue;
-                      try {
-                        returnValue = queue.take();
-
-                      } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                      }
-                      subscription.unsubscribe();
-                      return returnValue;
-                    }))));
+                    null, Schedulers.newThread(), cmd -> cmd.observe().toBlocking().first()))));
   }
 
-  private static HystrixObservableCommand.Setter setter() {
+  private static Stream<Arguments> provideCommandActionArguments() {
+    return Stream.concat(
+        baseArguments(),
+        Stream.of(
+            Arguments.of(
+                named(
+                    "toObservable block",
+                    new Parameter(
+                        Schedulers.computation(),
+                        Schedulers.newThread(),
+                        cmd -> {
+                          BlockingQueue<String> queue = new LinkedBlockingQueue<>();
+                          Subscription subscription =
+                              cmd.toObservable()
+                                  .subscribe(
+                                      next -> {
+                                        try {
+                                          queue.put(next);
+                                        } catch (InterruptedException e) {
+                                          throw new RuntimeException(e);
+                                        }
+                                      });
+                          String returnValue;
+                          try {
+                            returnValue = queue.take();
+
+                          } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                          }
+                          subscription.unsubscribe();
+                          return returnValue;
+                        })))));
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideCommandFallbackArguments")
+  void testCommandFallbacks(Parameter parameter) {
+
+    String result =
+        testing.runWithSpan(
+            "parent",
+            () -> {
+              HystrixObservableCommand<String> val =
+                  new HystrixObservableCommand<String>(setter("ExampleGroup")) {
+                    @Override
+                    protected Observable<String> construct() {
+                      Observable<String> err =
+                          Observable.defer(() -> Observable.error(new IllegalArgumentException()))
+                              .map(
+                                  error -> {
+                                    if (error instanceof Throwable) {
+                                      return ((Throwable) error).getMessage();
+                                    } else {
+                                      throw new IllegalStateException("Expected Throwable result");
+                                    }
+                                  })
+                              .repeat(1);
+                      if (parameter.observeOn != null) {
+                        err = err.observeOn(parameter.observeOn);
+                      }
+                      if (parameter.subscribeOn != null) {
+                        err = err.subscribeOn(parameter.subscribeOn);
+                      }
+                      return err;
+                    }
+
+                    @Override
+                    protected Observable<String> resumeWithFallback() {
+                      return Observable.just("Fallback!").repeat(1);
+                    }
+                  };
+              return parameter.operation.apply(val);
+            });
+
+    assertThat(Objects.equals(result, "Fallback!")).isTrue();
+
+    testing.waitAndAssertTraces(
+        trace ->
+            trace.hasSpansSatisfyingExactly(
+                span -> span.hasName("parent").hasNoParent(),
+                span ->
+                    span.hasName("ExampleGroup.HystrixObservableTest$2.execute")
+                        .hasParent(trace.getSpan(0))
+                        .hasStatus(StatusData.error())
+                        .hasEventsSatisfyingExactly(
+                            event ->
+                                event
+                                    .hasName("exception")
+                                    .hasAttributesSatisfyingExactly(
+                                        equalTo(
+                                            EXCEPTION_TYPE, "java.lang.IllegalArgumentException"),
+                                        satisfies(
+                                            EXCEPTION_STACKTRACE,
+                                            val -> val.isInstanceOf(String.class)))),
+                span ->
+                    span.hasName("ExampleGroup.HystrixObservableTest$2.fallback")
+                        .hasParent(trace.getSpan(1))
+                        .hasAttributesSatisfyingExactly(
+                            equalTo(stringKey("hystrix.command"), "HystrixObservableTest$2"),
+                            equalTo(stringKey("hystrix.group"), "ExampleGroup"),
+                            equalTo(booleanKey("hystrix.circuit_open"), false))));
+  }
+
+  private static Stream<Arguments> provideCommandFallbackArguments() {
+    return Stream.concat(
+        baseArguments(),
+        Stream.of(
+            Arguments.of(
+                named(
+                    "toObservable block",
+                    new Parameter(
+                        null,
+                        null,
+                        cmd -> {
+                          BlockingQueue<String> queue = new LinkedBlockingQueue<>();
+                          Subscription subscription =
+                              cmd.toObservable()
+                                  .subscribe(
+                                      next -> {
+                                        try {
+                                          queue.put(next);
+                                        } catch (InterruptedException e) {
+                                          throw new RuntimeException(e);
+                                        }
+                                      });
+                          String returnValue;
+                          try {
+                            returnValue = queue.take();
+                          } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                          }
+                          subscription.unsubscribe();
+                          return returnValue;
+                        })))));
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideCommandNoFallbackResultsInErrorArguments")
+  void testNoFallbackResultsInErrorForAction(Parameter parameter) {
+
+    Throwable exception =
+        catchException(
+            () ->
+                testing.runWithSpan(
+                    "parent",
+                    () -> {
+                      HystrixObservableCommand<String> val =
+                          new HystrixObservableCommand<String>(setter("FailingGroup")) {
+                            @Override
+                            protected Observable<String> construct() {
+                              Observable<String> err =
+                                  Observable.defer(
+                                          () -> Observable.error(new IllegalArgumentException()))
+                                      .map(
+                                          error -> {
+                                            if (error instanceof Throwable) {
+                                              return ((Throwable) error).getMessage();
+                                            } else {
+                                              throw new IllegalStateException(
+                                                  "Expected Throwable result");
+                                            }
+                                          })
+                                      .repeat(1);
+                              if (parameter.observeOn != null) {
+                                err = err.observeOn(parameter.observeOn);
+                              }
+                              if (parameter.subscribeOn != null) {
+                                err = err.subscribeOn(parameter.subscribeOn);
+                              }
+                              return err;
+                            }
+                          };
+                      return parameter.operation.apply(val);
+                    }));
+
+    assertThat(exception).hasCauseInstanceOf(IllegalArgumentException.class);
+
+    testing.waitAndAssertTraces(
+        trace ->
+            trace.hasSpansSatisfyingExactly(
+                span ->
+                    span.hasName("parent")
+                        .hasNoParent()
+                        .hasStatus(StatusData.error())
+                        .hasEventsSatisfyingExactly(
+                            event ->
+                                event
+                                    .hasName("exception")
+                                    .hasAttributesSatisfyingExactly(
+                                        satisfies(
+                                            EXCEPTION_STACKTRACE,
+                                            val -> val.isInstanceOf(String.class)),
+                                        equalTo(
+                                            EXCEPTION_TYPE,
+                                            "com.netflix.hystrix.exception.HystrixRuntimeException"),
+                                        equalTo(
+                                            EXCEPTION_MESSAGE,
+                                            "HystrixObservableTest$3 failed and no fallback available."))),
+                span ->
+                    span.hasName("FailingGroup.HystrixObservableTest$3.execute")
+                        .hasParent(trace.getSpan(0))
+                        .hasStatus(StatusData.error())
+                        .hasEventsSatisfyingExactly(
+                            event ->
+                                event
+                                    .hasName("exception")
+                                    .hasAttributesSatisfyingExactly(
+                                        equalTo(
+                                            EXCEPTION_TYPE, "java.lang.IllegalArgumentException"),
+                                        satisfies(
+                                            EXCEPTION_STACKTRACE,
+                                            val -> val.isInstanceOf(String.class))))
+                        .hasAttributesSatisfyingExactly(
+                            equalTo(stringKey("hystrix.command"), "HystrixObservableTest$3"),
+                            equalTo(stringKey("hystrix.group"), "FailingGroup"),
+                            equalTo(booleanKey("hystrix.circuit_open"), false)),
+                span ->
+                    span.hasName("FailingGroup.HystrixObservableTest$3.fallback")
+                        .hasParent(trace.getSpan(1))
+                        .hasEventsSatisfyingExactly(
+                            event ->
+                                event
+                                    .hasName("exception")
+                                    .hasAttributesSatisfyingExactly(
+                                        equalTo(
+                                            EXCEPTION_TYPE,
+                                            "java.lang.UnsupportedOperationException"),
+                                        satisfies(
+                                            EXCEPTION_STACKTRACE,
+                                            val -> val.isInstanceOf(String.class)),
+                                        equalTo(EXCEPTION_MESSAGE, "No fallback available.")))
+                        .hasAttributesSatisfyingExactly(
+                            equalTo(stringKey("hystrix.command"), "HystrixObservableTest$3"),
+                            equalTo(stringKey("hystrix.group"), "FailingGroup"),
+                            equalTo(booleanKey("hystrix.circuit_open"), false))));
+  }
+
+  private static Stream<Arguments> provideCommandNoFallbackResultsInErrorArguments() {
+    return Stream.concat(
+        baseArguments(),
+        Stream.of(
+            Arguments.of(
+                named(
+                    "toObservable block",
+                    new Parameter(
+                        Schedulers.computation(),
+                        Schedulers.newThread(),
+                        cmd -> {
+                          BlockingQueue<Throwable> queue = new LinkedBlockingQueue<>();
+                          Subscription subscription =
+                              cmd.toObservable()
+                                  .subscribe(
+                                      next -> {
+                                        try {
+                                          queue.put(new Exception("Unexpectedly got a next"));
+                                        } catch (InterruptedException e) {
+                                          throw new IllegalArgumentException(e);
+                                        }
+                                      },
+                                      next -> {
+                                        try {
+                                          queue.put(next);
+                                        } catch (InterruptedException e) {
+                                          throw new RuntimeException(e);
+                                        }
+                                      });
+                          Throwable returnValue;
+                          try {
+                            returnValue = queue.take();
+
+                          } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                          }
+                          subscription.unsubscribe();
+                          try {
+                            throw returnValue;
+                          } catch (Throwable e) {
+                            throw new IllegalArgumentException();
+                          }
+                        })))));
+  }
+
+  private static HystrixObservableCommand.Setter setter(String key) {
     HystrixObservableCommand.Setter setter =
-        HystrixObservableCommand.Setter.withGroupKey(
-            HystrixCommandGroupKey.Factory.asKey("ExampleGroup"));
+        HystrixObservableCommand.Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey(key));
     setter.andCommandPropertiesDefaults(
         HystrixCommandProperties.Setter().withExecutionTimeoutInMilliseconds(10_000));
     return setter;
