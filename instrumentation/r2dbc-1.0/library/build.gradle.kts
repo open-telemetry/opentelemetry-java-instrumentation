b18b1ff6734d0171aa5d4038dc89cdab80854ab6
Trask Stalnaker <trask.stalnaker@gmail.com>
2023-08-15 20:03:32 -0700

    Fix unmanaged dependency

--- instrumentation/r2dbc-1.0/library/build.gradle.kts
+++ instrumentation/r2dbc-1.0/library/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 dependencies {
   library("io.r2dbc:r2dbc-spi:1.0.0.RELEASE")
-  implementation("io.r2dbc:r2dbc-proxy:1.0.1.RELEASE")
+  implementation("io.r2dbc:r2dbc-proxy")
 
   testImplementation(project(":instrumentation:r2dbc-1.0:testing"))
   testImplementation(project(":instrumentation:reactor:reactor-3.1:library"))
