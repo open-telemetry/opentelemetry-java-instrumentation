64877c6beb4055d6e7a32021c5f39dcabe94c1b6
Lauri Tulmin <ltulmin@splunk.com>
2023-08-22 13:22:35 +0300

    update tag

--- smoke-tests/src/test/groovy/io/opentelemetry/smoketest/AppServerTest.groovy
+++ smoke-tests/src/test/groovy/io/opentelemetry/smoketest/AppServerTest.groovy
@@ -54,7 +54,7 @@ abstract class AppServerTest extends SmokeTest {
   @Override
   protected String getTargetImage(String jdk, String serverVersion, boolean windows) {
     String platformSuffix = windows ? "-windows" : ""
-    String extraTag = "20230821.5926514671"
+    String extraTag = "20230822.5936895750"
     String fullSuffix = "${serverVersion}-jdk$jdk$platformSuffix-$extraTag"
     return getTargetImagePrefix() + ":" + fullSuffix
   }
