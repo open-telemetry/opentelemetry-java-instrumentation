fabbf96fee356ed92cdcd198f0b76154e8a601d6
Jay DeLuca <jaydeluca4@gmail.com>
2023-08-23 20:13:19 -0400

    convert hibernate-precedure-call-4.3 tests from groovy to java

--- instrumentation/hibernate/hibernate-procedure-call-4.3/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/hibernate/v4_3/Value.java
+++ instrumentation/hibernate/hibernate-procedure-call-4.3/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/hibernate/v4_3/Value.java
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+package io.opentelemetry.javaagent.instrumentation.hibernate.v4_3;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
