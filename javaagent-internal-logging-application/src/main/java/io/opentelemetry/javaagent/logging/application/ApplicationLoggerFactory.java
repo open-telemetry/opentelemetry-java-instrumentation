b7f18c368de063d3ae1329055d751a9bbacc6b81
Lauri Tulmin <ltulmin@splunk.com>
2023-08-30 11:38:59 +0300

    Fix flaky application logging test

--- javaagent-internal-logging-application/src/main/java/io/opentelemetry/javaagent/logging/application/ApplicationLoggerFactory.java
+++ javaagent-internal-logging-application/src/main/java/io/opentelemetry/javaagent/logging/application/ApplicationLoggerFactory.java
@@ -53,6 +53,7 @@ final class ApplicationLoggerFactory extends ApplicationLoggerBridge
         .forEach(
             logger -> logger.replaceByActualLogger(applicationLoggerFactory.create(logger.name())));
     this.actual = applicationLoggerFactory;
+    inMemoryLogStore.setApplicationLoggerFactory(applicationLoggerFactory);
 
     // if there are any leftover logs left in the memory store, flush them - this will cause some
     // logs to go out of order, but at least we'll not lose any of them
