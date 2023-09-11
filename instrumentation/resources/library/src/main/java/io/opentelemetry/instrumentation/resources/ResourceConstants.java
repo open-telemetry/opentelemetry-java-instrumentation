1eb5320dd32adaa3b5298a00d23e52c9ea29d88f
Lauri Tulmin <ltulmin@splunk.com>
2023-09-11 20:17:38 +0300

    make class package private

--- instrumentation/resources/library/src/main/java/io/opentelemetry/instrumentation/resources/ResourceConstants.java
+++ instrumentation/resources/library/src/main/java/io/opentelemetry/instrumentation/resources/ResourceConstants.java
@@ -1,7 +1,7 @@
 package io.opentelemetry.instrumentation.resources;
 
 // TODO: this can be deleted after we start using the new semantic conventions artifact
-public final class ResourceConstants {
+final class ResourceConstants {
   public static final String SCHEMA_URL = "https://opentelemetry.io/schemas/1.21.0";
 
   private ResourceConstants() {}
