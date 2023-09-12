abba090b1bda15386518d4068b339bbacac97a84
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2023-09-12 16:31:43 +0200

    invert ifs

--- instrumentation-api-semconv/src/main/java/io/opentelemetry/instrumentation/api/instrumenter/http/HttpMetricsAdvice.java
+++ instrumentation-api-semconv/src/main/java/io/opentelemetry/instrumentation/api/instrumenter/http/HttpMetricsAdvice.java
@@ -21,141 +21,148 @@ import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
 final class HttpMetricsAdvice {
 
   static void applyStableClientDurationAdvice(DoubleHistogramBuilder builder) {
-    if (builder instanceof ExtendedDoubleHistogramBuilder) {
-      ((ExtendedDoubleHistogramBuilder) builder)
-          .setAdvice(
-              advice ->
-                  advice.setAttributes(
-                      asList(
-                          HttpAttributes.HTTP_REQUEST_METHOD,
-                          HttpAttributes.HTTP_RESPONSE_STATUS_CODE,
-                          NetworkAttributes.NETWORK_PROTOCOL_NAME,
-                          NetworkAttributes.NETWORK_PROTOCOL_VERSION,
-                          NetworkAttributes.SERVER_ADDRESS,
-                          NetworkAttributes.SERVER_PORT,
-                          NetworkAttributes.SERVER_SOCKET_ADDRESS)));
+    if (!(builder instanceof ExtendedDoubleHistogramBuilder)) {
+      return;
     }
+    ((ExtendedDoubleHistogramBuilder) builder)
+        .setAdvice(
+            advice ->
+                advice.setAttributes(
+                    asList(
+                        HttpAttributes.HTTP_REQUEST_METHOD,
+                        HttpAttributes.HTTP_RESPONSE_STATUS_CODE,
+                        NetworkAttributes.NETWORK_PROTOCOL_NAME,
+                        NetworkAttributes.NETWORK_PROTOCOL_VERSION,
+                        NetworkAttributes.SERVER_ADDRESS,
+                        NetworkAttributes.SERVER_PORT,
+                        NetworkAttributes.SERVER_SOCKET_ADDRESS)));
   }
 
   static void applyOldClientDurationAdvice(DoubleHistogramBuilder builder) {
-    if (builder instanceof ExtendedDoubleHistogramBuilder) {
-      ((ExtendedDoubleHistogramBuilder) builder)
-          .setAdvice(
-              advice ->
-                  advice.setAttributes(
-                      asList(
-                          SemanticAttributes.HTTP_METHOD,
-                          SemanticAttributes.HTTP_STATUS_CODE,
-                          SemanticAttributes.NET_PEER_NAME,
-                          SemanticAttributes.NET_PEER_PORT,
-                          SemanticAttributes.NET_PROTOCOL_NAME,
-                          SemanticAttributes.NET_PROTOCOL_VERSION,
-                          SemanticAttributes.NET_SOCK_PEER_ADDR)));
+    if (!(builder instanceof ExtendedDoubleHistogramBuilder)) {
+      return;
     }
+    ((ExtendedDoubleHistogramBuilder) builder)
+        .setAdvice(
+            advice ->
+                advice.setAttributes(
+                    asList(
+                        SemanticAttributes.HTTP_METHOD,
+                        SemanticAttributes.HTTP_STATUS_CODE,
+                        SemanticAttributes.NET_PEER_NAME,
+                        SemanticAttributes.NET_PEER_PORT,
+                        SemanticAttributes.NET_PROTOCOL_NAME,
+                        SemanticAttributes.NET_PROTOCOL_VERSION,
+                        SemanticAttributes.NET_SOCK_PEER_ADDR)));
   }
 
   static void applyClientRequestSizeAdvice(LongHistogramBuilder builder) {
-    if (builder instanceof ExtendedLongHistogramBuilder) {
-      ((ExtendedLongHistogramBuilder) builder)
-          .setAdvice(
-              advice ->
-                  advice.setAttributes(
-                      asList(
-                          // stable attributes
-                          HttpAttributes.HTTP_REQUEST_METHOD,
-                          HttpAttributes.HTTP_RESPONSE_STATUS_CODE,
-                          NetworkAttributes.NETWORK_PROTOCOL_NAME,
-                          NetworkAttributes.NETWORK_PROTOCOL_VERSION,
-                          NetworkAttributes.SERVER_ADDRESS,
-                          NetworkAttributes.SERVER_PORT,
-                          NetworkAttributes.SERVER_SOCKET_ADDRESS,
-                          // old attributes
-                          SemanticAttributes.HTTP_METHOD,
-                          SemanticAttributes.HTTP_STATUS_CODE,
-                          SemanticAttributes.NET_PEER_NAME,
-                          SemanticAttributes.NET_PEER_PORT,
-                          SemanticAttributes.NET_PROTOCOL_NAME,
-                          SemanticAttributes.NET_PROTOCOL_VERSION,
-                          SemanticAttributes.NET_SOCK_PEER_ADDR)));
+    if (!(builder instanceof ExtendedLongHistogramBuilder)) {
+      return;
     }
+    ((ExtendedLongHistogramBuilder) builder)
+        .setAdvice(
+            advice ->
+                advice.setAttributes(
+                    asList(
+                        // stable attributes
+                        HttpAttributes.HTTP_REQUEST_METHOD,
+                        HttpAttributes.HTTP_RESPONSE_STATUS_CODE,
+                        NetworkAttributes.NETWORK_PROTOCOL_NAME,
+                        NetworkAttributes.NETWORK_PROTOCOL_VERSION,
+                        NetworkAttributes.SERVER_ADDRESS,
+                        NetworkAttributes.SERVER_PORT,
+                        NetworkAttributes.SERVER_SOCKET_ADDRESS,
+                        // old attributes
+                        SemanticAttributes.HTTP_METHOD,
+                        SemanticAttributes.HTTP_STATUS_CODE,
+                        SemanticAttributes.NET_PEER_NAME,
+                        SemanticAttributes.NET_PEER_PORT,
+                        SemanticAttributes.NET_PROTOCOL_NAME,
+                        SemanticAttributes.NET_PROTOCOL_VERSION,
+                        SemanticAttributes.NET_SOCK_PEER_ADDR)));
   }
 
   static void applyStableServerDurationAdvice(DoubleHistogramBuilder builder) {
-    if (builder instanceof ExtendedDoubleHistogramBuilder) {
-      ((ExtendedDoubleHistogramBuilder) builder)
-          .setAdvice(
-              advice ->
-                  advice.setAttributes(
-                      asList(
-                          SemanticAttributes.HTTP_ROUTE,
-                          HttpAttributes.HTTP_REQUEST_METHOD,
-                          HttpAttributes.HTTP_RESPONSE_STATUS_CODE,
-                          NetworkAttributes.NETWORK_PROTOCOL_NAME,
-                          NetworkAttributes.NETWORK_PROTOCOL_VERSION,
-                          UrlAttributes.URL_SCHEME)));
+    if (!(builder instanceof ExtendedDoubleHistogramBuilder)) {
+      return;
     }
+    ((ExtendedDoubleHistogramBuilder) builder)
+        .setAdvice(
+            advice ->
+                advice.setAttributes(
+                    asList(
+                        SemanticAttributes.HTTP_ROUTE,
+                        HttpAttributes.HTTP_REQUEST_METHOD,
+                        HttpAttributes.HTTP_RESPONSE_STATUS_CODE,
+                        NetworkAttributes.NETWORK_PROTOCOL_NAME,
+                        NetworkAttributes.NETWORK_PROTOCOL_VERSION,
+                        UrlAttributes.URL_SCHEME)));
   }
 
   static void applyOldServerDurationAdvice(DoubleHistogramBuilder builder) {
-    if (builder instanceof ExtendedDoubleHistogramBuilder) {
-      ((ExtendedDoubleHistogramBuilder) builder)
-          .setAdvice(
-              advice ->
-                  advice.setAttributes(
-                      asList(
-                          SemanticAttributes.HTTP_SCHEME,
-                          SemanticAttributes.HTTP_ROUTE,
-                          SemanticAttributes.HTTP_METHOD,
-                          SemanticAttributes.HTTP_STATUS_CODE,
-                          SemanticAttributes.NET_HOST_NAME,
-                          SemanticAttributes.NET_HOST_PORT,
-                          SemanticAttributes.NET_PROTOCOL_NAME,
-                          SemanticAttributes.NET_PROTOCOL_VERSION)));
+    if (!(builder instanceof ExtendedDoubleHistogramBuilder)) {
+      return;
     }
+    ((ExtendedDoubleHistogramBuilder) builder)
+        .setAdvice(
+            advice ->
+                advice.setAttributes(
+                    asList(
+                        SemanticAttributes.HTTP_SCHEME,
+                        SemanticAttributes.HTTP_ROUTE,
+                        SemanticAttributes.HTTP_METHOD,
+                        SemanticAttributes.HTTP_STATUS_CODE,
+                        SemanticAttributes.NET_HOST_NAME,
+                        SemanticAttributes.NET_HOST_PORT,
+                        SemanticAttributes.NET_PROTOCOL_NAME,
+                        SemanticAttributes.NET_PROTOCOL_VERSION)));
   }
 
   static void applyServerRequestSizeAdvice(LongHistogramBuilder builder) {
-    if (builder instanceof ExtendedLongHistogramBuilder) {
-      ((ExtendedLongHistogramBuilder) builder)
-          .setAdvice(
-              advice ->
-                  advice.setAttributes(
-                      asList(
-                          // stable attributes
-                          SemanticAttributes.HTTP_ROUTE,
-                          HttpAttributes.HTTP_REQUEST_METHOD,
-                          HttpAttributes.HTTP_RESPONSE_STATUS_CODE,
-                          NetworkAttributes.NETWORK_PROTOCOL_NAME,
-                          NetworkAttributes.NETWORK_PROTOCOL_VERSION,
-                          UrlAttributes.URL_SCHEME,
-                          // old attributes
-                          SemanticAttributes.HTTP_SCHEME,
-                          SemanticAttributes.HTTP_ROUTE,
-                          SemanticAttributes.HTTP_METHOD,
-                          SemanticAttributes.HTTP_STATUS_CODE,
-                          SemanticAttributes.NET_HOST_NAME,
-                          SemanticAttributes.NET_HOST_PORT,
-                          SemanticAttributes.NET_PROTOCOL_NAME,
-                          SemanticAttributes.NET_PROTOCOL_VERSION)));
+    if (!(builder instanceof ExtendedLongHistogramBuilder)) {
+      return;
     }
+    ((ExtendedLongHistogramBuilder) builder)
+        .setAdvice(
+            advice ->
+                advice.setAttributes(
+                    asList(
+                        // stable attributes
+                        SemanticAttributes.HTTP_ROUTE,
+                        HttpAttributes.HTTP_REQUEST_METHOD,
+                        HttpAttributes.HTTP_RESPONSE_STATUS_CODE,
+                        NetworkAttributes.NETWORK_PROTOCOL_NAME,
+                        NetworkAttributes.NETWORK_PROTOCOL_VERSION,
+                        UrlAttributes.URL_SCHEME,
+                        // old attributes
+                        SemanticAttributes.HTTP_SCHEME,
+                        SemanticAttributes.HTTP_ROUTE,
+                        SemanticAttributes.HTTP_METHOD,
+                        SemanticAttributes.HTTP_STATUS_CODE,
+                        SemanticAttributes.NET_HOST_NAME,
+                        SemanticAttributes.NET_HOST_PORT,
+                        SemanticAttributes.NET_PROTOCOL_NAME,
+                        SemanticAttributes.NET_PROTOCOL_VERSION)));
   }
 
   static void applyServerActiveRequestsAdvice(LongUpDownCounterBuilder builder) {
-    if (builder instanceof ExtendedLongUpDownCounterBuilder) {
-      ((ExtendedLongUpDownCounterBuilder) builder)
-          .setAdvice(
-              advice ->
-                  advice.setAttributes(
-                      asList(
-                          // https://github.com/open-telemetry/opentelemetry-specification/blob/v1.20.0/specification/metrics/semantic_conventions/http-metrics.md#metric-httpserveractive_requests
-                          SemanticAttributes.HTTP_METHOD,
-                          SemanticAttributes.HTTP_SCHEME,
-                          SemanticAttributes.NET_HOST_NAME,
-                          SemanticAttributes.NET_HOST_PORT,
-                          // https://github.com/open-telemetry/semantic-conventions/blob/main/docs/http/http-metrics.md#metric-httpserveractive_requests
-                          HttpAttributes.HTTP_REQUEST_METHOD,
-                          UrlAttributes.URL_SCHEME)));
+    if (!(builder instanceof ExtendedLongUpDownCounterBuilder)) {
+      return;
     }
+    ((ExtendedLongUpDownCounterBuilder) builder)
+        .setAdvice(
+            advice ->
+                advice.setAttributes(
+                    asList(
+                        // https://github.com/open-telemetry/opentelemetry-specification/blob/v1.20.0/specification/metrics/semantic_conventions/http-metrics.md#metric-httpserveractive_requests
+                        SemanticAttributes.HTTP_METHOD,
+                        SemanticAttributes.HTTP_SCHEME,
+                        SemanticAttributes.NET_HOST_NAME,
+                        SemanticAttributes.NET_HOST_PORT,
+                        // https://github.com/open-telemetry/semantic-conventions/blob/main/docs/http/http-metrics.md#metric-httpserveractive_requests
+                        HttpAttributes.HTTP_REQUEST_METHOD,
+                        UrlAttributes.URL_SCHEME)));
   }
 
   private HttpMetricsAdvice() {}
