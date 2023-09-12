7ad7af4c272e807c4724ddfa617e14641802cea2
Lauri Tulmin <ltulmin@splunk.com>
2023-09-12 15:39:37 +0300

    spotless

--- instrumentation/reactor/reactor-kafka-1.0/testing/src/main/java/io/opentelemetry/javaagent/instrumentation/reactor/kafka/v1_0/AbstractReactorKafkaTest.java
+++ instrumentation/reactor/reactor-kafka-1.0/testing/src/main/java/io/opentelemetry/javaagent/instrumentation/reactor/kafka/v1_0/AbstractReactorKafkaTest.java
@@ -135,11 +135,7 @@ public abstract class AbstractReactorKafkaTest {
   protected void testSingleRecordProcess(
       Function<Consumer<ConsumerRecord<String, String>>, Disposable> subscriptionFunction) {
     Disposable disposable =
-        subscriptionFunction.apply(
-            record ->
-                testing.runWithSpan(
-                    "consumer",
-                    () -> {}));
+        subscriptionFunction.apply(record -> testing.runWithSpan("consumer", () -> {}));
     cleanup.deferCleanup(disposable::dispose);
 
     SenderRecord<String, String, Object> record =
