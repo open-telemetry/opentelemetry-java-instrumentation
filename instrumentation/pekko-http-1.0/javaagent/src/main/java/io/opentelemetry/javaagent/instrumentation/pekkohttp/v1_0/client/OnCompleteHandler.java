f622cd42690206afceae2b54d2d111c1e6519e05
samuel.wright <samuel.wright@kognic.com>
2023-09-27 10:38:29 +0200

    spotlessApply

--- instrumentation/pekko-http-1.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/pekkohttp/v1_0/client/OnCompleteHandler.java
+++ instrumentation/pekko-http-1.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/pekkohttp/v1_0/client/OnCompleteHandler.java
@@ -5,13 +5,13 @@
 
 package io.opentelemetry.javaagent.instrumentation.pekkohttp.v1_0.client;
 
+import static io.opentelemetry.javaagent.instrumentation.pekkohttp.v1_0.client.PekkoHttpClientSingletons.instrumenter;
+
 import io.opentelemetry.context.Context;
 import org.apache.pekko.http.scaladsl.model.HttpRequest;
 import org.apache.pekko.http.scaladsl.model.HttpResponse;
 import scala.runtime.AbstractFunction1;
 import scala.util.Try;
-
-import static io.opentelemetry.javaagent.instrumentation.pekkohttp.v1_0.client.PekkoHttpClientSingletons.instrumenter;
 
 public class OnCompleteHandler extends AbstractFunction1<Try<HttpResponse>, Void> {
   private final Context context;
