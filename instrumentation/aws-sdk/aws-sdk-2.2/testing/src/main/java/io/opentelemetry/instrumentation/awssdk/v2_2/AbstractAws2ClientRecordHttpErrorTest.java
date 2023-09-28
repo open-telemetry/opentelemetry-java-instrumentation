3a347a5e30c157889da0d7d0cfcc57f69ba401fc
Ping Xiang <pinxiang@amazon.com>
2023-09-28 23:28:10 +0000

    modify the test to verify the input stream of response body is still usable

--- instrumentation/aws-sdk/aws-sdk-2.2/testing/src/main/java/io/opentelemetry/instrumentation/awssdk/v2_2/AbstractAws2ClientRecordHttpErrorTest.java
+++ instrumentation/aws-sdk/aws-sdk-2.2/testing/src/main/java/io/opentelemetry/instrumentation/awssdk/v2_2/AbstractAws2ClientRecordHttpErrorTest.java
@@ -17,12 +17,25 @@ import io.opentelemetry.testing.internal.armeria.common.HttpResponse;
 import io.opentelemetry.testing.internal.armeria.common.HttpStatus;
 import io.opentelemetry.testing.internal.armeria.common.MediaType;
 import io.opentelemetry.testing.internal.armeria.testing.junit5.server.mock.MockWebServerExtension;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
+import software.amazon.awssdk.core.interceptor.Context;
+import software.amazon.awssdk.core.interceptor.ExecutionAttributes;
+import software.amazon.awssdk.core.interceptor.ExecutionInterceptor;
+import software.amazon.awssdk.http.SdkHttpResponse;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClientBuilder;
@@ -35,6 +48,7 @@ public abstract class AbstractAws2ClientRecordHttpErrorTest {
           AwsBasicCredentials.create("my-access-key", "my-secret-key"));
 
   private static final MockWebServerExtension server = new MockWebServerExtension();
+  protected static List<String> httpErrorMessages = new ArrayList<>();
 
   @BeforeAll
   public static void setupSpec() {
@@ -49,6 +63,51 @@ public abstract class AbstractAws2ClientRecordHttpErrorTest {
 
   protected abstract InstrumentationExtension getTesting();
 
+  // Introducing a new ExecutionInterceptor that's registered with the AWS SDK.
+  // It's positioned to execute after the TracingExecutionInterceptor used for SDK instrumentation.
+  // The purpose of this interceptor is to inspect the response body of failed HTTP requests.
+  // We aim to ensure that the HTTP error message remains accessible in the response body's
+  // InputStream
+  // even after the TracingExecutionInterceptor has processed it.
+  static class ResponseCheckInterceptor implements ExecutionInterceptor {
+    @Override
+    public Optional<InputStream> modifyHttpResponseContent(
+        Context.ModifyHttpResponse context, ExecutionAttributes executionAttributes) {
+      Optional<InputStream> responseBody = context.responseBody();
+      String errorMsg = extractHttpErrorMessage(context, executionAttributes);
+      if (errorMsg != null) {
+        return Optional.of(new ByteArrayInputStream(errorMsg.getBytes(Charset.defaultCharset())));
+      }
+      return responseBody;
+    }
+
+    private static String extractHttpErrorMessage(
+        Context.AfterTransmission context, ExecutionAttributes executionAttributes) {
+      SdkHttpResponse response = context.httpResponse();
+      if (executionAttributes == null) {
+        return "";
+      }
+
+      if (response != null && !response.isSuccessful()) {
+        Optional<InputStream> responseBody = context.responseBody();
+        if (responseBody.isPresent()) {
+          String errorMsg =
+              new BufferedReader(
+                      new InputStreamReader(responseBody.get(), Charset.defaultCharset()))
+                  .lines()
+                  .collect(Collectors.joining("\n"));
+          httpErrorMessages.add(errorMsg);
+          return errorMsg;
+        }
+      }
+      return null;
+    }
+  }
+
+  private static void cleanResponses() {
+    httpErrorMessages.clear();
+  }
+
   public boolean isRecordIndividualHttpErrorEnabled() {
     // See io.opentelemetry.instrumentation.awssdk.v2_2.autoconfigure.TracingExecutionInterceptor
     return ConfigPropertiesUtil.getBoolean(
@@ -56,15 +115,20 @@ public abstract class AbstractAws2ClientRecordHttpErrorTest {
   }
 
   @Test
+  // Suppressing deprecation because we use some deprecated attributes in the test
   @SuppressWarnings("deprecation")
   public void testSendDynamoDbRequestWithRetries() {
+    cleanResponses();
     // Setup and configuration
     String service = "DynamoDb";
     String operation = "PutItem";
     String method = "POST";
     String requestId = "UNKNOWN";
     DynamoDbClientBuilder builder = DynamoDbClient.builder();
-    builder.overrideConfiguration(createOverrideConfigurationBuilder().build());
+    ClientOverrideConfiguration.Builder overrideConfigBuilder =
+        createOverrideConfigurationBuilder()
+            .addExecutionInterceptor(new ResponseCheckInterceptor());
+    builder.overrideConfiguration(overrideConfigBuilder.build());
 
     DynamoDbClient client =
         builder
@@ -135,5 +199,11 @@ public abstract class AbstractAws2ClientRecordHttpErrorTest {
                     }
                   });
             });
+
+    // make sure the response body input stream is still available and check its content to be
+    // expected
+    assertThat(httpErrorMessages.size()).isEqualTo(2);
+    assertThat(httpErrorMessages.get(0)).isEqualTo("DynamoDB could not process your request");
+    assertThat(httpErrorMessages.get(1)).isEqualTo("DynamoDB is currently unavailable");
   }
 }
