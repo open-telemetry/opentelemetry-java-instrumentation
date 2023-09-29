87b2ae09c18830bacaab39e22ffb69c2354de6b7
Lauri Tulmin <ltulmin@splunk.com>
2023-09-29 19:10:49 +0300

    Remove obsolete comment (#9578)

--- conventions/src/main/kotlin/otel.java-conventions.gradle.kts
+++ conventions/src/main/kotlin/otel.java-conventions.gradle.kts
@@ -122,7 +122,6 @@ abstract class NettyAlignmentRule : ComponentMetadataRule {
     with(ctx.details) {
       if (id.group == "io.netty" && id.name != "netty") {
         if (id.version.startsWith("4.1.")) {
-          // netty 4.1.98 does not run correctly on MacOS
           belongsTo("io.netty:netty-bom:4.1.99.Final", false)
         } else if (id.version.startsWith("4.0.")) {
           belongsTo("io.netty:netty-bom:4.0.56.Final", false)
