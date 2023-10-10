6f0fd8e63179f123a19388e8a07a4e1a97eb3724
Lauri Tulmin <ltulmin@splunk.com>
2023-10-10 10:21:30 +0300

    Suppress nested http client spans in aws2 instrumentation (#9634)

---
+++ instrumentation/aws-sdk/aws-sdk-2.2/library/src/main/java/io/opentelemetry/instrumentation/awssdk/v2_2/AwsSdkHttpClientSuppressionAttributesExtractor.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.instrumentation.awssdk.v2_2;
+
+import io.opentelemetry.api.common.AttributesBuilder;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.instrumentation.api.instrumenter.AttributesExtractor;
+import io.opentelemetry.instrumentation.api.instrumenter.http.HttpClientAttributesExtractor;
+import io.opentelemetry.instrumentation.api.internal.SpanKey;
+import io.opentelemetry.instrumentation.api.internal.SpanKeyProvider;
+import javax.annotation.Nullable;
+import software.amazon.awssdk.core.interceptor.ExecutionAttributes;
+import software.amazon.awssdk.http.SdkHttpResponse;
+
+/**
+ * An attribute extractor that reports implementing HTTP client semantic conventions. Adding this
+ * extractor suppresses nested HTTP client instrumentations similarly to how using {@link
+ * HttpClientAttributesExtractor} would.
+ */
+class AwsSdkHttpClientSuppressionAttributesExtractor
+    implements AttributesExtractor<ExecutionAttributes, SdkHttpResponse>, SpanKeyProvider {
+
+  @Override
+  public void onStart(
+      AttributesBuilder attributes,
+      Context parentContext,
+      ExecutionAttributes executionAttributes) {}
+
+  @Override
+  public void onEnd(
+      AttributesBuilder attributes,
+      Context context,
+      ExecutionAttributes executionAttributes,
+      @Nullable SdkHttpResponse sdkHttpResponse,
+      @Nullable Throwable error) {}
+
+  @Nullable
+  @Override
+  public SpanKey internalGetSpanKey() {
+    return SpanKey.HTTP_CLIENT;
+  }
+}
