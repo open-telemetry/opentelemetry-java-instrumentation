4f3a7147e0e737ef7b65c10fe17106fa288e5d1f
Matthieu MOREL <matthieu.morel35@gmail.com>
2023-10-12 05:55:47 +0200

    Update PeerServiceResolver.java

--- instrumentation-api-semconv/src/main/java/io/opentelemetry/instrumentation/api/instrumenter/net/PeerServiceResolver.java
+++ instrumentation-api-semconv/src/main/java/io/opentelemetry/instrumentation/api/instrumenter/net/PeerServiceResolver.java
@@ -6,6 +6,7 @@
 package io.opentelemetry.instrumentation.api.instrumenter.net;
 
 import java.util.Map;
+import java.util.function.Supplier;
 import javax.annotation.Nullable;
 
 public interface PeerServiceResolver {
@@ -13,7 +14,7 @@ public interface PeerServiceResolver {
   public boolean isEmpty();
 
   @Nullable
-  public String resolveService(String host, @Nullable Integer port, @Nullable String path);
+  public String resolveService(String host, @Nullable Integer port, @Nullable Supplier<String> path);
 
   static PeerServiceResolver create(Map<String, String> mapping) {
     return new PeerServiceResolverImpl(mapping);
