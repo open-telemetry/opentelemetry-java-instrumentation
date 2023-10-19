f8122ac808a59c7c7874e7fd6e6f5a89edf367b7
Jean Bisutti <jean.bisutti@gmail.com>
2023-10-19 17:10:36 +0200

    Add explanation and link

--- smoke-tests-otel-starter/src/test/java/io/opentelemetry/smoketest/OtelSpringStarterJdbcDriverConfigSmokeTest.java
+++ smoke-tests-otel-starter/src/test/java/io/opentelemetry/smoketest/OtelSpringStarterJdbcDriverConfigSmokeTest.java
@@ -8,6 +8,7 @@ package io.opentelemetry.smoketest;
 import org.junit.jupiter.api.condition.DisabledInNativeImage;
 import org.springframework.test.context.ActiveProfiles;
 
-@DisabledInNativeImage // Spring native does not support the Spring profiles
+@DisabledInNativeImage // Spring native does not support the profile setting at runtime:
+// https://docs.spring.io/spring-boot/docs/current/reference/html/howto.html#howto.aot.conditions
 @ActiveProfiles(value = "jdbc-driver-config")
 class OtelSpringStarterJdbcDriverConfigSmokeTest extends OtelSpringStarterSmokeTest {}
