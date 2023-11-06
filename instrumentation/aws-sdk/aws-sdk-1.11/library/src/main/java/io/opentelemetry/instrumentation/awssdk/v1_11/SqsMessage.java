6d920c207288dfd9ea092283b03734b276cbf58c
Lauri Tulmin <ltulmin@splunk.com>
2023-11-06 22:21:53 +0200

    add comment

--- instrumentation/aws-sdk/aws-sdk-1.11/library/src/main/java/io/opentelemetry/instrumentation/awssdk/v1_11/SqsMessage.java
+++ instrumentation/aws-sdk/aws-sdk-1.11/library/src/main/java/io/opentelemetry/instrumentation/awssdk/v1_11/SqsMessage.java
@@ -7,6 +7,10 @@ package io.opentelemetry.instrumentation.awssdk.v1_11;
 
 import java.util.Map;
 
+/**
+ * A wrapper interface for {@link com.amazonaws.services.sqs.model.Message}. Using this wrapper
+ * avoids muzzle failure when sqs classes are not present.
+ */
 interface SqsMessage {
 
   Map<String, String> getAttributes();
