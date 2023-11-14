5daf6d168e39ec954a57daf35ab9f8c328603bf5
Jean Bisutti <jean.bisutti@gmail.com>
2023-11-14 18:03:13 +0100

    Update instrumentation/log4j/log4j-appender-2.17/library/src/main/java/io/opentelemetry/instrumentation/log4j/appender/v2_17/OpenTelemetryAppender.java
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>

--- instrumentation/log4j/log4j-appender-2.17/library/src/main/java/io/opentelemetry/instrumentation/log4j/appender/v2_17/OpenTelemetryAppender.java
+++ instrumentation/log4j/log4j-appender-2.17/library/src/main/java/io/opentelemetry/instrumentation/log4j/appender/v2_17/OpenTelemetryAppender.java
@@ -132,7 +132,7 @@ public class OpenTelemetryAppender extends AbstractAppender {
     /**
      * Log telemetry is emitted after the initialization of the OpenTelemetry Logback appender with
      * an {@link OpenTelemetry} object. This setting allows you to modify the size of the cache used
-     * to replay the first logs.
+     * to replay the logs that were emitted prior to setting the OpenTelemetry instance into the Logback appender.
      */
     @CanIgnoreReturnValue
     public B setFirstLogsCacheSize(int firstLogsCacheSize) {
