57b8a291b19733e6d9b7f4281ea0c8fe1343bdc2
Lauri Tulmin <ltulmin@splunk.com>
2023-11-17 18:49:29 +0200

    Remove latest dep limit for aws2 sqs test

--- instrumentation/aws-sdk/aws-sdk-2.2/testing/src/main/groovy/io/opentelemetry/instrumentation/awssdk/v2_2/AbstractAws2SqsSuppressReceiveSpansTest.groovy
+++ instrumentation/aws-sdk/aws-sdk-2.2/testing/src/main/groovy/io/opentelemetry/instrumentation/awssdk/v2_2/AbstractAws2SqsSuppressReceiveSpansTest.groovy
@@ -125,7 +125,7 @@ abstract class AbstractAws2SqsSuppressReceiveSpansTest extends InstrumentationSp
           attributes {
             "aws.agent" "java-aws-sdk"
             "aws.queue.name" "testSdkSqs"
-            "aws.requestId" "00000000-0000-0000-0000-000000000000"
+            "aws.requestId" { it == "00000000-0000-0000-0000-000000000000" || it == "UNKNOWN" }
             "rpc.system" "aws-api"
             "rpc.service" "Sqs"
             "rpc.method" "CreateQueue"
@@ -147,7 +147,7 @@ abstract class AbstractAws2SqsSuppressReceiveSpansTest extends InstrumentationSp
           attributes {
             "aws.agent" "java-aws-sdk"
             "aws.queue.url" "http://localhost:$sqsPort/000000000000/testSdkSqs"
-            "aws.requestId" "00000000-0000-0000-0000-000000000000"
+            "aws.requestId" { it == "00000000-0000-0000-0000-000000000000" || it == "UNKNOWN" }
             "rpc.system" "aws-api"
             "rpc.method" "SendMessage"
             "rpc.service" "Sqs"
@@ -209,7 +209,7 @@ abstract class AbstractAws2SqsSuppressReceiveSpansTest extends InstrumentationSp
             hasNoLinks()
             attributes {
               "aws.agent" "java-aws-sdk"
-              "aws.requestId" "00000000-0000-0000-0000-000000000000"
+              "aws.requestId" { it == "00000000-0000-0000-0000-000000000000" || it == "UNKNOWN" }
               "rpc.method" "ReceiveMessage"
               "aws.queue.url" "http://localhost:$sqsPort/000000000000/testSdkSqs"
               "rpc.system" "aws-api"
@@ -323,7 +323,7 @@ abstract class AbstractAws2SqsSuppressReceiveSpansTest extends InstrumentationSp
           attributes {
             "aws.agent" "java-aws-sdk"
             "aws.queue.url" "http://localhost:$sqsPort/000000000000/testSdkSqs"
-            "aws.requestId" "00000000-0000-0000-0000-000000000000"
+            "aws.requestId" { it.trim() == "00000000-0000-0000-0000-000000000000" || it == "UNKNOWN" }
             "rpc.system" "aws-api"
             "rpc.method" "SendMessageBatch"
             "rpc.service" "Sqs"
