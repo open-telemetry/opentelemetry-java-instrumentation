91055c21ccb10ee8d919df4d1da29cd256975adb
H L <doublecrash-github@yahoo.com>
2023-11-20 21:06:48 +0800

    move test to correct package

--- instrumentation/wicket-8.0/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/wicket/WicketTest.java
+++ instrumentation/wicket-8.0/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/wicket/WicketTest.java
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+package io.opentelemetry.javaagent.instrumentation.wicket;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 import hello.HelloApplication;
