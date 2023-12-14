a7e8ed81ff7e388a8cb79a483a9fa77a1b042666
Lauri Tulmin <ltulmin@splunk.com>
2023-12-15 01:04:25 +0200

    Associate value with class loader (#10051)

---
+++ javaagent-tooling/src/test/java/io/opentelemetry/javaagent/tooling/util/ClassLoaderValueTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.tooling.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.opentelemetry.instrumentation.test.utils.GcUtils;
+import java.lang.ref.WeakReference;
+import org.junit.jupiter.api.Test;
+
+class ClassLoaderValueTest {
+
+  @Test
+  void testValue() {
+    testClassLoader(this.getClass().getClassLoader());
+    testClassLoader(null);
+  }
+
+  void testClassLoader(ClassLoader classLoader) {
+    ClassLoaderValue<String> classLoaderValue = new ClassLoaderValue<>();
+    classLoaderValue.put(classLoader, "value");
+    assertThat(classLoaderValue.get(classLoader)).isEqualTo("value");
+  }
+
+  @Test
+  void testGc() throws InterruptedException {
+    ClassLoader testClassLoader = new ClassLoader() {};
+    ClassLoaderValue<Value> classLoaderValue = new ClassLoaderValue<>();
+    Value value = new Value();
+    classLoaderValue.put(testClassLoader, value);
+    WeakReference<Value> valueWeakReference = new WeakReference<>(value);
+    WeakReference<ClassLoader> classLoaderWeakReference = new WeakReference<>(testClassLoader);
+
+    assertThat(classLoaderWeakReference.get()).isNotNull();
+    assertThat(valueWeakReference.get()).isNotNull();
+
+    value = null;
+    testClassLoader = null;
+
+    GcUtils.awaitGc(classLoaderWeakReference);
+    GcUtils.awaitGc(valueWeakReference);
+
+    assertThat(classLoaderWeakReference.get()).isNull();
+    assertThat(valueWeakReference.get()).isNull();
+  }
+
+  private static class Value {}
+}
