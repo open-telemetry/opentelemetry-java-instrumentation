b34f81093fe9d95bd036a424532ba1ae137aef87
heyams <heya@microsoft.com>
2024-01-09 13:49:27 -0800

    Fix network type assertion failures

--- instrumentation/lettuce/lettuce-5.1/library/src/main/java/io/opentelemetry/instrumentation/lettuce/v5_1/LettuceServerAttributesGetter.java
+++ instrumentation/lettuce/lettuce-5.1/library/src/main/java/io/opentelemetry/instrumentation/lettuce/v5_1/LettuceServerAttributesGetter.java
@@ -5,11 +5,14 @@
 
 package io.opentelemetry.instrumentation.lettuce.v5_1;
 
+import io.opentelemetry.instrumentation.api.semconv.network.NetworkAttributesGetter;
 import io.opentelemetry.instrumentation.api.semconv.network.ServerAttributesGetter;
 import io.opentelemetry.instrumentation.lettuce.v5_1.OpenTelemetryTracing.OpenTelemetryEndpoint;
 import javax.annotation.Nullable;
 
-class LettuceServerAttributesGetter implements ServerAttributesGetter<OpenTelemetryEndpoint> {
+class LettuceServerAttributesGetter
+    implements ServerAttributesGetter<OpenTelemetryEndpoint>,
+        NetworkAttributesGetter<OpenTelemetryEndpoint, Void> {
 
   @Nullable
   @Override
