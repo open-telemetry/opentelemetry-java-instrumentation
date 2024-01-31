6c16f6d314d06254d067546e8871e78a95269783
heyams <heya@microsoft.com>
2024-01-31 14:28:50 -0800

    Update

--- instrumentation/cassandra/cassandra-4.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/cassandra/v4_0/CassandraServerAttributesExtractor.java
+++ instrumentation/cassandra/cassandra-4.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/cassandra/v4_0/CassandraServerAttributesExtractor.java
@@ -5,7 +5,10 @@
 
 package io.opentelemetry.javaagent.instrumentation.cassandra.v4_0;
 
+import com.datastax.oss.driver.api.core.config.DefaultDriverOption;
+import com.datastax.oss.driver.api.core.config.DriverExecutionProfile;
 import com.datastax.oss.driver.api.core.cql.ExecutionInfo;
+import com.datastax.oss.driver.api.core.cql.Statement;
 import com.datastax.oss.driver.api.core.metadata.Node;
 import io.opentelemetry.api.common.AttributesBuilder;
 import io.opentelemetry.context.Context;
@@ -15,10 +18,12 @@ import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import javax.annotation.Nullable;
 
-public class CassandraServerAttributesExtractor
+final class CassandraServerAttributesExtractor
     implements AttributesExtractor<CassandraRequest, ExecutionInfo> {
+
   @Override
-  public void onStart(AttributesBuilder attributes, Context context, CassandraRequest request) {}
+  public void onStart(
+      AttributesBuilder attributes, Context parentContext, CassandraRequest request) {}
 
   @Override
   public void onEnd(
@@ -30,15 +35,51 @@ public class CassandraServerAttributesExtractor
     if (executionInfo == null) {
       return;
     }
+
     Node coordinator = executionInfo.getCoordinator();
-    if (coordinator == null) {
-      return;
+    if (coordinator != null) {
+      SocketAddress address = coordinator.getEndPoint().resolve();
+      if (address instanceof InetSocketAddress) {
+        attributes.put(
+            SemanticAttributes.SERVER_ADDRESS, ((InetSocketAddress) address).getHostName());
+        attributes.put(SemanticAttributes.SERVER_PORT, ((InetSocketAddress) address).getPort());
+      }
+      if (coordinator.getDatacenter() != null) {
+        attributes.put(SemanticAttributes.DB_CASSANDRA_COORDINATOR_DC, coordinator.getDatacenter());
+      }
+      if (coordinator.getHostId() != null) {
+        attributes.put(
+            SemanticAttributes.DB_CASSANDRA_COORDINATOR_ID, coordinator.getHostId().toString());
+      }
     }
-    SocketAddress address = coordinator.getEndPoint().resolve();
-    if (address instanceof InetSocketAddress) {
-      attributes.put(
-          SemanticAttributes.SERVER_ADDRESS, ((InetSocketAddress) address).getHostName());
-      attributes.put(SemanticAttributes.SERVER_PORT, ((InetSocketAddress) address).getPort());
+    attributes.put(
+        SemanticAttributes.DB_CASSANDRA_SPECULATIVE_EXECUTION_COUNT,
+        executionInfo.getSpeculativeExecutionCount());
+
+    Statement<?> statement = executionInfo.getStatement();
+    String consistencyLevel;
+    DriverExecutionProfile config =
+        request.getSession().getContext().getConfig().getDefaultProfile();
+    if (statement.getConsistencyLevel() != null) {
+      consistencyLevel = statement.getConsistencyLevel().name();
+    } else {
+      consistencyLevel = config.getString(DefaultDriverOption.REQUEST_CONSISTENCY);
     }
+    attributes.put(SemanticAttributes.DB_CASSANDRA_CONSISTENCY_LEVEL, consistencyLevel);
+
+    if (statement.getPageSize() > 0) {
+      attributes.put(SemanticAttributes.DB_CASSANDRA_PAGE_SIZE, statement.getPageSize());
+    } else {
+      int pageSize = config.getInt(DefaultDriverOption.REQUEST_PAGE_SIZE);
+      if (pageSize > 0) {
+        attributes.put(SemanticAttributes.DB_CASSANDRA_PAGE_SIZE, pageSize);
+      }
+    }
+
+    Boolean idempotent = statement.isIdempotent();
+    if (idempotent == null) {
+      idempotent = config.getBoolean(DefaultDriverOption.REQUEST_DEFAULT_IDEMPOTENCE);
+    }
+    attributes.put(SemanticAttributes.DB_CASSANDRA_IDEMPOTENCE, idempotent);
   }
 }
