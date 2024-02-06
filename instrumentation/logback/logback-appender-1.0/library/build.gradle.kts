fb707942ddf6df5d4dc4e05abdddeb2eb9ea1322
Jean Bisutti <jean.bisutti@gmail.com>
2024-02-06 15:00:09 +0100

    Disable GraalVM metadata repository

--- instrumentation/logback/logback-appender-1.0/library/build.gradle.kts
+++ instrumentation/logback/logback-appender-1.0/library/build.gradle.kts
@@ -48,6 +48,11 @@ graalvmNative {
     buildArgs.add("--initialize-at-build-time=org.junit.jupiter.engine.config.InstantiatingConfigurationParameterConverter")
   }
 
+  // See https://github.com/graalvm/native-build-tools/issues/572
+  metadataRepository {
+    enabled.set(false)
+  }
+
   toolchainDetection.set(false)
 }
 
