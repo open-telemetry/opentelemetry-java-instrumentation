3e0e083014d809cf8a93b0f05bc18f0b354dbaec
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2024-02-07 14:25:36 +0100

    Created SpanOmitterDelegator

---
+++ instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/instrumenter/SpanOmitterDelegator.java
@@ -0,0 +1,36 @@
+package io.opentelemetry.instrumentation.api.instrumenter;
+
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.context.Context;
+import java.util.Collections;
+import java.util.List;
+
+class SpanOmitterDelegator implements SpanSuppressor {
+  private final List<SpanOmitter> spanOmitters;
+
+  SpanOmitterDelegator(List<SpanOmitter> spanOmitters) {
+    this.spanOmitters = spanOmitters;
+  }
+
+  static SpanSuppressor create(List<SpanOmitter> spanOmitters) {
+    return new SpanOmitterDelegator(Collections.unmodifiableList(spanOmitters));
+  }
+
+  @CanIgnoreReturnValue
+  @Override
+  public Context storeInContext(Context context, SpanKind spanKind, Span span) {
+    return context;
+  }
+
+  @Override
+  public boolean shouldSuppress(Context parentContext, SpanKind spanKind) {
+    for (SpanOmitter spanOmitter : spanOmitters) {
+      if(spanOmitter.shouldOmit(parentContext)){
+        return true;
+      }
+    }
+    return false;
+  }
+}
---
+++ instrumentation-api/src/test/java/io/opentelemetry/instrumentation/api/instrumenter/SpanOmitterDelegatorTest.java
@@ -0,0 +1,74 @@
+package io.opentelemetry.instrumentation.api.instrumenter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.context.Context;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
+import org.mockito.Mockito;
+
+class SpanOmitterDelegatorTest {
+  @Test
+  void returnContextAsReceived() {
+    Context context = mock(Context.class);
+
+    SpanSuppressor suppressor = SpanOmitterDelegator.create(Collections.emptyList());
+
+    assertThat(suppressor.storeInContext(context, SpanKind.CLIENT, Span.getInvalid())).isEqualTo(context);
+  }
+
+  @Test
+  void suppressSpanWhenAtLeastOneOmitterOmitsIt(){
+    Context context = mock(Context.class);
+    List<SpanOmitter> omitters = new ArrayList<>();
+    SpanOmitter omitter1 = createOmitter(false);
+    SpanOmitter omitter2 = createOmitter(true);
+    SpanOmitter omitter3 = createOmitter(false);
+    omitters.add(omitter1);
+    omitters.add(omitter2);
+    omitters.add(omitter3);
+
+    SpanSuppressor suppressor = SpanOmitterDelegator.create(omitters);
+
+    assertThat(suppressor.shouldSuppress(context, SpanKind.CLIENT)).isTrue();
+    verify(omitter1).shouldOmit(context);
+    verify(omitter2).shouldOmit(context);
+    verify(omitter3, never()).shouldOmit(context);
+  }
+
+  @Test
+  void shouldNotSuppressWhenNoOmitterOmitsIt(){
+    Context context = mock(Context.class);
+    List<SpanOmitter> omitters = new ArrayList<>();
+    SpanOmitter omitter1 = createOmitter(false);
+    SpanOmitter omitter2 = createOmitter(false);
+    SpanOmitter omitter3 = createOmitter(false);
+    omitters.add(omitter1);
+    omitters.add(omitter2);
+    omitters.add(omitter3);
+
+    SpanSuppressor suppressor = SpanOmitterDelegator.create(omitters);
+
+    assertThat(suppressor.shouldSuppress(context, SpanKind.CLIENT)).isFalse();
+    InOrder inOrder = Mockito.inOrder(omitter1, omitter2, omitter3);
+    inOrder.verify(omitter1).shouldOmit(context);
+    inOrder.verify(omitter2).shouldOmit(context);
+    inOrder.verify(omitter3).shouldOmit(context);
+  }
+
+  private static SpanOmitter createOmitter(boolean shouldOmit) {
+    SpanOmitter omitter = mock(SpanOmitter.class);
+    doReturn(shouldOmit).when(omitter).shouldOmit(any());
+    return omitter;
+  }
+}
