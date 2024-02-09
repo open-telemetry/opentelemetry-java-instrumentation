8fd9d15baaa5361825a8a922073825ed92d6f0ff
Trask Stalnaker <trask.stalnaker@gmail.com>
2024-02-09 14:57:39 -0800

    fix

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -98,6 +98,7 @@ val DEPENDENCIES = listOf(
   "io.netty:netty:3.10.6.Final",
   "io.opentelemetry.contrib:opentelemetry-aws-xray-propagator:1.28.0-alpha",
   "io.opentelemetry.proto:opentelemetry-proto:1.0.0-alpha",
+  "io.opentelemetry:opentelemetry-extension-annotations:1.18.0", // deprecated, no longer part of bom
   "org.assertj:assertj-core:3.25.3",
   "org.awaitility:awaitility:4.2.0",
   "com.google.code.findbugs:annotations:3.0.1u2",
