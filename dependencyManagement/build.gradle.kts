16ec9f38594d82384d24cc4334a12ace062550cb
Lauri Tulmin <ltulmin@splunk.com>
2024-02-09 18:50:45 +0200

    Allow minor version updates for logback-classic (#10451)

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -80,7 +80,7 @@ val CORE_DEPENDENCIES = listOf(
 // this top level to help consistently satisfy large numbers of transitive dependencies.
 val DEPENDENCIES = listOf(
   "io.r2dbc:r2dbc-proxy:1.1.4.RELEASE",
-  "ch.qos.logback:logback-classic:1.3.8", // 1.4+ requires Java 11+
+  "ch.qos.logback:logback-classic:1.3.14", // 1.4+ requires Java 11+
   "com.github.stefanbirkner:system-lambda:1.2.1",
   "com.github.stefanbirkner:system-rules:1.19.0",
   "uk.org.webcompere:system-stubs-jupiter:2.0.3",
