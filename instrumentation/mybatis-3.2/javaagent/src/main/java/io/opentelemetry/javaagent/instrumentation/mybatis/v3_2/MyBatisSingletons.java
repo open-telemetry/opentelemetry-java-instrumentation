f777c0eaf992a8eaf6a465b928e69a34c8e4473f
Steve Rao <raozihao.rzh@alibaba-inc.com>
2024-02-13 07:06:57 +0800

    Add support for MyBatis framework (#10258)
    Co-authored-by: Lauri Tulmin <tulmin@gmail.com>

---
+++ instrumentation/mybatis-3.2/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/mybatis/v3_2/MyBatisSingletons.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.mybatis.v3_2;
+
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.instrumentation.api.incubator.semconv.code.CodeAttributesExtractor;
+import io.opentelemetry.instrumentation.api.incubator.semconv.code.CodeAttributesGetter;
+import io.opentelemetry.instrumentation.api.incubator.semconv.code.CodeSpanNameExtractor;
+import io.opentelemetry.instrumentation.api.incubator.semconv.util.ClassAndMethod;
+import io.opentelemetry.instrumentation.api.instrumenter.Instrumenter;
+import io.opentelemetry.instrumentation.api.instrumenter.SpanKindExtractor;
+
+public final class MyBatisSingletons {
+  private static final String INSTRUMENTATION_NAME = "io.opentelemetry.mybatis-3.2";
+  private static final Instrumenter<ClassAndMethod, Void> INSTRUMENTER;
+
+  static {
+    CodeAttributesGetter<ClassAndMethod> codeAttributesGetter =
+        ClassAndMethod.codeAttributesGetter();
+
+    INSTRUMENTER =
+        Instrumenter.<ClassAndMethod, Void>builder(
+                GlobalOpenTelemetry.get(),
+                INSTRUMENTATION_NAME,
+                CodeSpanNameExtractor.create(codeAttributesGetter))
+            .addAttributesExtractor(CodeAttributesExtractor.create(codeAttributesGetter))
+            .buildInstrumenter(SpanKindExtractor.alwaysInternal());
+  }
+
+  public static Instrumenter<ClassAndMethod, Void> instrumenter() {
+    return INSTRUMENTER;
+  }
+
+  private MyBatisSingletons() {}
+}
