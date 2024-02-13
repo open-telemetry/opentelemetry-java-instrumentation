851ce0db47ed306cd508965489cf13bec7aa3fef
samuel.wright <samuel.wright@kognic.com>
2024-02-13 16:04:36 +0100

    fix test

--- javaagent-tooling/src/main/java/io/opentelemetry/javaagent/tooling/ignore/AdditionalLibraryIgnoredTypesConfigurer.java
+++ javaagent-tooling/src/main/java/io/opentelemetry/javaagent/tooling/ignore/AdditionalLibraryIgnoredTypesConfigurer.java
@@ -226,6 +226,7 @@ public class AdditionalLibraryIgnoredTypesConfigurer implements IgnoredTypesConf
 
     builder
         .ignoreClass("com.google.cloud.")
+        .allowClass("com.google.cloud.pubsub.")
         .ignoreClass("com.google.instrumentation.")
         .ignoreClass("com.google.j2objc.")
         .ignoreClass("com.google.gson.")
