bee21e06ef8ff24d37f2e37d1cca59aeeb8fb9e0
Jean Bisutti <jean.bisutti@gmail.com>
2024-02-13 15:59:10 +0100

    Add configurations to the Native Build Tools Gradle plugin

--- instrumentation/java-http-client/library/build.gradle.kts
+++ instrumentation/java-http-client/library/build.gradle.kts
@@ -12,6 +12,23 @@ dependencies {
   testImplementation(project(":instrumentation:java-http-client:testing"))
 }
 
+graalvmNative {
+
+  binaries.all {
+    resources.autodetect()
+
+    // Workaround for https://github.com/junit-team/junit5/issues/3405
+    buildArgs.add("--initialize-at-build-time=org.junit.platform.launcher.core.LauncherConfig")
+    buildArgs.add("--initialize-at-build-time=org.junit.jupiter.engine.config.InstantiatingConfigurationParameterConverter")
+  }
+
+  // See https://github.com/graalvm/native-build-tools/issues/572
+  metadataRepository {
+    enabled.set(false)
+  }
+
+  toolchainDetection.set(false)
+}
 
 // To be able to execute the tests with GraalVM native
 configurations.configureEach {
