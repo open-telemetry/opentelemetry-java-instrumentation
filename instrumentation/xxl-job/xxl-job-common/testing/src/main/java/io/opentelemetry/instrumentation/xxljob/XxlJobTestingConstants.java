53119af8ec2684ee800fbc8e30a2099bb214cab9
steverao <raozihao.rzh@alibaba-inc.com>
2024-02-16 11:55:25 +0800

    Fixed spotless job run failed

--- instrumentation/xxl-job/xxl-job-common/testing/src/main/java/io/opentelemetry/instrumentation/xxljob/XxlJobTestingConstants.java
+++ instrumentation/xxl-job/xxl-job-common/testing/src/main/java/io/opentelemetry/instrumentation/xxljob/XxlJobTestingConstants.java
@@ -35,11 +35,12 @@ public class XxlJobTestingConstants {
           + "  }\n"
           + "}\n";
 
-  public static final String GLUE_JOB_GROOVY_SOURCE = "import com.xxl.job.core.handler.IJobHandler\n"
-      + "\n"
-      + "class CustomizedGroovyHandler extends IJobHandler {\n"
-      + "  @Override\n"
-      + "  void execute() throws Exception {\n"
-      + "  }\n"
-      + "}\n";
+  public static final String GLUE_JOB_GROOVY_SOURCE =
+      "import com.xxl.job.core.handler.IJobHandler\n"
+          + "\n"
+          + "class CustomizedGroovyHandler extends IJobHandler {\n"
+          + "  @Override\n"
+          + "  void execute() throws Exception {\n"
+          + "  }\n"
+          + "}\n";
 }
