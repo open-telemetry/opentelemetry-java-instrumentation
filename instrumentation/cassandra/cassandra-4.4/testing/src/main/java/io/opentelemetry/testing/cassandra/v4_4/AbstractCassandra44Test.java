49439488009de8021fbafaee054b8482025182f4
heyams <heya@microsoft.com>
2024-02-16 21:08:36 -0800

    Try laurit's suggestion

--- instrumentation/cassandra/cassandra-4.4/testing/src/main/java/io/opentelemetry/testing/cassandra/v4_4/AbstractCassandra44Test.java
+++ instrumentation/cassandra/cassandra-4.4/testing/src/main/java/io/opentelemetry/testing/cassandra/v4_4/AbstractCassandra44Test.java
@@ -25,9 +25,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Named.named;
 
 import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.CqlSessionBuilder;
+import com.datastax.oss.driver.internal.core.metadata.SniEndPoint;
 import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.cassandra.v4.common.AbstractCassandraTest;
 import io.opentelemetry.instrumentation.api.semconv.network.internal.NetworkAttributes;
+import java.net.InetSocketAddress;
 import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -151,11 +154,16 @@ public abstract class AbstractCassandra44Test extends AbstractCassandraTest {
                     "users"))));
   }
 
-  // TODO (trask) this is causing sporadic test failures
-  // @Override
-  // protected CqlSessionBuilder addContactPoint(CqlSessionBuilder sessionBuilder) {
-  //   InetSocketAddress address = new InetSocketAddress("localhost", cassandraPort);
-  //   sessionBuilder.addContactEndPoint(new SniEndPoint(address, "localhost"));
-  //   return sessionBuilder;
-  // }
+  @Override
+  protected CqlSessionBuilder addContactPoint(CqlSessionBuilder sessionBuilder) {
+    InetSocketAddress address = new InetSocketAddress("localhost", cassandraPort);
+    sessionBuilder.addContactEndPoint(
+        new SniEndPoint(address, "localhost") {
+          @Override
+          public InetSocketAddress resolve() {
+            return address;
+          }
+        });
+    return sessionBuilder;
+  }
 }
