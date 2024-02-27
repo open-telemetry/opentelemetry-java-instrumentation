e67d73edaae10669e85ff7cc4a10463733b2c278
Lauri Tulmin <ltulmin@splunk.com>
2024-02-27 11:35:14 +0200

    Disable http and rpc metrics when advice can not be applied (#10671)

--- instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/semconv/http/HttpServerMetrics.java
+++ instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/semconv/http/HttpServerMetrics.java
@@ -17,6 +17,7 @@ import io.opentelemetry.context.ContextKey;
 import io.opentelemetry.instrumentation.api.instrumenter.InstrumenterBuilder;
 import io.opentelemetry.instrumentation.api.instrumenter.OperationListener;
 import io.opentelemetry.instrumentation.api.instrumenter.OperationMetrics;
+import io.opentelemetry.instrumentation.api.internal.OperationMetricsUtil;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
@@ -43,7 +44,7 @@ public final class HttpServerMetrics implements OperationListener {
    * @see InstrumenterBuilder#addOperationMetrics(OperationMetrics)
    */
   public static OperationMetrics get() {
-    return HttpServerMetrics::new;
+    return OperationMetricsUtil.create("http server", HttpServerMetrics::new);
   }
 
   private final DoubleHistogram duration;
