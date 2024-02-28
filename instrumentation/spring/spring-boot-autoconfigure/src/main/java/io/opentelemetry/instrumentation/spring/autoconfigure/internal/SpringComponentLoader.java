a6e54c88a51f721af0d82dfccb9ae509ba4ceddd
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-02-28 09:03:52 +0100

    remove ConfigurableProvider

--- instrumentation/spring/spring-boot-autoconfigure/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/internal/SpringComponentLoader.java
+++ instrumentation/spring/spring-boot-autoconfigure/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/internal/SpringComponentLoader.java
@@ -5,14 +5,11 @@
 
 package io.opentelemetry.instrumentation.spring.autoconfigure.internal;
 
-import io.opentelemetry.sdk.autoconfigure.ComponentLoader;
+import io.opentelemetry.sdk.autoconfigure.internal.ComponentLoader;
 import io.opentelemetry.sdk.autoconfigure.internal.SpiHelper;
-import io.opentelemetry.sdk.autoconfigure.spi.ConfigurableProvider;
 import io.opentelemetry.sdk.autoconfigure.spi.Ordered;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 import org.springframework.context.ApplicationContext;
 
@@ -53,24 +50,5 @@ public class SpringComponentLoader implements ComponentLoader {
     // provider
     result.addAll(beans);
     return result;
-  }
-
-  @Override
-  public <T extends ConfigurableProvider> Map<String, T> loadConfigurableProviders(
-      Class<T> spiClass) {
-    Map<String, T> components = new HashMap<>();
-    addComponents(components, spiHelper.load(spiClass));
-    // beans have a higher priority than spi and overwrite values with the same name
-    addComponents(
-        components,
-        applicationContext.getBeanProvider(spiClass).orderedStream().collect(Collectors.toList()));
-    return components;
-  }
-
-  private static <T> void addComponents(Map<String, T> target, List<T> components) {
-    for (T component : components) {
-      String name = ((ConfigurableProvider) component).getName();
-      target.put(name, component);
-    }
   }
 }
