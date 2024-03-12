29d5ac1bc99ddf79582a443fcb319fee5b325a7c
Lauri Tulmin <ltulmin@splunk.com>
2024-03-12 12:34:11 +0200

    add latest dep restriction

--- instrumentation/graphql-java/graphql-java-12.0/library/build.gradle.kts
+++ instrumentation/graphql-java/graphql-java-12.0/library/build.gradle.kts
@@ -7,4 +7,6 @@ dependencies {
   implementation(project(":instrumentation:graphql-java:graphql-java-common:library"))
 
   testImplementation(project(":instrumentation:graphql-java:graphql-java-common:testing"))
+
+  latestDepTestLibrary("com.graphql-java:graphql-java:19.+")
 }
