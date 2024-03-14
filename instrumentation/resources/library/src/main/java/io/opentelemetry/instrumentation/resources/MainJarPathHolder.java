8952683d4da04b875de710fa729fc2a983ae42ac
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-03-14 13:12:10 +0100

    extract MainJarPathHolder to make the cache more explicit

--- instrumentation/resources/library/src/main/java/io/opentelemetry/instrumentation/resources/MainJarPathHolder.java
+++ instrumentation/resources/library/src/main/java/io/opentelemetry/instrumentation/resources/MainJarPathHolder.java
@@ -7,9 +7,10 @@ package io.opentelemetry.instrumentation.resources;
 
 import java.nio.file.Path;
 import java.util.Optional;
-import java.util.function.Function;
 
 public class MainJarPathHolder {
+
+  private MainJarPathHolder() {}
 
   // visible for testing
   static void resetForTest() {
@@ -26,16 +27,11 @@ public class MainJarPathHolder {
 
   private static Optional<DetectionResult> detectionResult = Optional.empty();
 
-  private static final Function<MainJarPathFinder, Optional<Path>> jarPath =
-      (finder) -> {
-        if (!detectionResult.isPresent()) {
-          detectionResult =
-              Optional.of(new DetectionResult(Optional.ofNullable(finder.detectJarPath())));
-        }
-        return detectionResult.get().jarPath;
-      };
-
   static Optional<Path> getJarPath(MainJarPathFinder finder) {
-    return jarPath.apply(finder);
+    if (!detectionResult.isPresent()) {
+      detectionResult =
+          Optional.of(new DetectionResult(Optional.ofNullable(finder.detectJarPath())));
+    }
+    return detectionResult.get().jarPath;
   }
 }
