94628680fa8fe623ca9b6f781495628808066621
Lauri Tulmin <ltulmin@splunk.com>
2024-03-18 13:11:01 +0200

    Fix disabling virtual thread context propagation

--- instrumentation/executors/javaagent/build.gradle.kts
+++ instrumentation/executors/javaagent/build.gradle.kts
@@ -13,8 +13,8 @@ dependencies {
   bootstrap(project(":instrumentation:executors:bootstrap"))
 
   testImplementation(project(":instrumentation:executors:testing"))
-
   testImplementation("org.scala-lang:scala-library:2.11.12")
+  testCompileOnly(project(":instrumentation:executors:bootstrap"))
 }
 
 testing {
@@ -44,6 +44,10 @@ testing {
 
 tasks {
   withType<Test>().configureEach {
+    // needed for VirtualThreadTest on jdk21
+    jvmArgs("--add-opens=java.base/java.lang=ALL-UNNAMED")
+    jvmArgs("-XX:+IgnoreUnrecognizedVMOptions")
+
     jvmArgs(
       "-Dotel.instrumentation.executors.include=io.opentelemetry.javaagent.instrumentation.executors.ExecutorInstrumentationTest\$CustomThreadPoolExecutor,io.opentelemetry.javaagent.instrumentation.executors.ThreadPoolExecutorTest\$RunnableCheckingThreadPoolExecutor"
     )
