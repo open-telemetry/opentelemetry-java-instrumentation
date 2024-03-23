51ae208adf2dcfaad8a7c8b1820bbd1c90edec46
luyanbo <robert.lyb@alibaba-inc.com>
2024-03-24 01:05:42 +0800

    add jacoco to alibaba-druid-1.0/library

--- instrumentation/alibaba-druid-1.0/library/build.gradle.kts
+++ instrumentation/alibaba-druid-1.0/library/build.gradle.kts
@@ -1,10 +1,20 @@
 plugins {
   id("otel.library-instrumentation")
   id("otel.nullaway-conventions")
+
+  jacoco
 }
 
 dependencies {
   library("com.alibaba:druid:1.0.0")
 
   testImplementation(project(":instrumentation:alibaba-druid-1.0:testing"))
+}
+
+tasks.test {
+  finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+  dependsOn(tasks.test)
 }
