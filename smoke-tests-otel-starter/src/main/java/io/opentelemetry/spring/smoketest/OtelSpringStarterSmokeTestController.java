51228888c0200a811b379d7a8c56add413d55a92
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-04-08 12:49:21 +0200

    ServletWebServerApplicationContext is not available for every smoke test

--- smoke-tests-otel-starter/src/main/java/io/opentelemetry/spring/smoketest/OtelSpringStarterSmokeTestController.java
+++ smoke-tests-otel-starter/src/main/java/io/opentelemetry/spring/smoketest/OtelSpringStarterSmokeTestController.java
@@ -8,6 +8,7 @@ package io.opentelemetry.spring.smoketest;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.metrics.LongHistogram;
 import io.opentelemetry.api.metrics.Meter;
+import java.util.Optional;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,22 +21,28 @@ public class OtelSpringStarterSmokeTestController {
   public static final String URL = "/ping";
   public static final String TEST_HISTOGRAM = "histogram-test-otel-spring-starter";
   private final LongHistogram histogram;
-  private final RestTemplate restTemplate;
+  private final Optional<RestTemplate> restTemplate;
 
   public OtelSpringStarterSmokeTestController(
       OpenTelemetry openTelemetry,
       RestTemplateBuilder restTemplateBuilder,
-      ServletWebServerApplicationContext server) {
+      Optional<ServletWebServerApplicationContext> server) {
     Meter meter = openTelemetry.getMeter(OtelSpringStarterSmokeTestApplication.class.getName());
     histogram = meter.histogramBuilder(TEST_HISTOGRAM).ofLongs().build();
     restTemplate =
-        restTemplateBuilder.rootUri("http://localhost:" + server.getWebServer().getPort()).build();
+        server.map(
+            s ->
+                restTemplateBuilder
+                    .rootUri("http://localhost:" + s.getWebServer().getPort())
+                    .build());
   }
 
   @GetMapping(URL)
   public String ping() {
     histogram.record(10);
-    return restTemplate.getForObject("/pong", String.class);
+    return restTemplate
+        .map(t -> t.getForObject("/pong", String.class))
+        .orElseThrow(() -> new IllegalStateException("RestTemplate not available"));
   }
 
   @GetMapping("/pong")
