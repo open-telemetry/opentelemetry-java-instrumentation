6546cfd29b889c5b2098f5d3c28fd677a2cb57ac
Zachary Sistrunk <zsistrunk@cspire.com>
2024-04-17 08:43:37 -0500

    Explanatory comment

--- instrumentation/spring/spring-rmi-4.0/javaagent/src/test/java/springrmi/app/ejb/SpringRmiGreeterRemote.java
+++ instrumentation/spring/spring-rmi-4.0/javaagent/src/test/java/springrmi/app/ejb/SpringRmiGreeterRemote.java
@@ -9,6 +9,8 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 import springrmi.app.SpringRmiGreeter;
 
+// Real EJBs auto-generate a stub class similar to this. As we don't have a way to use the actual
+// auto-generated class, this wrapper is used to emulate it.
 class SpringRmiGreeterRemote implements Remote {
 
   private final SpringRmiGreeter impl;
