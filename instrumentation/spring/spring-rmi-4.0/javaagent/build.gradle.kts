ea561045956efda9e19a4485b13ba027bc873615
Zachary Sistrunk <zsistrunk@cspire.com>
2024-04-18 11:48:21 -0500

    Removing spring-test dependency The spring-test dependency was only added to use their JNDI mock, but that was removed in Spring 5. With the use of simple-jndi instead, spring-test was no longer required

--- instrumentation/spring/spring-rmi-4.0/javaagent/build.gradle.kts
+++ instrumentation/spring/spring-rmi-4.0/javaagent/build.gradle.kts
@@ -19,7 +19,6 @@ dependencies {
 
   library("org.springframework:spring-context:4.0.0.RELEASE")
   library("org.springframework:spring-aop:4.0.0.RELEASE")
-  testLibrary("org.springframework:spring-test:4.0.0.RELEASE")
   testLibrary("org.springframework.boot:spring-boot:1.1.0.RELEASE")
   testLibrary("org.apache.openejb:javaee-api:5.0-1") // Added to test remote-slsb functionality
   testLibrary("com.github.h-thurow:simple-jndi:0.24.0") // Spring's JNDI mock was removed in Spring 5
@@ -28,7 +27,6 @@ dependencies {
   // rmi remoting was removed in spring 6
   latestDepTestLibrary("org.springframework:spring-context:5.+") // documented limitation
   latestDepTestLibrary("org.springframework:spring-aop:5.+") // documented limitation
-  latestDepTestLibrary("org.springframework:spring-test:5.+") // documented limitation
   latestDepTestLibrary("org.springframework.boot:spring-boot:2.+") // documented limitation
 }
 
