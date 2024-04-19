c34c4e30ed5216fdac0dee98170541e20c4b286a
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-04-19 16:46:17 +0200

    remove db drivers

--- smoke-tests-otel-starter/build.gradle.kts
+++ smoke-tests-otel-starter/build.gradle.kts
@@ -24,9 +24,6 @@ dependencies {
 
   // webflux
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
-  runtimeOnly("io.r2dbc:r2dbc-h2")
-  implementation(project(":instrumentation:r2dbc-1.0:library"))
 
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation(project(":testing-common"))
