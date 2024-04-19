f4ff4e91fcaaa853fa8b921786fac65cb7007e93
Jay DeLuca <jaydeluca4@gmail.com>
2024-04-19 18:29:05 -0400

    refactor format for test args

--- instrumentation/jdbc/library/src/test/java/io/opentelemetry/instrumentation/jdbc/internal/OpenTelemetryConnectionTest.java
+++ instrumentation/jdbc/library/src/test/java/io/opentelemetry/instrumentation/jdbc/internal/OpenTelemetryConnectionTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.opentelemetry.instrumentation.jdbc;
+package io.opentelemetry.instrumentation.jdbc.internal;
 
 import static io.opentelemetry.instrumentation.jdbc.internal.JdbcInstrumenterFactory.createStatementInstrumenter;
 import static io.opentelemetry.sdk.testing.assertj.OpenTelemetryAssertions.equalTo;
@@ -13,17 +13,12 @@ import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.context.propagation.ContextPropagators;
 import io.opentelemetry.instrumentation.api.instrumenter.Instrumenter;
-import io.opentelemetry.instrumentation.jdbc.internal.DbRequest;
-import io.opentelemetry.instrumentation.jdbc.internal.OpenTelemetryCallableStatement;
-import io.opentelemetry.instrumentation.jdbc.internal.OpenTelemetryConnection;
-import io.opentelemetry.instrumentation.jdbc.internal.OpenTelemetryPreparedStatement;
-import io.opentelemetry.instrumentation.jdbc.internal.OpenTelemetryStatement;
+import io.opentelemetry.instrumentation.jdbc.TestConnection;
 import io.opentelemetry.instrumentation.jdbc.internal.dbinfo.DbInfo;
 import io.opentelemetry.instrumentation.testing.junit.InstrumentationExtension;
 import io.opentelemetry.instrumentation.testing.junit.LibraryInstrumentationExtension;
 import io.opentelemetry.semconv.ServerAttributes;
 import io.opentelemetry.semconv.incubating.DbIncubatingAttributes;
-import java.lang.reflect.Field;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -33,7 +28,7 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 class OpenTelemetryConnectionTest {
 
   @RegisterExtension
-  static final InstrumentationExtension testing = LibraryInstrumentationExtension.create();
+  private static final InstrumentationExtension testing = LibraryInstrumentationExtension.create();
 
   @Test
   void testVerifyCreateStatement() throws SQLException {
@@ -51,28 +46,13 @@ class OpenTelemetryConnectionTest {
           assertThat(statement.execute(query)).isTrue();
         });
 
-    testing.waitAndAssertTraces(
-        trace ->
-            trace.hasSpansSatisfyingExactly(
-                span -> span.hasName("parent").hasKind(SpanKind.INTERNAL).hasNoParent(),
-                span ->
-                    span.hasName("SELECT my_name.users")
-                        .hasKind(SpanKind.CLIENT)
-                        .hasParent(trace.getSpan(0))
-                        .hasAttributesSatisfyingExactly(
-                            equalTo(DbIncubatingAttributes.DB_SYSTEM, dbInfo.getSystem()),
-                            equalTo(DbIncubatingAttributes.DB_NAME, dbInfo.getName()),
-                            equalTo(DbIncubatingAttributes.DB_USER, dbInfo.getUser()),
-                            equalTo(DbIncubatingAttributes.DB_STATEMENT, query),
-                            equalTo(DbIncubatingAttributes.DB_OPERATION, "SELECT"),
-                            equalTo(DbIncubatingAttributes.DB_SQL_TABLE, "users"),
-                            equalTo(ServerAttributes.SERVER_ADDRESS, dbInfo.getHost()),
-                            equalTo(ServerAttributes.SERVER_PORT, dbInfo.getPort()))));
+    jdbcTraceAssertion(dbInfo, query);
 
     statement.close();
     connection.close();
   }
 
+  @SuppressWarnings("unchecked")
   @Test
   void testVerifyCreateStatementReturnsOtelWrapper() throws Exception {
     OpenTelemetry ot = OpenTelemetry.propagating(ContextPropagators.noop());
@@ -83,11 +63,8 @@ class OpenTelemetryConnectionTest {
     assertThat(connection.createStatement()).isInstanceOf(OpenTelemetryStatement.class);
     assertThat(connection.createStatement(0, 0)).isInstanceOf(OpenTelemetryStatement.class);
     assertThat(connection.createStatement(0, 0, 0)).isInstanceOf(OpenTelemetryStatement.class);
-
-    Field instrumenterField = OpenTelemetryStatement.class.getDeclaredField("instrumenter");
-    instrumenterField.setAccessible(true);
-
-    assertThat(instrumenterField.get(connection.createStatement())).isEqualTo(instrumenter);
+    assertThat(((OpenTelemetryStatement<Statement>) connection.createStatement()).instrumenter)
+        .isEqualTo(instrumenter);
 
     connection.close();
   }
@@ -108,28 +85,13 @@ class OpenTelemetryConnectionTest {
           assertThat(statement.execute()).isTrue();
         });
 
-    testing.waitAndAssertTraces(
-        trace ->
-            trace.hasSpansSatisfyingExactly(
-                span -> span.hasName("parent").hasKind(SpanKind.INTERNAL).hasNoParent(),
-                span ->
-                    span.hasName("SELECT my_name.users")
-                        .hasKind(SpanKind.CLIENT)
-                        .hasParent(trace.getSpan(0))
-                        .hasAttributesSatisfyingExactly(
-                            equalTo(DbIncubatingAttributes.DB_SYSTEM, dbInfo.getSystem()),
-                            equalTo(DbIncubatingAttributes.DB_NAME, dbInfo.getName()),
-                            equalTo(DbIncubatingAttributes.DB_USER, dbInfo.getUser()),
-                            equalTo(DbIncubatingAttributes.DB_STATEMENT, query),
-                            equalTo(DbIncubatingAttributes.DB_OPERATION, "SELECT"),
-                            equalTo(DbIncubatingAttributes.DB_SQL_TABLE, "users"),
-                            equalTo(ServerAttributes.SERVER_ADDRESS, dbInfo.getHost()),
-                            equalTo(ServerAttributes.SERVER_PORT, dbInfo.getPort()))));
+    jdbcTraceAssertion(dbInfo, query);
 
     statement.close();
     connection.close();
   }
 
+  @SuppressWarnings("unchecked")
   @Test
   void testVerifyPrepareStatementReturnsOtelWrapper() throws Exception {
     OpenTelemetry ot = OpenTelemetry.propagating(ContextPropagators.noop());
@@ -150,11 +112,9 @@ class OpenTelemetryConnectionTest {
         .isInstanceOf(OpenTelemetryPreparedStatement.class);
     assertThat(connection.prepareStatement(query, 0, 0, 0))
         .isInstanceOf(OpenTelemetryPreparedStatement.class);
-
-    Field instrumenterField = OpenTelemetryStatement.class.getDeclaredField("instrumenter");
-    instrumenterField.setAccessible(true);
-
-    assertThat(instrumenterField.get(connection.prepareStatement(query))).isEqualTo(instrumenter);
+    assertThat(
+            ((OpenTelemetryStatement<Statement>) connection.prepareStatement(query)).instrumenter)
+        .isEqualTo(instrumenter);
 
     connection.close();
   }
@@ -175,6 +135,47 @@ class OpenTelemetryConnectionTest {
           assertThat(statement.execute()).isTrue();
         });
 
+    jdbcTraceAssertion(dbInfo, query);
+
+    statement.close();
+    connection.close();
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  void testVerifyPrepareCallReturnsOtelWrapper() throws Exception {
+    OpenTelemetry ot = OpenTelemetry.propagating(ContextPropagators.noop());
+    Instrumenter<DbRequest, Void> instrumenter = createStatementInstrumenter(ot);
+    OpenTelemetryConnection connection =
+        new OpenTelemetryConnection(new TestConnection(), DbInfo.DEFAULT, instrumenter);
+    String query = "SELECT * FROM users";
+
+    assertThat(connection.prepareCall(query)).isInstanceOf(OpenTelemetryCallableStatement.class);
+
+    assertThat(connection.prepareCall(query)).isInstanceOf(OpenTelemetryCallableStatement.class);
+    assertThat(connection.prepareCall(query, 0, 0))
+        .isInstanceOf(OpenTelemetryCallableStatement.class);
+    assertThat(connection.prepareCall(query, 0, 0, 0))
+        .isInstanceOf(OpenTelemetryCallableStatement.class);
+    assertThat(((OpenTelemetryStatement<Statement>) connection.prepareCall(query)).instrumenter)
+        .isEqualTo(instrumenter);
+
+    connection.close();
+  }
+
+  private static DbInfo getDbInfo() {
+    return DbInfo.builder()
+        .system("my_system")
+        .subtype("my_sub_type")
+        .user("my_user")
+        .name("my_name")
+        .db("my_db")
+        .host("my_host")
+        .port(1234)
+        .build();
+  }
+
+  private static void jdbcTraceAssertion(DbInfo dbInfo, String query) {
     testing.waitAndAssertTraces(
         trace ->
             trace.hasSpansSatisfyingExactly(
@@ -192,44 +193,5 @@ class OpenTelemetryConnectionTest {
                             equalTo(DbIncubatingAttributes.DB_SQL_TABLE, "users"),
                             equalTo(ServerAttributes.SERVER_ADDRESS, dbInfo.getHost()),
                             equalTo(ServerAttributes.SERVER_PORT, dbInfo.getPort()))));
-
-    statement.close();
-    connection.close();
-  }
-
-  @Test
-  void testVerifyPrepareCallReturnsOtelWrapper() throws Exception {
-    OpenTelemetry ot = OpenTelemetry.propagating(ContextPropagators.noop());
-    Instrumenter<DbRequest, Void> instrumenter = createStatementInstrumenter(ot);
-    OpenTelemetryConnection connection =
-        new OpenTelemetryConnection(new TestConnection(), DbInfo.DEFAULT, instrumenter);
-    String query = "SELECT * FROM users";
-
-    assertThat(connection.prepareCall(query)).isInstanceOf(OpenTelemetryCallableStatement.class);
-
-    assertThat(connection.prepareCall(query)).isInstanceOf(OpenTelemetryCallableStatement.class);
-    assertThat(connection.prepareCall(query, 0, 0))
-        .isInstanceOf(OpenTelemetryCallableStatement.class);
-    assertThat(connection.prepareCall(query, 0, 0, 0))
-        .isInstanceOf(OpenTelemetryCallableStatement.class);
-
-    Field instrumenterField = OpenTelemetryStatement.class.getDeclaredField("instrumenter");
-    instrumenterField.setAccessible(true);
-
-    assertThat(instrumenterField.get(connection.prepareStatement(query))).isEqualTo(instrumenter);
-
-    connection.close();
-  }
-
-  private static DbInfo getDbInfo() {
-    return DbInfo.builder()
-        .system("my_system")
-        .subtype("my_sub_type")
-        .user("my_user")
-        .name("my_name")
-        .db("my_db")
-        .host("my_host")
-        .port(1234)
-        .build();
   }
 }
