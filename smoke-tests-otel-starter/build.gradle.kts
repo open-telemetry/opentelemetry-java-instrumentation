ed2a7bd4d4a802869a449ae125c3ff241687da05
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-04-23 10:07:49 +0200

    fix double dependency import

--- smoke-tests-otel-starter/build.gradle.kts
+++ smoke-tests-otel-starter/build.gradle.kts
@@ -17,12 +17,11 @@ dependencies {
   implementation("org.apache.commons:commons-dbcp2")
   implementation(project(":instrumentation:jdbc:library"))
   implementation("org.springframework.kafka:spring-kafka") // not tested here, just make sure there are no warnings when it's included
-  implementation("org.springframework.boot:spring-boot-starter-webflux") // not tested here, just make sure there are no warnings when it's included
   implementation("io.opentelemetry:opentelemetry-extension-trace-propagators")
   implementation(project(":instrumentation:spring:starters:spring-boot-starter"))
   implementation(platform(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES))
 
-  // webflux
+  // webflux / reactive style
   implementation("org.springframework.boot:spring-boot-starter-webflux")
 
   testImplementation("org.springframework.boot:spring-boot-starter-test")
