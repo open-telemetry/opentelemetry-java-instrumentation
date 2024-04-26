e79acea4f2290cfec2e0eaa149a83b4a72c17577
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-04-26 14:02:01 +0200

    remove workaround for older version

--- smoke-tests-otel-starter/src/main/java/io/opentelemetry/spring/smoketest/RuntimeHints.java
+++ smoke-tests-otel-starter/src/main/java/io/opentelemetry/spring/smoketest/RuntimeHints.java
@@ -6,8 +6,6 @@
 package io.opentelemetry.spring.smoketest;
 
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
-import org.springframework.aot.hint.TypeReference;
-import org.springframework.data.web.config.SpringDataJacksonConfiguration;
 
 // Necessary for GraalVM native test
 public class RuntimeHints implements RuntimeHintsRegistrar {
@@ -16,15 +14,5 @@ public class RuntimeHints implements RuntimeHintsRegistrar {
   public void registerHints(
       org.springframework.aot.hint.RuntimeHints hints, ClassLoader classLoader) {
     hints.resources().registerResourceBundle("org.apache.commons.dbcp2.LocalStrings");
-
-    // To remove from Spring Boot 3.2.3 release:
-    // https://github.com/spring-projects/spring-data-commons/issues/3025
-    hints
-        .reflection()
-        .registerType(
-            TypeReference.of("org.springframework.data.domain.Unpaged"),
-            hint -> {
-              hint.onReachableType(SpringDataJacksonConfiguration.PageModule.class);
-            });
   }
 }
