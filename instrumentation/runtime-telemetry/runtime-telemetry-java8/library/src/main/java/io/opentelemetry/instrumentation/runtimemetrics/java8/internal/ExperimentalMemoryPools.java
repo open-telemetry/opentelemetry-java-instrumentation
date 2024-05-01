745d880c06676a28d75c5567aa7bd5ad68c5da2d
SylvainJuge <763082+SylvainJuge@users.noreply.github.com>
2024-05-01 16:32:10 +0200

    use semconv attributes for jvm metrics + cleanup (#11257)
    Co-authored-by: Lauri Tulmin <ltulmin@splunk.com>

--- instrumentation/runtime-telemetry/runtime-telemetry-java8/library/src/main/java/io/opentelemetry/instrumentation/runtimemetrics/java8/internal/ExperimentalMemoryPools.java
+++ instrumentation/runtime-telemetry/runtime-telemetry-java8/library/src/main/java/io/opentelemetry/instrumentation/runtimemetrics/java8/internal/ExperimentalMemoryPools.java
@@ -5,14 +5,13 @@
 
 package io.opentelemetry.instrumentation.runtimemetrics.java8.internal;
 
-import static io.opentelemetry.api.common.AttributeKey.stringKey;
 import static java.util.Collections.singletonList;
 
 import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.metrics.Meter;
 import io.opentelemetry.api.metrics.ObservableLongMeasurement;
+import io.opentelemetry.semconv.JvmAttributes;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryPoolMXBean;
 import java.lang.management.MemoryType;
@@ -28,14 +27,6 @@ import java.util.function.Consumer;
  * at any time.
  */
 public final class ExperimentalMemoryPools {
-
-  // TODO: use the opentelemetry-semconv classes once we have metrics attributes there
-  private static final AttributeKey<String> JVM_MEMORY_POOL_NAME =
-      stringKey("jvm.memory.pool.name");
-  private static final AttributeKey<String> JVM_MEMORY_TYPE = stringKey("jvm.memory.type");
-
-  private static final String HEAP = "heap";
-  private static final String NON_HEAP = "non_heap";
 
   /** Register observers for java runtime experimental memory metrics. */
   public static List<AutoCloseable> registerObservers(OpenTelemetry openTelemetry) {
@@ -60,8 +51,8 @@ public final class ExperimentalMemoryPools {
     for (MemoryPoolMXBean pool : poolBeans) {
       attributeSets.add(
           Attributes.builder()
-              .put(JVM_MEMORY_POOL_NAME, pool.getName())
-              .put(JVM_MEMORY_TYPE, memoryType(pool.getType()))
+              .put(JvmAttributes.JVM_MEMORY_POOL_NAME, pool.getName())
+              .put(JvmAttributes.JVM_MEMORY_TYPE, memoryType(pool.getType()))
               .build());
     }
 
@@ -84,9 +75,9 @@ public final class ExperimentalMemoryPools {
   private static String memoryType(MemoryType memoryType) {
     switch (memoryType) {
       case HEAP:
-        return HEAP;
+        return JvmAttributes.JvmMemoryTypeValues.HEAP;
       case NON_HEAP:
-        return NON_HEAP;
+        return JvmAttributes.JvmMemoryTypeValues.NON_HEAP;
     }
     return "unknown";
   }
