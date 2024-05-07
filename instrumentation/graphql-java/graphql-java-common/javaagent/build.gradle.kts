2876bc6fa1122f13a84c85afb3e24f20b2bbfd55
Lauri Tulmin <ltulmin@splunk.com>
2024-05-07 10:37:16 +0300

    Delete unused build script

--- instrumentation/graphql-java/graphql-java-common/javaagent/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-  id("otel.javaagent-instrumentation")
-}
-
-dependencies {
-  implementation(project(":instrumentation:graphql-java:graphql-java-common:library"))
-
-  library("com.graphql-java:graphql-java:12.0")
-}
