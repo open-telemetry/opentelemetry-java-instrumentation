688e1119f939ad869ef228d7f05c641dd23a5052
Lauri Tulmin <ltulmin@splunk.com>
2024-05-08 12:40:43 +0300

    Fix oshi library tests on arm mac

--- instrumentation/oshi/library/build.gradle.kts
+++ instrumentation/oshi/library/build.gradle.kts
@@ -1,9 +1,13 @@
 plugins {
   id("otel.library-instrumentation")
+  id("com.google.osdetector")
 }
 
+// 5.5.0 is the first version that works on arm mac
+val oshiVersion = if (osdetector.os == "osx" && osdetector.arch == "aarch_64") "5.5.0" else "5.3.1"
+
 dependencies {
-  library("com.github.oshi:oshi-core:5.3.1")
+  library("com.github.oshi:oshi-core:$oshiVersion")
 
   testImplementation(project(":instrumentation:oshi:testing"))
 }
