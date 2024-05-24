035dad3f402595155c64795305607993f16a2d79
gary.wgy <gary.wgy@alibaba-inc.com>
2024-05-24 17:49:19 +0800

    apply spotless

--- instrumentation/grpc-1.6/library/src/test/java/io/opentelemetry/instrumentation/grpc/v1_6/MetadataSetterTest.java
+++ instrumentation/grpc-1.6/library/src/test/java/io/opentelemetry/instrumentation/grpc/v1_6/MetadataSetterTest.java
@@ -1,27 +1,32 @@
-package io.opentelemetry.instrumentation.grpc.v1_6;
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
-import io.opentelemetry.context.Context;
-import io.grpc.Metadata;
-import io.opentelemetry.instrumentation.grpc.v1_6.propagator.GrpcPropagator;
-import org.junit.jupiter.api.Test;
+package io.opentelemetry.instrumentation.grpc.v1_6;
 
 import static org.junit.Assert.assertTrue;
 
+import io.grpc.Metadata;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.instrumentation.grpc.v1_6.propagator.GrpcPropagator;
+import org.junit.jupiter.api.Test;
+
 class MetadataSetterTest {
 
-    @Test
-    void checkThatIterableSizeEqualsOne() {
-        GrpcPropagator tested = new GrpcPropagator();
-        Metadata metadata = new Metadata();
-        tested.inject(Context.current(), metadata, MetadataSetter.INSTANCE);
+  @Test
+  void checkThatIterableSizeEqualsOne() {
+    GrpcPropagator tested = new GrpcPropagator();
+    Metadata metadata = new Metadata();
+    tested.inject(Context.current(), metadata, MetadataSetter.INSTANCE);
 
-        int size = 0;
-        for (String s : metadata.getAll(
-                Metadata.Key.of(GrpcPropagator.FIELD, Metadata.ASCII_STRING_MARSHALLER))) {
-            if (s != null) {
-                size++;
-            }
-        }
-        assertTrue(size == 1);
+    int size = 0;
+    for (String s :
+        metadata.getAll(Metadata.Key.of(GrpcPropagator.FIELD, Metadata.ASCII_STRING_MARSHALLER))) {
+      if (s != null) {
+        size++;
+      }
     }
+    assertTrue(size == 1);
+  }
 }
--- instrumentation/grpc-1.6/library/src/test/java/io/opentelemetry/instrumentation/grpc/v1_6/propagator/GrpcPropagator.java
+++ instrumentation/grpc-1.6/library/src/test/java/io/opentelemetry/instrumentation/grpc/v1_6/propagator/GrpcPropagator.java
@@ -1,32 +1,36 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package io.opentelemetry.instrumentation.grpc.v1_6.propagator;
 
 import io.opentelemetry.context.Context;
 import io.opentelemetry.context.propagation.TextMapGetter;
 import io.opentelemetry.context.propagation.TextMapPropagator;
 import io.opentelemetry.context.propagation.TextMapSetter;
-
 import java.util.Collections;
 import java.util.List;
 
 public class GrpcPropagator implements TextMapPropagator {
 
-    public static final String FIELD = "X-grpc-field";
-    private static final String METADATA = "value";
+  public static final String FIELD = "X-grpc-field";
+  private static final String METADATA = "value";
 
-    @Override
-    public List<String> fields() {
-        return Collections.singletonList(FIELD);
-    }
+  @Override
+  public List<String> fields() {
+    return Collections.singletonList(FIELD);
+  }
 
-    @Override
-    public <C> void inject(Context context, C carrier, TextMapSetter<C> setter) {
-        for (int i = 0; i < 2; i++) {
-            setter.set(carrier, FIELD, METADATA);
-        }
+  @Override
+  public <C> void inject(Context context, C carrier, TextMapSetter<C> setter) {
+    for (int i = 0; i < 2; i++) {
+      setter.set(carrier, FIELD, METADATA);
     }
+  }
 
-    @Override
-    public <C> Context extract(Context context, C carrier, TextMapGetter<C> getter) {
-        return context;
-    }
+  @Override
+  public <C> Context extract(Context context, C carrier, TextMapGetter<C> getter) {
+    return context;
+  }
 }
