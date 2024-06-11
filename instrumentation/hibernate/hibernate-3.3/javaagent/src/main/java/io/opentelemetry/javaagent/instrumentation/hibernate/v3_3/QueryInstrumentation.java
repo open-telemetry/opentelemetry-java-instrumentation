8827c1e3eff104f56654120ef6147b25893dc20b
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2024-06-11 13:33:44 +0200

    hibernate-3

--- instrumentation/hibernate/hibernate-3.3/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/hibernate/v3_3/QueryInstrumentation.java
+++ instrumentation/hibernate/hibernate-3.3/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/hibernate/v3_3/QueryInstrumentation.java
@@ -14,13 +14,13 @@ import static net.bytebuddy.matcher.ElementMatchers.named;
 import static net.bytebuddy.matcher.ElementMatchers.namedOneOf;
 
 import io.opentelemetry.context.Context;
-import io.opentelemetry.context.Scope;
 import io.opentelemetry.instrumentation.api.util.VirtualField;
 import io.opentelemetry.javaagent.bootstrap.CallDepth;
 import io.opentelemetry.javaagent.bootstrap.Java8BytecodeBridge;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeTransformer;
 import io.opentelemetry.javaagent.instrumentation.hibernate.HibernateOperation;
+import io.opentelemetry.javaagent.instrumentation.hibernate.HibernateOperationScope;
 import io.opentelemetry.javaagent.instrumentation.hibernate.SessionInfo;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.description.type.TypeDescription;
@@ -49,17 +49,12 @@ public class QueryInstrumentation implements TypeInstrumentation {
   @SuppressWarnings("unused")
   public static class QueryMethodAdvice {
 
-    @Advice.OnMethodEnter(suppress = Throwable.class)
-    public static void startMethod(
-        @Advice.This Query query,
-        @Advice.Local("otelCallDepth") CallDepth callDepth,
-        @Advice.Local("otelHibernateOperation") HibernateOperation hibernateOperation,
-        @Advice.Local("otelContext") Context context,
-        @Advice.Local("otelScope") Scope scope) {
+    @Advice.OnMethodEnter(suppress = Throwable.class, inline = false)
+    public static Object startMethod(@Advice.This Query query) {
 
-      callDepth = CallDepth.forClass(HibernateOperation.class);
+      CallDepth callDepth = CallDepth.forClass(HibernateOperation.class);
       if (callDepth.getAndIncrement() > 0) {
-        return;
+        return callDepth;
       }
 
       VirtualField<Query, SessionInfo> queryVirtualField =
@@ -67,32 +62,21 @@ public class QueryInstrumentation implements TypeInstrumentation {
       SessionInfo sessionInfo = queryVirtualField.get(query);
 
       Context parentContext = Java8BytecodeBridge.currentContext();
-      hibernateOperation =
+      HibernateOperation hibernateOperation =
           new HibernateOperation(getOperationNameForQuery(query.getQueryString()), sessionInfo);
       if (!instrumenter().shouldStart(parentContext, hibernateOperation)) {
-        return;
+        return callDepth;
       }
 
-      context = instrumenter().start(parentContext, hibernateOperation);
-      scope = context.makeCurrent();
+      return HibernateOperationScope.startNew(
+          callDepth, hibernateOperation, parentContext, instrumenter());
     }
 
     @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
     public static void endMethod(
-        @Advice.Thrown Throwable throwable,
-        @Advice.Local("otelCallDepth") CallDepth callDepth,
-        @Advice.Local("otelHibernateOperation") HibernateOperation hibernateOperation,
-        @Advice.Local("otelContext") Context context,
-        @Advice.Local("otelScope") Scope scope) {
+        @Advice.Thrown Throwable throwable, @Advice.Enter Object enterScope) {
 
-      if (callDepth.decrementAndGet() > 0) {
-        return;
-      }
-
-      if (scope != null) {
-        scope.close();
-        instrumenter().end(context, hibernateOperation, null, throwable);
-      }
+      HibernateOperationScope.end(enterScope, instrumenter(), throwable);
     }
   }
 }
--- instrumentation/hibernate/hibernate-3.3/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/hibernate/v3_3/SessionFactoryInstrumentation.java
+++ instrumentation/hibernate/hibernate-3.3/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/hibernate/v3_3/SessionFactoryInstrumentation.java
@@ -51,7 +51,7 @@ public class SessionFactoryInstrumentation implements TypeInstrumentation {
   @SuppressWarnings("unused")
   public static class SessionFactoryAdvice {
 
-    @Advice.OnMethodExit(suppress = Throwable.class)
+    @Advice.OnMethodExit(suppress = Throwable.class, inline = false)
     public static void openSession(@Advice.Return Object session) {
 
       if (session instanceof Session) {
