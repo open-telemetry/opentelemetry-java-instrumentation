b5d512bdff50029ed4792a32d6bec4cd4fb4fc56
crossoverJie <crossoverJie@gmail.com>
2024-06-17 20:33:59 +0800

    Update instrumentation-api-incubator/src/main/java/io/opentelemetry/instrumentation/api/incubator/semconv/messaging/MessagingMetricsAdvice.java
    Co-authored-by: Lauri Tulmin <tulmin@gmail.com>

--- instrumentation-api-incubator/src/main/java/io/opentelemetry/instrumentation/api/incubator/semconv/messaging/MessagingMetricsAdvice.java
+++ instrumentation-api-incubator/src/main/java/io/opentelemetry/instrumentation/api/incubator/semconv/messaging/MessagingMetricsAdvice.java
@@ -15,7 +15,7 @@ import io.opentelemetry.semconv.ServerAttributes;
 import io.opentelemetry.semconv.incubating.MessagingIncubatingAttributes;
 import java.util.List;
 
-public class MessagingMetricsAdvice {
+final class MessagingMetricsAdvice {
   static final List<Double> DURATION_SECONDS_BUCKETS =
       unmodifiableList(
           asList(0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1.0, 2.5, 5.0, 7.5, 10.0));
