771cc58fe5d7902e1ce38bcbce2f14bbac55eb04
Lauri Tulmin <ltulmin@splunk.com>
2024-06-18 15:59:31 +0300

    resue code from @WithSpan implementation to extract method paramter attibutes

--- instrumentation/opentelemetry-instrumentation-annotations/opentelemetry-instrumentation-annotations-2.6/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/instrumentationannotations/v2_6/MetricsAnnotationHelper.java
+++ instrumentation/opentelemetry-instrumentation-annotations/opentelemetry-instrumentation-annotations-2.6/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/instrumentationannotations/v2_6/MetricsAnnotationHelper.java
@@ -9,37 +9,54 @@ import application.io.opentelemetry.instrumentation.annotations.MetricAttribute;
 import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.common.AttributesBuilder;
 import io.opentelemetry.api.metrics.Meter;
+import io.opentelemetry.instrumentation.api.annotation.support.SpanAttributesExtractor;
 import io.opentelemetry.javaagent.instrumentation.instrumentationannotations.MethodRequest;
 import java.lang.reflect.Parameter;
+import javax.annotation.Nullable;
 
 public abstract class MetricsAnnotationHelper {
   private static final String INSTRUMENTATION_NAME =
       "io.opentelemetry.opentelemetry-instrumentation-annotations-2.6";
   static final Meter METER = GlobalOpenTelemetry.get().getMeter(INSTRUMENTATION_NAME);
+  private static final SpanAttributesExtractor PARAMETER_ATTRIBUTES = createAttributesExtractor();
 
   static void extractMetricAttributes(
       MethodRequest methodRequest, AttributesBuilder attributesBuilder) {
-    Parameter[] parameters = methodRequest.method().getParameters();
-    for (int i = 0; i < parameters.length; i++) {
-      if (parameters[i].isAnnotationPresent(MetricAttribute.class)) {
-        MetricAttribute annotation = parameters[i].getAnnotation(MetricAttribute.class);
-        String attributeKey;
-        if (!annotation.value().isEmpty()) {
-          attributeKey = annotation.value();
-        } else if (parameters[i].isNamePresent()) {
-          attributeKey = parameters[i].getName();
-        } else {
-          continue;
-        }
-        attributesBuilder.put(attributeKey, methodRequest.args()[i].toString());
-      }
-    }
+    attributesBuilder.putAll(
+        PARAMETER_ATTRIBUTES.extract(methodRequest.method(), methodRequest.args()));
   }
 
   static void extractAdditionAttributes(String[] attributes, AttributesBuilder attributesBuilder) {
     int length = attributes.length;
     for (int i = 0; i + 1 < length; i += 2) {
       attributesBuilder.put(attributes[i], attributes[i + 1]);
+    }
+  }
+
+  private static SpanAttributesExtractor createAttributesExtractor() {
+    return SpanAttributesExtractor.create(
+        (method, parameters) -> {
+          String[] attributeNames = new String[parameters.length];
+          for (int i = 0; i < parameters.length; i++) {
+            attributeNames[i] = attributeName(parameters[i]);
+          }
+          return attributeNames;
+        });
+  }
+
+  @Nullable
+  private static String attributeName(Parameter parameter) {
+    MetricAttribute annotation = parameter.getDeclaredAnnotation(MetricAttribute.class);
+    if (annotation == null) {
+      return null;
+    }
+    String value = annotation.value();
+    if (!value.isEmpty()) {
+      return value;
+    } else if (parameter.isNamePresent()) {
+      return parameter.getName();
+    } else {
+      return null;
     }
   }
 }
