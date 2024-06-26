de28d298eeb6084c88c895eebdc7eda42d22a742
Lauri Tulmin <ltulmin@splunk.com>
2024-06-26 15:18:43 +0300

    add javadoc

--- instrumentation-annotations-support/src/main/java/io/opentelemetry/instrumentation/api/annotation/support/MethodBinder.java
+++ instrumentation-annotations-support/src/main/java/io/opentelemetry/instrumentation/api/annotation/support/MethodBinder.java
@@ -10,8 +10,10 @@ import java.lang.reflect.Method;
 import java.util.function.BiConsumer;
 import javax.annotation.Nullable;
 
+/** Helper class for binding method parameters and return value to attributes. */
 public final class MethodBinder {
 
+  /** Create binding for method return value. */
   @Nullable
   public static BiConsumer<AttributesBuilder, Object> bindReturnValue(
       Method method, String attributeName) {
@@ -23,6 +25,7 @@ public final class MethodBinder {
     return binding::apply;
   }
 
+  /** Create binding for method parameters. */
   @Nullable
   public static BiConsumer<AttributesBuilder, Object[]> bindParameters(
       Method method, ParameterAttributeNamesExtractor parameterAttributeNamesExtractor) {
