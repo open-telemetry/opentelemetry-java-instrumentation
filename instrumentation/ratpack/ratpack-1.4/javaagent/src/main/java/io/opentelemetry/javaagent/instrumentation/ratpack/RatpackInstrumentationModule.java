f6ff56da4ceee0c2f794451264b8f7ead8461aac
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2024-06-26 14:58:33 +0200

    spotless

--- instrumentation/ratpack/ratpack-1.4/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/ratpack/RatpackInstrumentationModule.java
+++ instrumentation/ratpack/ratpack-1.4/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/ratpack/RatpackInstrumentationModule.java
@@ -14,8 +14,8 @@ import io.opentelemetry.javaagent.extension.instrumentation.internal.Experimenta
 import java.util.List;
 
 @AutoService(InstrumentationModule.class)
-public class RatpackInstrumentationModule extends InstrumentationModule implements
-    ExperimentalInstrumentationModule {
+public class RatpackInstrumentationModule extends InstrumentationModule
+    implements ExperimentalInstrumentationModule {
   public RatpackInstrumentationModule() {
     super("ratpack", "ratpack-1.4");
   }
