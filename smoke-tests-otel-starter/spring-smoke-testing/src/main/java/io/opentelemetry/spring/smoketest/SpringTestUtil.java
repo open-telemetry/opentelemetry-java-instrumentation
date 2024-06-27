5b0f9fd4fe34da6f99d9663e21beeeadaa6073be
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-06-27 10:17:45 +0200

    fix rebase

--- smoke-tests-otel-starter/spring-smoke-testing/src/main/java/io/opentelemetry/spring/smoketest/SpringTestUtil.java
+++ smoke-tests-otel-starter/spring-smoke-testing/src/main/java/io/opentelemetry/spring/smoketest/SpringTestUtil.java
@@ -5,39 +5,36 @@
 
 package io.opentelemetry.spring.smoketest;
 
+import static io.opentelemetry.sdk.testing.assertj.OpenTelemetryAssertions.satisfies;
+
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
-import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.trace.SpanKind;
-import io.opentelemetry.sdk.testing.assertj.OpenTelemetryAssertions;
 import io.opentelemetry.sdk.testing.assertj.SpanDataAssert;
 import io.opentelemetry.semconv.HttpAttributes;
 import io.opentelemetry.semconv.UrlAttributes;
+import io.opentelemetry.semconv.incubating.HttpIncubatingAttributes;
 import org.assertj.core.api.AbstractLongAssert;
 
 public class SpringTestUtil {
-
-  private static final AttributeKey<Long> BODY_SIZE =
-      AttributeKey.longKey("http.response.body.size");
 
   private SpringTestUtil() {}
 
   public static void assertClientSpan(SpanDataAssert span, String path) {
     span.hasKind(SpanKind.CLIENT)
         .hasAttributesSatisfying(
-            OpenTelemetryAssertions.satisfies(UrlAttributes.URL_FULL, a -> a.endsWith(path)),
+            satisfies(UrlAttributes.URL_FULL, a -> a.endsWith(path)),
             // this attribute is set by the experimental http instrumentation
-            OpenTelemetryAssertions.satisfies(BODY_SIZE, AbstractLongAssert::isNotNegative));
+            satisfies(HttpIncubatingAttributes.HTTP_RESPONSE_BODY_SIZE, AbstractLongAssert::isNotNegative));
   }
 
   @CanIgnoreReturnValue
   public static SpanDataAssert assertServerSpan(SpanDataAssert span, String route) {
-    span.hasKind(SpanKind.SERVER)
+    return span.hasKind(SpanKind.SERVER)
         .hasAttribute(HttpAttributes.HTTP_REQUEST_METHOD, "GET")
         .hasAttribute(HttpAttributes.HTTP_RESPONSE_STATUS_CODE, 200L)
         .hasAttribute(HttpAttributes.HTTP_ROUTE, route)
         .hasAttributesSatisfying(
             // this attribute is set by the experimental http instrumentation
-            OpenTelemetryAssertions.satisfies(BODY_SIZE, AbstractLongAssert::isNotNegative));
-    return span;
+            satisfies(HttpIncubatingAttributes.HTTP_RESPONSE_BODY_SIZE, AbstractLongAssert::isNotNegative));
   }
 }
