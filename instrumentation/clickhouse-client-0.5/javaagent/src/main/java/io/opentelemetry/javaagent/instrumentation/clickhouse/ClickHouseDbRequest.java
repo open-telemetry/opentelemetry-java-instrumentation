9b7a830969c77cc44a485468c07cd61dd150f83c
Lauri Tulmin <ltulmin@splunk.com>
2024-07-09 11:01:45 +0300

    Fix build

--- instrumentation/clickhouse-client-0.5/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/clickhouse/ClickHouseDbRequest.java
+++ instrumentation/clickhouse-client-0.5/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/clickhouse/ClickHouseDbRequest.java
@@ -8,13 +8,13 @@ package io.opentelemetry.javaagent.instrumentation.clickhouse;
 import com.google.auto.value.AutoValue;
 import io.opentelemetry.instrumentation.api.incubator.semconv.db.SqlStatementInfo;
 import io.opentelemetry.instrumentation.api.incubator.semconv.db.SqlStatementSanitizer;
-import io.opentelemetry.javaagent.bootstrap.internal.CommonConfig;
+import io.opentelemetry.javaagent.bootstrap.internal.AgentCommonConfig;
 
 @AutoValue
 public abstract class ClickHouseDbRequest {
 
   private static final SqlStatementSanitizer sanitizer =
-      SqlStatementSanitizer.create(CommonConfig.get().isStatementSanitizationEnabled());
+      SqlStatementSanitizer.create(AgentCommonConfig.get().isStatementSanitizationEnabled());
 
   public static ClickHouseDbRequest create(String host, Integer port, String dbName, String sql) {
     return new AutoValue_ClickHouseDbRequest(host, port, dbName, sanitizer.sanitize(sql));
