8cf7b5c9dec133d1ca1471dbcce68b0a282839df
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-07-10 13:11:48 +0200

    add api compare

--- instrumentation/r2dbc-1.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/r2dbc/v1_0/R2dbcSingletons.java
+++ instrumentation/r2dbc-1.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/r2dbc/v1_0/R2dbcSingletons.java
@@ -7,8 +7,8 @@ package io.opentelemetry.javaagent.instrumentation.r2dbc.v1_0;
 
 import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.instrumentation.api.incubator.semconv.net.PeerServiceAttributesExtractor;
-import io.opentelemetry.instrumentation.r2dbc.v1_0.R2dbcTelemetry;
-import io.opentelemetry.instrumentation.r2dbc.v1_0.internal.R2dbcNetAttributesGetter;
+import io.opentelemetry.instrumentation.r2dbc.v1_0.shaded.R2dbcTelemetry;
+import io.opentelemetry.instrumentation.r2dbc.v1_0.shaded.internal.R2dbcNetAttributesGetter;
 import io.opentelemetry.javaagent.bootstrap.internal.AgentCommonConfig;
 import io.opentelemetry.javaagent.bootstrap.internal.AgentInstrumentationConfig;
 
