db3a78924947dbb831df6e00332613d590150c62
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-07-11 20:02:59 +0200

    rename spring autoconfigure to autoconfigure-2

--- instrumentation/spring/spring-boot-autoconfigure-3/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/v3/instrumentation/annotations/OpenTelemetryAnnotationsRuntimeHints.java
+++ instrumentation/spring/spring-boot-autoconfigure-3/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/v3/instrumentation/annotations/OpenTelemetryAnnotationsRuntimeHints.java
@@ -18,7 +18,7 @@ class OpenTelemetryAnnotationsRuntimeHints implements RuntimeHintsRegistrar {
         .reflection()
         .registerType(
             TypeReference.of(
-                "io.opentelemetry.instrumentation.spring.autoconfigure.instrumentation.annotations.InstrumentationWithSpanAspect"),
+                "io.opentelemetry.instrumentation.spring.autoconfigure.v2.instrumentation.annotations.InstrumentationWithSpanAspect"),
             hint -> hint.withMembers(MemberCategory.INVOKE_PUBLIC_METHODS));
   }
 }
