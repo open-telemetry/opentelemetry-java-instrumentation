2bc79a89dd80ab4d65ae21a26b1ad4bc17a44926
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2024-07-12 19:06:18 +0200

    use api instr. suppression

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -8,7 +8,7 @@ val dependencyVersions = hashMapOf<String, String>()
 rootProject.extra["versions"] = dependencyVersions
 
 // this line is managed by .github/scripts/update-sdk-version.sh
-val otelSdkVersion = "1.39.0"
+val otelSdkVersion = "1.41.0-SNAPSHOT"
 val otelContribVersion = "1.36.0-alpha"
 val otelSdkAlphaVersion = otelSdkVersion.replaceFirst("(-SNAPSHOT)?$".toRegex(), "-alpha$1")
 
