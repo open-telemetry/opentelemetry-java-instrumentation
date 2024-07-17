30f7c9c726cf4660a78538905609c81328cb4038
crossoverJie <crossoverJie@gmail.com>
2024-07-17 15:07:32 +0800

    fix muzzle

--- instrumentation/grpc-1.6/library/build.gradle.kts
+++ instrumentation/grpc-1.6/library/build.gradle.kts
@@ -7,6 +7,7 @@ val grpcVersion = "1.6.0"
 
 dependencies {
   library("io.grpc:grpc-core:$grpcVersion")
+  library("com.google.protobuf:protobuf-java:3.25.3")
 
   testLibrary("io.grpc:grpc-netty:$grpcVersion")
   testLibrary("io.grpc:grpc-protobuf:$grpcVersion")
