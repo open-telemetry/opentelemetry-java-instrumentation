9f6055ddd0ab1669c768b816a1fbea96dea18246
crossoverJie <crossoverJie@gmail.com>
2024-07-17 17:51:34 +0800

    fix ci

--- instrumentation/grpc-1.6/testing/src/main/java/io/opentelemetry/instrumentation/grpc/v1_6/AbstractGrpcStreamingTest.java
+++ instrumentation/grpc-1.6/testing/src/main/java/io/opentelemetry/instrumentation/grpc/v1_6/AbstractGrpcStreamingTest.java
@@ -28,6 +28,7 @@ import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
+import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.api.trace.Tracer;
@@ -51,6 +52,15 @@ import org.junit.jupiter.api.Test;
 import org.junitpioneer.jupiter.cartesian.CartesianTest;
 
 public abstract class AbstractGrpcStreamingTest {
+
+  private static final AttributeKey<Long> RPC_CLIENT_REQUEST_BODY_SIZE =
+      AttributeKey.longKey("rpc.client.request.body.size");
+  private static final AttributeKey<Long> RPC_CLIENT_RESPONSE_BODY_SIZE =
+      AttributeKey.longKey("rpc.client.response.body.size");
+  private static final AttributeKey<Long> RPC_SERVER_REQUEST_BODY_SIZE =
+      AttributeKey.longKey("rpc.server.request.body.size");
+  private static final AttributeKey<Long> RPC_SERVER_RESPONSE_BODY_SIZE =
+      AttributeKey.longKey("rpc.server.response.body.size");
 
   protected abstract ServerBuilder<?> configureServer(ServerBuilder<?> server);
 
@@ -159,6 +169,9 @@ public abstract class AbstractGrpcStreamingTest {
                 .sorted()
                 .collect(Collectors.toList()));
 
+    Helloworld.Response message = Helloworld.Response.newBuilder().setMessage("call " + 1).build();
+    int requestSerializedSize = message.getSerializedSize();
+
     List<Consumer<EventData>> clientEvents = new ArrayList<>();
     List<Consumer<EventData>> serverEvents = new ArrayList<>();
     for (long i = 0; i < clientMessageCount; i++) {
@@ -229,6 +242,8 @@ public abstract class AbstractGrpcStreamingTest {
                                     equalTo(RPC_GRPC_STATUS_CODE, (long) Status.Code.OK.value()),
                                     equalTo(SERVER_ADDRESS, "localhost"),
                                     equalTo(SERVER_PORT, (long) server.getPort())))
+                                    equalTo(RPC_CLIENT_RESPONSE_BODY_SIZE, requestSerializedSize),
+                                    equalTo(RPC_CLIENT_REQUEST_BODY_SIZE, requestSerializedSize),
                             .satisfies(
                                 spanData ->
                                     assertThat(spanData.getEvents())
@@ -246,6 +261,8 @@ public abstract class AbstractGrpcStreamingTest {
                                 equalTo(SERVER_PORT, server.getPort()),
                                 equalTo(NETWORK_TYPE, "ipv4"),
                                 equalTo(NETWORK_PEER_ADDRESS, "127.0.0.1"),
+                                equalTo(RPC_SERVER_REQUEST_BODY_SIZE, requestSerializedSize),
+                                equalTo(RPC_SERVER_RESPONSE_BODY_SIZE, requestSerializedSize),
                                 satisfies(NETWORK_PEER_PORT, val -> assertThat(val).isNotNull()))
                             .satisfies(
                                 spanData ->
