51d282ed87f4c719dbe1124993fb3b69e5258237
Adriano Machado <60320+ammachado@users.noreply.github.com>
2024-08-08 09:30:20 -0400

    Changes from PR review

--- instrumentation/jaxws/jaxws-3.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/jaxws/v3_0/JaxWsInstrumentationModule.java
+++ instrumentation/jaxws/jaxws-3.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/jaxws/v3_0/JaxWsInstrumentationModule.java
@@ -5,10 +5,11 @@
 
 package io.opentelemetry.javaagent.instrumentation.jaxws.v3_0;
 
+import static java.util.Collections.singletonList;
+
 import com.google.auto.service.AutoService;
 import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
-import java.util.Collections;
 import java.util.List;
 
 @AutoService(InstrumentationModule.class)
@@ -20,6 +21,6 @@ public class JaxWsInstrumentationModule extends InstrumentationModule {
 
   @Override
   public List<TypeInstrumentation> typeInstrumentations() {
-    return Collections.singletonList(new WebServiceProviderInstrumentation());
+    return singletonList(new WebServiceProviderInstrumentation());
   }
 }
