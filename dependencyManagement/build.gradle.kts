15d773f03e65c366b4d1a807b8ca6ee8925e7422
opentelemetrybot <107717825+opentelemetrybot@users.noreply.github.com>
2024-08-09 16:50:41 +0000

    Update the OpenTelemetry SDK version to 1.41.0

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -8,7 +8,7 @@ val dependencyVersions = hashMapOf<String, String>()
 rootProject.extra["versions"] = dependencyVersions
 
 // this line is managed by .github/scripts/update-sdk-version.sh
-val otelSdkVersion = "1.40.0"
+val otelSdkVersion = "1.41.0"
 val otelContribVersion = "1.37.0-alpha"
 val otelSdkAlphaVersion = otelSdkVersion.replaceFirst("(-SNAPSHOT)?$".toRegex(), "-alpha$1")
 
