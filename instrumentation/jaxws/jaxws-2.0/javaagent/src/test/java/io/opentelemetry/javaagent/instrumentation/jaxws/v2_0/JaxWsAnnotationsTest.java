f4b688b429eb8d48cfc184a330cf42e8d882c034
Adriano Machado <60320+ammachado@users.noreply.github.com>
2024-08-11 22:28:18 -0400

    Converting some Groovy jaxws tests to Java

---
+++ instrumentation/jaxws/jaxws-2.0/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/jaxws/v2_0/JaxWsAnnotationsTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.jaxws.v2_0;
+
+import static io.opentelemetry.sdk.testing.assertj.OpenTelemetryAssertions.equalTo;
+
+import io.opentelemetry.instrumentation.test.AgentInstrumentationSpecification;
+import io.opentelemetry.instrumentation.testing.junit.AgentInstrumentationExtension;
+import io.opentelemetry.semconv.incubating.CodeIncubatingAttributes;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+class JaxWsAnnotationsTest extends AgentInstrumentationSpecification {
+
+  @RegisterExtension
+  protected static final AgentInstrumentationExtension testing =
+      AgentInstrumentationExtension.create();
+
+  @Test
+  void webServiceProvidersGenerateSpans() {
+
+    SoapProvider soapProvider = new SoapProvider();
+    soapProvider.invoke(null);
+
+    testing.waitAndAssertTraces(
+        trace ->
+            trace.hasSpansSatisfyingExactly(
+                span ->
+                    span.hasName("SoapProvider.invoke")
+                        .hasAttributesSatisfyingExactly(
+                            equalTo(
+                                CodeIncubatingAttributes.CODE_NAMESPACE,
+                                "io.opentelemetry.javaagent.instrumentation.jaxws.v3_0.SoapProvider"),
+                            equalTo(CodeIncubatingAttributes.CODE_FUNCTION, "invoke"))));
+  }
+}
