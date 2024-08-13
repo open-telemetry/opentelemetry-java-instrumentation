8b93008b27164f9c64b15abbb9f70a918179119b
Serkan ÖZAL <sozal@catchpoint.com>
2024-08-13 18:21:14 +0300

    Add test

---
+++ javaagent-tooling/testing-java18/src/test/java/io/opentelemetry/javaagent/tooling/inetaddress/InetAddressResolverTest.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.tooling.inetaddress;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.InetAddress;
+import org.junit.jupiter.api.Test;
+
+public class InetAddressResolverTest {
+
+  @Test
+  void agentStartShouldNotTriggerLoadingCustomInetAddressResolvers() throws Exception {
+    // Agent start should not trigger loading (and instantiating) custom InetAddress resolvers
+    assertThat(TestAddressResolver.isInstantiated()).isFalse();
+
+    // Trigger loading (and instantiating) custom InetAddress resolvers manually
+    InetAddress.getAllByName("test");
+
+    // Verify that custom InetAddress resolver loaded and instantiated
+    assertThat(TestAddressResolver.isInstantiated()).isTrue();
+  }
+}
