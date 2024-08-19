0c065b898968fd25aa543acd4207aa753e641fd9
Lauri Tulmin <tulmin@gmail.com>
2024-08-19 17:40:47 +0300

    Update instrumentation/pulsar/pulsar-2.8/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/pulsar/v2_8/SendCallBackData.java

--- instrumentation/pulsar/pulsar-2.8/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/pulsar/v2_8/SendCallBackData.java
+++ instrumentation/pulsar/pulsar-2.8/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/pulsar/v2_8/SendCallBackData.java
@@ -8,7 +8,7 @@ package io.opentelemetry.javaagent.instrumentation.pulsar.v2_8;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.javaagent.instrumentation.pulsar.v2_8.telemetry.PulsarRequest;
 
-public class SendCallBackData {
+public final class SendCallBackData {
   public final Context context;
   public final PulsarRequest request;
 
