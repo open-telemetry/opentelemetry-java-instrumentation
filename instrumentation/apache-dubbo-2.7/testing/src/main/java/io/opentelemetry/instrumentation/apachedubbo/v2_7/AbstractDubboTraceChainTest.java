538ebf6a5b23dda731ff90bdbd14a11780f61447
shalk(xiao kun) <xshalk@outlook.com>
2024-08-19 10:41:02 +0800

    Update instrumentation/apache-dubbo-2.7/testing/src/main/java/io/opentelemetry/instrumentation/apachedubbo/v2_7/AbstractDubboTraceChainTest.java
    Co-authored-by: Jay DeLuca <jaydeluca4@gmail.com>

--- instrumentation/apache-dubbo-2.7/testing/src/main/java/io/opentelemetry/instrumentation/apachedubbo/v2_7/AbstractDubboTraceChainTest.java
+++ instrumentation/apache-dubbo-2.7/testing/src/main/java/io/opentelemetry/instrumentation/apachedubbo/v2_7/AbstractDubboTraceChainTest.java
@@ -239,6 +239,7 @@ abstract class AbstractDubboTraceChainTest {
                     span ->
                         span.hasName("org.apache.dubbo.rpc.service.GenericService/$invoke")
                             .hasKind(SpanKind.CLIENT)
+                            .hasParent(trace.getSpan(2))
                             .hasAttributesSatisfyingExactly(
                                 equalTo(RpcIncubatingAttributes.RPC_SYSTEM, "apache_dubbo"),
                                 equalTo(
