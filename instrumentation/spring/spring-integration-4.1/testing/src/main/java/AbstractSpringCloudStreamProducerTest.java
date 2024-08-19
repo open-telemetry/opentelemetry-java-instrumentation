2ee534011f73aee91407082b91d75b9fe8f26fe4
Gregor Zeitlinger <gregor.zeitlinger@zalando.de>
2024-08-19 12:50:50 +0200

    Update instrumentation/spring/spring-integration-4.1/testing/src/main/java/AbstractSpringCloudStreamProducerTest.java
    Co-authored-by: Jay DeLuca <jaydeluca4@gmail.com>

--- instrumentation/spring/spring-integration-4.1/testing/src/main/java/AbstractSpringCloudStreamProducerTest.java
+++ instrumentation/spring/spring-integration-4.1/testing/src/main/java/AbstractSpringCloudStreamProducerTest.java
@@ -35,6 +35,6 @@ public abstract class AbstractSpringCloudStreamProducerTest
                 span -> span.hasName("producer").hasKind(SpanKind.INTERNAL),
                 span -> span.hasName("testProducer.output publish").hasKind(SpanKind.PRODUCER).hasParent(trace.getSpan(0)),
                 span -> span.hasName("testConsumer.input process").hasKind(SpanKind.CONSUMER),
-                span -> span.hasName("consumer").hasKind(SpanKind.INTERNAL)));
+                span -> span.hasName("consumer").hasKind(SpanKind.INTERNAL)).hasParent(trace.getSpan(2)));
   }
 }
