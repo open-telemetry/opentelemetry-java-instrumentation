7675f762e8411f5ac57bbfabd24864e7c46e81a9
opentelemetrybot <107717825+opentelemetrybot@users.noreply.github.com>
2024-08-22 10:09:00 +0000

    Prepare release 1.33.6

--- version.gradle.kts
+++ version.gradle.kts
@@ -1,5 +1,5 @@
-val stableVersion = "1.33.5"
-val alphaVersion = "1.33.5-alpha"
+val stableVersion = "1.33.6"
+val alphaVersion = "1.33.6-alpha"
 
 allprojects {
   if (findProperty("otel.stable") != "true") {
