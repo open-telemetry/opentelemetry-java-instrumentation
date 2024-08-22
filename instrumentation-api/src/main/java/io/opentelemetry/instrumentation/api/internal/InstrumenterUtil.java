d6d20c5e564b2453b259088a396d1bd2d0aa3c78
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-08-22 15:09:01 +0200

    fix

--- instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/internal/InstrumenterUtil.java
+++ instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/internal/InstrumenterUtil.java
@@ -5,6 +5,7 @@
 
 package io.opentelemetry.instrumentation.api.internal;
 
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.context.propagation.TextMapGetter;
 import io.opentelemetry.context.propagation.TextMapSetter;
@@ -67,6 +68,7 @@ public final class InstrumenterUtil {
         builder, setter, spanKindExtractor);
   }
 
+  @CanIgnoreReturnValue
   public static <REQUEST, RESPONSE>
       InstrumenterBuilder<REQUEST, RESPONSE> propagateOperationListenersToOnEnd(
           InstrumenterBuilder<REQUEST, RESPONSE> builder) {
