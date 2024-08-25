0bce0e63019eeefe5f5b7691400e4bebfdcb5fde
xiepuhuan <puhuanxie@gmail.com>
2024-08-25 23:49:11 +0800

    Convert apache-httpclient-2.0 tests from groovy to java

---
+++ instrumentation/apache-httpclient/apache-httpclient-2.0/javaagent/src/test/java/AbstractCommonsHttpClientTest.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import io.opentelemetry.instrumentation.testing.junit.InstrumentationExtension;
+import io.opentelemetry.instrumentation.testing.junit.http.AbstractHttpClientTest;
+import io.opentelemetry.instrumentation.testing.junit.http.HttpClientInstrumentationExtension;
+import io.opentelemetry.instrumentation.testing.junit.http.HttpClientTestOptions;
+import java.net.URI;
+import java.util.Map;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpConnectionManager;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
+import org.apache.commons.httpclient.methods.DeleteMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.HeadMethod;
+import org.apache.commons.httpclient.methods.OptionsMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.PutMethod;
+import org.apache.commons.httpclient.methods.TraceMethod;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+abstract class AbstractCommonsHttpClientTest extends AbstractHttpClientTest<HttpMethod> {
+
+  @RegisterExtension
+  static final InstrumentationExtension testing = HttpClientInstrumentationExtension.forAgent();
+
+  private static final HttpConnectionManager connectionManager =
+      new MultiThreadedHttpConnectionManager();
+
+  private static final HttpClient client = new HttpClient(connectionManager);
+
+  private static final HttpClient clientWithReadTimeout = new HttpClient(connectionManager);
+
+  @BeforeAll
+  void setUp() {
+    client.setConnectionTimeout((int) connectTimeout().toMillis());
+    clientWithReadTimeout.setTimeout((int) readTimeout().toMillis());
+  }
+
+  HttpClient getClient(URI uri) {
+    if (uri.toString().contains("/read-timeout")) {
+      return clientWithReadTimeout;
+    }
+    return client;
+  }
+
+  @Override
+  public HttpMethod buildRequest(String method, URI uri, Map<String, String> headers) {
+    HttpMethod request;
+    switch (method) {
+      case "GET":
+        request = new GetMethod(uri.toString());
+        break;
+      case "PUT":
+        request = new PutMethod(uri.toString());
+        break;
+      case "POST":
+        request = new PostMethod(uri.toString());
+        break;
+      case "HEAD":
+        request = new HeadMethod(uri.toString());
+        break;
+      case "DELETE":
+        request = new DeleteMethod(uri.toString());
+        break;
+      case "OPTIONS":
+        request = new OptionsMethod(uri.toString());
+        break;
+      case "TRACE":
+        request = new TraceMethod(uri.toString());
+        break;
+      default:
+        throw new IllegalStateException("Unsupported method: " + method);
+    }
+
+    for (Map.Entry<String, String> entry : headers.entrySet()) {
+      request.setRequestHeader(entry.getKey(), entry.getValue());
+    }
+    return request;
+  }
+
+  @Override
+  protected void configure(HttpClientTestOptions.Builder optionsBuilder) {
+    optionsBuilder
+        .disableTestCallback()
+        .disableTestReusedRequest()
+        .disableTestNonStandardHttpMethod()
+        .disableTestCircularRedirects();
+  }
+}
---
+++ instrumentation/apache-httpclient/apache-httpclient-2.0/javaagent/src/test/java/CommonsHttpClientTest.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import java.net.URI;
+import java.util.Map;
+import org.apache.commons.httpclient.HttpMethod;
+
+public class CommonsHttpClientTest extends AbstractCommonsHttpClientTest {
+
+  @Override
+  public int sendRequest(HttpMethod request, String method, URI uri, Map<String, String> headers)
+      throws Exception {
+    try {
+      getClient(uri).executeMethod(request);
+      return request.getStatusCode();
+    } finally {
+      request.releaseConnection();
+    }
+  }
+}
