f92e3fb8c646edff1c357a9057fe66442e29e45b
Lauri Tulmin <ltulmin@splunk.com>
2024-08-27 09:32:24 +0300

    Add support for cxf 4.0 jaxws (#12077)

--- instrumentation/jaxws/jaxws-cxf-3.0/javaagent-unit-tests/build.gradle.kts
+++ instrumentation/jaxws/jaxws-cxf-3.0/javaagent-unit-tests/build.gradle.kts
@@ -6,7 +6,7 @@ dependencies {
   compileOnly("javax.servlet:javax.servlet-api:3.0.1")
   compileOnly("org.apache.cxf:cxf-rt-frontend-jaxws:3.0.0")
 
-  implementation(project(":instrumentation:jaxws:jaxws-2.0-cxf-3.0:javaagent"))
+  implementation(project(":instrumentation:jaxws:jaxws-cxf-3.0:javaagent"))
 
   testImplementation(project(":instrumentation-api"))
   testImplementation("org.apache.cxf:cxf-rt-frontend-jaxws:3.0.0")
