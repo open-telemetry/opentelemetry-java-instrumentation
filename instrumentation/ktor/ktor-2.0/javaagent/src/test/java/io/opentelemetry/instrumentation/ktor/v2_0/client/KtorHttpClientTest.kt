4d16035d894b974f2574ec3758f76fc0f19243fa
Lauri Tulmin <ltulmin@splunk.com>
2024-09-04 15:23:46 +0300

    restore removed code

--- instrumentation/ktor/ktor-2.0/javaagent/src/test/java/io/opentelemetry/instrumentation/ktor/v2_0/client/KtorHttpClientTest.kt
+++ instrumentation/ktor/ktor-2.0/javaagent/src/test/java/io/opentelemetry/instrumentation/ktor/v2_0/client/KtorHttpClientTest.kt
@@ -6,8 +6,16 @@
 package io.opentelemetry.instrumentation.ktor.v2_0.client
 
 import io.ktor.client.*
+import io.opentelemetry.instrumentation.testing.junit.http.HttpClientInstrumentationExtension
+import org.junit.jupiter.api.extension.RegisterExtension
 
 class KtorHttpClientTest : AbstractKtorHttpClientTest() {
+
+  companion object {
+    @JvmStatic
+    @RegisterExtension
+    private val TESTING = HttpClientInstrumentationExtension.forAgent()
+  }
 
   override fun HttpClientConfig<*>.installTracing() {
   }
