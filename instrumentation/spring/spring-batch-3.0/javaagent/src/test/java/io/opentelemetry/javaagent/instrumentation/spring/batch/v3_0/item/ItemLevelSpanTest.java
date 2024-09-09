2c698c9ab2b1f2d1ff95b1d17cb4bae39b68bbbb
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-09-09 13:00:21 +0200

    pr review

--- instrumentation/spring/spring-batch-3.0/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/spring/batch/v3_0/item/ItemLevelSpanTest.java
+++ instrumentation/spring/spring-batch-3.0/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/spring/batch/v3_0/item/ItemLevelSpanTest.java
@@ -9,12 +9,11 @@ import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.instrumentation.testing.junit.AgentInstrumentationExtension;
 import io.opentelemetry.instrumentation.testing.junit.InstrumentationExtension;
 import io.opentelemetry.javaagent.instrumentation.spring.batch.v3_0.runner.JobRunner;
-import io.opentelemetry.sdk.trace.data.SpanData;
+import io.opentelemetry.sdk.testing.assertj.SpanDataAssert;
 import java.lang.reflect.Field;
-import java.util.Comparator;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+import java.util.function.Consumer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.batch.core.Job;
@@ -35,115 +34,114 @@ abstract class ItemLevelSpanTest {
   }
 
   @Test
-  void shouldTraceItemReadProcessAndWriteCalls() {
+  public void shouldTraceItemReadProcessAndWriteCalls() {
     runner.runJob("itemsAndTaskletJob");
 
     testing.waitAndAssertTraces(
         trace -> {
-          Asserter with = new Asserter(trace, 37);
-          with.span(
-              0, span -> span.hasName("BatchJob itemsAndTaskletJob").hasKind(SpanKind.INTERNAL));
-          with.span(
-              1,
+          List<Consumer<SpanDataAssert>> assertions = new ArrayList<>();
+          assertions.add(
+              span -> span.hasName("BatchJob itemsAndTaskletJob").hasKind(SpanKind.INTERNAL));
+
+          // item step
+          assertions.add(
               span ->
                   span.hasName("BatchJob itemsAndTaskletJob.itemStep")
                       .hasKind(SpanKind.INTERNAL)
                       .hasParent(trace.getSpan(0)));
 
           // chunk 1, items 0-5
-          with.span(
-              2,
+          assertions.add(
               span ->
                   span.hasName("BatchJob itemsAndTaskletJob.itemStep.Chunk")
                       .hasKind(SpanKind.INTERNAL)
                       .hasParent(trace.getSpan(1)));
-          with.spans(
-              3,
-              7,
-              span ->
-                  span.hasName("BatchJob itemsAndTaskletJob.itemStep.ItemRead")
-                      .hasKind(SpanKind.INTERNAL)
-                      .hasParent(trace.getSpan(2)));
-          with.spans(
-              8,
-              12,
-              span ->
-                  span.hasName("BatchJob itemsAndTaskletJob.itemStep.ItemProcess")
-                      .hasKind(SpanKind.INTERNAL)
-                      .hasParent(trace.getSpan(2)));
-          with.span(
-              13,
+          for (int i = 3; i <= 7; i++) {
+            assertions.add(
+                span ->
+                    span.hasName("BatchJob itemsAndTaskletJob.itemStep.ItemRead")
+                        .hasKind(SpanKind.INTERNAL)
+                        .hasParent(trace.getSpan(2)));
+          }
+          for (int i = 8; i <= 12; i++) {
+            assertions.add(
+                span ->
+                    span.hasName("BatchJob itemsAndTaskletJob.itemStep.ItemProcess")
+                        .hasKind(SpanKind.INTERNAL)
+                        .hasParent(trace.getSpan(2)));
+          }
+          assertions.add(
               span ->
                   span.hasName("BatchJob itemsAndTaskletJob.itemStep.ItemWrite")
                       .hasKind(SpanKind.INTERNAL)
                       .hasParent(trace.getSpan(2)));
-          with.span(
-              14,
+
+          // chunk 2, items 5-10
+          assertions.add(
               span ->
                   span.hasName("BatchJob itemsAndTaskletJob.itemStep.Chunk")
                       .hasKind(SpanKind.INTERNAL)
                       .hasParent(trace.getSpan(1)));
-          with.spans(
-              15,
-              19,
-              span ->
-                  span.hasName("BatchJob itemsAndTaskletJob.itemStep.ItemRead")
-                      .hasKind(SpanKind.INTERNAL)
-                      .hasParent(trace.getSpan(14)));
-          with.spans(
-              20,
-              24,
-              span ->
-                  span.hasName("BatchJob itemsAndTaskletJob.itemStep.ItemProcess")
-                      .hasKind(SpanKind.INTERNAL)
-                      .hasParent(trace.getSpan(14)));
-          with.span(
-              25,
+          for (int i = 15; i <= 19; i++) {
+            assertions.add(
+                span ->
+                    span.hasName("BatchJob itemsAndTaskletJob.itemStep.ItemRead")
+                        .hasKind(SpanKind.INTERNAL)
+                        .hasParent(trace.getSpan(14)));
+          }
+          for (int i = 20; i <= 24; i++) {
+            assertions.add(
+                span ->
+                    span.hasName("BatchJob itemsAndTaskletJob.itemStep.ItemProcess")
+                        .hasKind(SpanKind.INTERNAL)
+                        .hasParent(trace.getSpan(14)));
+          }
+          assertions.add(
               span ->
                   span.hasName("BatchJob itemsAndTaskletJob.itemStep.ItemWrite")
                       .hasKind(SpanKind.INTERNAL)
                       .hasParent(trace.getSpan(14)));
+
           // chunk 3, items 10-13
-          with.span(
-              26,
+          assertions.add(
               span ->
                   span.hasName("BatchJob itemsAndTaskletJob.itemStep.Chunk")
                       .hasKind(SpanKind.INTERNAL)
                       .hasParent(trace.getSpan(1)));
           // +1 for last read returning end of stream marker
-          with.spans(
-              27,
-              30,
-              span ->
-                  span.hasName("BatchJob itemsAndTaskletJob.itemStep.ItemRead")
-                      .hasKind(SpanKind.INTERNAL)
-                      .hasParent(trace.getSpan(26)));
-          with.spans(
-              31,
-              33,
-              span ->
-                  span.hasName("BatchJob itemsAndTaskletJob.itemStep.ItemProcess")
-                      .hasKind(SpanKind.INTERNAL)
-                      .hasParent(trace.getSpan(26)));
-          with.span(
-              34,
+          for (int i = 27; i <= 30; i++) {
+            assertions.add(
+                span ->
+                    span.hasName("BatchJob itemsAndTaskletJob.itemStep.ItemRead")
+                        .hasKind(SpanKind.INTERNAL)
+                        .hasParent(trace.getSpan(26)));
+          }
+          for (int i = 31; i <= 33; i++) {
+            assertions.add(
+                span ->
+                    span.hasName("BatchJob itemsAndTaskletJob.itemStep.ItemProcess")
+                        .hasKind(SpanKind.INTERNAL)
+                        .hasParent(trace.getSpan(26)));
+          }
+          assertions.add(
               span ->
                   span.hasName("BatchJob itemsAndTaskletJob.itemStep.ItemWrite")
                       .hasKind(SpanKind.INTERNAL)
                       .hasParent(trace.getSpan(26)));
+
           // tasklet step
-          with.span(
-              35,
+          assertions.add(
               span ->
                   span.hasName("BatchJob itemsAndTaskletJob.taskletStep")
                       .hasKind(SpanKind.INTERNAL)
                       .hasParent(trace.getSpan(0)));
-          with.span(
-              36,
+          assertions.add(
               span ->
                   span.hasName("BatchJob itemsAndTaskletJob.taskletStep.Tasklet")
                       .hasKind(SpanKind.INTERNAL)
                       .hasParent(trace.getSpan(35)));
+
+          trace.hasSpansSatisfyingExactly(assertions);
         });
   }
 
@@ -153,143 +151,92 @@ abstract class ItemLevelSpanTest {
 
     testing.waitAndAssertTraces(
         trace -> {
-          Asserter with = new Asserter(trace, 19);
-
           // as chunks are processed in parallel we need to sort them to guarantee that they are
           // in the expected order
           // firstly compute child span count for each chunk, we'll sort chunks from larger to
           // smaller based on child count
-          List<SpanData> all = with.getAll();
-          Map<SpanData, Number> childCount = new HashMap<>();
-          all.forEach(
-              span -> {
-                if (span.getName().equals("BatchJob parallelItemsJob.parallelItemsStep.Chunk")) {
-                  childCount.put(
-                      span,
-                      all.stream()
-                          .filter(it -> it.getParentSpanId().equals(span.getSpanId()))
-                          .count());
-                }
-              });
-          // sort spans with a ranking function
-          all.sort(
-              Comparator.comparingInt(
-                  span -> {
-                    // job span is first
-                    if (span.getName().equals("BatchJob parallelItemsJob")) {
-                      return 0;
-                    }
-                    // step span is second
-                    if (span.getName().equals("BatchJob parallelItemsJob.parallelItemsStep")) {
-                      return 1;
-                    }
 
-                    // find the chunk this span belongs to
-                    SpanData chunkSpan = span;
-                    while (!chunkSpan
-                        .getName()
-                        .equals("BatchJob parallelItemsJob.parallelItemsStep.Chunk")) {
-                      for (SpanData it : all) {
-                        if (it.getSpanId().equals(chunkSpan.getParentSpanId())) {
-                          chunkSpan = it;
-                          break;
-                        }
-                      }
-                    }
-                    // sort larger chunks first
-                    return 100 - childCount.get(chunkSpan).intValue();
-                  }));
-          with.setSortedSpans(all);
-
-          with.span(
-              0, span -> span.hasName("BatchJob parallelItemsJob").hasKind(SpanKind.INTERNAL));
-
-          with.span(
-              1,
+          List<Consumer<SpanDataAssert>> assertions = new ArrayList<>();
+          assertions.add(
+              span -> span.hasName("BatchJob parallelItemsJob").hasKind(SpanKind.INTERNAL));
+          assertions.add(
               span ->
                   span.hasName("BatchJob parallelItemsJob.parallelItemsStep")
                       .hasKind(SpanKind.INTERNAL)
                       .hasParent(trace.getSpan(0)));
 
           // chunk 1, first two items; thread 1
-          with.span(
-              2,
+          assertions.add(
               span ->
                   span.hasName("BatchJob parallelItemsJob.parallelItemsStep.Chunk")
                       .hasKind(SpanKind.INTERNAL)
                       .hasParent(trace.getSpan(1)));
-          with.spans(
-              3,
-              4,
-              span ->
-                  span.hasName("BatchJob parallelItemsJob.parallelItemsStep.ItemRead")
-                      .hasKind(SpanKind.INTERNAL)
-                      .hasParent(trace.getSpan(2)));
-          with.spans(
-              5,
-              6,
-              span ->
-                  span.hasName("BatchJob parallelItemsJob.parallelItemsStep.ItemProcess")
-                      .hasKind(SpanKind.INTERNAL)
-                      .hasParent(trace.getSpan(2)));
-          with.span(
-              7,
+          for (int i = 3; i <= 4; i++) {
+            assertions.add(
+                span ->
+                    span.hasName("BatchJob parallelItemsJob.parallelItemsStep.ItemRead")
+                        .hasKind(SpanKind.INTERNAL)
+                        .hasParent(trace.getSpan(2)));
+          }
+          for (int i = 5; i <= 6; i++) {
+            assertions.add(
+                span ->
+                    span.hasName("BatchJob parallelItemsJob.parallelItemsStep.ItemProcess")
+                        .hasKind(SpanKind.INTERNAL)
+                        .hasParent(trace.getSpan(2)));
+          }
+          assertions.add(
               span ->
                   span.hasName("BatchJob parallelItemsJob.parallelItemsStep.ItemWrite")
                       .hasKind(SpanKind.INTERNAL)
                       .hasParent(trace.getSpan(2)));
 
           // chunk 2, items 3 & 4; thread 2
-          with.span(
-              8,
+          assertions.add(
               span ->
                   span.hasName("BatchJob parallelItemsJob.parallelItemsStep.Chunk")
                       .hasKind(SpanKind.INTERNAL)
                       .hasParent(trace.getSpan(1)));
-          with.spans(
-              9,
-              10,
-              span ->
-                  span.hasName("BatchJob parallelItemsJob.parallelItemsStep.ItemRead")
-                      .hasKind(SpanKind.INTERNAL)
-                      .hasParent(trace.getSpan(8)));
-          with.spans(
-              11,
-              12,
-              span ->
-                  span.hasName("BatchJob parallelItemsJob.parallelItemsStep.ItemProcess")
-                      .hasKind(SpanKind.INTERNAL)
-                      .hasParent(trace.getSpan(8)));
-          with.span(
-              13,
+          for (int i = 9; i <= 10; i++) {
+            assertions.add(
+                span ->
+                    span.hasName("BatchJob parallelItemsJob.parallelItemsStep.ItemRead")
+                        .hasKind(SpanKind.INTERNAL)
+                        .hasParent(trace.getSpan(8)));
+          }
+          for (int i = 11; i <= 12; i++) {
+            assertions.add(
+                span ->
+                    span.hasName("BatchJob parallelItemsJob.parallelItemsStep.ItemProcess")
+                        .hasKind(SpanKind.INTERNAL)
+                        .hasParent(trace.getSpan(8)));
+          }
+          assertions.add(
               span ->
                   span.hasName("BatchJob parallelItemsJob.parallelItemsStep.ItemWrite")
                       .hasKind(SpanKind.INTERNAL)
                       .hasParent(trace.getSpan(8)));
 
           // chunk 3, 5th item; thread 1
-          with.span(
-              14,
+          assertions.add(
               span ->
                   span.hasName("BatchJob parallelItemsJob.parallelItemsStep.Chunk")
                       .hasKind(SpanKind.INTERNAL)
                       .hasParent(trace.getSpan(1)));
           // +1 for last read returning end of stream marker
-          with.spans(
-              15,
-              16,
-              span ->
-                  span.hasName("BatchJob parallelItemsJob.parallelItemsStep.ItemRead")
-                      .hasKind(SpanKind.INTERNAL)
-                      .hasParent(trace.getSpan(14)));
-          with.span(
-              17,
+          for (int i = 15; i <= 16; i++) {
+            assertions.add(
+                span ->
+                    span.hasName("BatchJob parallelItemsJob.parallelItemsStep.ItemRead")
+                        .hasKind(SpanKind.INTERNAL)
+                        .hasParent(trace.getSpan(14)));
+          }
+          assertions.add(
               span ->
                   span.hasName("BatchJob parallelItemsJob.parallelItemsStep.ItemProcess")
                       .hasKind(SpanKind.INTERNAL)
                       .hasParent(trace.getSpan(14)));
-          with.span(
-              18,
+          assertions.add(
               span ->
                   span.hasName("BatchJob parallelItemsJob.parallelItemsStep.ItemWrite")
                       .hasKind(SpanKind.INTERNAL)
