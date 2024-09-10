53c7b96bf11e6604c6ca785b28fb715313fdf1ce
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-09-10 12:57:11 +0200

    pr review

--- instrumentation/spring/spring-batch-3.0/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/spring/batch/v3_0/item/JsrConfigItemLevelSpanTest.java
+++ instrumentation/spring/spring-batch-3.0/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/spring/batch/v3_0/item/JsrConfigItemLevelSpanTest.java
@@ -10,11 +10,11 @@ import io.opentelemetry.instrumentation.testing.junit.AgentInstrumentationExtens
 import io.opentelemetry.instrumentation.testing.junit.InstrumentationExtension;
 import io.opentelemetry.javaagent.instrumentation.spring.batch.v3_0.runner.JavaxBatchConfigRunner;
 import io.opentelemetry.sdk.testing.assertj.SpanDataAssert;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.RegisterExtension;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 class JsrConfigItemLevelSpanTest {
 
@@ -47,20 +47,19 @@ class JsrConfigItemLevelSpanTest {
                       .hasKind(SpanKind.INTERNAL)
                       .hasParent(trace.getSpan(1)));
 
-          for (int i = 3; i <= 7; i++) {
+          for (int i = 3; i <= 11; i += 2) {
             assertions.add(
                 span ->
                     span.hasName("BatchJob itemsAndTaskletJob.itemStep.ItemRead")
                         .hasKind(SpanKind.INTERNAL)
                         .hasParent(trace.getSpan(2)));
-          }
-          for (int i = 8; i <= 12; i++) {
             assertions.add(
                 span ->
                     span.hasName("BatchJob itemsAndTaskletJob.itemStep.ItemProcess")
                         .hasKind(SpanKind.INTERNAL)
                         .hasParent(trace.getSpan(2)));
           }
+
           assertions.add(
               span ->
                   span.hasName("BatchJob itemsAndTaskletJob.itemStep.ItemWrite")
@@ -73,25 +72,23 @@ class JsrConfigItemLevelSpanTest {
                   span.hasName("BatchJob itemsAndTaskletJob.itemStep.Chunk")
                       .hasKind(SpanKind.INTERNAL)
                       .hasParent(trace.getSpan(1)));
-          for (int i = 15; i <= 19; i++) {
+          for (int i = 15; i <= 23; i += 2) {
             assertions.add(
                 span ->
                     span.hasName("BatchJob itemsAndTaskletJob.itemStep.ItemRead")
                         .hasKind(SpanKind.INTERNAL)
-                        .hasParent(trace.getSpan(3)));
-          }
-          for (int i = 20; i <= 24; i++) {
+                        .hasParent(trace.getSpan(14)));
             assertions.add(
                 span ->
                     span.hasName("BatchJob itemsAndTaskletJob.itemStep.ItemProcess")
                         .hasKind(SpanKind.INTERNAL)
-                        .hasParent(trace.getSpan(3)));
+                        .hasParent(trace.getSpan(14)));
           }
           assertions.add(
               span ->
                   span.hasName("BatchJob itemsAndTaskletJob.itemStep.ItemWrite")
                       .hasKind(SpanKind.INTERNAL)
-                      .hasParent(trace.getSpan(3)));
+                      .hasParent(trace.getSpan(14)));
 
           // chunk 3, items 11-13
           assertions.add(
@@ -99,14 +96,12 @@ class JsrConfigItemLevelSpanTest {
                   span.hasName("BatchJob itemsAndTaskletJob.itemStep.Chunk")
                       .hasKind(SpanKind.INTERNAL)
                       .hasParent(trace.getSpan(1)));
-          for (int i = 27; i <= 29; i++) {
+          for (int i = 27; i <= 32; i += 2) {
             assertions.add(
                 span ->
                     span.hasName("BatchJob itemsAndTaskletJob.itemStep.ItemRead")
                         .hasKind(SpanKind.INTERNAL)
                         .hasParent(trace.getSpan(26)));
-          }
-          for (int i = 30; i <= 32; i++) {
             assertions.add(
                 span ->
                     span.hasName("BatchJob itemsAndTaskletJob.itemStep.ItemProcess")
@@ -133,6 +128,7 @@ class JsrConfigItemLevelSpanTest {
                   span.hasName("BatchJob itemsAndTaskletJob.taskletStep.Tasklet")
                       .hasKind(SpanKind.INTERNAL)
                       .hasParent(trace.getSpan(35)));
+
           trace.hasSpansSatisfyingExactly(assertions);
         });
   }
