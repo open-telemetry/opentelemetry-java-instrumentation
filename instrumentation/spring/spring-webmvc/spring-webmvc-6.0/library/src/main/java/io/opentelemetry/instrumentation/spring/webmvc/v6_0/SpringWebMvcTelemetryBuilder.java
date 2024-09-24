e397159bfbde829582183ce89c0d6426a50a053d
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-09-24 06:53:18 +0200

    http server and client builder for netty (#12083)

--- instrumentation/spring/spring-webmvc/spring-webmvc-6.0/library/src/main/java/io/opentelemetry/instrumentation/spring/webmvc/v6_0/SpringWebMvcTelemetryBuilder.java
+++ instrumentation/spring/spring-webmvc/spring-webmvc-6.0/library/src/main/java/io/opentelemetry/instrumentation/spring/webmvc/v6_0/SpringWebMvcTelemetryBuilder.java
@@ -32,8 +32,10 @@ public final class SpringWebMvcTelemetryBuilder {
   SpringWebMvcTelemetryBuilder(OpenTelemetry openTelemetry) {
     builder =
         new DefaultHttpServerInstrumenterBuilder<>(
-                INSTRUMENTATION_NAME, openTelemetry, SpringWebMvcHttpAttributesGetter.INSTANCE)
-            .setHeaderGetter(JakartaHttpServletRequestGetter.INSTANCE);
+            INSTRUMENTATION_NAME,
+            openTelemetry,
+            SpringWebMvcHttpAttributesGetter.INSTANCE,
+            JakartaHttpServletRequestGetter.INSTANCE);
   }
 
   /**
