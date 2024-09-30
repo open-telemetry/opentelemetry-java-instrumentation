ce610b7ad67ec97c58cd2646a8266b25928fe9fa
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-09-30 11:04:27 +0200

    convert servlet 5 tests to java

---
+++ instrumentation/servlet/servlet-5.0/jetty12-testing/src/test/java/io/opentelemetry/javaagent/instrumentation/servlet/v5_0/jetty12/JettyServlet5AsyncTest.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.servlet.v5_0.jetty12;
+
+import io.opentelemetry.javaagent.instrumentation.servlet.v5_0.tomcat.TestServlet5;
+import jakarta.servlet.Servlet;
+
+public class JettyServlet5AsyncTest extends JettyServlet5Test {
+
+  @Override
+  public Class<? extends Servlet> servlet() {
+    return TestServlet5.Async.class;
+  }
+
+  @Override
+  public boolean errorEndpointUsesSendError() {
+    return false;
+  }
+
+  @Override
+  public boolean isAsyncTest() {
+    return true;
+  }
+}
