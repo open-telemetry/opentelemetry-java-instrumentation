b5b5e09c9a880b979bfc0cafa6104533fffbbc94
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-09-30 10:13:24 +0200

    convert servlet 5 tests to java

---
+++ instrumentation/servlet/servlet-5.0/testing/src/main/java/io/opentelemetry/javaagent/instrumentation/servlet/v5_0/mapping/TomcatServlet5MappingTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.servlet.v5_0.mapping;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.UUID;
+import jakarta.servlet.Servlet;
+
+import io.opentelemetry.javaagent.instrumentation.servlet.v3_0.mapping.AbstractServlet3MappingTest;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+class TomcatServlet5MappingTest extends AbstractServlet3MappingTest<Tomcat, Context> {
+  @Override
+  protected Tomcat setupServer() throws Exception {
+    Tomcat tomcatServer = new Tomcat();
+
+    File baseDir = Files.createTempDirectory("tomcat").toFile();
+    baseDir.deleteOnExit();
+    tomcatServer.setBaseDir(baseDir.getAbsolutePath());
+
+    tomcatServer.setPort(port);
+    tomcatServer.getConnector().setEnableLookups(true); // get localhost instead of 127.0.0.1
+
+    File applicationDir = new File(baseDir, "/webapps/ROOT");
+    if (!applicationDir.exists()) {
+      applicationDir.mkdirs();
+      applicationDir.deleteOnExit();
+    }
+
+    Context servletContext =
+        tomcatServer.addWebapp(getContextPath(), applicationDir.getAbsolutePath());
+    // Speed up startup by disabling jar scanning:
+    servletContext.getJarScanner().setJarScanFilter((jarScanType, jarName) -> false);
+
+    setupServlets(servletContext);
+
+    tomcatServer.start();
+    return tomcatServer;
+  }
+
+  @Override
+  public void stopServer(Tomcat server) throws LifecycleException {
+    server.stop();
+    server.destroy();
+  }
+
+  @Override
+  public void addServlet(Context context, String path, Class<? extends Servlet> servlet)
+      throws Exception {
+    String name = UUID.randomUUID().toString();
+    Tomcat.addServlet(context, name, servlet.getConstructor().newInstance());
+    context.addServletMappingDecoded(path, name);
+  }
+
+  @Override
+  public String getContextPath() {
+    return "/tomcat-context";
+  }
+}
