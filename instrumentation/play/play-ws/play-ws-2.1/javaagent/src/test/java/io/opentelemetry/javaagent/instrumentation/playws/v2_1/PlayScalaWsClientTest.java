1977a57ec0d800cfd30b5ffae5cf9494801d9785
heyams <heya@microsoft.com>
2024-10-02 15:00:12 -0700

    Fix sytle one top level class

---
+++ instrumentation/play/play-ws/play-ws-2.1/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/playws/v2_1/PlayScalaWsClientTest.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.playws.v2_1;
+
+import io.opentelemetry.javaagent.instrumentation.playws.PlayScalaWsClientBaseTest;
+
+class PlayScalaWsClientTest extends PlayScalaWsClientBaseTest {}
