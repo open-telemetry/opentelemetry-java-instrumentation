dd5ea77bb9b380642a365a6227c767d687af2267
PJ Fanning <pjfanning@users.noreply.github.com>
2024-10-08 15:02:59 +0100

    Update PekkoHttpTestWebServerWithActor.scala

--- instrumentation/pekko/pekko-http-1.0/javaagent/src/test/scala/io/opentelemetry/javaagent/instrumentation/pekkohttp/v1_0/PekkoHttpTestWebServerWithActor.scala
+++ instrumentation/pekko/pekko-http-1.0/javaagent/src/test/scala/io/opentelemetry/javaagent/instrumentation/pekkohttp/v1_0/PekkoHttpTestWebServerWithActor.scala
@@ -16,19 +16,28 @@ import org.apache.pekko.util.Timeout
 import scala.concurrent.{Await, ExecutionContext}
 import scala.concurrent.duration.DurationInt
 
-class PekkoHttpTestWebServerWithActor(port: Int) {
-  implicit val system: ActorSystem = ActorSystem("http-server-with-actor")
-  // needed for the future flatMap/onComplete in the end
-  implicit val executionContext: ExecutionContext = system.dispatcher
-
-  private case object TestMessage
-  private class SpanTestActor extends Actor {
+object PekkoHttpTestWebServerWithActor {
+  case object TestMessage
+  class SpanTestActor extends Actor {
     def receive = { case TestMessage =>
       sender() ! spanSummary(Span.current())
     }
   }
 
-  val spanTestActor = system.actorOf(Props(new SpanTestActor))
+  def spanSummary(span: Span): String = {
+    val spanId = span.getSpanContext().getSpanId()
+    val traceId = span.getSpanContext().getTraceId()
+    s"Span(traceId=$traceId, spanId=$spanId)"
+  }
+}
+
+class PekkoHttpTestWebServerWithActor(port: Int) {
+  import PekkoHttpTestWebServerWithActor._
+  implicit val system: ActorSystem = ActorSystem("http-server-with-actor")
+  // needed for the future flatMap/onComplete in the end
+  implicit val executionContext: ExecutionContext = system.dispatcher
+
+  val spanTestActor = system.actorOf(Props[SpanTestActor]())
 
   val route = get {
     path("test") {
@@ -51,11 +60,5 @@ class PekkoHttpTestWebServerWithActor(port: Int) {
       system.terminate()
       binding = null
     }
-  }
-
-  def spanSummary(span: Span): String = {
-    val spanId = span.getSpanContext().getSpanId()
-    val traceId = span.getSpanContext().getTraceId()
-    s"Span(traceId=$traceId, spanId=$spanId)"
   }
 }
