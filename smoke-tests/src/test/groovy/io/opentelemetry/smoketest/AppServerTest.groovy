e5a40f63089bb728d7b07b26609e43af85dd83d4
Trask Stalnaker <trask.stalnaker@gmail.com>
2024-10-16 11:18:12 -0700

    Add Java 23 to smoke test matrix

--- smoke-tests/src/test/groovy/io/opentelemetry/smoketest/AppServerTest.groovy
+++ smoke-tests/src/test/groovy/io/opentelemetry/smoketest/AppServerTest.groovy
@@ -59,7 +59,7 @@ abstract class AppServerTest extends SmokeTest {
   @Override
   protected String getTargetImage(String jdk, String serverVersion, boolean windows) {
     String platformSuffix = windows ? "-windows" : ""
-    String extraTag = "20240216.7928274208"
+    String extraTag = "20241014.11321808438"
     String fullSuffix = "${serverVersion}-jdk$jdk$platformSuffix-$extraTag"
     return getTargetImagePrefix() + ":" + fullSuffix
   }
