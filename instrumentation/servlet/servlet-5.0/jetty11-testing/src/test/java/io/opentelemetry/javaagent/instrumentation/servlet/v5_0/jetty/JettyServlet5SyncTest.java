c165ca4dbd2103f3396437106624981c8b9a385e
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-10-19 03:44:03 +0200

    conver servlet 5 tests from groovy to java (#12364)

---
+++ instrumentation/servlet/servlet-5.0/jetty11-testing/src/test/java/io/opentelemetry/javaagent/instrumentation/servlet/v5_0/jetty/JettyServlet5SyncTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.servlet.v5_0.jetty;
+
+import io.opentelemetry.javaagent.instrumentation.servlet.v5_0.TestServlet5;
+import jakarta.servlet.Servlet;
+
+class JettyServlet5SyncTest extends JettyServlet5Test {
+  @Override
+  public Class<? extends Servlet> servlet() {
+    return TestServlet5.Sync.class;
+  }
+}
