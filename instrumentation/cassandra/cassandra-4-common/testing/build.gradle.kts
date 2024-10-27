038fbeca3ab33789d43e8ede2b37a0e08c4138a2
Trask Stalnaker <trask.stalnaker@gmail.com>
2024-10-27 11:13:54 -0700

    Remove jvm arg from module with no tests

--- instrumentation/cassandra/cassandra-4-common/testing/build.gradle.kts
+++ instrumentation/cassandra/cassandra-4-common/testing/build.gradle.kts
@@ -8,13 +8,3 @@ dependencies {
   implementation("org.testcontainers:testcontainers")
   implementation("com.datastax.oss:java-driver-core:4.0.0")
 }
-
-tasks {
-  val testStableSemconv by registering(Test::class) {
-    jvmArgs("-Dotel.semconv-stability.opt-in=database")
-  }
-
-  check {
-    dependsOn(testStableSemconv)
-  }
-}
