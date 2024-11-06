ea9c1daf6c42876b70158122644fa5caa9c26c8c
John Engelman <john.r.engelman@gmail.com>
2024-11-06 10:51:43 -0600

    (chore) update from review

--- instrumentation/ratpack/ratpack-1.7/javaagent/build.gradle.kts
+++ instrumentation/ratpack/ratpack-1.7/javaagent/build.gradle.kts
@@ -14,31 +14,14 @@ dependencies {
   library("io.ratpack:ratpack-core:1.7.0")
 
   implementation(project(":instrumentation:netty:netty-4.1:library"))
-  implementation(project(":instrumentation:ratpack:ratpack-1.4:javaagent"))
   implementation(project(":instrumentation:ratpack:ratpack-1.7:library"))
 
-  testImplementation(project(":instrumentation:ratpack:ratpack-1.4:testing"))
-
   testLibrary("io.ratpack:ratpack-test:1.7.0")
+  testImplementation(project(":instrumentation:ratpack:ratpack-1.4:testing"))
+  testInstrumentation(project(":instrumentation:ratpack:ratpack-1.4:javaagent"))
 
   if (JavaVersion.current().isCompatibleWith(JavaVersion.VERSION_11)) {
     testImplementation("com.sun.activation:jakarta.activation:1.2.2")
-  }
-}
-
-// to allow all tests to pass we need to choose a specific netty version
-if (!(findProperty("testLatestDeps") as Boolean)) {
-  configurations.configureEach {
-    if (!name.contains("muzzle")) {
-      resolutionStrategy {
-        eachDependency {
-          // specifying a fixed version for all libraries with io.netty group
-          if (requested.group == "io.netty" && requested.name != "netty-tcnative") {
-            useVersion("4.1.37.Final")
-          }
-        }
-      }
-    }
   }
 }
 
