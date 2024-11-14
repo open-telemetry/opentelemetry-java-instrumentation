9e9e69cc7a970879d4459b3cd30f3b68fd80ea78
yiyuanh <yiyuanh@amazon.com>
2024-11-13 22:11:26 -0800

    apply lint and add comments

--- instrumentation/aws-sdk/aws-sdk-2.2/library/src/main/java/io/opentelemetry/instrumentation/awssdk/v2_2/Serializer.java
+++ instrumentation/aws-sdk/aws-sdk-2.2/library/src/main/java/io/opentelemetry/instrumentation/awssdk/v2_2/Serializer.java
@@ -150,36 +150,77 @@ class Serializer {
         .orElse(null);
   }
 
+  // Model -> Path Mapping:
+  // Amazon Titan -> "/textGenerationConfig/maxTokenCount"
+  // Anthropic Claude -> "/max_tokens"
+  // Cohere Command -> "/max_tokens"
+  // Cohere Command R -> "/max_tokens"
+  // AI21 Jamba -> "/max_tokens"
+  // Meta Llama -> "/max_gen_len"
+  // Mistral AI -> "/max_tokens"
   @Nullable
   private static String getMaxTokens(JsonNode jsonBody) {
     return findFirstMatchingPath(
         jsonBody, "/textGenerationConfig/maxTokenCount", "/max_tokens", "/max_gen_len");
   }
 
+  // Model -> Path Mapping:
+  // Amazon Titan -> "/textGenerationConfig/temperature"
+  // Anthropic Claude -> "/temperature"
+  // Cohere Command -> "/temperature"
+  // Cohere Command R -> "/temperature"
+  // AI21 Jamba -> "/temperature"
+  // Meta Llama -> "/temperature"
+  // Mistral AI -> "/temperature"
   @Nullable
   private static String getTemperature(JsonNode jsonBody) {
     return findFirstMatchingPath(jsonBody, "/textGenerationConfig/temperature", "/temperature");
   }
 
+  // Model -> Path Mapping:
+  // Amazon Titan -> "/textGenerationConfig/topP"
+  // Anthropic Claude -> "/top_p"
+  // Cohere Command -> "/p"
+  // Cohere Command R -> "/p"
+  // AI21 Jamba -> "/top_p"
+  // Meta Llama -> "/top_p"
+  // Mistral AI -> "/top_p"
   @Nullable
   private static String getTopP(JsonNode jsonBody) {
     return findFirstMatchingPath(jsonBody, "/textGenerationConfig/topP", "/top_p", "/p");
   }
 
+  // Model -> Path Mapping:
+  // Amazon Titan -> "/results/0/completionReason"
+  // Anthropic Claude -> "/stop_reason"
+  // Cohere Command -> "/generations/0/finish_reason"
+  // Cohere Command R -> "/finish_reason"
+  // AI21 Jamba -> "/choices/0/finish_reason"
+  // Meta Llama -> "/stop_reason"
+  // Mistral AI -> "/outputs/0/stop_reason"
   @Nullable
   private static String getFinishReasons(JsonNode jsonBody) {
-    String finishReason = findFirstMatchingPath(
-        jsonBody,
-        "/results/0/completionReason",
-        "/stop_reason",
-        "/generations/0/finish_reason",
-        "/choices/0/finish_reason",
-        "/outputs/0/stop_reason",
-        "/finish_reason");
+    String finishReason =
+        findFirstMatchingPath(
+            jsonBody,
+            "/results/0/completionReason",
+            "/stop_reason",
+            "/generations/0/finish_reason",
+            "/choices/0/finish_reason",
+            "/outputs/0/stop_reason",
+            "/finish_reason");
 
     return finishReason != null ? "[" + finishReason + "]" : null;
   }
 
+  // Model -> Path Mapping:
+  // Amazon Titan -> "/inputTextTokenCount"
+  // Anthropic Claude -> "/usage/input_tokens"
+  // Cohere Command -> "/prompt"
+  // Cohere Command R -> "/message"
+  // AI21 Jamba -> "/usage/prompt_tokens"
+  // Meta Llama -> "/prompt_token_count"
+  // Mistral AI -> "/prompt"
   @Nullable
   private static String getInputTokens(JsonNode jsonBody) {
     // Try direct tokens counts first
@@ -199,6 +240,14 @@ class Serializer {
     return approximateTokenCount(jsonBody, "/prompt", "/message");
   }
 
+  // Model -> Path Mapping:
+  // Amazon Titan -> "/results/0/tokenCount"
+  // Anthropic Claude -> "/usage/output_tokens"
+  // Cohere Command -> "/generations/0/text"
+  // Cohere Command R -> "/text"
+  // AI21 Jamba -> "/usage/completion_tokens"
+  // Meta Llama -> "/generation_token_count"
+  // Mistral AI -> "/outputs/0/text"
   @Nullable
   private static String getOutputTokens(JsonNode jsonBody) {
     // Try direct token counts first
