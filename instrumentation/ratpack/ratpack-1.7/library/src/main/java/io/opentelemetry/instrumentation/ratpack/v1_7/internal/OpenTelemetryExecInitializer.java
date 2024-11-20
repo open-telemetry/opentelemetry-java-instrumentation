8a3f1eb03d6a6ae3f7ea6b2e3121a3789f8aa750
John Engelman <john.r.engelman@gmail.com>
2024-11-20 11:31:30 -0600

    Add agent instrumentation for Ratpack 1.7+ (#12572)

--- instrumentation/ratpack/ratpack-1.7/library/src/main/java/io/opentelemetry/instrumentation/ratpack/v1_7/internal/OpenTelemetryExecInitializer.java
+++ instrumentation/ratpack/ratpack-1.7/library/src/main/java/io/opentelemetry/instrumentation/ratpack/v1_7/internal/OpenTelemetryExecInitializer.java
@@ -3,13 +3,16 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.opentelemetry.instrumentation.ratpack.v1_7;
+package io.opentelemetry.instrumentation.ratpack.v1_7.internal;
 
-import io.opentelemetry.instrumentation.ratpack.v1_7.internal.ContextHolder;
 import ratpack.exec.ExecInitializer;
 import ratpack.exec.Execution;
 
-final class OpenTelemetryExecInitializer implements ExecInitializer {
+/**
+ * This class is internal and is hence not for public use. Its APIs are unstable and can change at
+ * any time.
+ */
+public final class OpenTelemetryExecInitializer implements ExecInitializer {
   public static final ExecInitializer INSTANCE = new OpenTelemetryExecInitializer();
 
   @Override
--- instrumentation/ratpack/ratpack-1.7/library/src/main/java/io/opentelemetry/instrumentation/ratpack/v1_7/internal/RatpackGetter.java
+++ instrumentation/ratpack/ratpack-1.7/library/src/main/java/io/opentelemetry/instrumentation/ratpack/v1_7/internal/RatpackGetter.java
@@ -3,12 +3,16 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.opentelemetry.instrumentation.ratpack.v1_7;
+package io.opentelemetry.instrumentation.ratpack.v1_7.internal;
 
 import io.opentelemetry.context.propagation.TextMapGetter;
 import javax.annotation.Nullable;
 import ratpack.http.Request;
 
+/**
+ * This class is internal and is hence not for public use. Its APIs are unstable and can change at
+ * any time.
+ */
 enum RatpackGetter implements TextMapGetter<Request> {
   INSTANCE;
 
