6fa9553674a2ec572b367cfea17b6706d4ab4952
Alex Kats <56042997+akats7@users.noreply.github.com>
2024-11-29 22:18:27 -0500

    Added dynamodb instrumenter for aws v1_11 sdk (#12756)
    Co-authored-by: Lauri Tulmin <ltulmin@splunk.com>

--- instrumentation/aws-sdk/aws-sdk-1.11/testing/src/main/java/io/opentelemetry/instrumentation/awssdk/v1_11/AbstractDynamoDbClientTest.java
+++ instrumentation/aws-sdk/aws-sdk-1.11/testing/src/main/java/io/opentelemetry/instrumentation/awssdk/v1_11/AbstractDynamoDbClientTest.java
@@ -5,13 +5,22 @@
 
 package io.opentelemetry.instrumentation.awssdk.v1_11;
 
+import static io.opentelemetry.api.common.AttributeKey.stringKey;
+import static io.opentelemetry.sdk.testing.assertj.OpenTelemetryAssertions.equalTo;
+import static io.opentelemetry.semconv.incubating.AwsIncubatingAttributes.AWS_DYNAMODB_TABLE_NAMES;
+import static io.opentelemetry.semconv.incubating.DbIncubatingAttributes.DB_SYSTEM;
+import static io.opentelemetry.semconv.incubating.DbIncubatingAttributes.DbSystemIncubatingValues.DYNAMODB;
+import static java.util.Collections.singletonList;
+
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
-import com.google.common.collect.ImmutableMap;
+import io.opentelemetry.sdk.testing.assertj.AttributeAssertion;
 import io.opentelemetry.testing.internal.armeria.common.HttpResponse;
 import io.opentelemetry.testing.internal.armeria.common.HttpStatus;
 import io.opentelemetry.testing.internal.armeria.common.MediaType;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public abstract class AbstractDynamoDbClientTest extends AbstractBaseAwsClientTest {
@@ -34,13 +43,14 @@ public abstract class AbstractDynamoDbClientTest extends AbstractBaseAwsClientTe
 
     server.enqueue(HttpResponse.of(HttpStatus.OK, MediaType.PLAIN_TEXT_UTF_8, ""));
 
+    List<AttributeAssertion> additionalAttributes =
+        Arrays.asList(
+            equalTo(stringKey("aws.table.name"), "sometable"),
+            equalTo(DB_SYSTEM, DYNAMODB),
+            equalTo(AWS_DYNAMODB_TABLE_NAMES, singletonList("sometable")));
+
     Object response = client.createTable(new CreateTableRequest("sometable", null));
     assertRequestWithMockedResponse(
-        response,
-        client,
-        "DynamoDBv2",
-        "CreateTable",
-        "POST",
-        ImmutableMap.of("aws.table.name", "sometable"));
+        response, client, "DynamoDBv2", "CreateTable", "POST", additionalAttributes);
   }
 }
--- instrumentation/aws-sdk/aws-sdk-1.11/testing/src/main/java/io/opentelemetry/instrumentation/awssdk/v1_11/AbstractS3ClientTest.java
+++ instrumentation/aws-sdk/aws-sdk-1.11/testing/src/main/java/io/opentelemetry/instrumentation/awssdk/v1_11/AbstractS3ClientTest.java
@@ -17,6 +17,7 @@ import static io.opentelemetry.semconv.UrlAttributes.URL_FULL;
 import static io.opentelemetry.semconv.incubating.RpcIncubatingAttributes.RPC_METHOD;
 import static io.opentelemetry.semconv.incubating.RpcIncubatingAttributes.RPC_SERVICE;
 import static io.opentelemetry.semconv.incubating.RpcIncubatingAttributes.RPC_SYSTEM;
+import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
 
@@ -27,14 +28,14 @@ import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.retry.PredefinedRetryPolicies;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.google.common.collect.ImmutableMap;
 import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.sdk.testing.assertj.AttributeAssertion;
 import io.opentelemetry.sdk.trace.data.StatusData;
 import io.opentelemetry.testing.internal.armeria.common.HttpResponse;
 import io.opentelemetry.testing.internal.armeria.common.HttpStatus;
 import io.opentelemetry.testing.internal.armeria.common.MediaType;
 import java.time.Duration;
-import java.util.Map;
+import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
@@ -60,7 +61,7 @@ public abstract class AbstractS3ClientTest extends AbstractBaseAwsClientTest {
       String operation,
       String method,
       Function<AmazonS3, Object> call,
-      Map<String, String> additionalAttributes)
+      List<AttributeAssertion> additionalAttributes)
       throws Exception {
 
     AmazonS3 client =
@@ -82,12 +83,12 @@ public abstract class AbstractS3ClientTest extends AbstractBaseAwsClientTest {
             "CreateBucket",
             "PUT",
             (Function<AmazonS3, Object>) c -> c.createBucket("testbucket"),
-            ImmutableMap.of("aws.bucket.name", "testbucket")),
+            singletonList(equalTo(stringKey("aws.bucket.name"), "testbucket"))),
         Arguments.of(
             "GetObject",
             "GET",
             (Function<AmazonS3, Object>) c -> c.getObject("someBucket", "someKey"),
-            ImmutableMap.of("aws.bucket.name", "someBucket")));
+            singletonList(equalTo(stringKey("aws.bucket.name"), "someBucket"))));
   }
 
   @Test
