7710237c8f68744ec3058b7490b9b9abde7ba4ea
Trask Stalnaker <trask.stalnaker@gmail.com>
2024-12-04 11:39:41 -0800

    Update develocity config

--- conventions/src/main/kotlin/otel.java-conventions.gradle.kts
+++ conventions/src/main/kotlin/otel.java-conventions.gradle.kts
@@ -366,13 +366,6 @@ tasks.withType<Test>().configureEach {
   // This value is quite big because with lower values (3 mins) we were experiencing large number of false positives
   timeout.set(Duration.ofMinutes(15))
 
-  develocity.testRetry {
-    // You can see tests that were retried by this mechanism in the collected test reports and build scans.
-    if (System.getenv().containsKey("CI") || rootProject.hasProperty("retryTests")) {
-      maxRetries.set(5)
-    }
-  }
-
   reports {
     junitXml.isOutputPerTestCase = true
   }
