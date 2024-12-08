ad478d5ba342bf81a1378efcf1d63636b4a45406
Trask Stalnaker <trask.stalnaker@gmail.com>
2024-12-08 08:17:55 -0800

    Split ArmeriaTelemetry into client and server

---
+++ instrumentation/armeria/armeria-1.3/library/src/main/java/io/opentelemetry/instrumentation/armeria/v1_3/internal/Experimental.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.instrumentation.armeria.v1_3.internal;
+
+import static java.util.logging.Level.FINE;
+
+import io.opentelemetry.instrumentation.armeria.v1_3.ArmeriaClientTelemetryBuilder;
+import io.opentelemetry.instrumentation.armeria.v1_3.ArmeriaServerTelemetryBuilder;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.logging.Logger;
+import javax.annotation.Nullable;
+
+/**
+ * This class is internal and is hence not for public use. Its APIs are unstable and can change at
+ * any time.
+ */
+// TODO (trask) update the above javadoc similar to
+//  https://github.com/open-telemetry/opentelemetry-java/pull/6886
+public class Experimental {
+
+  private static final Logger logger = Logger.getLogger(Experimental.class.getName());
+
+  @Nullable
+  private static final Method emitExperimentalClientTelemetryMethod =
+      getEmitExperimentalClientTelemetryMethod();
+
+  @Nullable private static final Method clientPeerServiceMethod = getClientPeerServiceMethod();
+
+  @Nullable
+  private static final Method emitExperimentalHttpServerMetricsMethod =
+      getEmitExperimentalHttpServerMetricsMethod();
+
+  public void setEmitExperimentalTelemetry(
+      ArmeriaClientTelemetryBuilder builder, boolean emitExperimentalTelemetry) {
+
+    if (emitExperimentalClientTelemetryMethod != null) {
+      try {
+        emitExperimentalClientTelemetryMethod.invoke(builder, emitExperimentalTelemetry);
+      } catch (IllegalAccessException | InvocationTargetException e) {
+        logger.log(FINE, e.getMessage(), e);
+      }
+    }
+  }
+
+  public void setEmitExperimentalTelemetry(
+      ArmeriaServerTelemetryBuilder builder, boolean emitExperimentalTelemetry) {
+
+    if (emitExperimentalHttpServerMetricsMethod != null) {
+      try {
+        emitExperimentalHttpServerMetricsMethod.invoke(builder, emitExperimentalTelemetry);
+      } catch (IllegalAccessException | InvocationTargetException e) {
+        logger.log(FINE, e.getMessage(), e);
+      }
+    }
+  }
+
+  public void setClientPeerService(ArmeriaClientTelemetryBuilder builder, String peerService) {
+
+    if (clientPeerServiceMethod != null) {
+      try {
+        clientPeerServiceMethod.invoke(builder, peerService);
+      } catch (IllegalAccessException | InvocationTargetException e) {
+        logger.log(FINE, e.getMessage(), e);
+      }
+    }
+  }
+
+  @Nullable
+  private static Method getEmitExperimentalClientTelemetryMethod() {
+    try {
+      Method method =
+          ArmeriaClientTelemetryBuilder.class.getDeclaredMethod(
+              "setEmitExperimentalHttpClientMetrics", boolean.class);
+      method.setAccessible(true);
+      return method;
+    } catch (NoSuchMethodException e) {
+      logger.log(FINE, e.getMessage(), e);
+      return null;
+    }
+  }
+
+  @Nullable
+  private static Method getClientPeerServiceMethod() {
+    try {
+      Method method =
+          ArmeriaClientTelemetryBuilder.class.getDeclaredMethod("setPeerService", String.class);
+      method.setAccessible(true);
+      return method;
+    } catch (NoSuchMethodException e) {
+      logger.log(FINE, e.getMessage(), e);
+      return null;
+    }
+  }
+
+  @Nullable
+  private static Method getEmitExperimentalHttpServerMetricsMethod() {
+    try {
+      return ArmeriaServerTelemetryBuilder.class.getMethod(
+          "setEmitExperimentalHttpServerMetrics", boolean.class);
+    } catch (NoSuchMethodException e) {
+      logger.log(FINE, e.getMessage(), e);
+      return null;
+    }
+  }
+}
