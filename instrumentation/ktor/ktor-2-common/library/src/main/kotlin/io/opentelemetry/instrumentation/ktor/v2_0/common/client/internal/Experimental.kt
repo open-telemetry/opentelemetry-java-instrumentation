3f54de5f104bedfe0db41499c04a1e6c828faa59
Trask Stalnaker <trask.stalnaker@gmail.com>
2024-12-10 19:53:26 -0800

    spotless

--- instrumentation/ktor/ktor-2-common/library/src/main/kotlin/io/opentelemetry/instrumentation/ktor/v2_0/common/client/internal/Experimental.kt
+++ instrumentation/ktor/ktor-2-common/library/src/main/kotlin/io/opentelemetry/instrumentation/ktor/v2_0/common/client/internal/Experimental.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package io.opentelemetry.instrumentation.ktor.v2_0.common.client.internal
 
 import io.opentelemetry.instrumentation.ktor.v2_0.common.client.AbstractKtorClientTelemetryBuilder
@@ -9,17 +14,13 @@ class Experimental private constructor() {
     @Volatile
     private var setEmitExperimentalTelemetry: BiConsumer<AbstractKtorClientTelemetryBuilder, Boolean>? = null
 
-    fun emitExperimentalTelemetry(
-      builder: AbstractKtorClientTelemetryBuilder
-    ) {
+    fun emitExperimentalTelemetry(builder: AbstractKtorClientTelemetryBuilder) {
       if (setEmitExperimentalTelemetry != null) {
         setEmitExperimentalTelemetry!!.accept(builder, true)
       }
     }
 
-    fun setSetEmitExperimentalTelemetry(
-      setEmitExperimentalTelemetry: BiConsumer<AbstractKtorClientTelemetryBuilder, Boolean>?
-    ) {
+    fun setSetEmitExperimentalTelemetry(setEmitExperimentalTelemetry: BiConsumer<AbstractKtorClientTelemetryBuilder, Boolean>?) {
       Companion.setEmitExperimentalTelemetry = setEmitExperimentalTelemetry
     }
   }
