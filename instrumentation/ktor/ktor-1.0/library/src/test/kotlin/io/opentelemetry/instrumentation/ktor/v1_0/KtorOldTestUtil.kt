6260a862251e508f7d6a4c0551fc0d26d8fc6bea
Trask Stalnaker <trask.stalnaker@gmail.com>
2024-12-12 14:32:27 -0800

    test deprecated code

--- instrumentation/ktor/ktor-1.0/library/src/test/kotlin/io/opentelemetry/instrumentation/ktor/v1_0/KtorOldTestUtil.kt
+++ instrumentation/ktor/ktor-1.0/library/src/test/kotlin/io/opentelemetry/instrumentation/ktor/v1_0/KtorOldTestUtil.kt
@@ -3,19 +3,19 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.opentelemetry.instrumentation.ktor.v2_0
+package io.opentelemetry.instrumentation.ktor.v1_0
 
-import io.ktor.server.application.*
+import io.ktor.application.*
 import io.opentelemetry.api.OpenTelemetry
 import io.opentelemetry.instrumentation.testing.junit.http.AbstractHttpServerTest
 
-class KtorTestUtil {
+class KtorOldTestUtil {
   companion object {
     fun installOpenTelemetry(application: Application, openTelemetry: OpenTelemetry) {
-      application.install(KtorServerTelemetry) {
+      application.install(KtorServerTracing) {
         setOpenTelemetry(openTelemetry)
-        capturedRequestHeaders(AbstractHttpServerTest.TEST_REQUEST_HEADER)
-        capturedResponseHeaders(AbstractHttpServerTest.TEST_RESPONSE_HEADER)
+        setCapturedRequestHeaders(listOf(AbstractHttpServerTest.TEST_REQUEST_HEADER))
+        setCapturedResponseHeaders(listOf(AbstractHttpServerTest.TEST_RESPONSE_HEADER))
       }
     }
   }
--- instrumentation/ktor/ktor-2.0/library/src/test/kotlin/io/opentelemetry/instrumentation/ktor/v2_0/KtorOldTestUtil.kt
+++ instrumentation/ktor/ktor-2.0/library/src/test/kotlin/io/opentelemetry/instrumentation/ktor/v2_0/KtorOldTestUtil.kt
@@ -7,12 +7,13 @@ package io.opentelemetry.instrumentation.ktor.v2_0
 
 import io.ktor.server.application.*
 import io.opentelemetry.api.OpenTelemetry
+import io.opentelemetry.instrumentation.ktor.v2_0.server.KtorServerTracing
 import io.opentelemetry.instrumentation.testing.junit.http.AbstractHttpServerTest
 
-class KtorTestUtil {
+class KtorOldTestUtil {
   companion object {
     fun installOpenTelemetry(application: Application, openTelemetry: OpenTelemetry) {
-      application.install(KtorServerTelemetry) {
+      application.install(KtorServerTracing) {
         setOpenTelemetry(openTelemetry)
         capturedRequestHeaders(AbstractHttpServerTest.TEST_REQUEST_HEADER)
         capturedResponseHeaders(AbstractHttpServerTest.TEST_RESPONSE_HEADER)
