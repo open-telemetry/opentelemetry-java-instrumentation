9eca729f404633ed3a47146b256b5a7c83658790
Trask Stalnaker <trask.stalnaker@gmail.com>
2024-12-16 19:16:45 -0800

    part1

--- instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/semconv/http/HttpClientAttributesExtractorBuilder.java
+++ instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/semconv/http/HttpClientAttributesExtractorBuilder.java
@@ -137,9 +137,32 @@ public final class HttpClientAttributesExtractorBuilder<REQUEST, RESPONSE> {
    */
   @CanIgnoreReturnValue
   public HttpClientAttributesExtractorBuilder<REQUEST, RESPONSE> setKnownMethods(
-      Set<String> knownMethods) {
+      Collection<String> knownMethods) {
     this.knownMethods = new HashSet<>(knownMethods);
     return this;
+  }
+
+  /**
+   * Configures the extractor to recognize an alternative set of HTTP request methods.
+   *
+   * <p>By default, this extractor defines "known" methods as the ones listed in <a
+   * href="https://www.rfc-editor.org/rfc/rfc9110.html#name-methods">RFC9110</a> and the PATCH
+   * method defined in <a href="https://www.rfc-editor.org/rfc/rfc5789.html">RFC5789</a>. If an
+   * unknown method is encountered, the extractor will use the value {@value HttpConstants#_OTHER}
+   * instead of it and put the original value in an extra {@code http.request.method_original}
+   * attribute.
+   *
+   * <p>Note: calling this method <b>overrides</b> the default known method sets completely; it does
+   * not supplement it.
+   *
+   * @param knownMethods A set of recognized HTTP request methods.
+   */
+  // don't deprecate this since users will get deprecation warning without a clean way to suppress
+  // it if they're using Set
+  @CanIgnoreReturnValue
+  public HttpClientAttributesExtractorBuilder<REQUEST, RESPONSE> setKnownMethods(
+      Set<String> knownMethods) {
+    return setKnownMethods((Collection<String>) knownMethods);
   }
 
   // visible for tests
