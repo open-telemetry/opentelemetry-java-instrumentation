807fd56e2cc4d5b3eb1252376bbabdcbef37578f
Cuichen Li <cuichen.li94@gmail.com>
2024-12-19 19:29:29 +0800

    one more

--- instrumentation/spring/spring-webflux/spring-webflux-5.0/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/spring/webflux/v5_0/server/SpringWebfluxTest.java
+++ instrumentation/spring/spring-webflux/spring-webflux-5.0/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/spring/webflux/v5_0/server/SpringWebfluxTest.java
@@ -491,9 +491,7 @@ public class SpringWebfluxTest {
                         .hasParent(trace.getSpan(0))
                         .hasAttributesSatisfyingExactly(
                             equalTo(CODE_FUNCTION, "handle"),
-                            satisfies(
-                                CODE_NAMESPACE,
-                                val -> val.isEqualTo("server.EchoHandlerFunction"))),
+                            equalTo(CODE_NAMESPACE, "server.EchoHandlerFunction")),
                 span ->
                     span.hasName("echo").hasParent(trace.getSpan(1)).hasTotalAttributeCount(0)));
   }
