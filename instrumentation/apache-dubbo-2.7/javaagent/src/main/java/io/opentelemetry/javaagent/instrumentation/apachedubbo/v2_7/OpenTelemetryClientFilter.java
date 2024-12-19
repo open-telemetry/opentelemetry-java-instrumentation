52181184586883e9aa12088c280d5fc42d1ffb5f
Steve Rao <zihaorao@126.com>
2024-12-19 23:16:48 +0800

    Fix wrong dubbo trace caused by using rpcContext.isProviderSide()

---
+++ instrumentation/apache-dubbo-2.7/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/apachedubbo/v2_7/OpenTelemetryClientFilter.java
@@ -0,0 +1,2 @@
+package io.opentelemetry.javaagent.instrumentation.apachedubbo.v2_7;public class OpenTelemetryConsumerFilter {
+}
---
+++ instrumentation/apache-dubbo-2.7/library-autoconfigure/src/main/java/io/opentelemetry/instrumentation/apachedubbo/v2_7/OpenTelemetryServerFilter.java
@@ -0,0 +1,2 @@
+package io.opentelemetry.instrumentation.apachedubbo.v2_7;public class OpenTelemetryServerFilter {
+}
