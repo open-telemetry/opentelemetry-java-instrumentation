c376a56e6e25cb40fa1ee07c1a5708e02f0d3606
Jay DeLuca <jaydeluca4@gmail.com>
2024-12-29 06:07:27 -0500

    start converting

--- instrumentation/ratpack/ratpack-1.7/library/src/test/java/io/opentelemetry/instrumentation/ratpack/v1_7/server/RatpackServerTest.java
+++ instrumentation/ratpack/ratpack-1.7/library/src/test/java/io/opentelemetry/instrumentation/ratpack/v1_7/server/RatpackServerTest.java
@@ -2,15 +2,24 @@ package io.opentelemetry.instrumentation.ratpack.v1_7.server;
 
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.baggage.propagation.W3CBaggagePropagator;
+import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.context.propagation.ContextPropagators;
 import io.opentelemetry.instrumentation.ratpack.v1_7.RatpackServerTelemetry;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
-import io.opentelemetry.sdk.OpenTelemetrySdkBuilder;
 import io.opentelemetry.sdk.testing.exporter.InMemorySpanExporter;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.data.SpanData;
 import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
+import ratpack.registry.Registry;
+import ratpack.test.embed.EmbeddedApp;
+import spock.util.concurrent.PollingConditions;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class RatpackServerTest {
 
@@ -31,10 +40,24 @@ class RatpackServerTest {
   }
 
   @Test
-  void testAddSpanOnHandlers() {
+  void testAddSpanOnHandlers() throws Exception {
+    EmbeddedApp app = EmbeddedApp.of(
+        spec -> {
+          spec.registry(registry -> Registry.of(regSpec -> telemetry.configureRegistry(regSpec)));
+          spec.handlers(chain -> chain.get("foo", ctx -> ctx.render("hi-foo")));
+        }
+    );
 
+    app.test( httpClient -> {
+      assertThat(httpClient.get("foo").getBody().getText()).isEqualTo("hi-foo");
+      new PollingConditions().eventually(() -> {
+        Map<String, SpanData> spans = spanExporter.getFinishedSpanItems().stream()
+            .collect(Collectors.toMap(SpanData::getName, span -> span));
+        assertThat(spans).containsKey("GET /foo");
+
+
+    });
 
   }
-
 
 }
