44e34c8cbf18fbef02208e3b57377e6038baefca
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-01-13 17:04:43 +0100

    add lettuce db client metrics

--- instrumentation/lettuce/lettuce-5.1/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/lettuce/v5_1/TracingHolder.java
+++ instrumentation/lettuce/lettuce-5.1/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/lettuce/v5_1/TracingHolder.java
@@ -5,7 +5,6 @@
 
 package io.opentelemetry.javaagent.instrumentation.lettuce.v5_1;
 
-import static io.opentelemetry.instrumentation.api.internal.OperationMetricsUtil.NOOP_OPERATION_LISTENER;
 
 import io.lettuce.core.tracing.Tracing;
 import io.opentelemetry.api.GlobalOpenTelemetry;
@@ -17,9 +16,6 @@ public final class TracingHolder {
   public static final Tracing TRACING =
       LettuceTelemetry.builder(GlobalOpenTelemetry.get())
           .setStatementSanitizationEnabled(AgentCommonConfig.get().isStatementSanitizationEnabled())
-          .setMetrics(
-              meter ->
-                  NOOP_OPERATION_LISTENER) // javaagent uses bytecode instrumentation for metrics
           .build()
           .newTracing();
 
