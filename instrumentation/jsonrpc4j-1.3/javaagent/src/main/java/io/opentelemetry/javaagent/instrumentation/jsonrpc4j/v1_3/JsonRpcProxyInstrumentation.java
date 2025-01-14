974c31541e9e9fff7be3a80c24fe169575248a10
chenlujjj <953546398@qq.com>
2025-01-14 19:48:04 +0800

    support instrument proxy client

--- instrumentation/jsonrpc4j-1.3/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/jsonrpc4j/v1_3/JsonRpcProxyInstrumentation.java
+++ instrumentation/jsonrpc4j-1.3/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/jsonrpc4j/v1_3/JsonRpcProxyInstrumentation.java
@@ -6,23 +6,14 @@
 package io.opentelemetry.javaagent.instrumentation.jsonrpc4j.v1_3;
 
 import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.hasClassesNamed;
-import static io.opentelemetry.javaagent.instrumentation.jsonrpc4j.v1_3.JsonRpcSingletons.CLIENT_INSTRUMENTER;
 import static net.bytebuddy.matcher.ElementMatchers.isMethod;
 import static net.bytebuddy.matcher.ElementMatchers.isPrivate;
 import static net.bytebuddy.matcher.ElementMatchers.isStatic;
 import static net.bytebuddy.matcher.ElementMatchers.named;
 
 import com.googlecode.jsonrpc4j.IJsonRpcClient;
-import com.googlecode.jsonrpc4j.ReflectionUtil;
-import io.opentelemetry.context.Context;
-import io.opentelemetry.context.Scope;
-import io.opentelemetry.instrumentation.jsonrpc4j.v1_3.SimpleJsonRpcRequest;
-import io.opentelemetry.instrumentation.jsonrpc4j.v1_3.SimpleJsonRpcResponse;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeTransformer;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 import java.util.Map;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.description.type.TypeDescription;
@@ -47,7 +38,7 @@ public class JsonRpcProxyInstrumentation implements TypeInstrumentation {
         this.getClass().getName() + "$CreateClientProxyAdvice");
   }
 
-  @SuppressWarnings({"unused", "unchecked"})
+  @SuppressWarnings({"unused"})
   public static class CreateClientProxyAdvice {
 
     @Advice.OnMethodExit(suppress = Throwable.class)
@@ -59,47 +50,8 @@ public class JsonRpcProxyInstrumentation implements TypeInstrumentation {
         @Advice.Return(readOnly = false) Object proxy) {
 
       proxy =
-          (T)
-              Proxy.newProxyInstance(
-                  classLoader,
-                  new Class<?>[] {proxyInterface},
-                  new InvocationHandler() {
-                    @Override
-                    public Object invoke(Object proxy, Method method, Object[] args)
-                        throws Throwable {
-
-                      Object arguments = ReflectionUtil.parseArguments(method, args);
-                      String methodName = method.getName(); // todo
-
-                      // before invoke
-                      Context parentContext = Context.current();
-                      SimpleJsonRpcRequest request = new SimpleJsonRpcRequest(method, args);
-                      if (!CLIENT_INSTRUMENTER.shouldStart(parentContext, request)) {
-                        return client.invoke(
-                            methodName, arguments, method.getGenericReturnType(), extraHeaders);
-                      }
-
-                      Context context = CLIENT_INSTRUMENTER.start(parentContext, request);
-                      Scope scope = context.makeCurrent();
-                      try {
-                        Object result =
-                            client.invoke(
-                                methodName, arguments, method.getGenericReturnType(), extraHeaders);
-                        scope.close();
-                        CLIENT_INSTRUMENTER.end(
-                            context,
-                            new SimpleJsonRpcRequest(method, args),
-                            new SimpleJsonRpcResponse(result),
-                            null);
-                        return result;
-                      } catch (Throwable t) {
-                        // after invoke
-                        scope.close();
-                        CLIENT_INSTRUMENTER.end(context, request, null, t);
-                        throw t;
-                      }
-                    }
-                  });
+          JsonRpcSingletons.instrumentCreateClientProxy(
+              classLoader, proxyInterface, client, extraHeaders, proxy);
     }
   }
 }
--- instrumentation/jsonrpc4j-1.3/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/jsonrpc4j/v1_3/JsonRpcSingletons.java
+++ instrumentation/jsonrpc4j-1.3/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/jsonrpc4j/v1_3/JsonRpcSingletons.java
@@ -5,12 +5,19 @@
 
 package io.opentelemetry.javaagent.instrumentation.jsonrpc4j.v1_3;
 
+import com.googlecode.jsonrpc4j.IJsonRpcClient;
 import com.googlecode.jsonrpc4j.InvocationListener;
 import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
 import io.opentelemetry.instrumentation.api.instrumenter.Instrumenter;
 import io.opentelemetry.instrumentation.jsonrpc4j.v1_3.JsonRpcTelemetry;
 import io.opentelemetry.instrumentation.jsonrpc4j.v1_3.SimpleJsonRpcRequest;
 import io.opentelemetry.instrumentation.jsonrpc4j.v1_3.SimpleJsonRpcResponse;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Map;
 
 public final class JsonRpcSingletons {
 
@@ -26,4 +33,49 @@ public final class JsonRpcSingletons {
   }
 
   private JsonRpcSingletons() {}
+
+  @SuppressWarnings({"unchecked"})
+  public static <T> T instrumentCreateClientProxy(
+      ClassLoader classLoader,
+      Class<T> proxyInterface,
+      IJsonRpcClient client,
+      Map<String, String> extraHeaders,
+      Object proxy) {
+
+    return (T)
+        Proxy.newProxyInstance(
+            classLoader,
+            new Class<?>[] {proxyInterface},
+            new InvocationHandler() {
+              @Override
+              public Object invoke(Object proxy1, Method method, Object[] args) throws Throwable {
+                // before invoke
+                Context parentContext = Context.current();
+                SimpleJsonRpcRequest request = new SimpleJsonRpcRequest(method, args);
+                if (!CLIENT_INSTRUMENTER.shouldStart(parentContext, request)) {
+                  return method.invoke(proxy, args);
+                }
+
+                Context context = CLIENT_INSTRUMENTER.start(parentContext, request);
+                Scope scope = context.makeCurrent();
+                try {
+                  Object result = method.invoke(proxy, args);
+                  // after invoke
+                  scope.close();
+                  CLIENT_INSTRUMENTER.end(
+                      context,
+                      new SimpleJsonRpcRequest(method, args),
+                      new SimpleJsonRpcResponse(result),
+                      null);
+                  return result;
+
+                } catch (Throwable t) {
+                  // after invoke
+                  scope.close();
+                  CLIENT_INSTRUMENTER.end(context, request, null, t);
+                  throw t;
+                }
+              }
+            });
+  }
 }
