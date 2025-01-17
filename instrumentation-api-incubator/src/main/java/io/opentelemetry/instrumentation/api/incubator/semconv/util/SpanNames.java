af2fb237fe2ae2d8073c0a0af5b213b507500a64
dingjiefei <1264677051@qq.com>
2025-01-18 05:57:02 +0800

    stabilise spanNames (#12487)
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>

--- instrumentation-api-incubator/src/main/java/io/opentelemetry/instrumentation/api/incubator/semconv/util/SpanNames.java
+++ instrumentation-api-incubator/src/main/java/io/opentelemetry/instrumentation/api/incubator/semconv/util/SpanNames.java
@@ -5,22 +5,21 @@
 
 package io.opentelemetry.instrumentation.api.incubator.semconv.util;
 
-import io.opentelemetry.instrumentation.api.internal.ClassNames;
-import io.opentelemetry.instrumentation.api.internal.cache.Cache;
 import java.lang.reflect.Method;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * This class has been stabilized and moved to {@link
+ * io.opentelemetry.instrumentation.api.semconv.util.SpanNames}
+ */
+@Deprecated
 public final class SpanNames {
-
-  private static final Cache<Class<?>, Map<String, String>> spanNameCaches = Cache.weak();
 
   /**
    * This method is used to generate a span name based on a method. Anonymous classes are named
    * based on their parent.
    */
   public static String fromMethod(Method method) {
-    return fromMethod(method.getDeclaringClass(), method.getName());
+    return io.opentelemetry.instrumentation.api.semconv.util.SpanNames.fromMethod(method);
   }
 
   /**
@@ -28,18 +27,8 @@ public final class SpanNames {
    * based on their parent.
    */
   public static String fromMethod(Class<?> clazz, String methodName) {
-    // the cache (ConcurrentHashMap) is naturally bounded by the number of methods in a class
-    Map<String, String> spanNameCache =
-        spanNameCaches.computeIfAbsent(clazz, c -> new ConcurrentHashMap<>());
-
-    // not using computeIfAbsent, because it would require a capturing (allocating) lambda
-    String spanName = spanNameCache.get(methodName);
-    if (spanName != null) {
-      return spanName;
-    }
-    spanName = ClassNames.simpleName(clazz) + "." + methodName;
-    spanNameCache.put(methodName, spanName);
-    return spanName;
+    return io.opentelemetry.instrumentation.api.semconv.util.SpanNames.fromMethod(
+        clazz, methodName);
   }
 
   private SpanNames() {}
