26e79f9b27cccc643a46a91386bb4fbaaf8c27e5
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2025-01-20 17:59:38 +0100

    inject proxies for indy

--- instrumentation/internal/internal-lambda/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/internal/lambda/LambdaInstrumentationModule.java
+++ instrumentation/internal/internal-lambda/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/internal/lambda/LambdaInstrumentationModule.java
@@ -10,13 +10,17 @@ import static java.util.Collections.singletonList;
 import com.google.auto.service.AutoService;
 import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
+import io.opentelemetry.javaagent.extension.instrumentation.internal.ExperimentalInstrumentationModule;
+import io.opentelemetry.javaagent.extension.instrumentation.internal.injection.ClassInjector;
+import io.opentelemetry.javaagent.extension.instrumentation.internal.injection.InjectionMode;
 import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
 import java.util.ArrayList;
 import java.util.List;
 import net.bytebuddy.utility.JavaModule;
 
 @AutoService(InstrumentationModule.class)
-public class LambdaInstrumentationModule extends InstrumentationModule {
+public class LambdaInstrumentationModule extends InstrumentationModule
+    implements ExperimentalInstrumentationModule {
   public LambdaInstrumentationModule() {
     super("internal-lambda");
   }
@@ -25,6 +29,20 @@ public class LambdaInstrumentationModule extends InstrumentationModule {
   public boolean defaultEnabled(ConfigProperties config) {
     // internal instrumentations are always enabled by default
     return true;
+  }
+
+  @Override
+  public void injectClasses(ClassInjector injector) {
+    injector
+        .proxyBuilder(
+            "io.opentelemetry.javaagent.instrumentation.internal.lambda.LambdaTransformer")
+        .inject(InjectionMode.CLASS_ONLY);
+    if (JavaModule.isSupported()) {
+      injector
+          .proxyBuilder(
+              "io.opentelemetry.javaagent.instrumentation.internal.lambda.Java9LambdaTransformer")
+          .inject(InjectionMode.CLASS_ONLY);
+    }
   }
 
   @Override
