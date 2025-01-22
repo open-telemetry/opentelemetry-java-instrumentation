293532bd702e2fd1aa264c8f318a945f5131a338
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-01-22 14:04:08 +0100

    fix annotation

--- instrumentation/spring/spring-boot-actuator-autoconfigure-2.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/spring/actuator/v2_0/OpenTelemetryMeterRegistryAutoConfiguration.java
+++ instrumentation/spring/spring-boot-actuator-autoconfigure-2.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/spring/actuator/v2_0/OpenTelemetryMeterRegistryAutoConfiguration.java
@@ -9,6 +9,7 @@ import io.micrometer.core.instrument.Clock;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.opentelemetry.javaagent.instrumentation.micrometer.v1_5.MicrometerSingletons;
 import org.springframework.boot.actuate.autoconfigure.metrics.CompositeMeterRegistryAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.MetricsAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.metrics.export.simple.SimpleMetricsExportAutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
@@ -18,12 +19,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+// see
+// https://github.com/spring-projects/spring-boot/blob/main/spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/metrics/export/prometheus/PrometheusMetricsExportAutoConfiguration.java
+// for an example of how configure a MeterRegistry properly
 // CompositeMeterRegistryAutoConfiguration configures the "final" composite registry
-@AutoConfigureBefore(CompositeMeterRegistryAutoConfiguration.class)
-// configure after the SimpleMeterRegistry has initialized; it is normally the last MeterRegistry
-// implementation to be configured, as it's used as a fallback
-// the OTel registry should be added in addition to that fallback and not replace it
-@AutoConfigureAfter(SimpleMetricsExportAutoConfiguration.class)
+@AutoConfigureBefore({
+  CompositeMeterRegistryAutoConfiguration.class,
+  SimpleMetricsExportAutoConfiguration.class
+})
+@AutoConfigureAfter(MetricsAutoConfiguration.class)
 @ConditionalOnBean(Clock.class)
 @ConditionalOnClass(MeterRegistry.class)
 public class OpenTelemetryMeterRegistryAutoConfiguration {
