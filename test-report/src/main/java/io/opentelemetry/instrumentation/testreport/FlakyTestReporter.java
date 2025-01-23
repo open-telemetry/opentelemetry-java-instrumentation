a8d4f26df867a99032434d6d27e51d4e1bf5021e
Lauri Tulmin <ltulmin@splunk.com>
2025-01-23 20:10:21 +0200

    try a different way for getting the job url

--- test-report/src/main/java/io/opentelemetry/instrumentation/testreport/FlakyTestReporter.java
+++ test-report/src/main/java/io/opentelemetry/instrumentation/testreport/FlakyTestReporter.java
@@ -275,6 +275,8 @@ public class FlakyTestReporter {
     String accessKey = System.getProperty("googleSheetsAccessKey");
     String buildScanUrl = System.getProperty("buildScanUrl");
     String jobUrl = System.getProperty("jobUrl");
+    System.err.println("buildScanUrl " + buildScanUrl);
+    System.err.println("jobUrl " + jobUrl);
     if (accessKey != null && !accessKey.isEmpty()) {
       reporter.report(accessKey, buildScanUrl, jobUrl);
     }
