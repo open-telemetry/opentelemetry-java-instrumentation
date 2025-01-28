a9a2262ab3ce91eee89e9d8cce0f94996ee6ebcc
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-01-28 18:56:21 +0100

    test more metric types

--- smoke-tests-otel-starter/spring-boot-3/src/test/java/io/opentelemetry/spring/smoketest/OtelSpringStarterSmokeTest.java
+++ smoke-tests-otel-starter/spring-boot-3/src/test/java/io/opentelemetry/spring/smoketest/OtelSpringStarterSmokeTest.java
@@ -5,6 +5,7 @@
 
 package io.opentelemetry.spring.smoketest;
 
+import org.assertj.core.api.AbstractIterableAssert;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(
@@ -16,6 +17,17 @@ import org.springframework.boot.test.context.SpringBootTest;
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
     properties = {
       // The headers are simply set here to make sure that headers can be parsed
-      "otel.exporter.otlp.headers.c=3"
+      "otel.exporter.otlp.headers.c=3",
+      "otel.instrumentation.runtime-telemetry-java17.enabled=true",
     })
-class OtelSpringStarterSmokeTest extends AbstractOtelSpringStarterSmokeTest {}
+class OtelSpringStarterSmokeTest extends AbstractOtelSpringStarterSmokeTest {
+
+  @Override
+  protected void assertAdditionalMetrics() {
+    // JFR based metrics
+    testing.waitAndAssertMetrics(
+        "io.opentelemetry.runtime-telemetry-java17",
+        "jvm.cpu.limit",
+        AbstractIterableAssert::isNotEmpty);
+  }
+}
