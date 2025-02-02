d97e175e90d7fed074a2b47e8e0767dc9e0821bf
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-02-02 09:31:45 -0800

    Workflow updates

--- .github/scripts/use-cla-approved-github-bot.sh
+++ .github/scripts/use-cla-approved-github-bot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-git config user.name opentelemetrybot
-git config user.email 107717825+opentelemetrybot@users.noreply.github.com
+git config user.name '${{ steps.app-token.outputs.app-slug }}[bot]'
+git config user.email  '${{ steps.get-user-id.outputs.user-id }}+${{ steps.app-token.outputs.app-slug }}[bot]@users.noreply.github.com'
