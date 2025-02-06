84abdcc5a34372da99e49cf5cbdfee02f4abe4e8
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-02-06 15:05:30 +0100

    sort the otel registry last explicitly

--- instrumentation/spring/spring-boot-actuator-autoconfigure-2.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/spring/actuator/v2_0/OpenTelemetryMeterRegistryAutoConfiguration.java
+++ instrumentation/spring/spring-boot-actuator-autoconfigure-2.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/spring/actuator/v2_0/OpenTelemetryMeterRegistryAutoConfiguration.java
@@ -7,9 +7,16 @@ package io.opentelemetry.javaagent.instrumentation.spring.actuator.v2_0;
 
 import io.micrometer.core.instrument.Clock;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
 import io.opentelemetry.javaagent.instrumentation.micrometer.v1_5.MicrometerSingletons;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.actuate.autoconfigure.metrics.CompositeMeterRegistryAutoConfiguration;
-import org.springframework.boot.actuate.autoconfigure.metrics.export.prometheus.PrometheusMetricsExportAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.metrics.export.simple.SimpleMetricsExportAutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
@@ -24,10 +31,7 @@ import org.springframework.context.annotation.Configuration;
 // configure after the SimpleMeterRegistry has initialized; it is normally the last MeterRegistry
 // implementation to be configured, as it's used as a fallback
 // the OTel registry should be added in addition to that fallback and not replace it
-@AutoConfigureAfter({
-  SimpleMetricsExportAutoConfiguration.class,
-  PrometheusMetricsExportAutoConfiguration.class
-})
+@AutoConfigureAfter(SimpleMetricsExportAutoConfiguration.class)
 @ConditionalOnBean(Clock.class)
 @ConditionalOnClass(MeterRegistry.class)
 public class OpenTelemetryMeterRegistryAutoConfiguration {
@@ -35,5 +39,31 @@ public class OpenTelemetryMeterRegistryAutoConfiguration {
   @Bean
   public MeterRegistry otelMeterRegistry() {
     return MicrometerSingletons.meterRegistry();
+  }
+
+  @Bean
+  static BeanPostProcessor postProcessCompositeMeterRegistry() {
+    return new BeanPostProcessor() {
+      @Override
+      public Object postProcessAfterInitialization(Object bean, String beanName) {
+        if (bean instanceof CompositeMeterRegistry) {
+          CompositeMeterRegistry original = (CompositeMeterRegistry) bean;
+          List<MeterRegistry> list = new ArrayList<>(original.getRegistries());
+          // sort otel registry last
+          list.sort(
+              Comparator.comparingInt(
+                  value -> value == MicrometerSingletons.meterRegistry() ? 1 : 0));
+          Set<MeterRegistry> registries = new LinkedHashSet<>(list);
+          return new CompositeMeterRegistry(
+              original.config().clock(), Collections.singletonList(original)) {
+            @Override
+            public Set<MeterRegistry> getRegistries() {
+              return registries;
+            }
+          };
+        }
+        return bean;
+      }
+    };
   }
 }
