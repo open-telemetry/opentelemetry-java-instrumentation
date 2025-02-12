511736865f4893443a9590af5e65cd511efdb257
Lauri Tulmin <ltulmin@splunk.com>
2025-02-12 15:39:57 +0200

    Add db client metrics for Aws sdk2 dynamodb

--- instrumentation/aws-sdk/aws-sdk-2.2/library/src/main/java/io/opentelemetry/instrumentation/awssdk/v2_2/internal/DynamoDbAttributesExtractor.java
+++ instrumentation/aws-sdk/aws-sdk-2.2/library/src/main/java/io/opentelemetry/instrumentation/awssdk/v2_2/internal/DynamoDbAttributesExtractor.java
@@ -3,21 +3,19 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.opentelemetry.instrumentation.awssdk.v1_11;
+package io.opentelemetry.instrumentation.awssdk.v2_2.internal;
 
-import com.amazonaws.Request;
-import com.amazonaws.Response;
 import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.AttributesBuilder;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.instrumentation.api.instrumenter.AttributesExtractor;
 import io.opentelemetry.instrumentation.api.internal.AttributesExtractorUtil;
 import io.opentelemetry.instrumentation.api.internal.SemconvStability;
-import java.util.Collections;
-import java.util.List;
 import javax.annotation.Nullable;
+import software.amazon.awssdk.core.interceptor.ExecutionAttributes;
+import software.amazon.awssdk.core.interceptor.SdkExecutionAttribute;
 
-class DynamoDbAttributesExtractor implements AttributesExtractor<Request<?>, Response<?>> {
+class DynamoDbAttributesExtractor implements AttributesExtractor<ExecutionAttributes, Response> {
 
   // copied from DbIncubatingAttributes
   private static final AttributeKey<String> DB_OPERATION = AttributeKey.stringKey("db.operation");
@@ -26,9 +24,6 @@ class DynamoDbAttributesExtractor implements AttributesExtractor<Request<?>, Res
   private static final AttributeKey<String> DB_SYSTEM = AttributeKey.stringKey("db.system");
   private static final AttributeKey<String> DB_SYSTEM_NAME =
       AttributeKey.stringKey("db.system.name");
-  // copied from AwsIncubatingAttributes
-  private static final AttributeKey<List<String>> AWS_DYNAMODB_TABLE_NAMES =
-      AttributeKey.stringArrayKey("aws.dynamodb.table_names");
 
   // copied from DbIncubatingAttributes.DbSystemIncubatingValues
   private static final String DYNAMODB = "dynamodb";
@@ -36,15 +31,17 @@ class DynamoDbAttributesExtractor implements AttributesExtractor<Request<?>, Res
   private static final String AWS_DYNAMODB = "aws.dynamodb";
 
   @Override
-  public void onStart(AttributesBuilder attributes, Context parentContext, Request<?> request) {
+  public void onStart(
+      AttributesBuilder attributes,
+      Context parentContext,
+      ExecutionAttributes executionAttributes) {
     if (SemconvStability.emitStableDatabaseSemconv()) {
       AttributesExtractorUtil.internalSet(attributes, DB_SYSTEM_NAME, AWS_DYNAMODB);
     }
     if (SemconvStability.emitOldDatabaseSemconv()) {
       AttributesExtractorUtil.internalSet(attributes, DB_SYSTEM, DYNAMODB);
     }
-
-    String operation = getOperationName(request.getOriginalRequest());
+    String operation = executionAttributes.getAttribute(SdkExecutionAttribute.OPERATION_NAME);
     if (operation != null) {
       if (SemconvStability.emitStableDatabaseSemconv()) {
         AttributesExtractorUtil.internalSet(attributes, DB_OPERATION_NAME, operation);
@@ -53,26 +50,13 @@ class DynamoDbAttributesExtractor implements AttributesExtractor<Request<?>, Res
         AttributesExtractorUtil.internalSet(attributes, DB_OPERATION, operation);
       }
     }
-
-    String tableName = RequestAccess.getTableName(request.getOriginalRequest());
-    AttributesExtractorUtil.internalSet(
-        attributes, AWS_DYNAMODB_TABLE_NAMES, Collections.singletonList(tableName));
-  }
-
-  private static String getOperationName(Object request) {
-    String name = request.getClass().getSimpleName();
-    if (!name.endsWith("Request")) {
-      return null;
-    }
-
-    return name.substring(0, name.length() - "Request".length());
   }
 
   @Override
   public void onEnd(
       AttributesBuilder attributes,
       Context context,
-      Request<?> request,
-      @Nullable Response<?> response,
+      ExecutionAttributes executionAttributes,
+      @Nullable Response response,
       @Nullable Throwable error) {}
 }
