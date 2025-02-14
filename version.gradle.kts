87fcbbb0efe9da3f5d9597240d29028461327a4f
otelbot <197425009+otelbot@users.noreply.github.com>
2025-02-14 23:38:37 +0000

    Prepare release 2.13.0

--- version.gradle.kts
+++ version.gradle.kts
@@ -1,5 +1,5 @@
-val stableVersion = "2.13.0-SNAPSHOT"
-val alphaVersion = "2.13.0-alpha-SNAPSHOT"
+val stableVersion = "2.13.0"
+val alphaVersion = "2.13.0-alpha"
 
 allprojects {
   if (findProperty("otel.stable") != "true") {
