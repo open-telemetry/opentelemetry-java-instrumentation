a63f50439de5e39c4e412cb6fd8628038b7aa1dd
Jean Bisutti <jean.bisutti@gmail.com>
2025-02-17 11:34:05 +0100

    Remove comment

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,6 @@ pluginManagement {
     id("org.jetbrains.kotlin.jvm") version "2.1.10"
     id("org.xbib.gradle.plugin.jflex") version "3.0.2"
     id("org.unbroken-dome.xjc") version "2.0.0"
-    // See https://github.com/graalvm/native-build-tools/issues/626
     id("org.graalvm.buildtools.native") version "0.10.5"
   }
 }
