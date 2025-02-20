d4b0b5cacb666ca8402a935ff356ed423fe41353
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-02-19 19:38:19 -0800

    elasticsearch-rest-common-5.0

--- instrumentation/elasticsearch/elasticsearch-rest-common-5.0/javaagent/build.gradle.kts
+++ instrumentation/elasticsearch/elasticsearch-rest-common-5.0/javaagent/build.gradle.kts
@@ -5,5 +5,5 @@ plugins {
 dependencies {
   compileOnly("org.elasticsearch.client:rest:5.0.0")
 
-  api(project(":instrumentation:elasticsearch:elasticsearch-rest-common:library"))
+  api(project(":instrumentation:elasticsearch:elasticsearch-rest-common-5.0:library"))
 }
