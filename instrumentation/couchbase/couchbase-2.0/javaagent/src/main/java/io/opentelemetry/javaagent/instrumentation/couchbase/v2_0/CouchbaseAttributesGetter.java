9fc3e309d91a7625659b7aeed19e36976eb37bc4
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-02-24 12:51:02 +0100

    error.type and db.response.status_code all dbs

--- instrumentation/couchbase/couchbase-2.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/couchbase/v2_0/CouchbaseAttributesGetter.java
+++ instrumentation/couchbase/couchbase-2.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/couchbase/v2_0/CouchbaseAttributesGetter.java
@@ -5,7 +5,6 @@
 
 package io.opentelemetry.javaagent.instrumentation.couchbase.v2_0;
 
-import com.couchbase.client.core.CouchbaseException;
 import io.opentelemetry.instrumentation.api.incubator.semconv.db.DbClientAttributesGetter;
 import io.opentelemetry.semconv.incubating.DbIncubatingAttributes;
 import javax.annotation.Nullable;
@@ -49,24 +48,5 @@ final class CouchbaseAttributesGetter
   @Nullable
   public String getDbOperationName(CouchbaseRequestInfo couchbaseRequest) {
     return couchbaseRequest.operation();
-  }
-
-  @Nullable
-  @Override
-  public String getResponseStatusFromException(Throwable throwable) {
-    if (throwable instanceof CouchbaseException) {
-      // todo
-      //      ResponseStatusDetails details = ((CouchbaseException) throwable).details();
-      // in newer versions of the SDK, this is available as a method on the exception
-      // check if it's available and contains the status
-      //      @InterfaceAudience.Public
-      //       @InterfaceStability.Experimental
-      //       public ResponseStatusDetails details() {
-      //           return this.responseStatusDetails;
-      //       }
-      //      throwable.
-      //      return throwable.getClass().getSimpleName();
-    }
-    return null;
   }
 }
