99a4c21c40fa5b6dbbeb19010922dbeb79177cf9
Krishna Chaitanya Surapaneni <chaitanya1004@gmail.com>
2025-02-26 09:41:05 -0700

    only consider 6.0+ classes

--- instrumentation/activej-http-6.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/activejhttp/ActivejHttpServerConnectionInstrumentationModule.java
+++ instrumentation/activej-http-6.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/activejhttp/ActivejHttpServerConnectionInstrumentationModule.java
@@ -5,12 +5,14 @@
 
 package io.opentelemetry.javaagent.instrumentation.activejhttp;
 
+import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.hasClassesNamed;
 import static java.util.Collections.singletonList;
 
 import com.google.auto.service.AutoService;
 import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import java.util.List;
+import net.bytebuddy.matcher.ElementMatcher;
 
 @AutoService(InstrumentationModule.class)
 public class ActivejHttpServerConnectionInstrumentationModule extends InstrumentationModule {
@@ -22,5 +24,10 @@ public class ActivejHttpServerConnectionInstrumentationModule extends Instrument
   @Override
   public List<TypeInstrumentation> typeInstrumentations() {
     return singletonList(new ActivejHttpServerConnectionInstrumentation());
+  }
+
+  @Override
+  public ElementMatcher.Junction<ClassLoader> classLoaderMatcher() {
+    return hasClassesNamed("io.activej.reactor.schedule.ScheduledPriorityQueue");
   }
 }
