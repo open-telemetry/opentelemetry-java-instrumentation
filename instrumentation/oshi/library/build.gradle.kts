e3c78e0b787c641b1fa4ae53907beb51764f98c5
Jay DeLuca <jaydeluca4@gmail.com>
2025-03-07 06:04:30 -0500

    only conditionally update oshi for test classpath for arm mac

--- instrumentation/oshi/library/build.gradle.kts
+++ instrumentation/oshi/library/build.gradle.kts
@@ -4,8 +4,12 @@ plugins {
 }
 
 dependencies {
-  // 5.5.0 is the first version that works on arm mac
-  library("com.github.oshi:oshi-core:5.5.0")
+  library("com.github.oshi:oshi-core:5.3.1")
+
+  if (osdetector.os == "osx" && osdetector.arch == "aarch_64" && !(findProperty("testLatestDeps") as Boolean)) {
+    // 5.5.0 is the first version that works on arm mac
+    configurations.testRuntimeClasspath.get().resolutionStrategy.force("com.github.oshi:oshi-core:5.5.0")
+  }
 
   testImplementation(project(":instrumentation:oshi:testing"))
 }
