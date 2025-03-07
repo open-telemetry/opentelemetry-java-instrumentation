554d574c0decadc4af342ab27fc019d93d914a12
Jay DeLuca <jaydeluca4@gmail.com>
2025-03-07 07:11:16 -0500

    move config overrides outside dependency block

--- instrumentation/oshi/library/build.gradle.kts
+++ instrumentation/oshi/library/build.gradle.kts
@@ -6,10 +6,10 @@ plugins {
 dependencies {
   library("com.github.oshi:oshi-core:5.3.1")
 
-  if (osdetector.os == "osx" && osdetector.arch == "aarch_64" && !(findProperty("testLatestDeps") as Boolean)) {
-    // 5.5.0 is the first version that works on arm mac
-    configurations.testRuntimeClasspath.get().resolutionStrategy.force("com.github.oshi:oshi-core:5.5.0")
-  }
-
   testImplementation(project(":instrumentation:oshi:testing"))
+}
+
+if (osdetector.os == "osx" && osdetector.arch == "aarch_64" && !(findProperty("testLatestDeps") as Boolean)) {
+  // 5.5.0 is the first version that works on arm mac
+  configurations.testRuntimeClasspath.get().resolutionStrategy.force("com.github.oshi:oshi-core:5.5.0")
 }
