d536bb5f9fee9680c90aad447e6331d3b8a3490b
Lauri Tulmin <ltulmin@splunk.com>
2025-03-11 10:53:02 +0200

    Fix instrumentation for latest axis2

--- instrumentation/jaxws/jaxws-3.0-axis2-1.6-testing/build.gradle.kts
+++ instrumentation/jaxws/jaxws-3.0-axis2-1.6-testing/build.gradle.kts
@@ -3,13 +3,16 @@ plugins {
 }
 
 dependencies {
-  testLibrary("org.apache.cxf:cxf-rt-frontend-jaxws:4.0.0")
-  testLibrary("org.apache.cxf:cxf-rt-transports-http:4.0.0")
+  val axis2Version = "2.0.0"
+  testLibrary("org.apache.axis2:axis2-jaxws:$axis2Version") {
+    exclude(group = "org.eclipse.jetty.ee9")
+  }
+  testLibrary("org.apache.axis2:axis2-transport-http:$axis2Version")
+  testLibrary("org.apache.axis2:axis2-transport-local:$axis2Version")
 
-  testImplementation("jakarta.servlet:jakarta.servlet-api:5.0.0")
   testImplementation(project(":instrumentation:jaxws:jaxws-3.0-common-testing"))
 
-  testInstrumentation(project(":instrumentation:jaxws:jaxws-cxf-3.0:javaagent"))
+  testInstrumentation(project(":instrumentation:jaxws:jaxws-2.0-axis2-1.6:javaagent"))
 
   testInstrumentation(project(":instrumentation:servlet:servlet-5.0:javaagent"))
   testInstrumentation(project(":instrumentation:jetty:jetty-11.0:javaagent"))
