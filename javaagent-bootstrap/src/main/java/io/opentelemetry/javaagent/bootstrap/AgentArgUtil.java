3024aef8ff28879a64941560ee63f0bf42ce0669
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-03-12 11:01:41 +0100

    support dynamic attach

---
+++ javaagent-bootstrap/src/main/java/io/opentelemetry/javaagent/bootstrap/AgentArgUtil.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.bootstrap;
+
+import javax.annotation.Nullable;
+
+public class AgentArgUtil {
+
+  private AgentArgUtil() {}
+
+  @SuppressWarnings("SystemOut")
+  public static void setSystemProperties(@Nullable String agentArgs) {
+    boolean debug = false;
+    if (agentArgs != null && !agentArgs.isEmpty()) {
+      String[] options = agentArgs.split(";");
+      for (String option : options) {
+        String[] keyValue = option.split("=");
+        if (keyValue.length == 2) {
+          if (keyValue[0].equals("otel.javaagent.debug")) {
+            debug = Boolean.parseBoolean(keyValue[1]);
+          } else {
+            System.setProperty(keyValue[0], keyValue[1]);
+            if (debug) {
+              System.out.println("Setting property [" + keyValue[0] + "] = " + keyValue[1]);
+            }
+          }
+        }
+      }
+    }
+  }
+}
