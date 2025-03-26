3277d7ecec38b2f81a58d39952dabaf994630c18
Boslet, Cory (cb645j) <cb645j@att.com>
2025-03-26 18:26:32 -0400

    Trigger build

--- instrumentation/camunda/camunda-7.0/testing/src/main/java/io/opentelemetry/instrumentation/camunda/v7_0/AbstractCamundaTest.java
+++ instrumentation/camunda/camunda-7.0/testing/src/main/java/io/opentelemetry/instrumentation/camunda/v7_0/AbstractCamundaTest.java
@@ -53,7 +53,7 @@ public abstract class AbstractCamundaTest {
   }
 
   @Test
-  void testProcessExecutionAllSuccess() {
+  void testProcessExecutionSuccess() {
     Deployment deployment =
         processEngine
             .getRepositoryService()
