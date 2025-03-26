6e797ebb1b29c1635857d7540478c61aa24335bf
Boslet, Cory (cb645j) <cb645j@att.com>
2025-03-26 14:46:57 -0400

    fix build errors and spotless

--- instrumentation/camunda/camunda-7.0/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/camunda/v7_0/CamundaTest.java
+++ instrumentation/camunda/camunda-7.0/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/camunda/v7_0/CamundaTest.java
@@ -1,7 +1,10 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package io.opentelemetry.javaagent.instrumentation.camunda.v7_0;
 
 import io.opentelemetry.instrumentation.camunda.v7_0.AbstractCamundaTest;
 
-public class CamundaTest extends AbstractCamundaTest {
-
-}
+public class CamundaTest extends AbstractCamundaTest {}
