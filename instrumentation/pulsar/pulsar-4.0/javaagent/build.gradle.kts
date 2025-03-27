e5baeb38f90bb8d6de33a98320b9ec84dc7e047c
crossoverJie <crossoverJie@gmail.com>
2025-03-27 23:51:16 +0800

    remove jersey-client

--- instrumentation/pulsar/pulsar-4.0/javaagent/build.gradle.kts
+++ instrumentation/pulsar/pulsar-4.0/javaagent/build.gradle.kts
@@ -14,7 +14,6 @@ muzzle {
 dependencies {
   library("org.apache.pulsar:pulsar-client:4.0.0")
   implementation(project(":instrumentation:pulsar:pulsar-common:javaagent"))
-  implementation("org.glassfish.jersey.core:jersey-client:2.42")
 
   testImplementation("org.testcontainers:pulsar")
   testImplementation("org.apache.pulsar:pulsar-client-admin:4.0.0")
