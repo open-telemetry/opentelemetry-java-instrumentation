024109a0d6347e84fa9bdce490044e61e67c3d86
Lauri Tulmin <ltulmin@splunk.com>
2025-03-28 16:45:52 +0200

    add mapping

--- instrumentation/internal/internal-class-loader/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/internal/classloader/AdviceUtil.java
+++ instrumentation/internal/internal-class-loader/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/internal/classloader/AdviceUtil.java
@@ -9,15 +9,19 @@ import io.opentelemetry.javaagent.extension.instrumentation.TypeTransformer;
 import io.opentelemetry.javaagent.tooling.Utils;
 import io.opentelemetry.javaagent.tooling.bytebuddy.ExceptionHandlers;
 import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.asm.Advice;
 import net.bytebuddy.description.method.MethodDescription;
 import net.bytebuddy.matcher.ElementMatcher;
 
 class AdviceUtil {
+  private static final Advice.WithCustomMapping adviceMapping =
+      Advice.withCustomMapping()
+          .with(new Advice.AssignReturned.Factory().withSuppressed(Throwable.class));
 
   static void applyInlineAdvice(
       TypeTransformer transformer, ElementMatcher<MethodDescription> matcher, String adviceClass) {
     transformer.applyTransformer(
-        new AgentBuilder.Transformer.ForAdvice()
+        new AgentBuilder.Transformer.ForAdvice(adviceMapping)
             .include(Utils.getBootstrapProxy(), Utils.getAgentClassLoader())
             .withExceptionHandler(ExceptionHandlers.defaultExceptionHandler())
             .advice(matcher, adviceClass));
