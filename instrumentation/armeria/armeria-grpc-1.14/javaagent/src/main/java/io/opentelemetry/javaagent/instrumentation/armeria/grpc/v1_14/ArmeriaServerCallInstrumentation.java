db122afcebcb7ce25174331b69050ba03c1a6970
Jonas Kunz <jonas.kunz@elastic.co>
2025-04-01 11:22:30 +0200

    Fix compilation warnings and spotless

--- instrumentation/armeria/armeria-grpc-1.14/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/armeria/grpc/v1_14/ArmeriaServerCallInstrumentation.java
+++ instrumentation/armeria/armeria-grpc-1.14/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/armeria/grpc/v1_14/ArmeriaServerCallInstrumentation.java
@@ -10,7 +10,6 @@ import static net.bytebuddy.matcher.ElementMatchers.namedOneOf;
 
 import com.linecorp.armeria.server.ServiceRequestContext;
 import io.grpc.ServerCall;
-import io.opentelemetry.instrumentation.api.util.VirtualField;
 import io.opentelemetry.instrumentation.grpc.v1_6.GrpcAuthorityStorage;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeTransformer;
