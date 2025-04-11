f9672efa460da910a112b7609d195f946ef6caca
Benjamin Confino <benjamic@uk.ibm.com>
2025-04-11 08:43:32 +0100

    add method to read files on zos with correct encoding

--- instrumentation/resources/library/src/main/java/io/opentelemetry/instrumentation/resources/ContainerResource.java
+++ instrumentation/resources/library/src/main/java/io/opentelemetry/instrumentation/resources/ContainerResource.java
@@ -9,11 +9,17 @@ import com.google.errorprone.annotations.MustBeClosed;
 import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.sdk.resources.Resource;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -50,12 +56,6 @@ public final class ContainerResource {
 
   // Visible for testing
   Resource buildResource() {
-    // disable container id detection on z/os
-    // https://github.com/open-telemetry/opentelemetry-java-instrumentation/issues/12418
-    String osName = System.getProperty("os.name");
-    if (osName.equalsIgnoreCase("z/OS") || osName.equalsIgnoreCase("OS/390")) {
-      return Resource.empty();
-    }
     return getContainerId()
         .map(id -> Resource.create(Attributes.of(CONTAINER_ID, id)))
         .orElseGet(Resource::empty);
@@ -77,19 +77,83 @@ public final class ContainerResource {
   // Exists for testing
   static class Filesystem {
 
+    private static final Logger logger = Logger.getLogger(Filesystem.class.getName());
+
     boolean isReadable(Path path) {
       return Files.isReadable(path);
     }
 
     @MustBeClosed
     Stream<String> lines(Path path) throws IOException {
-      return Files.lines(path);
+
+      String osName = getOsName();
+      if (!(osName.equalsIgnoreCase("z/OS") || osName.equalsIgnoreCase("OS/390"))) {
+        return Files.lines(path);
+      } else {
+        return zosLines(path);
+      }
+    }
+
+    // This method reads the /proc system using the correct Charset for z/OS
+    @MustBeClosed
+    Stream<String> zosLines(Path path) throws IOException {
+
+      List<Charset> charsetsToTest = new ArrayList<Charset>();
+
+      // Since this class needs to be compatible with Java9&11, use the 'native.encoding' property
+      // to get the nativeCharset on java17+
+      // rather than System.console().charset().
+      if (System.getProperty("native.encoding") != null) {
+        Charset nativeCharset = Charset.forName(System.getProperty("native.encoding"));
+        if (nativeCharset != null) {
+          charsetsToTest.add(nativeCharset);
+        }
+      } else {
+        // On older javas that property won't exist but the default charset will be the native
+        // encoding.
+        charsetsToTest.add(Charset.defaultCharset());
+      }
+
+      // As a fallback value, add a hardcoded reference to IBM1047 (Canonical name: Cp1047).
+      Charset ibm1047 = Charset.forName("Cp1047");
+      if (ibm1047 != null) { // safety check
+        charsetsToTest.add(ibm1047);
+      }
+
+      // The odds of /proc being UTF-8 but the native encoding saying otherwise is extremely low
+      // but since UTF-8 is the standard it should be checked.
+      charsetsToTest.add(StandardCharsets.UTF_8);
+
+      IOException exception = null;
+      for (Charset charset : charsetsToTest) {
+        try (BufferedReader charsetTester = Files.newBufferedReader(path, charset)) {
+          @SuppressWarnings("unused")
+          String line = charsetTester.readLine();
+
+          return Files.lines(path, charset);
+        } catch (IOException e) {
+          if (exception == null) {
+            exception = e;
+          } else {
+            exception.addSuppressed(e);
+          }
+        }
+      }
+
+      // If none of the charsets matched, log a warning and return an empty Stream
+      logger.log(
+          Level.WARNING, "Unable to read file " + path.toAbsolutePath().toString(), exception);
+      return Stream.empty();
     }
 
     List<String> lineList(Path path) throws IOException {
       try (Stream<String> lines = lines(path)) {
         return lines.collect(Collectors.toList());
       }
+    }
+
+    String getOsName() {
+      return System.getProperty("os.name");
     }
   }
 }
