8ec052080a7c72b5682222102184e87330285838
schmikei <keith.schmitty@gmail.com>
2025-04-16 14:32:25 -0400

    attempt to add jvm.file_descriptor.count metric to instrumentation

--- instrumentation/jmx-metrics/library/src/test/java/io/opentelemetry/instrumentation/jmx/rules/JvmTargetSystemTest.java
+++ instrumentation/jmx-metrics/library/src/test/java/io/opentelemetry/instrumentation/jmx/rules/JvmTargetSystemTest.java
@@ -163,6 +163,14 @@ class JvmTargetSystemTest extends TargetSystemTest {
                         .isGauge()
                         .hasDataPointsWithoutAttributes())
             .add(
+                "jvm.file_descriptor.count",
+                metric ->
+                    metric
+                        .hasDescription("Number of open file descriptors.")
+                        .hasUnit("{file_descriptors}")
+                        .isUpDownCounter()
+                        .hasDataPointsWithoutAttributes())
+            .add(
                 "jvm.system.cpu.load_1m",
                 metric ->
                     metric
