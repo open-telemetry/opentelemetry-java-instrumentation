9cc8650d14091e024863fdb70ba4b1e54756a8d2
crossoverJie <crossoverJie@gmail.com>
2025-04-17 18:18:45 +0800

    override metrics

--- instrumentation-api-incubator/src/test/java/io/opentelemetry/instrumentation/api/incubator/semconv/http/HttpClientExperimentalMetricsTest.java
+++ instrumentation-api-incubator/src/test/java/io/opentelemetry/instrumentation/api/incubator/semconv/http/HttpClientExperimentalMetricsTest.java
@@ -31,7 +31,9 @@ import io.opentelemetry.api.trace.TraceState;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.instrumentation.api.instrumenter.OperationListener;
 import io.opentelemetry.instrumentation.api.semconv.http.HttpClientMetrics;
+import io.opentelemetry.sdk.extension.incubator.metric.viewconfig.ViewConfig;
 import io.opentelemetry.sdk.metrics.SdkMeterProvider;
+import io.opentelemetry.sdk.metrics.SdkMeterProviderBuilder;
 import io.opentelemetry.sdk.testing.assertj.AttributeAssertion;
 import io.opentelemetry.sdk.testing.exporter.InMemoryMetricReader;
 import io.opentelemetry.semconv.ErrorAttributes;
@@ -40,6 +42,7 @@ import io.opentelemetry.semconv.NetworkAttributes;
 import io.opentelemetry.semconv.ServerAttributes;
 import io.opentelemetry.semconv.UrlAttributes;
 import io.opentelemetry.semconv.incubating.HttpIncubatingAttributes;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -164,6 +167,100 @@ class HttpClientExperimentalMetricsTest {
                     .hasHistogramSatisfying(
                         histogram ->
                             histogram.hasPointsSatisfying(point -> point.hasSum(400 /* bytes */))));
+  }
+
+  @Test
+  void collectsOverrideMetrics() throws Exception {
+    InMemoryMetricReader metricReader = InMemoryMetricReader.create();
+    SdkMeterProviderBuilder sdkMeterProviderBuilder =
+        SdkMeterProvider.builder().registerMetricReader(metricReader);
+    try (InputStream inputStream = getClass().getResourceAsStream("/view.yaml")) {
+      ViewConfig.registerViews(sdkMeterProviderBuilder, inputStream);
+    }
+    SdkMeterProvider meterProvider = sdkMeterProviderBuilder.build();
+
+    OperationListener listener =
+        HttpClientExperimentalMetrics.get().create(meterProvider.get("test"));
+
+    Attributes requestAttributes =
+        Attributes.builder()
+            .put(HttpAttributes.HTTP_REQUEST_METHOD, "GET")
+            .put(UrlAttributes.URL_FULL, "https://localhost:1234/")
+            .put(UrlAttributes.URL_PATH, "/")
+            .put(UrlAttributes.URL_QUERY, "q=a")
+            .put(ServerAttributes.SERVER_ADDRESS, "localhost")
+            .put(ServerAttributes.SERVER_PORT, 1234)
+            .put(UrlAttributes.URL_SCHEME, "https")
+            .build();
+
+    Attributes responseAttributes =
+        Attributes.builder()
+            .put(HttpAttributes.HTTP_RESPONSE_STATUS_CODE, 200)
+            .put(ErrorAttributes.ERROR_TYPE, "400")
+            .put(HttpIncubatingAttributes.HTTP_REQUEST_BODY_SIZE, 100)
+            .put(HttpIncubatingAttributes.HTTP_RESPONSE_BODY_SIZE, 200)
+            .put(NetworkAttributes.NETWORK_PROTOCOL_NAME, "http")
+            .put(NetworkAttributes.NETWORK_PROTOCOL_VERSION, "2.0")
+            .put(NetworkAttributes.NETWORK_PEER_ADDRESS, "1.2.3.4")
+            .put(NetworkAttributes.NETWORK_PEER_PORT, 8080)
+            .build();
+
+    List<AttributeAssertion> bodySizeAssertion =
+        new ArrayList<>(Arrays.asList(equalTo(ServerAttributes.SERVER_PORT, 1234)));
+    Context parent =
+        Context.root()
+            .with(
+                Span.wrap(
+                    SpanContext.create(
+                        "ff01020304050600ff0a0b0c0d0e0f00",
+                        "090a0b0c0d0e0f00",
+                        TraceFlags.getSampled(),
+                        TraceState.getDefault())));
+
+    Context context1 = listener.onStart(parent, requestAttributes, nanos(100));
+
+    assertThat(metricReader.collectAllMetrics()).isEmpty();
+
+    assertThat(metricReader.collectAllMetrics()).isEmpty();
+
+    listener.onEnd(context1, responseAttributes, nanos(250));
+
+    assertThat(metricReader.collectAllMetrics())
+        .satisfiesExactlyInAnyOrder(
+            metric ->
+                assertThat(metric)
+                    .hasName("http.client.request.body.size")
+                    .hasUnit("By")
+                    .hasDescription("Size of HTTP client request bodies.")
+                    .hasHistogramSatisfying(
+                        histogram ->
+                            histogram.hasPointsSatisfying(
+                                point ->
+                                    point
+                                        .hasSum(100 /* bytes */)
+                                        .hasAttributesSatisfying(bodySizeAssertion)
+                                        .hasExemplarsSatisfying(
+                                            exemplar ->
+                                                exemplar
+                                                    .hasTraceId("ff01020304050600ff0a0b0c0d0e0f00")
+                                                    .hasSpanId("090a0b0c0d0e0f00")))),
+            metric ->
+                assertThat(metric)
+                    .hasName("http.client.response.body.size")
+                    .hasUnit("By")
+                    .hasDescription("Size of HTTP client response bodies.")
+                    .hasHistogramSatisfying(
+                        histogram ->
+                            histogram.hasPointsSatisfying(
+                                point ->
+                                    point
+                                        .hasSum(200 /* bytes */)
+                                        .hasAttributesSatisfying(bodySizeAssertion)
+                                        .hasExemplarsSatisfying(
+                                            exemplar ->
+                                                exemplar
+                                                    .hasTraceId("ff01020304050600ff0a0b0c0d0e0f00")
+                                                    .hasSpanId("090a0b0c0d0e0f00")))));
   }
 
   @Test
