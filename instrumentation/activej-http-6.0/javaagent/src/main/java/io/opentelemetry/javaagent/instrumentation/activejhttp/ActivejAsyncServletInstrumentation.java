3b66c82c52276ccfbed1e03867c6a1a1df32f18b
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2025-04-22 18:48:54 +0200

    activejhttp

--- instrumentation/activej-http-6.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/activejhttp/ActivejAsyncServletInstrumentation.java
+++ instrumentation/activej-http-6.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/activejhttp/ActivejAsyncServletInstrumentation.java
@@ -25,8 +25,10 @@ import io.opentelemetry.context.Scope;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeTransformer;
 import net.bytebuddy.asm.Advice;
+import net.bytebuddy.asm.Advice.AssignReturned;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.matcher.ElementMatcher;
+import javax.annotation.Nullable;
 
 public class ActivejAsyncServletInstrumentation implements TypeInstrumentation {
 
@@ -52,39 +54,46 @@ public class ActivejAsyncServletInstrumentation implements TypeInstrumentation {
   @SuppressWarnings("unused")
   public static class ServeAdvice {
 
-    @Advice.OnMethodEnter(suppress = Throwable.class)
-    public static void methodEnter(
-        @Advice.This AsyncServlet asyncServlet,
-        @Advice.Argument(0) HttpRequest request,
-        @Advice.Local("otelContext") Context context,
-        @Advice.Local("otelScope") Scope scope,
-        @Advice.Local("httpRequest") HttpRequest httpRequest) {
-      Context parentContext = currentContext();
-      httpRequest = request;
-      if (!instrumenter().shouldStart(parentContext, request)) {
-        return;
-      }
-      context = instrumenter().start(parentContext, request);
-      scope = context.makeCurrent();
+    public static class AdviceLocals {
+      public HttpRequest httpRequest;
+      public Context context;
+      public Scope scope;
     }
 
+    @Nullable
+    @Advice.OnMethodEnter(suppress = Throwable.class)
+    public static AdviceLocals methodEnter(
+        @Advice.This AsyncServlet asyncServlet, @Advice.Argument(0) HttpRequest request) {
+
+      Context parentContext = currentContext();
+      if (!instrumenter().shouldStart(parentContext, request)) {
+        return null;
+      }
+      AdviceLocals locals = new AdviceLocals();
+      locals.httpRequest = request;
+      locals.context = instrumenter().start(parentContext, request);
+      locals.scope = locals.context.makeCurrent();
+      return locals;
+    }
+
+    @AssignReturned.ToReturned
     @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
-    public static void methodExit(
+    public static Promise<HttpResponse> methodExit(
         @Advice.This AsyncServlet asyncServlet,
-        @Advice.Return(readOnly = false) Promise<HttpResponse> responsePromise,
+        @Advice.Return Promise<HttpResponse> responsePromise,
         @Advice.Thrown Throwable throwable,
-        @Advice.Local("otelContext") Context context,
-        @Advice.Local("otelScope") Scope scope,
-        @Advice.Local("httpRequest") HttpRequest httpRequest) {
-      if (scope == null) {
-        return;
+        @Advice.Enter @Nullable AdviceLocals locals) {
+
+      if (locals == null || locals.scope == null) {
+        return responsePromise;
       }
-      scope.close();
+      locals.scope.close();
       if (throwable != null) {
-        instrumenter().end(context, httpRequest, null, throwable);
+        instrumenter().end(locals.context, locals.httpRequest, null, throwable);
       } else {
-        responsePromise = PromiseWrapper.wrap(responsePromise, httpRequest, context);
+        responsePromise = PromiseWrapper.wrap(responsePromise, locals.httpRequest, locals.context);
       }
+      return responsePromise;
     }
   }
 }
