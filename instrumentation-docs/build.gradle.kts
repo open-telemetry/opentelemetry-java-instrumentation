7d796abe27ebc4fef130f94a281dab6d05fd1ef6
Jay DeLuca <jaydeluca4@gmail.com>
2025-04-25 06:40:56 -0400

    Add ability to document configuration options in metadata.yaml (#13742)
    Co-authored-by: Lauri Tulmin <tulmin@gmail.com>

--- instrumentation-docs/build.gradle.kts
+++ instrumentation-docs/build.gradle.kts
@@ -8,6 +8,7 @@ otelJava {
 
 dependencies {
   implementation("org.yaml:snakeyaml:2.4")
+  implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.18.2")
   implementation("io.opentelemetry:opentelemetry-sdk-common")
 
   testImplementation(enforcedPlatform("org.junit:junit-bom:5.12.2"))
