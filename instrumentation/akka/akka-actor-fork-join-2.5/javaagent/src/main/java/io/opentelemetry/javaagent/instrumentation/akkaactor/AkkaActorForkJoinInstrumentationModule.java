b5492e8c5b41d2f47423435c45668f9208aedd35
SylvainJuge <763082+SylvainJuge@users.noreply.github.com>
2025-04-28 16:40:18 +0200

    indy ready migration - part1 (#13759)
    Co-authored-by: Jonas Kunz <jonas.kunz@elastic.co>
    Co-authored-by: Jonas Kunz <j+github@kunzj.de>

--- instrumentation/akka/akka-actor-fork-join-2.5/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/akkaactor/AkkaActorForkJoinInstrumentationModule.java
+++ instrumentation/akka/akka-actor-fork-join-2.5/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/akkaactor/AkkaActorForkJoinInstrumentationModule.java
@@ -10,12 +10,19 @@ import static java.util.Arrays.asList;
 import com.google.auto.service.AutoService;
 import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
+import io.opentelemetry.javaagent.extension.instrumentation.internal.ExperimentalInstrumentationModule;
 import java.util.List;
 
 @AutoService(InstrumentationModule.class)
-public class AkkaActorForkJoinInstrumentationModule extends InstrumentationModule {
+public class AkkaActorForkJoinInstrumentationModule extends InstrumentationModule
+    implements ExperimentalInstrumentationModule {
   public AkkaActorForkJoinInstrumentationModule() {
     super("akka-actor-fork-join", "akka-actor-fork-join-2.5", "akka-actor");
+  }
+
+  @Override
+  public boolean isIndyReady() {
+    return true;
   }
 
   @Override
