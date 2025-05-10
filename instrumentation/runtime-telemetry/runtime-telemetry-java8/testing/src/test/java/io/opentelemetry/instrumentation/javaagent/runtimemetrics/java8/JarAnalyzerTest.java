7db43767d824422093325454764fa2fa5269bd75
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-05-10 16:00:58 +0200

    fix amybiguous

--- instrumentation/runtime-telemetry/runtime-telemetry-java8/testing/src/test/java/io/opentelemetry/instrumentation/javaagent/runtimemetrics/java8/JarAnalyzerTest.java
+++ instrumentation/runtime-telemetry/runtime-telemetry-java8/testing/src/test/java/io/opentelemetry/instrumentation/javaagent/runtimemetrics/java8/JarAnalyzerTest.java
@@ -42,7 +42,7 @@ class JarAnalyzerTest {
   void processUrl_EmitsEvents(URL archiveUrl, Consumer<AttributesAssert> attributesConsumer) {
     ExtendedLogRecordBuilder builder = mock(ExtendedLogRecordBuilder.class);
     when(builder.setEventName(eq("package.info"))).thenReturn(builder);
-    when(builder.setAllAttributes(any())).thenReturn(builder);
+    when(builder.setAllAttributes(any(Attributes.class))).thenReturn(builder);
 
     JarAnalyzer.processUrl(builder, archiveUrl);
 
