77ed62e6aad09b519118921859a255c6a6b4d1b0
Lauri Tulmin <ltulmin@splunk.com>
2025-05-23 14:52:15 +0300

    Add instrumentation for vertx-sql-client 5.0

---
+++ instrumentation/vertx/vertx-sql-client/vertx-sql-client-5.0/javaagent/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+  id("otel.javaagent-instrumentation")
+}
+
+muzzle {
+  pass {
+    group.set("io.vertx")
+    module.set("vertx-sql-client")
+    versions.set("[5.0.0,)")
+    assertInverse.set(true)
+  }
+}
+
+dependencies {
+  val version = "5.0.0"
+  library("io.vertx:vertx-sql-client:$version")
+  library("io.vertx:vertx-codegen:$version")
+
+  implementation(project(":instrumentation:vertx:vertx-sql-client:vertx-sql-client-common:javaagent"))
+
+  testInstrumentation(project(":instrumentation:netty:netty-4.1:javaagent"))
+  testInstrumentation(project(":instrumentation:vertx:vertx-sql-client:vertx-sql-client-4.0:javaagent"))
+
+  testLibrary("io.vertx:vertx-pg-client:$version")
+}
+
+tasks {
+  withType<Test>().configureEach {
+    usesService(gradle.sharedServices.registrations["testcontainersBuildService"].service)
+  }
+
+  val testStableSemconv by registering(Test::class) {
+    jvmArgs("-Dotel.semconv-stability.opt-in=database")
+  }
+
+  check {
+    dependsOn(testStableSemconv)
+  }
+}
