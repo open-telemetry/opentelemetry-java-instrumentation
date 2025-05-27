b77a2d77ae19eb5c02f6806f90fb945e6422c143
Lauri Tulmin <ltulmin@splunk.com>
2025-05-27 09:17:33 +0300

    Fix tests for vertx-kafka-client 5.0 (#13922)

---
+++ instrumentation/vertx/vertx-kafka-client-3.6/vertx-kafka-client-5-testing/src/testNoReceiveTelemetry/java/io/opentelemetry/javaagent/instrumentation/vertx/kafka/v5_0/SingleRecordNoReceiveTelemetryVertxKafka5Test.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.vertx.kafka.v5_0;
+
+import io.opentelemetry.instrumentation.testing.junit.AgentInstrumentationExtension;
+import io.opentelemetry.instrumentation.testing.junit.InstrumentationExtension;
+import io.opentelemetry.javaagent.instrumentation.vertx.kafka.AbstractSingleRecordNoReceiveTelemetryVertxKafkaTest;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.kafka.client.consumer.KafkaConsumer;
+import io.vertx.kafka.client.producer.KafkaProducer;
+import io.vertx.kafka.client.producer.KafkaProducerRecord;
+import io.vertx.kafka.client.producer.RecordMetadata;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+public class SingleRecordNoReceiveTelemetryVertxKafka5Test
+    extends AbstractSingleRecordNoReceiveTelemetryVertxKafkaTest {
+  @RegisterExtension
+  private static final InstrumentationExtension testing = AgentInstrumentationExtension.create();
+
+  @Override
+  protected InstrumentationExtension testing() {
+    return testing;
+  }
+
+  @Override
+  protected boolean hasConsumerGroup() {
+    return true;
+  }
+
+  @Override
+  protected void closeKafkaConsumer(KafkaConsumer<?, ?> consumer) {
+    consumer.close();
+  }
+
+  @Override
+  protected void closeKafkaProducer(KafkaProducer<?, ?> producer) {
+    producer.close();
+  }
+
+  @Override
+  protected void closeVertx(Vertx vertx) {
+    vertx.close();
+  }
+
+  @Override
+  protected void sendRecord(
+      KafkaProducerRecord<String, String> record, Handler<AsyncResult<RecordMetadata>> handler) {
+    kafkaProducer.send(record).onComplete(handler);
+  }
+
+  @Override
+  protected void subscribe(String topic) {
+    kafkaConsumer.subscribe(topic);
+  }
+}
