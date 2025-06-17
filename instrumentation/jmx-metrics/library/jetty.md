380272ee0ff751da6283a1a4dbbad7945cb4cf64
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2025-06-17 14:20:19 +0200

    add jetty thread metrics

--- instrumentation/jmx-metrics/library/jetty.md
+++ instrumentation/jmx-metrics/library/jetty.md
@@ -2,15 +2,10 @@
 
 Here is the list of metrics based on MBeans exposed by Jetty.
 
-| Metric Name                    | Type          | Attributes   | Description                                          |
-| ------------------------------ | ------------- | ------------ | ---------------------------------------------------- |
-| jetty.session.sessionsCreated  | Counter       | resource     | The number of sessions established in total          |
-| jetty.session.sessionTimeTotal | Counter       | resource     | The total time sessions have been active             |
-| jetty.session.sessionTimeMax   | Gauge         | resource     | The maximum amount of time a session has been active |
-| jetty.session.sessionTimeMean  | Gauge         | resource     | The mean time sessions remain active                 |
-| jetty.threads.busyThreads      | UpDownCounter |              | The current number of busy threads                   |
-| jetty.threads.idleThreads      | UpDownCounter |              | The current number of idle threads                   |
-| jetty.threads.maxThreads       | UpDownCounter |              | The maximum number of threads in the pool            |
-| jetty.threads.queueSize        | UpDownCounter |              | The current number of threads in the queue           |
-| jetty.io.selectCount           | Counter       | resource, id | The number of select calls                           |
-| jetty.logging.LoggerCount      | UpDownCounter |              | The number of registered loggers by name             |
+| Metric Name             | Type          | Attributes | Description                               |
+|-------------------------|---------------|------------|-------------------------------------------|
+| jetty.thread.count      | UpDownCounter |            | The current number of threads             |
+| jetty.thread.limit      | UpDownCounter |            | The maximum number of threads in the pool |
+| jetty.thread.busy.count | UpDownCounter |            | The current number of busy threads        |
+| jetty.thread.idle.count | UpDownCounter |            | The current number of idle threads        |
+| jetty.thread.queue.size | UpDownCounter |            | The current job queue size                |
--- instrumentation/jmx-metrics/library/src/test/java/io/opentelemetry/instrumentation/jmx/rules/JettyIntegrationTest.java
+++ instrumentation/jmx-metrics/library/src/test/java/io/opentelemetry/instrumentation/jmx/rules/JettyIntegrationTest.java
@@ -34,19 +34,20 @@ public class JettyIntegrationTest extends TargetSystemTest {
 
     GenericContainer<?> container =
         new GenericContainer<>(
-                new ImageFromDockerfile()
-                    .withDockerfileFromBuilder(
-                        builder ->
-                            builder
-                                .from(DOCKER_IMAGE)
-                                .run(
-                                    "java",
-                                    "-jar",
-                                    "/usr/local/jetty/start.jar",
-                                    "--add-to-startd=jmx,stats,http")
-                                .run("mkdir -p /var/lib/jetty/webapps/ROOT/")
-                                .run("touch /var/lib/jetty/webapps/ROOT/index.html")
-                                .build()))
+            new ImageFromDockerfile()
+                .withDockerfileFromBuilder(
+                    builder ->
+                        builder
+                            .from(DOCKER_IMAGE)
+                            .run(
+                                "java",
+                                "-jar",
+                                "/usr/local/jetty/start.jar",
+                                // TODO: replace 'stats' with 'statistics' for jetty 12+
+                                "--add-to-startd=jmx,stats,http")
+                            .run("mkdir -p /var/lib/jetty/webapps/ROOT/")
+                            .run("touch /var/lib/jetty/webapps/ROOT/index.html")
+                            .build()))
             .withEnv("JAVA_OPTIONS", String.join(" ", jvmArgs))
             .withStartupTimeout(Duration.ofMinutes(2))
             .withExposedPorts(JETTY_PORT)
@@ -60,6 +61,22 @@ public class JettyIntegrationTest extends TargetSystemTest {
   }
 
   private static MetricsVerifier createMetricsVerifier() {
-    return MetricsVerifier.create().add("jetty.dummy.metric", metric -> metric.hasDescription("hello"));
+    return MetricsVerifier.create()
+        .add("jetty.thread.count", metric -> metric.hasDescription("The current number of threads")
+            .hasUnit("{thread}").hasDataPointsWithoutAttributes()
+            .isUpDownCounter())
+        .add("jetty.thread.limit", metric -> metric.hasDescription("The configured maximum number of threads in the pool")
+            .hasUnit("{thread}").hasDataPointsWithoutAttributes()
+            .isUpDownCounter())
+        .add("jetty.thread.idle.count", metric -> metric.hasDescription("The current number of idle threads")
+            .hasUnit("{thread}").hasDataPointsWithoutAttributes()
+            .isUpDownCounter())
+        .add("jetty.thread.busy.count", metric -> metric.hasDescription("The current number of busy threads")
+            .hasUnit("{thread}").hasDataPointsWithoutAttributes()
+            .isUpDownCounter())
+        .add("jetty.thread.queue.size", metric -> metric.hasDescription("The current job queue size")
+            .hasUnit("{thread}").hasDataPointsWithoutAttributes()
+            .isUpDownCounter())
+        ;
   }
 }
