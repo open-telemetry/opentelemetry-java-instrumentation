7d033ed7d367a796c4acf31166d736e430f3a8fb
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2025-06-18 10:57:23 +0200

    try to add jetty 12

--- instrumentation/jmx-metrics/library/jetty.md
+++ instrumentation/jmx-metrics/library/jetty.md
@@ -2,10 +2,11 @@
 
 Here is the list of metrics based on MBeans exposed by Jetty.
 
-| Metric Name             | Type          | Attributes | Description                               |
-|-------------------------|---------------|------------|-------------------------------------------|
-| jetty.thread.count      | UpDownCounter |            | The current number of threads             |
-| jetty.thread.limit      | UpDownCounter |            | The maximum number of threads in the pool |
-| jetty.thread.busy.count | UpDownCounter |            | The current number of busy threads        |
-| jetty.thread.idle.count | UpDownCounter |            | The current number of idle threads        |
-| jetty.thread.queue.size | UpDownCounter |            | The current job queue size                |
+| Metric Name             | Type          | Attributes                                 | Description                               |
+|-------------------------|---------------|--------------------------------------------|-------------------------------------------|
+| jetty.thread.count      | UpDownCounter | jetty.thread.pool.id                       | The current number of threads             |
+| jetty.thread.limit      | UpDownCounter | jetty.thread.pool.id                       | The maximum number of threads in the pool |
+| jetty.thread.busy.count | UpDownCounter | jetty.thread.pool.id                       | The current number of busy threads        |
+| jetty.thread.idle.count | UpDownCounter | jetty.thread.pool.id                       | The current number of idle threads        |
+| jetty.thread.queue.size | UpDownCounter | jetty.thread.pool.id                       | The current job queue size                |
+| jetty.io.select.count   | Counter       | jetty.selector.resource, jetty.selector.id | The number of select calls                |
--- instrumentation/jmx-metrics/library/src/test/java/io/opentelemetry/instrumentation/jmx/rules/JettyIntegrationTest.java
+++ instrumentation/jmx-metrics/library/src/test/java/io/opentelemetry/instrumentation/jmx/rules/JettyIntegrationTest.java
@@ -5,24 +5,27 @@
 
 package io.opentelemetry.instrumentation.jmx.rules;
 
+import static io.opentelemetry.instrumentation.jmx.rules.assertions.DataPointAttributes.attributeGroup;
+import static io.opentelemetry.instrumentation.jmx.rules.assertions.DataPointAttributes.attributeWithAnyValue;
+
+import io.opentelemetry.instrumentation.jmx.rules.assertions.AttributeMatcher;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.images.builder.ImageFromDockerfile;
 
 public class JettyIntegrationTest extends TargetSystemTest {
 
-  // TODO: test with multiple versions
-
-  private static final String DOCKER_IMAGE = "jetty:11";
   private static final int JETTY_PORT = 8080;
 
-  @Test
-  void testCollectedMetrics() {
+  @ParameterizedTest
+  @ValueSource(ints = {11, 12})
+  void testCollectedMetrics(int jettyMajorVersion) {
 
     List<String> yamlFiles = Collections.singletonList("jetty.yaml");
 
@@ -32,22 +35,29 @@ public class JettyIntegrationTest extends TargetSystemTest {
     jvmArgs.add(javaAgentJvmArgument());
     jvmArgs.addAll(javaPropertiesToJvmArgs(otelConfigProperties(yamlFiles)));
 
+    String jettyModules = "jmx,http,";
+    if (jettyMajorVersion >= 12) {
+      jettyModules += "statistics";
+    } else {
+      jettyModules += "stats";
+    }
+    String addModulesArg = "--add-to-startd=" + jettyModules;
+
     GenericContainer<?> container =
         new GenericContainer<>(
-            new ImageFromDockerfile()
-                .withDockerfileFromBuilder(
-                    builder ->
-                        builder
-                            .from(DOCKER_IMAGE)
-                            .run(
-                                "java",
-                                "-jar",
-                                "/usr/local/jetty/start.jar",
-                                // TODO: replace 'stats' with 'statistics' for jetty 12+
-                                "--add-to-startd=jmx,stats,http")
-                            .run("mkdir -p /var/lib/jetty/webapps/ROOT/")
-                            .run("touch /var/lib/jetty/webapps/ROOT/index.html")
-                            .build()))
+                new ImageFromDockerfile()
+                    .withDockerfileFromBuilder(
+                        builder ->
+                            builder
+                                .from("jetty:" + jettyMajorVersion)
+                                .run(
+                                    "java",
+                                    "-jar",
+                                    "/usr/local/jetty/start.jar",
+                                    addModulesArg)
+                                .run("mkdir -p /var/lib/jetty/webapps/ROOT/")
+                                .run("touch /var/lib/jetty/webapps/ROOT/index.html")
+                                .build()))
             .withEnv("JAVA_OPTIONS", String.join(" ", jvmArgs))
             .withStartupTimeout(Duration.ofMinutes(2))
             .withExposedPorts(JETTY_PORT)
@@ -61,22 +71,60 @@ public class JettyIntegrationTest extends TargetSystemTest {
   }
 
   private static MetricsVerifier createMetricsVerifier() {
+
+    AttributeMatcher threadPoolId = attributeWithAnyValue("jetty.thread.pool.id");
+
     return MetricsVerifier.create()
-        .add("jetty.thread.count", metric -> metric.hasDescription("The current number of threads")
-            .hasUnit("{thread}").hasDataPointsWithoutAttributes()
-            .isUpDownCounter())
-        .add("jetty.thread.limit", metric -> metric.hasDescription("The configured maximum number of threads in the pool")
-            .hasUnit("{thread}").hasDataPointsWithoutAttributes()
-            .isUpDownCounter())
-        .add("jetty.thread.idle.count", metric -> metric.hasDescription("The current number of idle threads")
-            .hasUnit("{thread}").hasDataPointsWithoutAttributes()
-            .isUpDownCounter())
-        .add("jetty.thread.busy.count", metric -> metric.hasDescription("The current number of busy threads")
-            .hasUnit("{thread}").hasDataPointsWithoutAttributes()
-            .isUpDownCounter())
-        .add("jetty.thread.queue.size", metric -> metric.hasDescription("The current job queue size")
-            .hasUnit("{thread}").hasDataPointsWithoutAttributes()
-            .isUpDownCounter())
-        ;
+        .add(
+            "jetty.thread.count",
+            metric ->
+                metric
+                    .hasDescription("The current number of threads")
+                    .hasUnit("{thread}")
+                    .hasDataPointsWithOneAttribute(threadPoolId)
+                    .isUpDownCounter())
+        .add(
+            "jetty.thread.limit",
+            metric ->
+                metric
+                    .hasDescription("The configured maximum number of threads in the pool")
+                    .hasUnit("{thread}")
+                    .hasDataPointsWithOneAttribute(threadPoolId)
+                    .isUpDownCounter())
+        .add(
+            "jetty.thread.idle.count",
+            metric ->
+                metric
+                    .hasDescription("The current number of idle threads")
+                    .hasUnit("{thread}")
+                    .hasDataPointsWithOneAttribute(threadPoolId)
+                    .isUpDownCounter())
+        .add(
+            "jetty.thread.busy.count",
+            metric ->
+                metric
+                    .hasDescription("The current number of busy threads")
+                    .hasUnit("{thread}")
+                    .hasDataPointsWithOneAttribute(threadPoolId)
+                    .isUpDownCounter())
+        .add(
+            "jetty.thread.queue.size",
+            metric ->
+                metric
+                    .hasDescription("The current job queue size")
+                    .hasUnit("{thread}")
+                    .hasDataPointsWithOneAttribute(threadPoolId)
+                    .isUpDownCounter())
+        .add(
+            "jetty.select.count",
+            metric ->
+                metric
+                    .hasDescription("The number of select calls")
+                    .hasUnit("{operation}")
+                    .hasDataPointsWithAttributes(
+                        attributeGroup(
+                            attributeWithAnyValue("jetty.selector.id"),
+                            attributeWithAnyValue("jetty.selector.context")))
+                    .isCounter());
   }
 }
