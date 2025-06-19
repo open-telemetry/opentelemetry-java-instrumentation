a47e8c0ed338142a0fb15dd5e4221981b0644473
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-06-19 13:31:38 +0200

    add test

---
+++ instrumentation/spring/spring-boot-autoconfigure/src/testDeclarativeConfig/java/io/opentelemetry/instrumentation/spring/autoconfigure/OpenTelemetryAutoConfigurationTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.instrumentation.spring.autoconfigure;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.opentelemetry.api.OpenTelemetry;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.context.annotation.Bean;
+
+/** Spring Boot auto configuration test for {@link OpenTelemetryAutoConfiguration}. */
+class OpenTelemetryAutoConfigurationTest {
+  @TestConfiguration
+  static class CustomTracerConfiguration {
+    @Bean
+    public OpenTelemetry customOpenTelemetry() {
+      return OpenTelemetry.noop();
+    }
+  }
+
+  private final ApplicationContextRunner contextRunner = new ApplicationContextRunner();
+
+  @Test
+  @DisplayName(
+      "when Application Context contains OpenTelemetry bean should NOT initialize openTelemetry")
+  void customOpenTelemetry() {
+    this.contextRunner
+        .withUserConfiguration(CustomTracerConfiguration.class)
+        .withConfiguration(AutoConfigurations.of(OpenTelemetryAutoConfiguration.class))
+        .run(
+            context ->
+                assertThat(context)
+                    .hasBean("customOpenTelemetry")
+                    .doesNotHaveBean("openTelemetry")
+                    .hasBean("otelProperties"));
+  }
+
+  @Test
+  @DisplayName(
+      "when Application Context DOES NOT contain OpenTelemetry bean should initialize openTelemetry")
+  void initializeProvidersAndOpenTelemetry() {
+    this.contextRunner
+        .withConfiguration(AutoConfigurations.of(OpenTelemetryAutoConfiguration.class))
+        .run(context -> assertThat(context).hasBean("openTelemetry").hasBean("otelProperties"));
+  }
+
+  @Test
+  void shouldInitializeSdkWhenNotDisabled() {
+    this.contextRunner
+        .withConfiguration(AutoConfigurations.of(OpenTelemetryAutoConfiguration.class))
+        .withPropertyValues("otel.sdk.disabled=false")
+        .run(
+            context -> assertThat(context).getBean("openTelemetry").isInstanceOf(OpenTelemetrySdk.class));
+  }
+
+  @Test
+  void shouldInitializeNoopOpenTelemetryWhenSdkIsDisabled() {
+    this.contextRunner
+        .withConfiguration(AutoConfigurations.of(OpenTelemetryAutoConfiguration.class))
+        .withPropertyValues(
+            "otel.sdk.disabled=true",
+            "otel.resource.attributes=service.name=workflow-backend-dev,service.version=3c8f9ce9")
+        .run(
+            context ->
+                assertThat(context).getBean("openTelemetry").isEqualTo(OpenTelemetry.noop()));
+  }
+}
