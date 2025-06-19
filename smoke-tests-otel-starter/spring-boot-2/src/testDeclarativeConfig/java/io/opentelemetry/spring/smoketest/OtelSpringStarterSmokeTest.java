e52abd3f73dd08c2b2769eabb5b6d95cb858954d
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-06-19 16:06:26 +0200

    add test

--- smoke-tests-otel-starter/spring-boot-2/src/testDeclarativeConfig/java/io/opentelemetry/spring/smoketest/OtelSpringStarterSmokeTest.java
+++ smoke-tests-otel-starter/spring-boot-2/src/testDeclarativeConfig/java/io/opentelemetry/spring/smoketest/OtelSpringStarterSmokeTest.java
@@ -16,7 +16,6 @@ import org.springframework.boot.test.context.SpringBootTest;
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
     properties = {
       // The headers are simply set here to make sure that headers can be parsed
-      "otel.exporter.otlp.headers.c=3",
-      "otel.instrumentation.runtime-telemetry.emit-experimental-telemetry=true",
+      "otel.instrumentation.runtime-telemetry.emit-experimental-telemetry=true",  // todo to file config
     })
 class OtelSpringStarterSmokeTest extends AbstractOtelSpringStarterSmokeTest {}
