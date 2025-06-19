e76e778388f147939c7a43b0e38db59f79f7300f
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-06-19 14:35:42 +0200

    add test

--- instrumentation/spring/spring-boot-autoconfigure/build.gradle.kts
+++ instrumentation/spring/spring-boot-autoconfigure/build.gradle.kts
@@ -190,17 +190,10 @@ testing {
     }
 
     val testDeclarativeConfig by registering(JvmTestSuite::class) {
-      otelJava {
-        minJavaVersionSupported.set(JavaVersion.VERSION_17)
-      }
       dependencies {
         implementation(project())
-        implementation("org.springframework.boot:spring-boot-starter-web:3.2.4")
         implementation("io.opentelemetry:opentelemetry-sdk")
-        implementation(project(":instrumentation:spring:spring-web:spring-web-3.1:library"))
-        implementation(project(":instrumentation:spring:spring-webmvc:spring-webmvc-6.0:library"))
-        implementation("jakarta.servlet:jakarta.servlet-api:5.0.0")
-        implementation("org.springframework.boot:spring-boot-starter-test:3.2.4") {
+        implementation("org.springframework.boot:spring-boot-starter-test:$springBootVersion") {
           exclude("org.junit.vintage", "junit-vintage-engine")
         }
       }
