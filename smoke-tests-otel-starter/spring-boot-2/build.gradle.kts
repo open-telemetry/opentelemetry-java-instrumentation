b19272e4504f353b56730db121ef61da950d7b4c
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-06-19 16:48:08 +0200

    add test

--- smoke-tests-otel-starter/spring-boot-2/build.gradle.kts
+++ smoke-tests-otel-starter/spring-boot-2/build.gradle.kts
@@ -38,6 +38,7 @@ testing {
       dependencies {
         implementation(project())
         implementation(project(":smoke-tests-otel-starter:spring-boot-common"))
+        implementation("org.springframework.boot:spring-boot-starter-web:2.6.15")
         implementation("org.springframework.boot:spring-boot-starter-test:2.6.15")
       }
     }
