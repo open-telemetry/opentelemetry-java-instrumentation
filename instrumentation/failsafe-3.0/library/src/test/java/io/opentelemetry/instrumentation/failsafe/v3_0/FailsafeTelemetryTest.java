3c126688c57cfc41f3740a4dac4fbf47d5c2034b
Onur Kayabasi <o.kayabasi@outlook.com>
2025-06-24 08:19:47 +0200

    PR comments addressed

--- instrumentation/failsafe-3.0/library/src/test/java/io/opentelemetry/instrumentation/failsafe/v3_0/FailsafeTelemetryTest.java
+++ instrumentation/failsafe-3.0/library/src/test/java/io/opentelemetry/instrumentation/failsafe/v3_0/FailsafeTelemetryTest.java
@@ -5,23 +5,89 @@
 
 package io.opentelemetry.instrumentation.failsafe.v3_0;
 
-import dev.failsafe.CircuitBreaker;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import dev.failsafe.CircuitBreakerOpenException;
+import dev.failsafe.Failsafe;
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.instrumentation.testing.junit.InstrumentationExtension;
 import io.opentelemetry.instrumentation.testing.junit.LibraryInstrumentationExtension;
+import io.opentelemetry.sdk.metrics.data.LongPointData;
+import io.opentelemetry.sdk.metrics.data.MetricData;
+import io.opentelemetry.sdk.testing.assertj.MetricAssert;
+import java.time.Duration;
+import java.util.Objects;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
-final class FailsafeTelemetryTest extends AbstractFailsafeTest {
+final class FailsafeTelemetryTest {
   @RegisterExtension
   static final InstrumentationExtension testing = LibraryInstrumentationExtension.create();
 
-  @Override
-  protected InstrumentationExtension testing() {
-    return testing;
+  @Nested
+  final class CircuitBreaker {
+    @Test
+    void should_Capture_CircuitBreaker_Metrics() {
+      // given
+      dev.failsafe.CircuitBreaker<Object> userCircuitBreaker =
+          dev.failsafe.CircuitBreaker.builder()
+              .handleResultIf(Objects::isNull)
+              .withFailureThreshold(2)
+              .withDelay(Duration.ZERO)
+              .withSuccessThreshold(2)
+              .build();
+      FailsafeTelemetry failsafeTelemetry = FailsafeTelemetry.create(testing.getOpenTelemetry());
+      dev.failsafe.CircuitBreaker<Object> instrumentedCircuitBreaker =
+          failsafeTelemetry.createCircuitBreaker(userCircuitBreaker, "testing");
+
+      // when
+      for (int i = 0; i < 5; i++) {
+        try {
+          int temp = i;
+          Failsafe.with(instrumentedCircuitBreaker)
+              .get(
+                  () -> {
+                    if (temp < 2) {
+                      return null;
+                    } else {
+                      return new Object();
+                    }
+                  });
+        } catch (CircuitBreakerOpenException e) {
+          assertThat(i, equalTo(2));
+        }
+      }
+
+      // then
+      testing.waitAndAssertMetrics(
+          "io.opentelemetry.failsafe-3.0",
+          metricAssert ->
+              assertCircuitBreakerMetric(metricAssert, "failsafe.circuitbreaker.failure.count", 2),
+          metricAssert ->
+              assertCircuitBreakerMetric(metricAssert, "failsafe.circuitbreaker.success.count", 3),
+          metricAssert ->
+              assertCircuitBreakerMetric(metricAssert, "failsafe.circuitbreaker.open.count", 1),
+          metricAssert ->
+              assertCircuitBreakerMetric(metricAssert, "failsafe.circuitbreaker.halfopen.count", 1),
+          metricAssert ->
+              assertCircuitBreakerMetric(metricAssert, "failsafe.circuitbreaker.closed.count", 1));
+    }
   }
 
-  @Override
-  protected CircuitBreaker<Object> configure(CircuitBreaker<Object> circuitBreaker) {
-    return FailsafeTelemetry.create(testing.getOpenTelemetry())
-        .createCircuitBreaker(circuitBreaker, "testing");
+  private static void assertCircuitBreakerMetric(
+      MetricAssert metricAssert, String counterName, long expectedValue) {
+    MetricData closeCountData = metricAssert.actual();
+    assertThat(closeCountData.getName(), equalTo(counterName));
+    assertTrue(closeCountData.getData().getPoints().stream().findFirst().isPresent());
+    LongPointData closeCountLongPointData =
+        (LongPointData) closeCountData.getData().getPoints().stream().findFirst().get();
+    assertThat(
+        closeCountLongPointData.getAttributes(),
+        equalTo(Attributes.of(AttributeKey.stringKey("name"), "testing")));
+    assertThat(closeCountLongPointData.getValue(), equalTo(expectedValue));
   }
 }
--- instrumentation/failsafe-3.0/testing/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-  id("otel.java-conventions")
-}
-
-dependencies {
-  api(project(":testing-common"))
-
-  api("dev.failsafe:failsafe:3.0.1")
-}
