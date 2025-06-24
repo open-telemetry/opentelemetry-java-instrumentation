e1d9749c95e089527726838761ea4169ca496daa
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-06-24 15:08:41 +0200

    make agent tracer available for spring starter

---
+++ instrumentation/spring/spring-boot-autoconfigure/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/internal/instrumentation/threaddetails/AgentTracerProviderConfigurer.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.instrumentation.spring.autoconfigure.internal.instrumentation.threaddetails;
+
+import io.opentelemetry.instrumentation.spring.autoconfigure.internal.ConditionalOnEnabledInstrumentation;
+import io.opentelemetry.instrumentation.tracer.AddThreadDetailsSpanProcessor;
+import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * This class is internal and is hence not for public use. Its APIs are unstable and can change at
+ * any time.
+ */
+@ConditionalOnEnabledInstrumentation(module = "thread-details")
+@Configuration
+@SuppressWarnings("OtelPrivateConstructorForUtilityClass")
+public class AgentTracerProviderConfigurer {
+
+  @Bean
+  public AutoConfigurationCustomizerProvider otelCustomizer() {
+    return p ->
+        p.addTracerProviderCustomizer(
+            (builder, config) -> {
+              builder.addSpanProcessor(new AddThreadDetailsSpanProcessor());
+              return builder;
+            });
+  }
+}
