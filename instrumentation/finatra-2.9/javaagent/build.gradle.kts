c6f8dd52258b8afa493a4908fd16013daa93f914
Jay DeLuca <jaydeluca4@gmail.com>
2025-06-25 07:53:02 -0400

    more

--- instrumentation/finatra-2.9/javaagent/build.gradle.kts
+++ instrumentation/finatra-2.9/javaagent/build.gradle.kts
@@ -86,5 +86,10 @@ tasks {
     jvmArgs("--add-opens=java.base/java.lang=ALL-UNNAMED")
     jvmArgs("-XX:+IgnoreUnrecognizedVMOptions")
     jvmArgs("-Dotel.instrumentation.common.experimental.controller-telemetry.enabled=true")
+
+    systemProperty("metaDataConfig", "otel.instrumentation.common.experimental.controller-telemetry.enabled=true")
+
+    systemProperty("collectMetadata", findProperty("collectMetadata")?.toString() ?: "false")
+    systemProperty("collectSpans", true)
   }
 }
