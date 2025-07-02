afff563b2b5a538a2ccc9d082411557b6b8eb925
Robert Munteanu <robert@lmn.ro>
2025-07-02 13:47:44 +0200

    sling: WIP for testing
    Add a new http-based test for Apache Sling based on the latest released Sling Started demo
    application.
    
    This commit shows how testing should work but it has a number of issues:
    - ignores advice failures
    - ignores muzzle failures
    - does not clean up the existing Sling instance
    - the code that starts up the Sling instance is fragile

--- instrumentation/sling/javaagent/build.gradle.kts
+++ instrumentation/sling/javaagent/build.gradle.kts
@@ -9,4 +9,5 @@ dependencies {
   api(project(":instrumentation:servlet:servlet-common:javaagent"))
   compileOnly("javax.servlet:javax.servlet-api:3.1.0")
   library("org.apache.sling:org.apache.sling.api:2.0.6") // first non-incubator release
+  testLibrary("org.apache.sling:org.apache.sling.feature.launcher:1.3.2")
 }
