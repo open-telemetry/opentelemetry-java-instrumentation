69fc94f0d97e4e82b7ba777016eb208dca5f708a
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-07-04 17:47:27 +0200

    early init config

---
+++ javaagent-tooling/src/main/java/io/opentelemetry/javaagent/tooling/config/LegacyConfigFileEarlyInitAgentConfig.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.tooling.config;
+
+import io.opentelemetry.instrumentation.api.internal.ConfigPropertiesUtil;
+import java.util.Map;
+import javax.annotation.Nullable;
+
+/**
+ * Agent config class that is only supposed to be used before the SDK (and {@link
+ * io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties}) is initialized.
+ */
+public final class LegacyConfigFileEarlyInitAgentConfig implements EarlyInitAgentConfig {
+
+  private final Map<String, String> configFileContents;
+
+  LegacyConfigFileEarlyInitAgentConfig() {
+    this.configFileContents = ConfigurationFile.getProperties();
+  }
+
+  @Nullable
+  @Override
+  public String getString(String propertyName) {
+    String value = ConfigPropertiesUtil.getString(propertyName);
+    if (value != null) {
+      return value;
+    }
+    return configFileContents.get(propertyName);
+  }
+
+  @Override
+  public boolean getBoolean(String propertyName, boolean defaultValue) {
+    String configFileValueStr = configFileContents.get(propertyName);
+    boolean configFileValue =
+        configFileValueStr == null ? defaultValue : Boolean.parseBoolean(configFileValueStr);
+    return ConfigPropertiesUtil.getBoolean(propertyName, configFileValue);
+  }
+
+  @Override
+  public int getInt(String propertyName, int defaultValue) {
+    try {
+      String configFileValueStr = configFileContents.get(propertyName);
+      int configFileValue =
+          configFileValueStr == null ? defaultValue : Integer.parseInt(configFileValueStr);
+      return ConfigPropertiesUtil.getInt(propertyName, configFileValue);
+    } catch (NumberFormatException ignored) {
+      return defaultValue;
+    }
+  }
+
+  @Override
+  public void logEarlyConfigErrorsIfAny() {
+    ConfigurationFile.logErrorIfAny();
+  }
+}
