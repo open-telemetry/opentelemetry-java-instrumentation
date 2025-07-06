e713b98ececed4157880868dea767bed49c76f3b
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-07-06 09:11:58 +0200

    map otel.javaagent.debug

--- javaagent-extension-api/src/main/java/io/opentelemetry/javaagent/extension/internal/DeclarativeConfigPropertiesBridge.java
+++ javaagent-extension-api/src/main/java/io/opentelemetry/javaagent/extension/internal/DeclarativeConfigPropertiesBridge.java
@@ -83,6 +83,8 @@ public final class DeclarativeConfigPropertiesBridge implements ConfigProperties
         "http.server.response_captured_headers");
   }
 
+  private final String logLevel;
+
   private static Map<String, String> getPeerServiceMapping(
       DeclarativeConfigPropertiesBridge bridge) {
     List<DeclarativeConfigProperties> configProperties =
@@ -97,7 +99,8 @@ public final class DeclarativeConfigPropertiesBridge implements ConfigProperties
                 e -> Objects.requireNonNull(e.getString("service"), "service must not be null")));
   }
 
-  public DeclarativeConfigPropertiesBridge(ConfigProvider configProvider) {
+  public DeclarativeConfigPropertiesBridge(ConfigProvider configProvider, String logLevel) {
+    this.logLevel = logLevel;
     DeclarativeConfigProperties inst = configProvider.getInstrumentationConfig();
     if (inst == null) {
       inst = DeclarativeConfigProperties.empty();
@@ -119,6 +122,10 @@ public final class DeclarativeConfigPropertiesBridge implements ConfigProperties
   @Nullable
   @Override
   public Boolean getBoolean(String propertyName) {
+    if ("otel.javaagent.debug".equals(propertyName)) {
+      return "DEBUG".equals(this.logLevel);
+    }
+
     return getPropertyValue(propertyName, DeclarativeConfigProperties::getBoolean);
   }
 
