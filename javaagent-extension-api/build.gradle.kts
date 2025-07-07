4f1d44315b737a036551991407659edb209118e0
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-07-07 14:55:19 +0200

    fix rebase

--- javaagent-extension-api/build.gradle.kts
+++ javaagent-extension-api/build.gradle.kts
@@ -13,11 +13,6 @@ dependencies {
 
   implementation(project(":instrumentation-api"))
   implementation(project(":instrumentation-api-incubator"))
-  implementation(project(":sdk-bridge")) {
-    // is part of bootstrap classpath
-    exclude(group = "io.opentelemetry", module = "opentelemetry-api-incubator")
-    exclude(group = "io.opentelemetry", module = "opentelemetry-sdk-extension-autoconfigure-spi")
-  }
 
   // autoconfigure is unstable, do not expose as api
   implementation("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure")
