72f2be0aa838fa253ce6b602b5606305d5feb424
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2025-07-08 14:38:10 +0200

    servlet-3.0

--- instrumentation/servlet/servlet-3.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/servlet/v3_0/AsyncDispatchAdvice.java
+++ instrumentation/servlet/servlet-3.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/servlet/v3_0/AsyncDispatchAdvice.java
@@ -19,13 +19,11 @@ import net.bytebuddy.asm.Advice;
 public class AsyncDispatchAdvice {
 
   @Advice.OnMethodEnter(suppress = Throwable.class)
-  public static void enter(
-      @Advice.This AsyncContext context,
-      @Advice.AllArguments Object[] args,
-      @Advice.Local("otelCallDepth") CallDepth callDepth) {
-    callDepth = CallDepth.forClass(AsyncContext.class);
+  public static CallDepth enter(
+      @Advice.This AsyncContext context, @Advice.AllArguments Object[] args) {
+    CallDepth callDepth = CallDepth.forClass(AsyncContext.class);
     if (callDepth.getAndIncrement() > 0) {
-      return;
+      return callDepth;
     }
 
     ServletRequest request = context.getRequest();
@@ -42,10 +40,11 @@ public class AsyncDispatchAdvice {
       // processing, and nothing can be done with the request anymore after this
       request.setAttribute(CONTEXT_ATTRIBUTE, currentContext);
     }
+    return callDepth;
   }
 
   @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
-  public static void exit(@Advice.Local("otelCallDepth") CallDepth callDepth) {
+  public static void exit(@Advice.Enter CallDepth callDepth) {
     callDepth.decrementAndGet();
   }
 }
