6fc47d730a5d8c4913216ab07b68fcdd90f8e5bd
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-07-18 14:31:33 +0200

    use unified config bridge

--- javaagent-tooling/src/main/java/io/opentelemetry/javaagent/tooling/OpenTelemetryInstaller.java
+++ javaagent-tooling/src/main/java/io/opentelemetry/javaagent/tooling/OpenTelemetryInstaller.java
@@ -7,7 +7,7 @@ package io.opentelemetry.javaagent.tooling;
 
 import io.opentelemetry.api.incubator.config.ConfigProvider;
 import io.opentelemetry.javaagent.bootstrap.OpenTelemetrySdkAccess;
-import io.opentelemetry.javaagent.extension.internal.DeclarativeConfigPropertiesBridge;
+import io.opentelemetry.javaagent.extension.internal.ConfigPropertiesUtil;
 import io.opentelemetry.javaagent.tooling.config.EarlyInitAgentConfig;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
@@ -16,8 +16,6 @@ import io.opentelemetry.sdk.autoconfigure.internal.AutoConfigureUtil;
 import io.opentelemetry.sdk.common.CompletableResultCode;
 import io.opentelemetry.sdk.resources.Resource;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 public final class OpenTelemetryInstaller {
 
@@ -44,23 +42,25 @@ public final class OpenTelemetryInstaller {
       return SdkAutoconfigureAccess.create(
           sdk,
           Resource.getDefault(),
-          new DeclarativeConfigPropertiesBridge(
-              configProvider, getEarlyInitProperties(earlyConfig)),
+          ConfigPropertiesUtil.resolveInstrumentationConfig(
+              configProvider.getInstrumentationConfig(),
+              ConfigPropertiesUtil.propertyTranslatorBuilder()
+                  .addTranslation(
+                      "otel.instrumentation.common.default-enabled", "common.default.enabled")
+                  .addTranslation("otel.javaagent", "agent")
+                  // these properties are used to initialize the SDK before the configuration file
+                  // is loaded
+                  // for consistency, we pass them to the bridge, so that they can be read later
+                  // with the same
+                  // value from the {@link DeclarativeConfigPropertiesBridge}
+                  .addFixedValue(
+                      "otel.javaagent.debug", earlyConfig.getBoolean("otel.javaagent.debug", false))
+                  .addFixedValue(
+                      "otel.javaagent.logging", earlyConfig.getString("otel.javaagent.logging"))),
           configProvider);
     }
 
     return autoConfiguredSdk;
-  }
-
-  // these properties are used to initialize the SDK before the configuration file is loaded
-  // for consistency, we pass them to the bridge, so that they can be read later with the same
-  // value from the {@link DeclarativeConfigPropertiesBridge}
-  private static Map<String, Object> getEarlyInitProperties(EarlyInitAgentConfig config) {
-    Map<String, Object> earlyInitProperties = new HashMap<>();
-    earlyInitProperties.put(
-        "otel.javaagent.debug", config.getBoolean("otel.javaagent.debug", false));
-    earlyInitProperties.put("otel.javaagent.logging", config.getString("otel.javaagent.logging"));
-    return earlyInitProperties;
   }
 
   private static void setForceFlush(OpenTelemetrySdk sdk) {
