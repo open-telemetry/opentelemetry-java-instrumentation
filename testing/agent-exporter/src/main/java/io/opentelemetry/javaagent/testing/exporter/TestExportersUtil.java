af324184627481f925498b83f3a129fa6e14ae03
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-07-22 10:31:21 +0200

    fix rebase

--- testing/agent-exporter/src/main/java/io/opentelemetry/javaagent/testing/exporter/TestExportersUtil.java
+++ testing/agent-exporter/src/main/java/io/opentelemetry/javaagent/testing/exporter/TestExportersUtil.java
@@ -5,18 +5,18 @@
 
 package io.opentelemetry.javaagent.testing.exporter;
 
-import io.opentelemetry.javaagent.testing.provider.TestLogRecordExporterComponentProvider;
-import io.opentelemetry.javaagent.testing.provider.TestMetricExporterComponentProvider;
-import io.opentelemetry.javaagent.testing.provider.TestSpanExporterComponentProvider;
+import io.opentelemetry.javaagent.testing.provider.AgentTestLogRecordExporterComponentProvider;
+import io.opentelemetry.javaagent.testing.provider.AgentTestMetricExporterComponentProvider;
+import io.opentelemetry.javaagent.testing.provider.AgentTestSpanExporterComponentProvider;
 
-class TestExportersUtil {
+public class TestExportersUtil {
   private TestExportersUtil() {}
 
-  static void initTestExporters() {
-    TestSpanExporterComponentProvider.setSpanExporter(AgentTestingExporterFactory.spanExporter);
-    TestMetricExporterComponentProvider.setMetricExporter(
+  public static void initTestExporters() {
+    AgentTestSpanExporterComponentProvider.setSpanExporter(AgentTestingExporterFactory.spanExporter);
+    AgentTestMetricExporterComponentProvider.setMetricExporter(
         AgentTestingExporterFactory.metricExporter);
-    TestLogRecordExporterComponentProvider.setLogRecordExporter(
+    AgentTestLogRecordExporterComponentProvider.setLogRecordExporter(
         AgentTestingExporterFactory.logExporter);
   }
 }
