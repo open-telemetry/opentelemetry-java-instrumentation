91d61c40fc6ee623785c4004f67e67ede8c371f9
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-07-24 20:54:35 +0200

    Revert "pr review"
    This reverts commit 643b8f8b2206b8b182a18b0c23b2c94ee9d71a52.

--- javaagent-extension-api/src/main/java/io/opentelemetry/javaagent/extension/internal/ConfigPropertiesTranslator.java
+++ javaagent-extension-api/src/main/java/io/opentelemetry/javaagent/extension/internal/ConfigPropertiesTranslator.java
@@ -7,7 +7,6 @@ package io.opentelemetry.javaagent.extension.internal;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
-import javax.annotation.Nullable;
 
 @SuppressWarnings("NonApiType")
 class ConfigPropertiesTranslator {
@@ -30,9 +29,8 @@ class ConfigPropertiesTranslator {
     return property;
   }
 
-  @SuppressWarnings("unchecked")
   @Nullable
-  public <T> T get(String propertyName) {
-    return (T) fixedValues.get(propertyName);
+  public Object get(String propertyName) {
+    return fixedValues.get(propertyName);
   }
 }
