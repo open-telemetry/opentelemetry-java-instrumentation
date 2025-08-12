a4626e73532dbc61734ee10cd4d5613acb439f77
robsunday <rniedziela@splunk.com>
2025-08-12 12:49:15 +0200

    Script fixes

--- instrumentation/jmx-metrics/library/src/test/resources/hadoop3-env.sh
+++ instrumentation/jmx-metrics/library/src/test/resources/hadoop3-env.sh
@@ -53,7 +53,8 @@
 # The java implementation to use. By default, this environment
 # variable is REQUIRED on ALL platforms except OS X!
 # export JAVA_HOME=
-export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
+JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
+export JAVA_HOME
 
 # Location of Hadoop.  By default, Hadoop will attempt to determine
 # this location based upon its execution path.
