8ec03dc0fb31b87757e38a4db83a9e710bbac1b7
Jay DeLuca <jaydeluca4@gmail.com>
2025-08-12 08:16:07 -0400

    Update smoke test images

--- smoke-tests/src/test/java/io/opentelemetry/smoketest/windows/WindowsTestContainerManager.java
+++ smoke-tests/src/test/java/io/opentelemetry/smoketest/windows/WindowsTestContainerManager.java
@@ -78,7 +78,7 @@ public class WindowsTestContainerManager extends AbstractTestContainerManager {
             .getId();
 
     String backendImageName =
-        "ghcr.io/open-telemetry/opentelemetry-java-instrumentation/smoke-test-fake-backend-windows:20221127.3559314891";
+        "ghcr.io/open-telemetry/opentelemetry-java-instrumentation/smoke-test-fake-backend-windows:20250811.16876216352";
     if (!imageExists(backendImageName)) {
       pullImage(backendImageName);
     }
