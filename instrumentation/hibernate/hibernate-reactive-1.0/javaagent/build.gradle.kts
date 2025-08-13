901e0ec9531e9bf85621142f3a0c66dc5c4e28b2
Lauri Tulmin <ltulmin@splunk.com>
2025-08-13 14:50:55 +0300

    Fix tests for hibernate reactive 4 (#14422)

--- instrumentation/hibernate/hibernate-reactive-1.0/javaagent/build.gradle.kts
+++ instrumentation/hibernate/hibernate-reactive-1.0/javaagent/build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
 
   testInstrumentation(project(":instrumentation:netty:netty-4.1:javaagent"))
   testInstrumentation(project(":instrumentation:vertx:vertx-sql-client:vertx-sql-client-4.0:javaagent"))
+  testInstrumentation(project(":instrumentation:vertx:vertx-sql-client:vertx-sql-client-5.0:javaagent"))
 
   library("io.vertx:vertx-sql-client:4.4.2")
   compileOnly("io.vertx:vertx-codegen:4.4.2")
@@ -45,12 +46,28 @@ testing {
     val hibernateReactive2Test by registering(JvmTestSuite::class) {
       dependencies {
         implementation("org.testcontainers:testcontainers")
+        implementation(project(":instrumentation:hibernate:hibernate-reactive-1.0:hibernate-reactive-2.0-testing"))
         if (latestDepTest) {
-          implementation("org.hibernate.reactive:hibernate-reactive-core:latest.release")
+          implementation("org.hibernate.reactive:hibernate-reactive-core:3.+")
           implementation("io.vertx:vertx-pg-client:4.+")
         } else {
           implementation("org.hibernate.reactive:hibernate-reactive-core:2.0.0.Final")
           implementation("io.vertx:vertx-pg-client:4.4.2")
+        }
+        compileOnly("io.vertx:vertx-codegen:4.4.2")
+      }
+    }
+
+    val hibernateReactive4Test by registering(JvmTestSuite::class) {
+      dependencies {
+        implementation("org.testcontainers:testcontainers")
+        implementation(project(":instrumentation:hibernate:hibernate-reactive-1.0:hibernate-reactive-2.0-testing"))
+        if (latestDepTest) {
+          implementation("org.hibernate.reactive:hibernate-reactive-core:latest.release")
+          implementation("io.vertx:vertx-pg-client:latest.release")
+        } else {
+          implementation("org.hibernate.reactive:hibernate-reactive-core:4.0.0.Final")
+          implementation("io.vertx:vertx-pg-client:5.0.0")
         }
         compileOnly("io.vertx:vertx-codegen:4.4.2")
       }
@@ -65,6 +82,9 @@ tasks {
   named("compileHibernateReactive2TestJava", JavaCompile::class).configure {
     options.release.set(11)
   }
+  named("compileHibernateReactive4TestJava", JavaCompile::class).configure {
+    options.release.set(17)
+  }
   val testJavaVersion =
     gradle.startParameter.projectProperties.get("testJavaVersion")?.let(JavaVersion::toVersion)
       ?: JavaVersion.current()
@@ -76,6 +96,11 @@ tasks {
       named("hibernateReactive1Test", Test::class).configure {
         enabled = false
       }
+    }
+  }
+  if (testJavaVersion.isJava8 || testJavaVersion.isJava11) {
+    named("hibernateReactive4Test", Test::class).configure {
+      enabled = false
     }
   }
 
--- instrumentation/hibernate/hibernate-reactive-1.0/javaagent/src/hibernateReactive4Test/java/io/opentelemetry/javaagent/instrumentation/hibernate/reactive/v4_0/HibernateReactiveTest.java
+++ instrumentation/hibernate/hibernate-reactive-1.0/javaagent/src/hibernateReactive4Test/java/io/opentelemetry/javaagent/instrumentation/hibernate/reactive/v4_0/HibernateReactiveTest.java
@@ -3,8 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.opentelemetry.javaagent.instrumentation.hibernate.reactive.v2_0;
+package io.opentelemetry.javaagent.instrumentation.hibernate.reactive.v4_0;
 
+import io.opentelemetry.javaagent.instrumentation.hibernate.reactive.v2_0.AbstractHibernateReactiveTest;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import java.util.concurrent.TimeUnit;
@@ -15,8 +16,7 @@ class HibernateReactiveTest extends AbstractHibernateReactiveTest {
   protected EntityManagerFactory createEntityManagerFactory() throws Exception {
     return vertx
         .getOrCreateContext()
-        .<EntityManagerFactory>executeBlocking(
-            promise -> promise.complete(Persistence.createEntityManagerFactory("test-pu")))
+        .executeBlocking(() -> Persistence.createEntityManagerFactory("test-pu"))
         .toCompletionStage()
         .toCompletableFuture()
         .get(30, TimeUnit.SECONDS);
