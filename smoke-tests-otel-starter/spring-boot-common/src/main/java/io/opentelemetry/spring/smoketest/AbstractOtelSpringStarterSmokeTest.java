373524392835376c4d7b719b68c63619590fc42c
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-08-16 12:05:53 +0200

    add thread details to spring starter

--- smoke-tests-otel-starter/spring-boot-common/src/main/java/io/opentelemetry/spring/smoketest/AbstractOtelSpringStarterSmokeTest.java
+++ smoke-tests-otel-starter/spring-boot-common/src/main/java/io/opentelemetry/spring/smoketest/AbstractOtelSpringStarterSmokeTest.java
@@ -30,6 +30,7 @@ import io.opentelemetry.semconv.UrlAttributes;
 import io.opentelemetry.semconv.incubating.CodeIncubatingAttributes;
 import io.opentelemetry.semconv.incubating.DbIncubatingAttributes;
 import io.opentelemetry.semconv.incubating.ServiceIncubatingAttributes;
+import io.opentelemetry.semconv.incubating.ThreadIncubatingAttributes;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -37,6 +38,7 @@ import java.util.Collections;
 import java.util.List;
 import org.assertj.core.api.AbstractCharSequenceAssert;
 import org.assertj.core.api.AbstractIterableAssert;
+import org.assertj.core.api.AbstractLongAssert;
 import org.assertj.core.api.MapAssert;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
@@ -180,9 +182,7 @@ class AbstractOtelSpringStarterSmokeTest extends AbstractSpringStarterSmokeTest 
                                 UrlAttributes.URL_FULL,
                                 stringAssert -> stringAssert.endsWith("/ping")),
                             equalTo(ServerAttributes.SERVER_ADDRESS, "localhost"),
-                            satisfies(
-                                ServerAttributes.SERVER_PORT,
-                                integerAssert -> integerAssert.isNotZero())),
+                            satisfies(ServerAttributes.SERVER_PORT, AbstractLongAssert::isNotZero)),
                 serverSpan ->
                     HttpSpanDataAssert.create(serverSpan)
                         .assertServerGetRequest("/ping")
@@ -207,10 +207,14 @@ class AbstractOtelSpringStarterSmokeTest extends AbstractSpringStarterSmokeTest 
                             equalTo(
                                 AttributeKey.stringArrayKey("http.request.header.key"),
                                 Collections.singletonList("value")),
+                            satisfies(ServerAttributes.SERVER_PORT, AbstractLongAssert::isNotZero),
                             satisfies(
-                                ServerAttributes.SERVER_PORT,
-                                integerAssert -> integerAssert.isNotZero())),
-                span -> withSpanAssert(span)));
+                                ThreadIncubatingAttributes.THREAD_ID,
+                                AbstractLongAssert::isNotZero),
+                            satisfies(
+                                ThreadIncubatingAttributes.THREAD_NAME,
+                                AbstractCharSequenceAssert::isNotBlank)),
+                AbstractSpringStarterSmokeTest::withSpanAssert));
 
     // Metric
     testing.waitAndAssertMetrics(
@@ -309,7 +313,7 @@ class AbstractOtelSpringStarterSmokeTest extends AbstractSpringStarterSmokeTest 
                 span -> HttpSpanDataAssert.create(span).assertClientGetRequest("/ping"),
                 span ->
                     span.hasKind(SpanKind.SERVER).hasAttribute(HttpAttributes.HTTP_ROUTE, "/ping"),
-                span -> withSpanAssert(span)));
+                AbstractSpringStarterSmokeTest::withSpanAssert));
   }
 
   @Test
