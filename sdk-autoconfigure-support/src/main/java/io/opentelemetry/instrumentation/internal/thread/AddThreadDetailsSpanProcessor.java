759bae89e09cd95f7d85e89c24257d753819d40f
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-08-16 12:05:53 +0200

    add thread details to spring starter

--- sdk-autoconfigure-support/src/main/java/io/opentelemetry/instrumentation/internal/thread/AddThreadDetailsSpanProcessor.java
+++ sdk-autoconfigure-support/src/main/java/io/opentelemetry/instrumentation/internal/thread/AddThreadDetailsSpanProcessor.java
@@ -3,22 +3,33 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.opentelemetry.javaagent.tooling;
+package io.opentelemetry.instrumentation.internal.thread;
 
+import static io.opentelemetry.api.common.AttributeKey.longKey;
+import static io.opentelemetry.api.common.AttributeKey.stringKey;
+
+import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.sdk.common.CompletableResultCode;
 import io.opentelemetry.sdk.trace.ReadWriteSpan;
 import io.opentelemetry.sdk.trace.ReadableSpan;
 import io.opentelemetry.sdk.trace.SpanProcessor;
-import io.opentelemetry.semconv.incubating.ThreadIncubatingAttributes;
 
+/**
+ * This class is internal and is hence not for public use. Its APIs are unstable and can change at
+ * any time.
+ */
 public class AddThreadDetailsSpanProcessor implements SpanProcessor {
+
+  // attributes are not stable yet
+  public static final AttributeKey<Long> THREAD_ID = longKey("thread.id");
+  public static final AttributeKey<String> THREAD_NAME = stringKey("thread.name");
 
   @Override
   public void onStart(Context context, ReadWriteSpan span) {
     Thread currentThread = Thread.currentThread();
-    span.setAttribute(ThreadIncubatingAttributes.THREAD_ID, currentThread.getId());
-    span.setAttribute(ThreadIncubatingAttributes.THREAD_NAME, currentThread.getName());
+    span.setAttribute(THREAD_ID, currentThread.getId());
+    span.setAttribute(THREAD_NAME, currentThread.getName());
   }
 
   @Override
