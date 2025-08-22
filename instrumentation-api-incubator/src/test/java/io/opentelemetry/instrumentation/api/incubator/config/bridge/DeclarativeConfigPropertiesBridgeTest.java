4ee8844b21e19a2f18704fc2e0eccba02af498b8
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-08-22 09:02:54 +0200

    move declarative config bridge

--- instrumentation-api-incubator/src/test/java/io/opentelemetry/instrumentation/api/incubator/config/bridge/DeclarativeConfigPropertiesBridgeTest.java
+++ instrumentation-api-incubator/src/test/java/io/opentelemetry/instrumentation/api/incubator/config/bridge/DeclarativeConfigPropertiesBridgeTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.opentelemetry.javaagent.extension.internal;
+package io.opentelemetry.instrumentation.api.incubator.config.bridge;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
