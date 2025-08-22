c5bf4e6e93e9ff249f0fcbe371df601fe96f4c48
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-08-22 14:46:23 +0200

    add context

--- testing-common/integration-tests/src/test/java/instrumentation/AgentInstrumentationTest.java
+++ testing-common/integration-tests/src/test/java/instrumentation/AgentInstrumentationTest.java
@@ -50,10 +50,14 @@ class AgentInstrumentationTest {
     for (ClassPath.ClassInfo info : getTestClasspath().getAllClasses()) {
       for (String bootstrapPrefix : BOOTSTRAP_PACKAGE_PREFIXES) {
         if (info.getName().startsWith(bootstrapPrefix)) {
-          Class<?> bootstrapClass = Class.forName(info.getName());
-          ClassLoader loader = bootstrapClass.getClassLoader();
-          if (loader != BOOTSTRAP_CLASSLOADER) {
-            bootstrapClassesIncorrectlyLoaded.add(bootstrapClass);
+          try {
+            Class<?> bootstrapClass = Class.forName(info.getName());
+            ClassLoader loader = bootstrapClass.getClassLoader();
+            if (loader != BOOTSTRAP_CLASSLOADER) {
+              bootstrapClassesIncorrectlyLoaded.add(bootstrapClass);
+            }
+          } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Failed to load bootstrap class: " + info.getName() + " in " + bootstrapPrefix, e);
           }
         }
       }
