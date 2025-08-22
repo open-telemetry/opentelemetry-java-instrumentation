74bce68e2a28a85824b8d4e2dc76ee02151f974f
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-08-22 07:07:11 +0200

    separate resource extraction from supported interfaces

---
+++ instrumentation/resources/library/src/main/java9/io/opentelemetry/instrumentation/resources/internal/ProcessArguments.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.instrumentation.resources.internal;
+
+/**
+ * This class is internal and is hence not for public use. Its APIs are unstable and can change
+ * at any time.
+ */
+public final class ProcessArguments {
+
+  public static String[] getProcessArguments() {
+    return ProcessHandle.current().info().arguments().orElseGet(() -> new String[0]);
+  }
+
+  private ProcessArguments() {}
+}
