71fdb863b4ff2bed1159a3618647127c47fe504f
Michael Munch <mmu@bankdata.dk>
2025-08-26 20:05:15 +0200

    Added url template

--- instrumentation/spring/spring-webflux/spring-webflux-5.3/library/src/main/java/io/opentelemetry/instrumentation/spring/webflux/v5_3/internal/WebClientHttpAttributesGetter.java
+++ instrumentation/spring/spring-webflux/spring-webflux-5.3/library/src/main/java/io/opentelemetry/instrumentation/spring/webflux/v5_3/internal/WebClientHttpAttributesGetter.java
@@ -7,19 +7,40 @@ package io.opentelemetry.instrumentation.spring.webflux.v5_3.internal;
 
 import static java.util.Collections.emptyList;
 
+import io.opentelemetry.instrumentation.api.incubator.semconv.http.HttpClientExperimentalAttributesGetter;
 import io.opentelemetry.instrumentation.api.semconv.http.HttpClientAttributesGetter;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
 import javax.annotation.Nullable;
 import org.springframework.web.reactive.function.client.ClientRequest;
 import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.WebClient;
 
 /**
  * This class is internal and is hence not for public use. Its APIs are unstable and can change at
  * any time.
  */
 public enum WebClientHttpAttributesGetter
-    implements HttpClientAttributesGetter<ClientRequest, ClientResponse> {
+    implements HttpClientExperimentalAttributesGetter<ClientRequest, ClientResponse> {
   INSTANCE;
+
+
+  private static final String URI_TEMPLATE_ATTRIBUTE = WebClient.class.getName() + ".uriTemplate";
+  private static final Pattern PATTERN_BEFORE_PATH = Pattern.compile("^https?://[^/]+/");
+
+  @Nullable
+  @Override
+  public String getUrlTemplate(ClientRequest clientRequest) {
+    Map<String, Object> attributes = clientRequest.attributes();
+    Object value = attributes.get(URI_TEMPLATE_ATTRIBUTE);
+    if (value instanceof String) {
+      String uriTemplate = (String) value;
+      String path = PATTERN_BEFORE_PATH.matcher(uriTemplate).replaceFirst("");
+      return path.startsWith("/") ? path : "/" + path;
+    }
+    return null;
+  }
 
   @Override
   public String getUrlFull(ClientRequest request) {
@@ -71,4 +92,6 @@ public enum WebClientHttpAttributesGetter
     }
     return null;
   }
+
+
 }
