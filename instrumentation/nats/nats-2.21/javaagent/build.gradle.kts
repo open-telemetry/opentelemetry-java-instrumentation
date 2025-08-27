1c88b1c88cb387f6e40a43f44774809b692c5166
Alix <alix.baggio@outlook.fr>
2025-08-27 16:39:34 +0200

    remove muzzle check for version without dependency

--- instrumentation/nats/nats-2.21/javaagent/build.gradle.kts
+++ instrumentation/nats/nats-2.21/javaagent/build.gradle.kts
@@ -7,6 +7,10 @@ muzzle {
     group.set("io.nats")
     module.set("jnats")
     versions.set("[2.17.2,)")
+
+    // Could not find io.nats:nats-parent:1.0-SNAPSHOT
+    skip("0.5.0", "0.5.1")
+
     assertInverse.set(true)
   }
 }
