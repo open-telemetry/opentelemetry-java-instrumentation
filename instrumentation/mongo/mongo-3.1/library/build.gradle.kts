130e72be739238412c55095bead80a57bb16469b
Jay DeLuca <jaydeluca4@gmail.com>
2025-08-28 15:30:56 -0400

    batch 9

--- instrumentation/mongo/mongo-3.1/library/build.gradle.kts
+++ instrumentation/mongo/mongo-3.1/library/build.gradle.kts
@@ -12,16 +12,26 @@ dependencies {
   testImplementation(project(":instrumentation:mongo:mongo-3.1:testing"))
 }
 
+testing {
+  suites {
+    val testStableSemconv by registering(JvmTestSuite::class) {
+      targets {
+        all {
+          testTask.configure {
+            jvmArgs("-Dotel.semconv-stability.opt-in=database")
+          }
+        }
+      }
+    }
+  }
+}
+
 tasks {
   withType<Test>().configureEach {
     usesService(gradle.sharedServices.registrations["testcontainersBuildService"].service)
   }
 
-  val testStableSemconv by registering(Test::class) {
-    jvmArgs("-Dotel.semconv-stability.opt-in=database")
-  }
-
   check {
-    dependsOn(testStableSemconv)
+    dependsOn(testing.suites.named("testStableSemconv"))
   }
 }
