95361449f18f644aedd3c077f5bec8caf09b26d5
Jay DeLuca <jaydeluca4@gmail.com>
2025-08-29 16:15:17 -0400

    the rest

--- instrumentation/mongo/mongo-3.1/javaagent/build.gradle.kts
+++ instrumentation/mongo/mongo-3.1/javaagent/build.gradle.kts
@@ -23,26 +23,18 @@ dependencies {
   testInstrumentation(project(":instrumentation:mongo:mongo-4.0:javaagent"))
 }
 
-testing {
-  suites {
-    val testStableSemconv by registering(JvmTestSuite::class) {
-      targets {
-        all {
-          testTask.configure {
-            jvmArgs("-Dotel.semconv-stability.opt-in=database")
-          }
-        }
-      }
-    }
-  }
-}
-
 tasks {
   withType<Test>().configureEach {
     usesService(gradle.sharedServices.registrations["testcontainersBuildService"].service)
   }
 
+  val testStableSemconv by registering(Test::class) {
+    testClassesDirs = sourceSets.test.get().output.classesDirs
+    classpath = sourceSets.test.get().runtimeClasspath
+    jvmArgs("-Dotel.semconv-stability.opt-in=database")
+  }
+
   check {
-    dependsOn(testing.suites)
+    dependsOn(testStableSemconv)
   }
 }
