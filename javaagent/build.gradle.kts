fadfff6106f2bc094fd5db18c08e52ebd39c1ae3
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-08-29 17:12:47 +0200

    rename to declarative-config-bridge

--- javaagent/build.gradle.kts
+++ javaagent/build.gradle.kts
@@ -69,7 +69,7 @@ dependencies {
     exclude("io.opentelemetry", "opentelemetry-sdk")
     exclude("io.opentelemetry", "opentelemetry-sdk-extension-autoconfigure")
     exclude("io.opentelemetry", "opentelemetry-sdk-extension-autoconfigure-spi")
-    exclude("io.opentelemetry.instrumentation", "sdk-bridge")
+    exclude("io.opentelemetry.instrumentation", "declarative-config-bridge")
   }
   baseJavaagentLibs(project(":javaagent-extension-api"))
 
