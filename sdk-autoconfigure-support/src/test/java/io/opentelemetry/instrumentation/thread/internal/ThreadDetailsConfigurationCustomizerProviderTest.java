90c437f31d4c2713e872f509016e95ea3aa8e9d2
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-09-01 18:27:07 +0200

    set enabled in test

--- sdk-autoconfigure-support/src/test/java/io/opentelemetry/instrumentation/thread/internal/ThreadDetailsConfigurationCustomizerProviderTest.java
+++ sdk-autoconfigure-support/src/test/java/io/opentelemetry/instrumentation/thread/internal/ThreadDetailsConfigurationCustomizerProviderTest.java
@@ -5,11 +5,14 @@
 
 package io.opentelemetry.instrumentation.thread.internal;
 
+import static java.util.Collections.singletonMap;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.extension.incubator.fileconfig.DeclarativeConfiguration;
 import io.opentelemetry.sdk.extension.incubator.fileconfig.DeclarativeConfigurationBuilder;
+import io.opentelemetry.sdk.extension.incubator.fileconfig.internal.model.ExperimentalLanguageSpecificInstrumentationModel;
+import io.opentelemetry.sdk.extension.incubator.fileconfig.internal.model.InstrumentationModel;
 import io.opentelemetry.sdk.extension.incubator.fileconfig.internal.model.OpenTelemetryConfigurationModel;
 import io.opentelemetry.sdk.extension.incubator.fileconfig.internal.model.TracerProviderModel;
 import org.junit.jupiter.api.Test;
@@ -23,7 +26,13 @@ class ThreadDetailsConfigurationCustomizerProviderTest {
             .customizeModel(
                 new OpenTelemetryConfigurationModel()
                     .withFileFormat("1.0-rc.1")
-                    .withTracerProvider(new TracerProviderModel()));
+                    .withTracerProvider(new TracerProviderModel())
+                    .withInstrumentationDevelopment(
+                        new InstrumentationModel()
+                            .withJava(
+                                new ExperimentalLanguageSpecificInstrumentationModel()
+                                    .withAdditionalProperty(
+                                        "thread_details", singletonMap("enabled", true)))));
 
     try (OpenTelemetrySdk sdk = DeclarativeConfiguration.create(model)) {
       assertThat(sdk.toString()).containsOnlyOnce("AddThreadDetailsSpanProcessor");
