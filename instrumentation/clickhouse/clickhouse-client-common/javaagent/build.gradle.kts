ba7169acd2ec5e8f0eff39798c1fce0b00c3f779
Lauri Tulmin <ltulmin@splunk.com>
2025-09-02 16:43:37 +0300

    review

--- instrumentation/clickhouse/clickhouse-client-common/javaagent/build.gradle.kts
+++ instrumentation/clickhouse/clickhouse-client-common/javaagent/build.gradle.kts
@@ -1,11 +1,8 @@
 plugins {
-  id("otel.java-conventions")
   id("otel.javaagent-instrumentation")
 }
 
 dependencies {
-  api(project(":instrumentation-api"))
-  api(project(":instrumentation-api-incubator"))
   compileOnly("com.google.auto.value:auto-value-annotations")
   annotationProcessor("com.google.auto.value:auto-value")
 }
