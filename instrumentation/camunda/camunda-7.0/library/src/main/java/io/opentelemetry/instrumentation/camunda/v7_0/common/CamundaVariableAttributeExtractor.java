9e5df91dcd84817663a21f0ca5219a3fc1078002
Boslet, Cory (cb645j) <cb645j@att.com>
2025-09-03 16:09:17 -0400

    update extract

--- instrumentation/camunda/camunda-7.0/library/src/main/java/io/opentelemetry/instrumentation/camunda/v7_0/common/CamundaVariableAttributeExtractor.java
+++ instrumentation/camunda/camunda-7.0/library/src/main/java/io/opentelemetry/instrumentation/camunda/v7_0/common/CamundaVariableAttributeExtractor.java
@@ -36,6 +36,6 @@ public class CamundaVariableAttributeExtractor
       AttributesBuilder attributes,
       Context context,
       CamundaCommonRequest request,
-      String response,
+      Void response,
       Throwable error) {}
 }
