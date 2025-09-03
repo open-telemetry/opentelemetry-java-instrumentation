0a2a414b669b989087bcd3edd3cc109fed0e89cf
Boslet, Cory (cb645j) <cb645j@att.com>
2025-09-03 11:55:47 -0400

    Accidentely removed file

---
+++ instrumentation/camunda/camunda-7.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/camunda/v7_0/behavior/CamundaActivityExecutionGetter.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+ package io.opentelemetry.javaagent.instrumentation.camunda.v7_0.behavior;
+
+import io.opentelemetry.context.propagation.TextMapGetter;
+import org.camunda.bpm.engine.impl.pvm.delegate.ActivityExecution;
+
+public class CamundaActivityExecutionGetter implements TextMapGetter<ActivityExecution> {
+
+  @Override
+  public Iterable<String> keys(ActivityExecution carrier) {
+    return carrier.getVariableNames();
+  }
+
+  @Override
+  public String get(ActivityExecution carrier, String key) {
+    Object variable = carrier.getVariables().get(key);
+    return variable == null ? null : variable.toString();
+  }
+}
