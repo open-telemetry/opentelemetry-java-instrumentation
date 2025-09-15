2088d86473625ca857b35317b010b0dc18b3cca4
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2025-09-15 14:23:10 +0200

    elasticsearch-rest-7.0

--- instrumentation/elasticsearch/elasticsearch-rest-7.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/elasticsearch/rest/v7_0/RestClientInstrumentation.java
+++ instrumentation/elasticsearch/elasticsearch-rest-7.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/elasticsearch/rest/v7_0/RestClientInstrumentation.java
@@ -6,6 +6,7 @@
 package io.opentelemetry.javaagent.instrumentation.elasticsearch.rest.v7_0;
 
 import static io.opentelemetry.javaagent.bootstrap.Java8BytecodeBridge.currentContext;
+import static io.opentelemetry.javaagent.instrumentation.elasticsearch.rest.v7_0.ElasticsearchRest7Singletons.ENDPOINT_DEFINITION;
 import static io.opentelemetry.javaagent.instrumentation.elasticsearch.rest.v7_0.ElasticsearchRest7Singletons.instrumenter;
 import static net.bytebuddy.matcher.ElementMatchers.isMethod;
 import static net.bytebuddy.matcher.ElementMatchers.named;
@@ -14,13 +15,14 @@ import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
 
 import io.opentelemetry.context.Context;
 import io.opentelemetry.context.Scope;
-import io.opentelemetry.instrumentation.api.util.VirtualField;
-import io.opentelemetry.instrumentation.elasticsearch.rest.common.v5_0.internal.ElasticsearchEndpointDefinition;
 import io.opentelemetry.instrumentation.elasticsearch.rest.common.v5_0.internal.ElasticsearchRestRequest;
 import io.opentelemetry.instrumentation.elasticsearch.rest.common.v5_0.internal.RestResponseListener;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeTransformer;
+import javax.annotation.Nullable;
 import net.bytebuddy.asm.Advice;
+import net.bytebuddy.asm.Advice.AssignReturned;
+import net.bytebuddy.asm.Advice.AssignReturned.ToArguments.ToArgument;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.matcher.ElementMatcher;
 import org.elasticsearch.client.Request;
@@ -50,101 +52,98 @@ public class RestClientInstrumentation implements TypeInstrumentation {
         this.getClass().getName() + "$PerformRequestAsyncAdvice");
   }
 
+  public static class AdviceScope {
+    private final ElasticsearchRestRequest request;
+    private final Context context;
+    private final Scope scope;
+
+    public AdviceScope(ElasticsearchRestRequest request, Context context, Scope scope) {
+      this.request = request;
+      this.context = context;
+      this.scope = scope;
+    }
+
+    public void end(@Nullable Throwable throwable) {
+      scope.close();
+      if (throwable != null) {
+        instrumenter().end(context, request, null, throwable);
+      }
+      // span ended in RestResponseListener
+    }
+  }
+
   @SuppressWarnings("unused")
   public static class PerformRequestAdvice {
 
+    @Nullable
     @Advice.OnMethodEnter(suppress = Throwable.class)
-    public static void onEnter(
-        @Advice.Argument(0) Request request,
-        @Advice.Local("otelRequest") ElasticsearchRestRequest otelRequest,
-        @Advice.Local("otelContext") Context context,
-        @Advice.Local("otelScope") Scope scope) {
+    public static AdviceScope onEnter(@Advice.Argument(0) Request request) {
 
       Context parentContext = currentContext();
-      VirtualField<Request, ElasticsearchEndpointDefinition> virtualField =
-          VirtualField.find(Request.class, ElasticsearchEndpointDefinition.class);
-      otelRequest =
+      ElasticsearchRestRequest otelRequest =
           ElasticsearchRestRequest.create(
               request.getMethod(),
               request.getEndpoint(),
               // set by elasticsearch-api-client instrumentation
-              virtualField.get(request),
+              ENDPOINT_DEFINITION.get(request),
               request.getEntity());
       if (!instrumenter().shouldStart(parentContext, otelRequest)) {
-        return;
+        return null;
       }
 
-      context = instrumenter().start(parentContext, otelRequest);
-      scope = context.makeCurrent();
+      Context context = instrumenter().start(parentContext, otelRequest);
+      return new AdviceScope(otelRequest, context, context.makeCurrent());
     }
 
     @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
     public static void stopSpan(
         @Advice.Return Response response,
         @Advice.Thrown Throwable throwable,
-        @Advice.Local("otelRequest") ElasticsearchRestRequest otelRequest,
-        @Advice.Local("otelContext") Context context,
-        @Advice.Local("otelScope") Scope scope) {
+        @Advice.Enter @Nullable AdviceScope adviceScope) {
 
-      if (scope == null) {
-        return;
+      if (adviceScope != null) {
+        adviceScope.end(throwable);
       }
-      scope.close();
-
-      instrumenter().end(context, otelRequest, response, throwable);
     }
   }
 
   @SuppressWarnings("unused")
   public static class PerformRequestAsyncAdvice {
 
+    @AssignReturned.ToArguments(@ToArgument(value = 1, index = 1))
     @Advice.OnMethodEnter(suppress = Throwable.class)
-    public static void onEnter(
+    public static Object[] onEnter(
         @Advice.Argument(0) Request request,
-        @Advice.Argument(value = 1, readOnly = false) ResponseListener responseListener,
-        @Advice.Local("otelRequest") ElasticsearchRestRequest otelRequest,
-        @Advice.Local("otelContext") Context context,
-        @Advice.Local("otelScope") Scope scope) {
+        @Advice.Argument(1) ResponseListener originalResponseListener) {
 
+      ResponseListener responseListener = originalResponseListener;
       Context parentContext = currentContext();
-      VirtualField<Request, ElasticsearchEndpointDefinition> virtualField =
-          VirtualField.find(Request.class, ElasticsearchEndpointDefinition.class);
-
-      otelRequest =
+      ElasticsearchRestRequest otelRequest =
           ElasticsearchRestRequest.create(
               request.getMethod(),
               request.getEndpoint(),
               // set by elasticsearch-api-client instrumentation
-              virtualField.get(request),
+              ENDPOINT_DEFINITION.get(request),
               request.getEntity());
       if (!instrumenter().shouldStart(parentContext, otelRequest)) {
-        return;
+        return new Object[] {null, responseListener};
       }
 
-      context = instrumenter().start(parentContext, otelRequest);
-      scope = context.makeCurrent();
-
+      Context context = instrumenter().start(parentContext, otelRequest);
+      AdviceScope adviceScope = new AdviceScope(otelRequest, context, context.makeCurrent());
       responseListener =
           new RestResponseListener(
               responseListener, parentContext, instrumenter(), context, otelRequest);
+      return new Object[] {adviceScope, responseListener};
     }
 
     @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
     public static void stopSpan(
-        @Advice.Thrown Throwable throwable,
-        @Advice.Local("otelRequest") ElasticsearchRestRequest otelRequest,
-        @Advice.Local("otelContext") Context context,
-        @Advice.Local("otelScope") Scope scope) {
-
-      if (scope == null) {
-        return;
+        @Advice.Thrown @Nullable Throwable throwable, @Advice.Enter Object[] enterResult) {
+      AdviceScope adviceScope = (AdviceScope) enterResult[0];
+      if (adviceScope != null) {
+        adviceScope.end(throwable);
       }
-      scope.close();
-
-      if (throwable != null) {
-        instrumenter().end(context, otelRequest, null, throwable);
-      }
-      // span ended in RestResponseListener
     }
   }
 }
