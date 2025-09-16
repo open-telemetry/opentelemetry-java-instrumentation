011201cd70b1953db38a3b5b8f68e106074604fd
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-09-16 10:04:20 +0200

    convert groovy smoke tests to java, part 1 (#14648)
    Co-authored-by: Lauri Tulmin <ltulmin@splunk.com>

---
+++ smoke-tests/src/test/java/io/opentelemetry/smoketest/SecurityManagerSmokeTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.smoketest;
+
+import static io.opentelemetry.sdk.testing.assertj.TracesAssert.assertThat;
+
+import java.util.Collections;
+import java.util.Map;
+import org.junit.jupiter.api.condition.DisabledIf;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisabledIf("io.opentelemetry.smoketest.TestContainerManager#useWindowsContainers")
+class SecurityManagerSmokeTest extends JavaSmokeTest {
+  @Override
+  protected String getTargetImage(String jdk) {
+    return "ghcr.io/open-telemetry/opentelemetry-java-instrumentation/smoke-test-security-manager:jdk"
+        + jdk
+        + "-20250915.17728045123";
+  }
+
+  @Override
+  protected Map<String, String> getExtraEnv() {
+    return Collections.singletonMap(
+        "OTEL_JAVAAGENT_EXPERIMENTAL_SECURITY_MANAGER_SUPPORT_ENABLED", "true");
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {8, 11, 17, 21, 23})
+  void securityManagerSmokeTest(int jdk) throws Exception {
+    withTarget(
+        jdk,
+        () ->
+            assertThat(waitForTraces())
+                .hasTracesSatisfyingExactly(
+                    trace -> trace.hasSpansSatisfyingExactly(span -> span.hasName("test"))));
+  }
+}
--- testing/proto-shaded-for-testing/build.gradle.kts
+++ testing/proto-shaded-for-testing/build.gradle.kts
@@ -5,12 +5,18 @@ plugins {
 
 dependencies {
   implementation("io.opentelemetry.proto:opentelemetry-proto")
+  implementation("com.google.protobuf:protobuf-java-util:4.32.1")
 }
 
 tasks {
   shadowJar {
     relocate("io.opentelemetry.proto", "io.opentelemetry.testing.internal.proto")
     relocate("com.google.protobuf", "io.opentelemetry.testing.internal.protobuf")
+    relocate("com.google.gson", "io.opentelemetry.testing.internal.gson")
+    relocate("com.google.common", "io.opentelemetry.testing.internal.guava")
+
+    enableAutoRelocation = true
+    relocationPrefix = "io.opentelemetry.testing.internal"
   }
 
   val extractShadowJar by registering(Copy::class) {
