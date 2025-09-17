1daf94882dd0fe0fca96f3041550a5428c96eefa
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-09-17 14:36:04 +0200

    map default enabled

--- instrumentation/spring/spring-boot-autoconfigure/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/internal/EarlyConfig.java
+++ instrumentation/spring/spring-boot-autoconfigure/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/internal/EarlyConfig.java
@@ -5,8 +5,10 @@
 
 package io.opentelemetry.instrumentation.spring.autoconfigure.internal;
 
+import io.opentelemetry.sdk.autoconfigure.spi.ConfigurationException;
 import java.util.HashMap;
 import java.util.Map;
+import javax.annotation.Nullable;
 import org.springframework.core.env.Environment;
 
 /**
@@ -22,45 +24,76 @@ public class EarlyConfig {
     DECLARATIVE_CONFIG_PROPERTY_MAPPING.put("otel.sdk.disabled", "otel.disabled");
     DECLARATIVE_CONFIG_PROPERTY_MAPPING.put(
         "otel.instrumentation.%s.enabled", "otel.instrumentation/development.java.%s.enabled");
-    DECLARATIVE_CONFIG_PROPERTY_MAPPING.put(
-        "otel.instrumentation.common.default-enabled",
-        "otel.instrumentation/development.java.common.default.enabled");
   }
 
   public static boolean otelEnabled(Environment environment) {
     return !environment.getProperty(
-        translatePropertyName(environment, "otel.sdk.disabled"), Boolean.class, false);
+        translatePropertyName(environment, "otel.sdk.disabled", null), Boolean.class, false);
   }
 
-  public static Boolean isDeclarativeConfig(Environment environment) {
+  public static boolean isDeclarativeConfig(Environment environment) {
     return environment.getProperty("otel.file_format", String.class) != null;
   }
 
-  public static String translatePropertyName(Environment environment, String name) {
+  public static boolean isDefaultEnabled(Environment environment) {
     if (isDeclarativeConfig(environment)) {
-      String value = DECLARATIVE_CONFIG_PROPERTY_MAPPING.get(name);
-      if (value != null) {
-        return value;
-      }
-      if (name.startsWith("otel.instrumentation.")) {
-        return String.format(
-            "otel.instrumentation/development.java.%s",
-            name.substring("otel.instrumentation.".length()));
-      }
+      String mode =
+          environment.getProperty(
+              "otel.instrumentation/development.java.spring_starter.instrumentation_mode",
+              String.class,
+              "default");
 
-      throw new IllegalStateException(
-          "No mapping found for property name: " + name + ". Please report this bug.");
+      switch (mode) {
+        case "none":
+          return false;
+        case "default":
+          return true;
+        default:
+          throw new ConfigurationException("Unknown instrumentation mode: " + mode);
+      }
     } else {
-      return name;
+      return environment.getProperty(
+          "otel.instrumentation.common.default-enabled", Boolean.class, true);
     }
+  }
+
+  public static String translatePropertyName(
+      Environment environment, String name, @Nullable String arg) {
+    if (isDeclarativeConfig(environment)) {
+      String key = declarativeConfigKey(name);
+      if (arg != null) {
+        key = String.format(key, arg);
+      }
+      return key.replace('-', '_');
+    } else {
+      String key = name;
+      if (arg != null) {
+        key = String.format(key, arg);
+      }
+      return key;
+    }
+  }
+
+  private static String declarativeConfigKey(String name) {
+    String value = DECLARATIVE_CONFIG_PROPERTY_MAPPING.get(name);
+    if (value != null) {
+      return value;
+    }
+    if (name.startsWith("otel.instrumentation.")) {
+      return String.format(
+          "otel.instrumentation/development.java.%s",
+          name.substring("otel.instrumentation.".length()));
+    }
+
+    throw new IllegalStateException(
+        "No mapping found for property name: " + name + ". Please report this bug.");
   }
 
   public static boolean isInstrumentationEnabled(
       Environment environment, String name, boolean defaultValue) {
     Boolean explicit =
         environment.getProperty(
-            String.format(
-                translatePropertyName(environment, "otel.instrumentation.%s.enabled"), name),
+            translatePropertyName(environment, "otel.instrumentation.%s.enabled", name),
             Boolean.class);
     if (explicit != null) {
       return explicit;
@@ -68,9 +101,6 @@ public class EarlyConfig {
     if (!defaultValue) {
       return false;
     }
-    return environment.getProperty(
-        translatePropertyName(environment, "otel.instrumentation.common.default-enabled"),
-        Boolean.class,
-        true);
+    return isDefaultEnabled(environment);
   }
 }
