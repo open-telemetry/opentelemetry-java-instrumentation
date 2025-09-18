ec3eb25a7f7611ee3ca0bfc15137145465d6b036
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-09-18 15:12:59 +0200

    fix

--- smoke-tests/src/test/java/io/opentelemetry/smoketest/propagation/OtTracePropagationTest.java
+++ smoke-tests/src/test/java/io/opentelemetry/smoketest/propagation/OtTracePropagationTest.java
@@ -34,7 +34,7 @@ class OtTracePropagationTest extends JavaSmokeTest {
   public void shouldPropagate() {
     startTarget(11);
     AggregatedHttpResponse response = client().get("/front").aggregate().join();
-    var traceId = testing.waitForTraces(1).get(0).get(0).getTraceId();
+    var traceId = testing.waitForTraces(1).get(0).get(0).getTraceId().substring(16);
 
     assertThat(response.contentUtf8()).matches("[0-9a-f]{16}" + traceId + ";[0]{16}" + traceId);
   }
