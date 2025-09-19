233d7d8a98a522b0a45f18cbfa12df73ceeef591
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-09-19 12:01:26 +0200

    convert groovy smoke test to java, part 2 (#14683)
    Co-authored-by: otelbot <197425009+otelbot@users.noreply.github.com>
    Co-authored-by: Lauri Tulmin <ltulmin@splunk.com>

--- smoke-tests/src/test/java/io/opentelemetry/smoketest/JavaSmokeTest.java
+++ smoke-tests/src/test/java/io/opentelemetry/smoketest/JavaSmokeTest.java
@@ -7,25 +7,38 @@ package io.opentelemetry.smoketest;
 
 import static java.util.Collections.emptyList;
 import static java.util.Collections.emptyMap;
+import static java.util.stream.Collectors.toSet;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import io.opentelemetry.sdk.logs.data.LogRecordData;
-import io.opentelemetry.sdk.metrics.data.MetricData;
-import io.opentelemetry.sdk.trace.data.SpanData;
+import io.opentelemetry.instrumentation.testing.internal.AutoCleanupExtension;
+import io.opentelemetry.instrumentation.testing.junit.InstrumentationExtension;
 import io.opentelemetry.smoketest.windows.WindowsTestContainerManager;
 import io.opentelemetry.testing.internal.armeria.client.WebClient;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Consumer;
-import org.junit.jupiter.api.BeforeEach;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.testcontainers.containers.output.OutputFrame;
+import org.testcontainers.containers.output.ToStringConsumer;
 
-public abstract class JavaSmokeTest {
+public abstract class JavaSmokeTest implements TelemetryRetrieverProvider {
+
+  @RegisterExtension
+  public static final InstrumentationExtension testing = SmokeTestInstrumentationExtension.create();
+
+  private static final Pattern TRACE_ID_PATTERN =
+      Pattern.compile(".*trace_id=(?<traceId>[a-zA-Z0-9]+).*");
   protected static final TestContainerManager containerManager = createContainerManager();
-  private JavaTelemetryRetriever telemetryRetriever;
+  private static TelemetryRetriever telemetryRetriever;
 
   protected String agentPath =
       System.getProperty("io.opentelemetry.smoketest.agent.shadowJar.path");
+
+  @RegisterExtension static final AutoCleanupExtension autoCleanup = AutoCleanupExtension.create();
 
   protected WebClient client() {
     return WebClient.of("h1c://localhost:" + containerManager.getTargetMappedPort(8080));
@@ -59,27 +72,20 @@ public abstract class JavaSmokeTest {
     return emptyList();
   }
 
-  @BeforeEach
-  void setUp() {
+  @BeforeAll
+  static void setUp() {
     containerManager.startEnvironmentOnce();
-    telemetryRetriever = new JavaTelemetryRetriever(containerManager.getBackendMappedPort());
+    telemetryRetriever = new TelemetryRetriever(containerManager.getBackendMappedPort());
   }
 
-  public void withTarget(int jdk, TargetRunner runner) throws Exception {
-    startTarget(jdk);
-    try {
-      runner.runInTarget();
-    } finally {
-      stopTarget();
-    }
-  }
-
-  public Consumer<OutputFrame> startTarget(int jdk) {
+  protected Consumer<OutputFrame> startTarget(int jdk) {
     return startTarget(String.valueOf(jdk), null, false);
   }
 
-  public Consumer<OutputFrame> startTarget(String jdk, String serverVersion, boolean windows) {
+  protected Consumer<OutputFrame> startTarget(String jdk, String serverVersion, boolean windows) {
     String targetImage = getTargetImage(jdk, serverVersion, windows);
+    autoCleanup.deferCleanup(() -> containerManager.stopTarget());
+
     return containerManager.startTarget(
         targetImage,
         agentPath,
@@ -106,30 +112,34 @@ public abstract class JavaSmokeTest {
     return null;
   }
 
-  public void cleanup() {
-    telemetryRetriever.clearTelemetry();
+  protected static void assertVersionLogged(Consumer<OutputFrame> output, String version) {
+    assertThat(
+            logLines(output)
+                .anyMatch(l -> l.contains("opentelemetry-javaagent - version: " + version)))
+        .isTrue();
   }
 
-  public void stopTarget() {
-    containerManager.stopTarget();
-    cleanup();
+  private static Stream<String> logLines(Consumer<OutputFrame> output) {
+    return ((ToStringConsumer) output).toUtf8String().lines();
   }
 
-  protected List<SpanData> waitForTraces() {
-    return telemetryRetriever.waitForTraces();
+  protected static Set<String> getLoggedTraceIds(Consumer<OutputFrame> output) {
+    return logLines(output).flatMap(JavaSmokeTest::findTraceId).collect(toSet());
   }
 
-  protected Collection<MetricData> waitForMetrics() {
-    return telemetryRetriever.waitForMetrics();
-  }
-
-  protected Collection<LogRecordData> waitForLogs() {
-    return telemetryRetriever.waitForLogs();
+  private static Stream<String> findTraceId(String log) {
+    var m = TRACE_ID_PATTERN.matcher(log);
+    return m.matches() ? Stream.of(m.group("traceId")) : Stream.empty();
   }
 
   private static TestContainerManager createContainerManager() {
     return TestContainerManager.useWindowsContainers()
         ? new WindowsTestContainerManager()
         : new LinuxTestContainerManager();
+  }
+
+  @Override
+  public TelemetryRetriever getTelemetryRetriever() {
+    return telemetryRetriever;
   }
 }
---
+++ smoke-tests/src/test/java/io/opentelemetry/smoketest/SpringBootSmokeTest.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.smoketest;
+
+import static io.opentelemetry.sdk.testing.assertj.OpenTelemetryAssertions.assertThat;
+import static io.opentelemetry.sdk.testing.assertj.OpenTelemetryAssertions.satisfies;
+
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.sdk.trace.data.SpanData;
+import io.opentelemetry.semconv.ServiceAttributes;
+import io.opentelemetry.semconv.incubating.OsIncubatingAttributes;
+import io.opentelemetry.semconv.incubating.TelemetryIncubatingAttributes;
+import io.opentelemetry.semconv.incubating.ThreadIncubatingAttributes;
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.condition.DisabledIf;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.testcontainers.containers.output.OutputFrame;
+
+@DisabledIf("io.opentelemetry.smoketest.TestContainerManager#useWindowsContainers")
+class SpringBootSmokeTest extends JavaSmokeTest {
+  @Override
+  protected String getTargetImage(String jdk) {
+    return "ghcr.io/open-telemetry/opentelemetry-java-instrumentation/smoke-test-spring-boot:jdk"
+        + jdk
+        + "-20241021.11448062567";
+  }
+
+  @Override
+  protected boolean getSetServiceName() {
+    return false;
+  }
+
+  @Override
+  protected Map<String, String> getExtraEnv() {
+    return Map.of("OTEL_METRICS_EXPORTER", "otlp", "OTEL_RESOURCE_ATTRIBUTES", "foo=bar");
+  }
+
+  @Override
+  protected TargetWaitStrategy getWaitStrategy() {
+    return new TargetWaitStrategy.Log(
+        Duration.ofMinutes(1), ".*Started SpringbootApplication in.*");
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {8, 11, 17, 21, 23})
+  void springBootSmokeTest(int jdk) throws Exception {
+    Consumer<OutputFrame> output = startTarget(jdk);
+    String currentAgentVersion;
+    try (JarFile agentJar = new JarFile(agentPath)) {
+      currentAgentVersion =
+          agentJar
+              .getManifest()
+              .getMainAttributes()
+              .getValue(Attributes.Name.IMPLEMENTATION_VERSION);
+    }
+
+    var response = client().get("/greeting").aggregate().join();
+    assertThat(response.contentUtf8()).isEqualTo("Hi!");
+
+    testing.waitAndAssertTraces(
+        trace ->
+            trace.hasSpansSatisfyingExactly(
+                span ->
+                    span.hasName("GET /greeting")
+                        .hasAttribute(
+                            satisfies(ThreadIncubatingAttributes.THREAD_ID, a -> a.isNotNull()))
+                        .hasAttribute(
+                            satisfies(ThreadIncubatingAttributes.THREAD_NAME, a -> a.isNotBlank()))
+                        .hasResourceSatisfying(
+                            resource ->
+                                resource
+                                    .hasAttribute(
+                                        TelemetryIncubatingAttributes.TELEMETRY_DISTRO_VERSION,
+                                        currentAgentVersion)
+                                    .hasAttribute(
+                                        satisfies(
+                                            OsIncubatingAttributes.OS_TYPE, a -> a.isNotNull()))
+                                    .hasAttribute(AttributeKey.stringKey("foo"), "bar")
+                                    .hasAttribute(
+                                        ServiceAttributes.SERVICE_NAME, "otel-spring-test-app")
+                                    .hasAttribute(
+                                        ServiceAttributes.SERVICE_VERSION,
+                                        "2.10.0-alpha-SNAPSHOT")),
+                span -> span.hasName("WebController.withSpan")));
+
+    // Check agent version is logged on startup
+    assertVersionLogged(output, currentAgentVersion);
+
+    // Check trace IDs are logged via MDC instrumentation
+    Set<String> loggedTraceIds = getLoggedTraceIds(output);
+    List<SpanData> spans = testing.spans();
+    Set<String> spanTraceIds = spans.stream().map(SpanData::getTraceId).collect(Collectors.toSet());
+    assertThat(loggedTraceIds).isEqualTo(spanTraceIds);
+
+    // Check JVM metrics are exported
+    testing.waitAndAssertMetrics(
+        "io.opentelemetry.runtime-telemetry-java8",
+        metric -> metric.hasName("jvm.memory.used"),
+        metric -> metric.hasName("jvm.memory.committed"),
+        metric -> metric.hasName("jvm.memory.limit"),
+        metric -> metric.hasName("jvm.memory.used_after_last_gc"));
+  }
+}
