c01a032f56971435de3135eed85ff921732850d0
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-09-21 10:07:26 +0200

    webshere

---
+++ smoke-tests/src/test/java/io/opentelemetry/smoketest/appserver/WebsphereSmokeTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.smoketest.appserver;
+
+import io.opentelemetry.smoketest.AppServer;
+import io.opentelemetry.smoketest.SmokeTestInstrumentationExtension;
+import io.opentelemetry.smoketest.TargetWaitStrategy;
+import java.time.Duration;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+public abstract class WebsphereSmokeTest extends AppServerTest {
+
+  @RegisterExtension
+  static final SmokeTestInstrumentationExtension testing =
+      builder("ghcr.io/open-telemetry/opentelemetry-java-instrumentation/smoke-test-servlet-websphere")
+          .waitStrategy(new TargetWaitStrategy.Log(Duration.ofMinutes(3), ".*Server server1 open for e-business.*"))
+          .build();
+
+  @Override
+  protected SmokeTestInstrumentationExtension testing() {
+    return testing;
+  }
+
+  @Override
+  protected String getSpanName(String path) {
+    if ("/app/hello.txt".equals(path) || "/app/file-that-does-not-exist".equals(path)) {
+      return "GET";
+    }
+    return super.getSpanName(path);
+  }
+
+  @Override
+  protected boolean testRequestOutsideDeployedApp() {
+    return false;
+  }
+
+  @AppServer(version = "8.5.5.22", jdk = "8-openj9")
+  public static class Websphere8Jdk8Openj9 extends WebsphereSmokeTest {}
+
+  @AppServer(version = "9.0.5.14", jdk = "8-openj9")
+  public static class Websphere9Jdk8Openj9 extends WebsphereSmokeTest {}
+}
+
