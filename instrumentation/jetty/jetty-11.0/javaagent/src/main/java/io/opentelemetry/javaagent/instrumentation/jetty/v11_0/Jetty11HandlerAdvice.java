c1faa230efe13e3dd0c9f3259b675007dca87ac3
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2025-09-25 16:38:11 +0200

    jetty-11.0

--- instrumentation/jetty/jetty-11.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/jetty/v11_0/Jetty11HandlerAdvice.java
+++ instrumentation/jetty/jetty-11.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/jetty/v11_0/Jetty11HandlerAdvice.java
@@ -14,52 +14,70 @@ import io.opentelemetry.javaagent.bootstrap.http.HttpServerResponseCustomizerHol
 import io.opentelemetry.javaagent.instrumentation.servlet.ServletRequestContext;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import javax.annotation.Nullable;
 import net.bytebuddy.asm.Advice;
 
 @SuppressWarnings("unused")
 public class Jetty11HandlerAdvice {
 
-  @Advice.OnMethodEnter(suppress = Throwable.class)
-  public static void onEnter(
-      @Advice.This Object source,
-      @Advice.Argument(2) HttpServletRequest request,
-      @Advice.Argument(3) HttpServletResponse response,
-      @Advice.Local("otelRequest") ServletRequestContext<HttpServletRequest> requestContext,
-      @Advice.Local("otelContext") Context context,
-      @Advice.Local("otelScope") Scope scope) {
+  public static class AdviceScope {
+    private final ServletRequestContext<HttpServletRequest> requestContext;
+    private final Context context;
+    private final Scope scope;
 
-    Context attachedContext = helper().getServerContext(request);
-    if (attachedContext != null) {
-      // We are inside nested handler, don't create new span
-      return;
+    private AdviceScope(
+        ServletRequestContext<HttpServletRequest> requestContext, Context context, Scope scope) {
+      this.requestContext = requestContext;
+      this.context = context;
+      this.scope = scope;
     }
 
-    Context parentContext = Java8BytecodeBridge.currentContext();
-    requestContext = new ServletRequestContext<>(request);
-
-    if (!helper().shouldStart(parentContext, requestContext)) {
-      return;
+    @Nullable
+    public static AdviceScope start(
+        Object source, HttpServletRequest request, HttpServletResponse response) {
+      Context attachedContext = helper().getServerContext(request);
+      if (attachedContext != null) {
+        // We are inside nested handler, don't create new span
+        return null;
+      }
+      Context parentContext = Java8BytecodeBridge.currentContext();
+      ServletRequestContext<HttpServletRequest> requestContext =
+          new ServletRequestContext<>(request);
+      if (!helper().shouldStart(parentContext, requestContext)) {
+        return null;
+      }
+      Context context = helper().start(parentContext, requestContext);
+      Scope scope = context.makeCurrent();
+      // Must be set here since Jetty handlers can use startAsync outside of servlet scope.
+      helper().setAsyncListenerResponse(context, response);
+      HttpServerResponseCustomizerHolder.getCustomizer()
+          .customize(context, response, Jetty11ResponseMutator.INSTANCE);
+      return new AdviceScope(requestContext, context, scope);
     }
 
-    context = helper().start(parentContext, requestContext);
-    scope = context.makeCurrent();
+    public void end(
+        @Nullable Throwable throwable, HttpServletRequest request, HttpServletResponse response) {
+      helper().end(requestContext, request, response, throwable, context, scope);
+    }
 
-    // Must be set here since Jetty handlers can use startAsync outside of servlet scope.
-    helper().setAsyncListenerResponse(context, response);
-
-    HttpServerResponseCustomizerHolder.getCustomizer()
-        .customize(context, response, Jetty11ResponseMutator.INSTANCE);
+    @Nullable
+    @Advice.OnMethodEnter(suppress = Throwable.class)
+    public static AdviceScope onEnter(
+        @Advice.This Object source,
+        @Advice.Argument(2) HttpServletRequest request,
+        @Advice.Argument(3) HttpServletResponse response) {
+      return AdviceScope.start(source, request, response);
+    }
   }
 
   @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
   public static void stopSpan(
       @Advice.Argument(2) HttpServletRequest request,
       @Advice.Argument(3) HttpServletResponse response,
-      @Advice.Thrown Throwable throwable,
-      @Advice.Local("otelRequest") ServletRequestContext<HttpServletRequest> requestContext,
-      @Advice.Local("otelContext") Context context,
-      @Advice.Local("otelScope") Scope scope) {
-
-    helper().end(requestContext, request, response, throwable, context, scope);
+      @Advice.Thrown @Nullable Throwable throwable,
+      @Advice.Enter @Nullable AdviceScope adviceScope) {
+    if (adviceScope != null) {
+      adviceScope.end(throwable, request, response);
+    }
   }
 }
