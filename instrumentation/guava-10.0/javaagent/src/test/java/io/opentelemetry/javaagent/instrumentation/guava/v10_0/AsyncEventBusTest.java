0377f5c0477cf55d07fd7d9ce083f7f9bf210b3c
Lauri Tulmin <ltulmin@splunk.com>
2025-09-29 13:51:00 +0300

    review

--- instrumentation/guava-10.0/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/guava/v10_0/AsyncEventBusTest.java
+++ instrumentation/guava-10.0/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/guava/v10_0/AsyncEventBusTest.java
@@ -5,11 +5,8 @@
 
 package io.opentelemetry.javaagent.instrumentation.guava.v10_0;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.google.common.eventbus.AsyncEventBus;
 import com.google.common.eventbus.Subscribe;
-import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.instrumentation.testing.junit.AgentInstrumentationExtension;
 import io.opentelemetry.instrumentation.testing.junit.InstrumentationExtension;
 import java.util.concurrent.ExecutorService;
@@ -17,49 +14,31 @@ import java.util.concurrent.Executors;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
-public class AsyncEventBusTest {
+class AsyncEventBusTest {
 
   @RegisterExtension
   static final InstrumentationExtension testing = AgentInstrumentationExtension.create();
 
   static final ExecutorService executor = Executors.newSingleThreadExecutor();
-
   static final AsyncEventBus asyncEventBus = new AsyncEventBus(executor);
 
   @Test
-  void testAsyncEventBusTakeEffect() {
+  void contextPropagation() {
     class Listener {
-      String receivedTraceId;
 
       @Subscribe
       public void onEvent(String event) {
-        testing.runWithSpan(
-            "listener",
-            () -> {
-              receivedTraceId = Span.current().getSpanContext().getTraceId();
-            });
+        testing.runWithSpan("listener", () -> {});
       }
     }
 
-    Listener listener = new Listener();
-    asyncEventBus.register(listener);
+    asyncEventBus.register(new Listener());
 
-    String[] parentTraceId = new String[1];
-    testing.runWithSpan(
-        "parent",
-        () -> {
-          parentTraceId[0] = Span.current().getSpanContext().getTraceId();
-          asyncEventBus.post("test");
-        });
+    testing.runWithSpan("parent", () -> asyncEventBus.post("test"));
 
     testing.waitAndAssertTraces(
         trace ->
             trace.hasSpansSatisfyingExactly(
                 span -> span.hasName("parent"), span -> span.hasName("listener")));
-
-    assertThat(listener.receivedTraceId)
-        .isNotNull()
-        .isNotEqualTo("00000000000000000000000000000000")
-        .isEqualTo(parentTraceId[0]);
   }
 }
