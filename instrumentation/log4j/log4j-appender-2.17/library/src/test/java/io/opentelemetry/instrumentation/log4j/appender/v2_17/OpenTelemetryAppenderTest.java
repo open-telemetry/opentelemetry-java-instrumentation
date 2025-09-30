28ceb8e575a06fbc60b7218d090303b870fe5aed
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-09-30 15:11:40 -0700

    Remove unnecessary test now

--- instrumentation/log4j/log4j-appender-2.17/library/src/test/java/io/opentelemetry/instrumentation/log4j/appender/v2_17/OpenTelemetryAppenderTest.java
+++ instrumentation/log4j/log4j-appender-2.17/library/src/test/java/io/opentelemetry/instrumentation/log4j/appender/v2_17/OpenTelemetryAppenderTest.java
@@ -31,29 +31,4 @@ class OpenTelemetryAppenderTest extends AbstractOpenTelemetryAppenderTest {
   protected InstrumentationExtension getTesting() {
     return testing;
   }
-
-  @Test
-  void logWithSpan() { // Does not work for log replay, but it is not likely to occur because
-    // the log replay is related to the case where an OpenTelemetry object is not yet available
-    // at the time the log is executed (and if no OpenTelemetry is available, the context
-    // propagation can't happen)
-    Span span1 =
-        testing.runWithSpan(
-            "span1",
-            () -> {
-              logger.info("log message");
-              return Span.current();
-            });
-
-    executeAfterLogsExecution();
-
-    testing.waitAndAssertLogRecords(
-        logRecord ->
-            logRecord
-                .hasSpanContext(span1.getSpanContext())
-                .hasAttributesSatisfying(
-                    equalTo(
-                        ThreadIncubatingAttributes.THREAD_NAME, Thread.currentThread().getName()),
-                    equalTo(ThreadIncubatingAttributes.THREAD_ID, Thread.currentThread().getId())));
-  }
 }
