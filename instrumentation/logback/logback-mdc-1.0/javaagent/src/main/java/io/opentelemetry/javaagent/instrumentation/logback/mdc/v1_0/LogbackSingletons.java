a929cc6b362c4f01c5bf208ee68b122a1ea76473
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2025-10-01 15:27:36 +0200

    logback-mdc

--- instrumentation/logback/logback-mdc-1.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/logback/mdc/v1_0/LogbackSingletons.java
+++ instrumentation/logback/logback-mdc-1.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/logback/mdc/v1_0/LogbackSingletons.java
@@ -5,6 +5,9 @@
 
 package io.opentelemetry.javaagent.instrumentation.logback.mdc.v1_0;
 
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.instrumentation.api.util.VirtualField;
 import io.opentelemetry.javaagent.bootstrap.internal.AgentCommonConfig;
 import io.opentelemetry.javaagent.bootstrap.internal.AgentInstrumentationConfig;
 
@@ -15,6 +18,9 @@ public final class LogbackSingletons {
   private static final String TRACE_ID_KEY = AgentCommonConfig.get().getTraceIdKey();
   private static final String SPAN_ID_KEY = AgentCommonConfig.get().getSpanIdKey();
   private static final String TRACE_FLAGS_KEY = AgentCommonConfig.get().getTraceFlagsKey();
+
+  public static final VirtualField<ILoggingEvent, Context> CONTEXT =
+      VirtualField.find(ILoggingEvent.class, Context.class);
 
   public static boolean addBaggage() {
     return ADD_BAGGAGE;
