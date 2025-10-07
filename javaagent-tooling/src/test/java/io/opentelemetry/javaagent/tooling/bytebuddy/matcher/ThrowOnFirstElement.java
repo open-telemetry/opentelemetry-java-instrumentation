b1aa5713b17fd2a90b93c3272dd6cc4c5624a531
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-10-07 19:00:40 +0200

    convert to java

--- javaagent-tooling/src/test/java/io/opentelemetry/javaagent/tooling/bytebuddy/matcher/ThrowOnFirstElement.java
+++ javaagent-tooling/src/test/java/io/opentelemetry/javaagent/tooling/bytebuddy/matcher/ThrowOnFirstElement.java
@@ -8,15 +8,15 @@ package io.opentelemetry.javaagent.tooling.bytebuddy.matcher;
 import java.util.Iterator;
 
 public class ThrowOnFirstElement implements Iterator<Object> {
+  private int current = 0;
+
   @Override
   public boolean hasNext() {
-    return i++ < 1;
+    return current++ < 1;
   }
 
   @Override
   public Object next() {
     throw new RuntimeException("iteration exception");
   }
-
-  private int i = 0;
 }
