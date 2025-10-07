22eeaf58689c5e58da2b57eed77538673d906826
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-10-07 14:31:40 +0200

    convert test

---
+++ javaagent/src/test/java/io/opentelemetry/javaagent/AgentLoadedIntoBootstrapTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.File;
+import java.util.Collections;
+import jvmbootstraptest.AgentLoadedChecker;
+import jvmbootstraptest.MyClassLoaderIsNotBootstrap;
+import org.junit.jupiter.api.Test;
+
+class AgentLoadedIntoBootstrapTest {
+
+  @Test
+  void agentLoadsInWhenSeparateJvmIsLaunched() throws Exception {
+    int exitCode = IntegrationTestUtils.runOnSeparateJvm(
+        AgentLoadedChecker.class.getName(),
+        new String[0],
+        new String[0],
+        Collections.emptyMap(),
+        true);
+
+    assertThat(exitCode).isEqualTo(0);
+  }
+
+  // this tests the case where someone adds the contents of opentelemetry-javaagent.jar by mistake
+  // to their application's "uber.jar"
+  //
+  // the reason this can cause issues is because we locate the agent jar based on the CodeSource of
+  // the OpenTelemetryAgent class, and then we add that jar file to the bootstrap class path
+  //
+  // but if we find the OpenTelemetryAgent class in an uber jar file, and we add that (whole) uber
+  // jar file to the bootstrap class loader, that can cause some applications to break, as there's a
+  // lot of application and library code that doesn't handle getClassLoader() returning null
+  // (e.g. https://github.com/qos-ch/logback/pull/291)
+  @Test
+  void applicationUberJarShouldNotBeAddedToTheBootstrapClassLoader() throws Exception {
+    String mainClassName = MyClassLoaderIsNotBootstrap.class.getName();
+    String pathToJar = IntegrationTestUtils.createJarWithClasses(
+        mainClassName,
+        MyClassLoaderIsNotBootstrap.class,
+        OpenTelemetryAgent.class).getPath();
+
+    try {
+      int exitCode = IntegrationTestUtils.runOnSeparateJvm(
+          mainClassName,
+          new String[0],
+          new String[0],
+          Collections.emptyMap(),
+          pathToJar,
+          true);
+
+      assertThat(exitCode).isEqualTo(0);
+    } finally {
+      new File(pathToJar).delete();
+    }
+  }
+}
