5e05e30d204f52e38e235647de83443c81ee3bcf
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-10-08 16:27:03 +0200

    pr review

--- muzzle/src/main/java/io/opentelemetry/javaagent/tooling/muzzle/AgentCachingPoolStrategy.java
+++ muzzle/src/main/java/io/opentelemetry/javaagent/tooling/muzzle/AgentCachingPoolStrategy.java
@@ -146,6 +146,7 @@ public class AgentCachingPoolStrategy implements AgentBuilder.PoolStrategy {
     return typePool(classFileLocator, classLoader);
   }
 
+  // visible for testing
   TypePool.CacheProvider getCacheProvider(ClassLoader classLoader) {
     if (classLoader == null) {
       return bootstrapCacheProvider;
@@ -167,6 +168,7 @@ public class AgentCachingPoolStrategy implements AgentBuilder.PoolStrategy {
    *
    * <p>The loaderHash exists to avoid calling get & strengthening the Reference.
    */
+  // visible for testing
   static final class TypeCacheKey {
     private final int loaderHash;
     @Nullable private final WeakReference<ClassLoader> loaderRef;
