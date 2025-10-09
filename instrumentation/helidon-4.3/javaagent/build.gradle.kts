31b8a888e4018bc672f3180ae69e6b8ef9de12d3
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2025-10-09 11:25:41 -0400

    Add Helidon Instrumentation (#13776)
    Co-authored-by: otelbot <197425009+otelbot@users.noreply.github.com>
    Co-authored-by: Lauri Tulmin <ltulmin@splunk.com>

---
+++ instrumentation/helidon-4.3/javaagent/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+  id("otel.javaagent-instrumentation")
+}
+
+muzzle {
+  pass {
+    group.set("io.helidon.webserver")
+    module.set("helidon-webserver")
+    versions.set("[4.3.0,)")
+    assertInverse.set(true)
+  }
+}
+
+otelJava {
+  minJavaVersionSupported.set(JavaVersion.VERSION_21)
+}
+
+dependencies {
+  library("io.helidon.webserver:helidon-webserver:4.3.0")
+  implementation(project(":instrumentation:helidon-4.3:library"))
+  testImplementation(project(":instrumentation:helidon-4.3:testing"))
+}
---
+++ instrumentation/helidon-4.3/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/helidon/v4_3/HelidonInstrumentation.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.helidon.v4_3;
+
+import static net.bytebuddy.matcher.ElementMatchers.isMethod;
+import static net.bytebuddy.matcher.ElementMatchers.isPublic;
+import static net.bytebuddy.matcher.ElementMatchers.isStatic;
+import static net.bytebuddy.matcher.ElementMatchers.named;
+
+import io.helidon.webserver.http.HttpRouting;
+import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
+import io.opentelemetry.javaagent.extension.instrumentation.TypeTransformer;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+
+public class HelidonInstrumentation implements TypeInstrumentation {
+
+  @Override
+  public ElementMatcher<TypeDescription> typeMatcher() {
+    return named("io.helidon.webserver.http.HttpRouting");
+  }
+
+  @Override
+  public void transform(TypeTransformer transformer) {
+    transformer.applyAdviceToMethod(
+        isMethod().and(isPublic()).and(isStatic()).and(named("builder")),
+        HelidonInstrumentation.class.getName() + "$BuildAdvice");
+  }
+
+  @SuppressWarnings("unused")
+  public static class BuildAdvice {
+
+    @Advice.OnMethodExit(suppress = Throwable.class)
+    public static void onExit(@Advice.Return HttpRouting.Builder httpContext) {
+      HelidonSingletons.FILTERS.forEach(httpContext::addFilter);
+    }
+  }
+}
