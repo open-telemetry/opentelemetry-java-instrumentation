8ae988a1ca3d8cc4ecfb0c41438212225778666b
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-10-11 11:08:46 -0700

    fix

--- smoke-tests/src/test/java/io/opentelemetry/smoketest/SpringBootSmokeTest.java
+++ smoke-tests/src/test/java/io/opentelemetry/smoketest/SpringBootSmokeTest.java
@@ -60,7 +60,7 @@ class SpringBootSmokeTest extends AbstractSmokeTest<Integer> {
                                         ServiceAttributes.SERVICE_NAME, "otel-spring-test-app")
                                     .hasAttribute(
                                         ServiceAttributes.SERVICE_VERSION,
-                                        "2.10.0-alpha-SNAPSHOT")),
+                                        "2.21.0-alpha-SNAPSHOT")),
                 span -> span.hasName("WebController.withSpan")));
 
     // Check agent version is logged on startup
