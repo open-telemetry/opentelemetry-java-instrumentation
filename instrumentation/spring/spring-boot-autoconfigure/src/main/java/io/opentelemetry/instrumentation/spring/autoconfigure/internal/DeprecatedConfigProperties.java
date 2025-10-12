4b770a0e7dbbbf2d1f69a55cc7cc85d1c637a83d
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-10-12 14:38:07 -0700

    fix

--- instrumentation/spring/spring-boot-autoconfigure/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/internal/DeprecatedConfigProperties.java
+++ instrumentation/spring/spring-boot-autoconfigure/src/main/java/io/opentelemetry/instrumentation/spring/autoconfigure/internal/DeprecatedConfigProperties.java
@@ -8,6 +8,7 @@ package io.opentelemetry.instrumentation.spring.autoconfigure.internal;
 import static java.util.logging.Level.WARNING;
 
 import java.util.logging.Logger;
+import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
 
 /**
  * This class is internal and is hence not for public use. Its APIs are unstable and can change at
@@ -18,19 +19,24 @@ public final class DeprecatedConfigProperties {
 
   private static final Logger logger = Logger.getLogger(DeprecatedConfigProperties.class.getName());
 
-  public static boolean getBoolean(
-      InstrumentationConfig config,
+  public static Boolean getBoolean(
+      ApplicationEnvironmentPreparedEvent event,
       String deprecatedPropertyName,
-      String newPropertyName,
-      boolean defaultValue) {
-    warnIfUsed(config, deprecatedPropertyName, newPropertyName);
-    boolean value = config.getBoolean(deprecatedPropertyName, defaultValue);
-    return config.getBoolean(newPropertyName, value);
+      String newPropertyName) {
+    warnIfUsed(event, deprecatedPropertyName, newPropertyName);
+    Boolean value = event.getEnvironment().getProperty(deprecatedPropertyName, Boolean.class);
+    if (value != null) {
+      return value;
+    }
+    return event.getEnvironment().getProperty(newPropertyName, Boolean.class);
   }
 
   private static void warnIfUsed(
-      InstrumentationConfig config, String deprecatedPropertyName, String newPropertyName) {
-    if (config.getString(deprecatedPropertyName) != null) {
+      ApplicationEnvironmentPreparedEvent event,
+      String deprecatedPropertyName,
+      String newPropertyName) {
+    String value = event.getEnvironment().getProperty(deprecatedPropertyName, String.class);
+    if (value != null) {
       logger.log(
           WARNING,
           "Deprecated property \"{0}\" was used; use the \"{1}\" property instead",
