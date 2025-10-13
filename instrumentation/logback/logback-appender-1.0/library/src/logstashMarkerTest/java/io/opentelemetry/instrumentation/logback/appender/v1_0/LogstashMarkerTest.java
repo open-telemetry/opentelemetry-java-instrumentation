cda71d4d37c40617f7da9941a77c45c7014b81dd
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-10-13 12:22:59 -0700

    Update instrumentation/logback/logback-appender-1.0/library/src/logstashMarkerTest/java/io/opentelemetry/instrumentation/logback/appender/v1_0/LogstashMarkerTest.java
    Co-authored-by: Lauri Tulmin <tulmin@gmail.com>

--- instrumentation/logback/logback-appender-1.0/library/src/logstashMarkerTest/java/io/opentelemetry/instrumentation/logback/appender/v1_0/LogstashMarkerTest.java
+++ instrumentation/logback/logback-appender-1.0/library/src/logstashMarkerTest/java/io/opentelemetry/instrumentation/logback/appender/v1_0/LogstashMarkerTest.java
@@ -19,7 +19,7 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class LogstashMarkerTest {
+class LogstashMarkerTest {
   private static final Logger logger = LoggerFactory.getLogger("TestLogger");
 
   @RegisterExtension
