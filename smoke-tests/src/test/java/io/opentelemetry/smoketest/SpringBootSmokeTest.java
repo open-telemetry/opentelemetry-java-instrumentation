65acb386a922519db0dbcee984792a12c5a9359b
otelbot <197425009+otelbot@users.noreply.github.com>
2025-10-13 19:07:02 +0000

    ./gradlew spotlessApply

--- smoke-tests/src/test/java/io/opentelemetry/smoketest/SpringBootSmokeTest.java
+++ smoke-tests/src/test/java/io/opentelemetry/smoketest/SpringBootSmokeTest.java
@@ -58,9 +58,7 @@ class SpringBootSmokeTest extends AbstractSmokeTest<Integer> {
                                     .hasAttribute(AttributeKey.stringKey("foo"), "bar")
                                     .hasAttribute(
                                         ServiceAttributes.SERVICE_NAME, "otel-spring-test-app")
-                                    .hasAttribute(
-                                        ServiceAttributes.SERVICE_VERSION,
-                                        "1.2.3")),
+                                    .hasAttribute(ServiceAttributes.SERVICE_VERSION, "1.2.3")),
                 span -> span.hasName("WebController.withSpan")));
 
     // Check agent version is logged on startup
