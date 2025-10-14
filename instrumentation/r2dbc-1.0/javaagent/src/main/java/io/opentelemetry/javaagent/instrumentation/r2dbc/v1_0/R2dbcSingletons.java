fd8d93f10b0ebc7d035f509c1b377885c69152a6
Lauri Tulmin <ltulmin@splunk.com>
2025-10-14 12:51:10 +0300

    fix

--- instrumentation/r2dbc-1.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/r2dbc/v1_0/R2dbcSingletons.java
+++ instrumentation/r2dbc-1.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/r2dbc/v1_0/R2dbcSingletons.java
@@ -8,29 +8,35 @@ package io.opentelemetry.javaagent.instrumentation.r2dbc.v1_0;
 import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.instrumentation.api.incubator.semconv.net.PeerServiceAttributesExtractor;
 import io.opentelemetry.instrumentation.r2dbc.v1_0.internal.shaded.R2dbcTelemetry;
+import io.opentelemetry.instrumentation.r2dbc.v1_0.internal.shaded.R2dbcTelemetryBuilder;
+import io.opentelemetry.instrumentation.r2dbc.v1_0.internal.shaded.internal.Experimental;
 import io.opentelemetry.instrumentation.r2dbc.v1_0.internal.shaded.internal.R2dbcNetAttributesGetter;
 import io.opentelemetry.javaagent.bootstrap.internal.AgentCommonConfig;
 import io.opentelemetry.javaagent.bootstrap.internal.AgentInstrumentationConfig;
 
 public final class R2dbcSingletons {
 
-  private static final R2dbcTelemetry TELEMETRY =
-      R2dbcTelemetry.builder(GlobalOpenTelemetry.get())
-          .setStatementSanitizationEnabled(
-              AgentInstrumentationConfig.get()
-                  .getBoolean(
-                      "otel.instrumentation.r2dbc.statement-sanitizer.enabled",
-                      AgentCommonConfig.get().isStatementSanitizationEnabled()))
-          .setEnableSqlCommenter(
-              AgentInstrumentationConfig.get()
-                  .getBoolean(
-                      "otel.instrumentation.r2dbc.experimental.sqlcommenter.enabled",
-                      AgentCommonConfig.get().isSqlCommenterEnabled()))
-          .addAttributesExtractor(
-              PeerServiceAttributesExtractor.create(
-                  R2dbcNetAttributesGetter.INSTANCE,
-                  AgentCommonConfig.get().getPeerServiceResolver()))
-          .build();
+  private static final R2dbcTelemetry TELEMETRY;
+
+  static {
+    R2dbcTelemetryBuilder builder =
+        R2dbcTelemetry.builder(GlobalOpenTelemetry.get())
+            .setStatementSanitizationEnabled(
+                AgentInstrumentationConfig.get()
+                    .getBoolean(
+                        "otel.instrumentation.r2dbc.statement-sanitizer.enabled",
+                        AgentCommonConfig.get().isStatementSanitizationEnabled()))
+            .addAttributesExtractor(
+                PeerServiceAttributesExtractor.create(
+                    R2dbcNetAttributesGetter.INSTANCE,
+                    AgentCommonConfig.get().getPeerServiceResolver()));
+    Experimental.setEnableSqlCommenter(builder,
+        AgentInstrumentationConfig.get()
+            .getBoolean(
+                "otel.instrumentation.r2dbc.experimental.sqlcommenter.enabled",
+                AgentCommonConfig.get().isSqlCommenterEnabled()));
+    TELEMETRY = builder.build();
+  }
 
   public static R2dbcTelemetry telemetry() {
     return TELEMETRY;
