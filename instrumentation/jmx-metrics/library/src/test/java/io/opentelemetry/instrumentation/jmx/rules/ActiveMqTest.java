a7939d54e18818ffdf166b5fd8b562b396c69ef0
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2025-10-15 14:17:46 +0200

    start adding tests

--- instrumentation/jmx-metrics/library/src/test/java/io/opentelemetry/instrumentation/jmx/rules/ActiveMqTest.java
+++ instrumentation/jmx-metrics/library/src/test/java/io/opentelemetry/instrumentation/jmx/rules/ActiveMqTest.java
@@ -1,23 +1,27 @@
 package io.opentelemetry.instrumentation.jmx.rules;
 
-import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
-import org.testcontainers.images.builder.ImageFromDockerfile;
+import static io.opentelemetry.instrumentation.jmx.rules.assertions.DataPointAttributes.attribute;
+import static io.opentelemetry.instrumentation.jmx.rules.assertions.DataPointAttributes.attributeGroup;
+
+import io.opentelemetry.instrumentation.jmx.rules.assertions.AttributeMatcher;
+import io.opentelemetry.instrumentation.jmx.rules.assertions.AttributeMatcherGroup;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.images.builder.ImageFromDockerfile;
 
-public class ActiveMqTest extends TargetSystemTest{
-
+public class ActiveMqTest extends TargetSystemTest {
 
   private static final int ACTIVEMQ_PORT = 61616;
 
   // TODO: test both 'classic' and 'artemis' variants
 
   @Test
-  void activemqTest(){
+  void activemqTest() {
     List<String> yamlFiles = Collections.singletonList("activemq.yaml");
 
     yamlFiles.forEach(this::validateYamlSyntax);
@@ -39,5 +43,48 @@ public class ActiveMqTest extends TargetSystemTest{
     copyYamlFilesToTarget(target, yamlFiles);
 
     startTarget(target);
+
+    verifyMetrics(createMetricsVerifier());
+  }
+
+  private static MetricsVerifier createMetricsVerifier() {
+
+    AttributeMatcher messagingSystem = attribute("messaging.system", "activemq");
+    // known attributes from the single topic available by default
+    AttributeMatcher destinationName = attribute("messaging.destination.name",
+        "ActiveMQ.Advisory.MasterBroker");
+    AttributeMatcher topic = attribute("activemq.destination.type", "topic");
+    AttributeMatcher broker = attribute("activemq.broker.name", "localhost");
+
+    AttributeMatcherGroup topicAttributes =
+        attributeGroup(messagingSystem, destinationName, topic, broker);
+
+    return MetricsVerifier.create()
+        // consumers and producers
+        .add("activemq.producer.count", metric -> metric.isUpDownCounter()
+            .hasUnit("{producer}")
+            .hasDataPointsWithAttributes(topicAttributes)
+            .hasDescription("The number of producers attached to this destination"))
+        .add("activemq.consumer.count", metric -> metric.isUpDownCounter()
+            .hasUnit("{consumer}")
+            .hasDataPointsWithAttributes(topicAttributes)
+            .hasDescription("The number of consumers subscribed to this destination"))
+        // message consumption and in-flight
+        .add("activemq.message.queue.size", metric -> metric.isUpDownCounter()
+            .hasUnit("{message}")
+            .hasDataPointsWithAttributes(topicAttributes)
+            .hasDescription("The current number of messages waiting to be consumed"))
+        .add("activemq.message.expired",metric -> metric.isCounter()
+            .hasUnit("{message}")
+            .hasDataPointsWithAttributes(topicAttributes)
+            .hasDescription("The number of messages not delivered because they expired"))
+        .add("activemq.message.enqueued",metric -> metric.isCounter()
+            .hasUnit("{message}")
+            .hasDataPointsWithAttributes(topicAttributes)
+            .hasDescription("The number of messages sent to this destination"))
+        .add("activemq.message.dequeued",metric -> metric.isCounter()
+            .hasUnit("{message}")
+            .hasDataPointsWithAttributes(topicAttributes)
+            .hasDescription("The number of messages acknowledged and removed from this destination"));
   }
 }
