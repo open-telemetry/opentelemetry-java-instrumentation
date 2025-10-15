0ebf5bde0f7f679ceda8237a9bf2369d3ad0dd49
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2025-10-15 19:33:31 +0200

    reformat tests

--- instrumentation/jmx-metrics/library/src/test/java/io/opentelemetry/instrumentation/jmx/rules/ActiveMqTest.java
+++ instrumentation/jmx-metrics/library/src/test/java/io/opentelemetry/instrumentation/jmx/rules/ActiveMqTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package io.opentelemetry.instrumentation.jmx.rules;
 
 import static io.opentelemetry.instrumentation.jmx.rules.assertions.DataPointAttributes.attribute;
@@ -30,14 +35,15 @@ public class ActiveMqTest extends TargetSystemTest {
     jvmArgs.add(javaAgentJvmArgument());
     jvmArgs.addAll(javaPropertiesToJvmArgs(otelConfigProperties(yamlFiles)));
 
-    GenericContainer<?> target = new GenericContainer<>(
-        new ImageFromDockerfile()
-            .withDockerfileFromBuilder(
-                builder -> builder.from("apache/activemq-classic:5.18.6").build()))
-        .withEnv("JAVA_TOOL_OPTIONS", String.join(" ", jvmArgs))
-        .withStartupTimeout(Duration.ofMinutes(2))
-        .withExposedPorts(ACTIVEMQ_PORT)
-        .waitingFor(Wait.forListeningPorts(ACTIVEMQ_PORT));
+    GenericContainer<?> target =
+        new GenericContainer<>(
+                new ImageFromDockerfile()
+                    .withDockerfileFromBuilder(
+                        builder -> builder.from("apache/activemq-classic:5.18.6").build()))
+            .withEnv("JAVA_TOOL_OPTIONS", String.join(" ", jvmArgs))
+            .withStartupTimeout(Duration.ofMinutes(2))
+            .withExposedPorts(ACTIVEMQ_PORT)
+            .waitingFor(Wait.forListeningPorts(ACTIVEMQ_PORT));
 
     copyAgentToTarget(target);
     copyYamlFilesToTarget(target, yamlFiles);
@@ -51,92 +57,158 @@ public class ActiveMqTest extends TargetSystemTest {
 
     AttributeMatcher messagingSystem = attribute("messaging.system", "activemq");
     // known attributes from the single topic available by default
-    AttributeMatcher destinationName = attribute("messaging.destination.name",
-        "ActiveMQ.Advisory.MasterBroker");
+    AttributeMatcher destinationName =
+        attribute("messaging.destination.name", "ActiveMQ.Advisory.MasterBroker");
     AttributeMatcher topic = attribute("activemq.destination.type", "topic");
     AttributeMatcher broker = attribute("activemq.broker.name", "localhost");
 
     AttributeMatcherGroup topicAttributes =
         attributeGroup(messagingSystem, destinationName, topic, broker);
 
-    AttributeMatcherGroup brokerAttributes =
-        attributeGroup(messagingSystem, broker);
+    AttributeMatcherGroup brokerAttributes = attributeGroup(messagingSystem, broker);
 
     return MetricsVerifier.create()
         // consumers and producers
-        .add("activemq.producer.count", metric -> metric.isUpDownCounter()
-            .hasUnit("{producer}")
-            .hasDataPointsWithAttributes(topicAttributes)
-            .hasDescription("The number of producers attached to this destination"))
-        .add("activemq.consumer.count", metric -> metric.isUpDownCounter()
-            .hasUnit("{consumer}")
-            .hasDataPointsWithAttributes(topicAttributes)
-            .hasDescription("The number of consumers subscribed to this destination"))
+        .add(
+            "activemq.producer.count",
+            metric ->
+                metric
+                    .isUpDownCounter()
+                    .hasUnit("{producer}")
+                    .hasDataPointsWithAttributes(topicAttributes)
+                    .hasDescription("The number of producers attached to this destination"))
+        .add(
+            "activemq.consumer.count",
+            metric ->
+                metric
+                    .isUpDownCounter()
+                    .hasUnit("{consumer}")
+                    .hasDataPointsWithAttributes(topicAttributes)
+                    .hasDescription("The number of consumers subscribed to this destination"))
         // message consumption and in-flight
-        .add("activemq.message.queue.size", metric -> metric.isUpDownCounter()
-            .hasUnit("{message}")
-            .hasDataPointsWithAttributes(topicAttributes)
-            .hasDescription("The current number of messages waiting to be consumed"))
-        .add("activemq.message.expired", metric -> metric.isCounter()
-            .hasUnit("{message}")
-            .hasDataPointsWithAttributes(topicAttributes)
-            .hasDescription("The number of messages not delivered because they expired"))
-        .add("activemq.message.enqueued", metric -> metric.isCounter()
-            .hasUnit("{message}")
-            .hasDataPointsWithAttributes(topicAttributes)
-            .hasDescription("The number of messages sent to this destination"))
-        .add("activemq.message.dequeued", metric -> metric.isCounter()
-            .hasUnit("{message}")
-            .hasDataPointsWithAttributes(topicAttributes)
-            .hasDescription(
-                "The number of messages acknowledged and removed from this destination"))
+        .add(
+            "activemq.message.queue.size",
+            metric ->
+                metric
+                    .isUpDownCounter()
+                    .hasUnit("{message}")
+                    .hasDataPointsWithAttributes(topicAttributes)
+                    .hasDescription("The current number of messages waiting to be consumed"))
+        .add(
+            "activemq.message.expired",
+            metric ->
+                metric
+                    .isCounter()
+                    .hasUnit("{message}")
+                    .hasDataPointsWithAttributes(topicAttributes)
+                    .hasDescription("The number of messages not delivered because they expired"))
+        .add(
+            "activemq.message.enqueued",
+            metric ->
+                metric
+                    .isCounter()
+                    .hasUnit("{message}")
+                    .hasDataPointsWithAttributes(topicAttributes)
+                    .hasDescription("The number of messages sent to this destination"))
+        .add(
+            "activemq.message.dequeued",
+            metric ->
+                metric
+                    .isCounter()
+                    .hasUnit("{message}")
+                    .hasDataPointsWithAttributes(topicAttributes)
+                    .hasDescription(
+                        "The number of messages acknowledged and removed from this destination"))
         // destination memory/temp usage and limits
-        .add("activemq.memory.destination.usage", metric -> metric.isUpDownCounter()
-            .hasUnit("By")
-            .hasDataPointsWithAttributes(topicAttributes)
-            .hasDescription("The amount of used memory"))
-        .add("activemq.memory.destination.limit", metric -> metric.isUpDownCounter()
-            .hasUnit("By")
-            .hasDataPointsWithAttributes(topicAttributes)
-            .hasDescription("The amount of configured memory limit"))
-        .add("activemq.temp.destination.utilization", metric -> metric.isGauge()
-            .hasUnit("1")
-            .hasDataPointsWithAttributes(topicAttributes)
-            .hasDescription("The percentage of non-persistent storage used"))
-        .add("activemq.temp.destination.limit", metric -> metric.isUpDownCounter()
-            .hasUnit("By")
-            .hasDataPointsWithAttributes(topicAttributes)
-            .hasDescription("The amount of configured non-persistent storage limit"))
+        .add(
+            "activemq.memory.destination.usage",
+            metric ->
+                metric
+                    .isUpDownCounter()
+                    .hasUnit("By")
+                    .hasDataPointsWithAttributes(topicAttributes)
+                    .hasDescription("The amount of used memory"))
+        .add(
+            "activemq.memory.destination.limit",
+            metric ->
+                metric
+                    .isUpDownCounter()
+                    .hasUnit("By")
+                    .hasDataPointsWithAttributes(topicAttributes)
+                    .hasDescription("The amount of configured memory limit"))
+        .add(
+            "activemq.temp.destination.utilization",
+            metric ->
+                metric
+                    .isGauge()
+                    .hasUnit("1")
+                    .hasDataPointsWithAttributes(topicAttributes)
+                    .hasDescription("The percentage of non-persistent storage used"))
+        .add(
+            "activemq.temp.destination.limit",
+            metric ->
+                metric
+                    .isUpDownCounter()
+                    .hasUnit("By")
+                    .hasDataPointsWithAttributes(topicAttributes)
+                    .hasDescription("The amount of configured non-persistent storage limit"))
         // broker metrics
-        .add("activemq.connection.count", metric ->metric.isUpDownCounter()
-            .hasUnit("{connection}")
-            .hasDataPointsWithAttributes(brokerAttributes)
-            .hasDescription("The number of active connections"))
-        .add("activemq.memory.utilization", metric -> metric.isGauge()
-            .hasUnit("1")
-            .hasDataPointsWithAttributes(brokerAttributes)
-            .hasDescription("The percentage of broker memory used"))
-        .add("activemq.memory.limit", metric -> metric.isUpDownCounter()
-            .hasUnit("By")
-            .hasDataPointsWithAttributes(brokerAttributes)
-            .hasDescription("The amount of configured broker memory limit"))
-        .add("activemq.store.utilization", metric -> metric.isGauge()
-            .hasUnit("1")
-            .hasDataPointsWithAttributes(brokerAttributes)
-            .hasDescription("The percentage of broker persistent storage used"))
-        .add("activemq.store.limit", metric -> metric.isUpDownCounter()
-            .hasUnit("By")
-            .hasDataPointsWithAttributes(brokerAttributes)
-            .hasDescription("The amount of configured broker persistent storage limit"))
-        .add("activemq.temp.utilization", metric -> metric.isGauge()
-            .hasUnit("1")
-            .hasDataPointsWithAttributes(brokerAttributes)
-            .hasDescription("The percentage of broker non-persistent storage used"))
-        .add("activemq.temp.limit", metric -> metric.isUpDownCounter()
-            .hasUnit("By")
-            .hasDataPointsWithAttributes(brokerAttributes)
-            .hasDescription("The amount of configured broker non-persistent storage limit"))
-        ;
-        // TODO broker memory/temp/storage usage and limits
+        .add(
+            "activemq.connection.count",
+            metric ->
+                metric
+                    .isUpDownCounter()
+                    .hasUnit("{connection}")
+                    .hasDataPointsWithAttributes(brokerAttributes)
+                    .hasDescription("The number of active connections"))
+        .add(
+            "activemq.memory.utilization",
+            metric ->
+                metric
+                    .isGauge()
+                    .hasUnit("1")
+                    .hasDataPointsWithAttributes(brokerAttributes)
+                    .hasDescription("The percentage of broker memory used"))
+        .add(
+            "activemq.memory.limit",
+            metric ->
+                metric
+                    .isUpDownCounter()
+                    .hasUnit("By")
+                    .hasDataPointsWithAttributes(brokerAttributes)
+                    .hasDescription("The amount of configured broker memory limit"))
+        .add(
+            "activemq.store.utilization",
+            metric ->
+                metric
+                    .isGauge()
+                    .hasUnit("1")
+                    .hasDataPointsWithAttributes(brokerAttributes)
+                    .hasDescription("The percentage of broker persistent storage used"))
+        .add(
+            "activemq.store.limit",
+            metric ->
+                metric
+                    .isUpDownCounter()
+                    .hasUnit("By")
+                    .hasDataPointsWithAttributes(brokerAttributes)
+                    .hasDescription("The amount of configured broker persistent storage limit"))
+        .add(
+            "activemq.temp.utilization",
+            metric ->
+                metric
+                    .isGauge()
+                    .hasUnit("1")
+                    .hasDataPointsWithAttributes(brokerAttributes)
+                    .hasDescription("The percentage of broker non-persistent storage used"))
+        .add(
+            "activemq.temp.limit",
+            metric ->
+                metric
+                    .isUpDownCounter()
+                    .hasUnit("By")
+                    .hasDataPointsWithAttributes(brokerAttributes)
+                    .hasDescription(
+                        "The amount of configured broker non-persistent storage limit"));
   }
 }
