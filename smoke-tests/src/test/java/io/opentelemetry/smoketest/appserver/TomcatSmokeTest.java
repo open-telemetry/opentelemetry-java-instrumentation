dc60031a44154985ce0dbf9ce59c481a44348825
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-10-15 13:28:56 -0700

    Update app server patch versions that are tested (#14993)

--- smoke-tests/src/test/java/io/opentelemetry/smoketest/appserver/TomcatSmokeTest.java
+++ smoke-tests/src/test/java/io/opentelemetry/smoketest/appserver/TomcatSmokeTest.java
@@ -61,57 +61,57 @@ abstract class TomcatSmokeTest extends AppServerTest {
   @AppServer(version = "8.5.98", jdk = "25-openj9")
   static class Tomcat8Jdk25Openj9 extends TomcatSmokeTest {}
 
-  @AppServer(version = "9.0.85", jdk = "8")
+  @AppServer(version = "9.0.111", jdk = "8")
   static class Tomcat9Jdk8 extends TomcatSmokeTest {}
 
-  @AppServer(version = "9.0.85", jdk = "8-openj9")
+  @AppServer(version = "9.0.111", jdk = "8-openj9")
   static class Tomcat9Jdk8Openj9 extends TomcatSmokeTest {}
 
-  @AppServer(version = "9.0.85", jdk = "11")
+  @AppServer(version = "9.0.111", jdk = "11")
   static class Tomcat9Jdk11 extends TomcatSmokeTest {}
 
-  @AppServer(version = "9.0.85", jdk = "11-openj9")
+  @AppServer(version = "9.0.111", jdk = "11-openj9")
   static class Tomcat9Jdk11Openj9 extends TomcatSmokeTest {}
 
-  @AppServer(version = "9.0.85", jdk = "17")
+  @AppServer(version = "9.0.111", jdk = "17")
   static class Tomcat9Jdk17 extends TomcatSmokeTest {}
 
-  @AppServer(version = "9.0.85", jdk = "17-openj9")
+  @AppServer(version = "9.0.111", jdk = "17-openj9")
   static class Tomcat9Jdk17Openj9 extends TomcatSmokeTest {}
 
-  @AppServer(version = "9.0.85", jdk = "21")
+  @AppServer(version = "9.0.111", jdk = "21")
   static class Tomcat9Jdk21 extends TomcatSmokeTest {}
 
-  @AppServer(version = "9.0.85", jdk = "21-openj9")
+  @AppServer(version = "9.0.111", jdk = "21-openj9")
   static class Tomcat9Jdk21Openj9 extends TomcatSmokeTest {}
 
-  @AppServer(version = "9.0.85", jdk = "25")
+  @AppServer(version = "9.0.111", jdk = "25")
   static class Tomcat9Jdk25 extends TomcatSmokeTest {}
 
-  @AppServer(version = "9.0.85", jdk = "25-openj9")
+  @AppServer(version = "9.0.111", jdk = "25-openj9")
   static class Tomcat9Jdk25Openj9 extends TomcatSmokeTest {}
 
-  @AppServer(version = "10.1.18", jdk = "11")
+  @AppServer(version = "10.1.48", jdk = "11")
   static class Tomcat10Jdk11 extends TomcatSmokeTest {}
 
-  @AppServer(version = "10.1.18", jdk = "11-openj9")
+  @AppServer(version = "10.1.48", jdk = "11-openj9")
   static class Tomcat10Jdk11Openj9 extends TomcatSmokeTest {}
 
-  @AppServer(version = "10.1.18", jdk = "17")
+  @AppServer(version = "10.1.48", jdk = "17")
   static class Tomcat10Jdk17 extends TomcatSmokeTest {}
 
-  @AppServer(version = "10.1.18", jdk = "17-openj9")
+  @AppServer(version = "10.1.48", jdk = "17-openj9")
   static class Tomcat10Jdk17Openj9 extends TomcatSmokeTest {}
 
-  @AppServer(version = "10.1.18", jdk = "21")
+  @AppServer(version = "10.1.48", jdk = "21")
   static class Tomcat10Jdk21 extends TomcatSmokeTest {}
 
-  @AppServer(version = "10.1.18", jdk = "21-openj9")
+  @AppServer(version = "10.1.48", jdk = "21-openj9")
   static class Tomcat10Jdk21Openj9 extends TomcatSmokeTest {}
 
-  @AppServer(version = "10.1.18", jdk = "25")
+  @AppServer(version = "10.1.48", jdk = "25")
   static class Tomcat10Jdk25 extends TomcatSmokeTest {}
 
-  @AppServer(version = "10.1.18", jdk = "25-openj9")
+  @AppServer(version = "10.1.48", jdk = "25-openj9")
   static class Tomcat10Jdk25Openj9 extends TomcatSmokeTest {}
 }
