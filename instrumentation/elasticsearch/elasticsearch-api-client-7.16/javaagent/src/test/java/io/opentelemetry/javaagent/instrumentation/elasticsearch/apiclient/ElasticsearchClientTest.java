5394500456f5d56a196ce24fa928e81efc692626
Lauri Tulmin <ltulmin@splunk.com>
2025-10-17 10:53:58 +0300

    add logging

--- instrumentation/elasticsearch/elasticsearch-api-client-7.16/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/elasticsearch/apiclient/ElasticsearchClientTest.java
+++ instrumentation/elasticsearch/elasticsearch-api-client-7.16/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/elasticsearch/apiclient/ElasticsearchClientTest.java
@@ -40,10 +40,15 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.elasticsearch.ElasticsearchContainer;
 
 @SuppressWarnings("deprecation") // using deprecated semconv
 class ElasticsearchClientTest {
+  private static final Logger logger = LoggerFactory.getLogger(ElasticsearchClientTest.class);
+
   @RegisterExtension
   static final InstrumentationExtension testing = AgentInstrumentationExtension.create();
 
@@ -62,6 +67,7 @@ class ElasticsearchClientTest {
     elasticsearch.withEnv(
         "ES_JAVA_OPTS",
         "-Xmx256m -Xms256m -Dlog4j2.disableJmx=true -Dlog4j2.disable.jmx=true -XX:-UseContainerSupport");
+    elasticsearch.withLogConsumer(new Slf4jLogConsumer(logger));
     elasticsearch.start();
 
     httpHost = HttpHost.create(elasticsearch.getHttpHostAddress());
