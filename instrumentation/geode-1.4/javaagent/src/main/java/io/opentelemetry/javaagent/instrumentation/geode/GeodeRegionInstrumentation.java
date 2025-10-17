50cfcf649dfc2ad36cd5bb367f0a5303057fc19b
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2025-10-17 17:34:42 +0200

    make geode indy-ready

--- instrumentation/geode-1.4/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/geode/GeodeRegionInstrumentation.java
+++ instrumentation/geode-1.4/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/geode/GeodeRegionInstrumentation.java
@@ -19,6 +19,7 @@ import io.opentelemetry.context.Context;
 import io.opentelemetry.context.Scope;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeTransformer;
+import javax.annotation.Nullable;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.matcher.ElementMatcher;
@@ -61,76 +62,83 @@ public class GeodeRegionInstrumentation implements TypeInstrumentation {
         this.getClass().getName() + "$QueryAdvice");
   }
 
+  public static class AdviceScope {
+    public GeodeRequest request;
+    public Context context;
+    public Scope scope;
+
+    public AdviceScope(GeodeRequest request, Context context, Scope scope) {
+      this.request = request;
+      this.context = context;
+      this.scope = scope;
+    }
+
+    @Nullable
+    public static AdviceScope start(
+        Region<?, ?> region, String methodName, @Nullable String query) {
+      Context parentContext = currentContext();
+      GeodeRequest request = GeodeRequest.create(region, methodName, query);
+      if (!instrumenter().shouldStart(parentContext, request)) {
+        return null;
+      }
+
+      Context context = instrumenter().start(parentContext, request);
+      return new AdviceScope(request, context, context.makeCurrent());
+    }
+
+    public void end(@Nullable Throwable throwable) {
+      if (scope != null) {
+        scope.close();
+      }
+      instrumenter().end(context, request, null, throwable);
+    }
+  }
+
   @SuppressWarnings("unused")
   public static class SimpleAdvice {
 
+    public static class AdviceLocals {
+      public GeodeRequest request;
+      public Context context;
+      public Scope scope;
+    }
+
+    @Nullable
     @Advice.OnMethodEnter(suppress = Throwable.class)
-    public static void onEnter(
-        @Advice.This Region<?, ?> region,
-        @Advice.Origin("#m") String methodName,
-        @Advice.Local("otelRequest") GeodeRequest request,
-        @Advice.Local("otelContext") Context context,
-        @Advice.Local("otelScope") Scope scope) {
-
-      Context parentContext = currentContext();
-      request = GeodeRequest.create(region, methodName, null);
-      if (!instrumenter().shouldStart(parentContext, request)) {
-        return;
-      }
-
-      context = instrumenter().start(parentContext, request);
-      scope = context.makeCurrent();
+    public static AdviceScope onEnter(
+        @Advice.This Region<?, ?> region, @Advice.Origin("#m") String methodName) {
+      return AdviceScope.start(region, methodName, null);
     }
 
     @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
     public static void stopSpan(
-        @Advice.Thrown Throwable throwable,
-        @Advice.Local("otelRequest") GeodeRequest request,
-        @Advice.Local("otelContext") Context context,
-        @Advice.Local("otelScope") Scope scope) {
-      if (scope == null) {
-        return;
+        @Advice.Thrown @Nullable Throwable throwable,
+        @Advice.Enter @Nullable AdviceScope adviceScope) {
+      if (adviceScope != null) {
+        adviceScope.end(throwable);
       }
-
-      scope.close();
-      instrumenter().end(context, request, null, throwable);
     }
   }
 
   @SuppressWarnings("unused")
   public static class QueryAdvice {
 
+    @Nullable
     @Advice.OnMethodEnter(suppress = Throwable.class)
-    public static void onEnter(
+    public static AdviceScope onEnter(
         @Advice.This Region<?, ?> region,
         @Advice.Origin("#m") String methodName,
-        @Advice.Argument(0) String query,
-        @Advice.Local("otelRequest") GeodeRequest request,
-        @Advice.Local("otelContext") Context context,
-        @Advice.Local("otelScope") Scope scope) {
-
-      Context parentContext = currentContext();
-      request = GeodeRequest.create(region, methodName, query);
-      if (!instrumenter().shouldStart(parentContext, request)) {
-        return;
-      }
-
-      context = instrumenter().start(parentContext, request);
-      scope = context.makeCurrent();
+        @Advice.Argument(0) String query) {
+      return AdviceScope.start(region, methodName, query);
     }
 
     @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
     public static void stopSpan(
-        @Advice.Thrown Throwable throwable,
-        @Advice.Local("otelRequest") GeodeRequest request,
-        @Advice.Local("otelContext") Context context,
-        @Advice.Local("otelScope") Scope scope) {
-      if (scope == null) {
-        return;
+        @Advice.Thrown @Nullable Throwable throwable,
+        @Advice.Enter @Nullable AdviceScope adviceScope) {
+      if (adviceScope != null) {
+        adviceScope.end(throwable);
       }
-
-      scope.close();
-      instrumenter().end(context, request, null, throwable);
     }
   }
 }
