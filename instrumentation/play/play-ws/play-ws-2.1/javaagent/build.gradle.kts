c37ef46298d54a57ca23fa73f0e5e9b46fc410fe
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-10-17 17:24:26 -0700

    more

--- instrumentation/play/play-ws/play-ws-2.1/javaagent/build.gradle.kts
+++ instrumentation/play/play-ws/play-ws-2.1/javaagent/build.gradle.kts
@@ -67,7 +67,7 @@ tasks {
     enabled = testLatestDeps
   }
 
-  test {
+  withType<Test>().configureEach {
     systemProperty("collectMetadata", findProperty("collectMetadata")?.toString() ?: "false")
     systemProperty("otel.instrumentation.common.peer-service-mapping", "127.0.0.1=test-peer-service,localhost=test-peer-service,192.0.2.1=test-peer-service")
   }
