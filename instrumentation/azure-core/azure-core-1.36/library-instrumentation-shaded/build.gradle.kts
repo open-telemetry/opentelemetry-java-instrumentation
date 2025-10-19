b5c9b30c9dea50eb29b8155ccd896c08888b5f01
Jared Tan <jian.tan@daocloud.io>
2025-10-19 21:27:45 +0800

    bump up azure-core-tracing-opentelemetry from 1.0.0-beta.45 to 1.0.0-beta.61

--- instrumentation/azure-core/azure-core-1.36/library-instrumentation-shaded/build.gradle.kts
+++ instrumentation/azure-core/azure-core-1.36/library-instrumentation-shaded/build.gradle.kts
@@ -6,9 +6,7 @@ plugins {
 group = "io.opentelemetry.javaagent.instrumentation"
 
 dependencies {
-  // this is the last good version that works with indy build
-  // update to 1.49 or latest once https://github.com/Azure/azure-sdk-for-java/pull/42586 is released.
-  implementation("com.azure:azure-core-tracing-opentelemetry:1.0.0-beta.45")
+  implementation("com.azure:azure-core-tracing-opentelemetry:1.0.0-beta.61")
 }
 
 tasks {
