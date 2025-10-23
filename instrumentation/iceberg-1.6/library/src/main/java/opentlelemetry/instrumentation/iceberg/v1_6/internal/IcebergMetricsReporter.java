caca4af3286ebe8bdd5f4fb8afe83ccb21eb53a8
Ghareeb Falazi <ghareeb.falazi@ibm.com>
2025-10-23 18:23:22 +0200

    initial ScanMetrics builders implementation

---
+++ instrumentation/iceberg-1.6/library/src/main/java/opentlelemetry/instrumentation/iceberg/v1_6/internal/IcebergMetricsReporter.java
@@ -0,0 +1,43 @@
+package opentlelemetry.instrumentation.iceberg.v1_6.internal;
+
+import org.apache.iceberg.metrics.CommitReport;
+import org.apache.iceberg.metrics.MetricsReport;
+import org.apache.iceberg.metrics.MetricsReporter;
+import org.apache.iceberg.metrics.ScanMetricsResult;
+import org.apache.iceberg.metrics.ScanReport;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.metrics.DoubleGauge;
+
+public class IcebergMetricsReporter implements MetricsReporter {
+  private static final String INSTRUMENTATION_NAME = "io.opentelemetry.iceberg_1.6";
+  
+  private final OpenTelemetry openTelemetry;
+
+  IcebergMetricsReporter(OpenTelemetry openTelemetry) {
+    this.openTelemetry = openTelemetry;
+  }
+
+  @Override
+  public void report(MetricsReport report) {
+    if (report instanceof ScanReport) {
+      reportScanMetrics((ScanReport) report);
+    } else if (report instanceof CommitReport) {
+      reportCommitMetrics((CommitReport) report);
+    } 
+  }
+
+  void reportScanMetrics(ScanReport scanReport) {
+    final ScanMetricsResult metrics = scanReport.scanMetrics();
+
+    if (metrics.indexedDeleteFiles() != null) {
+      DoubleGauge metric = ScanMetricsBuilder.indexedDeleteFiles(openTelemetry.getMeter(INSTRUMENTATION_NAME));
+      metric.set(metrics.indexedDeleteFiles().value());
+    }
+  }
+
+  void reportCommitMetrics(CommitReport commitReport) {
+    
+  }
+
+}
