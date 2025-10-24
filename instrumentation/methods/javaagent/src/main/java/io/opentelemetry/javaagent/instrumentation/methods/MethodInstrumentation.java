e6d34e7413d33642417bee7b345a767266787ba9
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2025-10-24 13:57:24 +0200

    post-review changes

--- instrumentation/methods/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/methods/MethodInstrumentation.java
+++ instrumentation/methods/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/methods/MethodInstrumentation.java
@@ -25,6 +25,7 @@ import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeTransformer;
 import java.util.Collection;
 import java.util.Map;
+import javax.annotation.Nullable;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.asm.Advice.AssignReturned;
 import net.bytebuddy.description.enumeration.EnumerationDescription;
@@ -92,30 +93,46 @@ public class MethodInstrumentation implements TypeInstrumentation {
   @SuppressWarnings("unused")
   public static class MethodAdvice {
 
-    public static class AdviceLocals {
-      public MethodAndType classAndMethod;
-      public Context context;
-      public Scope scope;
+    public static class AdviceScope {
+      private final MethodAndType classAndMethod;
+      private final Context context;
+      private final Scope scope;
+
+      public AdviceScope(MethodAndType classAndMethod, Context context, Scope scope) {
+        this.classAndMethod = classAndMethod;
+        this.context = context;
+        this.scope = scope;
+      }
+
+      @Nullable
+      public static AdviceScope start(
+          SpanKind spanKind, Class<?> declaringClass, String methodName) {
+        Context parentContext = currentContext();
+        MethodAndType methodAndType =
+            MethodAndType.create(ClassAndMethod.create(declaringClass, methodName), spanKind);
+
+        if (!instrumenter().shouldStart(parentContext, methodAndType)) {
+          return null;
+        }
+        Context context = instrumenter().start(parentContext, methodAndType);
+        return new AdviceScope(methodAndType, context, context.makeCurrent());
+      }
+
+      public Object end(
+          Class<?> methodReturnType, Object returnValue, @Nullable Throwable throwable) {
+        scope.close();
+        return AsyncOperationEndSupport.create(instrumenter(), Void.class, methodReturnType)
+            .asyncEnd(context, classAndMethod, returnValue, throwable);
+      }
     }
 
+    @Nullable
     @Advice.OnMethodEnter(suppress = Throwable.class)
-    public static AdviceLocals onEnter(
+    public static AdviceScope onEnter(
         @MethodSpanKind SpanKind spanKind,
         @Advice.Origin("#t") Class<?> declaringClass,
         @Advice.Origin("#m") String methodName) {
-
-      AdviceLocals locals = new AdviceLocals();
-      Context parentContext = currentContext();
-      locals.classAndMethod =
-          MethodAndType.create(ClassAndMethod.create(declaringClass, methodName), spanKind);
-
-      if (!instrumenter().shouldStart(parentContext, locals.classAndMethod)) {
-        return locals;
-      }
-
-      locals.context = instrumenter().start(parentContext, locals.classAndMethod);
-      locals.scope = locals.context.makeCurrent();
-      return locals;
+      return AdviceScope.start(spanKind, declaringClass, methodName);
     }
 
     @AssignReturned.ToReturned
@@ -123,15 +140,13 @@ public class MethodInstrumentation implements TypeInstrumentation {
     public static Object stopSpan(
         @MethodReturnType Class<?> methodReturnType,
         @Advice.Return(typing = Assigner.Typing.DYNAMIC) Object returnValue,
-        @Advice.Thrown Throwable throwable,
-        @Advice.Enter AdviceLocals locals) {
-      if (locals.scope == null) {
-        return returnValue;
-      }
-      locals.scope.close();
+        @Advice.Thrown @Nullable Throwable throwable,
+        @Advice.Enter @Nullable AdviceScope adviceScope) {
 
-      return AsyncOperationEndSupport.create(instrumenter(), Void.class, methodReturnType)
-          .asyncEnd(locals.context, locals.classAndMethod, returnValue, throwable);
+      if (adviceScope != null) {
+        return adviceScope.end(methodReturnType, returnValue, throwable);
+      }
+      return returnValue;
     }
   }
 }
