5c304a46daaa27a89f12c3c463deb43b4911f823
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2025-10-24 11:41:46 +0200

    make mybatis indy-ready

--- instrumentation/mybatis-3.2/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/mybatis/v3_2/MapperMethodInstrumentation.java
+++ instrumentation/mybatis-3.2/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/mybatis/v3_2/MapperMethodInstrumentation.java
@@ -5,7 +5,6 @@
 
 package io.opentelemetry.javaagent.instrumentation.mybatis.v3_2;
 
-import static io.opentelemetry.javaagent.bootstrap.Java8BytecodeBridge.currentContext;
 import static io.opentelemetry.javaagent.instrumentation.mybatis.v3_2.MyBatisSingletons.instrumenter;
 import static net.bytebuddy.matcher.ElementMatchers.named;
 
@@ -14,6 +13,7 @@ import io.opentelemetry.context.Scope;
 import io.opentelemetry.instrumentation.api.incubator.semconv.util.ClassAndMethod;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeTransformer;
+import javax.annotation.Nullable;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.matcher.ElementMatcher;
@@ -35,36 +35,52 @@ public class MapperMethodInstrumentation implements TypeInstrumentation {
   @SuppressWarnings("unused")
   public static class ExecuteAdvice {
 
+    public static class AdviceScope {
+      private final ClassAndMethod classAndMethod;
+      private final Context context;
+      private final Scope scope;
+
+      private AdviceScope(ClassAndMethod classAndMethod, Context context, Scope scope) {
+        this.classAndMethod = classAndMethod;
+        this.context = context;
+        this.scope = scope;
+      }
+
+      @Nullable
+      public static AdviceScope start(@Nullable SqlCommand command) {
+        if (command == null) {
+          return null;
+        }
+        ClassAndMethod classAndMethod = SqlCommandUtil.getClassAndMethod(command);
+        if (classAndMethod == null) {
+          return null;
+        }
+        Context parentContext = Context.current();
+        if (!instrumenter().shouldStart(parentContext, classAndMethod)) {
+          return null;
+        }
+        Context context = instrumenter().start(parentContext, classAndMethod);
+        return new AdviceScope(classAndMethod, context, context.makeCurrent());
+      }
+
+      public void end(@Nullable Throwable throwable) {
+        scope.close();
+        instrumenter().end(context, classAndMethod, null, throwable);
+      }
+    }
+
+    @Nullable
     @Advice.OnMethodEnter(suppress = Throwable.class)
-    public static void getMapperInfo(
-        @Advice.FieldValue("command") SqlCommand command,
-        @Advice.Local("otelRequest") ClassAndMethod request,
-        @Advice.Local("otelContext") Context context,
-        @Advice.Local("otelScope") Scope scope) {
-      if (command == null) {
-        return;
-      }
-      request = SqlCommandUtil.getClassAndMethod(command);
-      if (request == null) {
-        return;
-      }
-      Context parentContext = currentContext();
-      if (!instrumenter().shouldStart(parentContext, request)) {
-        return;
-      }
-      context = instrumenter().start(parentContext, request);
-      scope = context.makeCurrent();
+    public static AdviceScope getMapperInfo(@Advice.FieldValue("command") SqlCommand command) {
+      return AdviceScope.start(command);
     }
 
     @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
     public static void stopSpan(
-        @Advice.Thrown Throwable throwable,
-        @Advice.Local("otelRequest") ClassAndMethod request,
-        @Advice.Local("otelContext") Context context,
-        @Advice.Local("otelScope") Scope scope) {
-      if (scope != null) {
-        scope.close();
-        instrumenter().end(context, request, null, throwable);
+        @Advice.Thrown @Nullable Throwable throwable,
+        @Advice.Enter @Nullable AdviceScope adviceScope) {
+      if (adviceScope != null) {
+        adviceScope.end(throwable);
       }
     }
   }
