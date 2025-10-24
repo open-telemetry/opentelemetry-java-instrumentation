6c4c934380732443f9ddadf6a7ee314fc9d08cee
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2025-10-24 14:19:46 +0200

    testing-common

--- testing-common/integration-tests/src/main/java/context/ContextTestInstrumentationModule.java
+++ testing-common/integration-tests/src/main/java/context/ContextTestInstrumentationModule.java
@@ -10,10 +10,12 @@ import static java.util.Collections.singletonList;
 import com.google.auto.service.AutoService;
 import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
+import io.opentelemetry.javaagent.extension.instrumentation.internal.ExperimentalInstrumentationModule;
 import java.util.List;
 
 @AutoService(InstrumentationModule.class)
-public class ContextTestInstrumentationModule extends InstrumentationModule {
+public class ContextTestInstrumentationModule extends InstrumentationModule
+    implements ExperimentalInstrumentationModule {
   public ContextTestInstrumentationModule() {
     super("context-test-instrumentation");
   }
@@ -26,5 +28,10 @@ public class ContextTestInstrumentationModule extends InstrumentationModule {
   @Override
   public List<TypeInstrumentation> typeInstrumentations() {
     return singletonList(new ContextTestInstrumentation());
+  }
+
+  @Override
+  public boolean isIndyReady() {
+    return true;
   }
 }
