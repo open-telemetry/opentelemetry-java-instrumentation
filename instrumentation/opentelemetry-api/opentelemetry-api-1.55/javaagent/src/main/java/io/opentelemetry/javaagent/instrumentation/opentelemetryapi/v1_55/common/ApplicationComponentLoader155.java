3e8918d4b847444d852ea75c4b9a6e951fe3342c
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-10-29 17:41:35 +0100

    add ExtendedOpenTelemetry

---
+++ instrumentation/opentelemetry-api/opentelemetry-api-1.55/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/opentelemetryapi/v1_55/common/ApplicationComponentLoader155.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.opentelemetryapi.v1_55.common;
+
+import application.io.opentelemetry.common.ComponentLoader;
+
+public class ApplicationComponentLoader155 implements ComponentLoader {
+
+  private final io.opentelemetry.common.ComponentLoader componentLoader;
+
+  public ApplicationComponentLoader155(io.opentelemetry.common.ComponentLoader componentLoader) {
+    this.componentLoader = componentLoader;
+  }
+
+  @Override
+  public <T> Iterable<T> load(Class<T> aClass) {
+    return componentLoader.load(aClass);
+  }
+}
---
+++ instrumentation/opentelemetry-api/opentelemetry-api-1.55/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/opentelemetryapi/v1_55/incubator/ExtendedOpenTelemetryTest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.opentelemetryapi.v1_55.incubator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.incubator.ExtendedOpenTelemetry;
+import io.opentelemetry.api.incubator.config.DeclarativeConfigProperties;
+import io.opentelemetry.instrumentation.testing.junit.AgentInstrumentationExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+class ExtendedOpenTelemetryTest {
+  @RegisterExtension
+  static final AgentInstrumentationExtension testing = AgentInstrumentationExtension.create();
+
+  private ExtendedOpenTelemetry extendedOpenTelemetry;
+
+  @BeforeEach
+  void setup() {
+    extendedOpenTelemetry = (ExtendedOpenTelemetry) GlobalOpenTelemetry.get();
+  }
+
+  @Test
+  void getConfig() {
+    DeclarativeConfigProperties instrumentationConfig =
+        extendedOpenTelemetry.getConfigProvider().getInstrumentationConfig();
+    // todo test for not null with declarative config present
+    assertThat(instrumentationConfig).isNull();
+  }
+}
