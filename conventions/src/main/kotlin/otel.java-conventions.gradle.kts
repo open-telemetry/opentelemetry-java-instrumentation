16c1eeee8cf5362931cbb0aa22ec01533dfed912
Lauri Tulmin <ltulmin@splunk.com>
2025-10-30 08:18:59 +0200

    Try to work around openj9 jit crash

--- conventions/src/main/kotlin/otel.java-conventions.gradle.kts
+++ conventions/src/main/kotlin/otel.java-conventions.gradle.kts
@@ -342,7 +342,10 @@ tasks.withType<Test>().configureEach {
   val useJ9 = gradle.startParameter.projectProperties["testJavaVM"]?.run { this == "openj9" }
     ?: false
   if (useJ9 && testJavaVersion != null && testJavaVersion.isJava8) {
-    jvmArgs("-Xjit:exclude={io/opentelemetry/testing/internal/io/netty/buffer/HeapByteBufUtil.*},exclude={io/opentelemetry/testing/internal/io/netty/buffer/UnpooledHeapByteBuf.*},exclude={io/opentelemetry/testing/internal/io/netty/buffer/AbstractByteBuf.*}")
+    jvmArgs("-Xjit:exclude={io/opentelemetry/testing/internal/io/netty/buffer/HeapByteBufUtil.*}," +
+        "exclude={io/opentelemetry/testing/internal/io/netty/buffer/UnpooledHeapByteBuf.*}," +
+        "exclude={io/opentelemetry/testing/internal/io/netty/buffer/AbstractByteBuf.*}," +
+        "exclude={io/opentelemetry/testing/internal/io/netty/handler/codec/base64/Base64.*}")
   }
 
   // There's no real harm in setting this for all tests even if any happen to not be using context
