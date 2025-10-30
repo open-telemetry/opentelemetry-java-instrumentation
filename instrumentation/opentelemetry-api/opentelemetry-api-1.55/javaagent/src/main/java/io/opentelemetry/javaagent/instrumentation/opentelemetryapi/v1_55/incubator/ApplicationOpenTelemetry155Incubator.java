bae78fb2e7d5761e1ac70cda8d92a91930c665cb
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-10-30 11:13:13 +0100

    add ExtendedOpenTelemetry

--- instrumentation/opentelemetry-api/opentelemetry-api-1.55/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/opentelemetryapi/v1_55/incubator/ApplicationOpenTelemetry155Incubator.java
+++ instrumentation/opentelemetry-api/opentelemetry-api-1.55/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/opentelemetryapi/v1_55/incubator/ApplicationOpenTelemetry155Incubator.java
@@ -5,6 +5,7 @@
 
 package io.opentelemetry.javaagent.instrumentation.opentelemetryapi.v1_55.incubator;
 
+import application.io.opentelemetry.api.OpenTelemetry;
 import application.io.opentelemetry.api.incubator.ExtendedOpenTelemetry;
 import application.io.opentelemetry.api.incubator.config.ConfigProvider;
 import io.opentelemetry.javaagent.instrumentation.opentelemetryapi.v1_27.ApplicationOpenTelemetry127;
@@ -12,6 +13,10 @@ import io.opentelemetry.javaagent.instrumentation.opentelemetryapi.v1_55.incubat
 
 public class ApplicationOpenTelemetry155Incubator extends ApplicationOpenTelemetry127
     implements ExtendedOpenTelemetry {
+
+  // Accessed with reflection
+  @SuppressWarnings("unused")
+  public static final OpenTelemetry INSTANCE = new ApplicationOpenTelemetry155Incubator();
 
   private final ConfigProvider configProvider =
       new ApplicationConfigProvider155Incubator(
