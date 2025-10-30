37555397bd3f5f10f13a43fd20f35a37bd6d2b52
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-10-30 12:26:41 +0100

    add ExtendedOpenTelemetry

--- instrumentation/opentelemetry-api/opentelemetry-api-1.55/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/opentelemetryapi/v1_55/incubator/ApplicationOpenTelemetry155Incubator.java
+++ instrumentation/opentelemetry-api/opentelemetry-api-1.55/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/opentelemetryapi/v1_55/incubator/ApplicationOpenTelemetry155Incubator.java
@@ -8,21 +8,36 @@ package io.opentelemetry.javaagent.instrumentation.opentelemetryapi.v1_55.incuba
 import application.io.opentelemetry.api.OpenTelemetry;
 import application.io.opentelemetry.api.incubator.ExtendedOpenTelemetry;
 import application.io.opentelemetry.api.incubator.config.ConfigProvider;
+import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.javaagent.instrumentation.opentelemetryapi.v1_27.ApplicationOpenTelemetry127;
 import io.opentelemetry.javaagent.instrumentation.opentelemetryapi.v1_55.incubator.config.ApplicationConfigProvider155Incubator;
+import javax.annotation.Nullable;
 
 public class ApplicationOpenTelemetry155Incubator extends ApplicationOpenTelemetry127
     implements ExtendedOpenTelemetry {
 
   // Accessed with reflection
+  @Nullable
   @SuppressWarnings("unused")
-  public static final OpenTelemetry INSTANCE = new ApplicationOpenTelemetry155Incubator();
+  public static final OpenTelemetry INSTANCE = create();
 
-  private final ConfigProvider configProvider =
-      new ApplicationConfigProvider155Incubator(
-          ((io.opentelemetry.api.incubator.ExtendedOpenTelemetry)
-                  io.opentelemetry.api.GlobalOpenTelemetry.get())
+  private final ConfigProvider configProvider;
+
+  @Nullable
+  private static ApplicationOpenTelemetry155Incubator create() {
+    io.opentelemetry.api.OpenTelemetry openTelemetry = GlobalOpenTelemetry.get();
+    if (openTelemetry instanceof io.opentelemetry.api.incubator.ExtendedOpenTelemetry) {
+      return new ApplicationOpenTelemetry155Incubator(
+          ((io.opentelemetry.api.incubator.ExtendedOpenTelemetry) openTelemetry)
               .getConfigProvider());
+    }
+    return null;
+  }
+
+  public ApplicationOpenTelemetry155Incubator(
+      io.opentelemetry.api.incubator.config.ConfigProvider configProvider) {
+    this.configProvider = new ApplicationConfigProvider155Incubator(configProvider);
+  }
 
   @Override
   public ConfigProvider getConfigProvider() {
