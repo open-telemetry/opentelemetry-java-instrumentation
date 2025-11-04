a42c03e987a3269c684a95d10e02b7b36db1e733
Ghareeb Falazi <ghareeb.falazi@ibm.com>
2025-11-04 13:20:17 +0100

    make only class in testing project public to pass javadoc task

--- instrumentation/iceberg-1.8/library/build.gradle.kts
+++ instrumentation/iceberg-1.8/library/build.gradle.kts
@@ -7,3 +7,9 @@ dependencies {
   library("org.apache.iceberg:iceberg-core:1.8.1")
   testImplementation(project(":instrumentation:iceberg-1.8:testing"))
 }
+
+tasks {
+  test {
+    systemProperty("collectMetadata", findProperty("collectMetadata")?.toString() ?: "false")
+  }
+}
