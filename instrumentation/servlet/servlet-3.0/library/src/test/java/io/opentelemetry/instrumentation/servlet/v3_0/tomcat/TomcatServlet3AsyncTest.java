133ca091be855046f2492fd6076fe4be95836fc2
Tyler Benson <734411+tylerbenson@users.noreply.github.com>
2025-11-05 17:11:55 -0500

    fix package name

---
+++ instrumentation/servlet/servlet-3.0/library/src/test/java/io/opentelemetry/instrumentation/servlet/v3_0/tomcat/TomcatServlet3AsyncTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.instrumentation.servlet.v3_0.tomcat;
+
+import static io.opentelemetry.instrumentation.testing.junit.http.ServerEndpoint.SUCCESS;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.instrumentation.servlet.v3_0.TestServlet3;
+import io.opentelemetry.testing.internal.armeria.common.AggregatedHttpRequest;
+import io.opentelemetry.testing.internal.armeria.common.AggregatedHttpResponse;
+import io.opentelemetry.testing.internal.armeria.common.HttpMethod;
+import javax.servlet.Servlet;
+import org.junit.jupiter.api.Test;
+
+class TomcatServlet3AsyncTest extends TomcatServlet3Test {
+
+  @Override
+  public Class<? extends Servlet> servlet() {
+    return TestServlet3.Async.class;
+  }
+
+  @Override
+  public boolean errorEndpointUsesSendError() {
+    return false;
+  }
+
+  @Test
+  void startAsyncInSpan() {
+    AggregatedHttpRequest request =
+        AggregatedHttpRequest.of(
+            HttpMethod.GET, resolveAddress(SUCCESS, "h1c://") + "?startAsyncInSpan=true");
+    AggregatedHttpResponse response = client.execute(request).aggregate().join();
+
+    assertThat(response.status().code()).isEqualTo(SUCCESS.getStatus());
+    assertThat(response.contentUtf8()).isEqualTo(SUCCESS.getBody());
+
+    testing()
+        .waitAndAssertTraces(
+            trace ->
+                trace.hasSpansSatisfyingExactly(
+                    span ->
+                        span.hasName("GET " + getContextPath() + "/*")
+                            .hasKind(SpanKind.SERVER)
+                            .hasNoParent(),
+                    span ->
+                        span.hasName("startAsync")
+                            .hasKind(SpanKind.INTERNAL)
+                            .hasParent(trace.getSpan(0)),
+                    span ->
+                        span.hasName("controller")
+                            .hasKind(SpanKind.INTERNAL)
+                            .hasParent(trace.getSpan(0))));
+  }
+}
