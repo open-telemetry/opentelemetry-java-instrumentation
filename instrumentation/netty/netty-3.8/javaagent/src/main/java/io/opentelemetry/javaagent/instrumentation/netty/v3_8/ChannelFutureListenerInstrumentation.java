f044ea4128e6e7c091aa392c639c8d0ed74e69d1
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2025-11-06 15:02:59 +0100

    move some netty virtual fields for indy-ready

--- instrumentation/netty/netty-3.8/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/netty/v3_8/ChannelFutureListenerInstrumentation.java
+++ instrumentation/netty/netty-3.8/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/netty/v3_8/ChannelFutureListenerInstrumentation.java
@@ -7,19 +7,18 @@ package io.opentelemetry.javaagent.instrumentation.netty.v3_8;
 
 import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.hasClassesNamed;
 import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.implementsInterface;
+import static io.opentelemetry.javaagent.instrumentation.netty.v3_8.VirtualFieldHelper.CONNECTION_CONTEXT;
 import static net.bytebuddy.matcher.ElementMatchers.isMethod;
 import static net.bytebuddy.matcher.ElementMatchers.named;
 import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
 
 import io.opentelemetry.context.Context;
 import io.opentelemetry.context.Scope;
-import io.opentelemetry.instrumentation.api.util.VirtualField;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeTransformer;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.matcher.ElementMatcher;
-import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.ChannelFuture;
 
 public class ChannelFutureListenerInstrumentation implements TypeInstrumentation {
@@ -58,10 +57,7 @@ public class ChannelFutureListenerInstrumentation implements TypeInstrumentation
         return null;
       }
 
-      VirtualField<Channel, NettyConnectionContext> virtualField =
-          VirtualField.find(Channel.class, NettyConnectionContext.class);
-
-      NettyConnectionContext connectionContext = virtualField.get(future.getChannel());
+      NettyConnectionContext connectionContext = CONNECTION_CONTEXT.get(future.getChannel());
       if (connectionContext == null) {
         return null;
       }
