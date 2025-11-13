94def77370ef83c86f6566b430441fd7511ae019
Lauri Tulmin <ltulmin@splunk.com>
2025-11-13 10:04:08 +0200

    test with 3.2

--- instrumentation/jfinal-3.2/javaagent/build.gradle.kts
+++ instrumentation/jfinal-3.2/javaagent/build.gradle.kts
@@ -13,13 +13,13 @@ muzzle {
 
 if (!(findProperty("testLatestDeps") as Boolean)) {
   otelJava {
-    // jfinal 3.6 doesn't work with Java 9+
+    // jfinal 3.2 doesn't work with Java 9+
     maxJavaVersionForTests.set(JavaVersion.VERSION_1_8)
   }
 }
 
 dependencies {
-  library("com.jfinal:jfinal:3.6")
+  library("com.jfinal:jfinal:3.2")
   testLibrary("com.jfinal:jetty-server:2019.3")
   testInstrumentation(project(":instrumentation:jetty:jetty-8.0:javaagent"))
   testInstrumentation(project(":instrumentation:jetty:jetty-11.0:javaagent"))
