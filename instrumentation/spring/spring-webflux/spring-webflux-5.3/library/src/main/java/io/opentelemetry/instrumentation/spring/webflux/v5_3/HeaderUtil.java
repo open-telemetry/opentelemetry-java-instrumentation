fb2acf97a83e044a78f0e751c10b7b08c38db9c2
Lauri Tulmin <ltulmin@splunk.com>
2025-11-14 15:49:35 +0200

    Fix spring webflux latest dep tests (#15304)

---
+++ instrumentation/spring/spring-webflux/spring-webflux-5.3/library/src/main/java/io/opentelemetry/instrumentation/spring/webflux/v5_3/HeaderUtil.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.instrumentation.spring.webflux.v5_3;
+
+import static java.util.Collections.emptyList;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.util.List;
+import javax.annotation.Nullable;
+import org.springframework.http.HttpHeaders;
+
+class HeaderUtil {
+  @Nullable private static final MethodHandle GET_HEADERS;
+
+  static {
+    // since spring web 7.0
+    MethodHandle methodHandle =
+        findGetHeadersMethod(MethodType.methodType(List.class, String.class, List.class));
+    if (methodHandle == null) {
+      // up to spring web 7.0
+      methodHandle =
+          findGetHeadersMethod(MethodType.methodType(Object.class, Object.class, Object.class));
+    }
+    GET_HEADERS = methodHandle;
+  }
+
+  private static MethodHandle findGetHeadersMethod(MethodType methodType) {
+    try {
+      return MethodHandles.lookup().findVirtual(HttpHeaders.class, "getOrDefault", methodType);
+    } catch (Throwable t) {
+      return null;
+    }
+  }
+
+  @SuppressWarnings("unchecked") // casting MethodHandle.invoke result
+  static List<String> getHeader(HttpHeaders headers, String name) {
+    if (GET_HEADERS != null) {
+      try {
+        return (List<String>) GET_HEADERS.invoke(headers, name, emptyList());
+      } catch (Throwable t) {
+        // ignore
+      }
+    }
+    return emptyList();
+  }
+
+  private HeaderUtil() {}
+}
