8ad255438141e395f5edb86a909d2d3e1cb55b2f
jackshirazi <jacks@fasterj.com>
2025-11-17 18:17:03 +0000

    Create OptionChangeListener.java

---
+++ instrumentation-api-incubator/src/main/java/io/opentelemetry/instrumentation/api/incubator/config/OptionChangeListener.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.instrumentation.api.incubator.config;
+
+import javax.annotation.Nullable;
+
+/**
+ * Listener interface for option value changes.
+ *
+ * <p>This class is internal and is hence not for public use. Its APIs are unstable and can change at
+ * any time.
+ */
+public interface OptionChangeListener {
+
+  /**
+   * Called when the value of an option changes.
+   *
+   * @param key the option key
+   * @param newValue the new value, or {@code null} if the option was removed
+   * @param oldValue the previous value, or {@code null} if the option was not set
+   */
+  void onOptionChanged(String key, @Nullable String newValue, @Nullable String oldValue);
+}
