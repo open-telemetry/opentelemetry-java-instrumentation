6e4e36e118e4b6fd80fb2ccc86a462a8c622c732
pepeshore <wu.cc@alibaba-inc.com>
2025-11-18 08:27:10 +0800

    Add support for JFinal (#15216)
    Co-authored-by: otelbot <197425009+otelbot@users.noreply.github.com>
    Co-authored-by: Lauri Tulmin <ltulmin@splunk.com>
    Co-authored-by: Steve Rao <raozihao.rzh@alibaba-inc.com>
    Co-authored-by: Lauri Tulmin <tulmin@gmail.com>

---
+++ instrumentation/jfinal-3.2/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/jfinal/TestConfig.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.jfinal;
+
+import com.jfinal.config.Constants;
+import com.jfinal.config.Handlers;
+import com.jfinal.config.Interceptors;
+import com.jfinal.config.JFinalConfig;
+import com.jfinal.config.Plugins;
+import com.jfinal.config.Routes;
+import com.jfinal.template.Engine;
+
+public class TestConfig extends JFinalConfig {
+  @Override
+  public void configConstant(Constants me) {
+    me.setDevMode(true);
+  }
+
+  @Override
+  public void configRoute(Routes me) {
+    me.add("/", TestController.class);
+  }
+
+  @Override
+  public void configEngine(Engine me) {}
+
+  @Override
+  public void configPlugin(Plugins me) {}
+
+  @Override
+  public void configInterceptor(Interceptors me) {}
+
+  @Override
+  public void configHandler(Handlers me) {}
+}
