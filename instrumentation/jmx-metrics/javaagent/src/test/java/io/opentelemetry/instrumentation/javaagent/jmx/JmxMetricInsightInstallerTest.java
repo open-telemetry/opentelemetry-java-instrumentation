be7d07c6b0b00db9bf5df7adffacbccc1367018c
SylvainJuge <763082+SylvainJuge@users.noreply.github.com>
2025-11-19 18:44:13 +0100

    jmx idiomatic library API - part 1 (#15220)

--- instrumentation/jmx-metrics/javaagent/src/test/java/io/opentelemetry/instrumentation/javaagent/jmx/JmxMetricInsightInstallerTest.java
+++ instrumentation/jmx-metrics/javaagent/src/test/java/io/opentelemetry/instrumentation/javaagent/jmx/JmxMetricInsightInstallerTest.java
@@ -7,18 +7,13 @@ package io.opentelemetry.instrumentation.javaagent.jmx;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import io.opentelemetry.instrumentation.jmx.yaml.JmxConfig;
-import io.opentelemetry.instrumentation.jmx.yaml.JmxRule;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.instrumentation.jmx.JmxTelemetry;
 import io.opentelemetry.instrumentation.jmx.yaml.RuleParser;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import org.junit.jupiter.api.Test;
 
@@ -39,34 +34,14 @@ class JmxMetricInsightInstallerTest {
     assertThat(parser).isNotNull();
 
     Path path = Paths.get(PATH_TO_ALL_EXISTING_RULES);
-    assertThat(Files.exists(path)).isTrue();
+    assertThat(path).isNotEmptyDirectory();
 
-    File existingRulesDir = path.toFile();
-    File[] existingRules = existingRulesDir.listFiles();
-    Set<String> filesChecked = new HashSet<>();
+    for (String file : FILES_TO_BE_TESTED) {
+      Path filePath = path.resolve(file);
+      assertThat(filePath).isRegularFile();
 
-    for (File file : existingRules) {
-      // make sure we only test the files that we supposed to test
-      String fileName = file.getName();
-      if (FILES_TO_BE_TESTED.contains(fileName)) {
-        testRulesAreValid(file, parser);
-        filesChecked.add(fileName);
-      }
-    }
-    // make sure we checked all the files that are supposed to be here
-    assertThat(filesChecked).isEqualTo(FILES_TO_BE_TESTED);
-  }
-
-  void testRulesAreValid(File file, RuleParser parser) throws Exception {
-    try (InputStream inputStream = new FileInputStream(file)) {
-      JmxConfig config = parser.loadConfig(inputStream);
-      assertThat(config).isNotNull();
-
-      List<JmxRule> defs = config.getRules();
-      // make sure all the rules in that file are valid
-      for (JmxRule rule : defs) {
-        rule.buildMetricDef();
-      }
+      // loading rules from direct file access
+      JmxTelemetry.builder(OpenTelemetry.noop()).addCustomRules(filePath);
     }
   }
 }
---
+++ instrumentation/jmx-metrics/library/src/test/java/io/opentelemetry/instrumentation/jmx/JmxTelemetryTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.instrumentation.jmx;
+
+import static io.opentelemetry.sdk.testing.assertj.OpenTelemetryAssertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import io.opentelemetry.api.OpenTelemetry;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+public class JmxTelemetryTest {
+
+  @Test
+  void createDefault() {
+    JmxTelemetryBuilder builder = JmxTelemetry.builder(OpenTelemetry.noop());
+    assertThat(builder.build()).isNotNull();
+  }
+
+  @Test
+  void missingClasspathTarget() {
+    JmxTelemetryBuilder builder = JmxTelemetry.builder(OpenTelemetry.noop());
+    assertThatThrownBy(() -> builder.addClassPathRules("should-not-exist"))
+        .isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  void invalidClasspathTarget() {
+    JmxTelemetryBuilder builder = JmxTelemetry.builder(OpenTelemetry.noop());
+    assertThatThrownBy(() -> builder.addClassPathRules("invalid"))
+        .isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  void knownClassPathTarget() {
+    JmxTelemetry.builder(OpenTelemetry.noop()).addClassPathRules("jvm").build();
+  }
+
+  @Test
+  void invalidExternalYaml(@TempDir Path dir) throws Exception {
+    Path invalid = Files.createTempFile(dir, "invalid", ".yaml");
+    Files.write(invalid, ":this !is /not YAML".getBytes(StandardCharsets.UTF_8));
+    JmxTelemetryBuilder builder = JmxTelemetry.builder(OpenTelemetry.noop());
+    assertThatThrownBy(() -> builder.addCustomRules(invalid))
+        .isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  void invalidStartDelay() {
+    JmxTelemetryBuilder builder = JmxTelemetry.builder(OpenTelemetry.noop());
+    assertThatThrownBy(() -> builder.beanDiscoveryDelay(Duration.ofMillis(-1)))
+        .isInstanceOf(IllegalArgumentException.class);
+  }
+}
