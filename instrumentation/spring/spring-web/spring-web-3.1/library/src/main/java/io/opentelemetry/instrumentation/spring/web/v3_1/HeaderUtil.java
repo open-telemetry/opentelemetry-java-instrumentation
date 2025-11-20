98d3b47acc034a07f3359810f241fc119b7daba4
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-11-20 16:00:45 +0100

    pr review

--- instrumentation/spring/spring-web/spring-web-3.1/library/src/main/java/io/opentelemetry/instrumentation/spring/web/v3_1/HeaderUtil.java
+++ instrumentation/spring/spring-web/spring-web-3.1/library/src/main/java/io/opentelemetry/instrumentation/spring/web/v3_1/HeaderUtil.java
@@ -6,7 +6,6 @@
 package io.opentelemetry.instrumentation.spring.web.v3_1;
 
 import static java.util.Collections.emptyList;
-import static java.util.Objects.requireNonNull;
 
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
@@ -17,11 +16,11 @@ import javax.annotation.Nullable;
 import org.springframework.http.HttpHeaders;
 
 class HeaderUtil {
-  private static final MethodHandle GET_HEADERS;
+  @Nullable private static final MethodHandle GET_HEADERS;
 
   static {
     GET_HEADERS =
-        nonNullHandle(
+        firstAvailableHandle(
             findGetHeadersMethod(
                 MethodType.methodType(List.class, String.class)), // since spring web 7.0
             () ->
@@ -29,11 +28,10 @@ class HeaderUtil {
                     MethodType.methodType(List.class, Object.class))); // before spring web 7.0
   }
 
-  private static MethodHandle nonNullHandle(
+  @Nullable
+  private static MethodHandle firstAvailableHandle(
       @Nullable MethodHandle first, Supplier<? extends MethodHandle> supplier) {
-    return (first != null)
-        ? first
-        : requireNonNull(supplier.get(), "Could not find suitable get method on HttpHeaders");
+    return first != null ? first : supplier.get();
   }
 
   @Nullable
@@ -45,20 +43,19 @@ class HeaderUtil {
     }
   }
 
-  // before spring web 7.0 HttpHeaders implements Map<String, List<String>>, this triggers
-  // errorprone BadInstanceof warning since errorpone is not aware that this instanceof check does
-  // not pass with spring web 7.0+
   @SuppressWarnings("unchecked") // casting GET_HEADERS.invoke result
   static List<String> getHeader(HttpHeaders headers, String name) {
-    try {
-      List<String> result = (List<String>) GET_HEADERS.invoke(headers, name);
-      if (result == null) {
-        return emptyList();
+    if (GET_HEADERS != null) {
+      try {
+        List<String> result = (List<String>) GET_HEADERS.invoke(headers, name);
+        if (result != null) {
+          return result;
+        }
+      } catch (Throwable t) {
+        // ignore
       }
-      return result;
-    } catch (Throwable t) {
-      throw new IllegalStateException(t);
     }
+    return emptyList();
   }
 
   private HeaderUtil() {}
--- instrumentation/spring/spring-webflux/spring-webflux-5.3/library/src/main/java/io/opentelemetry/instrumentation/spring/webflux/v5_3/internal/HeaderUtil.java
+++ instrumentation/spring/spring-webflux/spring-webflux-5.3/library/src/main/java/io/opentelemetry/instrumentation/spring/webflux/v5_3/internal/HeaderUtil.java
@@ -11,6 +11,8 @@ import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.util.List;
+import java.util.function.Supplier;
+import javax.annotation.Nullable;
 import org.springframework.http.HttpHeaders;
 
 /**
@@ -19,31 +21,44 @@ import org.springframework.http.HttpHeaders;
  * guarantees are made.
  */
 public final class HeaderUtil {
-  private static final MethodHandle GET_HEADERS;
+
+  @Nullable private static final MethodHandle GET_HEADERS;
 
   static {
-    // since spring web 7.0
-    GET_HEADERS = findGetHeadersMethod(MethodType.methodType(List.class, String.class, List.class));
+    GET_HEADERS =
+        firstAvailableHandle(
+            findGetHeadersMethod(
+                MethodType.methodType(List.class, String.class)), // since spring web 7.0
+            () ->
+                findGetHeadersMethod(
+                    MethodType.methodType(List.class, Object.class))); // before spring web 7.0
   }
 
+  @Nullable
+  private static MethodHandle firstAvailableHandle(
+      @Nullable MethodHandle first, Supplier<? extends MethodHandle> supplier) {
+    return first != null ? first : supplier.get();
+  }
+
+  @Nullable
   private static MethodHandle findGetHeadersMethod(MethodType methodType) {
     try {
-      return MethodHandles.lookup().findVirtual(HttpHeaders.class, "getOrDefault", methodType);
+      return MethodHandles.lookup().findVirtual(HttpHeaders.class, "get", methodType);
     } catch (Throwable t) {
       return null;
     }
   }
 
-  // before spring web 7.0 HttpHeaders implements Map<String, List<String>>, this triggers
-  // errorprone BadInstanceof warning since errorpone is not aware that this instanceof check does
-  // not pass with spring web 7.0+
   @SuppressWarnings("unchecked") // casting GET_HEADERS.invoke result
   public static List<String> getHeader(HttpHeaders headers, String name) {
-    if (headers != null) {
+    if (GET_HEADERS != null) {
       try {
-        return (List<String>) GET_HEADERS.invoke(headers, name, emptyList());
+        List<String> result = (List<String>) GET_HEADERS.invoke(headers, name);
+        if (result != null) {
+          return result;
+        }
       } catch (Throwable t) {
-        throw new IllegalStateException(t);
+        // ignore
       }
     }
     return emptyList();
