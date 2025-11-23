fa071fca992260749a7cc28c4493c398f8392239
Jay DeLuca <jaydeluca4@gmail.com>
2025-11-23 07:52:38 -0500

    Remove spring-security-config latestDeps constraint

--- instrumentation/spring/spring-security-config-6.0/library/build.gradle.kts
+++ instrumentation/spring/spring-security-config-6.0/library/build.gradle.kts
@@ -20,9 +20,6 @@ dependencies {
   testImplementation(platform("org.junit:junit-bom:6.0.1"))
 
   testLibrary("org.springframework:spring-test:6.0.0")
-  // remove after 7.0 is released for spring security
-  // spring-test 7 requires spring-context 7
-  latestDepTestLibrary("org.springframework:spring-context:latest.release")
 }
 
 otelJava {
