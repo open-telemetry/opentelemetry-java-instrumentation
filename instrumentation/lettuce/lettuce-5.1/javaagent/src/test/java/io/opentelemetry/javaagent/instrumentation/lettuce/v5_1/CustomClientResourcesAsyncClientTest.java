a6a7d3f8759dac573edf40dc1e6ff81d9eeeedb4
牧思 <liuziming.lzm@alibaba-inc.com>
2025-11-27 13:42:32 +0800

    feat: support custom ClientResources in lettuce framework
    Change-Id: I877035498887c35e8bc1a13e422979f948e43a3a

---
+++ instrumentation/lettuce/lettuce-5.1/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/lettuce/v5_1/CustomClientResourcesAsyncClientTest.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.lettuce.v5_1;
+
+import io.lettuce.core.RedisClient;
+import io.opentelemetry.instrumentation.testing.junit.AgentInstrumentationExtension;
+import io.opentelemetry.instrumentation.testing.junit.InstrumentationExtension;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+class CustomClientResourcesAsyncClientTest extends LettuceAsyncClientTest {
+
+  @RegisterExtension
+  static final InstrumentationExtension testing = AgentInstrumentationExtension.create();
+
+  @Override
+  public InstrumentationExtension testing() {
+    return testing;
+  }
+
+  @Override
+  protected RedisClient createClient(String uri) {
+    return RedisClient.create(CustomClientResources.create(), uri);
+  }
+
+  @Override
+  protected boolean connectHasSpans() {
+    return Boolean.getBoolean("testLatestDeps");
+  }
+}
+
