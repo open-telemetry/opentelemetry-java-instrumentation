55146b8fac39ce671d68872eae2bacb90b29579c
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-11-27 17:51:42 +0100

    fix

--- instrumentation/aws-sdk/aws-sdk-2.2/testing/src/main/java/io/opentelemetry/instrumentation/awssdk/v2_2/AbstractAws2ClientRecordHttpErrorTest.java
+++ instrumentation/aws-sdk/aws-sdk-2.2/testing/src/main/java/io/opentelemetry/instrumentation/awssdk/v2_2/AbstractAws2ClientRecordHttpErrorTest.java
@@ -126,8 +126,7 @@ public abstract class AbstractAws2ClientRecordHttpErrorTest {
   public boolean isRecordIndividualHttpErrorEnabled() {
     // See io.opentelemetry.instrumentation.awssdk.v2_2.autoconfigure.TracingExecutionInterceptor
     return ConfigPropertiesUtil.getBoolean(
-            getTesting().getOpenTelemetry(),
-            "otel.instrumentation.aws-sdk.experimental-record-individual-http-error")
+            getTesting().getOpenTelemetry(), "aws_sdk", "experimental_record_individual_http_error")
         .orElse(false);
   }
 
