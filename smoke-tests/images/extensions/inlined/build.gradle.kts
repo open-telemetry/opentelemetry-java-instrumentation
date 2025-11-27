dda3c8574fe4ae4923e8d281a8c19383e304d5f0
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2025-11-27 15:42:03 +0100

    start creating instrumentation

--- smoke-tests/images/extensions/inlined/build.gradle.kts
+++ smoke-tests/images/extensions/inlined/build.gradle.kts
@@ -1,3 +1,10 @@
 plugins {
   id("otel.java-conventions")
 }
+
+dependencies {
+  compileOnly("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure-spi")
+  compileOnly("io.opentelemetry.instrumentation:opentelemetry-instrumentation-api")
+  compileOnly("io.opentelemetry.instrumentation:opentelemetry-instrumentation-api-incubator")
+  compileOnly("io.opentelemetry.javaagent:opentelemetry-javaagent-extension-api")
+}
---
+++ smoke-tests/images/extensions/inlined/src/main/java/io/opentelemetry/smoketest/extensions/inlined/SmokeInlinedInstrumentation.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.smoketest.extensions.inlined;
+
+import static net.bytebuddy.matcher.ElementMatchers.isPublic;
+import static net.bytebuddy.matcher.ElementMatchers.named;
+import static net.bytebuddy.matcher.ElementMatchers.namedOneOf;
+import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
+
+import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
+import io.opentelemetry.javaagent.extension.instrumentation.TypeTransformer;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+
+public class SmokeInlinedInstrumentation implements TypeInstrumentation {
+
+  @Override
+  public ElementMatcher<TypeDescription> typeMatcher() {
+    return named("io.opentelemetry.smoketest.extensions.app.AppMain");
+  }
+
+  @Override
+  public void transform(TypeTransformer typeTransformer) {
+    typeTransformer.applyAdviceToMethod(
+        namedOneOf("testReturnValue").and(takesArgument(0, int.class)).and(isPublic()),
+        this.getClass().getName() + "$ModifyReturnValueAdvice");
+  }
+
+  @SuppressWarnings("unused")
+  public static class ModifyReturnValueAdvice {
+
+    @Advice.OnMethodExit(suppress = Throwable.class)
+    public static void onExit(@Advice.Argument(value = 1, readOnly = false) int returnValue) {
+      returnValue = returnValue + 1;
+    }
+  }
+}
