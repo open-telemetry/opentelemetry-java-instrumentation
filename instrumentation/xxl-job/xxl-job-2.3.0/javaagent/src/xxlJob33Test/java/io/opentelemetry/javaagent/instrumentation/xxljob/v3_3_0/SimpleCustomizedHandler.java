2196e70d87ae66dcff681eea808351e2f284b66c
Lauri Tulmin <ltulmin@splunk.com>
2025-11-30 18:56:22 +0200

    Fix xxljob latest dep tests (#15490)

--- instrumentation/xxl-job/xxl-job-2.3.0/javaagent/src/xxlJob33Test/java/io/opentelemetry/javaagent/instrumentation/xxljob/v3_3_0/SimpleCustomizedHandler.java
+++ instrumentation/xxl-job/xxl-job-2.3.0/javaagent/src/xxlJob33Test/java/io/opentelemetry/javaagent/instrumentation/xxljob/v3_3_0/SimpleCustomizedHandler.java
@@ -3,15 +3,15 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.opentelemetry.javaagent.instrumentation.xxljob.v2_3_0;
+package io.opentelemetry.javaagent.instrumentation.xxljob.v3_3_0;
 
 import com.xxl.job.core.context.XxlJobHelper;
 import com.xxl.job.core.handler.IJobHandler;
 
-class CustomizedFailedHandler extends IJobHandler {
+class SimpleCustomizedHandler extends IJobHandler {
 
   @Override
   public void execute() {
-    XxlJobHelper.handleFail();
+    XxlJobHelper.handleSuccess();
   }
 }
