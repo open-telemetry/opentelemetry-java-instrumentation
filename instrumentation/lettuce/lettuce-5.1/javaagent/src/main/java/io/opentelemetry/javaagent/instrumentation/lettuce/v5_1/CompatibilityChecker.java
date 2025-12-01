73820236b88f22411675b24e85eae90195125adf
牧思 <liuziming.lzm@alibaba-inc.com>
2025-12-01 22:43:02 +0800

    chore: simplify CompatibilityChecker.java
    Change-Id: I61132543b67152a3716f559be1f1c0b980a59bb4

--- instrumentation/lettuce/lettuce-5.1/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/lettuce/v5_1/CompatibilityChecker.java
+++ instrumentation/lettuce/lettuce-5.1/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/lettuce/v5_1/CompatibilityChecker.java
@@ -6,32 +6,26 @@
 package io.opentelemetry.javaagent.instrumentation.lettuce.v5_1;
 
 import io.lettuce.core.tracing.Tracing;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
-public class CompatibilityChecker {
+public final class CompatibilityChecker {
 
   private CompatibilityChecker() {}
 
-  private static Boolean isCompatible;
-  private static final Lock lock = new ReentrantLock();
+  private static final boolean isCompatible = isCompatible();
+
+  private static boolean isCompatible() {
+    try {
+      Tracing.getContext();
+      return true;
+    } catch (Throwable t) {
+      return false;
+    }
+  }
 
   // related to https://github.com/open-telemetry/opentelemetry-java-instrumentation/issues/10997
   // if users are using incompatible versions of reactor-core and lettuce
   // then just disable the instrumentation
   public static boolean checkCompatible() {
-    lock.lock();
-    try {
-      if (isCompatible != null) {
-        return isCompatible;
-      }
-      Tracing.getContext();
-      isCompatible = true;
-    } catch (Throwable t) {
-      isCompatible = false;
-    } finally {
-      lock.unlock();
-    }
     return isCompatible;
   }
 }
