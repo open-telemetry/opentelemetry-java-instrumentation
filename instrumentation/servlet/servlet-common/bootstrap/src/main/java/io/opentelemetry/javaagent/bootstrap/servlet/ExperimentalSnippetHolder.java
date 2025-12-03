165a99b878d8fdc0a09b2f687cb9da6de654d85c
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-12-03 11:22:11 +0100

    pr review

--- instrumentation/servlet/servlet-common/bootstrap/src/main/java/io/opentelemetry/javaagent/bootstrap/servlet/ExperimentalSnippetHolder.java
+++ instrumentation/servlet/servlet-common/bootstrap/src/main/java/io/opentelemetry/javaagent/bootstrap/servlet/ExperimentalSnippetHolder.java
@@ -6,25 +6,16 @@
 package io.opentelemetry.javaagent.bootstrap.servlet;
 
 import io.opentelemetry.api.GlobalOpenTelemetry;
-import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.instrumentation.api.internal.ConfigPropertiesUtil;
-import java.util.Optional;
 
 public class ExperimentalSnippetHolder {
 
   private static volatile String snippet = getSnippetSetting();
 
   private static String getSnippetSetting() {
-    OpenTelemetry openTelemetry = GlobalOpenTelemetry.get();
-    Optional<String> result =
-        // otel.experimental.* does not fit the usual pattern of configuration properties for
-        // instrumentations, so we need to handle both declarative and non-declarative configs here
-        ConfigPropertiesUtil.isDeclarativeConfig(openTelemetry)
-            ? ConfigPropertiesUtil.getString(
-                openTelemetry, "servlet", "experimental", "javascript-snippet")
-            : Optional.ofNullable(
-                ConfigPropertiesUtil.getString("otel.experimental.javascript-snippet"));
-    return result.orElse("");
+    return ConfigPropertiesUtil.getString(
+            GlobalOpenTelemetry.get(), "servlet", "experimental", "javascript-snippet")
+        .orElse("");
   }
 
   public static void setSnippet(String newValue) {
