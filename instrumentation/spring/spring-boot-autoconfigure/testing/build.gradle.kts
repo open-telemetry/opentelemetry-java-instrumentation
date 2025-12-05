3b18b9450a163c74d6124ccef1ce586f69a6c7f6
Lauri Tulmin <ltulmin@splunk.com>
2025-12-05 14:52:58 +0200

    fix jApiCmp

--- instrumentation/spring/spring-boot-autoconfigure/testing/build.gradle.kts
+++ instrumentation/spring/spring-boot-autoconfigure/testing/build.gradle.kts
@@ -1,7 +1,4 @@
-import org.gradle.kotlin.dsl.testImplementation
-
 plugins {
-  id("otel.library-instrumentation")
   id("otel.java-conventions")
 }
 
@@ -9,17 +6,15 @@ val springBootVersion = "2.7.18"
 
 dependencies {
   compileOnly("org.springframework.boot:spring-boot-restclient:4.0.0")
-  implementation("org.springframework.kafka:spring-kafka:2.9.0")
+  compileOnly("org.springframework.kafka:spring-kafka:2.9.0")
 
-  library("org.springframework.boot:spring-boot-starter-test:$springBootVersion") {
-    exclude("org.junit.vintage", "junit-vintage-engine")
-  }
-  library("org.springframework.boot:spring-boot-starter-data-r2dbc:$springBootVersion")
+  compileOnly("org.springframework.boot:spring-boot-starter-test:$springBootVersion")
+  compileOnly("org.springframework.boot:spring-boot-starter-data-r2dbc:$springBootVersion")
 
-  implementation(project(":instrumentation:micrometer:micrometer-1.5:library"))
-  implementation(project(":instrumentation:spring:spring-boot-autoconfigure"))
-  implementation("io.opentelemetry.javaagent:opentelemetry-testing-common")
-  implementation("io.opentelemetry:opentelemetry-sdk")
-  implementation("io.opentelemetry:opentelemetry-sdk-testing")
-  implementation("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure-spi")
+  compileOnly(project(":instrumentation:micrometer:micrometer-1.5:library"))
+  compileOnly(project(":instrumentation:spring:spring-boot-autoconfigure"))
+  compileOnly("io.opentelemetry.javaagent:opentelemetry-testing-common")
+  compileOnly("io.opentelemetry:opentelemetry-sdk")
+  compileOnly("io.opentelemetry:opentelemetry-sdk-testing")
+  compileOnly("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure-spi")
 }
