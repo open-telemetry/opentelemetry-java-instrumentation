3c4a06907e62fb4bcc8613cc662bb4f0634648f1
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-12-11 13:06:15 -0800

    deprecated fallback

--- instrumentation/servlet/servlet-common/bootstrap/src/main/java/io/opentelemetry/javaagent/bootstrap/servlet/ExperimentalSnippetHolder.java
+++ instrumentation/servlet/servlet-common/bootstrap/src/main/java/io/opentelemetry/javaagent/bootstrap/servlet/ExperimentalSnippetHolder.java
@@ -5,17 +5,40 @@
 
 package io.opentelemetry.javaagent.bootstrap.servlet;
 
+import static java.util.logging.Level.WARNING;
+
 import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.instrumentation.api.internal.ConfigPropertiesUtil;
+import java.util.Optional;
+import java.util.logging.Logger;
 
 public class ExperimentalSnippetHolder {
+
+  private static final Logger logger = Logger.getLogger(ExperimentalSnippetHolder.class.getName());
+
+  private static final String DEPRECATED_PROPERTY_NAME = "otel.experimental.javascript-snippet";
+  private static final String NEW_PROPERTY_NAME =
+      "otel.instrumentation.servlet.experimental.javascript-snippet";
 
   private static volatile String snippet = getSnippetSetting();
 
   private static String getSnippetSetting() {
-    return ConfigPropertiesUtil.getString(
-            GlobalOpenTelemetry.get(), "servlet", "javascript-snippet/development")
-        .orElse("");
+    Optional<String> snippet =
+        ConfigPropertiesUtil.getString(
+            GlobalOpenTelemetry.get(), "servlet", "javascript-snippet/development");
+    // Can remove deprecated fallback in 2.24.0
+    return snippet.orElseGet(
+        () -> {
+          String deprecatedValue = ConfigPropertiesUtil.getString(DEPRECATED_PROPERTY_NAME);
+          if (deprecatedValue != null) {
+            logger.log(
+                WARNING,
+                "Deprecated property \"{0}\" was used; use the \"{1}\" property instead",
+                new Object[] {DEPRECATED_PROPERTY_NAME, NEW_PROPERTY_NAME});
+            return deprecatedValue;
+          }
+          return "";
+        });
   }
 
   public static void setSnippet(String newValue) {
