ff31147eebb19ed9c7b5a025ab542a81949fdc2f
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-12-11 12:32:22 +0100

    remove param that is always false

--- instrumentation/aws-sdk/aws-sdk-2.2/library/src/main/java/io/opentelemetry/instrumentation/awssdk/v2_2/internal/AbstractAwsSdkTelemetryFactory.java
+++ instrumentation/aws-sdk/aws-sdk-2.2/library/src/main/java/io/opentelemetry/instrumentation/awssdk/v2_2/internal/AbstractAwsSdkTelemetryFactory.java
@@ -17,24 +17,24 @@ public abstract class AbstractAwsSdkTelemetryFactory {
   protected abstract List<String> getCapturedHeaders();
 
   private boolean captureExperimentalSpanAttributes() {
-    return getBoolean(false, "aws_sdk", "span_attributes/development");
+    return getBoolean("aws_sdk", "span_attributes/development");
   }
 
   protected abstract boolean messagingReceiveInstrumentationEnabled();
 
   private boolean useMessagingPropagator() {
-    return getBoolean(false, "aws_sdk", "use_propagator_for_messaging/development");
+    return getBoolean("aws_sdk", "use_propagator_for_messaging/development");
   }
 
   private boolean recordIndividualHttpError() {
-    return getBoolean(false, "aws_sdk", "record_individual_http_error/development");
+    return getBoolean("aws_sdk", "record_individual_http_error/development");
   }
 
   private boolean genaiCaptureMessageContent() {
-    return getBoolean(false, "genai", "capture_message_content");
+    return getBoolean("genai", "capture_message_content");
   }
 
-  protected abstract boolean getBoolean(boolean defaultValue, String... name);
+  protected abstract boolean getBoolean(String... name);
 
   public AwsSdkTelemetry telemetry() {
     return AwsSdkTelemetry.builder(GlobalOpenTelemetry.get())
