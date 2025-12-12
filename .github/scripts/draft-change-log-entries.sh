e543a47e08ee9fc2d89077be7c16c35d955d25f8
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-12-11 20:40:18 -0800

    Update changelog for upcoming release

--- .github/scripts/draft-change-log-entries.sh
+++ .github/scripts/draft-change-log-entries.sh
@@ -41,10 +41,51 @@ echo
 echo "### 🧰 Tooling"
 echo
 
-git log --reverse \
-        --perl-regexp \
-        --author='^(?!renovate\[bot\] )' \
-        --pretty=format:"- %s" \
-        "$range" \
-  | sed -E 's, *\(#([0-9]+)\)$,\n  ([#\1](https://github.com/open-telemetry/opentelemetry-java-instrumentation/pull/\1)),'
-echo
+# Group commits by file type
+declare -A src_main_commits
+declare -A no_src_main_commits
+
+while IFS= read -r commit_hash; do
+  files=$(git diff-tree --no-commit-id --name-only -r "$commit_hash")
+  
+  has_src_main=false
+  
+  while IFS= read -r file; do
+    if [[ $file =~ /src/main/ ]] && [[ ! $file =~ ^smoke-tests/ ]] && [[ ! $file =~ ^smoke-tests-otel-starter/ ]] && [[ ! $file =~ /testing/ ]]; then
+      has_src_main=true
+      break
+    fi
+  done <<< "$files"
+  
+  commit_msg=$(git log --format=%s -n 1 "$commit_hash" | sed -E 's, *\(#([0-9]+)\)$,\n  ([#\1](https://github.com/open-telemetry/opentelemetry-java-instrumentation/pull/\1)),')
+  
+  # Categorize commit
+  if [[ $has_src_main == true ]]; then
+    src_main_commits["$commit_hash"]="$commit_msg"
+  else
+    no_src_main_commits["$commit_hash"]="$commit_msg"
+  fi
+done < <(git log --reverse --perl-regexp --author='^(?!renovate\[bot\] )' --pretty=format:"%H" "$range")
+
+# Output grouped commits
+if [[ ${#src_main_commits[@]} -gt 0 ]]; then
+  echo "#### Changes with src/main updates"
+  echo
+  for commit_hash in $(git log --reverse --perl-regexp --author='^(?!renovate\[bot\] )' --pretty=format:"%H" "$range"); do
+    if [[ -n ${src_main_commits[$commit_hash]} ]]; then
+      echo "- $(git log --format=%s -n 1 "$commit_hash" | sed -E 's, *\(#([0-9]+)\)$, ([#\1](https://github.com/open-telemetry/opentelemetry-java-instrumentation/pull/\1)),')"
+    fi
+  done
+  echo
+fi
+
+if [[ ${#no_src_main_commits[@]} -gt 0 ]]; then
+  echo "#### Changes without src/main updates"
+  echo
+  for commit_hash in $(git log --reverse --perl-regexp --author='^(?!renovate\[bot\] )' --pretty=format:"%H" "$range"); do
+    if [[ -n ${no_src_main_commits[$commit_hash]} ]]; then
+      echo "- $(git log --format=%s -n 1 "$commit_hash" | sed -E 's, *\(#([0-9]+)\)$, ([#\1](https://github.com/open-telemetry/opentelemetry-java-instrumentation/pull/\1)),')"
+    fi
+  done
+  echo
+fi
