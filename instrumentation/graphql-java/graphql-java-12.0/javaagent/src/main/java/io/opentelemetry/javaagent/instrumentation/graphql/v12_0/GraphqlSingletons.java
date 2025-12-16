f0682ac15caa8bf7e9e3eeae167a65c562bf6a53
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-12-16 10:13:00 -0800

    Simplified Declarative Configuration API, Option B

--- instrumentation/graphql-java/graphql-java-12.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/graphql/v12_0/GraphqlSingletons.java
+++ instrumentation/graphql-java/graphql-java-12.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/graphql/v12_0/GraphqlSingletons.java
@@ -5,13 +5,11 @@
 
 package io.opentelemetry.javaagent.instrumentation.graphql.v12_0;
 
-import static io.opentelemetry.api.incubator.config.DeclarativeConfigProperties.empty;
-
 import graphql.execution.instrumentation.Instrumentation;
 import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.api.incubator.ExtendedOpenTelemetry;
-import io.opentelemetry.api.incubator.config.DeclarativeConfigProperties;
+import io.opentelemetry.instrumentation.api.incubator.config.internal.DeclarativeConfigUtil;
+import io.opentelemetry.instrumentation.api.incubator.config.internal.ExtendedDeclarativeConfigProperties;
 import io.opentelemetry.instrumentation.graphql.internal.InstrumentationUtil;
 import io.opentelemetry.instrumentation.graphql.v12_0.GraphQLTelemetry;
 
@@ -51,24 +49,16 @@ public final class GraphqlSingletons {
     private final boolean addOperationNameToSpanName;
 
     Configuration(OpenTelemetry openTelemetry) {
-      DeclarativeConfigProperties javaConfig = empty();
-      if (openTelemetry instanceof ExtendedOpenTelemetry) {
-        ExtendedOpenTelemetry extendedOpenTelemetry = (ExtendedOpenTelemetry) openTelemetry;
-        DeclarativeConfigProperties instrumentationConfig =
-            extendedOpenTelemetry.getConfigProvider().getInstrumentationConfig();
-        if (instrumentationConfig != null) {
-          javaConfig = instrumentationConfig.getStructured("java", empty());
-        }
-      }
-      DeclarativeConfigProperties graphqlConfig = javaConfig.getStructured("graphql", empty());
+      ExtendedDeclarativeConfigProperties instrumentationConfig =
+          DeclarativeConfigUtil.get(openTelemetry);
 
-      this.captureQuery = graphqlConfig.getBoolean("capture_query", true);
+      this.captureQuery = instrumentationConfig.getBoolean("graphql", "capture_query").orElse(true);
       this.querySanitizerEnabled =
-          graphqlConfig.getStructured("query_sanitizer", empty()).getBoolean("enabled", true);
+          instrumentationConfig.getBoolean("graphql", "query_sanitizer", "enabled").orElse(true);
       this.addOperationNameToSpanName =
-          graphqlConfig
-              .getStructured("add_operation_name_to_span_name", empty())
-              .getBoolean("enabled", false);
+          instrumentationConfig
+              .getBoolean("graphql", "add_operation_name_to_span_name", "enabled")
+              .orElse(false);
     }
   }
 
