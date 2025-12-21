8393319d0083358d4c45f3a41b8e454c33f1d186
Lenin Jaganathan <lenin.jaganathan@gmail.com>
2025-12-21 00:53:07 -0800

    Move changes to common module Signed-off-by: Lenin Jaganathan <lenin.jaganathan@gmail.com>
    Signed-off-by: Lenin Jaganathan <lenin.jaganathan@gmail.com>

--- instrumentation/spring/spring-boot-autoconfigure/src/testSpring3/java/io/opentelemetry/instrumentation/spring/autoconfigure/internal/instrumentation/web/RestClientInstrumentationAutoConfigurationTest.java
+++ instrumentation/spring/spring-boot-autoconfigure/src/testSpring3/java/io/opentelemetry/instrumentation/spring/autoconfigure/internal/instrumentation/web/RestClientInstrumentationAutoConfigurationTest.java
@@ -5,14 +5,11 @@
 
 package io.opentelemetry.instrumentation.spring.autoconfigure.internal.instrumentation.web;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.instrumentation.api.incubator.config.internal.InstrumentationConfig;
 import io.opentelemetry.instrumentation.spring.autoconfigure.internal.AbstractRestClientInstrumentationAutoConfigurationTest;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
-import org.springframework.web.client.RestClient;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 
 class RestClientInstrumentationAutoConfigurationTest
     extends AbstractRestClientInstrumentationAutoConfigurationTest {
@@ -23,51 +20,13 @@ class RestClientInstrumentationAutoConfigurationTest
   }
 
   @Override
-  protected Class<?> postProcessorClass() {
+  protected Class<RestClientBeanPostProcessor> postProcessorClass() {
     return RestClientBeanPostProcessor.class;
   }
 
-  @Test
-  void shouldNotCreateNewBeanWhenInterceptorAlreadyPresent() {
-    contextRunner
-        .withPropertyValues("otel.instrumentation.spring-web.enabled=true")
-        .run(
-            context -> {
-              RestClientBeanPostProcessor beanPostProcessor =
-                  context.getBean(
-                      "otelRestClientBeanPostProcessor", RestClientBeanPostProcessor.class);
-
-              RestClient restClientWithInterceptor =
-                  RestClient.builder()
-                      .requestInterceptor(
-                          RestClientBeanPostProcessor.getInterceptor(
-                              context.getBean(OpenTelemetry.class),
-                              context.getBean(InstrumentationConfig.class)))
-                      .build();
-
-              RestClient processed =
-                  (RestClient)
-                      beanPostProcessor.postProcessAfterInitialization(
-                          restClientWithInterceptor, "testBean");
-
-              // Should return the same instance when interceptor is already present
-              assertThat(processed).isSameAs(restClientWithInterceptor);
-
-              // Verify only one interceptor exists
-              processed
-                  .mutate()
-                  .requestInterceptors(
-                      interceptors -> {
-                        long count =
-                            interceptors.stream()
-                                .filter(
-                                    rti ->
-                                        rti.getClass()
-                                            .getName()
-                                            .startsWith("io.opentelemetry.instrumentation"))
-                                .count();
-                        assertThat(count).isEqualTo(1);
-                      });
-            });
+  @Override
+  protected ClientHttpRequestInterceptor getInterceptor(
+      OpenTelemetry openTelemetry, InstrumentationConfig config) {
+    return RestClientBeanPostProcessor.getInterceptor(openTelemetry, config);
   }
 }
