b436af2c64c712758a0fe24a730a4d7574215b75
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-12-24 12:56:55 +0100

    use special_mapping

--- instrumentation/servlet/servlet-common/bootstrap/src/main/java/io/opentelemetry/javaagent/bootstrap/servlet/ExperimentalSnippetHolder.java
+++ instrumentation/servlet/servlet-common/bootstrap/src/main/java/io/opentelemetry/javaagent/bootstrap/servlet/ExperimentalSnippetHolder.java
@@ -6,22 +6,15 @@
 package io.opentelemetry.javaagent.bootstrap.servlet;
 
 import io.opentelemetry.api.GlobalOpenTelemetry;
-import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.instrumentation.api.incubator.config.internal.DeclarativeConfigUtil;
-import io.opentelemetry.instrumentation.api.internal.ConfigPropertiesUtil;
 
 public class ExperimentalSnippetHolder {
 
   private static volatile String snippet = getSnippetSetting();
 
   private static String getSnippetSetting() {
-    OpenTelemetry openTelemetry = GlobalOpenTelemetry.get();
-    return DeclarativeConfigUtil.getInstrumentationConfig(openTelemetry, "servlet")
-        .getString(
-            "javascript_snippet/development",
-            // use ConfigPropertiesUtil only to prevent that the deprecated property is used in
-            // declarative config
-            ConfigPropertiesUtil.getString("otel.experimental.javascript-snippet", ""));
+    return DeclarativeConfigUtil.getInstrumentationConfig(GlobalOpenTelemetry.get(), "servlet")
+        .getString("javascript_snippet/development", "");
   }
 
   public static void setSnippet(String newValue) {
