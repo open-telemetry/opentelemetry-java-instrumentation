8f1737af8aa825d636b84ae32977a845aabd29b4
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-12-29 08:57:20 -0800

    Move one AgentConfig usage to declarative configuration API (#15698)

--- javaagent-tooling/src/main/java/io/opentelemetry/javaagent/tooling/config/AgentConfig.java
+++ javaagent-tooling/src/main/java/io/opentelemetry/javaagent/tooling/config/AgentConfig.java
@@ -9,18 +9,6 @@ import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
 
 public final class AgentConfig {
 
-  public static boolean isInstrumentationEnabled(
-      ConfigProperties config, Iterable<String> instrumentationNames, boolean defaultEnabled) {
-    for (String name : instrumentationNames) {
-      String propertyName = "otel.instrumentation." + name + ".enabled";
-      Boolean enabled = config.getBoolean(propertyName);
-      if (enabled != null) {
-        return enabled;
-      }
-    }
-    return defaultEnabled;
-  }
-
   public static boolean isDebugModeEnabled(ConfigProperties config) {
     return config.getBoolean("otel.javaagent.debug", false);
   }
