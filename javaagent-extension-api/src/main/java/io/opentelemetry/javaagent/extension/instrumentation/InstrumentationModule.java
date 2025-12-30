70b70ba37908d8ada37d49a5fc376f6d7b60bbbd
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-12-30 10:20:12 -0800

    Convert last AgentInstrumentationConfig usages

--- javaagent-extension-api/src/main/java/io/opentelemetry/javaagent/extension/instrumentation/InstrumentationModule.java
+++ javaagent-extension-api/src/main/java/io/opentelemetry/javaagent/extension/instrumentation/InstrumentationModule.java
@@ -9,7 +9,8 @@ import static java.util.Arrays.asList;
 import static java.util.Collections.unmodifiableSet;
 import static net.bytebuddy.matcher.ElementMatchers.any;
 
-import io.opentelemetry.javaagent.bootstrap.internal.AgentInstrumentationConfig;
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.instrumentation.api.incubator.config.internal.DeclarativeConfigUtil;
 import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
 import io.opentelemetry.sdk.autoconfigure.spi.Ordered;
 import java.util.Collections;
@@ -162,7 +163,8 @@ public abstract class InstrumentationModule implements Ordered {
 
     static {
       indyEnabled =
-          AgentInstrumentationConfig.get().getBoolean("otel.javaagent.experimental.indy", false);
+          DeclarativeConfigUtil.getInstrumentationConfig(GlobalOpenTelemetry.get(), "agent")
+              .getBoolean("indy/development", false);
       if (indyEnabled) {
         logger.info("Enabled indy for instrumentation modules");
       }
