dd408ae4e930e71317a06aa978a21b9905cc2d31
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-01-07 19:24:54 -0800

    Convert lettuce-5.1 to use Instrumenter

---
+++ instrumentation/lettuce/lettuce-5.1/library/src/main/java/io/opentelemetry/instrumentation/lettuce/v5_1/LettuceDbAttributesGetter.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.instrumentation.lettuce.v5_1;
+
+import io.opentelemetry.instrumentation.api.incubator.semconv.db.DbClientAttributesGetter;
+import java.net.InetSocketAddress;
+import javax.annotation.Nullable;
+
+final class LettuceDbAttributesGetter implements DbClientAttributesGetter<LettuceRequest, Void> {
+
+  private static final String REDIS = "redis";
+
+  @Override
+  public String getDbSystemName(LettuceRequest request) {
+    return REDIS;
+  }
+
+  @Nullable
+  @Override
+  public String getDbNamespace(LettuceRequest request) {
+    Long databaseIndex = request.getDatabaseIndex();
+    return databaseIndex != null ? String.valueOf(databaseIndex) : null;
+  }
+
+  @Deprecated
+  @Nullable
+  @Override
+  public String getDbQueryText(LettuceRequest request) {
+    return request.getStatement();
+  }
+
+  @Nullable
+  @Override
+  public String getDbOperationName(LettuceRequest request) {
+    return request.getCommand();
+  }
+
+  @Nullable
+  @Override
+  public String getServerAddress(LettuceRequest request) {
+    InetSocketAddress address = request.getAddress();
+    if (address != null) {
+      return address.getHostString();
+    }
+    return null;
+  }
+
+  @Nullable
+  @Override
+  public Integer getServerPort(LettuceRequest request) {
+    InetSocketAddress address = request.getAddress();
+    if (address != null) {
+      return address.getPort();
+    }
+    return null;
+  }
+
+  @Nullable
+  @Override
+  public InetSocketAddress getNetworkPeerInetSocketAddress(
+      LettuceRequest request, @Nullable Void unused) {
+    return request.getAddress();
+  }
+}
