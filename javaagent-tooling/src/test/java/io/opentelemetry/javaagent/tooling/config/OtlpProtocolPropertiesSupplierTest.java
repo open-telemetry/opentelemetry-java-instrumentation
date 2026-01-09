32a40007c47d177dfc3e749d40075484d2226705
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2026-01-09 08:31:07 +0100

    split tests to separate PR

--- javaagent-tooling/src/test/java/io/opentelemetry/javaagent/tooling/config/OtlpProtocolPropertiesSupplierTest.java
+++ javaagent-tooling/src/test/java/io/opentelemetry/javaagent/tooling/config/OtlpProtocolPropertiesSupplierTest.java
@@ -10,6 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.javaagent.tooling.OpenTelemetryInstaller;
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
+import io.opentelemetry.sdk.autoconfigure.internal.AutoConfigureUtil;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -34,9 +35,9 @@ class OtlpProtocolPropertiesSupplierTest {
             this.getClass().getClassLoader(), EarlyInitAgentConfig.create());
 
     // then
-    assertThat(autoConfiguredSdk.getOpenTelemetrySdk().toString())
-        .contains("OtlpGrpc")
-        .doesNotContain("OtlpHttp");
+    assertThat(
+            AutoConfigureUtil.getConfig(autoConfiguredSdk).getString("otel.exporter.otlp.protocol"))
+        .isEqualTo("grpc");
   }
 
   @Test
@@ -47,8 +48,8 @@ class OtlpProtocolPropertiesSupplierTest {
             this.getClass().getClassLoader(), EarlyInitAgentConfig.create());
 
     // then
-    assertThat(autoConfiguredSdk.getOpenTelemetrySdk().toString())
-        .contains("OtlpHttp")
-        .doesNotContain("OtlpGrpc");
+    assertThat(
+            AutoConfigureUtil.getConfig(autoConfiguredSdk).getString("otel.exporter.otlp.protocol"))
+        .isEqualTo("http/protobuf");
   }
 }
