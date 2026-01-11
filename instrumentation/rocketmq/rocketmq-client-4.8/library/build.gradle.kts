5823a8ab52ec95b882454911c9b940d177046aa7
Jay DeLuca <jaydeluca4@gmail.com>
2026-01-11 07:57:37 -0500

    fix library tests

--- instrumentation/rocketmq/rocketmq-client-4.8/library/build.gradle.kts
+++ instrumentation/rocketmq/rocketmq-client-4.8/library/build.gradle.kts
@@ -21,4 +21,7 @@ tasks.withType<Test>().configureEach {
   jvmArgs("-Dotel.instrumentation.common.experimental.controller-telemetry.enabled=true")
   // with default settings tests will fail when disk is 90% full
   jvmArgs("-Drocketmq.broker.diskSpaceWarningLevelRatio=1.0")
+
+  // used for experimental attributes test assertion logic which looks for this property
+  jvmArgs("-Dotel.instrumentation.rocketmq-client.experimental-span-attributes=true")
 }
