c876d94271e719e7b1cb7fcca1dca2899413a981
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-01-13 09:23:05 -0800

    Add javadoc to Experimental HTTP library methods (#15850)

--- instrumentation/spring/spring-webflux/spring-webflux-5.3/library/src/main/java/io/opentelemetry/instrumentation/spring/webflux/v5_3/internal/Experimental.java
+++ instrumentation/spring/spring-webflux/spring-webflux-5.3/library/src/main/java/io/opentelemetry/instrumentation/spring/webflux/v5_3/internal/Experimental.java
@@ -25,6 +25,12 @@ public final class Experimental {
   private static volatile BiConsumer<SpringWebfluxServerTelemetryBuilder, Boolean>
       setEmitExperimentalServerTelemetry;
 
+  /**
+   * Sets whether experimental HTTP client telemetry should be emitted.
+   *
+   * @param builder the client telemetry builder
+   * @param emitExperimentalTelemetry {@code true} to emit experimental telemetry
+   */
   public static void setEmitExperimentalTelemetry(
       SpringWebfluxClientTelemetryBuilder builder, boolean emitExperimentalTelemetry) {
     if (setEmitExperimentalClientTelemetry != null) {
@@ -32,6 +38,12 @@ public final class Experimental {
     }
   }
 
+  /**
+   * Sets whether experimental HTTP server telemetry should be emitted.
+   *
+   * @param builder the server telemetry builder
+   * @param emitExperimentalTelemetry {@code true} to emit experimental telemetry
+   */
   public static void setEmitExperimentalTelemetry(
       SpringWebfluxServerTelemetryBuilder builder, boolean emitExperimentalTelemetry) {
     if (setEmitExperimentalServerTelemetry != null) {
