b966f4337e35867c7f7f96446b2e608c2074f014
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-01-17 10:46:50 -0800

    fix

--- instrumentation/jdbc/library/src/main/java/io/opentelemetry/instrumentation/jdbc/internal/OpenTelemetryPreparedStatement.java
+++ instrumentation/jdbc/library/src/main/java/io/opentelemetry/instrumentation/jdbc/internal/OpenTelemetryPreparedStatement.java
@@ -429,12 +429,12 @@ class OpenTelemetryPreparedStatement<S extends PreparedStatement> extends OpenTe
   @Override
   protected <T, E extends Exception> T wrapCall(String sql, ThrowingSupplier<T, E> callable)
       throws E {
-    DbRequest request = DbRequest.create(dbInfo, sql, null, parameters, true);
+    DbRequest request = DbRequest.create(dbInfo, sql, null, parameters, false);
     return wrapCall(request, callable);
   }
 
   private <T, E extends Exception> T wrapBatchCall(ThrowingSupplier<T, E> callable) throws E {
-    DbRequest request = DbRequest.create(dbInfo, query, batchSize, parameters, true);
+    DbRequest request = DbRequest.create(dbInfo, query, batchSize, parameters, false);
     return wrapCall(request, callable);
   }
 
