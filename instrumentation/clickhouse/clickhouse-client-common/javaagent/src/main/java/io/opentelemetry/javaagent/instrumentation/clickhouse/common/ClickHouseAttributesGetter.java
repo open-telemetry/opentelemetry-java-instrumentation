6e67fb5b1840244f36d17766359bf7aa23062f90
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-01-23 19:56:30 -0800

    ClickHouse: support db.query.summary

--- instrumentation/clickhouse/clickhouse-client-common/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/clickhouse/common/ClickHouseAttributesGetter.java
+++ instrumentation/clickhouse/clickhouse-client-common/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/clickhouse/common/ClickHouseAttributesGetter.java
@@ -22,19 +22,31 @@ final class ClickHouseAttributesGetter
   @Nullable
   @Override
   public String getDbQueryText(ClickHouseDbRequest request) {
-    if (request.getSqlStatementInfo() == null) {
-      return null;
+    if (request.getSqlStatementInfoWithSummary() != null) {
+      return request.getSqlStatementInfoWithSummary().getQueryText();
     }
-    return request.getSqlStatementInfo().getQueryText();
+    if (request.getSqlStatementInfo() != null) {
+      return request.getSqlStatementInfo().getQueryText();
+    }
+    return null;
   }
 
   @Nullable
   @Override
   public String getDbOperationName(ClickHouseDbRequest request) {
-    if (request.getSqlStatementInfo() == null) {
-      return null;
+    if (request.getSqlStatementInfo() != null) {
+      return request.getSqlStatementInfo().getOperationName();
     }
-    return request.getSqlStatementInfo().getOperationName();
+    return null;
+  }
+
+  @Nullable
+  @Override
+  public String getDbQuerySummary(ClickHouseDbRequest request) {
+    if (request.getSqlStatementInfoWithSummary() != null) {
+      return request.getSqlStatementInfoWithSummary().getQuerySummary();
+    }
+    return null;
   }
 
   @SuppressWarnings("deprecation") // using deprecated DbSystemIncubatingValues
