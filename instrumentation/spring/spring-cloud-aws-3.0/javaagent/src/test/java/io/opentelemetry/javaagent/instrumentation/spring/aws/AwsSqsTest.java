bd3935bbe60d8a4a47df9c85ac2587eee64ba927
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-01-28 14:38:02 -0800

    more

--- instrumentation/spring/spring-cloud-aws-3.0/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/spring/aws/AwsSqsTest.java
+++ instrumentation/spring/spring-cloud-aws-3.0/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/spring/aws/AwsSqsTest.java
@@ -44,6 +44,7 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@SuppressWarnings("deprecation") // using deprecated semconv
 @SpringBootTest(
     webEnvironment = SpringBootTest.WebEnvironment.NONE,
     classes = AwsSqsTestApplication.class)
