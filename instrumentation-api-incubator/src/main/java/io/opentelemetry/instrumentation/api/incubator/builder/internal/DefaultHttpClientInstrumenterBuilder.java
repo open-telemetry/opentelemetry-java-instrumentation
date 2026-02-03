27957015320f73b68c64698fd0b7f2d299fb9421
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-02-03 10:29:30 -0800

    Handle sensitive query params in HTTP server instrumentation

--- instrumentation-api-incubator/src/main/java/io/opentelemetry/instrumentation/api/incubator/builder/internal/DefaultHttpClientInstrumenterBuilder.java
+++ instrumentation-api-incubator/src/main/java/io/opentelemetry/instrumentation/api/incubator/builder/internal/DefaultHttpClientInstrumenterBuilder.java
@@ -33,6 +33,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
@@ -172,14 +173,16 @@ public final class DefaultHttpClientInstrumenterBuilder<REQUEST, RESPONSE> {
   }
 
   /**
-   * Configures the instrumentation to redact sensitive URL parameters.
+   * Configures the instrumentation to redact specific URL query parameters.
    *
-   * @param redactQueryParameters {@code true} if the sensitive URL parameters have to be redacted.
+   * @param sensitiveQueryParameters the set of query parameter names whose values should be
+   *     redacted.
    */
   @CanIgnoreReturnValue
-  public DefaultHttpClientInstrumenterBuilder<REQUEST, RESPONSE> setRedactQueryParameters(
-      boolean redactQueryParameters) {
-    Experimental.setRedactQueryParameters(httpAttributesExtractorBuilder, redactQueryParameters);
+  public DefaultHttpClientInstrumenterBuilder<REQUEST, RESPONSE> setSensitiveQueryParameters(
+      Set<String> sensitiveQueryParameters) {
+    Experimental.setSensitiveQueryParameters(
+        httpAttributesExtractorBuilder, sensitiveQueryParameters);
     return this;
   }
 
@@ -252,7 +255,7 @@ public final class DefaultHttpClientInstrumenterBuilder<REQUEST, RESPONSE> {
     set(
         config::shouldEmitExperimentalHttpClientTelemetry,
         this::setEmitExperimentalHttpClientTelemetry);
-    set(config::redactQueryParameters, this::setRedactQueryParameters);
+    set(config::getSensitiveQueryParameters, this::setSensitiveQueryParameters);
     addAttributesExtractor(
         HttpClientServicePeerAttributesExtractor.create(attributesGetter, openTelemetry));
     return this;
