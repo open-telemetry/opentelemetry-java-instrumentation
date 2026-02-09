b56518f34de28fce2aef200532d99ecb78c0759a
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-02-09 11:56:51 -0800

    Update instrumentation-api-incubator/src/main/java/io/opentelemetry/instrumentation/api/incubator/semconv/rpc/RpcAttributesGetter.java

--- instrumentation-api-incubator/src/main/java/io/opentelemetry/instrumentation/api/incubator/semconv/rpc/RpcAttributesGetter.java
+++ instrumentation-api-incubator/src/main/java/io/opentelemetry/instrumentation/api/incubator/semconv/rpc/RpcAttributesGetter.java
@@ -69,31 +69,4 @@ public interface RpcAttributesGetter<REQUEST, RESPONSE> {
       REQUEST request, @Nullable RESPONSE response, @Nullable Throwable error) {
     return null;
   }
-
-  /**
-   * Returns whether the RPC method is recognized as a predefined method by the RPC framework or
-   * library.
-   *
-   * <p>Some RPC frameworks or libraries provide a fixed set of recognized methods for client stubs
-   * and server implementations. Instrumentations for such frameworks MUST return {@code true} only
-   * when the method is recognized by the framework or library.
-   *
-   * <p>When the method is not recognized (for example, when the server receives a request for a
-   * method that is not predefined on the server), or when instrumentation is not able to reliably
-   * detect if the method is predefined, this method MUST return {@code false}.
-   *
-   * <p>When this method returns {@code false}, the {@code rpc.method} attribute will be set to
-   * {@code "_OTHER"} and the {@code rpc.method_original} attribute will be set to the original
-   * method name.
-   *
-   * <p>Note: If the RPC instrumentation could end up converting valid RPC methods to {@code
-   * "_OTHER"}, then it SHOULD provide a way to configure the list of recognized RPC methods.
-   *
-   * @param request the request object
-   * @return {@code true} if the method is recognized as predefined by the framework, {@code false}
-   *     otherwise
-   */
-  default boolean isPredefined(REQUEST request) {
-    return false;
-  }
 }
