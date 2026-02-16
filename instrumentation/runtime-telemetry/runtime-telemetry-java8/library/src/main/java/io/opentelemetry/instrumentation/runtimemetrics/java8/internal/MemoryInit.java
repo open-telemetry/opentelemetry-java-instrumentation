098dbf111173348353eb9ec37a71901edea9d2df
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-02-16 08:14:32 -0800

    Make runtime-telemetry deprecated classes now internal (#16173)

--- instrumentation/runtime-telemetry/runtime-telemetry-java8/library/src/main/java/io/opentelemetry/instrumentation/runtimemetrics/java8/internal/MemoryInit.java
+++ instrumentation/runtime-telemetry/runtime-telemetry-java8/library/src/main/java/io/opentelemetry/instrumentation/runtimemetrics/java8/internal/MemoryInit.java
@@ -25,12 +25,8 @@ import java.util.function.Consumer;
  *
  * <p>This class is internal and is hence not for public use. Its APIs are unstable and can change
  * at any time.
- *
- * @deprecated Use {@link io.opentelemetry.instrumentation.runtimemetrics.java8.RuntimeMetrics}
- *     instead, and configure metric views to select specific metrics.
  */
-@Deprecated
-public final class ExperimentalMemoryPools {
+public final class MemoryInit {
 
   /** Register observers for java runtime experimental memory metrics. */
   public static List<AutoCloseable> registerObservers(OpenTelemetry openTelemetry) {
@@ -86,5 +82,5 @@ public final class ExperimentalMemoryPools {
     return "unknown";
   }
 
-  private ExperimentalMemoryPools() {}
+  private MemoryInit() {}
 }
