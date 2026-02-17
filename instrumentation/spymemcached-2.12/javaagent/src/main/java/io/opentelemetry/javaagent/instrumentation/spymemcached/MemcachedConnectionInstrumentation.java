c86e14b832b3651cf849b529ec7a4be1b20adb2b
Steve Rao <raozihao.rzh@alibaba-inc.com>
2026-02-18 03:07:21 +0800

    Add server address and port for Spymemcached (#15242)
    Co-authored-by: Lauri Tulmin <tulmin@gmail.com>
    Co-authored-by: Lauri Tulmin <ltulmin@splunk.com>
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>

---
+++ instrumentation/spymemcached-2.12/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/spymemcached/MemcachedConnectionInstrumentation.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.spymemcached;
+
+import static net.bytebuddy.matcher.ElementMatchers.isMethod;
+import static net.bytebuddy.matcher.ElementMatchers.named;
+import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
+import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
+
+import io.opentelemetry.javaagent.bootstrap.Java8BytecodeBridge;
+import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
+import io.opentelemetry.javaagent.extension.instrumentation.TypeTransformer;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+import net.spy.memcached.ops.Operation;
+
+public class MemcachedConnectionInstrumentation implements TypeInstrumentation {
+
+  @Override
+  public ElementMatcher<TypeDescription> typeMatcher() {
+    return named("net.spy.memcached.MemcachedConnection");
+  }
+
+  @Override
+  public void transform(TypeTransformer transformer) {
+    transformer.applyAdviceToMethod(
+        isMethod()
+            .and(named("addOperation"))
+            .and(takesArguments(2))
+            .and(takesArgument(0, named("net.spy.memcached.MemcachedNode")))
+            .and(takesArgument(1, named("net.spy.memcached.ops.Operation"))),
+        this.getClass().getName() + "$AddOperationAdvice");
+  }
+
+  @SuppressWarnings("unused")
+  public static class AddOperationAdvice {
+    @Advice.OnMethodExit(suppress = Throwable.class)
+    public static void onExit(@Advice.Argument(1) Operation operation) {
+      SpymemcachedRequestHolder.setHandlingNode(
+          Java8BytecodeBridge.currentContext(), operation.getHandlingNode());
+    }
+  }
+}
