a024c9796a0c884031e19fc350c4ba79bc314c33
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-02-16 20:56:33 -0800

    Simplify InfluxDB instrumentation

--- instrumentation/influxdb-2.4/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/influxdb/v2_4/InfluxDbAttributesGetter.java
+++ instrumentation/influxdb-2.4/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/influxdb/v2_4/InfluxDbAttributesGetter.java
@@ -5,42 +5,26 @@
 
 package io.opentelemetry.javaagent.instrumentation.influxdb.v2_4;
 
-import io.opentelemetry.instrumentation.api.incubator.semconv.db.DbClientAttributesGetter;
+import io.opentelemetry.instrumentation.api.incubator.semconv.db.SqlClientAttributesGetter;
+import java.util.Collection;
+import java.util.Collections;
 import javax.annotation.Nullable;
 
-final class InfluxDbAttributesGetter implements DbClientAttributesGetter<InfluxDbRequest, Void> {
+final class InfluxDbAttributesGetter implements SqlClientAttributesGetter<InfluxDbRequest, Void> {
 
-  @Nullable
   @Override
-  public String getDbQueryText(InfluxDbRequest request) {
-    if (request.getSqlQueryWithSummary() != null) {
-      return request.getSqlQueryWithSummary().getQueryText();
+  public Collection<String> getRawQueryTexts(InfluxDbRequest request) {
+    String sql = request.getSql();
+    if (sql == null) {
+      return Collections.emptyList();
     }
-    if (request.getSqlQuery() != null) {
-      return request.getSqlQuery().getQueryText();
-    }
-    return null;
+    return Collections.singletonList(sql);
   }
 
   @Nullable
   @Override
   public String getDbOperationName(InfluxDbRequest request) {
-    if (request.getOperationName() != null) {
-      return request.getOperationName();
-    }
-    if (request.getSqlQuery() != null) {
-      return request.getSqlQuery().getOperationName();
-    }
-    return null;
-  }
-
-  @Nullable
-  @Override
-  public String getDbQuerySummary(InfluxDbRequest request) {
-    if (request.getSqlQueryWithSummary() != null) {
-      return request.getSqlQueryWithSummary().getQuerySummary();
-    }
-    return null;
+    return request.getOperationName();
   }
 
   @Override
@@ -53,5 +37,15 @@ final class InfluxDbAttributesGetter implements DbClientAttributesGetter<InfluxD
   public String getDbNamespace(InfluxDbRequest request) {
     String namespace = request.getNamespace();
     return "".equals(namespace) ? null : namespace;
+  }
+
+  @Override
+  public String getServerAddress(InfluxDbRequest request) {
+    return request.getHost();
+  }
+
+  @Override
+  public Integer getServerPort(InfluxDbRequest request) {
+    return request.getPort();
   }
 }
