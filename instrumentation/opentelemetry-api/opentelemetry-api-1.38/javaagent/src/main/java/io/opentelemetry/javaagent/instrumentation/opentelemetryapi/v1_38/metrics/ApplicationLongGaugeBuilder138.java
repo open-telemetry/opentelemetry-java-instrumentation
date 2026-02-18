fe72f1bd3eb3d95bfcc7a110563815b1d80a2baa
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-02-18 08:53:42 -0800

    Enforce error prone's UnnecessarilyFullyQualified rule (#16160)

--- instrumentation/opentelemetry-api/opentelemetry-api-1.38/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/opentelemetryapi/v1_38/metrics/ApplicationLongGaugeBuilder138.java
+++ instrumentation/opentelemetry-api/opentelemetry-api-1.38/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/opentelemetryapi/v1_38/metrics/ApplicationLongGaugeBuilder138.java
@@ -5,22 +5,22 @@
 
 package io.opentelemetry.javaagent.instrumentation.opentelemetryapi.v1_38.metrics;
 
-import application.io.opentelemetry.api.metrics.LongGauge;
+import io.opentelemetry.api.metrics.LongGauge;
+import io.opentelemetry.api.metrics.LongGaugeBuilder;
 import io.opentelemetry.javaagent.instrumentation.opentelemetryapi.v1_10.metrics.ApplicationLongGaugeBuilder;
 
 public class ApplicationLongGaugeBuilder138 extends ApplicationLongGaugeBuilder {
 
-  private final io.opentelemetry.api.metrics.LongGaugeBuilder agentBuilder;
+  private final LongGaugeBuilder agentBuilder;
 
-  protected ApplicationLongGaugeBuilder138(
-      io.opentelemetry.api.metrics.LongGaugeBuilder agentBuilder) {
+  protected ApplicationLongGaugeBuilder138(LongGaugeBuilder agentBuilder) {
     super(agentBuilder);
     this.agentBuilder = agentBuilder;
   }
 
   @Override
-  public LongGauge build() {
-    io.opentelemetry.api.metrics.LongGauge agentLongGauge = agentBuilder.build();
+  public application.io.opentelemetry.api.metrics.LongGauge build() {
+    LongGauge agentLongGauge = agentBuilder.build();
     return new ApplicationLongGauge138(agentLongGauge);
   }
 }
