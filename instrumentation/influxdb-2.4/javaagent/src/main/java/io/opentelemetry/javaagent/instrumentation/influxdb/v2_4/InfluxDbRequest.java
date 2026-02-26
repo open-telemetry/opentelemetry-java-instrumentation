7ea5354e81c0e44727d399261e7580603e9e769a
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-02-26 12:10:05 -0800

    Fix influxdb-2.4 merge conflicts: revert InfluxDbRequest to simple form and add getSqlDialect()

--- instrumentation/influxdb-2.4/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/influxdb/v2_4/InfluxDbRequest.java
+++ instrumentation/influxdb-2.4/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/influxdb/v2_4/InfluxDbRequest.java
@@ -5,34 +5,19 @@
 
 package io.opentelemetry.javaagent.instrumentation.influxdb.v2_4;
 
-import static io.opentelemetry.instrumentation.api.incubator.semconv.db.SqlDialect.DOUBLE_QUOTES_ARE_IDENTIFIERS;
-import static io.opentelemetry.instrumentation.api.internal.SemconvStability.emitOldDatabaseSemconv;
-import static io.opentelemetry.instrumentation.api.internal.SemconvStability.emitStableDatabaseSemconv;
-
 import com.google.auto.value.AutoValue;
-import io.opentelemetry.instrumentation.api.incubator.semconv.db.SqlQuery;
-import io.opentelemetry.instrumentation.api.incubator.semconv.db.SqlQueryAnalyzer;
-import io.opentelemetry.javaagent.bootstrap.internal.AgentCommonConfig;
 import javax.annotation.Nullable;
 
 @AutoValue
 public abstract class InfluxDbRequest {
 
-  private static final SqlQueryAnalyzer analyzer =
-      SqlQueryAnalyzer.create(AgentCommonConfig.get().isQuerySanitizationEnabled());
-
   public static InfluxDbRequest create(
-      String host, int port, String namespace, @Nullable String operationName, @Nullable String sql) {
-    // "String literals must be surrounded by single quotes."
-    // https://docs.influxdata.com/influxdb/v2/reference/syntax/influxql/spec/#strings
-    SqlQuery sqlQuery =
-        emitOldDatabaseSemconv() ? analyzer.analyze(sql, DOUBLE_QUOTES_ARE_IDENTIFIERS) : null;
-    SqlQuery sqlQueryWithSummary =
-        emitStableDatabaseSemconv()
-            ? analyzer.analyzeWithSummary(sql, DOUBLE_QUOTES_ARE_IDENTIFIERS)
-            : null;
-    return new AutoValue_InfluxDbRequest(
-        host, port, namespace, operationName, sqlQuery, sqlQueryWithSummary);
+      String host,
+      int port,
+      String namespace,
+      @Nullable String operationName,
+      @Nullable String sql) {
+    return new AutoValue_InfluxDbRequest(host, port, namespace, operationName, sql);
   }
 
   public abstract String getHost();
