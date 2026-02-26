2352c20d4d736adf4da655f6862f960b06d889b2
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-02-26 15:48:37 -0800

    Reduce test diff

--- instrumentation-api-incubator/src/test/java/io/opentelemetry/instrumentation/api/incubator/semconv/rpc/RpcServerMetricsTest.java
+++ instrumentation-api-incubator/src/test/java/io/opentelemetry/instrumentation/api/incubator/semconv/rpc/RpcServerMetricsTest.java
@@ -6,7 +6,6 @@
 package io.opentelemetry.instrumentation.api.incubator.semconv.rpc;
 
 import static io.opentelemetry.instrumentation.api.incubator.semconv.rpc.RpcCommonAttributesExtractor.RPC_SYSTEM_NAME;
-import static io.opentelemetry.instrumentation.api.incubator.semconv.rpc.RpcMetricsContextCustomizers.OLD_RPC_METHOD_CONTEXT_KEY;
 import static io.opentelemetry.instrumentation.api.internal.SemconvStability.emitOldRpcSemconv;
 import static io.opentelemetry.instrumentation.api.internal.SemconvStability.emitStableRpcSemconv;
 import static io.opentelemetry.sdk.testing.assertj.OpenTelemetryAssertions.assertThat;
@@ -21,9 +20,7 @@ import static io.opentelemetry.semconv.incubating.RpcIncubatingAttributes.RPC_SE
 import static io.opentelemetry.semconv.incubating.RpcIncubatingAttributes.RPC_SYSTEM;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
-import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.Attributes;
-import io.opentelemetry.api.common.AttributesBuilder;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.SpanContext;
 import io.opentelemetry.api.trace.TraceFlags;
@@ -33,11 +30,8 @@ import io.opentelemetry.instrumentation.api.instrumenter.OperationListener;
 import io.opentelemetry.instrumentation.api.internal.SemconvStability;
 import io.opentelemetry.sdk.metrics.SdkMeterProvider;
 import io.opentelemetry.sdk.metrics.data.MetricData;
-import io.opentelemetry.sdk.testing.assertj.AttributeAssertion;
 import io.opentelemetry.sdk.testing.exporter.InMemoryMetricReader;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 
 @SuppressWarnings("deprecation") // using deprecated semconv
@@ -51,11 +45,20 @@ class RpcServerMetricsTest {
 
     OperationListener listener = RpcServerMetrics.get().create(meterProvider.get("test"));
 
-    Attributes requestAttributes1 =
-        buildRequestAttributes("grpc", "myservice.EchoService", "exampleMethod", true);
-
     Attributes requestAttributes2 =
-        buildRequestAttributes("grpc", "myservice.EchoService", "exampleMethod", false);
+        Attributes.builder()
+            .put(RPC_SYSTEM, emitOldRpcSemconv() ? "grpc" : null)
+            .put(RPC_SERVICE, emitOldRpcSemconv() ? "myservice.EchoService" : null)
+            .put(
+                RPC_METHOD,
+                emitStableRpcSemconv() ? "myservice.EchoService/exampleMethod" : "exampleMethod")
+            .put(
+                RPC_SYSTEM_NAME,
+                emitStableRpcSemconv() ? SemconvStability.stableRpcSystemName("grpc") : null)
+            .build();
+
+    Attributes requestAttributes1 =
+        requestAttributes2.toBuilder().put(RpcSizeAttributesExtractor.RPC_REQUEST_SIZE, 10).build();
 
     Attributes responseAttributes1 =
         Attributes.builder()
@@ -83,54 +86,51 @@ class RpcServerMetricsTest {
                         "090a0b0c0d0e0f00",
                         TraceFlags.getSampled(),
                         TraceState.getDefault())));
-    if (emitOldRpcSemconv() && emitStableRpcSemconv()) {
-      parent = parent.with(OLD_RPC_METHOD_CONTEXT_KEY, "exampleMethod");
-    }
-
-    Context context1 = listener.onStart(parent, requestAttributes1, nanos(100));
+    Context context1 =
+        listener.onStart(
+            withDualEmitContextCustomizer(parent, "exampleMethod"), requestAttributes1, nanos(100));
 
     assertThat(metricReader.collectAllMetrics()).isEmpty();
 
-    Context context2Root = Context.root();
-    if (emitOldRpcSemconv() && emitStableRpcSemconv()) {
-      context2Root = context2Root.with(OLD_RPC_METHOD_CONTEXT_KEY, "exampleMethod");
-    }
-    Context context2 = listener.onStart(context2Root, requestAttributes2, nanos(150));
+    Context context2 =
+        listener.onStart(
+            withDualEmitContextCustomizer(Context.root(), "exampleMethod"),
+            requestAttributes2,
+            nanos(150));
 
     assertThat(metricReader.collectAllMetrics()).isEmpty();
 
     listener.onEnd(context1, responseAttributes1, nanos(250));
 
-    // Calculate expected metric count:
-    // - dup mode: old duration + stable duration + request.size + response.size = 4
-    // - old-only mode: old duration + request.size + response.size = 3
-    // - stable-only mode: stable duration only = 1
-    int expectedMetricCount = 1; // At minimum, we always have one duration metric
-    if (emitOldRpcSemconv() && emitStableRpcSemconv()) {
-      expectedMetricCount = 4; // Both durations + both size metrics
-    } else if (emitOldRpcSemconv()) {
-      expectedMetricCount = 3; // Old duration + both size metrics
-    }
-
-    // Collect metrics once (delta reader consumes on each call)
     Collection<MetricData> metrics1 = metricReader.collectAllMetrics();
-    assertThat(metrics1).hasSize(expectedMetricCount);
 
-    // Build expected attributes for OLD metrics (size + old duration) - alphabetically sorted
-    List<AttributeAssertion> oldMetricAttributes1 = new ArrayList<>();
-    if (emitOldRpcSemconv()) {
-      oldMetricAttributes1.add(equalTo(NETWORK_TRANSPORT, "tcp"));
-      oldMetricAttributes1.add(equalTo(NETWORK_TYPE, "ipv4"));
-      oldMetricAttributes1.add(equalTo(RPC_METHOD, "exampleMethod"));
-      oldMetricAttributes1.add(equalTo(RPC_SERVICE, "myservice.EchoService"));
-      oldMetricAttributes1.add(equalTo(RPC_SYSTEM, "grpc"));
-      oldMetricAttributes1.add(equalTo(SERVER_ADDRESS, "example.com"));
-      oldMetricAttributes1.add(equalTo(SERVER_PORT, 8080));
-    }
-
-    // Size metrics are only recorded in old semconv mode
     if (emitOldRpcSemconv()) {
       assertThat(metrics1)
+          .anySatisfy(
+              metric ->
+                  assertThat(metric)
+                      .hasName("rpc.server.duration")
+                      .hasUnit("ms")
+                      .hasHistogramSatisfying(
+                          histogram ->
+                              histogram.hasPointsSatisfying(
+                                  point ->
+                                      point
+                                          .hasSum(150 /* millis */)
+                                          .hasAttributesSatisfyingExactly(
+                                              equalTo(RPC_SYSTEM, "grpc"),
+                                              equalTo(RPC_SERVICE, "myservice.EchoService"),
+                                              equalTo(RPC_METHOD, "exampleMethod"),
+                                              equalTo(SERVER_ADDRESS, "example.com"),
+                                              equalTo(SERVER_PORT, 8080),
+                                              equalTo(NETWORK_TRANSPORT, "tcp"),
+                                              equalTo(NETWORK_TYPE, "ipv4"))
+                                          .hasExemplarsSatisfying(
+                                              exemplar ->
+                                                  exemplar
+                                                      .hasTraceId(
+                                                          "ff01020304050600ff0a0b0c0d0e0f00")
+                                                      .hasSpanId("090a0b0c0d0e0f00")))))
           .anySatisfy(
               metric ->
                   assertThat(metric)
@@ -144,8 +144,13 @@ class RpcServerMetricsTest {
                                       point
                                           .hasSum(20 /* bytes */)
                                           .hasAttributesSatisfyingExactly(
-                                              oldMetricAttributes1.toArray(
-                                                  new AttributeAssertion[0]))
+                                              equalTo(RPC_SYSTEM, "grpc"),
+                                              equalTo(RPC_SERVICE, "myservice.EchoService"),
+                                              equalTo(RPC_METHOD, "exampleMethod"),
+                                              equalTo(SERVER_ADDRESS, "example.com"),
+                                              equalTo(SERVER_PORT, 8080),
+                                              equalTo(NETWORK_TRANSPORT, "tcp"),
+                                              equalTo(NETWORK_TYPE, "ipv4"))
                                           .hasExemplarsSatisfying(
                                               exemplar ->
                                                   exemplar
@@ -165,8 +170,13 @@ class RpcServerMetricsTest {
                                       point
                                           .hasSum(10 /* bytes */)
                                           .hasAttributesSatisfyingExactly(
-                                              oldMetricAttributes1.toArray(
-                                                  new AttributeAssertion[0]))
+                                              equalTo(RPC_SYSTEM, "grpc"),
+                                              equalTo(RPC_SERVICE, "myservice.EchoService"),
+                                              equalTo(RPC_METHOD, "exampleMethod"),
+                                              equalTo(SERVER_ADDRESS, "example.com"),
+                                              equalTo(SERVER_PORT, 8080),
+                                              equalTo(NETWORK_TRANSPORT, "tcp"),
+                                              equalTo(NETWORK_TYPE, "ipv4"))
                                           .hasExemplarsSatisfying(
                                               exemplar ->
                                                   exemplar
@@ -175,19 +185,7 @@ class RpcServerMetricsTest {
                                                       .hasSpanId("090a0b0c0d0e0f00")))));
     }
 
-    // Assert stable duration metric if emitting stable semconv
     if (emitStableRpcSemconv()) {
-      // Build expected attributes for STABLE metrics - alphabetically sorted
-      List<AttributeAssertion> stableMetricAttributes1 = new ArrayList<>();
-      stableMetricAttributes1.add(equalTo(NETWORK_TRANSPORT, "tcp"));
-      stableMetricAttributes1.add(equalTo(RPC_METHOD, "myservice.EchoService/exampleMethod"));
-      stableMetricAttributes1.add(
-          equalTo(
-              AttributeKey.stringKey("rpc.system.name"),
-              SemconvStability.stableRpcSystemName("grpc")));
-      stableMetricAttributes1.add(equalTo(SERVER_ADDRESS, "example.com"));
-      stableMetricAttributes1.add(equalTo(SERVER_PORT, 8080));
-
       assertThat(metrics1)
           .anySatisfy(
               metric ->
@@ -201,33 +199,15 @@ class RpcServerMetricsTest {
                                       point
                                           .hasSum(0.15)
                                           .hasAttributesSatisfyingExactly(
-                                              stableMetricAttributes1.toArray(
-                                                  new AttributeAssertion[0]))
-                                          .hasExemplarsSatisfying(
-                                              exemplar ->
-                                                  exemplar
-                                                      .hasTraceId(
-                                                          "ff01020304050600ff0a0b0c0d0e0f00")
-                                                      .hasSpanId("090a0b0c0d0e0f00")))));
-    }
-
-    // Assert old duration metric if emitting old semconv
-    if (emitOldRpcSemconv()) {
-      assertThat(metrics1)
-          .anySatisfy(
-              metric ->
-                  assertThat(metric)
-                      .hasName("rpc.server.duration")
-                      .hasUnit("ms")
-                      .hasHistogramSatisfying(
-                          histogram ->
-                              histogram.hasPointsSatisfying(
-                                  point ->
-                                      point
-                                          .hasSum(150.0)
-                                          .hasAttributesSatisfyingExactly(
-                                              oldMetricAttributes1.toArray(
-                                                  new AttributeAssertion[0]))
+                                              equalTo(NETWORK_TRANSPORT, "tcp"),
+                                              equalTo(
+                                                  RPC_METHOD,
+                                                  "myservice.EchoService/exampleMethod"),
+                                              equalTo(
+                                                  RPC_SYSTEM_NAME,
+                                                  SemconvStability.stableRpcSystemName("grpc")),
+                                              equalTo(SERVER_ADDRESS, "example.com"),
+                                              equalTo(SERVER_PORT, 8080))
                                           .hasExemplarsSatisfying(
                                               exemplar ->
                                                   exemplar
@@ -238,37 +218,29 @@ class RpcServerMetricsTest {
 
     listener.onEnd(context2, responseAttributes2, nanos(300));
 
-    // Collect metrics once (delta reader consumes on each call)
-    // Note: metrics2 doesn't include size metrics since context2 has no size attributes
-    int expectedMetricCount2 = emitOldRpcSemconv() && emitStableRpcSemconv() ? 2 : 1;
     Collection<MetricData> metrics2 = metricReader.collectAllMetrics();
-    assertThat(metrics2).hasSize(expectedMetricCount2);
 
-    // Build expected attributes for OLD metrics (no server.address or network.type for context2) -
-    // alphabetically sorted
-    List<AttributeAssertion> oldMetricAttributes2 = new ArrayList<>();
     if (emitOldRpcSemconv()) {
-      oldMetricAttributes2.add(equalTo(NETWORK_TRANSPORT, "tcp"));
-      oldMetricAttributes2.add(equalTo(RPC_METHOD, "exampleMethod"));
-      oldMetricAttributes2.add(equalTo(RPC_SERVICE, "myservice.EchoService"));
-      oldMetricAttributes2.add(equalTo(RPC_SYSTEM, "grpc"));
-      oldMetricAttributes2.add(equalTo(SERVER_PORT, 8080));
+      assertThat(metrics2)
+          .anySatisfy(
+              metric ->
+                  assertThat(metric)
+                      .hasName("rpc.server.duration")
+                      .hasUnit("ms")
+                      .hasHistogramSatisfying(
+                          histogram ->
+                              histogram.hasPointsSatisfying(
+                                  point ->
+                                      point
+                                          .hasSum(150 /* millis */)
+                                          .hasAttributesSatisfyingExactly(
+                                              equalTo(RPC_SYSTEM, "grpc"),
+                                              equalTo(RPC_SERVICE, "myservice.EchoService"),
+                                              equalTo(RPC_METHOD, "exampleMethod"),
+                                              equalTo(SERVER_PORT, 8080),
+                                              equalTo(NETWORK_TRANSPORT, "tcp")))));
     }
 
-    // Build expected attributes for STABLE metrics (no server.address for context2) -
-    // alphabetically sorted
-    List<AttributeAssertion> stableMetricAttributes2 = new ArrayList<>();
-    if (emitStableRpcSemconv()) {
-      stableMetricAttributes2.add(equalTo(NETWORK_TRANSPORT, "tcp"));
-      stableMetricAttributes2.add(equalTo(RPC_METHOD, "myservice.EchoService/exampleMethod"));
-      stableMetricAttributes2.add(
-          equalTo(
-              AttributeKey.stringKey("rpc.system.name"),
-              SemconvStability.stableRpcSystemName("grpc")));
-      stableMetricAttributes2.add(equalTo(SERVER_PORT, 8080));
-    }
-
-    // Assert stable duration metric if emitting stable semconv
     if (emitStableRpcSemconv()) {
       assertThat(metrics2)
           .anySatisfy(
@@ -283,27 +255,14 @@ class RpcServerMetricsTest {
                                       point
                                           .hasSum(0.15)
                                           .hasAttributesSatisfyingExactly(
-                                              stableMetricAttributes2.toArray(
-                                                  new AttributeAssertion[0])))));
-    }
-
-    // Assert old duration metric if emitting old semconv
-    if (emitOldRpcSemconv()) {
-      assertThat(metrics2)
-          .anySatisfy(
-              metric ->
-                  assertThat(metric)
-                      .hasName("rpc.server.duration")
-                      .hasUnit("ms")
-                      .hasHistogramSatisfying(
-                          histogram ->
-                              histogram.hasPointsSatisfying(
-                                  point ->
-                                      point
-                                          .hasSum(150.0)
-                                          .hasAttributesSatisfyingExactly(
-                                              oldMetricAttributes2.toArray(
-                                                  new AttributeAssertion[0])))));
+                                              equalTo(NETWORK_TRANSPORT, "tcp"),
+                                              equalTo(
+                                                  RPC_METHOD,
+                                                  "myservice.EchoService/exampleMethod"),
+                                              equalTo(
+                                                  RPC_SYSTEM_NAME,
+                                                  SemconvStability.stableRpcSystemName("grpc")),
+                                              equalTo(SERVER_PORT, 8080)))));
     }
   }
 
@@ -311,27 +270,25 @@ class RpcServerMetricsTest {
     return MILLISECONDS.toNanos(millis);
   }
 
-  private static Attributes buildRequestAttributes(
-      String system, String service, String method, boolean withSize) {
-    AttributesBuilder builder = Attributes.builder();
+  @SuppressWarnings("deprecation")
+  private static Context withDualEmitContextCustomizer(Context context, String method) {
+    return RpcMetricsContextCustomizers.<String>dualEmitContextCustomizer(
+            new RpcAttributesGetter<String, Void>() {
+              @Override
+              public String getSystem(String request) {
+                return null;
+              }
 
-    if (emitOldRpcSemconv()) {
-      builder.put(RPC_SYSTEM, system);
-      builder.put(RPC_SERVICE, service);
-      if (!emitStableRpcSemconv()) {
-        builder.put(RPC_METHOD, method);
-      }
-    }
+              @Override
+              public String getService(String request) {
+                return null;
+              }
 
-    if (emitStableRpcSemconv()) {
-      builder.put(RPC_SYSTEM_NAME, SemconvStability.stableRpcSystemName(system));
-      builder.put(RPC_METHOD, service + "/" + method);
-    }
-
-    if (withSize) {
-      builder.put(RpcSizeAttributesExtractor.RPC_REQUEST_SIZE, 10);
-    }
-
-    return builder.build();
+              @Override
+              public String getMethod(String request) {
+                return request;
+              }
+            })
+        .onStart(context, method, Attributes.empty());
   }
 }
