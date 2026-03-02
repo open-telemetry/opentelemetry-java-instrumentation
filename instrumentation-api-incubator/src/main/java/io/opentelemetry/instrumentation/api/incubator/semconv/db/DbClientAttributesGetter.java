45d8c7ff1557c36fab496f7d65cbd9f78c4248d5
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-03-02 07:16:48 -0800

    Add `db.system.name` support to r2dbc (#16251)

--- instrumentation-api-incubator/src/main/java/io/opentelemetry/instrumentation/api/incubator/semconv/db/DbClientAttributesGetter.java
+++ instrumentation-api-incubator/src/main/java/io/opentelemetry/instrumentation/api/incubator/semconv/db/DbClientAttributesGetter.java
@@ -41,6 +41,21 @@ public interface DbClientAttributesGetter<REQUEST, RESPONSE>
   // TODO: make this required to implement
   String getDbSystemName(REQUEST request);
 
+  /**
+   * Returns the old semconv {@code db.system} value. Defaults to {@link #getDbSystemName} for
+   * instrumentations where the old and new values are the same.
+   *
+   * <p>Implementors whose old {@code db.system} value differs from the new {@code db.system.name}
+   * value <b>must override</b> this method; otherwise old-semconv spans will silently emit the new
+   * value for {@code db.system}.
+   *
+   * @deprecated Use {@link #getDbSystemName} instead.
+   */
+  @Deprecated // to be removed in 3.0
+  default String getDbSystem(REQUEST request) {
+    return getDbSystemName(request);
+  }
+
   @Nullable
   String getDbNamespace(REQUEST request);
 
