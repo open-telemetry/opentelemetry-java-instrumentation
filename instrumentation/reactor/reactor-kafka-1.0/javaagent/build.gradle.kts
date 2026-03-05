1843a48da723a73a7536bd44805346a9836d4c35
Jay DeLuca <jaydeluca4@gmail.com>
2026-03-05 16:57:07 -0500

    Fix vertx and reactor Kafka telemetry interception (#16395)

--- instrumentation/reactor/reactor-kafka-1.0/javaagent/build.gradle.kts
+++ instrumentation/reactor/reactor-kafka-1.0/javaagent/build.gradle.kts
@@ -87,9 +87,15 @@ tasks {
   withType<Test>().configureEach {
     usesService(gradle.sharedServices.registrations["testcontainersBuildService"].service)
     systemProperty("collectMetadata", findProperty("collectMetadata")?.toString() ?: "false")
+    jvmArgs("-Dotel.instrumentation.messaging.experimental.receive-telemetry.enabled=true")
+  }
+
+  val testExperimental by registering(Test::class) {
+    testClassesDirs = sourceSets.test.get().output.classesDirs
+    classpath = sourceSets.test.get().runtimeClasspath
 
     jvmArgs("-Dotel.instrumentation.kafka.experimental-span-attributes=true")
-    jvmArgs("-Dotel.instrumentation.messaging.experimental.receive-telemetry.enabled=true")
+    systemProperty("metadataConfig", "otel.instrumentation.kafka.experimental-span-attributes=true")
   }
 
   test {
@@ -97,6 +103,6 @@ tasks {
   }
 
   check {
-    dependsOn(testing.suites)
+    dependsOn(testing.suites, testExperimental)
   }
 }
