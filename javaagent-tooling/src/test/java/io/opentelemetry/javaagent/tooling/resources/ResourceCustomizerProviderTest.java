94bd834048f711ece42fb3f4a1ddd05f454c48ce
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-03-06 16:36:39 -0800

    Fix javaagent-tooling tests for DeclarativeConfigurationCustomizer interface change
    DeclarativeConfigurationCustomizer is no longer a functional interface
    in SDK 1.60.0. Replace lambdas with anonymous class implementations in
    SpanLoggingCustomizerProviderTest and ResourceCustomizerProviderTest.

--- javaagent-tooling/src/test/java/io/opentelemetry/javaagent/tooling/resources/ResourceCustomizerProviderTest.java
+++ javaagent-tooling/src/test/java/io/opentelemetry/javaagent/tooling/resources/ResourceCustomizerProviderTest.java
@@ -9,7 +9,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.opentelemetry.api.incubator.config.DeclarativeConfigProperties;
+import io.opentelemetry.sdk.extension.incubator.fileconfig.DeclarativeConfigurationCustomizer;
 import io.opentelemetry.sdk.extension.incubator.fileconfig.internal.model.OpenTelemetryConfigurationModel;
+import io.opentelemetry.sdk.logs.export.LogRecordExporter;
+import io.opentelemetry.sdk.metrics.export.MetricExporter;
+import io.opentelemetry.sdk.trace.export.SpanExporter;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import org.junit.jupiter.api.Test;
 
 class ResourceCustomizerProviderTest {
@@ -20,17 +27,37 @@ class ResourceCustomizerProviderTest {
   void customize() {
     new ResourceCustomizerProvider()
         .customize(
-            customizer -> {
-              OpenTelemetryConfigurationModel configurationModel =
-                  customizer.apply(new OpenTelemetryConfigurationModel());
+            new DeclarativeConfigurationCustomizer() {
+              @Override
+              public void addModelCustomizer(
+                  Function<OpenTelemetryConfigurationModel, OpenTelemetryConfigurationModel>
+                      customizer) {
+                OpenTelemetryConfigurationModel configurationModel =
+                    customizer.apply(new OpenTelemetryConfigurationModel());
 
-              try {
-                assertThat(objectMapper.writeValueAsString(configurationModel.getResource()))
-                    .isEqualTo(
-                        "{\"attributes\":[],\"detection/development\":{\"detectors\":[{\"opentelemetry_javaagent_distribution\":null}]}}");
-              } catch (JsonProcessingException e) {
-                throw new AssertionError(e);
+                try {
+                  assertThat(objectMapper.writeValueAsString(configurationModel.getResource()))
+                      .isEqualTo(
+                          "{\"attributes\":[],\"detection/development\":{\"detectors\":[{\"opentelemetry_javaagent_distribution\":null}]}}");
+                } catch (JsonProcessingException e) {
+                  throw new AssertionError(e);
+                }
               }
+
+              @Override
+              public <T extends SpanExporter> void addSpanExporterCustomizer(
+                  Class<T> exporterType,
+                  BiFunction<T, DeclarativeConfigProperties, T> customizer) {}
+
+              @Override
+              public <T extends MetricExporter> void addMetricExporterCustomizer(
+                  Class<T> exporterType,
+                  BiFunction<T, DeclarativeConfigProperties, T> customizer) {}
+
+              @Override
+              public <T extends LogRecordExporter> void addLogRecordExporterCustomizer(
+                  Class<T> exporterType,
+                  BiFunction<T, DeclarativeConfigProperties, T> customizer) {}
             });
   }
 }
