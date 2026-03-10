78720498ac85e65c298e80d207c9c7f1689c1598
Lauri Tulmin <ltulmin@splunk.com>
2026-03-10 15:49:36 +0200

    add strindex value

--- testing-common/src/main/java/io/opentelemetry/instrumentation/testing/internal/TelemetryConverter.java
+++ testing-common/src/main/java/io/opentelemetry/instrumentation/testing/internal/TelemetryConverter.java
@@ -387,8 +387,10 @@ public class TelemetryConverter {
         return Value.of(value.getBytesValue().toByteArray());
       case VALUE_NOT_SET:
         return EMPTY_VALUE;
+      case STRING_VALUE_STRINDEX:
+      default:
+        throw new IllegalStateException("Unexpected attribute: " + value.getValueCase());
     }
-    throw new IllegalStateException("Unexpected attribute: " + value.getValueCase());
   }
 
   private static boolean isDouble(List<NumberDataPoint> points) {
@@ -562,6 +564,8 @@ public class TelemetryConverter {
             converted.put(valueKey(key), EMPTY_VALUE);
           }
           break;
+        case STRING_VALUE_STRINDEX:
+          throw new IllegalStateException("Unexpected attribute: " + value.getValueCase());
       }
     }
     return converted.build();
@@ -630,6 +634,8 @@ public class TelemetryConverter {
             converted.put(valueKey(key), EMPTY_VALUE);
           }
           break;
+        case STRING_VALUE_STRINDEX:
+          throw new IllegalStateException("Unexpected attribute: " + value.getValueCase());
       }
     }
     return converted.build();
