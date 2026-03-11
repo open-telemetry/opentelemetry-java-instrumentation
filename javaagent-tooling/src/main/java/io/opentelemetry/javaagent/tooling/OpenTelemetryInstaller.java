925d98dc85e8abfb26503d67f9762a9f859d1e56
Lauri Tulmin <ltulmin@splunk.com>
2026-03-11 17:24:33 +0200

    Configure semconv-stability.opt-in with declarative config api

--- javaagent-tooling/src/main/java/io/opentelemetry/javaagent/tooling/OpenTelemetryInstaller.java
+++ javaagent-tooling/src/main/java/io/opentelemetry/javaagent/tooling/OpenTelemetryInstaller.java
@@ -8,6 +8,7 @@ package io.opentelemetry.javaagent.tooling;
 import static java.util.Arrays.asList;
 
 import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.instrumentation.api.internal.SemconvStability;
 import io.opentelemetry.instrumentation.config.bridge.ConfigPropertiesBackedConfigProvider;
 import io.opentelemetry.javaagent.bootstrap.OpenTelemetrySdkAccess;
 import io.opentelemetry.javaagent.extension.instrumentation.internal.AgentDistributionConfig;
@@ -49,6 +50,7 @@ public final class OpenTelemetryInstaller {
 
     setForceFlush(sdk);
     GlobalOpenTelemetry.set(sdk);
+    SemconvStability.configure(sdk);
 
     return SdkAutoconfigureAccess.create(
         sdk, SdkAutoconfigureAccess.getResource(autoConfiguredSdk), configProperties);
