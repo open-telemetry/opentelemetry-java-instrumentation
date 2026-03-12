a515aa1a22db153c62fbbf7bc6a28ce24da9b280
otelbot <197425009+otelbot@users.noreply.github.com>
2026-03-12 19:16:18 +0000

    Review fixes for apache-dbcp-2.0:javaagent
    Automated code review of instrumentation/apache-dbcp-2.0/javaagent.

--- instrumentation/apache-dbcp-2.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/apachedbcp/v2_0/ApacheDbcpInstrumentationModule.java
+++ instrumentation/apache-dbcp-2.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/apachedbcp/v2_0/ApacheDbcpInstrumentationModule.java
@@ -5,17 +5,24 @@
 
 package io.opentelemetry.javaagent.instrumentation.apachedbcp.v2_0;
 
+import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.hasClassesNamed;
 import static java.util.Collections.singletonList;
 
 import com.google.auto.service.AutoService;
 import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import java.util.List;
+import net.bytebuddy.matcher.ElementMatcher;
 
 @AutoService(InstrumentationModule.class)
 public class ApacheDbcpInstrumentationModule extends InstrumentationModule {
   public ApacheDbcpInstrumentationModule() {
     super("apache-dbcp", "apache-dbcp-2.0");
+  }
+
+  @Override
+  public ElementMatcher.Junction<ClassLoader> classLoaderMatcher() {
+    return hasClassesNamed("org.apache.commons.dbcp2.BasicDataSource");
   }
 
   @Override
