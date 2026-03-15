012f22ad7ead5da7287336f5e097768baae16332
otelbot <197425009+otelbot@users.noreply.github.com>
2026-03-15 19:52:19 +0000

    Review fixes for aws-sdk-1.11:library
    Automated code review of instrumentation/aws-sdk/aws-sdk-1.11/library.

--- instrumentation/aws-sdk/aws-sdk-1.11/library/src/main/java/io/opentelemetry/instrumentation/awssdk/v1_11/AwsSdkExperimentalAttributesExtractor.java
+++ instrumentation/aws-sdk/aws-sdk-1.11/library/src/main/java/io/opentelemetry/instrumentation/awssdk/v1_11/AwsSdkExperimentalAttributesExtractor.java
@@ -37,10 +37,7 @@ class AwsSdkExperimentalAttributesExtractor
       AttributeKey<String> key,
       Object request,
       Function<Object, String> getter) {
-    String value = getter.apply(request);
-    if (value != null) {
-      attributes.put(key, value);
-    }
+    attributes.put(key, getter.apply(request));
   }
 
   @Override
