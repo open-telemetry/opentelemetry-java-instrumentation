166de598fb27ecb1186b6b3e12cb9edee04884ab
otelbot <197425009+otelbot@users.noreply.github.com>
2026-03-16 11:16:55 +0000

    Review fixes for aws-lambda-events-2.2:testing
    Automated code review of instrumentation/aws-lambda/aws-lambda-events-2.2/testing.

--- instrumentation/aws-lambda/aws-lambda-events-2.2/testing/src/main/java/io/opentelemetry/instrumentation/awslambdaevents/v2_2/AbstractAwsLambdaSqsEventHandlerTest.java
+++ instrumentation/aws-lambda/aws-lambda-events-2.2/testing/src/main/java/io/opentelemetry/instrumentation/awslambdaevents/v2_2/AbstractAwsLambdaSqsEventHandlerTest.java
@@ -151,8 +151,8 @@ public abstract class AbstractAwsLambdaSqsEventHandlerTest {
       Constructor<SQSEvent.SQSMessage> ctor = SQSEvent.SQSMessage.class.getDeclaredConstructor();
       ctor.setAccessible(true);
       return ctor.newInstance();
-    } catch (Throwable t) {
-      throw new AssertionError(t);
+    } catch (ReflectiveOperationException e) {
+      throw new LinkageError(e.getMessage(), e);
     }
   }
 }
