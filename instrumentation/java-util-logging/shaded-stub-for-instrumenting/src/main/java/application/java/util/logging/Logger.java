b2c5feaf2a787b03e53c0cf3836b9db86718a8b2
otelbot <197425009+otelbot@users.noreply.github.com>
2026-03-18 21:57:54 +0000

    Review fixes for java-util-logging:shaded-stub-for-instrumenting
    Automated code review of instrumentation/java-util-logging/shaded-stub-for-instrumenting.

--- instrumentation/java-util-logging/shaded-stub-for-instrumenting/src/main/java/application/java/util/logging/Logger.java
+++ instrumentation/java-util-logging/shaded-stub-for-instrumenting/src/main/java/application/java/util/logging/Logger.java
@@ -10,7 +10,7 @@ import java.util.logging.Level;
 // java.util.logging.Logger shaded so that it can be used in instrumentation
 // of java.util.logging.Logger itself, and then its usage can be unshaded
 // after java.util.logging.Logger is shaded to the "PatchLogger"
-public class Logger {
+public final class Logger {
 
   public String getName() {
     throw new UnsupportedOperationException();
