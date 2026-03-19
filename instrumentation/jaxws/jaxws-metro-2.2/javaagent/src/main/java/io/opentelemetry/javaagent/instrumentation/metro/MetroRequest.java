be27ec7bc60f46c6edb7189437bd85ac79276916
otelbot <197425009+otelbot@users.noreply.github.com>
2026-03-19 09:50:27 +0000

    Review fixes for jaxws-metro-2.2:javaagent
    Automated code review of instrumentation/jaxws/jaxws-metro-2.2/javaagent.

--- instrumentation/jaxws/jaxws-metro-2.2/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/metro/MetroRequest.java
+++ instrumentation/jaxws/jaxws-metro-2.2/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/metro/MetroRequest.java
@@ -7,16 +7,19 @@ package io.opentelemetry.javaagent.instrumentation.metro;
 
 import com.sun.xml.ws.api.message.Packet;
 import com.sun.xml.ws.api.server.WSEndpoint;
+import javax.annotation.Nullable;
+import javax.xml.namespace.QName;
 
 public class MetroRequest {
   private final Packet packet;
-  private final String spanName;
+  @Nullable private final String spanName;
 
   public MetroRequest(WSEndpoint<?> endpoint, Packet packet) {
     this.packet = packet;
     this.spanName = getSpanName(endpoint, packet);
   }
 
+  @Nullable
   public String spanName() {
     return spanName;
   }
@@ -25,9 +28,14 @@ public class MetroRequest {
     return packet;
   }
 
+  @Nullable
   private static String getSpanName(WSEndpoint<?> endpoint, Packet packet) {
+    QName wsdlOperation = packet.getWSDLOperation();
+    if (wsdlOperation == null) {
+      return null;
+    }
     String serviceName = endpoint.getServiceName().getLocalPart();
-    String operationName = packet.getWSDLOperation().getLocalPart();
+    String operationName = wsdlOperation.getLocalPart();
     return serviceName + "/" + operationName;
   }
 }
