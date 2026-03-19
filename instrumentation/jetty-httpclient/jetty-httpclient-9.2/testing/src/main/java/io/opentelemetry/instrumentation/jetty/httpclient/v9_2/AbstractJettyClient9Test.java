83584cf8c478d0f2b1a356903f8439ae081f564a
otelbot <197425009+otelbot@users.noreply.github.com>
2026-03-19 17:31:28 +0000

    Review fixes for jetty-httpclient-9.2:testing
    Automated code review of instrumentation/jetty-httpclient/jetty-httpclient-9.2/testing.

--- instrumentation/jetty-httpclient/jetty-httpclient-9.2/testing/src/main/java/io/opentelemetry/instrumentation/jetty/httpclient/v9_2/AbstractJettyClient9Test.java
+++ instrumentation/jetty-httpclient/jetty-httpclient-9.2/testing/src/main/java/io/opentelemetry/instrumentation/jetty/httpclient/v9_2/AbstractJettyClient9Test.java
@@ -29,7 +29,7 @@ public abstract class AbstractJettyClient9Test extends AbstractHttpClientTest<Re
   private HttpClient httpsClient;
 
   @BeforeEach
-  public void before() throws Exception {
+  void before() throws Exception {
     // Start the main Jetty HttpClient and a https client
     client = createStandardClient();
     client.setConnectTimeout(CONNECTION_TIMEOUT.toMillis());
@@ -42,7 +42,7 @@ public abstract class AbstractJettyClient9Test extends AbstractHttpClientTest<Re
   }
 
   @AfterEach
-  public void after() throws Exception {
+  void after() throws Exception {
     client.stop();
     httpsClient.stop();
   }
