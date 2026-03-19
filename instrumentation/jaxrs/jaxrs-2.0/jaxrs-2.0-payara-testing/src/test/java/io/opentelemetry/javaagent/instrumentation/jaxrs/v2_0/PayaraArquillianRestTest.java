f090074426127b4e843e37ab4e612713531a6175
Lauri Tulmin <ltulmin@splunk.com>
2026-03-19 18:18:32 +0200

    Move jaxrs-2.0 arquillian tests into a package

--- instrumentation/jaxrs/jaxrs-2.0/jaxrs-2.0-payara-testing/src/test/java/io/opentelemetry/javaagent/instrumentation/jaxrs/v2_0/PayaraArquillianRestTest.java
+++ instrumentation/jaxrs/jaxrs-2.0/jaxrs-2.0-payara-testing/src/test/java/io/opentelemetry/javaagent/instrumentation/jaxrs/v2_0/PayaraArquillianRestTest.java
@@ -3,4 +3,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+package io.opentelemetry.javaagent.instrumentation.jaxrs.v2_0;
+
 public class PayaraArquillianRestTest extends AbstractArquillianRestTest {}
