c2a0bdb0d93dbf1809d833a4e91394f4e8c0256e
Lauri Tulmin <ltulmin@splunk.com>
2026-03-19 19:34:18 +0200

    Move jaxrs-2.0 arquillian tests into a package (#16758)

--- instrumentation/jaxrs/jaxrs-2.0/jaxrs-2.0-tomee-testing/src/test/java/io/opentelemetry/javaagent/instrumentation/jaxrs/v2_0/TomeeArquillianRestTest.java
+++ instrumentation/jaxrs/jaxrs-2.0/jaxrs-2.0-tomee-testing/src/test/java/io/opentelemetry/javaagent/instrumentation/jaxrs/v2_0/TomeeArquillianRestTest.java
@@ -3,4 +3,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+package io.opentelemetry.javaagent.instrumentation.jaxrs.v2_0;
+
 public class TomeeArquillianRestTest extends AbstractArquillianRestTest {}
