18e33abebece03e6cf54a3809a1f0b3ea3b242c1
otelbot <197425009+otelbot@users.noreply.github.com>
2026-03-20 17:39:25 +0000

    Review fixes for kubernetes-client-7.0:javaagent
    Automated code review of instrumentation/kubernetes-client-7.0/javaagent.

--- instrumentation/kubernetes-client-7.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/kubernetesclient/KubernetesExperimentalAttributesExtractor.java
+++ instrumentation/kubernetes-client-7.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/kubernetesclient/KubernetesExperimentalAttributesExtractor.java
@@ -17,9 +17,16 @@ class KubernetesExperimentalAttributesExtractor
   @Override
   public void onStart(AttributesBuilder attributes, Context parentContext, Request request) {
     KubernetesRequestDigest digest = KubernetesRequestDigest.parse(request);
+    if (digest.isNonResourceRequest()) {
+      return;
+    }
+    KubernetesResource resourceMeta = digest.getResourceMeta();
+    if (resourceMeta == null) {
+      return;
+    }
     attributes
-        .put("kubernetes-client.namespace", digest.getResourceMeta().getNamespace())
-        .put("kubernetes-client.name", digest.getResourceMeta().getName());
+        .put("kubernetes-client.namespace", resourceMeta.getNamespace())
+        .put("kubernetes-client.name", resourceMeta.getName());
   }
 
   @Override
