ad3bcc2b431d4f4aa9150d8d09dcbc32cdbabe59
Jay DeLuca <jaydeluca4@gmail.com>
2026-03-20 13:10:31 -0400

    Fix clickhouse client v1 muzzle config (#16644)
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>

--- instrumentation/clickhouse/clickhouse-client-v1-0.5/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/clickhouse/clientv1/v0_5/ClickHouseClientV1InstrumentationModule.java
+++ instrumentation/clickhouse/clickhouse-client-v1-0.5/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/clickhouse/clientv1/v0_5/ClickHouseClientV1InstrumentationModule.java
@@ -5,6 +5,7 @@
 
 package io.opentelemetry.javaagent.instrumentation.clickhouse.clientv1.v0_5;
 
+import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.hasClassesNamed;
 import static java.util.Collections.singletonList;
 
 import com.google.auto.service.AutoService;
@@ -12,6 +13,7 @@ import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModul
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import io.opentelemetry.javaagent.extension.instrumentation.internal.ExperimentalInstrumentationModule;
 import java.util.List;
+import net.bytebuddy.matcher.ElementMatcher;
 
 @AutoService(InstrumentationModule.class)
 public class ClickHouseClientV1InstrumentationModule extends InstrumentationModule
@@ -24,6 +26,12 @@ public class ClickHouseClientV1InstrumentationModule extends InstrumentationModu
   @Override
   public boolean isHelperClass(String className) {
     return "com.clickhouse.client.ClickHouseRequestAccess".equals(className);
+  }
+
+  @Override
+  public ElementMatcher.Junction<ClassLoader> classLoaderMatcher() {
+    // Unrelated class which was added in 0.5.0, the minimum version where tests pass
+    return hasClassesNamed("com.clickhouse.client.config.ClickHouseProxyType");
   }
 
   @Override
