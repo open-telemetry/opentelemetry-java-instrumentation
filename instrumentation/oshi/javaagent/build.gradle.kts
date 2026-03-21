348af01f5587a8a5a1f704ca8a22193a75a7c6a8
otelbot <197425009+otelbot@users.noreply.github.com>
2026-03-21 23:54:51 +0000

    Review fixes for oshi:javaagent
    Automated code review of instrumentation/oshi/javaagent.

--- instrumentation/oshi/javaagent/build.gradle.kts
+++ instrumentation/oshi/javaagent/build.gradle.kts
@@ -6,7 +6,8 @@ muzzle {
   pass {
     group.set("com.github.oshi")
     module.set("oshi-core")
-    versions.set("[5.3.1,)")
+    versions.set("[5.0.0,)")
+    assertInverse.set(true)
     // Could not parse POM https://repo.maven.apache.org/maven2/com/github/oshi/oshi-core/6.1.1/oshi-core-6.1.1.pom
     skip("6.1.1")
   }
