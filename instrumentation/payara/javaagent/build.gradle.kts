ca47a27611538b74ce1161a81221bde3441a4477
otelbot <197425009+otelbot@users.noreply.github.com>
2026-03-22 00:20:32 +0000

    Review fixes for payara:javaagent
    Automated code review of instrumentation/payara/javaagent.

--- instrumentation/payara/javaagent/build.gradle.kts
+++ instrumentation/payara/javaagent/build.gradle.kts
@@ -1,3 +1,16 @@
 plugins {
   id("otel.javaagent-instrumentation")
 }
+
+muzzle {
+  pass {
+    group.set("fish.payara.extras")
+    module.set("payara-embedded-web")
+    versions.set("[5.182,)")
+    assertInverse.set(true)
+  }
+}
+
+dependencies {
+  library("fish.payara.extras:payara-embedded-web:5.2021.2")
+}
