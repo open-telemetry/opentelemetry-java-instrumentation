a48976e8f57273949307813781d45b9098eec1f9
SylvainJuge <763082+SylvainJuge@users.noreply.github.com>
2026-03-27 15:44:16 +0100

    make scaka-fork-join indy-ready (#17134)
    Co-authored-by: Lauri Tulmin <tulmin@gmail.com>

--- instrumentation/scala-fork-join-2.8/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/scalaexecutors/ScalaConcurrentInstrumentationModule.java
+++ instrumentation/scala-fork-join-2.8/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/scalaexecutors/ScalaConcurrentInstrumentationModule.java
@@ -10,10 +10,12 @@ import static java.util.Arrays.asList;
 import com.google.auto.service.AutoService;
 import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
+import io.opentelemetry.javaagent.extension.instrumentation.internal.ExperimentalInstrumentationModule;
 import java.util.List;
 
 @AutoService(InstrumentationModule.class)
-public class ScalaConcurrentInstrumentationModule extends InstrumentationModule {
+public class ScalaConcurrentInstrumentationModule extends InstrumentationModule
+    implements ExperimentalInstrumentationModule {
   public ScalaConcurrentInstrumentationModule() {
     super("scala-fork-join", "scala-fork-join-2.8");
   }
@@ -21,5 +23,10 @@ public class ScalaConcurrentInstrumentationModule extends InstrumentationModule 
   @Override
   public List<TypeInstrumentation> typeInstrumentations() {
     return asList(new ScalaForkJoinPoolInstrumentation(), new ScalaForkJoinTaskInstrumentation());
+  }
+
+  @Override
+  public boolean isIndyReady() {
+    return true;
   }
 }
