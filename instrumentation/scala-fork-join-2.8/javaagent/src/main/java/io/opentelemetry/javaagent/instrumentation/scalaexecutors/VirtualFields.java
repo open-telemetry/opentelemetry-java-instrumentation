6a949b1dfc994f8f6ec3c8bf03bf370263cf2f96
Lauri Tulmin <tulmin@gmail.com>
2026-03-27 16:08:30 +0200

    Apply suggestions from code review
    Co-authored-by: Lauri Tulmin <tulmin@gmail.com>

--- instrumentation/scala-fork-join-2.8/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/scalaexecutors/VirtualFields.java
+++ instrumentation/scala-fork-join-2.8/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/scalaexecutors/VirtualFields.java
@@ -15,10 +15,8 @@ public class VirtualFields {
   public static final VirtualField<ForkJoinTask<?>, PropagatedContext>
       FORK_JOIN_TASK_PROPAGATED_CONTEXT =
           VirtualField.find(ForkJoinTask.class, PropagatedContext.class);
-
   public static final VirtualField<Runnable, PropagatedContext> RUNNABLE_PROPAGATED_CONTEXT =
       VirtualField.find(Runnable.class, PropagatedContext.class);
-
   public static final VirtualField<Callable<?>, PropagatedContext> CALLABLE_PROPAGATED_CONTEXT =
       VirtualField.find(Callable.class, PropagatedContext.class);
 
