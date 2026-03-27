560ec517d22d6afc36689d163639be2e60551645
otelbot <197425009+otelbot@users.noreply.github.com>
2026-03-27 23:29:08 +0000

    Review fixes for vertx-http-client-common:javaagent
    Automated code review of instrumentation/vertx/vertx-http-client/vertx-http-client-common/javaagent.

--- instrumentation/vertx/vertx-http-client/vertx-http-client-common/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/vertx/client/Contexts.java
+++ instrumentation/vertx/vertx-http-client/vertx-http-client-common/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/vertx/client/Contexts.java
@@ -7,7 +7,7 @@ package io.opentelemetry.javaagent.instrumentation.vertx.client;
 
 import io.opentelemetry.context.Context;
 
-public class Contexts {
+public final class Contexts {
   public final Context parentContext;
   public final Context context;
 
