2a3d6a5c58d25b17a9e36c0d620d545cb70cdb23
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-03-26 21:10:44 -0700

    Use assertAll for Couchbase cleanup

--- instrumentation/couchbase/couchbase-common/testing/src/main/java/io/opentelemetry/instrumentation/couchbase/springdata/AbstractCouchbaseSpringTemplateTest.java
+++ instrumentation/couchbase/couchbase-common/testing/src/main/java/io/opentelemetry/instrumentation/couchbase/springdata/AbstractCouchbaseSpringTemplateTest.java
@@ -18,6 +18,7 @@ import static io.opentelemetry.semconv.incubating.DbIncubatingAttributes.DB_SYST
 import static io.opentelemetry.semconv.incubating.DbIncubatingAttributes.DbSystemNameIncubatingValues.COUCHBASE;
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Named.named;
 
 import com.couchbase.client.java.Bucket;
@@ -82,12 +83,10 @@ public abstract class AbstractCouchbaseSpringTemplateTest extends AbstractCouchb
         });
   }
 
-  @AfterAll
-  void cleanUp() throws Exception {
-    for (AutoCloseable closeable : cleanup) {
-      closeable.close();
+    @AfterAll
+    void cleanUp() {
+        assertAll(cleanup.stream().map(closeable -> closeable::close));
     }
-  }
 
   private static Stream<Arguments> templates() {
     return Stream.of(
