025edcbb811b93abe6141a066121934b7a607b4d
otelbot <197425009+otelbot@users.noreply.github.com>
2026-04-01 04:03:20 +0000

    Review fixes for grails-3.0:javaagent
    Automated code review of instrumentation/grails-3.0/javaagent.

--- instrumentation/grails-3.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/grails/DefaultGrailsControllerClassInstrumentation.java
+++ instrumentation/grails-3.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/grails/DefaultGrailsControllerClassInstrumentation.java
@@ -65,7 +65,7 @@ public class DefaultGrailsControllerClassInstrumentation implements TypeInstrume
         return new AdviceScope(handlerData, context, context.makeCurrent());
       }
 
-      public void end(Throwable throwable) {
+      public void end(@Nullable Throwable throwable) {
         scope.close();
         instrumenter().end(context, handlerData, null, throwable);
       }
