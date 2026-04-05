0668d314a2e8e9f6a724e5fd30de1d851564f86c
otelbot <197425009+otelbot@users.noreply.github.com>
2026-04-05 00:36:11 +0000

    Review fixes for mongo-3.1:testing
    Automated code review of instrumentation/mongo/mongo-3.1/testing.

--- instrumentation/mongo/mongo-3.1/testing/src/main/java/io/opentelemetry/instrumentation/mongo/v3_1/AbstractMongo31ClientTest.java
+++ instrumentation/mongo/mongo-3.1/testing/src/main/java/io/opentelemetry/instrumentation/mongo/v3_1/AbstractMongo31ClientTest.java
@@ -209,14 +209,15 @@ public abstract class AbstractMongo31ClientTest
   @Test
   void testClientFailure() {
     MongoClientOptions options = MongoClientOptions.builder().serverSelectionTimeout(10).build();
-    MongoClient client = new MongoClient(new ServerAddress(host, PortUtils.UNUSABLE_PORT), options);
-
-    assertThatExceptionOfType(MongoTimeoutException.class)
-        .isThrownBy(
-            () -> {
-              MongoDatabase db = client.getDatabase("test_db");
-              db.createCollection(createCollectionName());
-            });
+    try (MongoClient client =
+        new MongoClient(new ServerAddress(host, PortUtils.UNUSABLE_PORT), options)) {
+      assertThatExceptionOfType(MongoTimeoutException.class)
+          .isThrownBy(
+              () -> {
+                MongoDatabase db = client.getDatabase("test_db");
+                db.createCollection(createCollectionName());
+              });
+    }
     // Unfortunately not caught by our instrumentation.
     assertThat(testing().spans()).isEmpty();
   }
