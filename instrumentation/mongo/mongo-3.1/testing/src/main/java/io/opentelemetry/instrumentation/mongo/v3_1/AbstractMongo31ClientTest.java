0a067558e67010d1508b9ee965e2d8a57f302a47
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-04-04 18:21:38 -0700

    cleanupext

--- instrumentation/mongo/mongo-3.1/testing/src/main/java/io/opentelemetry/instrumentation/mongo/v3_1/AbstractMongo31ClientTest.java
+++ instrumentation/mongo/mongo-3.1/testing/src/main/java/io/opentelemetry/instrumentation/mongo/v3_1/AbstractMongo31ClientTest.java
@@ -19,6 +19,7 @@ import com.mongodb.client.result.DeleteResult;
 import com.mongodb.client.result.UpdateResult;
 import io.opentelemetry.instrumentation.mongo.testing.AbstractMongoClientTest;
 import io.opentelemetry.instrumentation.test.utils.PortUtils;
+import io.opentelemetry.instrumentation.testing.internal.AutoCleanupExtension;
 import java.util.ArrayList;
 import org.bson.BsonDocument;
 import org.bson.BsonString;
@@ -26,9 +27,12 @@ import org.bson.Document;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 public abstract class AbstractMongo31ClientTest
     extends AbstractMongoClientTest<MongoCollection<Document>> {
+
+  @RegisterExtension static final AutoCleanupExtension cleanup = AutoCleanupExtension.create();
 
   protected abstract void configureMongoClientOptions(MongoClientOptions.Builder options);
 
@@ -209,15 +213,15 @@ public abstract class AbstractMongo31ClientTest
   @Test
   void testClientFailure() {
     MongoClientOptions options = MongoClientOptions.builder().serverSelectionTimeout(10).build();
-    try (MongoClient client =
-        new MongoClient(new ServerAddress(host, PortUtils.UNUSABLE_PORT), options)) {
-      assertThatExceptionOfType(MongoTimeoutException.class)
-          .isThrownBy(
-              () -> {
-                MongoDatabase db = client.getDatabase("test_db");
-                db.createCollection(createCollectionName());
-              });
-    }
+    MongoClient client = new MongoClient(new ServerAddress(host, PortUtils.UNUSABLE_PORT), options);
+    cleanup.deferCleanup(client);
+
+    assertThatExceptionOfType(MongoTimeoutException.class)
+        .isThrownBy(
+            () -> {
+              MongoDatabase db = client.getDatabase("test_db");
+              db.createCollection(createCollectionName());
+            });
     // Unfortunately not caught by our instrumentation.
     assertThat(testing().spans()).isEmpty();
   }
