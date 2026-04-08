fe9e5d1c8085e17fc25dbc5945bd2f9b1766c622
otelbot <197425009+otelbot@users.noreply.github.com>
2026-04-08 01:11:44 +0000

    Review fixes for servlet-5.0:testing
    Automated code review of instrumentation/servlet/servlet-5.0/testing.

--- instrumentation/servlet/servlet-5.0/testing/src/main/java/io/opentelemetry/javaagent/instrumentation/servlet/v5_0/TestServlet5.java
+++ instrumentation/servlet/servlet-5.0/testing/src/main/java/io/opentelemetry/javaagent/instrumentation/servlet/v5_0/TestServlet5.java
@@ -294,6 +294,7 @@ public class TestServlet5 {
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
       if (req.getServletPath().equals("/recursive")) {
         resp.getWriter().print("Hello Recursive");
+        return;
       }
 
       int depth = Integer.parseInt(req.getParameter("depth"));
