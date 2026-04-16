c0f0fe86ef43153f0d4da1a0e5f0ad039063ee1a
Lauri Tulmin <ltulmin@splunk.com>
2026-04-16 13:57:11 +0300

    reorder dependency

--- instrumentation/spring/spring-boot-autoconfigure/testing/build.gradle.kts
+++ instrumentation/spring/spring-boot-autoconfigure/testing/build.gradle.kts
@@ -6,11 +6,11 @@ val springBootVersion = "2.7.18"
 
 dependencies {
   compileOnly("org.springframework.boot:spring-boot-restclient:4.0.0")
-  compileOnly("org.springframework.boot:spring-boot-starter-webflux:$springBootVersion")
   compileOnly("org.springframework.kafka:spring-kafka:2.9.0")
 
   compileOnly("org.springframework.boot:spring-boot-starter-test:$springBootVersion")
   compileOnly("org.springframework.boot:spring-boot-starter-data-r2dbc:$springBootVersion")
+  compileOnly("org.springframework.boot:spring-boot-starter-webflux:${springBootVersion}")
 
   compileOnly(project(":instrumentation:micrometer:micrometer-1.5:library"))
   compileOnly(project(":instrumentation:spring:spring-boot-autoconfigure"))
