59d36d161081c22e6eb5d0e85e3c76a7d64b4b99
otelbot <197425009+otelbot@users.noreply.github.com>
2026-04-17 21:26:57 +0000

    Review fixes for aws-sdk-1.11:javaagent
    Automated code review of instrumentation/aws-sdk/aws-sdk-1.11/javaagent.

--- instrumentation/aws-sdk/aws-sdk-1.11/javaagent/build.gradle.kts
+++ instrumentation/aws-sdk/aws-sdk-1.11/javaagent/build.gradle.kts
@@ -158,6 +158,7 @@ tasks {
   val testStableSemconv by registering(Test::class) {
     testClassesDirs = sourceSets.test.get().output.classesDirs
     classpath = sourceSets.test.get().runtimeClasspath
+    usesService(gradle.sharedServices.registrations["testcontainersBuildService"].service)
 
     jvmArgs("-Dotel.semconv-stability.opt-in=database")
     systemProperty("metadataConfig", "otel.semconv-stability.opt-in=database")
