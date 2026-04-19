620f458ec3ebf0aaa319ac481a183bdee44b6446
otelbot <197425009+otelbot@users.noreply.github.com>
2026-04-19 01:22:59 +0000

    Review fixes for google-http-client-1.19:javaagent
    Automated code review of instrumentation/google-http-client-1.19/javaagent.

--- instrumentation/google-http-client-1.19/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/googlehttpclient/GoogleHttpClientAsyncTest.java
+++ instrumentation/google-http-client-1.19/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/googlehttpclient/GoogleHttpClientAsyncTest.java
@@ -7,17 +7,21 @@ package io.opentelemetry.javaagent.instrumentation.googlehttpclient;
 
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpResponse;
+import io.opentelemetry.instrumentation.testing.internal.AutoCleanupExtension;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 class GoogleHttpClientAsyncTest extends AbstractGoogleHttpClientTest {
 
+  @RegisterExtension final AutoCleanupExtension cleanup = AutoCleanupExtension.create();
+
   private final ExecutorService executor = Executors.newFixedThreadPool(4);
 
-  @AfterAll
-  void tearDown() {
-    executor.shutdown();
+  @BeforeAll
+  void setUpExecutor() {
+    cleanup.deferAfterAll(executor::shutdown);
   }
 
   @Override
