a3aefb360e50b49a129e46a40494bc2a1a3e30b5
otelbot <197425009+otelbot@users.noreply.github.com>
2026-04-19 22:52:14 +0000

    Review fixes for jaxrs-2.0-jersey-2.0:javaagent
    Automated code review of instrumentation/jaxrs/jaxrs-2.0/jaxrs-2.0-jersey-2.0/javaagent.

--- instrumentation/jaxrs/jaxrs-2.0/jaxrs-2.0-jersey-2.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/jaxrs/v2_0/JerseyInstrumentationModule.java
+++ instrumentation/jaxrs/jaxrs-2.0/jaxrs-2.0-jersey-2.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/jaxrs/v2_0/JerseyInstrumentationModule.java
@@ -25,7 +25,7 @@ public class JerseyInstrumentationModule extends InstrumentationModule
   @Override
   public ElementMatcher.Junction<ClassLoader> classLoaderMatcher() {
     return hasClassesNamed(
-        // added in jaxrs-api 2.0
+        // added in JAX-RS 1.0, removed in JAX-RS 3.0 (renamed to jakarta.ws.rs.Path)
         "javax.ws.rs.Path",
         // added in jersey-server 2.0
         "org.glassfish.jersey.server.ContainerRequest");
