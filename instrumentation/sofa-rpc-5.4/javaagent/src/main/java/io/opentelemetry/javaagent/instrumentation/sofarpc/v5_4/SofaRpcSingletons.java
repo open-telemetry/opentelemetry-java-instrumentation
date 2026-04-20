675b98c83ffe4af13880c9e45d17e6feac6a493d
fangxiu <fangxiu.wf@alibaba-inc.com>
2026-04-20 14:49:59 +0800

    Fix sofa-rpc-5.4 javaagent: replace PeerServiceAttributesExtractor with ServicePeerAttributesExtractor
    - PeerServiceAttributesExtractor (net package) removed in main
    - Replace with ServicePeerAttributesExtractor.create(getter, openTelemetry)
    - Remove AgentCommonConfig.getPeerServiceResolver() (no longer exists)

--- instrumentation/sofa-rpc-5.4/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/sofarpc/v5_4/SofaRpcSingletons.java
+++ instrumentation/sofa-rpc-5.4/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/sofarpc/v5_4/SofaRpcSingletons.java
@@ -7,10 +7,9 @@ package io.opentelemetry.javaagent.instrumentation.sofarpc.v5_4;
 
 import com.alipay.sofa.rpc.filter.Filter;
 import io.opentelemetry.api.GlobalOpenTelemetry;
-import io.opentelemetry.instrumentation.api.incubator.semconv.net.PeerServiceAttributesExtractor;
+import io.opentelemetry.instrumentation.api.incubator.semconv.service.peer.ServicePeerAttributesExtractor;
 import io.opentelemetry.instrumentation.sofarpc.v5_4.SofaRpcTelemetry;
 import io.opentelemetry.instrumentation.sofarpc.v5_4.internal.SofaRpcClientNetworkAttributesGetter;
-import io.opentelemetry.javaagent.bootstrap.internal.AgentCommonConfig;
 
 public final class SofaRpcSingletons {
   public static final Filter CLIENT_FILTER;
@@ -20,9 +19,8 @@ public final class SofaRpcSingletons {
     SofaRpcTelemetry telemetry =
         SofaRpcTelemetry.builder(GlobalOpenTelemetry.get())
             .addAttributesExtractor(
-                PeerServiceAttributesExtractor.create(
-                    new SofaRpcClientNetworkAttributesGetter(),
-                    AgentCommonConfig.get().getPeerServiceResolver()))
+                ServicePeerAttributesExtractor.create(
+                    new SofaRpcClientNetworkAttributesGetter(), GlobalOpenTelemetry.get()))
             .build();
     CLIENT_FILTER = telemetry.newClientFilter();
     SERVER_FILTER = telemetry.newServerFilter();
