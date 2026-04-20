52e162a64707e87a95ce7c387e12ccea97324ae8
otelbot <197425009+otelbot@users.noreply.github.com>
2026-04-20 15:07:37 +0000

    Review fixes for jetty-8.0:javaagent
    Automated code review of instrumentation/jetty/jetty-8.0/javaagent.

--- instrumentation/jetty/jetty-8.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/jetty/v8_0/Jetty8InstrumentationModule.java
+++ instrumentation/jetty/jetty-8.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/jetty/v8_0/Jetty8InstrumentationModule.java
@@ -9,10 +9,18 @@ import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.
 import static java.util.Arrays.asList;
 
 import com.google.auto.service.AutoService;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.instrumentation.servlet.internal.ServletRequestContext;
+import io.opentelemetry.javaagent.bootstrap.http.HttpServerResponseCustomizerHolder;
 import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import io.opentelemetry.javaagent.instrumentation.jetty.common.JettyHandlerInstrumentation;
 import java.util.List;
+import javax.annotation.Nullable;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import net.bytebuddy.asm.Advice;
 import net.bytebuddy.matcher.ElementMatcher;
 
 @AutoService(InstrumentationModule.class)
@@ -32,8 +40,70 @@ public class Jetty8InstrumentationModule extends InstrumentationModule {
   public List<TypeInstrumentation> typeInstrumentations() {
     return asList(
         new JettyHandlerInstrumentation(
-            "javax.servlet",
-            Jetty8InstrumentationModule.class.getPackage().getName() + ".Jetty8HandlerAdvice"),
+            "javax.servlet", getClass().getName() + "$Jetty8HandlerAdvice"),
         new JettyQueuedThreadPoolInstrumentation());
+  }
+
+  @SuppressWarnings("unused")
+  public static class Jetty8HandlerAdvice {
+
+    public static class AdviceScope {
+      private final ServletRequestContext<HttpServletRequest> requestContext;
+      private final Context context;
+      private final Scope scope;
+
+      private AdviceScope(
+          ServletRequestContext<HttpServletRequest> requestContext, Context context, Scope scope) {
+        this.requestContext = requestContext;
+        this.context = context;
+        this.scope = scope;
+      }
+
+      @Nullable
+      public static AdviceScope start(HttpServletRequest request, HttpServletResponse response) {
+        Context attachedContext = Jetty8Singletons.helper().getServerContext(request);
+        if (attachedContext != null) {
+          // We are inside nested handler, don't create new span
+          return null;
+        }
+        Context parentContext = Context.current();
+        ServletRequestContext<HttpServletRequest> requestContext =
+            new ServletRequestContext<>(request);
+        if (!Jetty8Singletons.helper().shouldStart(parentContext, requestContext)) {
+          return null;
+        }
+        Context context = Jetty8Singletons.helper().start(parentContext, requestContext);
+        Scope scope = context.makeCurrent();
+        // Must be set here since Jetty handlers can use startAsync outside of servlet scope.
+        Jetty8Singletons.helper().setAsyncListenerResponse(context, response);
+        HttpServerResponseCustomizerHolder.getCustomizer()
+            .customize(context, response, new Jetty8ResponseMutator());
+        return new AdviceScope(requestContext, context, scope);
+      }
+
+      public void end(
+          @Nullable Throwable throwable, HttpServletRequest request, HttpServletResponse response) {
+        Jetty8Singletons.helper().end(requestContext, request, response, throwable, context, scope);
+      }
+    }
+
+    @Advice.OnMethodEnter(suppress = Throwable.class, inline = false)
+    @Nullable
+    public static AdviceScope onEnter(
+        @Advice.Argument(2) HttpServletRequest request,
+        @Advice.Argument(3) HttpServletResponse response) {
+      return AdviceScope.start(request, response);
+    }
+
+    @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class, inline = false)
+    public static void stopSpan(
+        @Advice.Argument(2) HttpServletRequest request,
+        @Advice.Argument(3) HttpServletResponse response,
+        @Advice.Thrown @Nullable Throwable throwable,
+        @Advice.Enter @Nullable AdviceScope adviceScope) {
+      if (adviceScope != null) {
+        adviceScope.end(throwable, request, response);
+      }
+    }
   }
 }
