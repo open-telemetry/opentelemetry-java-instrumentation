c443c90ac8ebd8dc6d6836ccd5ef0db626d8bc85
otelbot <197425009+otelbot@users.noreply.github.com>
2026-04-21 21:53:52 +0000

    Review fixes for jsp-2.3:javaagent
    Automated code review of instrumentation/jsp-2.3/javaagent.

--- instrumentation/jsp-2.3/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/jsp/JspSpanAssertionBuilder.java
+++ instrumentation/jsp-2.3/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/jsp/JspSpanAssertionBuilder.java
@@ -18,52 +18,52 @@ class JspSpanAssertionBuilder {
   private Class<?> exceptionClass;
   private boolean errorMessageOptional;
 
-  public JspSpanAssertionBuilder withParent(SpanData parent) {
+  JspSpanAssertionBuilder withParent(SpanData parent) {
     this.parent = parent;
     return this;
   }
 
-  public JspSpanAssertionBuilder withMethod(String method) {
+  JspSpanAssertionBuilder withMethod(String method) {
     this.method = method;
     return this;
   }
 
-  public JspSpanAssertionBuilder withRoute(String route) {
+  JspSpanAssertionBuilder withRoute(String route) {
     this.route = route;
     return this;
   }
 
-  public JspSpanAssertionBuilder withClassName(String className) {
+  JspSpanAssertionBuilder withClassName(String className) {
     this.className = className;
     return this;
   }
 
-  public JspSpanAssertionBuilder withRequestUrlOverride(String requestUrlOverride) {
+  JspSpanAssertionBuilder withRequestUrlOverride(String requestUrlOverride) {
     this.requestUrlOverride = requestUrlOverride;
     return this;
   }
 
-  public JspSpanAssertionBuilder withForwardOrigin(String forwardOrigin) {
+  JspSpanAssertionBuilder withForwardOrigin(String forwardOrigin) {
     this.forwardOrigin = forwardOrigin;
     return this;
   }
 
-  public JspSpanAssertionBuilder withResponseStatus(int responseStatus) {
+  JspSpanAssertionBuilder withResponseStatus(int responseStatus) {
     this.responseStatus = responseStatus;
     return this;
   }
 
-  public JspSpanAssertionBuilder withExceptionClass(Class<?> exceptionClass) {
+  JspSpanAssertionBuilder withExceptionClass(Class<?> exceptionClass) {
     this.exceptionClass = exceptionClass;
     return this;
   }
 
-  public JspSpanAssertionBuilder withErrorMessageOptional(boolean errorMessageOptional) {
+  JspSpanAssertionBuilder withErrorMessageOptional(boolean errorMessageOptional) {
     this.errorMessageOptional = errorMessageOptional;
     return this;
   }
 
-  public JspSpan build() {
+  JspSpan build() {
     JspSpan serverSpan = new JspSpan();
     serverSpan.setParent(this.parent);
     serverSpan.setMethod(this.method);
