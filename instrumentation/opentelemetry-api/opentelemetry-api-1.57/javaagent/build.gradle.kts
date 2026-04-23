26240d7e75bea80241e21ae6c0aecd344ba94ab0
otelbot <197425009+otelbot@users.noreply.github.com>
2026-04-23 20:02:43 +0000

    Review fixes for opentelemetry-api-1.57:javaagent
    Automated code review of instrumentation/opentelemetry-api/opentelemetry-api-1.57/javaagent.

--- instrumentation/opentelemetry-api/opentelemetry-api-1.57/javaagent/build.gradle.kts
+++ instrumentation/opentelemetry-api/opentelemetry-api-1.57/javaagent/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
 
 dependencies {
   compileOnly(project(":opentelemetry-api-shaded-for-instrumenting", configuration = "v1_57"))
+
+  implementation(project(":instrumentation:opentelemetry-api:opentelemetry-api-1.0:javaagent"))
 }
 
 configurations.configureEach {
