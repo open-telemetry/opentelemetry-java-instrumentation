31cf25f6f9abf2e0ab3b1d1c04567ceebc2a8010
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-04-23 17:56:45 -0700

    Restore public visibility for cross-package consumers in kotlinx-coroutines

--- instrumentation/opentelemetry-instrumentation-annotations-1.16/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/instrumentationannotations/KotlinCoroutineUtil.java
+++ instrumentation/opentelemetry-instrumentation-annotations-1.16/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/instrumentationannotations/KotlinCoroutineUtil.java
@@ -11,11 +11,11 @@ import net.bytebuddy.description.method.MethodDescription;
 import net.bytebuddy.description.method.ParameterList;
 import net.bytebuddy.matcher.ElementMatcher;
 
-class KotlinCoroutineUtil {
+public class KotlinCoroutineUtil {
 
   private KotlinCoroutineUtil() {}
 
-  static ElementMatcher<MethodDescription> isKotlinSuspendMethod() {
+  public static ElementMatcher<MethodDescription> isKotlinSuspendMethod() {
     // kotlin suspend methods return Object and take kotlin.coroutines.Continuation as last argument
     return returns(Object.class)
         .and(
