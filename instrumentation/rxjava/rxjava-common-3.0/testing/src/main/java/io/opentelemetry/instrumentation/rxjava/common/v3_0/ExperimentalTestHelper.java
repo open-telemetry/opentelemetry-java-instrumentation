349c0e1b6f87e85615a9610f022c097c7ae63827
otelbot <197425009+otelbot@users.noreply.github.com>
2026-04-24 16:51:55 +0000

    Review fixes for rxjava-common-3.0:testing
    Automated code review of instrumentation/rxjava/rxjava-common-3.0/testing.

--- instrumentation/rxjava/rxjava-common-3.0/testing/src/main/java/io/opentelemetry/instrumentation/rxjava/common/v3_0/ExperimentalTestHelper.java
+++ instrumentation/rxjava/rxjava-common-3.0/testing/src/main/java/io/opentelemetry/instrumentation/rxjava/common/v3_0/ExperimentalTestHelper.java
@@ -5,14 +5,11 @@
 
 package io.opentelemetry.instrumentation.rxjava.common.v3_0;
 
-import javax.annotation.Nullable;
-
-public class ExperimentalTestHelper {
+class ExperimentalTestHelper {
   private static final boolean isEnabled =
       Boolean.getBoolean("otel.instrumentation.rxjava.experimental-span-attributes");
 
-  @Nullable
-  public static Boolean experimentalCanceled(boolean value) {
+  static Boolean experimentalCanceled(boolean value) {
     if (isEnabled) {
       return value;
     }
