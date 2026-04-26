e0c1c950b5db2eeb69f69144e1629ae75def08dd
otelbot <197425009+otelbot@users.noreply.github.com>
2026-04-26 21:20:33 +0000

    Review fixes for internal-eclipse-osgi-3.6:javaagent
    Automated code review of instrumentation/internal/internal-eclipse-osgi-3.6/javaagent.

--- instrumentation/internal/internal-eclipse-osgi-3.6/javaagent/build.gradle.kts
+++ instrumentation/internal/internal-eclipse-osgi-3.6/javaagent/build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
   id("otel.javaagent-instrumentation")
 }
 
+dependencies {
+  compileOnly(project(":javaagent-bootstrap"))
+}
+
 // this instrumentation applies to the class 'org.eclipse.osgi.internal.loader.BundleLoader'
 // which is present in the following artifacts dating back to version 3.6 (2010):
 //
