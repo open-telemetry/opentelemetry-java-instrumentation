7f4314c27d1ba7199a22e93a1558e848c21f11cf
Andreas Svanberg <andreas.svanberg@mensa.se>
2026-04-26 10:41:47 +0200

    Improve span naming when serving a resource
    Requesting a page replaces the span name with the fully qualified class
    name of the Java page class. However, the same does not happen if the
    request hits a resource. In Wicket, a resource can serve things like
    class path resources, file paths, web context files, or anything else.
    
    This change applies the same span renaming as for pages by replacing it
    with the fully qualified class name of the Java ResourceReference.
    
    Instead of many spans named "GET /*" it will be split into for example;
    GET /com.company.resource.ProfilePicture
    GET /com.company.resource.RolesJSON
    GET /org.apache.wicket.request.resource.ContextRelativeResourceReference
    GET /org.apache.wicket.request.resource.PackageResourceReference
    GET /org.apache.wicket.resource.JQueryResourceReference

---
+++ instrumentation/wicket-8.0/common-testing/src/main/java/hello/HelloResource.java
@@ -0,0 +1,18 @@
+package hello;
+
+import org.apache.wicket.request.resource.CharSequenceResource;
+import org.apache.wicket.request.resource.IResource;
+import org.apache.wicket.request.resource.ResourceReference;
+
+public class HelloResource extends ResourceReference {
+  private static final long serialVersionUID = 1L;
+
+  public HelloResource() {
+    super("hello-resource");
+  }
+
+  @Override
+  public IResource getResource() {
+    return new CharSequenceResource("text/plain", "hello resource");
+  }
+}
