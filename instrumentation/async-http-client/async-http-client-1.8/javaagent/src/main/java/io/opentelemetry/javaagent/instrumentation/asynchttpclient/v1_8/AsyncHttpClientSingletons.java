7328787683067e3087b7f70ef656e9502e499d27
otelbot <197425009+otelbot@users.noreply.github.com>
2026-04-26 00:43:24 +0000

    Review fixes for async-http-client-1.8:javaagent
    Automated code review of instrumentation/async-http-client/async-http-client-1.8/javaagent.

--- instrumentation/async-http-client/async-http-client-1.8/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/asynchttpclient/v1_8/AsyncHttpClientSingletons.java
+++ instrumentation/async-http-client/async-http-client-1.8/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/asynchttpclient/v1_8/AsyncHttpClientSingletons.java
@@ -10,14 +10,14 @@ import com.ning.http.client.Response;
 import io.opentelemetry.instrumentation.api.instrumenter.Instrumenter;
 import io.opentelemetry.javaagent.instrumentation.asynchttpclient.common.v1_8.AsyncHttpClientInstrumenterFactory;
 
-public class AsyncHttpClientSingletons {
+class AsyncHttpClientSingletons {
   private static final String INSTRUMENTATION_NAME = "io.opentelemetry.async-http-client-1.8";
 
   private static final Instrumenter<Request, Response> instrumenter =
       AsyncHttpClientInstrumenterFactory.create(
           INSTRUMENTATION_NAME, AsyncHttpClient18Helper.INSTANCE);
 
-  public static Instrumenter<Request, Response> instrumenter() {
+  static Instrumenter<Request, Response> instrumenter() {
     return instrumenter;
   }
 
