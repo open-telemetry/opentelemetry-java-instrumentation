a6e22aa40da2db8200f0a9f6dce6ec055f7594b1
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-04-25 19:58:08 -0700

    Restore public access on AsyncHttpClientSingletons (1.8) for cross-package advice

--- instrumentation/async-http-client/async-http-client-1.8/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/asynchttpclient/v1_8/AsyncHttpClientSingletons.java
+++ instrumentation/async-http-client/async-http-client-1.8/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/asynchttpclient/v1_8/AsyncHttpClientSingletons.java
@@ -10,14 +10,14 @@ import com.ning.http.client.Response;
 import io.opentelemetry.instrumentation.api.instrumenter.Instrumenter;
 import io.opentelemetry.javaagent.instrumentation.asynchttpclient.common.v1_8.AsyncHttpClientInstrumenterFactory;
 
-class AsyncHttpClientSingletons {
+public class AsyncHttpClientSingletons {
   private static final String INSTRUMENTATION_NAME = "io.opentelemetry.async-http-client-1.8";
 
   private static final Instrumenter<Request, Response> instrumenter =
       AsyncHttpClientInstrumenterFactory.create(
           INSTRUMENTATION_NAME, AsyncHttpClient18Helper.INSTANCE);
 
-  static Instrumenter<Request, Response> instrumenter() {
+  public static Instrumenter<Request, Response> instrumenter() {
     return instrumenter;
   }
 
