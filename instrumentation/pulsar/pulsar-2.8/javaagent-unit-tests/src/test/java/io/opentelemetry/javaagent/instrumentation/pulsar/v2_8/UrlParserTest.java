8d5ac3115ddf660423d478cf0529d41d4811b3e3
otelbot <197425009+otelbot@users.noreply.github.com>
2026-04-28 08:20:57 +0000

    Review fixes for pulsar-2.8:javaagent-unit-tests
    Automated code review of instrumentation/pulsar/pulsar-2.8/javaagent-unit-tests.

--- instrumentation/pulsar/pulsar-2.8/javaagent-unit-tests/src/test/java/io/opentelemetry/javaagent/instrumentation/pulsar/v2_8/UrlParserTest.java
+++ instrumentation/pulsar/pulsar-2.8/javaagent-unit-tests/src/test/java/io/opentelemetry/javaagent/instrumentation/pulsar/v2_8/UrlParserTest.java
@@ -7,7 +7,6 @@ package io.opentelemetry.javaagent.instrumentation.pulsar.v2_8;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import io.opentelemetry.javaagent.instrumentation.pulsar.v2_8.UrlParser.UrlData;
 import org.junit.jupiter.api.Test;
 
 class UrlParserTest {
@@ -19,25 +18,33 @@ class UrlParserTest {
     assertThat(UrlParser.parseUrl("localhost:1,localhost:2")).isNull();
     assertThat(UrlParser.parseUrl("localhost:1;localhost:2")).isNull();
 
-    {
-      UrlData url = UrlParser.parseUrl("pulsar://localhost:1");
-      assertThat(url.getHost()).isEqualTo("localhost");
-      assertThat(url.getPort()).isEqualTo(1);
-    }
-    {
-      UrlData url = UrlParser.parseUrl("pulsar://localhost:1/foo");
-      assertThat(url.getHost()).isEqualTo("localhost");
-      assertThat(url.getPort()).isEqualTo(1);
-    }
-    {
-      UrlData url = UrlParser.parseUrl("pulsar://localhost");
-      assertThat(url.getHost()).isEqualTo("localhost");
-      assertThat(url.getPort()).isNull();
-    }
-    {
-      UrlData url = UrlParser.parseUrl("pulsar://localhost:xxx");
-      assertThat(url.getHost()).isEqualTo("localhost");
-      assertThat(url.getPort()).isNull();
-    }
+    assertThat(UrlParser.parseUrl("pulsar://localhost:1"))
+        .isNotNull()
+        .satisfies(
+            url -> {
+              assertThat(url.getHost()).isEqualTo("localhost");
+              assertThat(url.getPort()).isEqualTo(1);
+            });
+    assertThat(UrlParser.parseUrl("pulsar://localhost:1/foo"))
+        .isNotNull()
+        .satisfies(
+            url -> {
+              assertThat(url.getHost()).isEqualTo("localhost");
+              assertThat(url.getPort()).isEqualTo(1);
+            });
+    assertThat(UrlParser.parseUrl("pulsar://localhost"))
+        .isNotNull()
+        .satisfies(
+            url -> {
+              assertThat(url.getHost()).isEqualTo("localhost");
+              assertThat(url.getPort()).isNull();
+            });
+    assertThat(UrlParser.parseUrl("pulsar://localhost:xxx"))
+        .isNotNull()
+        .satisfies(
+            url -> {
+              assertThat(url.getHost()).isEqualTo("localhost");
+              assertThat(url.getPort()).isNull();
+            });
   }
 }
