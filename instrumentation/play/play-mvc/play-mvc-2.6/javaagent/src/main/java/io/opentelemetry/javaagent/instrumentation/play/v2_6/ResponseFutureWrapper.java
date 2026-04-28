37511f4a38a199db32a43e5f7f64843dd09d8c35
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-04-28 11:27:06 -0700

    revert

--- instrumentation/play/play-mvc/play-mvc-2.6/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/play/v2_6/ResponseFutureWrapper.java
+++ instrumentation/play/play-mvc/play-mvc-2.6/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/play/v2_6/ResponseFutureWrapper.java
@@ -6,19 +6,15 @@
 package io.opentelemetry.javaagent.instrumentation.play.v2_6;
 
 import static io.opentelemetry.javaagent.instrumentation.play.v2_6.Play26Singletons.instrumenter;
-import static java.util.logging.Level.FINE;
 
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import io.opentelemetry.context.Context;
-import java.util.logging.Logger;
 import play.api.mvc.Result;
 import scala.concurrent.ExecutionContext;
 import scala.concurrent.Future;
 import scala.runtime.AbstractFunction1;
 
 public class ResponseFutureWrapper {
-
-  private static final Logger logger = Logger.getLogger(ResponseFutureWrapper.class.getName());
 
   public static Future<Result> wrap(
       Future<Result> future, Context context, ExecutionContext executionContext) {
@@ -28,11 +24,7 @@ public class ResponseFutureWrapper {
           @Override
           @CanIgnoreReturnValue
           public Result apply(Result result) {
-            try {
-              instrumenter().end(context, null, null, null);
-            } catch (Throwable t) {
-              logger.log(FINE, "error in play instrumentation", t);
-            }
+            instrumenter().end(context, null, null, null);
             return result;
           }
         },
@@ -40,11 +32,7 @@ public class ResponseFutureWrapper {
           @Override
           @CanIgnoreReturnValue
           public Throwable apply(Throwable throwable) {
-            try {
-              instrumenter().end(context, null, null, throwable);
-            } catch (Throwable t) {
-              logger.log(FINE, "error in play instrumentation", t);
-            }
+            instrumenter().end(context, null, null, throwable);
             return throwable;
           }
         },
