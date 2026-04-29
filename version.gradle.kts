7e67a9f42804f96e49e477c1a5d1942ffae6d9ae
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-04-29 01:34:49 -0700

    Pin japicmp baseline in version.gradle.kts (atomic with apidiff snapshots) (#18391)

--- version.gradle.kts
+++ version.gradle.kts
@@ -1,10 +1,13 @@
 val stableVersion = "2.28.0-SNAPSHOT"
 val alphaVersion = "2.28.0-alpha-SNAPSHOT"
 
+val apidiffBaselineVersion = "2.27.0"
+
 allprojects {
   if (findProperty("otel.stable") != "true") {
     version = alphaVersion
   } else {
     version = stableVersion
   }
+  extra["apidiffBaselineVersion"] = apidiffBaselineVersion
 }
