7a548ea0ce2709022bf3f83f5af1d7ab224e68b1
Lauri Tulmin <ltulmin@splunk.com>
2026-04-29 11:29:22 +0300

    restore check

--- instrumentation/thrift-0.13/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/thrift/v0_13/ThriftInstrumentationModule.java
+++ instrumentation/thrift-0.13/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/thrift/v0_13/ThriftInstrumentationModule.java
@@ -5,12 +5,14 @@
 
 package io.opentelemetry.javaagent.instrumentation.thrift.v0_13;
 
+import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.hasClassesNamed;
 import static java.util.Arrays.asList;
 
 import com.google.auto.service.AutoService;
 import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import java.util.List;
+import net.bytebuddy.matcher.ElementMatcher;
 
 @AutoService(InstrumentationModule.class)
 public class ThriftInstrumentationModule extends InstrumentationModule {
@@ -19,15 +21,12 @@ public class ThriftInstrumentationModule extends InstrumentationModule {
     super("thrift", "thrift-0.13");
   }
 
-  /*
   @Override
   public ElementMatcher.Junction<ClassLoader> classLoaderMatcher() {
     // limit support to 0.13.0 since we don't test with earlier versions
     // added in 0.13.0
     return hasClassesNamed("org.apache.thrift.protocol.ShortStack");
   }
-
-   */
 
   @Override
   public List<TypeInstrumentation> typeInstrumentations() {
