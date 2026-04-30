a0741108f27f1c727f1ca887d6a6b19452b82307
Anuraag Agrawal <anuraaga@gmail.com>
2026-04-30 13:59:34 +0900

    JMS

--- instrumentation/jms/jms-1.1/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/jms/v1_1/AbstractJms1Test.java
+++ instrumentation/jms/jms-1.1/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/jms/v1_1/AbstractJms1Test.java
@@ -167,7 +167,7 @@ abstract class AbstractJms1Test {
     Destination destination = destinationFactory.create(session);
     TextMessage sentMessage = session.createTextMessage("a message");
     sentMessage.setStringProperty("Test_Message_Header", "test");
-    sentMessage.setStringProperty("Uncaptured-Header", "password");
+    sentMessage.setStringProperty("Uncaptured_Header", "password");
     sentMessage.setIntProperty("Test_Message_Int_Header", 1234);
 
     MessageProducer producer = session.createProducer(destination);
