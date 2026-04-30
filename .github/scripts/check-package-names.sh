834359d29191a8b774455b065707f728445ba112
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-04-29 18:39:21 -0700

    Refactor check-package-names.sh to validate both library and javaagent source sets
    Extracts a check_source_set function and runs it for both library and
    javaagent source roots. Previously only library/ was validated.
    
    This adds enforcement for javaagent module/package naming and produces
    explicit allow-lists for the historical exceptions and the no-base-version
    common modules.

--- .github/scripts/check-package-names.sh
+++ .github/scripts/check-package-names.sh
@@ -1,70 +1,178 @@
 #!/bin/bash -e
 
-for dir in $(find instrumentation -name "*.java" | grep library/src/main/java | sed 's#/[^/]*$##' | sort -u); do
+if command -v rg > /dev/null 2>&1; then
+  case "$(uname -s)" in
+    CYGWIN* | MINGW* | MSYS*) path_separator="//" ;;
+    *) path_separator="/" ;;
+  esac
 
-  module_name=$(echo "$dir" | sed 's#.*/\([^/]*\)/library/src/main/java[0-9]*/.*#\1#')
+  source_dirs=$(rg --files --path-separator "$path_separator" instrumentation -g '*.java' \
+    | grep -E '/(library|javaagent)/src/main/java[0-9]*/' \
+    | sed 's#/[^/]*$##' \
+    | sort -u)
+else
+  source_dirs=$(find instrumentation \( -path "*/library/src/main/java*/*.java" -o -path "*/javaagent/src/main/java*/*.java" \) -print \
+    | sed 's#/[^/]*$##' \
+    | sort -u)
+fi
 
-  if [[ "$module_name" =~ ^java- ]]; then
-    continue
-  fi
-  if [[ "$module_name" == "jmx-metrics" ]]; then
-    continue
-  fi
-  if [[ "$module_name" == "runtime-telemetry" ]]; then
-    continue
-  fi
-  if [[ "$module_name" == "graphql-java-common-12.0" ]]; then
-    continue
-  fi
+check_source_set() {
+  local source_set="$1"
+  local expected_prefix="$2"
 
-  # these are possibly problematic
-  if [[ "$dir" == "instrumentation/grpc-1.6/library/src/main/java/io/grpc/override" ]]; then
-    continue
-  fi
-  if [[ "$dir" == "instrumentation/lettuce/lettuce-5.1/library/src/main/java/io/lettuce/core/protocol" ]]; then
-    continue
-  fi
-  if [[ "$dir" == "instrumentation/nats/nats-2.17/library/src/main/java/io/nats/client/impl" ]]; then
-    continue
-  fi
-  if [[ "$dir" == "instrumentation/rxjava/rxjava-1.0/library/src/main/java/rx" ]]; then
-    continue
-  fi
-  if [[ "$dir" == "instrumentation/elasticsearch/elasticsearch-rest-7.0/library/src/main/java/org/elasticsearch/client" ]]; then
-    continue
-  fi
+  while IFS= read -r dir; do
+    if [[ "$dir" != *"/$source_set/src/main/java"* ]]; then
+      continue
+    fi
 
-  # some common modules don't have any base version (might have a variant instead, ex: javax)
-  # - jdbc
-  # - lettuce-common
-  # - netty-common
-  # - resources
-  # - servlet-common
-  # - servlet-common-javax
-  if [[ ! "$module_name" =~ [0-9]$ && "$module_name" != "jdbc" && "$module_name" != "lettuce-common" && "$module_name" != "netty-common" && "$module_name" != "resources" && "$module_name" != "servlet-common" && "$module_name" != "servlet-common-javax" ]]; then
-    echo "module name doesn't have a base version: $dir"
-    exit 1
-  fi
+    module_path=${dir%%/$source_set/src/main/java*}
+    module_name=${module_path##*/}
 
-  # convention: if module ends with -java (followed by version), remove -java from the package name
-  simple_module_name=$(echo "$module_name" | sed 's/-[0-9.]*$//' | sed 's/-java$//' | sed 's/-//g')
-  base_version=$(echo "$module_name" | sed 's/.*-\([0-9.]*\)$/\1/' | sed 's/\./_/g')
+    if [[ "$module_name" =~ ^java- ]]; then
+      continue
+    fi
+    if [[ "$module_name" == "jmx-metrics" ]]; then
+      continue
+    fi
+    # these are possibly problematic
+    if [[ "$source_set" == "library" ]]; then
+      if [[ "$dir" == "instrumentation/grpc-1.6/library/src/main/java/io/grpc/override" ]]; then
+        continue
+      fi
+      if [[ "$dir" == "instrumentation/lettuce/lettuce-5.1/library/src/main/java/io/lettuce/core/protocol" ]]; then
+        continue
+      fi
+      if [[ "$dir" == "instrumentation/nats/nats-2.17/library/src/main/java/io/nats/client/impl" ]]; then
+        continue
+      fi
+      if [[ "$dir" == "instrumentation/rxjava/rxjava-1.0/library/src/main/java/rx" ]]; then
+        continue
+      fi
+      if [[ "$dir" == "instrumentation/elasticsearch/elasticsearch-rest-7.0/library/src/main/java/org/elasticsearch/client" ]]; then
+        continue
+      fi
+      if [[ "$dir" == "instrumentation/servlet/servlet-common/library/src/main/java/io/opentelemetry/instrumentation/servlet/internal" ]]; then
+        continue
+      fi
+    fi
 
-  if [[ "$module_name" =~ [0-9]$ ]]; then
-    expected_package_name="io/opentelemetry/instrumentation/$simple_module_name/v$base_version"
-  else
-    expected_package_name="io/opentelemetry/instrumentation/$simple_module_name"
-  fi
+    if [[ "$source_set" == "javaagent" ]]; then
+      # advice packages that must live under the instrumented library's own namespace
+      case "$dir" in
+        instrumentation/clickhouse/clickhouse-client-v1-0.5/javaagent/src/main/java/com/clickhouse/client*) continue ;;
+        instrumentation/finagle-http-23.11/javaagent/src/main/java/com/twitter/finagle*) continue ;;
+        instrumentation/finagle-http-23.11/javaagent/src/main/java/io/netty/channel*) continue ;;
+        instrumentation/reactor/reactor-netty/reactor-netty-1.0/javaagent/src/main/java/reactor/netty/http/client*) continue ;;
+        instrumentation/spring/spring-webmvc/spring-webmvc-3.1/javaagent/src/main/java/org/springframework/web/servlet/v3_1*) continue ;;
+        instrumentation/spring/spring-webmvc/spring-webmvc-6.0/javaagent/src/main/java/org/springframework/web/servlet/v6_0*) continue ;;
+        instrumentation/vertx/vertx-redis-client-4.0/javaagent/src/main/java/io/vertx/redis/client/impl*) continue ;;
+        instrumentation/vertx/vertx-sql-client/vertx-sql-client-common-4.0/javaagent/src/main/java/io/vertx/sqlclient/impl*) continue ;;
+      esac
 
-  package_name=$(echo "$dir" | sed 's#.*/src/main/java[0-9]*/##')
+      # historical javaagent modules that do not follow the module-name <-> package-name convention
+      case "$dir" in
+        instrumentation/akka/akka-actor-fork-join-2.5/javaagent/*) continue ;;
+        instrumentation/aws-sdk/aws-sdk-1.11/javaagent/src/main/java/io/opentelemetry/instrumentation/awssdk/v1_11) continue ;;
+        instrumentation/aws-sdk/aws-sdk-2.2/javaagent/src/main/java/io/opentelemetry/instrumentation/awssdk/v2_2/internal) continue ;;
+        instrumentation/internal/internal-application-logger/javaagent/*) continue ;;
+        instrumentation/jaxrs/jaxrs-2.0/jaxrs-2.0-annotations/javaagent/*) continue ;;
+        instrumentation/jaxrs/jaxrs-2.0/jaxrs-2.0-common/javaagent/*) continue ;;
+        instrumentation/jaxrs/jaxrs-3.0/jaxrs-3.0-annotations/javaagent/*) continue ;;
+        instrumentation/jaxrs/jaxrs-3.0/jaxrs-3.0-common/javaagent/*) continue ;;
+        instrumentation/jaxrs/jaxrs-common/javaagent/*) continue ;;
+        instrumentation/opentelemetry-api/opentelemetry-api-1.0/javaagent/*) continue ;;
+        instrumentation/opentelemetry-extension-annotations-1.0/javaagent/*) continue ;;
+        instrumentation/opentelemetry-instrumentation-annotations-1.16/javaagent/*) continue ;;
+        instrumentation/opentelemetry-instrumentation-api/javaagent/*) continue ;;
+        instrumentation/servlet/servlet-common/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/servlet) continue ;;
+        instrumentation/servlet/servlet-common/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/servlet/snippet) continue ;;
+        instrumentation/spring/spring-boot-resources/javaagent/*) continue ;;
+      esac
+    fi
 
-  # deal with differences like module name elasticsearch-rest and package name elasticsearch.rest
-  expected_package_name_normalized=$(echo "$expected_package_name" | sed 's#/##g')
-  package_name_normalized=$(echo "$package_name" | sed 's#/##g')
+    # some common modules don't have any base version (might have a variant instead, ex: javax)
+    if [[ ! "$module_name" =~ [0-9]$ ]]; then
+      case "$source_set:$module_name" in
+        # library:
+        library:jdbc) ;;
+        library:lettuce-common) ;;
+        library:netty-common) ;;
+        library:oshi) ;;
+        library:resources) ;;
+        library:runtime-telemetry) ;;
+        library:servlet-common) ;;
+        library:servlet-common-javax) ;;
+        # javaagent:
+        javaagent:elasticsearch-transport-common) ;;
+        javaagent:executors) ;;
+        javaagent:external-annotations) ;;
+        javaagent:http-url-connection) ;;
+        javaagent:internal-application-logger) ;;
+        javaagent:internal-class-loader) ;;
+        javaagent:internal-lambda) ;;
+        javaagent:internal-reflection) ;;
+        javaagent:internal-url-class-loader) ;;
+        javaagent:jaxrs-common) ;;
+        javaagent:jdbc) ;;
+        javaagent:jetty-common) ;;
+        javaagent:jsf-common-jakarta) ;;
+        javaagent:jsf-common-javax) ;;
+        javaagent:methods) ;;
+        javaagent:opensearch-rest-common) ;;
+        javaagent:opentelemetry-instrumentation-api) ;;
+        javaagent:oshi) ;;
+        javaagent:payara) ;;
+        javaagent:quarkus-resteasy-reactive) ;;
+        javaagent:rmi) ;;
+        javaagent:runtime-telemetry) ;;
+        javaagent:servlet-common) ;;
+        javaagent:spring-boot-resources) ;;
+        javaagent:spring-cloud-gateway-common) ;;
+        javaagent:spring-webmvc-common) ;;
+        javaagent:tomcat-common) ;;
+        javaagent:tomcat-jdbc) ;;
+        *)
+          echo "module name doesn't have a base version: $dir"
+          exit 1
+          ;;
+      esac
+    fi
 
-  if [[ "$package_name_normalized" != "$expected_package_name_normalized"* ]]; then
-    echo "ERROR: $dir"
-    exit 1
-  fi
+    # build expected package name by walking the module name's dash-separated tokens:
+    # a version token (e.g. "3.0") becomes "/v3_0", any other token becomes "/<token>";
+    # the literal token "java" is elided (e.g. graphql-java-20.0 -> graphql/v20_0).
+    # this also handles multi-version modules like jaxrs-2.0-resteasy-3.1 -> jaxrs/v2_0/resteasy/v3_1.
+    expected_package_name="$expected_prefix"
+    IFS='-' read -ra module_parts <<< "$module_name"
+    for part in "${module_parts[@]}"; do
+      if [[ "$part" == "java" ]]; then
+        continue
+      fi
+      if [[ "$part" =~ ^[0-9][0-9.]*$ ]]; then
+        expected_package_name="$expected_package_name/v${part//./_}"
+      else
+        expected_package_name="$expected_package_name/$part"
+      fi
+    done
 
-done
+    if [[ "$dir" =~ /src/main/java[0-9]*/(.*)$ ]]; then
+      package_name=${BASH_REMATCH[1]}
+    else
+      echo "ERROR: $dir"
+      exit 1
+    fi
+
+    # deal with differences like module name elasticsearch-rest and package name elasticsearch.rest
+    expected_package_name_normalized=${expected_package_name//\//}
+    package_name_normalized=${package_name//\//}
+
+    if [[ "$package_name_normalized" != "$expected_package_name_normalized"* ]]; then
+      echo "ERROR: $dir"
+      exit 1
+    fi
+
+  done <<< "$source_dirs"
+}
+
+check_source_set "library" "io/opentelemetry/instrumentation"
+check_source_set "javaagent" "io/opentelemetry/javaagent/instrumentation"
