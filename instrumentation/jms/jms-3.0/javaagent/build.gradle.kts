82272bf53dbc61fe10c3d5bbefc1211d32446b02
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-04-29 17:15:45 -0700

    Rename hibernate-common, jedis-common, jms-common, redisson-common modules to versioned form
    Adds base versions to four common javaagent modules and moves their
    internal classes into versioned packages (per #16090 / #18181 convention):
    
    - hibernate-common -> hibernate-common-3.3
      io.opentelemetry.javaagent.instrumentation.hibernate -> .hibernate.common.v3_3
    - jedis-common -> jedis-common-1.4
      io.opentelemetry.javaagent.instrumentation.jedis -> .jedis.common.v1_4
    - jms-common -> jms-common-1.1
      io.opentelemetry.javaagent.instrumentation.jms -> .jms.common.v1_1
      (bootstrap class JmsReceiveContextHolder stays in
       io.opentelemetry.javaagent.bootstrap.jms)
    - redisson-common -> redisson-common-3.0
      io.opentelemetry.javaagent.instrumentation.redisson -> .redisson.common.v3_0
    
    Cross-version test helpers under .../testing/ keep their unversioned
    package names since they are reused by multiple version modules.

--- instrumentation/jms/jms-3.0/javaagent/build.gradle.kts
+++ instrumentation/jms/jms-3.0/javaagent/build.gradle.kts
@@ -22,7 +22,7 @@ muzzle {
 }
 
 dependencies {
-  implementation(project(":instrumentation:jms:jms-common:javaagent"))
+  implementation(project(":instrumentation:jms:jms-common-1.1:javaagent"))
 
   library("jakarta.jms:jakarta.jms-api:3.0.0")
 
