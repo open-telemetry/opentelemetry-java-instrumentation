b8e3c263573e303f11dc605e8b66c015e4c7e28b
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-05-02 10:13:29 -0700

    Switch off of deprecated elasticsearch-oss image

--- instrumentation/elasticsearch/elasticsearch-rest-6.4/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/elasticsearch/rest/v6_4/ElasticsearchRest6Test.java
+++ instrumentation/elasticsearch/elasticsearch-rest-6.4/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/elasticsearch/rest/v6_4/ElasticsearchRest6Test.java
@@ -56,8 +56,7 @@ class ElasticsearchRest6Test {
 
   @BeforeAll
   static void setUp() {
-    elasticsearch =
-        new ElasticsearchContainer("docker.elastic.co/elasticsearch/elasticsearch-oss:6.8.16");
+    elasticsearch = new ElasticsearchContainer("docker.elastic.co/elasticsearch/elasticsearch:6.8.16");
     // limit memory usage
     elasticsearch.withEnv(
         "ES_JAVA_OPTS",
--- instrumentation/elasticsearch/elasticsearch-rest-7.0/library/src/test/java/io/opentelemetry/instrumentation/elasticsearch/rest/v7_0/ElasticsearchRest7Test.java
+++ instrumentation/elasticsearch/elasticsearch-rest-7.0/library/src/test/java/io/opentelemetry/instrumentation/elasticsearch/rest/v7_0/ElasticsearchRest7Test.java
@@ -52,8 +52,7 @@ class ElasticsearchRest7Test {
 
   @BeforeAll
   static void setUp() {
-    elasticsearch =
-        new ElasticsearchContainer("docker.elastic.co/elasticsearch/elasticsearch-oss:7.10.2");
+    elasticsearch = new ElasticsearchContainer("docker.elastic.co/elasticsearch/elasticsearch:7.10.2");
     cleanup.deferAfterAll(elasticsearch::stop);
     // limit memory usage
     elasticsearch.withEnv(
