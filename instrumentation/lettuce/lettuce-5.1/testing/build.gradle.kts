b6f499a905628c67455974079305e927fb4552f7
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-05-02 09:11:54 -0700

    Fix lettuce testing dependency exposure

--- instrumentation/lettuce/lettuce-5.1/testing/build.gradle.kts
+++ instrumentation/lettuce/lettuce-5.1/testing/build.gradle.kts
@@ -8,7 +8,7 @@ dependencies {
   // 6.0+ added protocolVersion access which allows forcing RESP2 for consistency in tests
   compileOnly("io.lettuce:lettuce-core:6.0.0.RELEASE")
 
-  implementation("org.testcontainers:testcontainers")
+  api("org.testcontainers:testcontainers")
   implementation("com.google.guava:guava")
 
   implementation("io.opentelemetry:opentelemetry-api")
