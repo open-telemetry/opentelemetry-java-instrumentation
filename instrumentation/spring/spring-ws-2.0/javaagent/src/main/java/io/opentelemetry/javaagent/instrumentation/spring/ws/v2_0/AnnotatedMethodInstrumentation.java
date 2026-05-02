4b49978a3a8311eadc18b56e64f2bfa49711ffea
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-05-02 10:28:04 -0700

    revert

--- instrumentation/spring/spring-ws-2.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/spring/ws/v2_0/AnnotatedMethodInstrumentation.java
+++ instrumentation/spring/spring-ws-2.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/spring/ws/v2_0/AnnotatedMethodInstrumentation.java
@@ -25,7 +25,7 @@ import net.bytebuddy.matcher.ElementMatcher;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 
 class AnnotatedMethodInstrumentation implements TypeInstrumentation {
-  private static final String[] annotationClasses =
+  private static final String[] ANNOTATION_CLASSES =
       new String[] {
         "org.springframework.ws.server.endpoint.annotation.PayloadRoot",
         "org.springframework.ws.soap.server.endpoint.annotation.SoapAction",
@@ -39,13 +39,13 @@ class AnnotatedMethodInstrumentation implements TypeInstrumentation {
 
   @Override
   public ElementMatcher<TypeDescription> typeMatcher() {
-    return declaresMethod(isAnnotatedWith(namedOneOf(annotationClasses)));
+    return declaresMethod(isAnnotatedWith(namedOneOf(ANNOTATION_CLASSES)));
   }
 
   @Override
   public void transform(TypeTransformer transformer) {
     transformer.applyAdviceToMethod(
-        isMethod().and(isAnnotatedWith(namedOneOf(annotationClasses))),
+        isMethod().and(isAnnotatedWith(namedOneOf(ANNOTATION_CLASSES))),
         getClass().getName() + "$AnnotatedMethodAdvice");
   }
 
