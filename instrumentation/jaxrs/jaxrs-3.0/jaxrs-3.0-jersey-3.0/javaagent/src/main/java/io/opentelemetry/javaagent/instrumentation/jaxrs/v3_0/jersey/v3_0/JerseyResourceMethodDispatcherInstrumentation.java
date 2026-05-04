5ac5592181f158a8816a0cbd79627d1c03f1c1c7
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-05-04 07:12:01 -0700

    remove unused

--- instrumentation/jaxrs/jaxrs-3.0/jaxrs-3.0-jersey-3.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/jaxrs/v3_0/jersey/v3_0/JerseyResourceMethodDispatcherInstrumentation.java
+++ instrumentation/jaxrs/jaxrs-3.0/jaxrs-3.0-jersey-3.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/jaxrs/v3_0/jersey/v3_0/JerseyResourceMethodDispatcherInstrumentation.java
@@ -6,7 +6,6 @@
 package io.opentelemetry.javaagent.instrumentation.jaxrs.v3_0.jersey.v3_0;
 
 import static net.bytebuddy.matcher.ElementMatchers.named;
-import static net.bytebuddy.matcher.ElementMatchers.namedOneOf;
 import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
 
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
@@ -27,12 +26,7 @@ class JerseyResourceMethodDispatcherInstrumentation implements TypeInstrumentati
   public void transform(TypeTransformer transformer) {
     transformer.applyAdviceToMethod(
         named("dispatch")
-            .and(
-                takesArgument(
-                    1,
-                    namedOneOf(
-                        "jakarta.ws.rs.core.Request",
-                        "org.glassfish.jersey.server.ContainerRequest"))),
+            .and(takesArgument(1, named("org.glassfish.jersey.server.ContainerRequest"))),
         getClass().getName() + "$DispatchAdvice");
   }
 
