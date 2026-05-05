c23d08f3e62a6631b1c5791615be973b3f56d8e6
otelbot <197425009+otelbot@users.noreply.github.com>
2026-05-05 18:53:45 +0000

    Cleanup for vertx-kafka-client-3.6:vertx-kafka-client-5-testing
    Automated module cleanup of instrumentation/vertx/vertx-kafka-client-3.6/vertx-kafka-client-5-testing.

--- instrumentation/vertx/vertx-kafka-client-3.6/vertx-kafka-client-5-testing/build.gradle.kts
+++ instrumentation/vertx/vertx-kafka-client-3.6/vertx-kafka-client-5-testing/build.gradle.kts
@@ -43,6 +43,7 @@ testing {
 tasks {
   withType<Test>().configureEach {
     usesService(gradle.sharedServices.registrations["testcontainersBuildService"].service)
+    systemProperty("collectMetadata", otelProps.collectMetadata)
     systemProperty("testLatestDeps", otelProps.testLatestDeps)
   }
 
