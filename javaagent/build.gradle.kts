0e067771955c978b39caa04b8dcbcb7820fc1995
Lauri Tulmin <ltulmin@splunk.com>
2026-05-05 11:22:38 +0300

    Generate class with instrumetnation version

--- javaagent/build.gradle.kts
+++ javaagent/build.gradle.kts
@@ -185,6 +185,9 @@ tasks {
     filesMatching("META-INF/io/opentelemetry/instrumentation/**") {
       duplicatesStrategy = DuplicatesStrategy.EXCLUDE
     }
+    filesMatching("**/InstrumentationVersion.class") {
+      duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    }
     exclude("META-INF/LICENSE")
     exclude("META-INF/NOTICE")
     exclude("META-INF/maven/**")
