6803ce8a8739f1de11b3804d7d428a4af4b4d8e7
otelbot <197425009+otelbot@users.noreply.github.com>
2026-05-05 05:47:06 +0000

    Cleanup for servlet-3.0:javaagent
    Automated module cleanup of instrumentation/servlet/servlet-3.0/javaagent.

--- instrumentation/servlet/servlet-3.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/servlet/v3_0/Servlet3HttpServerResponseMutator.java
+++ instrumentation/servlet/servlet-3.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/servlet/v3_0/Servlet3HttpServerResponseMutator.java
@@ -8,9 +8,8 @@ package io.opentelemetry.javaagent.instrumentation.servlet.v3_0;
 import io.opentelemetry.javaagent.bootstrap.http.HttpServerResponseMutator;
 import javax.servlet.http.HttpServletResponse;
 
-public enum Servlet3HttpServerResponseMutator
+public class Servlet3HttpServerResponseMutator
     implements HttpServerResponseMutator<HttpServletResponse> {
-  INSTANCE;
 
   @Override
   public void appendHeader(HttpServletResponse response, String name, String value) {
