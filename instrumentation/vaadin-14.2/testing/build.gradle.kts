84825f44b8de01b04014898a18d3fe2fae457bd9
otelbot <197425009+otelbot@users.noreply.github.com>
2026-05-05 18:17:14 +0000

    Cleanup for vaadin-14.2:testing
    Automated module cleanup of instrumentation/vaadin-14.2/testing.

--- instrumentation/vaadin-14.2/testing/build.gradle.kts
+++ instrumentation/vaadin-14.2/testing/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 dependencies {
   compileOnly("com.vaadin:vaadin-spring-boot-starter:14.2.0")
 
-  api("org.testcontainers:testcontainers-selenium")
+  implementation("org.testcontainers:testcontainers-selenium")
   implementation("io.opentelemetry.javaagent:opentelemetry-testing-common")
   implementation("org.seleniumhq.selenium:selenium-java:4.8.3")
 }
