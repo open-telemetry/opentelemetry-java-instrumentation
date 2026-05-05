4de7b292a9493365c9b2d58ca261a6c46dc05c6d
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-05-05 13:12:15 -0700

    revert

--- instrumentation/vibur-dbcp-11.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/viburdbcp/v11_0/ViburDbcpInstrumentationModule.java
+++ instrumentation/vibur-dbcp-11.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/viburdbcp/v11_0/ViburDbcpInstrumentationModule.java
@@ -5,17 +5,27 @@
 
 package io.opentelemetry.javaagent.instrumentation.viburdbcp.v11_0;
 
+import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.hasClassesNamed;
 import static java.util.Collections.singletonList;
 
 import com.google.auto.service.AutoService;
 import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import java.util.List;
+import net.bytebuddy.matcher.ElementMatcher;
 
 @AutoService(InstrumentationModule.class)
 public class ViburDbcpInstrumentationModule extends InstrumentationModule {
   public ViburDbcpInstrumentationModule() {
     super("vibur-dbcp", "vibur-dbcp-11.0");
+  }
+
+  @Override
+  public ElementMatcher.Junction<ClassLoader> classLoaderMatcher() {
+    // in 11.0, the ViburDBCPDataSource#getPool() method signature was changed - this is detected by
+    // muzzle
+    // added in 10.0
+    return hasClassesNamed("org.vibur.dbcp.ViburConfig");
   }
 
   @Override
