d45e431604252c0fb6b1ee9cf722d28cbb2a27c4
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2026-05-08 12:19:11 +0000

    test(kubernetes-client-7.0): parameterize KubernetesRequestUtilsTest with shared case class Signed-off-by: Gregor Zeitlinger <gregor.zeitlinger@grafana.com>

--- instrumentation/kubernetes-client-7.0/javaagent-unit-tests/src/test/java/io/opentelemetry/javaagent/instrumentation/kubernetesclient/v7_0/KubernetesRequestUtilsTest.java
+++ instrumentation/kubernetes-client-7.0/javaagent-unit-tests/src/test/java/io/opentelemetry/javaagent/instrumentation/kubernetesclient/v7_0/KubernetesRequestUtilsTest.java
@@ -15,6 +15,47 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 class KubernetesRequestUtilsTest {
 
+  static final class ParseCase {
+    final String name;
+    final String urlPath;
+    final Resource expected;
+
+    ParseCase(String name, String urlPath, Resource expected) {
+      this.name = name;
+      this.urlPath = urlPath;
+      this.expected = expected;
+    }
+
+    @Override
+    public String toString() {
+      return name;
+    }
+  }
+
+  static final class Resource {
+    final String apiGroup;
+    final String apiVersion;
+    final String resource;
+    final String subResource;
+    final String namespace;
+    final String name;
+
+    Resource(
+        String apiGroup,
+        String apiVersion,
+        String resource,
+        String subResource,
+        String namespace,
+        String name) {
+      this.apiGroup = apiGroup;
+      this.apiVersion = apiVersion;
+      this.resource = resource;
+      this.subResource = subResource;
+      this.namespace = namespace;
+      this.name = name;
+    }
+  }
+
   @Test
   void isResourceRequest() {
     assertThat(KubernetesRequestDigest.isResourceRequest("/api")).isFalse();
@@ -38,46 +79,28 @@ class KubernetesRequestUtilsTest {
         .isTrue();
   }
 
-  @ParameterizedTest
+  @ParameterizedTest(name = "{0}")
   @MethodSource("parseCoreResourceArguments")
-  void parseCoreResource(
-      String urlPath,
-      String apiGroup,
-      String apiVersion,
-      String resource,
-      String subResource,
-      String namespace,
-      String name)
-      throws ParseKubernetesResourceException {
-    assertThat(KubernetesResource.parseCoreResource(urlPath).getApiGroup()).isEqualTo(apiGroup);
-    assertThat(KubernetesResource.parseCoreResource(urlPath).getApiVersion()).isEqualTo(apiVersion);
-    assertThat(KubernetesResource.parseCoreResource(urlPath).getResource()).isEqualTo(resource);
-    assertThat(KubernetesResource.parseCoreResource(urlPath).getSubResource())
-        .isEqualTo(subResource);
-    assertThat(KubernetesResource.parseCoreResource(urlPath).getNamespace()).isEqualTo(namespace);
-    assertThat(KubernetesResource.parseCoreResource(urlPath).getName()).isEqualTo(name);
+  void parseCoreResource(String name, ParseCase testCase) throws ParseKubernetesResourceException {
+    KubernetesResource actual = KubernetesResource.parseCoreResource(testCase.urlPath);
+    assertResourceEquals(actual, testCase.expected);
   }
 
-  @ParameterizedTest
+  @ParameterizedTest(name = "{0}")
   @MethodSource("parseRegularResourceArguments")
-  void parseRegularResource(
-      String urlPath,
-      String apiGroup,
-      String apiVersion,
-      String resource,
-      String subResource,
-      String namespace,
-      String name)
+  void parseRegularResource(String name, ParseCase testCase)
       throws ParseKubernetesResourceException {
-    assertThat(KubernetesResource.parseRegularResource(urlPath).getApiGroup()).isEqualTo(apiGroup);
-    assertThat(KubernetesResource.parseRegularResource(urlPath).getApiVersion())
-        .isEqualTo(apiVersion);
-    assertThat(KubernetesResource.parseRegularResource(urlPath).getResource()).isEqualTo(resource);
-    assertThat(KubernetesResource.parseRegularResource(urlPath).getSubResource())
-        .isEqualTo(subResource);
-    assertThat(KubernetesResource.parseRegularResource(urlPath).getNamespace())
-        .isEqualTo(namespace);
-    assertThat(KubernetesResource.parseRegularResource(urlPath).getName()).isEqualTo(name);
+    KubernetesResource actual = KubernetesResource.parseRegularResource(testCase.urlPath);
+    assertResourceEquals(actual, testCase.expected);
+  }
+
+  private static void assertResourceEquals(KubernetesResource actual, Resource expected) {
+    assertThat(actual.getApiGroup()).isEqualTo(expected.apiGroup);
+    assertThat(actual.getApiVersion()).isEqualTo(expected.apiVersion);
+    assertThat(actual.getResource()).isEqualTo(expected.resource);
+    assertThat(actual.getSubResource()).isEqualTo(expected.subResource);
+    assertThat(actual.getNamespace()).isEqualTo(expected.namespace);
+    assertThat(actual.getName()).isEqualTo(expected.name);
   }
 
   @ParameterizedTest
@@ -105,72 +128,59 @@ class KubernetesRequestUtilsTest {
 
   private static Stream<Arguments> parseRegularResourceArguments() {
     return Stream.of(
-        Arguments.of("/apis/apps/v1/deployments", "apps", "v1", "deployments", null, null, null),
-        Arguments.of(
-            "/apis/apps/v1/namespaces/default/deployments",
-            "apps",
-            "v1",
-            "deployments",
-            null,
-            "default",
-            null),
-        Arguments.of(
-            "/apis/apps/v1/namespaces/default/deployments/foo",
-            "apps",
-            "v1",
-            "deployments",
-            null,
-            "default",
-            "foo"),
-        Arguments.of(
-            "/apis/apps/v1/namespaces/default/deployments/foo/status",
-            "apps",
-            "v1",
-            "deployments",
-            "status",
-            "default",
-            "foo"),
-        Arguments.of(
-            "/apis/example.io/v1alpha1/foos", "example.io", "v1alpha1", "foos", null, null, null),
-        Arguments.of(
-            "/apis/example.io/v1alpha1/namespaces/default/foos",
-            "example.io",
-            "v1alpha1",
-            "foos",
-            null,
-            "default",
-            null),
-        Arguments.of(
-            "/apis/example.io/v1alpha1/namespaces/default/foos/foo",
-            "example.io",
-            "v1alpha1",
-            "foos",
-            null,
-            "default",
-            "foo"),
-        Arguments.of(
-            "/apis/example.io/v1alpha1/namespaces/default/foos/foo/status",
-            "example.io",
-            "v1alpha1",
-            "foos",
-            "status",
-            "default",
-            "foo"));
+            new ParseCase(
+                "cluster-scoped list",
+                "/apis/apps/v1/deployments",
+                new Resource("apps", "v1", "deployments", null, null, null)),
+            new ParseCase(
+                "namespaced list",
+                "/apis/apps/v1/namespaces/default/deployments",
+                new Resource("apps", "v1", "deployments", null, "default", null)),
+            new ParseCase(
+                "namespaced named",
+                "/apis/apps/v1/namespaces/default/deployments/foo",
+                new Resource("apps", "v1", "deployments", null, "default", "foo")),
+            new ParseCase(
+                "namespaced named subresource",
+                "/apis/apps/v1/namespaces/default/deployments/foo/status",
+                new Resource("apps", "v1", "deployments", "status", "default", "foo")),
+            new ParseCase(
+                "custom resource cluster-scoped list",
+                "/apis/example.io/v1alpha1/foos",
+                new Resource("example.io", "v1alpha1", "foos", null, null, null)),
+            new ParseCase(
+                "custom resource namespaced list",
+                "/apis/example.io/v1alpha1/namespaces/default/foos",
+                new Resource("example.io", "v1alpha1", "foos", null, "default", null)),
+            new ParseCase(
+                "custom resource namespaced named",
+                "/apis/example.io/v1alpha1/namespaces/default/foos/foo",
+                new Resource("example.io", "v1alpha1", "foos", null, "default", "foo")),
+            new ParseCase(
+                "custom resource namespaced named subresource",
+                "/apis/example.io/v1alpha1/namespaces/default/foos/foo/status",
+                new Resource("example.io", "v1alpha1", "foos", "status", "default", "foo")))
+        .map(c -> Arguments.of(c.name, c));
   }
 
   private static Stream<Arguments> parseCoreResourceArguments() {
     return Stream.of(
-        Arguments.of("/api/v1/pods", "", "v1", "pods", null, null, null),
-        Arguments.of("/api/v1/namespaces/default/pods", "", "v1", "pods", null, "default", null),
-        Arguments.of(
-            "/api/v1/namespaces/default/pods/foo", "", "v1", "pods", null, "default", "foo"),
-        Arguments.of(
-            "/api/v1/namespaces/default/pods/foo/exec",
-            "",
-            "v1",
-            "pods",
-            "exec",
-            "default",
-            "foo"));
+            new ParseCase(
+                "cluster-scoped list",
+                "/api/v1/pods",
+                new Resource("", "v1", "pods", null, null, null)),
+            new ParseCase(
+                "namespaced list",
+                "/api/v1/namespaces/default/pods",
+                new Resource("", "v1", "pods", null, "default", null)),
+            new ParseCase(
+                "namespaced named",
+                "/api/v1/namespaces/default/pods/foo",
+                new Resource("", "v1", "pods", null, "default", "foo")),
+            new ParseCase(
+                "namespaced named subresource",
+                "/api/v1/namespaces/default/pods/foo/exec",
+                new Resource("", "v1", "pods", "exec", "default", "foo")))
+        .map(c -> Arguments.of(c.name, c));
   }
 }
