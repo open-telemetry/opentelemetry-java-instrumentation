93b37ee309aa1fb4bbef874c4c137ca321c3b51b
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2026-05-12 08:41:45 +0000

    fix: export incubator model dependency from config bridge Signed-off-by: Gregor Zeitlinger <gregor.zeitlinger@grafana.com>

--- declarative-config-bridge/build.gradle.kts
+++ declarative-config-bridge/build.gradle.kts
@@ -8,8 +8,8 @@ group = "io.opentelemetry.instrumentation"
 
 dependencies {
   compileOnly("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure")
-  // Only declarative-config users need the incubator model classes at runtime.
-  compileOnly("io.opentelemetry:opentelemetry-sdk-extension-incubator")
+  // DefaultInstrumentationConfigApplier exposes incubator model types in the public API.
+  api("io.opentelemetry:opentelemetry-sdk-extension-incubator")
   implementation("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure-spi")
   implementation("io.opentelemetry:opentelemetry-api-incubator")
 
