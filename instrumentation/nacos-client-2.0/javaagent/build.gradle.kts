83a03bbb2049639356e26d8f88e3dce097769ab4
peachisai <2581009893@qq.com>
2026-05-16 08:54:15 +0800

    fix version issues

--- instrumentation/nacos-client-2.0/javaagent/build.gradle.kts
+++ instrumentation/nacos-client-2.0/javaagent/build.gradle.kts
@@ -6,13 +6,14 @@ muzzle {
   pass {
     group.set("com.alibaba.nacos")
     module.set("nacos-client")
-    versions.set("[2.0.0,)")
+    versions.set("[2.0.0,3.0.0)")
     assertInverse.set(true)
   }
 }
 
 dependencies {
   library("com.alibaba.nacos:nacos-client:2.0.0")
+  latestDepTestLibrary("com.alibaba.nacos:nacos-client:2.+") // only supports Nacos 2.x
 
   testImplementation(project(":instrumentation-api"))
   testImplementation(project(":instrumentation-api-incubator"))
