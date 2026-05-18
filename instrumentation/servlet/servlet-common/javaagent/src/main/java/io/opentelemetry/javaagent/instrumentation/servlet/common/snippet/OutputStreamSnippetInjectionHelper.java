d52a5ff2c5aaeb8e5eb2a7149d05a998c7815fdc
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-05-18 07:09:08 -0700

    Rename servlet common snippet package (#18777)

--- instrumentation/servlet/servlet-common/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/servlet/common/snippet/OutputStreamSnippetInjectionHelper.java
+++ instrumentation/servlet/servlet-common/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/servlet/common/snippet/OutputStreamSnippetInjectionHelper.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.opentelemetry.javaagent.instrumentation.servlet.snippet;
+package io.opentelemetry.javaagent.instrumentation.servlet.common.snippet;
 
 import static java.util.logging.Level.FINE;
 
