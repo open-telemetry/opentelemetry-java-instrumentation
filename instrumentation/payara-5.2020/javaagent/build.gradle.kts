1c2e519b898b85fbb3783b809c91c2e48bb04add
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-05-22 11:21:25 -0700

    Rename Payara module to include base version

--- instrumentation/payara-5.2020/javaagent/build.gradle.kts
+++ instrumentation/payara-5.2020/javaagent/build.gradle.kts
@@ -5,5 +5,5 @@ plugins {
 // No muzzle check because this instrumentation is written in ASM and muzzle won't work with it.
 
 dependencies {
-  library("fish.payara.extras:payara-embedded-web:5.2021.2")
+  library("fish.payara.extras:payara-embedded-web:5.2020.2")
 }
