0bcb87f8eb249b60bef645b3ab77e5e078122fa2
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2026-05-22 15:45:56 +0200

    pekko

---
+++ instrumentation/pekko/pekko-http-1.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/pekkohttp/v1_0/package-info.java
@@ -0,0 +1,5 @@
+@ClassLoadingStrategy(ClassLoadingTarget.INSTRUMENTATION_SHARED)
+package io.opentelemetry.javaagent.instrumentation.pekkohttp.v1_0;
+
+import io.opentelemetry.javaagent.extension.instrumentation.internal.ClassLoadingStrategy;
+import io.opentelemetry.javaagent.extension.instrumentation.internal.ClassLoadingTarget;
---
+++ instrumentation/pekko/pekko-http-1.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/pekkohttp/v1_0/server/package-info.java
@@ -0,0 +1,5 @@
+@ClassLoadingStrategy(ClassLoadingTarget.INSTRUMENTATION_SHARED)
+package io.opentelemetry.javaagent.instrumentation.pekkohttp.v1_0.server;
+
+import io.opentelemetry.javaagent.extension.instrumentation.internal.ClassLoadingStrategy;
+import io.opentelemetry.javaagent.extension.instrumentation.internal.ClassLoadingTarget;
