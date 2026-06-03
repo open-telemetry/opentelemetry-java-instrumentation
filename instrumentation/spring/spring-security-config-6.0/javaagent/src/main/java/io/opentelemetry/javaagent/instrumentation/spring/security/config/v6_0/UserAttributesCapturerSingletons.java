faf6d8f9e9eacd1204b6a7759ec5cc6d6c5199ce
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-06-03 08:13:21 -0700

    spring security declarative config

--- instrumentation/spring/spring-security-config-6.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/spring/security/config/v6_0/UserAttributesCapturerSingletons.java
+++ instrumentation/spring/spring-security-config-6.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/spring/security/config/v6_0/UserAttributesCapturerSingletons.java
@@ -8,6 +8,7 @@ package io.opentelemetry.javaagent.instrumentation.spring.security.config.v6_0;
 import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.incubator.config.DeclarativeConfigProperties;
 import io.opentelemetry.instrumentation.api.incubator.config.internal.DeclarativeConfigUtil;
+import io.opentelemetry.instrumentation.api.internal.SemconvStability;
 import io.opentelemetry.instrumentation.spring.security.config.v6_0.UserAttributesCapturer;
 import io.opentelemetry.javaagent.bootstrap.internal.AgentCommonConfig;
 
@@ -26,17 +27,26 @@ public class UserAttributesCapturerSingletons {
     capturer.setRoleEnabled(AgentCommonConfig.get().getUserConfig().isRoleEnabled());
     capturer.setScopeEnabled(AgentCommonConfig.get().getUserConfig().isScopeEnabled());
 
-    DeclarativeConfigProperties endUserConfig =
-        DeclarativeConfigUtil.getInstrumentationConfig(GlobalOpenTelemetry.get(), "spring_security")
-            .get("enduser");
-    String rolePrefix = endUserConfig.get("role").getString("granted_authority_prefix");
+    DeclarativeConfigProperties springSecurityConfig =
+        DeclarativeConfigUtil.getInstrumentationConfig(
+            GlobalOpenTelemetry.get(), "spring_security");
+
+    boolean v3Preview = SemconvStability.v3Preview();
+    String rolePrefix =
+        v3Preview
+            ? springSecurityConfig.get("user").get("roles").getString("granted_authority_prefix")
+            : springSecurityConfig.get("enduser").get("role").getString("granted_authority_prefix");
     if (rolePrefix != null) {
       capturer.setRoleGrantedAuthorityPrefix(rolePrefix);
     }
 
-    String scopePrefix = endUserConfig.get("scope").getString("granted_authority_prefix");
-    if (scopePrefix != null) {
-      capturer.setScopeGrantedAuthorityPrefix(scopePrefix);
+    // enduser.scope has no user.* equivalent and is not captured when v3 preview is enabled
+    if (!v3Preview) {
+      String scopePrefix =
+          springSecurityConfig.get("enduser").get("scope").getString("granted_authority_prefix");
+      if (scopePrefix != null) {
+        capturer.setScopeGrantedAuthorityPrefix(scopePrefix);
+      }
     }
     return capturer;
   }
