450054709a5ca6d0821d74c6fe3d0877650f9172
Lauri Tulmin <ltulmin@splunk.com>
2026-06-03 18:54:39 +0300

    Move shaded client tests into a suite

--- instrumentation/hbase/hbase-client-2.0/javaagent/build.gradle.kts
+++ instrumentation/hbase/hbase-client-2.0/javaagent/build.gradle.kts
@@ -18,32 +18,53 @@ muzzle {
 
 dependencies {
   library("org.apache.hbase:hbase-client:2.0.0")
-  latestDepTestLibrary("org.apache.hbase:hbase-client:2.4.+") // native on-by-default instrumentation after this version
+
   compileOnly("com.google.auto.value:auto-value-annotations")
   annotationProcessor("com.google.auto.value:auto-value")
+
   testImplementation(project(":instrumentation:hbase:hbase-common:testing"))
+
+  latestDepTestLibrary("org.apache.hbase:hbase-client:2.4.+") // native on-by-default instrumentation after this version
+}
+
+testing {
+  suites {
+    val shadedClientTest by registering(JvmTestSuite::class) {
+      dependencies {
+        implementation("org.apache.hbase:hbase-shaded-client:${baseVersion("2.0.0").orLatest("2.4.+")}")
+        implementation(project(":instrumentation:hbase:hbase-common:testing"))
+      }
+    }
+  }
+}
+
+abstract class HbaseBuildService : BuildService<BuildServiceParameters.None>
+
+// HBase test container binds fixed host ports, disallow running tests in parallel.
+gradle.sharedServices.registerIfAbsent("hbaseBuildService", HbaseBuildService::class.java) {
+  maxParallelUsages.convention(1)
 }
 
 tasks {
   withType<Test>().configureEach {
     usesService(gradle.sharedServices.registrations["testcontainersBuildService"].service)
+    usesService(gradle.sharedServices.registrations["hbaseBuildService"].service)
     systemProperty("collectMetadata", otelProps.collectMetadata)
   }
 
-  val testStableSemconv by registering(Test::class) {
-    // HBase test container binds fixed host ports, so do not run it alongside the default test task.
-    mustRunAfter(named("test"))
+  val stableSemconvSuites = testing.suites.withType(JvmTestSuite::class)
+    .map { suite ->
+      register<Test>("${suite.name}StableSemconv") {
+        testClassesDirs = suite.sources.output.classesDirs
+        classpath = suite.sources.runtimeClasspath
 
-    testClassesDirs = sourceSets.test.get().output.classesDirs
-    classpath = sourceSets.test.get().runtimeClasspath
-
-    jvmArgs("-Dotel.semconv-stability.opt-in=database")
-    systemProperty("metadataConfig", "otel.semconv-stability.opt-in=database")
-    systemProperty("collectMetadata", otelProps.collectMetadata)
-  }
+        jvmArgs("-Dotel.semconv-stability.opt-in=database")
+        systemProperty("metadataConfig", "otel.semconv-stability.opt-in=database")
+      }
+    }
 
   check {
-    dependsOn(testStableSemconv)
+    dependsOn(testing.suites, stableSemconvSuites)
   }
 
   if (otelProps.denyUnsafe) {
--- instrumentation/hbase/hbase-common/testing/build.gradle.kts
+++ instrumentation/hbase/hbase-common/testing/build.gradle.kts
@@ -6,6 +6,5 @@ dependencies {
   api("io.opentelemetry.javaagent:opentelemetry-testing-common")
 
   compileOnly("org.apache.hbase:hbase-client:2.0.0")
-  compileOnly("com.github.docker-java:docker-java-api:3.3.0")
-  compileOnly("org.testcontainers:testcontainers")
+  implementation("org.testcontainers:testcontainers")
 }
