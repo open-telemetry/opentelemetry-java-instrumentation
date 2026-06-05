58c5e03fff414442f785d9041af33f6f23c1bbb6
Phil Clay <philsttr@users.noreply.github.com>
2026-06-05 13:06:13 -0600

    Updated jboss-logmanager-appender-1.1 test to verify excluded keys

--- instrumentation/jboss-logmanager/jboss-logmanager-appender-1.1/javaagent/build.gradle.kts
+++ instrumentation/jboss-logmanager/jboss-logmanager-appender-1.1/javaagent/build.gradle.kts
@@ -26,7 +26,7 @@ if (otelProps.testLatestDeps) {
 
 tasks.test {
   // TODO run tests both with and without experimental log attributes
-  jvmArgs("-Dotel.instrumentation.jboss-logmanager.experimental.capture-mdc-attributes=*")
+  jvmArgs("-Dotel.instrumentation.jboss-logmanager.experimental.capture-mdc-attributes=*,!excludedKey")
   jvmArgs("-Dotel.instrumentation.jboss-logmanager.experimental-log-attributes=true")
   jvmArgs("-Dotel.instrumentation.java-util-logging.experimental-log-attributes=true")
 }
