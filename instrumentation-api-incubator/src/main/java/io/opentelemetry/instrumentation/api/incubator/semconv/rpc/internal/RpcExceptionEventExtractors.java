b956239e4ac4426c1dd4ce477ecf2fb034525d47
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-06-06 16:36:41 -0700

    Relax RPC helper builder types

--- instrumentation-api-incubator/src/main/java/io/opentelemetry/instrumentation/api/incubator/semconv/rpc/internal/RpcExceptionEventExtractors.java
+++ instrumentation-api-incubator/src/main/java/io/opentelemetry/instrumentation/api/incubator/semconv/rpc/internal/RpcExceptionEventExtractors.java
@@ -5,7 +5,6 @@
 
 package io.opentelemetry.instrumentation.api.incubator.semconv.rpc.internal;
 
-import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import io.opentelemetry.api.logs.Severity;
 import io.opentelemetry.instrumentation.api.instrumenter.InstrumenterBuilder;
 import io.opentelemetry.instrumentation.api.internal.Experimental;
@@ -20,34 +19,28 @@ public final class RpcExceptionEventExtractors {
    * Configures the RPC client exception event name and severity. Only takes effect when emitting
    * exceptions as logs is enabled via the {@code otel.semconv.exception.signal.preview} flag.
    */
-  @CanIgnoreReturnValue
-  public static <REQUEST, RESPONSE>
-      InstrumenterBuilder<REQUEST, RESPONSE> setRpcClientExceptionEventExtractor(
-          InstrumenterBuilder<REQUEST, RESPONSE> builder) {
+  public static <REQUEST> void setRpcClientExceptionEventExtractor(
+      InstrumenterBuilder<REQUEST, ?> builder) {
     Experimental.setExceptionEventExtractor(
         builder,
         (logRecordBuilder, context, request) -> {
           logRecordBuilder.setEventName("rpc.client.call.exception");
           logRecordBuilder.setSeverity(Severity.WARN);
         });
-    return builder;
   }
 
   /**
    * Configures the RPC server exception event name and severity. Only takes effect when emitting
    * exceptions as logs is enabled via the {@code otel.semconv.exception.signal.preview} flag.
    */
-  @CanIgnoreReturnValue
-  public static <REQUEST, RESPONSE>
-      InstrumenterBuilder<REQUEST, RESPONSE> setRpcServerExceptionEventExtractor(
-          InstrumenterBuilder<REQUEST, RESPONSE> builder) {
+  public static <REQUEST> void setRpcServerExceptionEventExtractor(
+      InstrumenterBuilder<REQUEST, ?> builder) {
     Experimental.setExceptionEventExtractor(
         builder,
         (logRecordBuilder, context, request) -> {
           logRecordBuilder.setEventName("rpc.server.call.exception");
           logRecordBuilder.setSeverity(Severity.ERROR);
         });
-    return builder;
   }
 
   private RpcExceptionEventExtractors() {}
