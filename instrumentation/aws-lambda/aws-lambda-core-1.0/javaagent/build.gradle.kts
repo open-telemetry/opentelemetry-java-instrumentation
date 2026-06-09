efba62a177e63cb37fa7531f0d72cd6266bb9c12
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-06-09 02:45:43 -0700

    Emit FaaS exceptions as logs under the opt-in preview (#18892)

--- instrumentation/aws-lambda/aws-lambda-core-1.0/javaagent/build.gradle.kts
+++ instrumentation/aws-lambda/aws-lambda-core-1.0/javaagent/build.gradle.kts
@@ -20,11 +20,25 @@ dependencies {
   testImplementation(project(":instrumentation:aws-lambda:aws-lambda-core-1.0:testing"))
 }
 
-tasks.test {
-  // required on jdk17
-  jvmArgs("--add-opens=java.base/java.lang=ALL-UNNAMED")
-  jvmArgs("--add-opens=java.base/java.util=ALL-UNNAMED")
-  jvmArgs("-XX:+IgnoreUnrecognizedVMOptions")
+tasks {
+  withType<Test>().configureEach {
+    // required on jdk17
+    jvmArgs("--add-opens=java.base/java.lang=ALL-UNNAMED")
+    jvmArgs("--add-opens=java.base/java.util=ALL-UNNAMED")
+    jvmArgs("-XX:+IgnoreUnrecognizedVMOptions")
 
-  systemProperty("collectMetadata", otelProps.collectMetadata)
+    systemProperty("collectMetadata", otelProps.collectMetadata)
+  }
+
+  val testExceptionSignalLogs by registering(Test::class) {
+    testClassesDirs = sourceSets.test.get().output.classesDirs
+    classpath = sourceSets.test.get().runtimeClasspath
+
+    jvmArgs("-Dotel.semconv.exception.signal.preview=logs")
+    systemProperty("metadataConfig", "otel.semconv.exception.signal.preview=logs")
+  }
+
+  check {
+    dependsOn(testExceptionSignalLogs)
+  }
 }
