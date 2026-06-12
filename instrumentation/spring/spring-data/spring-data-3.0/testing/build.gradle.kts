7a342393906c7a215240f0b0731825fe6a3a828f
Lauri Tulmin <ltulmin@splunk.com>
2026-06-12 13:46:23 +0300

    delte unneeded latest dep limit

--- instrumentation/spring/spring-data/spring-data-3.0/testing/build.gradle.kts
+++ instrumentation/spring/spring-data/spring-data-3.0/testing/build.gradle.kts
@@ -20,9 +20,6 @@ dependencies {
   testImplementation("org.hsqldb:hsqldb:2.0.0")
   testImplementation("com.h2database:h2:1.4.197")
   testImplementation("io.r2dbc:r2dbc-h2:1.0.0.RELEASE")
-
-  // latest Spring Data is not yet compatible with Spring Framework 7.
-  latestDepTestLibrary("org.springframework:spring-test:6.+") // related dependency
 }
 
 otelJava {
