e4505092b1bf59deb63239b2f3b1e3c1b475442a
Lauri Tulmin <ltulmin@splunk.com>
2026-06-15 14:37:04 +0300

    Disable hbase tests on openj9

--- instrumentation/hbase-client-2.0/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/hbase/client/v2_0/HbaseClient20Test.java
+++ instrumentation/hbase-client-2.0/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/hbase/client/v2_0/HbaseClient20Test.java
@@ -8,8 +8,10 @@ package io.opentelemetry.javaagent.instrumentation.hbase.client.v2_0;
 import io.opentelemetry.instrumentation.testing.junit.AgentInstrumentationExtension;
 import io.opentelemetry.instrumentation.testing.junit.InstrumentationExtension;
 import io.opentelemetry.javaagent.instrumentation.hbase.testing.AbstractHbaseTest;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
+@DisabledIfSystemProperty(named = "java.vm.name", matches = ".*OpenJ9.*")
 class HbaseClient20Test extends AbstractHbaseTest {
 
   @RegisterExtension
