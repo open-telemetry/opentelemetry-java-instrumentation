7b37832a89b75266e968dbd61cdcca83e8f246ce
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-06-23 10:36:03 -0700

    Deprecate jedis-1.4 module and disable under v3-preview

--- instrumentation/jedis/jedis-1.4/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/jedis/v1_4/JedisInstrumentationModule.java
+++ instrumentation/jedis/jedis-1.4/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/jedis/v1_4/JedisInstrumentationModule.java
@@ -10,16 +10,24 @@ import static java.util.Arrays.asList;
 import static net.bytebuddy.matcher.ElementMatchers.not;
 
 import com.google.auto.service.AutoService;
+import io.opentelemetry.javaagent.bootstrap.internal.AgentCommonConfig;
 import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import java.util.List;
 import net.bytebuddy.matcher.ElementMatcher;
 
+@Deprecated // disabled under v3-preview; to be removed in 3.0
 @AutoService(InstrumentationModule.class)
 public class JedisInstrumentationModule extends InstrumentationModule {
 
   public JedisInstrumentationModule() {
     super("jedis", "jedis-1.4");
+  }
+
+  @Override
+  public boolean defaultEnabled() {
+    // disabled under v3-preview, where jedis 1.x is no longer supported
+    return super.defaultEnabled() && !AgentCommonConfig.get().isV3Preview();
   }
 
   @Override
