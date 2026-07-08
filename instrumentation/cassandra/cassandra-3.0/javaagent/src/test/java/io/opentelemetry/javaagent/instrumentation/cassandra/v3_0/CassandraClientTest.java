cebe198639dbbbee34d397e1c62e9ac76a37d127
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-07-08 12:01:24 -0700

    Port Cassandra batch operation expectations

--- instrumentation/cassandra/cassandra-3.0/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/cassandra/v3_0/CassandraClientTest.java
+++ instrumentation/cassandra/cassandra-3.0/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/cassandra/v3_0/CassandraClientTest.java
@@ -395,6 +395,8 @@ class CassandraClientTest {
                 .queryText("INSERT INTO batch_test.records (id, num) values (?, ?)")
                 .querySummary("BATCH INSERT batch_test.records")
                 .batchSize(2)
+                .operationName("BATCH INSERT")
+                .collectionName("batch_test.records")
                 .build()),
         argumentSet(
             "twoDifferentOperations",
@@ -415,6 +417,8 @@ class CassandraClientTest {
                     "INSERT INTO batch_test.records (id, num) values (4, ?); UPDATE batch_test.records SET num = ? WHERE id = ?")
                 .querySummary("BATCH")
                 .batchSize(2)
+                .operationName("BATCH")
+                .collectionName("batch_test.records")
                 .build()));
   }
 
