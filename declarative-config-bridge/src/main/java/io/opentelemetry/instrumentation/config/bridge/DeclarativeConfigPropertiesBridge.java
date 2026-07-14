041f34600b963efaa53c7bc16bbce4b8d889c9e8
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-07-13 18:06:07 -0700

    Fix declarative config bridge javadoc

--- declarative-config-bridge/src/main/java/io/opentelemetry/instrumentation/config/bridge/DeclarativeConfigPropertiesBridge.java
+++ declarative-config-bridge/src/main/java/io/opentelemetry/instrumentation/config/bridge/DeclarativeConfigPropertiesBridge.java
@@ -27,26 +27,30 @@ import javax.annotation.Nullable;
  * <p>Only properties starting with "otel.instrumentation." are resolved. Others return null (or
  * default value if provided).
  *
- * @deprecated Use {@link ConfigPropertiesBackedConfigProvider} when bridging {@link
- *     ConfigProperties} into declarative configuration. This class will be removed in 3.0.
- *     <p>To resolve:
- *     <ul>
- *       <li>"otel.instrumentation" refers to the ".instrumentation.java" node
- *       <li>The portion of the property after "otel.instrumentation." is split into segments based
- *           on ".".
- *       <li>For each N-1 segment, we walk down the tree to find the relevant leaf {@link
- *           DeclarativeConfigProperties}.
- *       <li>We extract the property from the resolved {@link DeclarativeConfigProperties} using the
- *           last segment as the property key.
- *     </ul>
- *     <p>For example, given the following YAML, asking for {@code
- *     ConfigProperties#getString("otel.instrumentation.common.string_key")} yields "value":
- *     <pre>
+ * <p>To resolve:
+ *
+ * <ul>
+ *   <li>"otel.instrumentation" refers to the ".instrumentation.java" node
+ *   <li>The portion of the property after "otel.instrumentation." is split into segments based on
+ *       ".".
+ *   <li>For each N-1 segment, we walk down the tree to find the relevant leaf {@link
+ *       DeclarativeConfigProperties}.
+ *   <li>We extract the property from the resolved {@link DeclarativeConfigProperties} using the
+ *       last segment as the property key.
+ * </ul>
+ *
+ * <p>For example, given the following YAML, asking for {@code
+ * ConfigProperties#getString("otel.instrumentation.common.string_key")} yields "value":
+ *
+ * <pre>
  *   instrumentation:
  *     java:
  *       common:
  *         string_key: value
  * </pre>
+ *
+ * @deprecated Use {@link ConfigPropertiesBackedConfigProvider} when bridging {@link
+ *     ConfigProperties} into declarative configuration. This class will be removed in 3.0.
  */
 @Deprecated // will be removed in 3.0
 final class DeclarativeConfigPropertiesBridge implements ConfigProperties {
