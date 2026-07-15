805db774042343899df567d59fb71dbe944c0946
YaoYingLong <906271196@qq.com>
2026-07-15 22:39:36 +0800

    feat: add support for hbase-client 1.4 (#19087)
    Co-authored-by: Lauri Tulmin <ltulmin@splunk.com>

---
+++ instrumentation/hbase/hbase-client-common-1.4/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/hbase/client/common/HbaseAttributesGetter.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.hbase.client.common;
+
+import io.opentelemetry.instrumentation.api.incubator.semconv.db.DbClientAttributesGetter;
+import io.opentelemetry.semconv.incubating.DbIncubatingAttributes.DbSystemNameIncubatingValues;
+import java.net.InetSocketAddress;
+import javax.annotation.Nullable;
+import org.apache.hadoop.hbase.TableName;
+
+final class HbaseAttributesGetter implements DbClientAttributesGetter<HbaseRequest, Void> {
+
+  @Nullable
+  @Override
+  public String getDbSystemName(HbaseRequest hbaseRequest) {
+    return DbSystemNameIncubatingValues.HBASE;
+  }
+
+  @Nullable
+  @Override
+  public String getDbNamespace(HbaseRequest hbaseRequest) {
+    TableName tableName = hbaseRequest.getTableName();
+    return tableName == null ? null : tableName.getNamespaceAsString();
+  }
+
+  @Nullable
+  @Override
+  public String getDbCollectionName(HbaseRequest hbaseRequest) {
+    TableName tableName = hbaseRequest.getTableName();
+    return tableName == null ? null : tableName.getNameAsString();
+  }
+
+  @Nullable
+  @Override
+  // Old database semconv still uses db.name, so we must implement the deprecated hook.
+  @SuppressWarnings("deprecation")
+  public String getDbName(HbaseRequest hbaseRequest) {
+    TableName tableName = hbaseRequest.getTableName();
+    return tableName == null ? null : tableName.getNameAsString();
+  }
+
+  @Nullable
+  @Override
+  // Old database semconv still uses db.user, so we must implement the deprecated hook.
+  @SuppressWarnings("deprecation")
+  public String getUser(HbaseRequest hbaseRequest) {
+    return hbaseRequest.getUser();
+  }
+
+  @Nullable
+  @Override
+  public String getDbQueryText(HbaseRequest hbaseRequest) {
+    return null;
+  }
+
+  @Nullable
+  @Override
+  public String getDbOperationName(HbaseRequest hbaseRequest) {
+    return hbaseRequest.getOperation();
+  }
+
+  @Nullable
+  @Override
+  public Long getDbOperationBatchSize(HbaseRequest hbaseRequest) {
+    return hbaseRequest.getOperationBatchSize();
+  }
+
+  @Nullable
+  @Override
+  public InetSocketAddress getNetworkPeerInetSocketAddress(
+      HbaseRequest request, @Nullable Void unused) {
+    if (request.getHost() == null || request.getPort() == null) {
+      return null;
+    }
+    return InetSocketAddress.createUnresolved(request.getHost(), request.getPort());
+  }
+
+  @Nullable
+  @Override
+  public String getServerAddress(HbaseRequest request) {
+    return request.getHost();
+  }
+
+  @Nullable
+  @Override
+  public Integer getServerPort(HbaseRequest request) {
+    return request.getPort();
+  }
+}
