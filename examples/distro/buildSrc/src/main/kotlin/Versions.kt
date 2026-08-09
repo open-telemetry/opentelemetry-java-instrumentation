b456e5cd5717a4a3bcbb29faf426be5315600b47
otelbot <197425009+otelbot@users.noreply.github.com>
2026-08-09 16:03:07 +0000

    Update the OpenTelemetry SDK version to 1.65.0

--- examples/distro/buildSrc/src/main/kotlin/Versions.kt
+++ examples/distro/buildSrc/src/main/kotlin/Versions.kt
@@ -1,5 +1,5 @@
 // this line is managed by .github/scripts/update-sdk-version.sh
-const val opentelemetrySdkVersion = "1.64.0"
+const val opentelemetrySdkVersion = "1.65.0"
 
 // these lines are managed by .github/scripts/update-version.sh
 const val opentelemetryJavaagentVersion = "2.31.0-SNAPSHOT"
