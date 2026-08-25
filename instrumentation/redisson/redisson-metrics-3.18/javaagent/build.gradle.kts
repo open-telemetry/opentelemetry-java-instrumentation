200ed04011fef82dbcbab4905471571654363c7c
YaoYingLong <906271196@qq.com>
2026-08-25 20:46:24 +0800

    Add Redisson metrics instrumentation for 3.18+ (#19634)

--- instrumentation/redisson/redisson-metrics-3.18/javaagent/build.gradle.kts
+++ instrumentation/redisson/redisson-metrics-3.18/javaagent/build.gradle.kts
@@ -6,18 +6,18 @@ muzzle {
   pass {
     group.set("org.redisson")
     module.set("redisson")
-    versions.set("[3.26.0,)")
+    versions.set("[3.18.0,3.26.0)")
     assertInverse.set(true)
   }
 }
 
 dependencies {
+  library("org.redisson:redisson:3.18.0")
+
   implementation(project(":instrumentation:redisson:redisson-metrics-common-3.18:javaagent"))
-
-  library("org.redisson:redisson:3.26.0")
-
+  testInstrumentation(project(":instrumentation:redisson:redisson-metrics-3.26:javaagent"))
   testImplementation(project(":instrumentation:redisson:redisson-metrics-common-3.18:testing"))
-  testInstrumentation(project(":instrumentation:redisson:redisson-metrics-3.18:javaagent"))
+  latestDepTestLibrary("org.redisson:redisson:3.25.+") // see redisson-metrics-3.26 module
 }
 
 tasks {
