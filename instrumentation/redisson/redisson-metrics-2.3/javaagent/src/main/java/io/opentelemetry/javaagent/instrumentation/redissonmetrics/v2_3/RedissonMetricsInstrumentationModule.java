ae813b4cadf0024d4df1d876ae14a9c81ad5daba
YaoYingLong <906271196@qq.com>
2026-09-07 18:19:14 +0800

    Add Redisson 2.3-3.17 connection pool metrics (#19152)
    Co-authored-by: Lauri Tulmin <tulmin@gmail.com>

--- instrumentation/redisson/redisson-metrics-2.3/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/redissonmetrics/v2_3/RedissonMetricsInstrumentationModule.java
+++ instrumentation/redisson/redisson-metrics-2.3/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/redissonmetrics/v2_3/RedissonMetricsInstrumentationModule.java
@@ -3,11 +3,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.opentelemetry.javaagent.instrumentation.redissonmetrics.v3_18;
+package io.opentelemetry.javaagent.instrumentation.redissonmetrics.v2_3;
 
 import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.hasClassesNamed;
 import static java.util.Arrays.asList;
-import static net.bytebuddy.matcher.ElementMatchers.not;
 
 import com.google.auto.service.AutoService;
 import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
@@ -20,15 +19,16 @@ import net.bytebuddy.matcher.ElementMatcher;
 public class RedissonMetricsInstrumentationModule extends InstrumentationModule {
 
   public RedissonMetricsInstrumentationModule() {
-    super("redisson-metrics", "redisson-metrics-3.18");
+    super("redisson-metrics", "redisson-metrics-2.3");
   }
 
   @Override
   public ElementMatcher.Junction<ClassLoader> classLoaderMatcher() {
-    // added in 3.18.0
-    return hasClassesNamed("org.redisson.misc.AsyncSemaphore")
-        // added in 3.26.0
-        .and(not(hasClassesNamed("org.redisson.connection.ConnectionsHolder")));
+    return hasClassesNamed(
+        // added in 2.3.0
+        "org.redisson.api.RFuture",
+        // removed in 3.18.0 (moved to org.redisson.misc)
+        "org.redisson.pubsub.AsyncSemaphore");
   }
 
   @Override
