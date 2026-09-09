a7e80a30c0cda54fad9b697a922df0017dd580fc
walleseve <iwa.wong+github@gmail.com>
2026-09-09 08:57:35 -0700

    Simplify Bedrock guardrail compatibility tests

--- instrumentation/aws-sdk/aws-sdk-2.2/library/src/testBedrockRuntime/java/io/opentelemetry/instrumentation/awssdk/v2_2/internal/BedrockRuntimeAttributesExtractorTest.java
+++ instrumentation/aws-sdk/aws-sdk-2.2/library/src/testBedrockRuntime/java/io/opentelemetry/instrumentation/awssdk/v2_2/internal/BedrockRuntimeAttributesExtractorTest.java
@@ -29,6 +29,8 @@ import software.amazon.awssdk.core.SdkRequest;
 import software.amazon.awssdk.core.interceptor.ExecutionAttributes;
 import software.amazon.awssdk.services.bedrockruntime.model.ConverseRequest;
 import software.amazon.awssdk.services.bedrockruntime.model.ConverseStreamRequest;
+import software.amazon.awssdk.services.bedrockruntime.model.GuardrailConfiguration;
+import software.amazon.awssdk.services.bedrockruntime.model.GuardrailStreamConfiguration;
 import software.amazon.awssdk.services.bedrockruntime.model.InvokeModelRequest;
 import software.amazon.awssdk.services.bedrockruntime.model.InvokeModelWithResponseStreamRequest;
 
@@ -78,20 +80,15 @@ class BedrockRuntimeAttributesExtractorTest {
 
   @ParameterizedTest
   @MethodSource("converseRequests")
-  void extractsGuardrailIdentifierFromConverseRequest(SdkRequest request)
-      throws ReflectiveOperationException {
+  void extractsGuardrailIdentifierFromConverseRequest(boolean streaming) {
     assumeTrue(testLatestDeps());
 
-    SdkRequest requestWithGuardrail = withGuardrailConfiguration(request);
-
-    assertThat(extract(requestWithGuardrail).get(AWS_BEDROCK_GUARDRAIL_ID))
+    assertThat(extract(LatestConverseRequests.create(streaming)).get(AWS_BEDROCK_GUARDRAIL_ID))
         .isEqualTo("guardrail-id");
   }
 
   private static Stream<Arguments> converseRequests() {
-    return Stream.of(
-        argumentSet("Converse", ConverseRequest.builder().modelId("model-id").build()),
-        argumentSet("ConverseStream", ConverseStreamRequest.builder().modelId("model-id").build()));
+    return Stream.of(argumentSet("Converse", false), argumentSet("ConverseStream", true));
   }
 
   @Test
@@ -113,27 +110,29 @@ class BedrockRuntimeAttributesExtractorTest {
     return attributes.build();
   }
 
-  private static SdkRequest withGuardrailConfiguration(SdkRequest request)
-      throws ReflectiveOperationException {
-    String configClassName =
-        request instanceof ConverseStreamRequest
-            ? "GuardrailStreamConfiguration"
-            : "GuardrailConfiguration";
-    Class<?> configClass =
-        Class.forName("software.amazon.awssdk.services.bedrockruntime.model." + configClassName);
-    Class<?> configBuilderClass =
-        Class.forName(
-            "software.amazon.awssdk.services.bedrockruntime.model." + configClassName + "$Builder");
-    Object configBuilder = configClass.getMethod("builder").invoke(null);
-    configBuilderClass
-        .getMethod("guardrailIdentifier", String.class)
-        .invoke(configBuilder, "guardrail-id");
-    configBuilderClass.getMethod("guardrailVersion", String.class).invoke(configBuilder, "1");
-    Object config = configBuilderClass.getMethod("build").invoke(configBuilder);
+  // Keep references to types introduced after the floor version isolated from floor-version runs.
+  private static final class LatestConverseRequests {
+    private static SdkRequest create(boolean streaming) {
+      if (streaming) {
+        return ConverseStreamRequest.builder()
+            .modelId("model-id")
+            .guardrailConfig(
+                GuardrailStreamConfiguration.builder()
+                    .guardrailIdentifier("guardrail-id")
+                    .guardrailVersion("1")
+                    .build())
+            .build();
+      }
+      return ConverseRequest.builder()
+          .modelId("model-id")
+          .guardrailConfig(
+              GuardrailConfiguration.builder()
+                  .guardrailIdentifier("guardrail-id")
+                  .guardrailVersion("1")
+                  .build())
+          .build();
+    }
 
-    SdkRequest.Builder requestBuilder = request.toBuilder();
-    Class<?> requestBuilderClass = Class.forName(request.getClass().getName() + "$Builder");
-    requestBuilderClass.getMethod("guardrailConfig", configClass).invoke(requestBuilder, config);
-    return requestBuilder.build();
+    private LatestConverseRequests() {}
   }
 }
