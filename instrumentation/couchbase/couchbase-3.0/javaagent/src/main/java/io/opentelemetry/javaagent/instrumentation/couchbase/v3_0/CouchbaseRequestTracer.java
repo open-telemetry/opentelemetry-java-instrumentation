18140f27dbfddb20b8b856c9d36da7de7f57a1d8
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2026-09-14 12:33:07 +0000

    Expand NullAway instrumentation batch
    Signed-off-by: Gregor Zeitlinger <gregor.zeitlinger@grafana.com>

--- instrumentation/couchbase/couchbase-3.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/couchbase/v3_0/CouchbaseRequestTracer.java
+++ instrumentation/couchbase/couchbase-3.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/couchbase/v3_0/CouchbaseRequestTracer.java
@@ -137,6 +137,7 @@ public final class CouchbaseRequestTracer implements RequestTracer {
     }
 
     @Override
+    @Nullable
     public RequestContext requestContext() {
       return requestContext;
     }
--- instrumentation/r2dbc-1.0/javaagent/build.gradle.kts
+++ instrumentation/r2dbc-1.0/javaagent/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
   id("otel.javaagent-instrumentation")
   id("otel.java-conventions")
+  id("otel.nullaway-conventions")
 }
 
 muzzle {
